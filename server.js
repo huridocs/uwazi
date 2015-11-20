8997649e708a89ed5253f41262f7715ae7ac376d
A Happy Cat <acasadotorres@gmail.com>
2015-11-20 17:12:36 +0100

    removed webpack server from the server, now will be launched as a separate process

--- server.js
+++ server.js
@@ -1,50 +1,21 @@
 require('babel-core/register')({
-	stage: 0
+  stage: 0
 });
 
 require.extensions['.scss'] = function() {
-	return;
+  return;
 };
 
 var express = require('express');
 const app = express();
 
-var webpack = require('webpack');
-var webpackMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config.js');
-
-const isDeveloping = process.env.NODE_ENV !== 'production';
-
-if (isDeveloping) {
-  const compiler = webpack(config);
-
-  app.use(webpackMiddleware(compiler, {
-    publicpath: config.output.publicpath,
-    contentBase: 'app',
-    watch: false,
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  }));
-
-  app.use(webpackHotMiddleware(compiler, {
-    path: '/__webpack_hmr'
-  }));
-}
-
 require('./app/react/server.js')(app);
 require('./app/api/api.js')(app);
 
-const port = isDeveloping ? 3000 : process.env.PORT;
+const port = process.env.PORT ? 3000 : process.env.PORT;
 app.listen(port, 'localhost', function onStart(err) {
-	if (err) {
-		console.log(err);
-	}
-	console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
 });
