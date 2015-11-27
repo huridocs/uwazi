a87b482440ce8065b86bf1b2dc0fa3aff1f1563e
daneryl <daneryl@gmail.com>
2015-11-27 11:19:36 -0500

    forced remove of weird json dump on restore.sh

--- utils/restore.sh
+++ utils/restore.sh
@@ -2,4 +2,4 @@
 curl -X DELETE http://127.0.0.1:5984/uwazi/
 curl -X PUT http://127.0.0.1:5984/uwazi/
 ./couchdb-dump.sh -r -H 127.0.0.1 -d uwazi -f dump.json
-rm dump.json-nodesign\"\"
+rm -f dump.json-nodesign\"\"
