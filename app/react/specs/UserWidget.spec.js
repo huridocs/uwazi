8e876955008d9b81e7002362ffa40e8859484bc1
daneryl <daneryl@gmail.com>
2015-11-27 09:42:40 -0500

    changed name on UserWidget spec file

---
+++ app/react/specs/UserWidget.spec.js
@@ -0,0 +1,73 @@
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import { Link } from 'react-router'
+
+import UserWidget from '../components/UserWidget.js'
+//import fetch from 'isomorphic-fetch'
+import {events} from '../utils/index'
+
+describe('Component', () => {
+
+  let component;
+
+  let fetch_mock = function(){
+      let res = new window.Response('{"username":"Iron Man"}', {
+          status: 200,
+          headers: {
+            'Content-type': 'application/json'
+        }
+      });
+
+      let promise_mock = Promise.resolve(res);
+      return promise_mock;
+  };
+
+  let fetch_rejected = () => {return Promise.reject()};
+
+  beforeEach(() => {
+    spyOn(events, 'on');
+    component = TestUtils.renderIntoDocument(<UserWidget fetch={fetch_rejected}/>);
+  })
+
+  describe('on instance', () => {
+    it('should subscribe to login event with fetchUser', () => {
+      expect(events.on).toHaveBeenCalledWith('login', component.fetchUser);
+    });
+  });
+
+  it('should render the login link', () => {
+    let loginLink = TestUtils.findRenderedDOMComponentWithTag(component, 'a');
+    expect(loginLink.textContent).toEqual('Login');
+  });
+
+  describe('When the user is loged in', () => {
+    beforeEach(() => {
+      component.setState({username: 'Thor'});
+    })
+
+    it('should render the lout link', () => {
+      let loginLink = TestUtils.findRenderedDOMComponentWithTag(component, 'a');
+      expect(loginLink.textContent).toEqual('Logout');
+    });
+
+    it('should render the username', () => {
+      expect(ReactDOM.findDOMNode(component).textContent).toMatch('Thor');
+    });
+  })
+
+  describe('when fething user', () => {
+    beforeEach(() => {
+      component = TestUtils.renderIntoDocument(<UserWidget fetch={fetch_mock}/>);
+    })
+
+    it('should set the username on the state and render it', (done) => {
+      component.fetchUser()
+      .then(() => {
+        expect(ReactDOM.findDOMNode(component).textContent).toMatch('Iron Man');
+        done();
+      });
+    })
+  });
+
+});
