2e48bbad9521cfae3f337e5dac0a7622f0635970
daneryl <daneryl@gmail.com>
2015-11-30 10:21:52 -0500

    created karma config for integration server

---
+++ karma_ci.conf.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    browsers: [ 'Firefox' ], // Chrome, PhantomJS
+    singleRun: true, //just run once by default
+    frameworks: [ 'jasmine' ], //use the mocha test framework
+    files: [
+      'tests.webpack.js' //just load this file
+    ],
+    plugins: [ 'karma-firefox-launcher', 'karma-jasmine',
+      'karma-sourcemap-loader', 'karma-webpack', 'karma-coverage'
+    ],
+    preprocessors: {
+      'tests.webpack.js': [ 'webpack', 'sourcemap' ] //preprocess with webpack and our sourcemap loader
+    },
+    reporters: [ 'dots', 'coverage' ], //report results in this format
+    webpack: { //kind of a copy of your webpack config
+      devtool: 'eval', //just do inline source maps instead of the default
+      module: {
+        loaders: [
+          { test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+              stage: 0,
+              cacheDirectory: true
+            }
+          },
+          {
+            test: /\.scss$/,
+            loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
+            include: path.join(__dirname, 'app')
+          },  {
+            test: /\.(jpe?g|png|eot|woff|ttf|gif|svg)(\?.*)?$/i,
+            loader: 'file-loader',
+            include: path.join(__dirname, 'app')
+          }
+        ],
+        postLoaders: [ { //delays coverage til after tests are run, fixing transpiled source coverage error
+            test: /\.js$/,
+            exclude: /(spec|node_modules|bower_components)\//,
+            loader: 'istanbul-instrumenter' } ]
+      }
+    },
+    webpackServer: {
+      noInfo: true //please don't spam the console when running in karma!
+    },
+    coverageReporter: {
+      type: 'html', //produces a html document after code is run
+      dir: 'coverage/' //path to created html doc
+    }
+  });
+};
