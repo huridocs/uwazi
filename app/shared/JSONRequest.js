b91eff38f5da1892adf70c980ca8353daef498a7
daneryl <daneryl@gmail.com>
2015-12-11 10:01:53 -0500

    changed JSONRequest to shared folder

---
+++ app/shared/JSONRequest.js
@@ -0,0 +1,37 @@
+import fetch from 'isomorphic-fetch';
+
+let _fetch = (url, data, method) => {
+
+  let response;
+
+  return fetch(url, {
+    method: method,
+    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    credentials: 'same-origin',
+    body: JSON.stringify(data)
+  })
+  .then((res) => {
+    response = res;
+    return res.json()
+  })
+  .then((json) => {
+    return {
+      json: json,
+      status: response.status
+    }
+  });
+}
+
+export default {
+  post: (url, data) => {
+    return _fetch(url, data, 'POST');
+  },
+
+  get: (url, data) => {
+    return _fetch(url, data, 'GET');
+  },
+
+  delete: (url, data) => {
+    return _fetch(url, data, 'DELETE');
+  }
+}
