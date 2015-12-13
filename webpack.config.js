7493b91e3913fa0c0ca0b6f1db0f482d5ee336eb
daneryl <daneryl@gmail.com>
2015-12-12 19:14:29 -0500

    webpack config to allow import of css files

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   devtool: '#source-map',
@@ -12,7 +13,9 @@ module.exports = {
     path: path.join(__dirname, '/dist/'),
     filename: 'bundle.js'
   },
-  plugins: [],
+  plugins: [
+    // new ExtractTextPlugin("styles.css")
+  ],
   module: {
     loaders: [{
       test: /\.js?$/,
@@ -22,11 +25,18 @@ module.exports = {
         stage: 0,
         cacheDirectory: true
       }
-    }, {
+    },
+    {
       test: /\.scss$/,
       loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
       include: path.join(__dirname, 'app')
-    },  {
+    },
+    {
+      test: /\.css$/,
+      loader: "style-loader!css-loader",
+      include: [path.join(__dirname, 'app'), path.join(__dirname, 'node_modules')]
+    },
+    {
       test: /\.(jpe?g|png|eot|woff|ttf|gif|svg)(\?.*)?$/i,
       loader: 'file-loader',
       include: path.join(__dirname, 'app')
