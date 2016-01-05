1dcafa206511208b80c98d36a36b5138d469e1e2
daneryl <daneryl@gmail.com>
2016-01-05 12:42:15 -0500

    added onChange support for SelectField

--- app/react/components/Form/fields/specs/SelectField.spec.js
+++ app/react/components/Form/fields/specs/SelectField.spec.js
@@ -8,18 +8,33 @@ import SelectField from '../SelectField.js'
 describe('SelectField', () => {
 
   let component;
+  let options = [
+    {label: '1', value: '1'},
+    {label: '2', value: '2'}
+  ];
 
   beforeEach(() => {
-    let options = [
-      {label: '1', value: '1'},
-      {label: '2', value: '2'}
-    ];
     component = TestUtils.renderIntoDocument(<SelectField defaultValue='2' options={options}/>);
   });
 
   describe('value()', () => {
     it('should return the select value', () => {
       expect(component.value()).toBe('2');
+    });
+  });
+
+  describe('onChange()', () => {
+    it('should execute onChange callback passed in props', () => {
+      let change_executed = false;
+      let change = () => {
+        change_executed = true;
+      };
+
+      component = TestUtils.renderIntoDocument(<SelectField defaultValue='2' options={options} onChange={change}/>);
+      TestUtils.Simulate.change(component.field);
+
+      expect(change_executed).toBe(true);
+
     });
   });
 
