f3751e163a3bb45f8987dd0bee8b55292dba9030
daneryl <daneryl@gmail.com>
2016-01-26 17:44:49 -0500

    restore.sh now removes uwazi index from elasticsearch also

--- utils/restore.sh
+++ utils/restore.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+curl -XDELETE 'http://localhost:9200/uwazi/'
 curl -X DELETE http://127.0.0.1:5984/uwazi/
 curl -X PUT http://127.0.0.1:5984/uwazi/
 ./couchdb-dump.sh -r -H 127.0.0.1 -d uwazi -f dump.json
