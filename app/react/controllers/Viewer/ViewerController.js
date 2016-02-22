6d48f096bc2fd1166450d78db95a21b5e65e9110
daneryl <daneryl@gmail.com>
2016-02-22 19:09:30 +0100

    fake references can be created on a document

--- app/react/controllers/Viewer/ViewerController.js
+++ app/react/controllers/Viewer/ViewerController.js
@@ -5,6 +5,7 @@ import api from '../../utils/singleton_api'
 import RouteHandler from '../App/RouteHandler'
 import './scss/viewer.scss'
 import LogoIcon from '../../components/Logo/LogoIcon.js'
+import wrap from 'wrap-range-text'
 
 
 class ViewerController extends RouteHandler {
@@ -17,12 +18,37 @@ class ViewerController extends RouteHandler {
   };
 
   static emptyState(){
-    return {value:{pages:[], css:[]}, showmenu: false};
+    return {value:{pages:[], css:[]}, showmenu: false, textSelected: false, textSelectedTop: 0};
   };
 
   toggleMenu = () => {this.setState({showmenu: !this.state.showmenu})};
 
   closeMenu = () => {this.setState({showmenu: false})};
+
+  textSelection = () => {
+    if(window.getSelection().toString() === ''){
+      return this.setState({textSelected: false, openModal: false});
+    }
+
+    let range = window.getSelection().getRangeAt(0);
+
+    this.setState({textSelected: true, textSelectedTop: range.getClientRects()[0].top + this.pagesContainer.scrollTop-60});
+  };
+  
+  openModal = () => {
+    this.setState({textSelected: false, openModal:true});
+  };
+
+  closeModal = () => {
+    this.setState({openModal:false});
+  };
+
+  createRef = () => {
+    let wrapper = document.createElement('span');
+    wrapper.classList.add('reference');
+    wrap(wrapper, this.range);
+    this.closeModal();
+  };
 
   render = () => {
     let menuClass = 'navbar-collapse collapse';
@@ -30,6 +56,33 @@ class ViewerController extends RouteHandler {
     if(this.state.showmenu) {
       menuClass += ' in';
     }
+
+    let display = "none";
+    if(this.state.textSelected){
+      display = "block";
+    };
+
+    let textSelectionLinkStyles = {
+      position:"absolute",
+      display: display,
+      'zIndex': "9999",
+      top: this.state.textSelectedTop
+    };
+
+    let displayModal = "none";
+    if(this.state.openModal){
+      displayModal = "block";
+    };
+
+    let modalStyles = {
+      position:"absolute",
+      display: displayModal,
+      'zIndex': "9999",
+      top: this.state.textSelectedTop,
+      background:'red',
+      width: '100%',
+      height: '50px'
+    };
 
     return (
       <div className="viewer">
@@ -64,12 +117,22 @@ class ViewerController extends RouteHandler {
         <div className='container-fluid contents-wrapper'>
           <div className="row panels-layout viewer__pages">
             <div className="col-xs-12 col-sm-8 panels-layout__panel no-padding active">
-              <div className="panel-content pages">
-                {this.state.value.pages.map((page, index) => {
-                  let html = {__html: page}
-                  let id = 'pf'+index;
-                  return <div id={id} key={index} dangerouslySetInnerHTML={html} ></div>
-                })}
+              <div className="panel-content" ref={(ref) => this.pagesContainer = ref}>
+                <a href="#" style={textSelectionLinkStyles} onClick={this.openModal}>text selected</a>
+
+                <div style={modalStyles}>
+                  <a href="#" onClick={this.closeModal}>close</a>
+                  <br/>MODAL !!
+                  <a href="#" onClick={this.createRef}>OK</a>
+                </div>
+
+                <div className="pages" onMouseUp={this.textSelection}>
+                  {this.state.value.pages.map((page, index) => {
+                    let html = {__html: page}
+                    let id = 'pf'+index;
+                    return <div id={id} key={index} dangerouslySetInnerHTML={html} ></div>
+                  })}
+                </div>
               </div>
             </div>
             <div className="col-xs-12 col-sm-4 panels-layout__panel no-padding">
