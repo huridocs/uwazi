14bf7e1fdeec117ac2eda068c4c993c3d05c39b1
A Happy Cat <acasadotorres@gmail.com>
2016-02-23 15:09:11 +0100

    viewer metadata styles

--- app/react/controllers/Viewer/ViewerController.js
+++ app/react/controllers/Viewer/ViewerController.js
@@ -89,20 +89,60 @@ class ViewerController extends RouteHandler {
             </div>
             <div className={panelClass}>
               <div className="panel-content">
-              <ul className="panel-tools">
-                <li>
-                  <a href="#" className=""><i className="fa fa-bookmark-o"></i></a>
-                </li>
-                <li>
-                  <a href="#" className=""><i className="fa fa-search"></i></a>
-                </li>
-                <li>
-                  <a href="#" className=""><i className="glyphicon glyphicon-text-size"></i></a>
-                </li>
-                <li>
-                  <a href="#" className=""><i className="fa fa-cloud-download"></i></a>
-                </li>
-              </ul>
+                <ul className="panel-tools">
+                  <li>
+                    <a href="#" className=""><i className="fa fa-bookmark-o"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" className=""><i className="fa fa-search"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" className=""><i className="glyphicon glyphicon-text-size"></i></a>
+                  </li>
+                  <li>
+                    <a href="#" className=""><i className="fa fa-cloud-download"></i></a>
+                  </li>
+                </ul>
+                <div className="document-metadata">
+                  <h1>003/12 Peter Joseph Chacha v The United Republic ofTanzania</h1>
+                  <h2 className="toc">Table of contents</h2>
+                  <ul>
+                    <li>
+                      <a href="#">Summary of alledged facts</a>
+                      <ul>
+                        <li><a href="#">Complaints submission of admisibility</a></li>
+                        <li><a href="#">The african comitee analisys</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#">Decission on admissibility</a></li>
+                    <li><a href="#">The complaint</a></li>
+                  </ul>
+                  <h2 className="metadata">Document properties (metadata)</h2>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Country</td>
+                        <td>Kenya</td>
+                      </tr>
+                      <tr>
+                        <td>Date</td>
+                        <td>09/10/2015</td>
+                      </tr>
+                      <tr>
+                        <td>Language</td>
+                        <td>EN - ES</td>
+                      </tr>
+                      <tr>
+                        <td>Type</td>
+                        <td>Decission</td>
+                      </tr>
+                      <tr>
+                        <td>Mechanism</td>
+                        <td>African Comittee of Experts on the Rights and Welfare of the Child</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
