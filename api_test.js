416c3a4fa54f747df14617cba46737b93d4123a5
daneryl <daneryl@gmail.com>
2016-04-18 20:04:12 -0500

    new and more flexible jasmine reporter for server specs

--- api_test.js
+++ api_test.js
@@ -13,23 +13,24 @@ require('babel-core/register')({
 
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
-var reporters = require('jasmine-reporters');
+var SpecReporter = require('jasmine-spec-reporter');
 
 var db_config = require('./app/api/config/database.js');
 db_config.db_url = db_config.development;
 
 jasmine.loadConfig({
-    spec_dir: 'app/',
-    spec_files: [
-        'api/**/*[sS]pec.js',
-        'shared/**/*[sS]pec.js'
-    ]
+  spec_dir: 'app/',
+  spec_files: [
+    'api/**/*[sS]pec.js',
+    'shared/**/*[sS]pec.js'
+  ]
 });
 
-jasmine.addReporter(new reporters.TerminalReporter({
-  verbosity: 2,
-  color: true,
-  showStack: true
+jasmine.addReporter(new SpecReporter({
+  displayStacktrace: 'summary',
+  displaySuccessfulSpec: false,
+  displayFailedSpec: false,
+  displaySpecDuration: true
 }));
 
 jasmine.execute();
