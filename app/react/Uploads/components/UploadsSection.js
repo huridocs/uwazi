4539687e8eac540d51415fa1422fe342a2483983
daneryl <daneryl@gmail.com>
2016-05-01 10:42:04 -0500

    UploadsSection reposition when form panel opens

---
+++ app/react/Uploads/components/UploadsSection.js
@@ -0,0 +1,42 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+import UploadBox from 'app/Uploads/components/UploadBox';
+import UploadsList from 'app/Uploads/components/UploadsList';
+import UploadsFormPanel from 'app/Uploads/components/UploadsFormPanel';
+
+export class UploadsSection extends Component {
+  render() {
+    let className = 'document-viewer';
+    if (this.props.panelIsOpen) {
+      className = 'document-viewer with-panel';
+    }
+    return (
+      <div>
+        <main className={'col-sm-8 col-sm-offset-2 ' + className}>
+          <UploadBox />
+          <UploadsList />
+        </main>
+        <UploadsFormPanel />
+      </div>
+    );
+  }
+}
+
+UploadsSection.propTypes = {
+  panelIsOpen: PropTypes.bool,
+  targetDocument: PropTypes.bool
+};
+
+UploadsSection.contextTypes = {
+  store: PropTypes.object
+};
+
+const mapStateToProps = (state) => {
+  let uiState = state.uploads.uiState.toJS();
+  return {
+    panelIsOpen: !!uiState.documentBeingEdited
+  };
+};
+
+export default connect(mapStateToProps)(UploadsSection);
