d5a43438b1cae37af91ac233fa169f36b8fb1009
daneryl <daneryl@gmail.com>
2016-05-01 12:09:20 -0500

    uploadspanel shows the document form

--- app/react/Uploads/components/UploadsFormPanel.js
+++ app/react/Uploads/components/UploadsFormPanel.js
@@ -1,7 +1,9 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
+import Immutable from 'immutable';
 
 import SidePanel from 'app/Layout/SidePanel';
+import DocumentForm from 'app/Uploads/components/DocumentForm';
 
 export class UploadsFormPanel extends Component {
   render() {
@@ -9,19 +11,30 @@ export class UploadsFormPanel extends Component {
     return (
       <SidePanel {...sidePanelprops}>
         <h1>Form</h1>
+        <DocumentForm
+          initialValues={this.props.document.toJS()}
+          templates={this.props.templates.toJS()}
+          thesauris={this.props.thesauris.toJS()}
+        />
       </SidePanel>
     );
   }
 }
 
 UploadsFormPanel.propTypes = {
-  open: PropTypes.bool
+  open: PropTypes.bool,
+  document: PropTypes.object,
+  templates: PropTypes.object,
+  thesauris: PropTypes.object
 };
 
 const mapStateToProps = (state) => {
   let uiState = state.uploads.uiState;
   return {
-    open: typeof uiState.get('documentBeingEdited') === 'string'
+    open: typeof uiState.get('documentBeingEdited') === 'string',
+    templates: state.uploads.templates,
+    thesauris: state.uploads.thesauris,
+    document: state.uploads.documents.find(doc => uiState.get('documentBeingEdited') === doc.get('_id')) || Immutable.fromJS({})
   };
 };
 
