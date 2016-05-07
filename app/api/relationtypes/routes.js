da2e66caac121a747a81ae814d5ea78b9593d7cb
A Happy Cat <acasadotorres@gmail.com>
2016-05-07 14:05:32 +0200

    relationtypes outes

--- app/api/relationtypes/routes.js
+++ app/api/relationtypes/routes.js
@@ -1,10 +1,24 @@
+import relationtypes from 'api/relationtypes/relationtypes';
+
 export default app => {
   app.post('/api/relationtypes', (req, res) => {
+    relationtypes.save(req.body)
+    .then(res.json);
   });
 
   app.get('/api/relationtypes', (req, res) => {
+    if (req.query._id) {
+      relationtypes.getById(req.query._id)
+      .then(res.json);
+      return;
+    }
+
+    relationtypes.getAll()
+    .then(res.json);
   });
 
   app.delete('/api/relationtypes', (req, res) => {
+    relationtypes.delete(req.query._id)
+    .then(res.json);
   });
-}
+};
---
+++ app/api/relationtypes/specs/routes.spec.js
@@ -0,0 +1,66 @@
+import relationtypesRoutes from 'api/relationtypes/routes.js';
+import database from 'api/utils/database.js';
+import instrumentRoutes from 'api/utils/instrumentRoutes';
+import relationtypes from 'api/relationtypes/relationtypes';
+import {catchErrors} from 'api/utils/jasmineHelpers';
+
+describe('relationtypes routes', () => {
+  let routes;
+  let mockRequest = new Promise((resolve) => resolve({relationtypes: 'response'}));
+
+  beforeEach(() => {
+    routes = instrumentRoutes(relationtypesRoutes);
+  });
+
+  describe('GET', () => {
+    it('should ask relationtypes for all documents', (done) => {
+      spyOn(relationtypes, 'getAll').and.returnValue(mockRequest);
+      routes.get('/api/relationtypes', {query: {}})
+      .then((response) => {
+        expect(relationtypes.getAll).toHaveBeenCalled();
+        expect(response).toEqual({relationtypes: 'response'});
+        done();
+      })
+      .catch(catchErrors(done));
+    });
+
+    describe('when passing id', () => {
+      it('should ask for the specific relationtype', (done) => {
+        spyOn(relationtypes, 'getById').and.returnValue(mockRequest);
+        routes.get('/api/relationtypes', {query: {_id: 'someId'}})
+        .then((response) => {
+          expect(relationtypes.getById).toHaveBeenCalledWith('someId');
+          expect(response).toEqual({relationtypes: 'response'});
+          done();
+        })
+        .catch(catchErrors(done));
+      });
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should delete the relationtype', (done) => {
+      spyOn(relationtypes, 'delete').and.returnValue(mockRequest);
+      routes.delete('/api/relationtypes', {query: {_id: 'someId'}})
+      .then((response) => {
+        expect(relationtypes.delete).toHaveBeenCalledWith('someId');
+        expect(response).toEqual({relationtypes: 'response'});
+        done();
+      })
+      .catch(catchErrors(done));
+    });
+  });
+
+  describe('POST', () => {
+    it('should save the relationtype', (done) => {
+      spyOn(relationtypes, 'save').and.returnValue(mockRequest);
+      routes.post('/api/relationtypes', {body: {name: 'my new template'}})
+      .then((response) => {
+        expect(relationtypes.save).toHaveBeenCalledWith({name: 'my new template'});
+        expect(response).toEqual({relationtypes: 'response'});
+        done();
+      })
+      .catch(catchErrors(done));
+    });
+  });
+});
