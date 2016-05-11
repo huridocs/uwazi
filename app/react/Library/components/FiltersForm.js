31a15df0a8ae60dec7d86a3d91896c936f37f360
A Happy Cat <acasadotorres@gmail.com>
2016-05-11 17:10:15 +0200

    rendering messages when there are no filters

--- app/react/Library/components/FiltersForm.js
+++ app/react/Library/components/FiltersForm.js
@@ -10,41 +10,68 @@ import {searchDocuments} from 'app/Library/actions/libraryActions';
 export class FiltersForm extends Component {
 
   render() {
+    let fields = this.props.fields.toJS();
+    console.log(fields);
     return (
-      <Form model="search" id="filtersForm" onSubmit={this.props.searchDocuments}>
-      {this.props.fields.toJS().map((property, index) => {
-        if (property.type === 'select') {
+      <div className="filters-box">
+        <Form model="search" id="filtersForm" onSubmit={this.props.searchDocuments}>
+        {fields.map((property, index) => {
+          if (property.type === 'select') {
+            return (
+              <FormGroup key={index}>
+                <SelectField model={`search.filters.${property.name}`} >
+                  <ul className="search__filter">
+                    <li>
+                      {property.label}
+                      {property.required ? <span className="required">*</span> : ''}
+                      <figure className="switcher switcher-active"></figure>
+                    </li>
+                    <li className="wide">
+                      <Select options={property.options} />
+                    </li>
+                  </ul>
+                </SelectField>
+              </FormGroup>
+              );
+          }
           return (
             <FormGroup key={index}>
-              <SelectField model={`search.filters.${property.name}`} >
+              <Field model={`search.filters.${property.name}`} >
                 <ul className="search__filter">
                   <li>
                     {property.label}
                     {property.required ? <span className="required">*</span> : ''}
+                    <figure className="switcher switcher-active"></figure>
                   </li>
-                  <li className="wide">
-                    <Select options={property.options} />
-                  </li>
+                  <li className="wide"><input className="form-control" /></li>
                 </ul>
-              </SelectField>
+              </Field>
             </FormGroup>
             );
-        }
-        return (
-          <FormGroup key={index}>
-            <Field model={`search.filters.${property.name}`} >
-              <ul className="search__filter">
-                <li>
-                  {property.label}
-                  {property.required ? <span className="required">*</span> : ''}
-                </li>
-                <li className="wide"><input className="form-control" /></li>
-              </ul>
-            </Field>
-          </FormGroup>
-          );
-      })}
-      </Form>
+        })}
+        </Form>
+        {(() => {
+          let documentTypes = this.props.documentTypes.toJS();
+          let filtering = Object.keys(documentTypes).reduce((result, key) => {
+            return result || documentTypes[key];
+          }, false);
+
+          if (!filtering) {
+            return <div className="empty-state select-type">
+                    <i className="fa fa-arrow-up"></i><b>Filter the results</b>
+                    <p>Select at least one type of document to start filtering the results.</p>
+                  </div>;
+          }
+
+          console.log(filtering, fields.length === 0);
+          if (filtering && fields.length === 0) {
+            return <div className="empty-state no-filters">
+                    <i className="fa fa-close"></i><b>No common filters</b>
+                    <p>The combination of document types has no filters in common.</p>
+                  </div>;
+          }
+        })()}
+      </div>
     );
   }
 }
@@ -52,13 +79,15 @@ export class FiltersForm extends Component {
 FiltersForm.propTypes = {
   fields: PropTypes.object.isRequired,
   searchDocuments: PropTypes.func,
-  search: PropTypes.object
+  search: PropTypes.object,
+  documentTypes: PropTypes.object
 };
 
 export function mapStateToProps(state) {
   return {
     fields: state.library.filters.get('properties'),
-    search: state.search
+    search: state.search,
+    documentTypes: state.library.filters.get('documentTypes')
   };
 }
 
