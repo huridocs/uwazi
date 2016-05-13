f69752a27e5ad7f8d9e16dc9a0d9184de8d3bd18
daneryl <daneryl@gmail.com>
2016-05-13 13:49:26 -0500

    added demo database config

--- app/api/config/database.js
+++ app/api/config/database.js
@@ -1,5 +1,6 @@
 export default {
+  demo: 'http://127.0.0.1:5984/uwazi_demo',
   development: 'http://127.0.0.1:5984/uwazi_development',
   testing: 'http://127.0.0.1:5984/uwazi_testing',
   production: 'http://127.0.0.1:5984/uwazi'
-}
+};
