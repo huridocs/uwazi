0f6004605c74bdf466e477d3c518ed3b70dccc6f
daneryl <daneryl@gmail.com>
2016-06-04 12:48:59 -0500

    fixed a bug when preparing document data to show and a select has no value

--- app/react/Documents/helpers.js
+++ app/react/Documents/helpers.js
@@ -3,13 +3,13 @@ export default {
 
     let template = templates.find(t => t._id === doc.template);
 
-    if (!template) {
+    if (!template || !thesauris.length) {
       return Object.assign({}, doc, {metadata: [], documentType: ''});
     }
 
     let metadata = template.properties.map((property) => {
       let value = doc.metadata[property.name];
-      if (property.type === 'select') {
+      if (property.type === 'select' && value) {
         value = thesauris.find(t => t._id === property.content).values.find(v => v.id === doc.metadata[property.name]).label;
       }
       return {label: property.label, value};
