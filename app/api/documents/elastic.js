8fad4e661d3bd2391e53205c37af9022a7962a0e
Néstor Salceda <nestor.salceda@gmail.com>
2016-06-08 18:31:44 +0200

    use environment variables for selecting database and elasticsearc index

--- app/api/documents/elastic.js
+++ app/api/documents/elastic.js
@@ -1,7 +1,7 @@
 import elasticSearch from 'elasticsearch';
 
 let elastic = new elasticSearch.Client({
-  host: 'localhost:9200'
+  host: process.env.ELASTICSEARCH_URL || 'localhost:9200'
 });
 
 export default elastic;
