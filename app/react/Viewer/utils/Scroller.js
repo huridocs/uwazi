b8b0d6cd34eb7b540a8ce72b4307ee694c562209
A Happy Cat <acasadotorres@gmail.com>
2016-06-09 10:30:03 +0200

    Scroller utility

---
+++ app/react/Viewer/utils/Scroller.js
@@ -0,0 +1,53 @@
+export default {
+  getElement(selector) {
+    return document.querySelector(selector);
+  },
+
+  isVisible(selector, parentSelector) {
+    let element = this.getElement(selector);
+    let parent = this.getElement(parentSelector);
+    let elementOffsetToParent = element.getBoundingClientRect().top - parent.getBoundingClientRect().top + parent.scrollTop;
+
+    return parent.offsetHeight + parent.scrollTop >= elementOffsetToParent + element.scrollHeight &&
+     parent.scrollTop <= elementOffsetToParent;
+  },
+
+  to(selector, parentSelector, opt = {}) {
+    let options = this.getOptions(opt);
+    if (this.isVisible(selector, parentSelector)) {
+      return;
+    }
+    let element = this.getElement(selector);
+    let parent = this.getElement(parentSelector);
+    let elementOffsetToParent = element.getBoundingClientRect().top - parent.getBoundingClientRect().top + parent.scrollTop;
+
+    let parentVisibleScroll = parent.scrollHeight - parent.offsetHeight;
+    let elementPositionInParent = parent.scrollHeight - elementOffsetToParent;
+
+    let scrollTop = parentVisibleScroll - elementPositionInParent + options.offset + element.scrollHeight * 2;
+
+    let start = Date.now();
+    let timeout = window.setInterval(() => {
+      let t = (Date.now() - start) / options.duration;
+      let multiplier = this[options.animation](t);
+      parent.scrollTop += multiplier * (scrollTop - parent.scrollTop);
+      if (multiplier >= 0.9) {
+        parent.scrollTop = scrollTop;
+        window.clearInterval(timeout);
+      }
+    }, 25);
+  },
+
+  getOptions(options) {
+    let defaultOptions = {duration: 400, offset: 0, animation: 'easeIn'};
+    return Object.assign(defaultOptions, options);
+  },
+
+  easeIn(t) {
+    return t * t;
+  },
+
+  easeOut(t) {
+    return t * (2 - t);
+  }
+};
---
+++ app/react/Viewer/utils/specs/Scroller.spec.js
@@ -0,0 +1,88 @@
+import scroller from '../Scroller.js';
+
+describe('scroller', () => {
+  let container;
+  let ul;
+  let firstLi;
+  let secondLi;
+  let thirdLi;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    container.className = 'container';
+    ul = document.createElement('ul');
+    ul.style.height = '100px';
+    ul.style.overflow = 'scroll';
+    ul.style.padding = '0';
+    ul.style.margin = '0';
+    firstLi = document.createElement('li');
+    secondLi = document.createElement('li');
+    thirdLi = document.createElement('li');
+    ul.appendChild(firstLi);
+    ul.appendChild(secondLi);
+    ul.appendChild(thirdLi);
+    container.appendChild(ul);
+    document.body.appendChild(container);
+  });
+
+  const cleanDom = () => {
+    document.body.removeChild(container);
+  };
+
+  describe('getElement()', () => {
+    it('should return the first element matching the selector', () => {
+      expect(scroller.getElement('.container')).toBe(container);
+      expect(scroller.getElement('.container li')).toBe(firstLi);
+      expect(scroller.getElement('.container li:nth-child(2)')).toBe(secondLi);
+      cleanDom();
+    });
+  });
+
+  describe('isVisible()', () => {
+    it('should return false when the element is hidden because scroll is up', () => {
+      firstLi.style.height	= '120px';
+
+      expect(scroller.isVisible('li:nth-child(2)', 'ul')).toBe(false);
+      cleanDom();
+    });
+
+    it('should return false when the element is hidden because scroll is down', () => {
+      firstLi.style.height	= '10px';
+      secondLi.style.height	= '10px';
+      thirdLi.style.height	= '100px';
+      ul.scrollTop = 50;
+      expect(scroller.isVisible('li:nth-child(1)', 'ul')).toBe(false);
+      cleanDom();
+    });
+
+    it('should return true when the element is completely visible', () => {
+      firstLi.style.height	= '120px';
+      ul.scrollTop = 60;
+      expect(scroller.isVisible('li:nth-child(2)', 'ul')).toBe(true);
+      cleanDom();
+    });
+  });
+
+  describe('to()', () => {
+    it('should scroll the parent to make the element visible', (done) => {
+      firstLi.style.height	= '120px';
+      scroller.to('li:nth-child(2)', 'ul', {duration: 0});
+      window.setTimeout(() => {
+        expect(scroller.isVisible('li:nth-child(2)', 'ul')).toBe(true);
+        cleanDom();
+        done();
+      }, 26);
+    });
+
+    it('should do nothing if the element is visible', () => {
+      firstLi.style.height	= '100px';
+      secondLi.style.height	= '40px';
+      thirdLi.style.height	= '40px';
+      ul.scrollTop = 80;
+      expect(scroller.isVisible('li:nth-child(2)', 'ul')).toBe(true);
+      scroller.to('li:nth-child(2)', 'ul');
+      expect(ul.scrollTop).toBe(80);
+      cleanDom();
+    });
+  });
+});
