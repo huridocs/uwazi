977b37e391f3b29f0eb0a33a071ea5f77329daa0
danicatalan <danicatalan@hotmail.com>
2016-06-14 13:11:19 +0200

    fix desk viewport

--- app/react/Viewer/components/Viewer.js
+++ app/react/Viewer/components/Viewer.js
@@ -35,7 +35,7 @@ export class Viewer extends Component {
   render() {
     let className = 'document-viewer col-xs-12';
     if (this.props.panelIsOpen) {
-      className = 'document-viewer with-panel col-xs-12 ';
+      className = 'document-viewer with-panel col-xs-12 col-sm-8';
     }
     if (this.props.targetDoc) {
       className = 'document-viewer show-target-document col-xs-12';
