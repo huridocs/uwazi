f0641b4ee82adc185a841e25c75bfb9c4ab74854
danicatalan <danicatalan@hotmail.com>
2016-06-14 13:05:45 +0200

    sidebar width

--- app/react/Uploads/components/UploadsSection.js
+++ app/react/Uploads/components/UploadsSection.js
@@ -32,7 +32,7 @@ export class UploadsSection extends Component {
   render() {
     let className = 'document-viewer col-xs-12';
     if (this.props.panelIsOpen) {
-      className = 'document-viewer with-panel col-xs-12 col-sm-8 col-md-9';
+      className = 'document-viewer with-panel col-xs-12 ';
     }
     return (
       <div className="row">
