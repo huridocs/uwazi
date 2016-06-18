277b1940773ccd6ac8b242c5e59d1728dcb51212
A Happy Cat <acasadotorres@gmail.com>
2016-06-18 12:15:13 +0200

    deleted commented code

--- app/react/Library/components/ViewMetadataPanel.js
+++ app/react/Library/components/ViewMetadataPanel.js
@@ -3,10 +3,8 @@ import {connect} from 'react-redux';
 import SidePanel from 'app/Layout/SidePanel';
 import documents from 'app/Documents';
 import {bindActionCreators} from 'redux';
-//import {saveDocument} from '../actions/documentActions';
 import {unselectDocument} from '../actions/libraryActions';
 
-//import DocumentForm from '../containers/DocumentForm';
 import {ShowDocument} from 'app/Documents';
 
 export class ViewMetadataPanel extends Component {
@@ -22,9 +20,6 @@ export class ViewMetadataPanel extends Component {
         <h1>Metadata</h1>
         <i className="fa fa-close close-modal" onClick={this.props.unselectDocument}/>
         {(() => {
-          //if (docBeingEdited) {
-            //return <DocumentForm onSubmit={this.submit.bind(this)} />;
-          //}
           return <ShowDocument doc={doc}/>;
         })()}
       </SidePanel>
@@ -34,7 +29,6 @@ export class ViewMetadataPanel extends Component {
 
 ViewMetadataPanel.propTypes = {
   doc: PropTypes.object,
-  //docBeingEdited: PropTypes.bool,
   open: PropTypes.bool,
   saveDocument: PropTypes.func,
   unselectDocument: PropTypes.func
@@ -46,7 +40,6 @@ const mapStateToProps = ({library}) => {
     doc: documents.helpers.prepareMetadata(library.ui.get('selectedDocument') ? library.ui.get('selectedDocument').toJS() : {},
                                            library.filters.get('templates').toJS(),
                                            library.filters.get('thesauris').toJS())
-    //docBeingEdited: !!library.docForm._id
   };
 };
 
