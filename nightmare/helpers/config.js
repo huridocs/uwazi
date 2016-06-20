5dd46a312f1bc5a8eda3abeae2d2c4d56fd34b2a
Carlos Jimenez ruiz <cjr.creativo@gmail.com>
2016-06-20 19:53:03 +0200

    refactor on smoke tests and thesauris scrolling down test added

--- nightmare/helpers/config.js
+++ nightmare/helpers/config.js
@@ -1,6 +1,6 @@
 export default {
-  url: 'http://213.167.241.120:3000',
-  // url: 'http://localhost:3000',
+  // url: 'http://213.167.241.120:3000',
+  url: 'http://localhost:3000',
   waitTime: 350,
   waitLongTime: 450
 };
--- nightmare/smoke.spec.js
+++ nightmare/smoke.spec.js
@@ -1,26 +1,40 @@
 import Nightmare from 'nightmare';
 import {login} from './helpers/login.js';
 import config from './helpers/config.js';
+import {catchErrors} from 'api/utils/jasmineHelpers';
 
 
-describe('Smoke test', () => {
+describe('Smoke test,', () => {
   let nightmare = new Nightmare({show: true}).viewport(1100, 600);
 
   let getInnerText = (selector) => {
     return document.querySelector(selector).innerText;
   };
 
-  describe('while logged out', () => {
+  let isElementVisible = (selector) => {
+    let element = document.querySelector(selector);
+    let rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  describe('while logged out,', () => {
     it('/my_account should get redirected to /login', (done) => {
       nightmare
       .goto(config.url + '/logout')
+      .wait(config.waitTime)
       .goto(config.url + '/my_account')
       .wait(config.waitTime)
       .url()
       .then((url) => {
         expect(url).toBe(config.url + '/login');
         done();
-      });
+      })
+      .catch(catchErrors(done));
     });
 
     it('/uploads should get redirected to /login', (done) => {
@@ -32,7 +46,8 @@ describe('Smoke test', () => {
       .then((url) => {
         expect(url).toBe(config.url + '/login');
         done();
-      });
+      })
+      .catch(catchErrors(done));
     });
 
     it('/metadata should get redirected to /login', (done) => {
@@ -44,7 +59,8 @@ describe('Smoke test', () => {
       .then((url) => {
         expect(url).toBe(config.url + '/login');
         done();
-      });
+      })
+      .catch(catchErrors(done));
     });
 
     it('/templates/new should get redirected to /login', (done) => {
@@ -56,7 +72,8 @@ describe('Smoke test', () => {
       .then((url) => {
         expect(url).toBe(config.url + '/login');
         done();
-      });
+      })
+      .catch(catchErrors(done));
     });
 
     it('/relationtypes/new should get redirected to /login', (done) => {
@@ -68,7 +85,8 @@ describe('Smoke test', () => {
       .then((url) => {
         expect(url).toBe(config.url + '/login');
         done();
-      });
+      })
+      .catch(catchErrors(done));
     });
 
     it('/thesauris/new should get redirected to /login', (done) => {
@@ -80,10 +98,11 @@ describe('Smoke test', () => {
       .then((url) => {
         expect(url).toBe(config.url + '/login');
         done();
-      });
+      })
+      .catch(catchErrors(done));
     });
 
-    describe('library', () => {
+    describe('library,', () => {
       it('should check if library nav button works', (done) => {
         nightmare
         .goto(config.url + '/logout')
@@ -96,8 +115,8 @@ describe('Smoke test', () => {
         });
       });
 
-      describe('bottom right floating menu', () => {
-        it('should check if the bottom right menu appear by clicking its icon', (done) => {
+      describe('bottom right floating menu,', () => {
+        it('should check if the filters bottom right menu appear by clicking its icon', (done) => {
           nightmare
           .click('.float-btn__main')
           .wait(config.waitTime)
@@ -105,72 +124,27 @@ describe('Smoke test', () => {
           .then((exists) => {
             expect(exists).toBe(true);
             done();
-          });
-        });
-
-        it('should check if the bottom right menu checkbox for select all works', (done) => {
-          nightmare
-          .click('.float-btn__main.cta')
-          .click('.search__filter.search__filter--type input')
-          .evaluate(getInnerText, '.filters-box .title')
-          .then((innerText) => {
-            expect(innerText).toBe('Common filters for Decision, Ruling and Judgement');
-            done();
-          });
-        });
-
-        it('should untick all then check if the bottom right menu checkbox for decision works', (done) => {
-          nightmare
-          .click('.search__filter.search__filter--type input')
-          .click('.search__filter.search__filter--type li:nth-child(3) input')
-          .wait(config.waitTime)
-          .evaluate(getInnerText, '.filters-box .title')
-          .then((innerText) => {
-            expect(innerText).toBe('Filters for Decision');
-            done();
-          });
-        });
-
-        it('should untick all then check if the bottom right menu checkbox for ruling works', (done) => {
-          nightmare
-          .click('.search__filter.search__filter--type li:nth-child(3) input')
-          .click('.search__filter.search__filter--type li:nth-child(4) input')
-          .wait(config.waitTime)
-          .evaluate(getInnerText, '.filters-box .title')
-          .then((innerText) => {
-            expect(innerText).toBe('Filters for Ruling');
-            done();
-          });
-        });
-
-        it('should untick all then check if the bottom right menu checkbox for judgement works', (done) => {
-          nightmare
-          .click('.search__filter.search__filter--type li:nth-child(4) input')
-          .click('.search__filter.search__filter--type li:nth-child(5) input')
-          .wait(config.waitTime)
-          .evaluate(getInnerText, '.filters-box .title')
-          .then((innerText) => {
-            expect(innerText).toBe('Filters for Judgement');
-            done();
-          });
+          })
+          .catch(catchErrors(done));
         });
       });
     });
   });
 
-  describe('while logged in', () => {
-    describe('login success', () => {
+  describe('while logged in,', () => {
+    describe('login success,', () => {
       it('should redirect to home page', (done) => {
         login(nightmare, 'admin', 'admin')
         .url()
         .then((url) => {
           expect(url).toBe(config.url + '/');
           done();
-        });
+        })
+        .catch(catchErrors(done));
       });
     });
 
-    describe('my_account', () => {
+    describe('my_account,', () => {
       it('should check if user/admin nav button works', (done) => {
         nightmare
         .click('.fa-user')
@@ -179,11 +153,12 @@ describe('Smoke test', () => {
         .then((url) => {
           expect(url).toBe(config.url + '/my_account');
           done();
-        });
+        })
+        .catch(catchErrors(done));
       });
     });
 
-    describe('uploads', () => {
+    describe('uploads,', () => {
       it('should check if uploads nav button works', (done) => {
         nightmare
         .click('.nav .fa-cloud-upload')
@@ -192,74 +167,60 @@ describe('Smoke test', () => {
         .then((url) => {
           expect(url).toBe(config.url + '/uploads');
           done();
-        });
+        })
+        .catch(catchErrors(done));
       });
 
-      describe('when the user clicks in a document', () => {
-        it('a side panel with the metadata form appears', (done) => {
+      describe('when the user clicks in a document,', () => {
+        it('a side panel with the metadata form should appear', (done) => {
           nightmare
           .click('.document-viewer .item-name')
           .exists('.side-panel.is-active')
           .then((result) => {
             expect(result).toBe(true);
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
-        it('should close the side-panel by clicking on the panel cross', (done) => {
+        it('clicking on the panel cross should close the side-panel', (done) => {
           nightmare
           .click('.side-panel .close-modal')
           .exists('.side-panel.is-hidden')
           .then((result) => {
             expect(result).toBe(true);
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
-        it('the bottom right menu is disabled before user click on a document', (done) => {
-          nightmare
-          .exists('.float-btn__main.disabled')
-          .then((result) => {
-            expect(result).toBe(true);
-            done();
-          });
-        });
-
-        it('the bottom right menu becomes active when user click on a document', (done) => {
+        it('the bottom right menu should become active', (done) => {
           nightmare
           .click('.document-viewer .item-name')
-          .exists('.float-btn__main.cta')
+          .exists('.side-panel.is-active')
           .then((result) => {
             expect(result).toBe(true);
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
-        it('when the bottom right menu becomes active, child elements become active on rollover', (done) => {
+        it('while the bottom right menu becomes active, child elements should become active on rollover', (done) => {
           nightmare
           .mouseover('.float-btn__main.cta')
           .exists('.float-btn.btn-fixed.active')
           .then((exists) => {
             expect(exists).toBe(true);
             done();
-          });
-        });
-      });
-      describe('item-metadata', () => {
-        describe('when the user clicks the document-metadata of a document', () => {
-          it('a window showing the status of a document should appear', () => {
-            nightmare
-            .click('.item-metadata')
-            .click('.cancel-button')
-            .wait(config.waitTime);
-          });
+          })
+          .catch(catchErrors(done));
         });
       });
     });
 
-    describe('document-viewer', () => {
-      describe('when the user clicks the file icon of a document', () => {
-        it('should open the document', (done) => {
+    describe('document-viewer,', () => {
+      describe('when the user clicks the file icon of a document,', () => {
+        it('it should open the document', (done) => {
           nightmare
           .click('.fa-file-o')
           .wait(config.waitLongTime)
@@ -268,7 +229,8 @@ describe('Smoke test', () => {
             expect(innerText).not.toBe('');
             expect(typeof innerText).not.toBe('undefined');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('there should be a menu on the bottom right of the document', (done) => {
@@ -278,22 +240,24 @@ describe('Smoke test', () => {
           .then((exists) => {
             expect(exists).toBe(true);
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
-        it('the bottom right of the document menu should become active on rollover', (done) => {
+        it('the bottom right menu should become active on rollover', (done) => {
           nightmare
           .mouseover('.float-btn.btn-fixed')
           .exists('.float-btn.btn-fixed.active')
           .then((exists) => {
             expect(exists).toBe(true);
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
       });
     });
 
-    describe('library', () => {
+    describe('library,', () => {
       it('should check if library nav button works', (done) => {
         nightmare
         .click('.nav .fa-th')
@@ -306,7 +270,7 @@ describe('Smoke test', () => {
       });
     });
 
-    describe('metadata', () => {
+    describe('metadata,', () => {
       it('should check if metadata nav button works', (done) => {
         nightmare
         .click('.nav .fa-list-alt')
@@ -317,7 +281,7 @@ describe('Smoke test', () => {
           done();
         });
       });
-      describe('document type section', () => {
+      describe('document type section,', () => {
         it('should click the 1st of Document Type section', (done) => {
           nightmare
           .click('.row.metadata .col-sm-4 a')
@@ -326,7 +290,8 @@ describe('Smoke test', () => {
           .then((innerText) => {
             expect(innerText).toBe('CONSTRUCTION BLOCKS');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the back button to go back to /metadata', (done) => {
@@ -337,7 +302,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/metadata');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the add document type button', (done) => {
@@ -348,7 +314,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/templates/new');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the back button to go back to /metadata', (done) => {
@@ -359,11 +326,12 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/metadata');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
       });
 
-      describe('relation types section', () => {
+      describe('relation types section,', () => {
         it('should click the 1st of relation types section', (done) => {
           nightmare
           .click('.row.metadata .col-sm-4:nth-child(2) a')
@@ -372,7 +340,8 @@ describe('Smoke test', () => {
           .then((innerText) => {
             expect(innerText).toBe('Relation Type name');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the back button to go back to /metadata', (done) => {
@@ -383,7 +352,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/metadata');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the add relation type button', (done) => {
@@ -394,7 +364,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/relationtypes/new');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the back button to go back to /metadata', (done) => {
@@ -405,11 +376,12 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/metadata');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
       });
 
-      describe('thesauris section', () => {
+      describe('thesauris section,', () => {
         it('should click the 1st of thesauris section', (done) => {
           nightmare
           .click('.row.metadata .col-sm-4:nth-child(3) a')
@@ -418,7 +390,19 @@ describe('Smoke test', () => {
           .then((innerText) => {
             expect(innerText).toBe('Thesauri name');
             done();
-          });
+          })
+          .catch(catchErrors(done));
+        });
+
+        it('should check if the user can see last value by scrolling down', (done) => {
+          nightmare
+          .scrollTo(0, 9000)
+          .evaluate(isElementVisible, 'input[name="values[41].label"]')
+          .then((result) => {
+            expect(result).toBe(true);
+            done();
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the back button to go back to /metadata', (done) => {
@@ -429,7 +413,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/metadata');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the add thesauri button', (done) => {
@@ -440,7 +425,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/thesauris/new');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
 
         it('should click the back button to go back to /metadata', (done) => {
@@ -451,7 +437,8 @@ describe('Smoke test', () => {
           .then((url) => {
             expect(url).toBe(config.url + '/metadata');
             done();
-          });
+          })
+          .catch(catchErrors(done));
         });
       });
     });
