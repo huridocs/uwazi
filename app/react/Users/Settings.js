23cf2bed387db183c13afab371c08063c72f161e
danicatalan <danicatalan@hotmail.com>
2016-07-27 14:11:42 +0200

    include metadata inside settings

--- app/react/Users/Settings.js
+++ app/react/Users/Settings.js
@@ -24,16 +24,62 @@ export class Settings extends RouteHandler {
 
   render() {
     let section = this.props.section;
+    var title = 'Document types'; var itemList = ['Decision', 'Ruling', 'Judgement'];
+    // var title = 'Relation types'; var itemList = ['Against', 'Support', 'Similar'];
+    // var title = 'Thesauris'; var itemList = ['Countries'];
     return (
         <div className="row admin-content">
           <Helmet title="Settings" />
           <div className="col-xs-12 col-sm-4">
             <SettingsNavigation/>
+
+            <div className="panel panel-default">
+              <div className="panel-heading">Metadata</div>
+              <div className="list-group">
+                <button className="list-group-item">Document types</button>
+                <button className="list-group-item">Relation types</button>
+                <button className="list-group-item">Thesauris</button>
+              </div>
+            </div>
+
           </div>
           <div className="col-xs-12 col-sm-8">
             {(()=>{
               if (section === 'account') {
-                return <AccountSettings/>;
+                //return <AccountSettings/>;
+
+                return (
+                  <div className="panel panel-default">
+                    <div className="panel-heading">{title}</div>
+                    <ul className="list-group">
+                      {itemList.map(function(name){
+                          return (
+                            <li className="list-group-item">
+                              <a className="" href="#">{name}
+                                <small>(7 items)</small>
+                              </a>
+                              <div className="list-group-item-actions">
+                                <a className="btn btn-default btn-xs" href="#">
+                                  <i className="fa fa-pencil"></i>
+                                  <span>Edit</span>
+                                </a>
+                                <a className="btn btn-danger btn-xs template-remove" href="#">
+                                  <i className="fa fa-trash"></i>
+                                  <span>Delete</span>
+                                </a>
+                              </div>
+                            </li>
+                          )
+                      })}
+                    </ul>
+                    <div className="panel-body">
+                      <button className="btn btn-success">
+                        <i className="fa fa-plus"></i>
+                        <span>Add document type</span>
+                      </button>
+                    </div>
+                  </div>
+                )
               }
               if (section === 'collection') {
                 return <CollectionSettings/>;
