9e269c4ff3abd62a1257b44820d9bcc156a8a6e0
Carlos Jimenez ruiz <cjr.creativo@gmail.com>
2016-08-18 15:33:49 +0200

    login helper fixed

--- nightmare/helpers/login.js
+++ nightmare/helpers/login.js
@@ -1,7 +1,7 @@
 import config from './config.js';
 
 const loginNavButton = '#app > div.content > header > div > div > ul > li:nth-child(2) > a';
-// const searchButton = '#app > div.content > header > div > div > div > a';
+const settingsNavButton = '#app > div.content > header > div > div > ul > li:nth-child(3)';
 
 export function login(nightmare, userName, userPassword) {
   return nightmare
@@ -11,5 +11,6 @@ export function login(nightmare, userName, userPassword) {
         .wait('#username')
         .type('input[name="username"]', userName)
         .type('input[name="password"]', userPassword)
-        .click('button[type="submit"]');
+        .click('button[type="submit"]')
+        .wait(settingsNavButton);
 }
