97adc4c8641d3a2651c7e6b7fd77e15bc840f078
Carlos Jimenez ruiz <cjr.creativo@gmail.com>
2016-08-22 18:46:18 +0200

    document templates tests finished but drag and drop to properties not added yet

--- nightmare/paths/metadata.spec.js
+++ nightmare/paths/metadata.spec.js
@@ -8,16 +8,21 @@ realMouse(Nightmare);
 const settingsNavButton = '#app > div.content > header > div > div > ul > li:nth-child(3) > a';
 const settingsHeader = '#app > div.content > div > div > div.col-xs-12.col-sm-4 > div > div:nth-child(1) > div.panel-heading';
 const thesaurisButton = '#app > div.content > div > div > div.col-xs-12.col-sm-4 > div > div:nth-child(2) > div.list-group > a:nth-child(3)';
+const documentsButton = '#app > div.content > div > div > div.col-xs-12.col-sm-4 > div > div:nth-child(2) > div.list-group > a:nth-child(1)';
 const thesaurisBackButton = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > form > div > div.panel-heading > a';
+const documentsBackButton = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > main > div > form > div > a';
 const addNewThesauri = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > div.panel-body > a';
+const addNewDocument = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > div.panel-body > a';
 const addNewValueToThesauriButton = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > form > div > div.panel-body > a';
 const firstThesauriValForm = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > form > div > ul > li:nth-child(2) > div > div > input';
 const secondThesauriValForm = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > form > div > ul > li:nth-child(3) > div > div > input';
 const saveThesauriButton = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > form > div > div.panel-heading > button';
+const saveDocumentButton = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > main > div > form > div > button';
 const thesauriNameForm = '#thesauriName';
+const documentTemplateNameForm = '#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > main > div > form > div > div > input';
 const deleteButtonConfirmation = 'body > div.ReactModalPortal > div > div > div > div.modal-footer > button.btn.confirm-button.btn-danger';
 
-describe('metadata path', () => {
+fdescribe('metadata path', () => {
   let nightmare = new Nightmare({show: true, typeInterval: 10}).viewport(1100, 600);
 
   describe('login', () => {
@@ -36,7 +41,7 @@ describe('metadata path', () => {
   });
 
   describe('in settings', () => {
-    it('should click Thesauris button', (done) => {
+    it('should click Thesauris button and then click on add new thesauri button', (done) => {
       nightmare
       .wait(thesaurisButton)
       .realClick(thesaurisButton)
@@ -103,6 +108,70 @@ describe('metadata path', () => {
         )
         .catch(catchErrors(done));
       });
+    });
+
+    it('should click Documents button and then click on add new document button', (done) => {
+      nightmare
+      .wait(documentsButton)
+      .realClick(documentsButton)
+      .wait(addNewDocument)
+      .realClick(addNewDocument)
+      .wait(saveDocumentButton)
+      .exists(saveDocumentButton)
+      .then((result) => {
+        expect(result).toBe(true);
+        done();
+      })
+      .catch(catchErrors(done));
+    });
+
+    describe('in Documents', () => {
+      it('should create a new document template with no properties added', (done) => {
+        //DRAG PROPERTIES AND DROP INTO TEMPLATE TO BE ADDED TO THIS TEST.
+        nightmare
+        .wait(documentTemplateNameForm)
+        .type(documentTemplateNameForm, 'test document template')
+        .wait(saveDocumentButton)
+        .realClick(saveDocumentButton)
+        .wait('.alert.alert-success')
+        .exists('.alert.alert-success')
+        .then((result) => {
+          expect(result).toBe(true);
+          done();
+        })
+        .catch(catchErrors(done));
+      });
+
+      it('should go back to Documents then delete the created document template', (done) => {
+        nightmare
+        .wait(documentsBackButton)
+        .realClick(documentsBackButton)
+        .wait(() => {
+          let itemFound = false;
+          let documentTemplatesList = document.querySelectorAll('#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > ul li');
+          documentTemplatesList.forEach((documentTemplate) => {
+            if (documentTemplate.innerText.match('test')) {
+              itemFound = true;
+            }
+          });
+          return itemFound;
+        })
+        .evaluate(() => {
+          let documentTemplate = document.querySelectorAll('#app > div.content > div > div > div.col-xs-12.col-sm-8 > div > ul li');
+          documentTemplate.forEach((documentTemplate) => {
+            if (documentTemplate.innerText.match('test')) {
+              documentTemplate.querySelector('.fa-trash').click();
+            }
+          });
+        })
+        .wait(deleteButtonConfirmation)
+        .realClick(deleteButtonConfirmation)
+        .then(
+          done
+        )
+        .catch(catchErrors(done));
+      });
+
     });
   });
 
