1a3a444680ae3ee441105af7d4ec4bc0288d36a3
danicatalan <danicatalan@hotmail.com>
2016-08-22 19:00:58 +0200

    add toc design

--- app/react/Documents/components/ShowToc.js
+++ app/react/Documents/components/ShowToc.js
@@ -11,12 +11,12 @@ export class ShowToc extends Component {
   render() {
     const {toc} = this.props;
     return (
-      <div className="side-panel-content">
-        <ul className="view">
+      <div className="toc">
+        <ul className="toc-view">
           {toc.map((tocElement, index) => {
             return (
-              <li key={index}>
-                <a href="#" onClick={this.scrollTo.bind(this, tocElement)}>{tocElement.label}</a>
+              <li className="toc-indent-1" key={index}>
+                <a className="toc-view-link" href="#" onClick={this.scrollTo.bind(this, tocElement)}>{tocElement.label}</a>
               </li>
               );
           })}
