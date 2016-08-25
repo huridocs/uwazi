2cabe8dee14fec6185e9c6cf52ba3ce2df7813d4
Néstor Salceda <nestor.salceda@gmail.com>
2016-08-25 23:32:34 +0200

    Add DATABASE_NAME from environment variables

--- app/api/config/database.js
+++ app/api/config/database.js
@@ -1,6 +1,8 @@
+const COUCHDBURL = process.env.COUCHDB_URL;
+const DATABASE_NAME = process.env.DATABASE_NAME;
 export default {
   demo: 'http://127.0.0.1:5984/uwazi_demo',
-  development: process.env.COUCHDB_URL || 'http://127.0.0.1:5984/uwazi_development',
-  testing: 'http://127.0.0.1:5984/uwazi_testing',
-  production: 'http://127.0.0.1:5984/uwazi'
+  development: COUCHDBURL ? `${COUCHDBURL}/${DATABASE_NAME}` : 'http://127.0.0.1:5984/uwazi_development',
+  testing: COUCHDBURL ? `${COUCHDBURL}/${DATABASE_NAME}` : 'http://127.0.0.1:5984/uwazi_testing',
+  production: `http://127.0.0.1:5984/${DATABASE_NAME}`
 };
