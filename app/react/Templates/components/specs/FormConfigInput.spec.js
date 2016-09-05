03cdaf15b70c03c851a21c3f0bca38e9b23d0ab2
RafaPolit <rafaelpolit@gmail.com>
2016-09-05 13:49:31 -0500

    Template configuration now allows selecting a showInCard option.

--- app/react/Templates/components/specs/FormConfigInput.spec.js
+++ app/react/Templates/components/specs/FormConfigInput.spec.js
@@ -31,6 +31,7 @@ describe('FormConfigInput', () => {
     expect(formFields.nodes[0].props.model).toBe('template.data.properties[0].label');
     expect(formFields.nodes[1].props.model).toBe('template.data.properties[0].required');
     expect(formFields.nodes[2].props.model).toBe('template.data.properties[0].filter');
+    expect(formFields.nodes[3].props.model).toBe('template.data.properties[0].showInCard');
   });
 
   describe('validation', () => {
