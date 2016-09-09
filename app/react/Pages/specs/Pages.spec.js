66abf18c2cd0be1c279eaea6a60b70997d63c8b4
RafaPolit <rafaelpolit@gmail.com>
2016-09-09 09:35:56 -0500

    Tests for PagesAPI and route component.

--- app/react/Pages/specs/Pages.spec.js
+++ app/react/Pages/specs/Pages.spec.js
@@ -1,37 +1,36 @@
-// This is a copy paste! Complete rewrite needed.
+import {Pages} from '../Pages';
+import PagesAPI from '../PagesAPI';
 
-import {Settings} from '../Settings';
-import UsersAPI from 'app/Users/UsersAPI';
-import TemplatesAPI from 'app/Templates/TemplatesAPI';
-import ThesaurisAPI from 'app/Thesauris/ThesaurisAPI';
-import RelationTypesAPI from 'app/RelationTypes/RelationTypesAPI';
-
-describe('Settings', () => {
+describe('Pages', () => {
   describe('requestState', () => {
-    let user = {name: 'doe'};
-    let templates = [{_id: 1, name: 'Decision'}];
-    let thesauris = [{_id: 1, name: 'Countries'}, {_id: '12314', name: 'Judges', type: 'template'}];
-    let dictionaries = [{_id: 1, name: 'Countries'}];
-    let relationTypes = [{_id: 1, name: 'Supports'}];
+    let pages = [{_id: 1, name: 'Page 1'}];
 
     beforeEach(() => {
-      spyOn(UsersAPI, 'currentUser').and.returnValue(Promise.resolve(user));
-      spyOn(TemplatesAPI, 'get').and.returnValue(Promise.resolve(templates));
-      spyOn(ThesaurisAPI, 'get').and.returnValue(Promise.resolve(thesauris));
-      spyOn(ThesaurisAPI, 'getDictionaries').and.returnValue(Promise.resolve(dictionaries));
-      spyOn(RelationTypesAPI, 'get').and.returnValue(Promise.resolve(relationTypes));
+      spyOn(PagesAPI, 'list').and.returnValue(Promise.resolve(pages));
     });
 
     it('should get the current user, and metadata', (done) => {
-      Settings.requestState()
+      Pages.requestState()
       .then((state) => {
-        expect(state.user).toEqual(user);
-        expect(state.templates).toEqual(templates);
-        expect(state.thesauris).toEqual(thesauris);
-        expect(state.dictionaries).toEqual(dictionaries);
-        expect(state.relationTypes).toEqual(relationTypes);
+        expect(state.pages).toEqual(pages);
         done();
       });
+    });
+  });
+
+  describe('setReduxState', () => {
+    let pagesClass;
+    let dispatch;
+
+    beforeEach(() => {
+      pagesClass = new Pages({pamars: {}});
+      dispatch = jasmine.createSpy('dispatch');
+      pagesClass.context = {store: {dispatch}};
+      pagesClass.setReduxState({pages: 'pages'});
+    });
+
+    it('should set pages in state', () => {
+      expect(dispatch.calls.argsFor(0)).toEqual([{type: 'pages/SET', value: 'pages'}]);
     });
   });
 });
--- app/react/Pages/specs/PagesAPI.spec.js
+++ app/react/Pages/specs/PagesAPI.spec.js
@@ -1,44 +1,25 @@
-// This is a copy paste! Complete rewrite needed.
-import entitiesAPI from '../EntitiesAPI';
+import pagesAPI from '../PagesAPI';
 import {APIURL} from 'app/config.js';
 import backend from 'fetch-mock';
 
-describe('EntitiesAPI', () => {
-  let arrayResponse = [{entities: 'array'}];
-  let searchResponse = [{entities: 'search'}];
-  let filteredSearchResult = [{entities: 'Alfred'}];
-  let singleResponse = [{entities: 'single'}];
-  let listResponse = [{entities: 'list'}];
+describe('pagesAPI', () => {
+  let arrayResponse = [{pages: 'array'}];
+  let singleResponse = [{pages: 'single'}];
+  let listResponse = [{pages: 'list'}];
 
   beforeEach(() => {
     backend.restore();
     backend
-    .mock(APIURL + 'entities', 'GET', {body: JSON.stringify({rows: arrayResponse})})
-    .mock(APIURL + 'entities/search', 'GET', {body: JSON.stringify(searchResponse)})
-    .mock(APIURL + 'entities/list?keys=%5B%221%22%2C%222%22%5D', 'GET', {body: JSON.stringify({rows: listResponse})})
-    .mock(APIURL + 'entities/uploads', 'GET', {body: JSON.stringify({rows: 'uploads'})})
-    .mock(APIURL + 'entities/count_by_template?templateId=templateId', 'GET', {body: JSON.stringify(1)})
-    .mock(APIURL + 'entities/match_title?searchTerm=term', 'GET', {body: JSON.stringify(searchResponse)})
-    .mock(APIURL + 'entities/search?searchTerm=Batman&joker=true', 'GET', {body: JSON.stringify(filteredSearchResult)})
-    .mock(APIURL + 'entities?_id=documentId', 'GET', {body: JSON.stringify({rows: singleResponse})})
-    .mock(APIURL + 'entities?_id=id', 'DELETE', {body: JSON.stringify({backednResponse: 'testdelete'})})
-    .mock(APIURL + 'entities', 'POST', {body: JSON.stringify({backednResponse: 'test'})});
-  });
-
-  describe('uploads', () => {
-    it('should request uploads', (done) => {
-      entitiesAPI.uploads()
-      .then((response) => {
-        expect(response).toEqual('uploads');
-        done();
-      })
-      .catch(done.fail);
-    });
+    .mock(APIURL + 'pages', 'GET', {body: JSON.stringify({rows: arrayResponse})})
+    .mock(APIURL + 'pages?_id=documentId', 'GET', {body: JSON.stringify({rows: singleResponse})})
+    .mock(APIURL + 'pages/list?keys=%5B%221%22%2C%222%22%5D', 'GET', {body: JSON.stringify({rows: listResponse})})
+    .mock(APIURL + 'pages', 'POST', {body: JSON.stringify({backednResponse: 'post'})})
+    .mock(APIURL + 'pages?_id=id', 'DELETE', {body: JSON.stringify({backednResponse: 'delete'})});
   });
 
   describe('get()', () => {
-    it('should request entities', (done) => {
-      entitiesAPI.get()
+    it('should request pages', (done) => {
+      pagesAPI.get()
       .then((response) => {
         expect(response).toEqual(arrayResponse);
         done();
@@ -47,8 +28,8 @@ describe('EntitiesAPI', () => {
     });
 
     describe('when passing an id', () => {
-      it('should request for the thesauri', (done) => {
-        entitiesAPI.get('documentId')
+      it('should request for the page', (done) => {
+        pagesAPI.get('documentId')
         .then((response) => {
           expect(response).toEqual(singleResponse);
           done();
@@ -59,8 +40,8 @@ describe('EntitiesAPI', () => {
   });
 
   describe('list()', () => {
-    it('should request entities list', (done) => {
-      entitiesAPI.list(['1', '2'])
+    it('should request pages list', (done) => {
+      pagesAPI.list(['1', '2'])
       .then((response) => {
         expect(response).toEqual(listResponse);
         done();
@@ -69,57 +50,14 @@ describe('EntitiesAPI', () => {
     });
   });
 
-  describe('getSuggestions()', () => {
-    it('should match_title ', (done) => {
-      entitiesAPI.getSuggestions('term')
-      .then((response) => {
-        expect(response).toEqual(searchResponse);
-        done();
-      })
-      .catch(done.fail);
-    });
-  });
-
-  describe('countByTemplate()', () => {
-    it('should count_by_template', (done) => {
-      entitiesAPI.countByTemplate('templateId')
-      .then((response) => {
-        expect(response).toEqual(1);
-        done();
-      })
-      .catch(done.fail);
-    });
-  });
-
-  describe('search()', () => {
-    it('should search entities', (done) => {
-      entitiesAPI.search()
-      .then((response) => {
-        expect(response).toEqual(searchResponse);
-        done();
-      })
-      .catch(done.fail);
-    });
-
-    describe('when passing filters', () => {
-      it('should search for it', (done) => {
-        entitiesAPI.search({searchTerm: 'Batman', joker: true})
-        .then((response) => {
-          expect(response).toEqual(filteredSearchResult);
-          done();
-        })
-        .catch(done.fail);
-      });
-    });
-  });
 
   describe('save()', () => {
-    it('should post the document data to /entities', (done) => {
-      let data = {name: 'document name'};
-      entitiesAPI.save(data)
+    it('should post the document data to /pages', (done) => {
+      let data = {title: 'document name'};
+      pagesAPI.save(data)
       .then((response) => {
-        expect(JSON.parse(backend.lastOptions(APIURL + 'entities').body)).toEqual(data);
-        expect(response).toEqual({backednResponse: 'test'});
+        expect(JSON.parse(backend.lastOptions(APIURL + 'pages').body)).toEqual(data);
+        expect(response).toEqual({backednResponse: 'post'});
         done();
       })
       .catch(done.fail);
@@ -129,9 +67,9 @@ describe('EntitiesAPI', () => {
   describe('delete()', () => {
     it('should delete the document', (done) => {
       let document = {_id: 'id'};
-      entitiesAPI.delete(document)
+      pagesAPI.delete(document)
       .then((response) => {
-        expect(response).toEqual({backednResponse: 'testdelete'});
+        expect(response).toEqual({backednResponse: 'delete'});
         done();
       })
       .catch(done.fail);
