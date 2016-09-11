7084103dbf5d1692ba9ba4a3ead8cf41c75ff487
A Happy Cat <acasadotorres@gmail.com>
2016-09-11 11:37:23 +0200

    fixed error when reloading in other page than library and going to library

--- app/react/Library/reducers/reducer.js
+++ app/react/Library/reducers/reducer.js
@@ -10,7 +10,7 @@ import {modelReducer, formReducer} from 'react-redux-form';
 export default combineReducers({
   metadata: modelReducer('library.metadata'),
   metadataForm: formReducer('library.metadata'),
-  aggregations: createReducer('library/aggregations', []),
+  aggregations: createReducer('library/aggregations', {}),
   documents: documents,
   ui: libraryUI,
   filters: libraryFilters
