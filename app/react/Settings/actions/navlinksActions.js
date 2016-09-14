0d7e7bc83ed153149a8e51fc2c1d331172939f93
RafaPolit <rafaelpolit@gmail.com>
2016-09-14 13:50:45 -0500

    Configured actions for the navlinks.

--- app/react/Settings/actions/navlinksActions.js
+++ app/react/Settings/actions/navlinksActions.js
@@ -1,17 +1,36 @@
 import {actions as formActions} from 'react-redux-form';
+import {actions} from 'app/BasicReducer';
+import * as types from 'app/Settings/actions/actionTypes';
+import api from 'app/Settings/SettingsAPI';
+import {notify} from 'app/Notifications';
 
 import ID from 'shared/uniqueID';
 
-export function addLink() {
-  return function (dispatch, getState) {
-    let links = getState().settings.navlinksData.links.slice(0);
-    const link = {title: `Item ${links.length + 1}`, localID: ID()};
-    dispatch(formActions.push('settings.navlinksData.links', link));
-  };
+export function loadLinks(links) {
+  return formActions.load('settings.navlinksData', {links});
+}
+
+export function addLink(links) {
+  const link = {title: `Item ${links.length + 1}`, localID: ID()};
+  return formActions.push('settings.navlinksData.links', link);
 }
 
 export function sortLink(originIndex, targetIndex) {
+  return formActions.move('settings.navlinksData.links', originIndex, targetIndex);
+}
+
+export function removeLink(index) {
+  return formActions.remove('settings.navlinksData.links', index);
+}
+
+export function saveLinks(data) {
   return function (dispatch) {
-    dispatch(formActions.move('settings.navlinksData.links', originIndex, targetIndex));
+    dispatch({type: types.SAVING_NAVLINKS});
+    return api.save(data)
+    .then((response) => {
+      dispatch({type: types.NAVLINKS_SAVED, data: response});
+      dispatch(actions.set('settings/collection', response));
+      dispatch(notify('Saved successfully.', 'success'));
+    });
   };
 }
