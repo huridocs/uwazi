a889223cbeb32201467d21c04fcf703201821ad6
RafaPolit <rafaelpolit@gmail.com>
2016-09-19 19:06:02 -0500

    Added template to fixtures.

--- app/api/references/specs/fixtures.js
+++ app/api/references/specs/fixtures.js
@@ -11,10 +11,10 @@ export default {
     {"_id":"c08ef2532f0bd008ac5174b45e033c04","type":"reference", "title":"targetDocument", "targetDocument": "target"},
     {"_id":"c08ef2532f0bd008ac5174b45e033c05","type":"reference", "title":"targetDocument", "targetDocument": "target"},
     {"_id":"c08ef2532f0bd008ac5174b45e033c06","type":"reference", "title":"targetDocument1", "targetDocument": "target1"},
-    {"_id":"source1", title: "source1 title", type: "document"},
-    {"_id":"doc3", title: "doc3 title", type: "entity"},
-    {"_id":"doc4", title: "doc4 title", type: "document"},
-    {"_id":"doc5", title: "doc5 title", type: "document"},
+    {"_id":"source1", title: "source1 title", type: "document", template: "template3_id"},
+    {"_id":"doc3", title: "doc3 title", type: "entity", template: "template1_id"},
+    {"_id":"doc4", title: "doc4 title", type: "document", template: "template1_id"},
+    {"_id":"doc5", title: "doc5 title", type: "document", template: "template2_id"},
 
     //selectValues
     {"_id":"selectValue", title: "selectValue", type: "entity"},
