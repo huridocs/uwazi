00bfc20a24449e17b2822ea507e99812c97c0927
RafaPolit <rafaelpolit@gmail.com>
2016-09-20 13:52:11 -0500

    Corrected affected tests due to fixture change.

--- app/api/references/specs/references.spec.js
+++ app/api/references/specs/references.spec.js
@@ -17,7 +17,7 @@ describe('references', () => {
     it('should return all the references in the database', (done) => {
       references.getAll()
       .then((result) => {
-        expect(result.rows.length).toBe(7);
+        expect(result.rows.length).toBe(8);
         expect(result.rows[0].type).toBe('reference');
         expect(result.rows[0].title).toBe('reference1');
         done();
@@ -26,7 +26,7 @@ describe('references', () => {
   });
 
   describe('saveEntityBasedReferences', () => {
-    fit('should create references for each option on selects/multiselects using entities (not affecting document references)', (done) => {
+    it('should create references for each option on selects/multiselects using entities (not affecting document references)', (done) => {
       const entity = {
         _id: 'id_testing',
         template,
@@ -68,7 +68,7 @@ describe('references', () => {
         return references.getByDocument(entity._id);
       })
       .then((refs) => {
-        expect(refs.length).toBe(1);
+        expect(refs.length).toBe(2);
         done();
       })
       .catch(catchErrors(done));
@@ -98,12 +98,13 @@ describe('references', () => {
           return references.getByDocument(entity._id);
         })
         .then((refs) => {
-          expect(refs.length).toBe(2);
+          expect(refs.length).toBe(3);
 
           expect(refs.find((ref) => ref.targetDocument === 'value1')._id).not.toBe(generatedIds[0]);
-          expect(refs.find((ref) => ref.targetDocument === 'value1').sourceDocument).toEqual('id_testing');
-          expect(refs.find((ref) => ref.targetDocument === 'value2')._id).toBe(generatedIds[1]);
-          expect(refs.find((ref) => ref.targetDocument === 'value2').sourceDocument).toEqual('id_testing');
+          expect(refs.find((ref) => ref.targetDocument === 'value1').sourceDocument).toBe('id_testing');
+          expect(refs.filter((ref) => ref.targetDocument === 'value2')[0]._id).toBe(generatedIds[1]);
+          expect(refs.filter((ref) => ref.targetDocument === 'value2')[0].sourceDocument).toBe('id_testing');
+          expect(refs.filter((ref) => ref.targetDocument === 'value2')[1]._id).toBe('c08ef2532f0bd008ac5174b45e033c10');
 
           done();
         })
