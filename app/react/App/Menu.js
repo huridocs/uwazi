a038d2d009a4d86ee8e4deb6ad8dd8e40626d4a2
danicatalan <danicatalan@hotmail.com>
2016-10-03 17:05:39 +0200

    include i18n dropdown

--- app/react/App/Menu.js
+++ app/react/App/Menu.js
@@ -24,20 +24,32 @@ class Menu extends Component {
     return (
       <ul onClick={this.props.onClick} className={this.props.className}>
         <li className="menuItems">
-          <ul>{navLinks}</ul>
+          <ul className="menuNav-list">{navLinks}</ul>
         </li>
         <li className="menuActions">
-          <ul>
-            <li><a onClick={this.goToLibrary.bind(this)} className="btn btn-default"><i className="fa fa-th"></i></a></li>
+          <ul className="menuNav-list">
+            <li className="menuNav-item">
+              <div className="Dropdown">
+                <ul className="Dropdown-list">
+                  <li className="Dropdown-option is-active"><span>ES</span></li>
+                  <li className="Dropdown-option"><span>EN</span></li>
+                  <li className="Dropdown-option"><span>PO</span></li>
+                </ul>
+                <span className="Dropdown-label">
+                  <i className="fa fa-caret-down"></i>
+                </span>
+              </div>
+            </li>
+            <li className="menuNav-item"><a onClick={this.goToLibrary.bind(this)} className="menuNav-btn btn btn-default"><i className="fa fa-th"></i></a></li>
             <NeedAuthorization>
-              <li><Link to='/uploads' className="btn btn-default"><span><i className="fa fa-cloud-upload"></i></span></Link></li>
+              <li className="menuNav-item"><Link to='/uploads' className="menuNav-btn btn btn-default"><span><i className="fa fa-cloud-upload"></i></span></Link></li>
             </NeedAuthorization>
             <NeedAuthorization>
-              <li><Link to='/settings/account' className="btn btn-default"><i className="fa fa-cog"></i></Link></li>
+              <li className="menuNav-item"><Link to='/settings/account' className="menuNav-btn btn btn-default"><i className="fa fa-cog"></i></Link></li>
             </NeedAuthorization>
             {(() => {
               if (!user._id) {
-                return <li><Link to='/login' className="btn btn-default"><i className="fa fa-power-off"></i></Link></li>;
+                return <li className="menuNav-item"><Link to='/login' className="menuNav-btn btn btn-default"><i className="fa fa-power-off"></i></Link></li>;
               }
             })()}
           </ul>
