d3aed9c680777d994d4c7e3698acdf1aa3bae722
RafaPolit <rafaelpolit@gmail.com>
2016-10-05 16:40:08 -0500

    Added secondary timeline.

--- app/react/Timeline/components/TimelineViewer.js
+++ app/react/Timeline/components/TimelineViewer.js
@@ -12,6 +12,9 @@ import ReferencesAPI from 'app/Viewer/referencesAPI';
 import moment from 'moment';
 
 // const countryTemplate = 'e8e039070aa95f8f964c281d450d1022';
+const caseTemplate = 'cd951f1feec188a75916812d43252418';
+const matterTemplate = '6e2bfa14cc35c78b202a63e5c63ec969';
+
 const renderableTemplates = {
   judgement: '5c7180d3dd310766a1c6817c165ed5f4',
   admissibilityReport: 'e8e039070aa95f8f964c281d4583100d',
@@ -38,8 +41,8 @@ export class TimelineViewer extends Component {
     }, '');
   }
 
-  fetchReferences(entity) {
-    return ReferencesAPI.get(entity._id);
+  fetchReferences(entityId) {
+    return ReferencesAPI.get(entityId);
   }
 
   fetchReferenceData(references) {
@@ -55,10 +58,6 @@ export class TimelineViewer extends Component {
     });
 
     return Promise.all(fetchPromises);
-  }
-
-  fetchSecondLevelReferences(references) {
-    return Promise.all(references.map(r => ReferencesAPI.get(r.connectedDocument)));
   }
 
   assignAdditionalData(reference) {
@@ -105,7 +104,19 @@ export class TimelineViewer extends Component {
     return years;
   }
 
-  arrangeYears(references, caseDates) {
+  splitByOrigin(years) {
+    const related = Object.keys(years).reduce((memo, year) => {
+      memo[year] = years[year].filter(i => i.origin === 'related');
+      return memo;
+    }, {});
+    const main = Object.keys(years).reduce((memo, year) => {
+      memo[year] = years[year].filter(i => i.origin !== 'related');
+      return memo;
+    }, {});
+    return {main, related};
+  }
+
+  arrangeYears(references, caseDates, relatedReferencesExist) {
     let years = {};
     references.forEach(reference => {
       const isDesiredTemplate = desiredTemplates.indexOf(reference.data.template !== -1);
@@ -120,28 +131,56 @@ export class TimelineViewer extends Component {
       this.assignDataToYear(years, date.timestamp, date);
     });
 
-    return this.normalizeYears(years);
+    let normalizedYears = this.normalizeYears(years);
+    return relatedReferencesExist ? this.splitByOrigin(normalizedYears) : {main: years};
   }
 
   getTimelineInfo(entity) {
     let usefulReferences;
+    const isCase = entity.template === caseTemplate;
+    const isMatter = entity.template === matterTemplate;
+    // let relatedEntity;
 
-    this.fetchReferences(entity)
+    this.fetchReferences(entity._id)
     .then(references => {
-      usefulReferences = references.filter(r => desiredTemplates.indexOf(r.connectedDocumentTemplate) !== -1);
-      return this.fetchReferenceData(usefulReferences);
+      let getRelatedReferences = Promise.resolve([]);
+      if (isCase) {
+        let caseMatter = references.find(r => r.connectedDocumentTemplate === matterTemplate);
+        if (caseMatter) {
+          getRelatedReferences = this.fetchReferences(caseMatter.connectedDocument);
+        }
+      }
+
+      if (isMatter) {
+        let matterCase = references.find(r => r.connectedDocumentTemplate === caseTemplate);
+        if (matterCase) {
+          getRelatedReferences = this.fetchReferences(matterCase.connectedDocument);
+        }
+      }
+
+      return Promise.all([references, getRelatedReferences]);
     })
-    .then(referencesData => {
-      return Promise.all([referencesData, this.fetchSecondLevelReferences(usefulReferences)]);
+    .then(([references, relatedReferences]) => {
+      usefulReferences = references
+                         .map(r => {
+                           r.origin = isCase || !relatedReferences.length ? 'main' : 'related';
+                           return r;
+                         })
+                         .concat(relatedReferences.map(r => {
+                           r.origin = isCase ? 'related' : 'main';
+                           return r;
+                         }))
+                         .filter(r => desiredTemplates.indexOf(r.connectedDocumentTemplate) !== -1);
+      return Promise.all([this.fetchReferenceData(usefulReferences), relatedReferences]);
     })
-    .then(([referencesData, secondLevelReferences]) => {
+    .then(([referencesData, relatedReferences]) => {
       const conformedReferences = usefulReferences.map((reference, index) => {
-        return {reference, data: referencesData[index][0], children: secondLevelReferences[index]};
+        return {reference, origin: reference.origin, data: referencesData[index][0]};
       });
 
       this.setState({
         references: conformedReferences,
-        years: this.arrangeYears(conformedReferences, this.getCaseDates(entity))
+        years: this.arrangeYears(conformedReferences, this.getCaseDates(entity), Boolean(relatedReferences.length))
       });
     });
   }
@@ -159,14 +198,14 @@ export class TimelineViewer extends Component {
   }
 
   render() {
-    let years = '';
+    let years = {main: '', related: ''};
     if (this.state && this.state.years) {
-      years = Object.keys(this.state.years).map(year =>
+      years.main = Object.keys(this.state.years.main).map(year =>
         <div key={year} className="timeline-year">
           <div className={`timeline-label ${year % 5 === 0 ? 'timeline-label-text' : ''}`}>
             <span>{year}</span>
           </div>
-          {this.state.years[year].map((reference, index) => {
+          {this.state.years.main[year].map((reference, index) => {
             if (reference.reference) {
               return (
                 <Link to={`/${reference.reference.connectedDocumentType}/${reference.data._id}`}
@@ -197,15 +236,44 @@ export class TimelineViewer extends Component {
           })}
         </div>
       );
+
+      if (this.state.years.related) {
+        years.related = Object.keys(this.state.years.related).map(year =>
+          <div key={year} className="timeline-year">
+            {this.state.years.related[year].map((reference, index) => {
+              return (
+                <Link to={`/${reference.reference.connectedDocumentType}/${reference.data._id}`}
+                      key={index}
+                      className={`timeline-item ${reference.additionalData.className}`}
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      data-animation="false"
+                      title={`${moment.utc(reference.additionalData.date * 1000).format('ll')}\n${reference.data.title}`}>
+                  <ShowIf if={reference.additionalData.type === 'judgement'}>
+                    <i className="fa fa-legal"></i>
+                  </ShowIf>
+                </Link>
+              );
+            })}
+          </div>
+        );
+      }
     }
     return (
-      <div className="timeline">
-        {(() => {
-          if (!this.state || !this.state.years) {
-            return <Loader/>;
-          }
-        })()}
-        {years}
+      <div>
+        <ShowIf if={Boolean(this.state && this.state.years && this.state.years.related)}>
+          <div className="timeline">
+            {years.related}
+          </div>
+        </ShowIf>
+        <div className="timeline">
+          {(() => {
+            if (!this.state || !this.state.years) {
+              return <Loader/>;
+            }
+          })()}
+          {years.main}
+        </div>
       </div>
     );
   }
