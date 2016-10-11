c5b9f621b4ef9672b06104f437d5acf25a4b953f
RafaPolit <rafaelpolit@gmail.com>
2016-10-11 11:42:49 -0500

    Optimized reducer to prevent creation each time.

--- app/react/Entities/reducers/referencesReducer.js
+++ app/react/Entities/reducers/referencesReducer.js
@@ -4,11 +4,12 @@ import createReducer from 'app/BasicReducer';
 import {fromJS} from 'immutable';
 
 const initialState = [];
+const reducer = createReducer('entityView/references', []);
 
 export default function (state = initialState, action = {}) {
   if (action.type === connectionTypes.CONNECTION_CREATED) {
     return state.push(fromJS(action.connection));
   }
 
-  return createReducer('entityView/references', [])(state, action);
+  return reducer(state, action);
 }
