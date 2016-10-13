d3f7a2fbc547461e8141db6e43c0ad7dbc8e07c1
RafaPolit <rafaelpolit@gmail.com>
2016-10-13 14:38:21 -0500

    Begun testing actions.

---
+++ app/react/Connections/actions/specs/actions.spec.js
@@ -0,0 +1,43 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import api from 'app/utils/api';
+
+import * as actions from '../actions';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('Connections actions', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({});
+    spyOn(api, 'get').and.returnValue(Promise.resolve({json: {rows: [{type: 'entity'}, {type: 'doc'}]}}));
+  });
+
+  describe('Search-related actions', () => {
+    describe('immidiateSearch', () => {
+      fit('should search for connections', () => {
+        actions.immidiateSearch(store.dispatch, 'term');
+        expect(api.get).toHaveBeenCalledWith('search', {searchTerm: 'term', fields: ['doc.title']});
+        expect(store.getActions()).toContain({type: 'SEARCHING_CONNECTIONS'});
+      });
+
+      fit('should set the results upon getting results', (done) => {
+        actions.immidiateSearch(store.dispatch, 'term')
+        .then(() => {
+          const expectedAction = {type: 'connections/searchResults/SET', value: [{type: 'entity'}, {type: 'doc'}]};
+          expect(store.getActions()).toContain(expectedAction);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('startNewConnection', () => {
+    fit('should perform an immediate empty search', () => {
+      actions.startNewConnection('type', 'sourceId')(store.dispatch);
+      expect(api.get).toHaveBeenCalledWith('search', {searchTerm: '', fields: ['doc.title']});
+    });
+  });
+});
