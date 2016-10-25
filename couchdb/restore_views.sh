e437085c107a22bc70c9d2eaee4d4d40ccb300b8
Néstor Salceda <nestor.salceda@gmail.com>
2016-10-25 18:48:07 +0200

    Add host information to restore_views shell script

--- couchdb/restore_views.sh
+++ couchdb/restore_views.sh
@@ -3,9 +3,9 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P );
 cd "$parent_path"
 
 echo -e "\nremoving views of ${1:-uwazi_development} database"
-../node_modules/couchdb-dump/bin/cdbdump -k -d ${1:-uwazi_development} | 
-../node_modules/couchdb-dump/bin/cdbmorph -f ./utils/remove_views.js | 
-../node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi_development}
+../node_modules/couchdb-dump/bin/cdbdump -k -h localhost -d ${1:-uwazi_development} |
+../node_modules/couchdb-dump/bin/cdbmorph -f ./utils/remove_views.js |
+../node_modules/couchdb-dump/bin/cdbload -h localhost -d ${1:-uwazi_development}
 
 echo -e "\nimporting views into ${1:-uwazi_development} database"
-../node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi_development} < ./views.js
+../node_modules/couchdb-dump/bin/cdbload -h localhost -d ${1:-uwazi_development} -v < ./views.js
