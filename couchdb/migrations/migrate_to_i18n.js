b4183eea5ed5e9240a5fc67f0d3c1811a0db1458
A Happy Cat <acasadotorres@gmail.com>
2016-11-08 18:01:42 +0100

    updated migration to handle references and metadata

--- couchdb/migrations/migrate_to_i18n.js
+++ couchdb/migrations/migrate_to_i18n.js
@@ -8,43 +8,124 @@ let language = {key: 'en', label: 'English', default: true};
 
 let documentsDone = 0;
 
+let documentsMigrated = {};
+
+function migrateDoc(doc) {
+  doc.language = language.key;
+  doc.sharedId = ID();
+  return request.post(dbURL + '/_design/entities/_update/partialUpdate/' + doc._id, doc)
+  .then(() => documentsMigrated[doc._id] = doc.sharedId)
+  .catch((error) => {
+    console.log('migrateDoc', doc._id, error);
+  });
+}
+
 function migrate(offset = 0) {
-  Promise.all([request.get(dbURL + '/_all_docs?limit=' + limit + '&skip=' + offset)])
+  return Promise.all([request.get(dbURL + '/_all_docs?limit=' + limit + '&skip=' + offset)])
   .then(([docsResponse]) => {
     if (offset >= docsResponse.json.total_rows) {
       return;
     }
-    P.resolve(docsResponse.json.rows).map((_doc) => {
+
+    return P.resolve(docsResponse.json.rows).map((_doc) => {
       return request.get(dbURL + '/' + _doc.id)
       .then((response) => {
         let doc = response.json;
         documentsDone += 1;
         console.log (`${documentsDone} of ${docsResponse.json.total_rows}`);
-        if (doc.type === 'page' || doc.type === 'document' || doc.type === 'entity') {
-          doc.language = language.key;
-          doc.sharedId = ID();
-          // return request.post(dbURL + '/' + _doc.id, doc);
+        if ((doc.type === 'page' || doc.type === 'document' || doc.type === 'entity')) {
+          return migrateDoc(doc);
         }
 
         if (doc.type === 'settings') {
           doc.languages = [language];
-          // return request.post(dbURL + '/' + _doc.id, doc);
+          return request.post(dbURL + '/_design/entities/_update/partialUpdate/' + _doc.id, doc)
+          .catch((error) => {
+            console.log('Saving settings', error);
+          });
         }
       });
     }, {concurrency: 1})
     .then(() => {
-      migrate(docsResponse.json.offset + limit);
+      return migrate(docsResponse.json.offset + limit);
     });
   })
   .catch((error) => {
-    console.log(error);
+    console.log('migrate', error);
   });
 }
 
-migrate();
+function migrateDocMetadata(doc) {
+  if (!doc.template) {
+    return;
+  }
+  return request.get(dbURL + '/' + doc.template)
+  .then((response) => {
+    let template = response.json;
+    template.properties.forEach((property) => {
+      if (property.content) {
+        let value = doc.metadata[property.name];
+        if (documentsMigrated[value]) {
+          doc.metadata[property.name] = documentsMigrated[value];
+        }
+      }
+    });
+
+    return request.post(dbURL + '/_design/entities/_update/partialUpdate/' + doc._id, doc)
+    .catch((error) => {
+      console.log('save migrateDocMetadata', doc._id, error);
+    });
+  })
+  .catch((error) => {
+    console.log('migrateDocMetadata', error);
+  });
+}
+
+function migrateMetadata(offset = 0) {
+  return Promise.all([request.get(dbURL + '/_all_docs?limit=' + limit + '&skip=' + offset), request.get(dbURL + '/_design/templates/_view/all')])
+  .then(([docsResponse]) => {
+    if (offset >= docsResponse.json.total_rows) {
+      return;
+    }
+
+    return P.resolve(docsResponse.json.rows).map((_doc) => {
+      return request.get(dbURL + '/' + _doc.id)
+      .then((response) => {
+        let doc = response.json;
+        documentsDone += 1;
+        console.log (`${documentsDone} of ${docsResponse.json.total_rows}`);
+        if ((doc.type === 'document' || doc.type === 'entity')) {
+          return migrateDocMetadata(doc);
+        }
+
+        if ((doc.type === 'reference')) {
+            doc.sourceDocument = documentsMigrated[doc.sourceDocument];
+            doc.targetDocument = documentsMigrated[doc.targetDocument];
+            return request.post(dbURL + '/_design/entities/_update/partialUpdate/' + doc._id, doc)
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    }, {concurrency: 1})
+    .then(() => {
+      return migrateMetadata(docsResponse.json.offset + limit);
+    });
+  })
+  .catch((error) => {
+    console.log('migrateMetadata', error);
+  });
+}
+
+migrate()
+.then(() => {
+  console.log('Migrating references and relations');
+  documentsDone = 0;
+  migrateMetadata();
+});
 
 let translation = {
-  locale: 'es',
+  locale: language.key,
   type: 'translation',
   contexts: [
     {
@@ -96,4 +177,7 @@ let translation = {
   ]
 };
 
-request.post(dbURL, translation);
+// request.post(dbURL, translation)
+// .catch((error) => {
+//   console.log(error);
+// });
