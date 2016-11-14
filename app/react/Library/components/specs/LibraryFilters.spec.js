17c55132e073db7e512e06e531e75ce227a51970
A Happy Cat <acasadotorres@gmail.com>
2016-11-14 13:57:58 +0100

    fixed lint errors

--- app/react/Library/components/specs/LibraryFilters.spec.js
+++ app/react/Library/components/specs/LibraryFilters.spec.js
@@ -1,31 +1,8 @@
-import React from 'react';
-import {shallow} from 'enzyme';
 import Immutable from 'immutable';
 
-import {LibraryFilters, mapStateToProps} from 'app/Library/components/LibraryFilters';
-import {MultiSelect} from 'app/Forms';
+import {mapStateToProps} from 'app/Library/components/LibraryFilters';
 
 describe('LibraryFilters', () => {
-  let component;
-  let templates;
-  let props;
-
-  beforeEach(() => {
-    templates = Immutable.fromJS([{name: 'decision'}, {name: 'ruling'}]);
-    props = {
-      templates,
-      aggregations: Immutable.fromJS({types: {buckets: [{key: 'decision', doc_count: 2}]}}),
-      filters: Immutable.fromJS({
-        documentTypes: []
-      }),
-      searchTerm: 'Bruce Wayne',
-      form: {isBatman: {value: true}},
-      searchDocuments: jasmine.createSpy('searchDocuments'),
-      search: {sort: 'title'}
-    };
-    component = shallow(<LibraryFilters {...props} />);
-  });
-
   describe('maped state', () => {
     it('should contain the filters store and the filters form', () => {
       let store = {
