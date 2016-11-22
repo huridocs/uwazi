7d0372c65a635dfa25badafe2469b0d23126baec
daneryl <daneryl@gmail.com>
2016-11-22 20:36:05 +0100

    Document activateReference passing new required parameters

--- app/react/Viewer/components/Document.js
+++ app/react/Viewer/components/Document.js
@@ -22,7 +22,7 @@ export class Document extends Component {
 
   handleClick(e) {
     if (e.target.className && e.target.className.indexOf('reference') !== -1 && !this.text.selected()) {
-      return this.props.activateReference(e.target.getAttribute('data-id'), this.props.references);
+      return this.props.activateReference(this.props.references.find(r => r._id === e.target.getAttribute('data-id')), this.props.doc.get('pdfInfo').toJS(), this.props.references);
     }
     if (this.props.executeOnClickHandler) {
       this.props.onClick();
