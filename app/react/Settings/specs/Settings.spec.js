954a32e6247967fc481712d597c9509711ca39cf
daneryl <daneryl@gmail.com>
2016-11-30 13:04:28 +0100

    Settings no longer requests for templates/thesauris

--- app/react/Settings/specs/Settings.spec.js
+++ app/react/Settings/specs/Settings.spec.js
@@ -1,6 +1,5 @@
 import {Settings} from '../Settings';
 import UsersAPI from 'app/Users/UsersAPI';
-import TemplatesAPI from 'app/Templates/TemplatesAPI';
 import ThesaurisAPI from 'app/Thesauris/ThesaurisAPI';
 import RelationTypesAPI from 'app/RelationTypes/RelationTypesAPI';
 import {I18NApi} from 'app/I18N';
@@ -8,16 +7,12 @@ import {I18NApi} from 'app/I18N';
 describe('Settings', () => {
   describe('requestState', () => {
     let user = {name: 'doe'};
-    let templates = [{_id: 1, name: 'Decision'}];
-    let thesauris = [{_id: 1, name: 'Countries'}, {_id: '12314', name: 'Judges', type: 'template'}];
     let dictionaries = [{_id: 1, name: 'Countries'}];
     let relationTypes = [{_id: 1, name: 'Supports'}];
     let translations = [{_id: 1, locale: 'es', values: {}}];
 
     beforeEach(() => {
       spyOn(UsersAPI, 'currentUser').and.returnValue(Promise.resolve(user));
-      spyOn(TemplatesAPI, 'get').and.returnValue(Promise.resolve(templates));
-      spyOn(ThesaurisAPI, 'get').and.returnValue(Promise.resolve(thesauris));
       spyOn(ThesaurisAPI, 'getDictionaries').and.returnValue(Promise.resolve(dictionaries));
       spyOn(RelationTypesAPI, 'get').and.returnValue(Promise.resolve(relationTypes));
       spyOn(I18NApi, 'get').and.returnValue(Promise.resolve(translations));
@@ -27,8 +22,6 @@ describe('Settings', () => {
       Settings.requestState()
       .then((state) => {
         expect(state.user).toEqual(user);
-        expect(state.templates).toEqual(templates);
-        expect(state.thesauris).toEqual(thesauris);
         expect(state.dictionaries).toEqual(dictionaries);
         expect(state.translations).toEqual(translations);
         done();
