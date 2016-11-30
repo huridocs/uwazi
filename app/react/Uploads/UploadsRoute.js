5b078e0255dc96b941fc0d20a1f78e2fcf27baf2
daneryl <daneryl@gmail.com>
2016-11-30 12:13:05 +0100

    UploadsRoute do not request for thesauris and templates anymore

--- app/react/Uploads/UploadsRoute.js
+++ app/react/Uploads/UploadsRoute.js
@@ -3,25 +3,20 @@ import React from 'react';
 import RouteHandler from 'app/App/RouteHandler';
 import {setUploads} from 'app/Uploads/actions/uploadsActions';
 import searchAPI from 'app/Search/SearchAPI';
-import templatesAPI from 'app/Templates/TemplatesAPI';
-import thesaurisAPI from 'app/Thesauris/ThesaurisAPI';
-import {actions} from 'app/BasicReducer';
 
 import UploadsSection from 'app/Uploads/components/UploadsSection';
 
 export default class UploadsRoute extends RouteHandler {
 
   static requestState() {
-    return Promise.all([searchAPI.unpublished(), templatesAPI.get(), thesaurisAPI.get()])
-    .then(([documents, templates, thesauris]) => {
-      return {uploads: {documents}, templates, thesauris};
+    return searchAPI.unpublished()
+    .then((documents) => {
+      return {uploads: {documents}};
     });
   }
 
-  setReduxState({uploads: {documents}, templates, thesauris}) {
+  setReduxState({uploads: {documents}}) {
     this.context.store.dispatch(setUploads(documents));
-    this.context.store.dispatch(actions.set('templates', templates));
-    this.context.store.dispatch(actions.set('thesauris', thesauris));
   }
 
   render() {
