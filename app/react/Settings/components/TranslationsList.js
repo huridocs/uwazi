20724324fbff1e2ffb338f857f3d1f49afb48b1b
A Happy Cat <acasadotorres@gmail.com>
2017-01-02 19:24:59 +0100

    contexts sorted alphabetically

--- app/react/Settings/components/TranslationsList.js
+++ app/react/Settings/components/TranslationsList.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {I18NLink, t} from 'app/I18N';
+import {advancedSort} from 'app/utils/advancedSort';
 
 import {notify} from 'app/Notifications/actions/notificationsActions';
 
@@ -9,12 +10,13 @@ export class TranslationsList extends Component {
 
   render() {
     let {settings, translations} = this.props;
-    let defaultLanguage = settings.toJS().languages.find((lang) => lang.default).key;
-    let defaultTranslation = translations.toJS().find((translation) => translation.locale === defaultLanguage);
+    let defaultLanguage = settings.get('languages').find((lang) => lang.get('default')).get('key');
+    let defaultTranslation = translations.find((translation) => translation.get('locale') === defaultLanguage);
+    let contexts = advancedSort(defaultTranslation.get('contexts').toJS(), {property: 'label'});
     return <div className="panel panel-default">
       <div className="panel-heading">{t('System', 'Translations')}</div>
       <ul className="list-group relation-types">
-        {defaultTranslation.contexts.map((context, index) => {
+        {contexts.map((context, index) => {
           return <li key={index} className="list-group-item">
               {context.type}
               <I18NLink to={'/settings/translations/edit/' + encodeURIComponent(context.id)}>{context.label}</I18NLink>
