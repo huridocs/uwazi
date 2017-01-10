c73441ade2ec8d677cf0fedba522ab57cbe032d9
daneryl <daneryl@gmail.com>
2017-01-10 11:38:37 +0100

    alert function overwriten on karma tests

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,5 +1,6 @@
 require('es6-promise').polyfill();
 // require('whatwg-fetch');
 require('isomorphic-fetch');
+window.alert = function(){};
 var context = require.context('./app/react', true, /\.spec\.js$/); //make sure you have your directory and regex test set correctly!
 context.keys().forEach(context)
