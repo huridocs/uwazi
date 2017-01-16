b3de2d5c618bd71a4c0205bc4fe7270c02acbcf3
A Happy Cat <acasadotorres@gmail.com>
2017-01-16 12:16:35 +0100

    fixes for 708

--- nightmare/paths/translations.spec.js
+++ nightmare/paths/translations.spec.js
@@ -1,14 +1,11 @@
 /*eslint max-nested-callbacks: ["error", 10]*/
-import Nightmare from 'nightmare';
-import realMouse from 'nightmare-real-mouse';
+import createNightmare from '../helpers/nightmare';
 import config from '../helpers/config.js';
 import selectors from '../helpers/selectors.js';
 import {catchErrors} from 'api/utils/jasmineHelpers';
 
-realMouse(Nightmare);
-
 describe('translations path', () => {
-  let nightmare = new Nightmare({show: true, typeInterval: 10}).viewport(1100, 600);
+  const nightmare = createNightmare();
 
   describe('login', () => {
     it('should log in as admin then click the settings nav button', (done) => {
