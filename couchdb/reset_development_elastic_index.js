0509c011cff6c080b94a4d15353a9e69d2aa5508
daneryl <daneryl@gmail.com>
2017-02-05 18:35:45 +0100

    fixed reset_development path resolver

--- couchdb/reset_development_elastic_index.js
+++ couchdb/reset_development_elastic_index.js
@@ -1,4 +1,18 @@
-require('babel-core/register')();
+require('babel-core/register')({
+  "presets": ["es2015", "react"],
+  "retainLines": "true",
+  "plugins": [
+    "transform-class-properties",
+    "add-module-exports",
+    ['module-resolver', {
+      alias: {
+        app: '../app/react',
+        shared: '../app/shared',
+        api: '../app/api'
+      }
+    }]
+  ]
+});
 
 var db_config = require('../app/api/config/database.js');
 db_config.db_url = db_config.development;
