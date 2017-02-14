09dd84f7862939ddd7bbba7598a4c1074fe53215
daneryl <daneryl@gmail.com>
2017-02-14 15:12:13 +0100

    clean up i18n and settings routes

--- app/api/i18n/routes.js
+++ app/api/i18n/routes.js
@@ -4,33 +4,19 @@ import needsAuthorization from '../auth/authMiddleware';
 export default app => {
   app.get('/api/translations', (req, res) => {
     translations.get()
-    .then((response) => res.json(response))
-    .catch((e) => {
-      console.trace(e);
-    });
+    .then(response => res.json(response))
+    .catch(error => res.json({error}))
   });
 
   app.post('/api/translations', needsAuthorization, (req, res) => {
     translations.save(req.body)
-    .then((response) => res.json(response))
-    .catch((e) => {
-      console.trace(e);
-    });
+    .then(response => res.json(response))
+    .catch(error => res.json({error}))
   });
-
-  //app.post('/api/translations/addEntries', needsAuthorization, (req, res) => {
-    //translations.addEntries(req.body)
-    //.then((response) => res.json(response))
-    //.catch((e) => {
-      //console.trace(e);
-    //});
-  //});
 
   app.post('/api/translations/addentry', needsAuthorization, (req, res) => {
     translations.addEntry(req.body.context, req.body.key, req.body.value)
-    .then((response) => res.json(response))
-    .catch((e) => {
-      console.trace(e);
-    });
+    .then(response => res.json(response))
+    .catch(error => res.json({error}))
   });
 };
--- app/api/settings/routes.js
+++ app/api/settings/routes.js
@@ -4,14 +4,13 @@ import needsAuthorization from '../auth/authMiddleware';
 export default app => {
   app.post('/api/settings', needsAuthorization, (req, res) => {
     settings.save(req.body)
-    .then((response) => res.json(response));
+    .then(response => res.json(response))
+    .catch(error => res.json({error}));
   });
 
   app.get('/api/settings', (req, res) => {
     settings.get()
     .then((response) => res.json(response))
-    .catch((e) => {
-      console.trace(e);
-    });
+    .catch(error => res.json({error}));
   });
 };
