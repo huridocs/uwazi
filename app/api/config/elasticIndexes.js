92e6e750eb8d2434ed8d08849ec95126451517fb
A Happy Cat <acasadotorres@gmail.com>
2017-05-03 12:58:12 +0200

    Added uwazi_testing index in elastic indexes and set it for the backend specs

--- app/api/config/elasticIndexes.js
+++ app/api/config/elasticIndexes.js
@@ -2,6 +2,7 @@ const INDEX_NAME = process.env.INDEX_NAME;
 
 export default {
   production: INDEX_NAME ? INDEX_NAME  :'uwazi',
+  testing: INDEX_NAME ? INDEX_NAME  :'uwazi_testing',
   development: INDEX_NAME ? INDEX_NAME  :'uwazi_development',
   demo: INDEX_NAME ? INDEX_NAME  :'uwazi_demo'
 };
