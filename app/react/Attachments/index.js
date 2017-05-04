33c2ced19fafe45f16588fda0241e426c03b6452
RafaPolit <rafaelpolit@gmail.com>
2017-05-04 13:47:55 -0500

    Renamed addAttachmentsReducer to manageAttachmentsReducer.

--- app/react/Attachments/index.js
+++ app/react/Attachments/index.js
@@ -1,11 +1,11 @@
 import AttachmentsList from './components/AttachmentsList';
 import UploadAttachment from './components/UploadAttachment';
-import addAttachmentsReducer from './reducers/addAttachmentsReducer';
+import manageAttachmentsReducer from './reducers/manageAttachmentsReducer';
 import reducer from './reducers/reducer';
 
 export default {
   AttachmentsList,
   UploadAttachment,
-  addAttachmentsReducer,
+  manageAttachmentsReducer,
   reducer
 };
