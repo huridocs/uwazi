01ff5450f9d147eda71b63eedd0e65d57c5656f8
RafaPolit <rafaelpolit@gmail.com>
2017-05-10 12:46:07 -0500

    Updated route catch to use the new res.error middleware.

--- app/api/settings/routes.js
+++ app/api/settings/routes.js
@@ -5,14 +5,12 @@ export default app => {
   app.post('/api/settings', needsAuthorization, (req, res) => {
     settings.save(req.body)
     .then(response => res.json(response))
-    .catch(error => {
-      res.json({error});
-    });
+    .catch(res.error);
   });
 
   app.get('/api/settings', (req, res) => {
     settings.get()
     .then((response) => res.json(response))
-    .catch(error => res.json({error}));
+    .catch(res.error);
   });
 };
