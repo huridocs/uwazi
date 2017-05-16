90016f4cb267dd9b5bd664aca2502c02ce447598
daneryl <daneryl@gmail.com>
2017-05-16 14:02:03 +0200

    fixed eslints

--- app/react/Entities/containers/specs/EntityForm.spec.js
+++ app/react/Entities/containers/specs/EntityForm.spec.js
@@ -1,7 +1,2 @@
-import * as entityActions from '../../actions/actions';
-import entitiesUtils from '../../utils/filterBaseProperties';
-
-import {mapDispatchToProps} from '../EntityForm';
-
 describe('Viewer EntityForm', () => {
 });
--- app/react/Viewer/containers/specs/DocumentForm.spec.js
+++ app/react/Viewer/containers/specs/DocumentForm.spec.js
@@ -1,7 +1,2 @@
-import * as documentActions from '../../actions/documentActions';
-import entitiesUtils from 'app/Entities/utils/filterBaseProperties';
-
-import {mapDispatchToProps} from '../DocumentForm';
-
 describe('Viewer DocumentForm', () => {
 });
