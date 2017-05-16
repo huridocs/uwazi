b05af3082308d0353a89c6b0f24cd9b177840b1e
A Happy Cat <acasadotorres@gmail.com>
2017-05-16 20:52:12 +0200

    Settings secction now requests the settings

--- app/react/Settings/specs/Settings.spec.js
+++ app/react/Settings/specs/Settings.spec.js
@@ -1,5 +1,6 @@
 import {Settings} from '../Settings';
 import UsersAPI from 'app/Users/UsersAPI';
+import SettingsAPI from '../SettingsAPI';
 import ThesaurisAPI from 'app/Thesauris/ThesaurisAPI';
 import RelationTypesAPI from 'app/RelationTypes/RelationTypesAPI';
 import {I18NApi} from 'app/I18N';
@@ -10,12 +11,14 @@ describe('Settings', () => {
     let dictionaries = [{_id: 1, name: 'Countries'}];
     let relationTypes = [{_id: 1, name: 'Supports'}];
     let translations = [{_id: 1, locale: 'es', values: {}}];
+    let settings = {siteName: 'BatCave'};
 
     beforeEach(() => {
       spyOn(UsersAPI, 'currentUser').and.returnValue(Promise.resolve(user));
       spyOn(ThesaurisAPI, 'getDictionaries').and.returnValue(Promise.resolve(dictionaries));
       spyOn(RelationTypesAPI, 'get').and.returnValue(Promise.resolve(relationTypes));
       spyOn(I18NApi, 'get').and.returnValue(Promise.resolve(translations));
+      spyOn(SettingsAPI, 'get').and.returnValue(Promise.resolve(settings));
     });
 
     it('should get the current user, and metadata', (done) => {
@@ -24,6 +27,7 @@ describe('Settings', () => {
         expect(state.user).toEqual(user);
         expect(state.dictionaries).toEqual(dictionaries);
         expect(state.translations).toEqual(translations);
+        expect(state.settings.collection).toEqual(settings);
         done();
       });
     });
