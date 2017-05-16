5fbe8be7b7c64e0c3c2473093028bfd8e5e806c9
daneryl <daneryl@gmail.com>
2017-05-16 14:00:11 +0200

    removed tests of entityForm and DocumentForm containers using filterBaseProperties

--- app/react/Entities/containers/specs/EntityForm.spec.js
+++ app/react/Entities/containers/specs/EntityForm.spec.js
@@ -4,16 +4,4 @@ import entitiesUtils from '../../utils/filterBaseProperties';
 import {mapDispatchToProps} from '../EntityForm';
 
 describe('Viewer EntityForm', () => {
-  describe('mapDispatchToProps', () => {
-    beforeEach(() => {
-      spyOn(entityActions, 'saveEntity');
-      spyOn(entitiesUtils, 'filterBaseProperties').and.callFake(data => 'filtered ' + data);
-    });
-
-    it('should only send document base data on submit', () => {
-      expect(entityActions.saveEntity).not.toHaveBeenCalled();
-      mapDispatchToProps(() => {}).onSubmit('data');
-      expect(entityActions.saveEntity).toHaveBeenCalledWith('filtered data');
-    });
-  });
 });
