ac98f123a0bd9757d88fdae74b73b07184f306c3
RafaPolit <rafaelpolit@gmail.com>
2017-05-25 15:54:35 -0500

    Added sending templates to default sorting in uploads.

--- app/react/Uploads/specs/UploadsRoute.spec.js
+++ app/react/Uploads/specs/UploadsRoute.spec.js
@@ -8,6 +8,9 @@ import RouteHandler from 'app/App/RouteHandler';
 import * as actionTypes from 'app/Library/actions/actionTypes.js';
 import {fromJS as Immutable} from 'immutable';
 
+import searchAPI from 'app/Search/SearchAPI';
+import prioritySortingCriteria from 'app/utils/prioritySortingCriteria';
+
 describe('UploadsRoute', () => {
   let documents = [{title: 'Something to publish'}, {title: 'My best recipes'}];
   let aggregations = [{1: '23'}, {2: '123'}];
@@ -15,24 +18,37 @@ describe('UploadsRoute', () => {
   let instance;
   let context;
   let props = {};
-  let globalResources = {templates: Immutable([]), thesauris: Immutable([])};
+  let templates = [
+    {name: 'Decision', _id: 'abc1', properties: [{name: 'p', filter: true, type: 'text', prioritySorting: true}]},
+    {name: 'Ruling', _id: 'abc2', properties: []}
+  ];
+  let globalResources = {templates: Immutable(templates), thesauris: Immutable([])};
 
   beforeEach(() => {
     RouteHandler.renderedFromServer = true;
     context = {store: {dispatch: jasmine.createSpy('dispatch')}};
     component = shallow(<UploadsRoute {...props}/>, {context});
     instance = component.instance();
+
     spyOn(searchAPI, 'search').and.returnValue(Promise.resolve(documents));
   });
 
-  afterEach(() => backend.restore());
-
   describe('static requestState()', () => {
     it('should request unpublished documents, templates and thesauris', (done) => {
-      let query;
+      const query = {filters: {}, types: []};
       let params;
+
+      const expectedSearch = {
+        sort: prioritySortingCriteria.get({templates: Immutable(templates)}).sort,
+        order: prioritySortingCriteria.get({templates: Immutable(templates)}).order,
+        filters: query.filters,
+        types: query.types,
+        unpublished: true
+      };
+
       UploadsRoute.requestState(params, query, globalResources)
       .then((state) => {
+        expect(searchAPI.search).toHaveBeenCalledWith(expectedSearch);
         expect(state.uploads.documents).toEqual(documents);
         done();
       })
