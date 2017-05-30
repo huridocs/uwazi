0237a3e64b11ad24e34ba2f92b58984bbb5de184
danicatalan <danicatalan@hotmail.com>
2017-05-30 09:50:48 +0200

    fix settings > menu layout

--- app/react/Settings/components/NavlinksSettings.js
+++ app/react/Settings/components/NavlinksSettings.js
@@ -27,54 +27,52 @@ export class NavlinksSettings extends Component {
 
     return (
       <div className="NavlinksSettings">
-        <div className="col-xs-12">
-          <Form model="settings.navlinksData"
-                onSubmit={this.props.saveLinks.bind(this, payload)}
-                className="navLinks"
-                validators={validator(links)}>
+        <Form model="settings.navlinksData"
+              onSubmit={this.props.saveLinks.bind(this, payload)}
+              className="navLinks"
+              validators={validator(links)}>
 
-            <div className="panel panel-default">
+          <div className="panel panel-default">
 
-              <div className="panel-heading">
-                <div className={nameGroupClass}>
-                  {t('System', 'Menu')}
-                </div>
-                &nbsp;
-                <button type="submit"
-                        className="btn btn-success"
-                        disabled={!!this.props.savingNavlinks}>
-                  <i className="fa fa-save"/> {t('System', 'Save')}
-                </button>
+            <div className="panel-heading">
+              <div className={nameGroupClass}>
+                {t('System', 'Menu')}
               </div>
-
-              <ul className="list-group">
-                {links.map((link, i) => {
-                  return (
-                    <NavlinkForm key={link.localID || link._id}
-                                 index={i}
-                                 id={link.localID || link._id}
-                                 link={link}
-                                 sortLink={this.props.sortLink} />
-                  );
-                })}
-              </ul>
-              <div className="panel-body">
-                <a className="btn btn-success"
-                   onClick={this.props.addLink.bind(this, links)}>
-                  <i className="fa fa-plus"></i>&nbsp;<span>{t('System', 'Add link')}</span>
-                </a>
-              </div>
-
+              &nbsp;
+              <button type="submit"
+                      className="btn btn-success"
+                      disabled={!!this.props.savingNavlinks}>
+                <i className="fa fa-save"/> {t('System', 'Save')}
+              </button>
             </div>
 
-            <div className="alert alert-info full-width">
-              <i className="fa fa-lightbulb-o"></i>
-              From here you control the Menu navigation links.<br /><br />
-              Use only relative URLs (starting with a /) and not fully formed URLs like http://www.google.com.<br />
-              If you copied a page universal URL, be sure to delete the first part (http://yourdomain.com).
+            <ul className="list-group">
+              {links.map((link, i) => {
+                return (
+                  <NavlinkForm key={link.localID || link._id}
+                               index={i}
+                               id={link.localID || link._id}
+                               link={link}
+                               sortLink={this.props.sortLink} />
+                );
+              })}
+            </ul>
+            <div className="panel-body">
+              <a className="btn btn-success"
+                 onClick={this.props.addLink.bind(this, links)}>
+                <i className="fa fa-plus"></i>&nbsp;<span>{t('System', 'Add link')}</span>
+              </a>
             </div>
 
-          </Form>
+          </div>
+
+
+        </Form>
+        <div className="alert alert-info">
+          <i className="fa fa-lightbulb-o"></i>
+          From here you control the Menu navigation links.<br /><br />
+          Use only relative URLs (starting with a /) and not fully formed URLs like http://www.google.com.<br />
+          If you copied a page universal URL, be sure to delete the first part (http://yourdomain.com).
         </div>
       </div>
     );
