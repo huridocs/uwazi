6e179270dc9a184fc245f3ce1759009884f079ee
daneryl <daneryl@gmail.com>
2017-07-14 17:57:46 +0200

    fixed reprocess documents migration to ignore unexistent document

--- app/api/migrations/reprocess_documents.js
+++ app/api/migrations/reprocess_documents.js
@@ -1,6 +1,7 @@
 import P from 'bluebird';
 import PDF from '../upload/PDF.js';
 import entities from '../entities/entitiesModel';
+import fs from 'fs';
 import mongoose from 'mongoose';
 
 entities.get({type: 'document'}, {_id: 1})
@@ -8,7 +9,13 @@ entities.get({type: 'document'}, {_id: 1})
   return P.resolve(documents).map(({_id}) => {
     return entities.get({_id})
     .then(([doc]) => {
+      if (!doc.file) {
+        return;
+      }
       const file = __dirname + '/../../../uploaded_documents/' + doc.file.filename;
+      if (!fs.existsSync(file)) {
+        return;
+      }
       return new PDF(file).convert()
       .then((conversion) => {
         doc.fullText = conversion.fullText;
@@ -22,5 +29,9 @@ entities.get({type: 'document'}, {_id: 1})
   }, {concurrency: 1});
 })
 .then(() => {
+  mongoose.disconnect();
+})
+.catch((e) => {
+  console.log(e);
   mongoose.disconnect();
 });
