0a180fe32265dcbea75311875433a80b32a674cc
daneryl <daneryl@gmail.com>
2017-07-20 11:47:17 +0200

    fixed build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 webpack
-babel -D app/ -d ./build/app --ignore app/api/swagger/
-cp -fR app/api/swagger/ build/app/api/swagger
+babel -D app/ -d ./build/app
 mv dist ./build
 cp server.js build/server.js
 cp -fR public build
