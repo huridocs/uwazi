b4a5824d36708bb60f4ff6e9a1f5cd5797ae0415
daneryl <daneryl@gmail.com>
2017-07-20 10:33:40 +0200

    build.sh ignores swagger folder compilation

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 webpack
-babel -D app/ -d ./build/app
+babel -D app/ -d ./build/app --ignore app/api/swagger/
+cp -fR app/api/swagger/ build/app/api/swagger
 mv dist ./build
 cp server.js build/server.js
