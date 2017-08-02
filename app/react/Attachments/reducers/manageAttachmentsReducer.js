3e5655f76112edd1bffb9ae5e5d37a84c0f2b3d7
RafaPolit <rafaelpolit@gmail.com>
2017-08-02 13:20:23 -0500

    Extended abstract manageAttachmentsReducer to be able to handle more complex data structures.

--- app/react/Attachments/reducers/manageAttachmentsReducer.js
+++ app/react/Attachments/reducers/manageAttachmentsReducer.js
@@ -1,25 +1,40 @@
 import {fromJS as Immutable} from 'immutable';
 import * as attachmentsTypes from 'app/Attachments/actions/actionTypes';
 
-export default function manageAttachmentsReducer(originalReducer) {
-  return (state = {}, action = {}) => {
-    if (action.type === attachmentsTypes.ATTACHMENT_COMPLETE && state.get('_id') === action.entity) {
-      const attachments = state.get('attachments') || Immutable([]);
-      return state.set('attachments', attachments.push(Immutable(action.file)));
+const getId = (state, setInArray) => {
+  return state.getIn(setInArray.concat(['_id']));
+};
+
+const getAttachments = (state, setInArray) => {
+  return state.getIn(setInArray.concat(['attachments'])) || Immutable([]);
+};
+
+export default function manageAttachmentsReducer(originalReducer, {useDefaults = true, setInArray = []} = {}) {
+  return (orignialState, originalAction) => {
+    let state = orignialState;
+    let action = originalAction || {};
+
+    if (useDefaults) {
+      state = orignialState || {};
     }
 
-    if (action.type === attachmentsTypes.ATTACHMENT_DELETED && state.get('_id') === action.entity) {
-      const attachments = state.get('attachments') || Immutable([]);
-      return state.set('attachments', attachments.filterNot(a => a.get('filename') === action.file.filename));
+    if (action.type === attachmentsTypes.ATTACHMENT_COMPLETE && getId(state, setInArray) === action.entity) {
+      const attachments = getAttachments(state, setInArray);
+      return state.setIn(setInArray.concat(['attachments']), attachments.push(Immutable(action.file)));
     }
 
-    if (action.type === attachmentsTypes.ATTACHMENT_RENAMED && state.get('_id') === action.entity) {
-      if (state.get('_id') === action.file._id) {
-        return state.setIn(['file', 'originalname'], action.file.originalname);
+    if (action.type === attachmentsTypes.ATTACHMENT_DELETED && getId(state, setInArray) === action.entity) {
+      const attachments = getAttachments(state, setInArray);
+      return state.setIn(setInArray.concat(['attachments']), attachments.filterNot(a => a.get('filename') === action.file.filename));
+    }
+
+    if (action.type === attachmentsTypes.ATTACHMENT_RENAMED && getId(state, setInArray) === action.entity) {
+      if (getId(state, setInArray) === action.file._id) {
+        return state.setIn(setInArray.concat(['file', 'originalname']), action.file.originalname);
       }
 
-      const attachments = state.get('attachments') || Immutable([]);
-      return state.set('attachments', attachments.map(a => {
+      const attachments = getAttachments(state, setInArray);
+      return state.setIn(setInArray.concat(['attachments']), attachments.map(a => {
         if (a.get('_id') === action.file._id) {
           return a.set('originalname', action.file.originalname);
         }
--- app/react/Attachments/reducers/specs/manageAttachmentsReducer.spec.js
+++ app/react/Attachments/reducers/specs/manageAttachmentsReducer.spec.js
@@ -74,4 +74,68 @@ describe('manageAttachmentsReducer', () => {
       });
     });
   });
+
+  describe('When setIn is defined and _id matches action.entity', () => {
+    let action;
+    let state;
+
+    beforeEach(() => {
+      action = {entity: 'eId'};
+      state = Immutable.fromJS({selectedDocuments: [{_id: 'eId'}]});
+    });
+
+    describe('When ATTACHMENT_COMPLETE', () => {
+      it('should append the action file to attachments', () => {
+        action.type = 'ATTACHMENT_COMPLETE';
+        action.file = {file: 'file'};
+        const attachments = manageAttachmentsReducer(null, {setInArray: ['selectedDocuments', 0]})(state, action)
+                            .getIn(['selectedDocuments', 0, 'attachments']).toJS();
+
+        expect(attachments.length).toBe(1);
+        expect(attachments[0]).toEqual(action.file);
+      });
+    });
+
+    describe('When ATTACHMENT_DELETED', () => {
+      it('should remove the action file from attachments', () => {
+        action.type = 'ATTACHMENT_DELETED';
+        action.file = {filename: 'file to be deleted'};
+        state = state.setIn(['selectedDocuments', 0, 'attachments'],
+                            Immutable.fromJS([{filename: 'file to be deleted'}, {filename: 'file to remain'}]));
+
+        const attachments = manageAttachmentsReducer(null, {setInArray: ['selectedDocuments', 0]})(state, action)
+                            .getIn(['selectedDocuments', 0, 'attachments']).toJS();
+
+        expect(attachments.length).toBe(1);
+        expect(attachments[0]).toEqual({filename: 'file to remain'});
+      });
+    });
+
+    describe('When ATTACHMENT_RENAMED', () => {
+      it('should rename the document file originalname', () => {
+        action.type = 'ATTACHMENT_RENAMED';
+        action.file = {_id: 'eId', originalname: 'new name'};
+        state = state.setIn(['selectedDocuments', 0, 'file'], Immutable.fromJS({originalname: 'original name'}));
+
+        const file = manageAttachmentsReducer(null, {setInArray: ['selectedDocuments', 0]})(state, action)
+                     .getIn(['selectedDocuments', 0, 'file']).toJS();
+
+        expect(file.originalname).toBe('new name');
+      });
+
+      it('should rename the selected attachment title', () => {
+        action.type = 'ATTACHMENT_RENAMED';
+        action.file = {_id: 2, originalname: 'new name'};
+        state = state.setIn(['selectedDocuments', 0, 'attachments'],
+                            Immutable.fromJS([{_id: 1, originalname: 'file to remain'}, {_id: 2, originalname: 'file to be edited'}]));
+
+        const attachments = manageAttachmentsReducer(null, {setInArray: ['selectedDocuments', 0]})(state, action)
+                            .getIn(['selectedDocuments', 0, 'attachments']).toJS();
+
+        expect(attachments.length).toBe(2);
+        expect(attachments[0].originalname).toBe('file to remain');
+        expect(attachments[1].originalname).toBe('new name');
+      });
+    });
+  });
 });
