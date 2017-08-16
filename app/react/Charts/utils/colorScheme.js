2228588d4d223acbfb85c83f7e62c09b2d9bfe78
RafaPolit <rafaelpolit@gmail.com>
2017-08-16 11:23:15 -0500

    Added light version of colors for stacking purposes.

--- app/react/Charts/utils/colorScheme.js
+++ app/react/Charts/utils/colorScheme.js
@@ -1,1 +1,7 @@
 export default ['#0088FE', '#00ABC4', '#00C49F', '#A8C400', '#FFBB28', '#FF8042', '#D24040', '#A250B3'];
+
+const light = ['#7fc3fe', '#7fd4e1', '#7fe1ce', '#d3e17f', '#ffdc93', '#ffbfa0', '#e89f9f', '#d0a7d8'];
+
+export {
+  light
+};
