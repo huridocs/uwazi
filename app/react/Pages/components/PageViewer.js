2cce5aa7a99f5666cab901c318fc10a410e68433
RafaPolit <rafaelpolit@gmail.com>
2017-08-16 14:53:28 -0500

    Extended pageViewer to allow custom markup hooks.

--- app/react/Pages/components/PageViewer.js
+++ app/react/Pages/components/PageViewer.js
@@ -8,44 +8,92 @@ import extendedMarked from 'app/utils/marked';
 import markdownEscapedValues from 'app/utils/markdownEscapedValues';
 
 import {ItemList} from './ItemList';
-import * as CustomHookComponents from 'app/CustomHooks';
+import CustomHookComponents from 'app/CustomHooks';
 
 const listPlaceholder = '{---UWAZILIST---}';
 const customHooksEscape = '{customhook}';
 const customHooksPlaceholder = '{---UWAZICUSTOMHOOKS---}';
 
 export class PageViewer extends Component {
-  render() {
-    const {page, itemLists} = this.props;
-    const lists = itemLists.toJS();
-    let originalText = page.getIn(['metadata', 'content']) || '';
 
-    // TEST!!!
+  errorHtml(index) {
+    return <p key={index} className="pageSection customHook error">
+            <br />
+              <strong><i>Custom Hook markup error: unsuported values! Please check your configuration</i></strong>
+            <br />
+           </p>;
+  }
+
+  prepareCustomHooks(originalText) {
     const customHookValues = markdownEscapedValues(originalText, '(...)', customHooksEscape);
     let content = originalText || '';
 
     customHookValues.forEach(value => {
       content = content.replace(`${customHooksEscape}(${value})`, customHooksPlaceholder);
     });
-    // -----------
-    const tokens = marked.lexer(content, {sanitize: true});
 
-    const sections = tokens.reduce((memo, token) => {
-      const listMatches = new RegExp(listPlaceholder, 'g').exec(token.text);
-      // TEST!!!
-      const customHookMatches = new RegExp(customHooksPlaceholder, 'g').exec(token.text);
-      // --------
-      if (token.type === 'paragraph' && listMatches) {
-        memo.push('list');
-      // TEST!!!
-      } else if (token.type === 'paragraph' && customHookMatches) {
-        memo.push('customhook');
-      // --------
-      } else if (!memo.length || memo[memo.length - 1] !== 'markdown') {
-        memo.push('markdown');
+    return {customHookValues, content};
+  }
+
+  reduceSections(memo, token) {
+    const listMatches = new RegExp(listPlaceholder, 'g').exec(token.text);
+    const customHookMatches = new RegExp(customHooksPlaceholder, 'g').exec(token.text);
+
+    if (token.type === 'paragraph' && listMatches) {
+      memo.push('list');
+    } else if (token.type === 'paragraph' && customHookMatches) {
+      memo.push('customhook');
+    } else if (!memo.length || memo[memo.length - 1] !== 'markdown') {
+      memo.push('markdown');
+    }
+
+    return memo;
+  }
+
+  conformCustomHook(index, customHookValues) {
+    let output = <p key={index}>Custom hook</p>;
+    try {
+      const props = rison.decode(`(${customHookValues.shift()})`);
+      if (!CustomHookComponents[props.component]) {
+        throw new Error('Invalid  component');
       }
-      return memo;
-    }, []);
+      const Element = CustomHookComponents[props.component];
+      output = <Element className="pageSection customHook" key={index} {...props} />;
+    } catch (err) {
+      output = this.errorHtml(index);
+    }
+    return output;
+  }
+
+  prepareHtml(sections, lists, customHookValues, htmlSplits) {
+    return sections.map((type, index) => {
+      if (type === 'list' && lists.length) {
+        const listData = lists.shift();
+        return <div key={index} className="pageSection markdownViewer">
+                 <ItemList link={`/library/${listData.params}`} items={listData.items}/>
+               </div>;
+      }
+      if (type === 'customhook' && customHookValues.length) {
+        return this.conformCustomHook(index, customHookValues);
+      }
+      if (type === 'markdown' && htmlSplits.length) {
+        const __html = htmlSplits.shift();
+        return <div key={index}
+                    className="pageSection markdownViewer"
+                    dangerouslySetInnerHTML={{__html}}/>;
+      }
+      return false;
+    });
+  }
+
+  render() {
+    const {page, itemLists} = this.props;
+    const lists = itemLists.toJS();
+    let originalText = page.getIn(['metadata', 'content']) || '';
+
+    const {customHookValues, content} = this.prepareCustomHooks(originalText);
+    const tokens = marked.lexer(content, {sanitize: true});
+    const sections = tokens.reduce(this.reduceSections, []);
 
     const html = extendedMarked(content);
     let htmlSplits = html.split(`<p>${listPlaceholder}</p>\n`).filter(i => i.length);
@@ -53,49 +101,13 @@ export class PageViewer extends Component {
       return memo.concat(htmlSplit.split(`<p>${customHooksPlaceholder}</p>\n`).filter(i => i.length));
     }, []);
 
-    const pageHtml = sections.map((type, index) => {
-      if (type === 'list' && lists.length) {
-        const listData = lists.shift();
-        return <div key={index} className="markdownViewer">
-                 <ItemList link={`/library/${listData.params}`} items={listData.items}/>
-               </div>;
-      }
-      // TEST!!!!
-      if (type === 'customhook' && customHookValues.length) {
-        let output = <p key={index}>Custom hook</p>;
-        try {
-          const options = rison.decode(`(${customHookValues.shift()})`);
-          if (!CustomHookComponents[options.component]) {
-            throw new Error('Invalid  component');
-          }
-          const Element = CustomHookComponents[options.component];
-          output = <Element key={index} />;
-        } catch (err) {
-          output = <p key={index}>
-                     <br />
-                       <strong><i>Custom Hook markup error: unsuported values, please check your configuration</i></strong>
-                     <br />
-                   </p>;
-        }
-        return output;
-      }
-      // -------
-      if (type === 'markdown' && htmlSplits.length) {
-        const __html = htmlSplits.shift();
-        return <div key={index}
-                    className="markdownViewer"
-                    dangerouslySetInnerHTML={{__html}}/>;
-      }
-      return false;
-    });
-
     return (
       <div className="row">
         <Helmet title={page.get('title') ? page.get('title') : 'Page'} />
         <main className="document-viewer page-viewer">
           <div className="main-wrapper">
             <div className="document">
-              {pageHtml}
+              {this.prepareHtml(sections, lists, customHookValues, htmlSplits)}
             </div>
           </div>
         </main>
--- app/react/Pages/components/specs/PageViewer.spec.js
+++ app/react/Pages/components/specs/PageViewer.spec.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {shallow} from 'enzyme';
 import {fromJS as Immutable} from 'immutable';
+import CustomHookComponents from 'app/CustomHooks';
 
 import {PageViewer} from '../PageViewer';
 import {ItemList} from '../ItemList';
@@ -28,20 +29,30 @@ describe('PageViewer', () => {
     });
   });
 
-  describe('Custom list markdown element', () => {
+  describe('Custom markdown elements', () => {
     beforeEach(() => {
+      CustomHookComponents.validcomponent = class customComponent extends Component {
+        render() {
+          return <p>Custom</p>;
+        }
+      };
+
       props.page = props.page.setIn(['metadata', 'content'],
         '## title\nSome text with a [URL](http://google.com) inside.' +
         '\n\n{---UWAZILIST---}' +
         '\n\nWhich should be in its own line, separated with TWO line breaks (to create a new <p> Element)' +
         '\n\nAnd should allow multiple lists with different values' +
+        '\n\n{customhook}(component:validcomponent,prop:\'a prop\')' +
+        '\n\nShould allow markdown between hooks' +
         '\n\n{---UWAZILIST---}' +
+        '\n\n{customhook}(component:validcomponent)' +
         '\n\n{---UWAZILIST---}' +
+        '\n\n{customhook}(component:invalidcomponent)' +
         '\n\n```javascript\nCode\n```'
       );
     });
 
-    it('should render the separated markdownViewer containers', () => {
+    it('should render the separated containers', () => {
       props.itemLists = Immutable([
         {params: 'param1', items: ['items1']},
         {params: 'param2', items: ['items2']},
@@ -49,23 +60,38 @@ describe('PageViewer', () => {
       ]);
 
       render();
-      expect(component.find('.markdownViewer').at(0).props().dangerouslySetInnerHTML.__html).toContain('title');
-      expect(component.find('.markdownViewer').at(0).props().dangerouslySetInnerHTML.__html).toContain('Some text with a');
+      expect(component.find('.pageSection').at(0).props().dangerouslySetInnerHTML.__html).toContain('title');
+      expect(component.find('.pageSection').at(0).props().dangerouslySetInnerHTML.__html).toContain('Some text with a');
 
-      expect(component.find('.markdownViewer').at(1).find(ItemList).props().link).toBe('/library/param1');
-      expect(component.find('.markdownViewer').at(1).find(ItemList).props().items).toEqual(props.itemLists.getIn([0, 'items']).toJS());
+      expect(component.find('.pageSection').at(1).find(ItemList).props().link).toBe('/library/param1');
+      expect(component.find('.pageSection').at(1).find(ItemList).props().items).toEqual(props.itemLists.getIn([0, 'items']).toJS());
 
-      expect(component.find('.markdownViewer').at(2).props().dangerouslySetInnerHTML.__html).toContain('should be in its own line');
-      expect(component.find('.markdownViewer').at(2).props().dangerouslySetInnerHTML.__html).toContain('should allow multiple lists');
+      expect(component.find('.pageSection').at(2).props().dangerouslySetInnerHTML.__html).toContain('should be in its own line');
+      expect(component.find('.pageSection').at(2).props().dangerouslySetInnerHTML.__html).toContain('should allow multiple lists');
 
+      expect(component.find('.pageSection').at(3).props().className).toContain('customHook');
 
-      expect(component.find('.markdownViewer').at(3).find(ItemList).props().link).toBe('/library/param2');
-      expect(component.find('.markdownViewer').at(3).find(ItemList).props().items).toEqual(props.itemLists.getIn([1, 'items']).toJS());
+      expect(component.find('.pageSection').at(4).props().dangerouslySetInnerHTML.__html).toContain('markdown between hooks');
 
-      expect(component.find('.markdownViewer').at(4).find(ItemList).props().link).toBe('/library/param3');
-      expect(component.find('.markdownViewer').at(4).find(ItemList).props().items).toEqual(props.itemLists.getIn([2, 'items']).toJS());
+      expect(component.find('.pageSection').at(5).find(ItemList).props().link).toBe('/library/param2');
+      expect(component.find('.pageSection').at(5).find(ItemList).props().items).toEqual(props.itemLists.getIn([1, 'items']).toJS());
 
-      expect(component.find('.markdownViewer').at(5).props().dangerouslySetInnerHTML.__html).toContain('code');
+      expect(component.find('.pageSection').at(6).props().className).toContain('customHook');
+
+      expect(component.find('.pageSection').at(7).find(ItemList).props().link).toBe('/library/param3');
+      expect(component.find('.pageSection').at(7).find(ItemList).props().items).toEqual(props.itemLists.getIn([2, 'items']).toJS());
+
+      expect(component.find('.pageSection').at(8).props().className).toContain('customHook error');
+
+      expect(component.find('.pageSection').at(9).props().dangerouslySetInnerHTML.__html).toContain('code');
+
+      expect(component.find(CustomHookComponents.validcomponent).at(0).props())
+      .toEqual({className: 'pageSection customHook', component: 'validcomponent', prop: 'a prop'});
+
+      expect(component.find(CustomHookComponents.validcomponent).at(1).props())
+      .toEqual({className: 'pageSection customHook', component: 'validcomponent'});
+
+      expect(component.find('.customHook.error').at(0).text()).toContain('Custom Hook markup error');
     });
 
     it('should not fail if itemLists lenght is less (because of rendering empty or with props not loaded)', () => {
@@ -76,10 +102,10 @@ describe('PageViewer', () => {
 
       render();
 
-      expect(component.find('.markdownViewer').at(3).find(ItemList).props().link).toBe('/library/param3');
-      expect(component.find('.markdownViewer').at(3).find(ItemList).props().items).toEqual(props.itemLists.getIn([1, 'items']).toJS());
+      expect(component.find('.markdownViewer').at(4).find(ItemList).props().link).toBe('/library/param3');
+      expect(component.find('.markdownViewer').at(4).find(ItemList).props().items).toEqual(props.itemLists.getIn([1, 'items']).toJS());
 
-      expect(component.find('.markdownViewer').at(4).props().dangerouslySetInnerHTML.__html).toContain('code');
+      expect(component.find('.markdownViewer').at(5).props().dangerouslySetInnerHTML.__html).toContain('code');
     });
   });
 });
