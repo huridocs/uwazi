1bc7fa1cb01e0bd0e1f0d335641ed20bbd545ef0
daneryl <daneryl@gmail.com>
2017-08-21 12:47:17 +0200

    updated webpack to 3.5.5

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -20,14 +20,14 @@ config.plugins = config.plugins.concat([
     }
   }),
   new webpack.optimize.AggressiveMergingPlugin(),
-  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-  new CompressionPlugin({
-    asset: "[path].gz[query]",
-    algorithm: "gzip",
-    test: /\.js$|\.css$|\.html$/,
-    threshold: 10240,
-    minRatio: 0.8
-  })
+  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
+  //new CompressionPlugin({
+    //asset: "[path].gz[query]",
+    //algorithm: "gzip",
+    //test: /\.js$|\.css$|\.html$/,
+    //threshold: 10240,
+    //minRatio: 0.8
+  //})
 ])
 
 module.exports = config;
