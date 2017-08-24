e1f94502d57ef9091cdbb5ecae1d25e257e587a8
daneryl <daneryl@gmail.com>
2017-08-24 11:30:27 +0200

    removed unused webpack build code

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -22,13 +22,6 @@ config.plugins = config.plugins.concat([
   }),
   new webpack.optimize.AggressiveMergingPlugin(),
   new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
-  //new CompressionPlugin({
-    //asset: "[path].gz[query]",
-    //algorithm: "gzip",
-    //test: /\.js$|\.css$|\.html$/,
-    //threshold: 10240,
-    //minRatio: 0.8
-  //})
 ])
 
 module.exports = config;
