a073a4bf78eaff82389af5bf102b3d7138444607
daneryl <daneryl@gmail.com>
2017-09-05 12:50:38 +0200

    google fonts css loading on the head

--- app/react/App/Root.js
+++ app/react/App/Root.js
@@ -42,6 +42,9 @@ class Root extends Component {
             rel="stylesheet"
             type="text/css"
           />
+          <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700|Roboto+Slab:100,300,400,700|Roboto:100,300,400,500,700,900"
+          />
           <link rel="shortcut icon" href="/public/favicon.ico"/>
         </head>
         <body>
