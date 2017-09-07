5bfc47f1e4b3b10bc6b6b990e364e757a8382350
RafaPolit <rafaelpolit@gmail.com>
2017-09-07 11:20:37 -0500

    Removed singled out tests.

--- app/react/utils/specs/marked.spec.js
+++ app/react/utils/specs/marked.spec.js
@@ -5,7 +5,7 @@ describe('marked (extended Marked util)', () => {
     expect(marked('#Header\n\nText')).toBe('<p>#Header</p>\n<p>Text</p>\n');
   });
 
-  fdescribe('YouTube markup', () => {
+  describe('YouTube markup', () => {
     it('should append a youtube iframe inside a video container div', () => {
       const markup = 'First paragraph\n\n{youtube}(https://www.youtube.com/watch?v=Pd4K2CtaVBo)\n\n' +
                      'Second paragraph\n\n{youtube}(https://youtu.be/qzzM4PT2Av8)';
@@ -22,7 +22,7 @@ describe('marked (extended Marked util)', () => {
     });
   });
 
-  fdescribe('Vimeo markup', () => {
+  describe('Vimeo markup', () => {
     it('should append a vimeo iframe inside a video container div', () => {
       const markup = 'First paragraph\n\n{vimeo}(http://vimeo.com/6701902)\n\n' +
                      'Second paragraph\n\n{youtube}(https://youtu.be/qzzM4PT2Av8)\n\n' +
@@ -31,7 +31,6 @@ describe('marked (extended Marked util)', () => {
                      '{vimeo}(http://vimeo.com/channels/staffpicks/67019026)\n\n' +
                      '{vimeo}(https://vimeo.com/15414122)';
 
-      /* <iframe src="https://player.vimeo.com/video/222826119" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> */
       expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/6701902');
       expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://www.youtube.com/embed/qzzM4PT2Av8');
       expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/67019023');
