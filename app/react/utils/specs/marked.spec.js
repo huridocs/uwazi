8902fd4f6398157c697da68ee1ec3395a0252885
RafaPolit <rafaelpolit@gmail.com>
2017-09-07 11:02:26 -0500

    Added vimeo support to markdown fields.

--- app/react/utils/specs/marked.spec.js
+++ app/react/utils/specs/marked.spec.js
@@ -5,7 +5,7 @@ describe('marked (extended Marked util)', () => {
     expect(marked('#Header\n\nText')).toBe('<p>#Header</p>\n<p>Text</p>\n');
   });
 
-  describe('YouTube markup', () => {
+  fdescribe('YouTube markup', () => {
     it('should append a youtube iframe inside a video container div', () => {
       const markup = 'First paragraph\n\n{youtube}(https://www.youtube.com/watch?v=Pd4K2CtaVBo)\n\n' +
                      'Second paragraph\n\n{youtube}(https://youtu.be/qzzM4PT2Av8)';
@@ -17,7 +17,34 @@ describe('marked (extended Marked util)', () => {
       const markup = 'First paragraph {youtube}(https://www.youtube.com/watch?v=Pd4K2CtaVBo)\n\n' +
                      'Second paragraph\n\n{youtube}(https://youtu.be/qzzM4PT2Av8)';
       expect(marked(markup)).not.toContain('<div class="video-container"><iframe src="https://www.youtube.com/embed/Pd4K2CtaVBo');
-      expect(marked(markup)).toContain('<br /><strong><i>YouTube markup error:');
+      expect(marked(markup)).toContain('<br /><strong><i>youtube markup error:');
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://www.youtube.com/embed/qzzM4PT2Av8');
+    });
+  });
+
+  fdescribe('Vimeo markup', () => {
+    it('should append a vimeo iframe inside a video container div', () => {
+      const markup = 'First paragraph\n\n{vimeo}(http://vimeo.com/6701902)\n\n' +
+                     'Second paragraph\n\n{youtube}(https://youtu.be/qzzM4PT2Av8)\n\n' +
+                     '{vimeo}(http://player.vimeo.com/video/67019023)\n\n' +
+                     'Third paragraph\n\n{vimeo}(http://player.vimeo.com/video/6775546?title=0&byline=0&portrait=0)\n\n' +
+                     '{vimeo}(http://vimeo.com/channels/staffpicks/67019026)\n\n' +
+                     '{vimeo}(https://vimeo.com/15414122)';
+
+      /* <iframe src="https://player.vimeo.com/video/222826119" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> */
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/6701902');
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://www.youtube.com/embed/qzzM4PT2Av8');
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/67019023');
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/6775546');
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/67019026');
+      expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/15414122');
+    });
+
+    it('should append an error if video embeded in middle of a paragraph', () => {
+      const markup = 'First paragraph {vimeo}(http://vimeo.com/6701902)\n\n' +
+                     'Second paragraph\n\n{youtube}(https://youtu.be/qzzM4PT2Av8)';
+      expect(marked(markup)).not.toContain('<div class="video-container"><iframe src="https://player.vimeo.com/video/6701902');
+      expect(marked(markup)).toContain('<br /><strong><i>vimeo markup error:');
       expect(marked(markup)).toContain('<div class="video-container"><iframe src="https://www.youtube.com/embed/qzzM4PT2Av8');
     });
   });
