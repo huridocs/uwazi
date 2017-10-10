55b56ab8396b67f8fb90da6573ae33afd10300e4
A Happy Cat <acasadotorres@gmail.com>
2017-10-10 10:57:36 +0200

    User role is no longer unique as intended

--- app/api/users/usersModel.js
+++ app/api/users/usersModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   username: {type: String, unique: true, required: true},
   password: {type: String, select: false, required: true},
   email: {type: String, unique: true, required: true},
-  role: {type: String, unique: true, required: true}
+  role: {type: String, unique: false, required: true}
 });
 
 let Model = mongoose.model('users', userSchema);
