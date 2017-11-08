dd64f8b4b967ae28a3ad592c18e4d0c27ef6817c
daneryl <daneryl@gmail.com>
2017-11-08 17:50:08 +0100

    added 2 new documents to the cejil timeline

--- app/react/Timeline/utils/timelineFixedData.js
+++ app/react/Timeline/utils/timelineFixedData.js
@@ -19,6 +19,8 @@ export default {
     judgement: '58b2f3a35d59f31e1345b4ac',
     admissibilityReport: '58b2f3a35d59f31e1345b479',
     orderOfThePresident: '58b2f3a35d59f31e1345b482',
-    orderOfTheCourt: '58b2f3a35d59f31e1345b471'
+    orderOfTheCourt: '58b2f3a35d59f31e1345b471',
+    letterOfSubmission: '59ee461f40f4a54920bd9b87',
+    meritsReport: '59ee427f40f4a54920bd9b67'
   }
 };
