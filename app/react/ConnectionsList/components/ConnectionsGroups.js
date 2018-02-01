54aa030c13e1eb318f8dfc971b0a80e8b8f33cfb
RafaPolit <rafaelpolit@gmail.com>
2018-02-01 17:50:23 -0500

    Added blank state to ConnectionsGroups.

--- app/react/ConnectionsList/components/ConnectionsGroups.js
+++ app/react/ConnectionsList/components/ConnectionsGroups.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
-import ShowIf from 'app/App/ShowIf';
+import {t} from 'app/I18N';
 
 import ConnectionsGroup from './ConnectionsGroup';
 
@@ -10,20 +10,26 @@ export class ConnectionsGroups extends Component {
   render() {
     const {connectionsGroups} = this.props;
 
-    return (
-      <ShowIf if={Boolean(connectionsGroups.size)}>
-        <div>
-          <div className="nested-selector">
-            <ul className="multiselect is-active">
-              {connectionsGroups.map(group =>
-                <ConnectionsGroup key={group.get('key')}
-                                  group={group} />
-              )}
-            </ul>
-          </div>
-        </div>
-      </ShowIf>
-    );
+    let Results = <div className="blank-state">
+                    <i className="fa fa-exchange"></i>
+                    <h4>{t('System', 'No Relationships')}</h4>
+                    <p>{t('System', 'No Relationships description')}</p>
+                  </div>;
+
+    if (connectionsGroups.size) {
+      Results = <div>
+                  <div className="nested-selector">
+                    <ul className="multiselect is-active">
+                      {connectionsGroups.map(group =>
+                        <ConnectionsGroup key={group.get('key')}
+                                          group={group} />
+                      )}
+                    </ul>
+                  </div>
+                </div>;
+    }
+
+    return Results;
   }
 }
 
