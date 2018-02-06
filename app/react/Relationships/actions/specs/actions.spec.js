1917916187c3da8fc3fb70836a3d854a037e18cc
RafaPolit <rafaelpolit@gmail.com>
2018-02-06 10:46:31 -0500

    Begun testing actions.

--- app/react/Relationships/actions/specs/actions.spec.js
+++ app/react/Relationships/actions/specs/actions.spec.js
@@ -1,15 +1,24 @@
+import * as types from '../actionTypes';
+
 import * as actions from '../actions';
 
-describe('ConnectionsList actions', () => {
+fdescribe('Relationships actions', () => {
+  describe('parseResults', () => {
+    it('should pass action with results, parentEntity and editing boolean value', () => {
+      expect(actions.parseResults('results', 'parentEntity', true))
+      .toEqual({type: types.PARSE_RELATIONSHIPS_RESULTS, results: 'results', parentEntity: 'parentEntity', editing: true});
+    });
+  });
+
   describe('selectConnection', () => {
     it('should set the connection in the state', () => {
-      expect(actions.selectConnection('connection')).toEqual({type: 'connectionsList/connection/SET', value: 'connection'});
+      expect(actions.selectConnection('connection')).toEqual({type: 'relationships/connection/SET', value: 'connection'});
     });
   });
 
   describe('unselectConnection', () => {
     it('should set the connection in the state', () => {
-      expect(actions.unselectConnection()).toEqual({type: 'connectionsList/connection/SET', value: {}});
+      expect(actions.unselectConnection()).toEqual({type: 'relationships/connection/SET', value: {}});
     });
   });
 });
