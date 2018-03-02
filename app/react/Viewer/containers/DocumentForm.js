84d746c418bf4e58486a61bbbd62af4d9a092ff8
Daneryl <daneryl@gmail.com>
2018-03-02 09:36:33 +0100

    Document and EntityForm reload relationships

--- app/react/Viewer/containers/DocumentForm.js
+++ app/react/Viewer/containers/DocumentForm.js
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {actions, MetadataForm} from 'app/Metadata';
+import {actions as relationshipActions} from 'app/Relationships';
 import {saveDocument} from '../actions/documentActions';
 
 function mapStateToProps({documentViewer, templates, thesauris}) {
@@ -14,7 +15,17 @@ function mapStateToProps({documentViewer, templates, thesauris}) {
 }
 
 export function mapDispatchToProps(dispatch) {
-  return bindActionCreators({changeTemplate: actions.changeTemplate, onSubmit: saveDocument}, dispatch);
+  return bindActionCreators({
+    changeTemplate: actions.changeTemplate,
+    onSubmit: (doc) => {
+      return (disp) => {
+        return saveDocument(doc)(disp)
+        .then(() => {
+          disp(relationshipActions.reloadRelationships(doc.sharedId));
+        });
+      };
+    }
+  }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MetadataForm);
