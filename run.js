cc2b238c8ae23c111d6d24ea81b45610cac75c40
Daneryl <daneryl@gmail.com>
2018-03-23 08:26:14 +0100

    throw an error instead of a a console.log on run.js

--- run.js
+++ run.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 if (process.argv[2]) {
   const script = process.argv[2];
   if (!fs.existsSync(script)) {
-    console.log(`Script ${process.argv[2]} does not exist !!`);
-    process.exit();
+    throw new Error(`Script ${process.argv[2]} does not exist !!`);
   }
   require(script);
 }
