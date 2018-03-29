7a51b82728e11da8454dee8f6a06015c1d580549
Daneryl <daneryl@gmail.com>
2018-03-29 16:16:14 +0200

    added defaultFilter checkbox to relationship property config

--- app/react/Templates/components/FormConfigRelationship.js
+++ app/react/Templates/components/FormConfigRelationship.js
@@ -1,32 +1,30 @@
+import { Field } from 'react-redux-form';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+import { Select } from 'app/ReactReduxForms';
+import { t } from 'app/I18N';
 import FilterSuggestions from 'app/Templates/components/FilterSuggestions';
-import {Select} from 'app/ReactReduxForms';
-import {connect} from 'react-redux';
-import {Field} from 'react-redux-form';
-import {t} from 'app/I18N';
 
 export class FormConfigRelationship extends Component {
-
-  contentValidation() {
-    return {required: (val) => val && val.trim() !== ''};
+  static contentValidation() {
+    return { required: val => val && val.trim() !== '' };
   }
 
   render() {
-    const {index, data, formState} = this.props;
+    const { index, data, formState } = this.props;
     const thesauris = this.props.thesauris.toJS();
-    const ptoperty = data.properties[index];
+    const property = data.properties[index];
     const relationTypes = this.props.relationTypes.toJS();
 
-    const options = thesauris.filter((thesauri) => {
-      return thesauri._id !== data._id && thesauri.type === 'template';
-    });
+    const options = thesauris.filter(thesauri => thesauri._id !== data._id && thesauri.type === 'template');
 
     let labelClass = 'form-group';
-    let labelKey = `properties.${index}.label`;
-    let requiredLabel = formState.$form.errors[labelKey + '.required'];
-    let duplicatedLabel = formState.$form.errors[labelKey + '.duplicated'];
-    let relationTypeError = formState.$form.errors[`properties.${index}.relationType.required`] && formState.$form.submitFailed;
+    const labelKey = `properties.${index}.label`;
+    const requiredLabel = formState.$form.errors[`${labelKey}.required`];
+    const duplicatedLabel = formState.$form.errors[`${labelKey}.duplicated`];
+    const relationTypeError = formState.$form.errors[`properties.${index}.relationType.required`] && formState.$form.submitFailed;
     if (requiredLabel || duplicatedLabel) {
       labelClass += ' has-error';
     }
@@ -42,26 +40,30 @@ export class FormConfigRelationship extends Component {
 
         <div className="form-group">
           <label>{t('System', 'Select list')}</label>
-          <Select model={`template.data.properties[${index}].content`}
-                  options={options}
-                  optionsLabel="name"
-                  placeholder="Any entity or document"
-                  optionsValue="_id" />
+          <Select
+            model={`template.data.properties[${index}].content`}
+            options={options}
+            optionsLabel="name"
+            placeholder="Any entity or document"
+            optionsValue="_id"
+          />
         </div>
 
         <div className={relationTypeError ? 'form-group has-error' : 'form-group'}>
           <label>{t('System', 'Relationship')}<span className="required">*</span></label>
-          <Select model={`template.data.properties[${index}].relationType`}
-                  options={relationTypes}
-                  optionsLabel="name"
-                  validators={this.contentValidation()}
-                  optionsValue="_id" />
+          <Select
+            model={`template.data.properties[${index}].relationType`}
+            options={relationTypes}
+            optionsLabel="name"
+            validators={FormConfigRelationship.contentValidation()}
+            optionsValue="_id"
+          />
         </div>
 
         <Field model={`template.data.properties[${index}].required`}>
-          <input id={'required' + this.props.index} type="checkbox"/>
+          <input id={`required${this.props.index}`} type="checkbox"/>
           &nbsp;
-          <label className="property-label" htmlFor={'required' + this.props.index}>
+          <label className="property-label" htmlFor={`required${this.props.index}`}>
             Required property
             <i className="property-help fa fa-question-circle">
               <div className="property-description">You won't be able to publish a document if this property is empty.</div>
@@ -70,9 +72,9 @@ export class FormConfigRelationship extends Component {
         </Field>
 
         <Field model={`template.data.properties[${index}].showInCard`}>
-          <input id={'showInCard' + this.props.index} type="checkbox"/>
+          <input id={`showInCard${this.props.index}`} type="checkbox"/>
           &nbsp;
-          <label className="property-label" htmlFor={'showInCard' + this.props.index}>
+          <label className="property-label" htmlFor={`showInCard${this.props.index}`}>
             Show in cards
             <i className="property-help fa fa-question-circle">
               <div className="property-description">This property will appear in the library cards as part of the basic info.</div>
@@ -82,9 +84,9 @@ export class FormConfigRelationship extends Component {
 
         <div>
           <Field model={`template.data.properties[${index}].filter`}>
-            <input id={'filter' + this.props.index} type="checkbox"/>
+            <input id={`filter${this.props.index}`} type="checkbox"/>
             &nbsp;
-            <label className="property-label" htmlFor={'filter' + this.props.index}>
+            <label className="property-label" htmlFor={`filter${this.props.index}`}>
               Use as filter
               <i className="property-help fa fa-question-circle">
                 <div className="property-description">
@@ -94,9 +96,28 @@ export class FormConfigRelationship extends Component {
               </i>
             </label>
           </Field>
-          <FilterSuggestions {...ptoperty} />
+          {property.filter && (
+            <Field className="filter" model={`template.data.properties[${index}].defaultfilter`}>
+              <input
+                id={`defaultfilter${this.props.index}`}
+                type="checkbox"
+                disabled={!property.filter}
+              />
+              &nbsp;
+              <label className="property-label" htmlFor={`defaultfilter${this.props.index}`}>
+                Default filter
+                <i className="property-help fa fa-question-circle">
+                  <div className="property-description">
+                    Use this property as a default filter in the library.
+                    When there are no document types selected, this property will show as a default filter for your collection.
+                  </div>
+                </i>
+              </label>
+            </Field>
+          )
+          }
+          <FilterSuggestions {...property} />
         </div>
-
       </div>
     );
   }
@@ -107,8 +128,7 @@ FormConfigRelationship.propTypes = {
   relationTypes: PropTypes.object,
   data: PropTypes.object,
   index: PropTypes.number,
-  formState: PropTypes.object,
-  formKey: PropTypes.string
+  formState: PropTypes.object
 };
 
 export function mapStateToProps(state) {
