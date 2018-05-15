b17433f9d635942666eb896648279d1a4d5d5e73
A Happy Cat <acasadotorres@gmail.com>
2018-05-15 20:25:41 +0200

    fixed problem when saving geolocation fields empty

---
+++ app/api/migrations/migrations/2-sanitize_empty_geolocations/index.js
@@ -0,0 +1,41 @@
+export default {
+  delta: 2,
+
+  name: 'sanitize_empty_geolocations',
+
+  description: 'Delete empty geolocation properties',
+
+  up(db) {
+    process.stdout.write(`${this.name}...\r\n`);
+    return new Promise((resolve, reject) => {
+      const cursor = db.collection('entities').find();
+      let index = 1;
+      cursor.on('data', (entity) => {
+        cursor.pause();
+        const metadataProperties = Object.keys(entity.metadata);
+        const metadata = metadataProperties.reduce((_metadata, k) => {
+          const isGeolocation = k.indexOf('_geolocation') > -1;
+          const value = entity.metadata[k];
+          const noLat = value && !value.lat;
+          const noLon = value && !value.lon;
+          if (isGeolocation && noLat && noLon) {
+            return _metadata;
+          }
+          _metadata[k] = entity.metadata[k];
+          return _metadata;
+        }, {});
+        db.collection('entities').findOneAndUpdate(entity, { $set: { metadata } }, () => {
+          process.stdout.write(`processed -> ${index}\r`);
+          index += 1;
+          cursor.resume();
+        });
+      });
+
+      cursor.on('err', reject);
+      cursor.on('end', () => {
+        process.stdout.write(`processed -> ${index}\r\n`);
+        resolve();
+      });
+    });
+  }
+};
---
+++ app/api/migrations/migrations/2-sanitize_empty_geolocations/specs/fixtures.js
@@ -0,0 +1,6 @@
+export default {
+  entities: [
+    { title: 'test_doc', metadata: { geolocation_geolocation: {}, description: 'one' } },
+    { title: 'test_doc2', metadata: { geolocation_geolocation: {}, description: 'two' } }
+  ]
+};
