b83fe9545c3c749f9ed91fa2560cca329f71e108
A Happy Cat <acasadotorres@gmail.com>
2018-06-01 15:13:05 +0200

    Thesauri form with capability to create groups

--- app/react/Thesauris/actions/thesauriActions.js
+++ app/react/Thesauris/actions/thesauriActions.js
@@ -1,5 +1,5 @@
-import {actions as formActions} from 'react-redux-form';
-import {t} from 'app/I18N';
+import { actions as formActions } from 'react-redux-form';
+import { t } from 'app/I18N';
 
 import * as types from 'app/Thesauris/actions/actionTypes';
 import api from 'app/Thesauris/ThesaurisAPI';
@@ -7,26 +7,39 @@ import * as notifications from 'app/Notifications/actions/notificationsActions';
 
 
 export function saveThesauri(thesauri) {
-  return function (dispatch) {
-    return api.save(thesauri).then((_thesauri) => {
-      dispatch({type: types.THESAURI_SAVED});
-      notifications.notify(t('System', 'Thesaurus saved'), 'success')(dispatch);
-      dispatch(formActions.change('thesauri.data', _thesauri));
-    });
+  return dispatch => api.save(thesauri).then((_thesauri) => {
+    dispatch({ type: types.THESAURI_SAVED });
+    notifications.notify(t('System', 'Thesaurus saved'), 'success')(dispatch);
+    dispatch(formActions.change('thesauri.data', _thesauri));
+  });
+}
+
+export function addValue(group) {
+  return (dispatch, getState) => {
+    const values = getState().thesauri.data.values.slice(0);
+    if (group !== undefined) {
+      values[group] = Object.assign({}, values[group]);
+      values[group].values = values[group].values.slice(0);
+      values[group].values.push({ label: '' });
+    } else {
+      values.push({ label: '' });
+    }
+
+    dispatch(formActions.change('thesauri.data.values', values));
   };
 }
 
-export function addValue() {
-  return function (dispatch, getState) {
-    let values = getState().thesauri.data.values.slice(0);
-    values.push({label: ''});
+export function addGroup() {
+  return (dispatch, getState) => {
+    const values = getState().thesauri.data.values.slice(0);
+    values[values.length - 1] = { label: '', values: [{ label: '' }] };
     dispatch(formActions.change('thesauri.data.values', values));
   };
 }
 
 export function removeValue(index) {
-  return function (dispatch, getState) {
-    let values = getState().thesauri.data.values.slice(0);
+  return (dispatch, getState) => {
+    const values = getState().thesauri.data.values.slice(0);
     values.splice(index, 1);
     dispatch(formActions.change('thesauri.data.values', values));
   };
--- app/react/Thesauris/components/ThesauriForm.js
+++ app/react/Thesauris/components/ThesauriForm.js
@@ -1,108 +1,156 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import {bindActionCreators} from 'redux';
-import {Field, Form, actions as formActions} from 'react-redux-form';
-import {connect} from 'react-redux';
-import {I18NLink} from 'app/I18N';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { Field, Form, actions as formActions } from 'react-redux-form';
+import { connect } from 'react-redux';
+import { I18NLink } from 'app/I18N';
 import ShowIf from 'app/App/ShowIf';
 import 'app/Thesauris/scss/thesauris.scss';
-import {notEmpty} from 'app/Metadata/helpers/validator';
+import { notEmpty } from 'app/Metadata/helpers/validator';
 
 import FormGroup from 'app/DocumentForm/components/FormGroup';
-import {saveThesauri, addValue, removeValue} from 'app/Thesauris/actions/thesauriActions';
+import { saveThesauri, addValue, removeValue, addGroup } from 'app/Thesauris/actions/thesauriActions';
 
 export class ThesauriForm extends Component {
+  static validation(thesauris, id) {
+    return {
+      name: {
+        duplicated: val => !thesauris.find(thesauri => thesauri._id !== id && thesauri.name.trim().toLowerCase() === val.trim().toLowerCase()),
+        required: notEmpty
+      }
+    };
+  }
+
+  componentWillReceiveProps(props) {
+    props.thesauri.values.forEach((value, index) => {
+      if (value.values && value.values[value.values.length - 1].label !== '') {
+        props.addValue(index);
+      }
+    });
+
+    if (props.thesauri.values.length && props.thesauri.values[props.thesauri.values.length - 1].label !== '') {
+      props.addValue();
+    }
+  }
 
   componentWillUnmount() {
     this.props.resetForm('thesauri.data');
     this.props.setInitial('thesauri.data');
   }
 
-  validation(thesauris, id) {
-    return {
-      name: {
-        duplicated: (val) => {
-          return !thesauris.find((thesauri) => {
-            return thesauri._id !== id && thesauri.name.trim().toLowerCase() === val.trim().toLowerCase();
-          });
-        },
-        required: notEmpty
-      }
-    };
+  renderGroup(value, groupIndex) {
+    return (
+      <ul key={`group-${groupIndex}`} className="sub-group">
+        <li className="list-group-item list-group-name">
+          <FormGroup>
+            <Field model={`thesauri.data.values[${groupIndex}].label`}>
+              <input className="form-control" type="text" placeholder="Group name" />
+              <button type="button" className="btn btn-xs btn-danger" onClick={this.props.removeValue.bind(null, groupIndex)}>
+                <i className="fa fa-trash" /> Delete
+              </button>
+            </Field>
+          </FormGroup>
+        </li>
+        <li className="list-group-item"><b>Items:</b></li>
+        {value.values.map((_value, index) => (
+          this.renderValue(_value, index, groupIndex)
+        ))}
+      </ul>
+    );
+  }
+
+  renderValue(value, index, groupIndex) {
+    if (value.values) {
+      return this.renderGroup(value, index);
+    }
+
+    let model = `thesauri.data.values[${index}].label`;
+    if (groupIndex) {
+      model = `thesauri.data.values[${groupIndex}].values[${index}].label`;
+    }
+
+    return (
+      <li key={`item-${groupIndex || ''}${index}`} className="list-group-item">
+        <FormGroup>
+          <Field model={model}>
+            <input className="form-control" type="text" placeholder="Item name" />
+            <button type="button" className="btn btn-xs btn-danger" onClick={this.props.removeValue.bind(null, index, groupIndex)}>
+              <i className="fa fa-trash" /> Delete
+            </button>
+          </Field>
+        </FormGroup>
+      </li>
+    );
   }
 
   render() {
-    let isNew = this.props.new;
-    let id = this.props.thesauri._id;
+    const isNew = this.props.new;
+    const id = this.props.thesauri._id;
     if (!isNew && !id) {
       return false;
     }
 
     return (
       <div className="thesauri">
-          <Form
-            model="thesauri.data"
-            onSubmit={this.props.saveThesauri}
-            validators={this.validation(this.props.thesauris.toJS(), this.props.thesauri._id)}
-          >
-            <div className="panel panel-default thesauri">
-              <div className="panel-heading">
-                <FormGroup {...this.props.state.name} submitFailed={this.props.state.submitFailed}>
-                  <Field model=".name">
-                    <input id="thesauriName" className="form-control" type="text" placeholder="Thesauri name" />
-                    <ShowIf if={this.props.state.$form.touched && this.props.state.name && this.props.state.name.errors.duplicated}>
-                      <div className="validation-error">
-                        <i className="fa fa-exclamation-triangle"></i>&nbsp;Duplicated name
-                      </div>
-                    </ShowIf>
-                  </Field>
-                </FormGroup>
-              </div>
-              <ul className="thesauri-values list-group">
-                <li className="list-group-item"><b>Items:</b></li>
-                {this.props.thesauri.values.map((value, index) => {
-                  return <li key={index} className="list-group-item">
-                          <FormGroup key={index}>
-                            <Field model={`thesauri.data.values[${index}].label`}>
-                              <input className="form-control" type="text" placeholder="Item name" />
-                              <a className="btn btn-xs btn-danger" onClick={this.props.removeValue.bind(null, index)}>
-                                <i className="fa fa-trash-alt"></i> Delete
-                              </a>
-                            </Field>
-                          </FormGroup>
-                        </li>;
-                })}
-              </ul>
-              <div className="settings-footer">
-                <I18NLink to="/settings/dictionaries" className="btn btn-default">
-                  <i className="fa fa-arrow-left"></i>
-                  <span className="btn-label">Back</span>
-                </I18NLink>
-                <a className="btn btn-primary" onClick={this.props.addValue}>
-                  <i className="fa fa-plus"></i>
-                  <span className="btn-label">Add item</span>
-                </a>
-                <button className="btn btn-success save-template">
-                  <i className="far fa-save"/>
-                  <span className="btn-label">Save</span>
-                </button>
-              </div>
+        <Form
+          model="thesauri.data"
+          onSubmit={this.props.saveThesauri}
+          validators={ThesauriForm.validation(this.props.thesauris.toJS(), this.props.thesauri._id)}
+        >
+          <div className="panel panel-default thesauri">
+            <div className="panel-heading">
+              <FormGroup {...this.props.state.name} submitFailed={this.props.state.submitFailed}>
+                <Field model=".name">
+                  <input id="thesauriName" className="form-control" type="text" placeholder="Thesauri name" />
+                  <ShowIf if={this.props.state.$form.touched && this.props.state.name && this.props.state.name.errors.duplicated}>
+                    <div className="validation-error">
+                      <i className="fa fa-exclamation-triangle" />&nbsp;Duplicated name
+                    </div>
+                  </ShowIf>
+                </Field>
+              </FormGroup>
             </div>
-          </Form>
+            <ul className="thesauri-values list-group">
+              <li className="list-group-item"><b>Items:</b></li>
+              {this.props.thesauri.values.map((value, index) => (
+                this.renderValue(value, index)
+              ))}
+            </ul>
+            <div className="settings-footer">
+              <I18NLink to="/settings/dictionaries" className="btn btn-default">
+                <i className="fa fa-arrow-left" />
+                <span className="btn-label">Back</span>
+              </I18NLink>
+              <a className="btn btn-primary" onClick={this.props.addGroup}>
+                <i className="fa fa-plus" />
+                <span className="btn-label">Add group</span>
+              </a>
+              <button className="btn btn-success save-template">
+                <i className="fa fa-save"/>
+                <span className="btn-label">Save</span>
+              </button>
+            </div>
+          </div>
+        </Form>
       </div>
     );
   }
 }
 
+ThesauriForm.defaultTypes = {
+  new: false
+};
+
 ThesauriForm.propTypes = {
-  resetForm: PropTypes.func,
-  setInitial: PropTypes.func,
-  saveThesauri: PropTypes.func,
-  addValue: PropTypes.func,
-  removeValue: PropTypes.func,
-  thesauris: PropTypes.object,
-  thesauri: PropTypes.object,
-  state: PropTypes.object,
+  resetForm: PropTypes.func.isRequired,
+  setInitial: PropTypes.func.isRequired,
+  saveThesauri: PropTypes.func.isRequired,
+  addValue: PropTypes.func.isRequired,
+  addGroup: PropTypes.func.isRequired,
+  removeValue: PropTypes.func.isRequired,
+  thesauris: PropTypes.object.isRequired,
+  thesauri: PropTypes.object.isRequired,
+  state: PropTypes.object.isRequired,
   new: PropTypes.bool
 };
 
@@ -118,6 +166,7 @@ function bindActions(dispatch) {
   return bindActionCreators({
     saveThesauri,
     addValue,
+    addGroup,
     removeValue,
     resetForm: formActions.reset,
     setInitial: formActions.setInitial,
@@ -125,6 +174,6 @@ function bindActions(dispatch) {
   }, dispatch);
 }
 
-let form = connect(mapStateToProps, bindActions)(ThesauriForm);
+const form = connect(mapStateToProps, bindActions)(ThesauriForm);
 
 export default form;
