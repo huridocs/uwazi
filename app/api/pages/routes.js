a6e3d4a0ddbcc2380772c264d53d89c359c54114
RafaPolit <rafaelpolit@gmail.com>
2018-06-07 15:26:49 -0500

    Reverted changes that, uncomprehensibly, fail.

--- app/api/pages/routes.js
+++ app/api/pages/routes.js
@@ -2,27 +2,23 @@ import pages from './pages';
 import needsAuthorization from '../auth/authMiddleware';
 
 export default (app) => {
-  app.post('/api/pages', needsAuthorization(), (req, res) => {
-    return pages.save(req.body, req.user, req.language)
-    .then(response => res.json(response))
-    .catch(error => res.json({error}));
-  });
+  app.post('/api/pages', needsAuthorization(), (req, res) => pages.save(req.body, req.user, req.language)
+  .then(response => res.json(response))
+  .catch(error => res.json({ error })));
 
-  app.get('/api/pages/list', (req, res) => {
-    return pages.get({language: req.language})
-    .then(response => res.json({rows: response}))
-    .catch(error => res.json({error}));
-  });
+  app.get('/api/pages/list', (req, res) => pages.get({ language: req.language })
+  .then(response => res.json({ rows: response }))
+  .catch(error => res.json({ error })));
 
   app.get('/api/pages', (req, res) => {
     pages.getById(req.query.sharedId, req.language)
-    .then(res.json)
-    .catch(res.error);
+    .then(response => res.json(response))
+    .catch(err => res.error(err));
   });
 
   app.delete('/api/pages', needsAuthorization(), (req, res) => {
     pages.delete(req.query.sharedId)
     .then(response => res.json(response))
-    .catch(error => res.json({error}));
+    .catch(error => res.json({ error }));
   });
 };
