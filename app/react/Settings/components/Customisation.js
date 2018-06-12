3bbf40d1e856232d9a83d7951081bf5e2b392096
Daneryl <daneryl@gmail.com>
2018-06-12 14:36:23 +0200

    customisation form now loading settings on load

--- app/react/Settings/components/Customisation.js
+++ app/react/Settings/components/Customisation.js
@@ -1,18 +1,27 @@
+import { Field, Form, actions as formActions } from 'react-redux-form';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Immutable from 'immutable';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Form, Field } from 'react-redux-form';
+
 import { t } from 'app/I18N';
 
 export class Customisation extends Component {
+  componentDidMount() {
+    this.props.loadForm('settings', this.props.settings.toJS());
+  }
+
   render() {
     return (
       <div className="panel panel-default">
         <div className="panel-heading">{t('System', 'Customisation')}</div>
         <div className="panel-body" />
-        <Form model="settings" >
+        <Form model="settings.settings" >
           <div className="form-group">
             <label className="form-group-label" htmlFor="collection_name">{t('System', 'Custom CSS')}</label>
             <Field model=".customCSS">
-              <textarea className="form-control" rows="30" ></textarea>
+              <textarea className="form-control" rows="30" />
             </Field>
           </div>
         </Form>
@@ -21,4 +30,12 @@ export class Customisation extends Component {
   }
 }
 
-export default Customisation;
+Customisation.propTypes = {
+  loadForm: PropTypes.func.isRequired,
+  settings: PropTypes.instanceOf(Immutable.Map).isRequired
+};
+
+const mapStateToProps = state => ({ settings: state.settings.collection });
+const mapDispatchToProps = dispatch => bindActionCreators({ loadForm: formActions.load }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Customisation);
--- app/react/Settings/components/specs/Customisation.spec.js
+++ app/react/Settings/components/specs/Customisation.spec.js
@@ -1,12 +1,27 @@
+import Immutable from 'immutable';
 import React from 'react';
 
 import { shallow } from 'enzyme';
 
-import Customisation from '../Customisation';
+import { Customisation } from '../Customisation';
 
 describe('Customisation', () => {
+  let component;
+  let loadForm;
+
+  beforeEach(() => {
+    loadForm = jasmine.createSpy('loadForm');
+    component = shallow(<Customisation settings={Immutable.fromJS({ customCSS: 'custom css' })} loadForm={loadForm} />);
+  });
+
   it('should render Customisation component', () => {
-    const component = shallow(<Customisation />);
     expect(component).toMatchSnapshot();
+  });
+
+  it('should load the form with the settings on component mount', () => {
+    const instance = component.instance();
+
+    instance.componentDidMount();
+    expect(loadForm).toHaveBeenCalledWith('settings', { customCSS: 'custom css' });
   });
 });
