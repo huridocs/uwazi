be11375dc2d513bec1af480c9bdf84e063506416
Daneryl <daneryl@gmail.com>
2018-06-13 14:55:34 +0200

    saveSettingsAction

--- app/react/Settings/actions/specs/settingsAction.spec.js
+++ app/react/Settings/actions/specs/settingsAction.spec.js
@@ -1,22 +1,25 @@
-import saveSettings from '../settingsActions';
 import SettingsAPI from 'app/Settings/SettingsAPI';
-import { notify } from 'app/Notifications';
+
+import saveSettings from '../settingsActions';
 
 describe('saveSettings', () => {
+  let dispatch;
+  beforeEach(() => {
+    spyOn(SettingsAPI, 'save').and.returnValue(Promise.resolve());
+    dispatch = jasmine.createSpy('dispatch');
+  });
+
   it('should save the settings using the api', () => {
-    spyOn(SettingsAPI, 'save');
-    let dispatch = jasmine.createSpy('dispatch');
     saveSettings({ customSettings: 'anything' })(dispatch);
 
     expect(SettingsAPI.save).toHaveBeenCalledWith({ customSettings: 'anything' });
   });
 
-  it('should call notify after saving settings', () => {
-    spyOn(SettingsAPI, 'save');
-
-    let dispatch = jasmine.createSpy('dispatch');
-    saveSettings({ customSettings: 'anything' })(dispatch);
-
-    expect(dispatch).toHaveBeenCalled();
+  it('should call notify after saving settings', (done) => {
+    saveSettings({ customSettings: 'anything' })(dispatch)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalled();
+      done();
+    });
   });
 });
