af989770f39fafdfdd07730d86fd5a18c7f47100
Eric Wamugu <wamugu@gmail.com>
2018-06-13 11:48:19 +0300

    custom css settings progress

--- app/react/Settings/reducers/reducer.js
+++ app/react/Settings/reducers/reducer.js
@@ -1,14 +1,14 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import createReducer from 'app/BasicReducer';
-import {modelReducer, formReducer} from 'react-redux-form';
+import { modelReducer, formReducer } from 'react-redux-form';
 
 import ui from './uiReducer.js';
 
 export default combineReducers({
   collection: createReducer('settings/collection', {}),
-  navlinksData: modelReducer('settings.navlinksData', {links: []}),
+  navlinksData: modelReducer('settings.navlinksData', { links: [] }),
   navlinksFormState: formReducer('settings.navlinksData'),
-  form: modelReducer('account.form', {username: '', password: ''}),
+  form: modelReducer('account.form', { username: '', password: '' }),
   formState: formReducer('account.form'),
   settingForm: formReducer('settings'),
   settings: modelReducer('settings', {}),
