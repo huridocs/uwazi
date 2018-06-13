7d2266328cae2cabbc31141f223154b3387fc531
Eric Wamugu <wamugu@gmail.com>
2018-06-13 10:14:02 +0300

    custom css settings progress

--- app/react/Settings/actions/settingsActions.js
+++ app/react/Settings/actions/settingsActions.js
@@ -1,10 +1,16 @@
 import SettingsAPI from 'app/Settings/SettingsAPI';
-import {notify} from 'app/Notifications';
+import { notify } from 'app/Notifications';
+import { t } from 'app/I18N';
 
 const saveSettings = (data) => {
-  return async (dispatch) => {
-    // console.log(data);
-  };
+  const settings = Object.assign({}, data);
+  settings.customCSS = data.settings.customCSS;
+  SettingsAPI.save(settings)
+  .then((result) => {
+    notify(t('System', 'Settings updated'), 'success');
+    // this.props.setSettings(result);
+    console.log(result);
+  });
 };
 
 export {
