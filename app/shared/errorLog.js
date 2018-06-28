75d0259ed14012708c30a40ad9589b191998c787
txau <jaumec@gmail.com>
2018-06-27 18:57:40 -0600

    Added some testing

--- app/shared/errorLog.js
+++ app/shared/errorLog.js
@@ -1,8 +1,8 @@
 import winston from 'winston';
 import graylog2 from 'graylog2';
 
-const DATABASE_NAME = process.env.DATABASE_NAME ? process.env.DATABASE_NAME : 'localhost';
-const LOGS_DIR = process.env.LOGS_DIR ? process.env.LOGS_DIR : `${__dirname}/../../log`;
+let DATABASE_NAME = 'localhost';
+let LOGS_DIR = `${__dirname}/../../log`;
 
 const formatMessage = (info) => {
   const message = (typeof info.message === 'object')
@@ -15,58 +15,70 @@ const formatMessage = (info) => {
 
 const formatter = winston.format.printf(info => formatMessage(info));
 
-const errorLogger = winston.createLogger({
-  transports: [
-    new winston.transports.File({
-      filename: `${LOGS_DIR}/error.log`,
-      handleExceptions: true,
-      level: 'error',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        formatter
-      )
-    }),
-    new winston.transports.Console({
-      handleExceptions: true,
-      level: 'error',
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        formatter
-      )
-    })
-  ]
-});
-
-const graylog = new graylog2.graylog({ //eslint-disable-line new-cap
-  servers: [
-        { host: process.env.USE_GRAYLOG, port: 12201 }
-  ],
-  hostname: DATABASE_NAME,
-  facility: 'Uwazi instances'
-});
-
-graylog.on('error', (error) => {
-  console.error('Error while trying to write to graylog2:', error); //eslint-disable-line no-console
-});
-
 class GrayLogTransport extends winston.Transport {
+  constructor(opts) {
+    super(opts);
+
+    this.graylog = new graylog2.graylog({ //eslint-disable-line new-cap
+      servers: [
+            { host: opts.server, port: 12201 }
+      ],
+      hostname: DATABASE_NAME,
+      facility: 'Uwazi instances'
+    });
+
+    this.graylog.on('error', (error) => {
+      console.error('Error while trying to write to graylog2:', error); //eslint-disable-line no-console
+    });
+  }
+
   log(info, callback) {
     setImmediate(() => {
       this.emit('logged', info);
     });
 
-    graylog.log(formatMessage(info));
+    this.graylog.log(formatMessage(info));
     callback();
   }
 }
 
-if (process.env.USE_GRAYLOG) {
-  errorLogger.add(new GrayLogTransport({
-    format: winston.format.combine(
-      winston.format.timestamp(),
-      formatter
-    )
-  }));
-}
+export const createErrorLog = () => {
+  DATABASE_NAME = process.env.DATABASE_NAME ? process.env.DATABASE_NAME : 'localhost';
+  LOGS_DIR = process.env.LOGS_DIR ? process.env.LOGS_DIR : `${__dirname}/../../log`;
 
-export default errorLogger;
+  const logger = winston.createLogger({
+    transports: [
+      new winston.transports.File({
+        filename: `${LOGS_DIR}/error.log`,
+        handleExceptions: true,
+        level: 'error',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          formatter
+        )
+      }),
+      new winston.transports.Console({
+        handleExceptions: true,
+        level: 'error',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          formatter
+        )
+      })
+    ]
+  });
+
+  if (process.env.USE_GRAYLOG) {
+    logger.add(new GrayLogTransport({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        formatter
+      ),
+      server: process.env.USE_GRAYLOG
+    }));
+  }
+
+  return logger;
+};
+
+export default createErrorLog();
---
+++ app/shared/specs/errorLog.spec.js
@@ -0,0 +1,70 @@
+import { createErrorLog } from '../errorLog';
+
+describe('errorLog', () => {
+  it('should have a file and a console transports', () => {
+    const anErrorLog = createErrorLog();
+
+    expect(anErrorLog.transports[0].constructor.name).toBe('File');
+    expect(anErrorLog.transports[1].constructor.name).toBe('Console');
+    expect(anErrorLog.transports.length).toBe(2);
+  });
+
+  it('should call 2 transports with instance name and message', () => {
+    const anErrorLog = createErrorLog();
+
+    spyOn(anErrorLog.transports[0], 'log');
+    spyOn(anErrorLog.transports[1], 'log');
+
+    anErrorLog.error('a message');
+
+    let calledArgs = anErrorLog.transports[0].log.calls.mostRecent().args[0];
+    calledArgs = Object.getOwnPropertySymbols(calledArgs).map(s => calledArgs[s]);
+
+    expect(calledArgs[1]).toContain('a message');
+    expect(calledArgs[1]).toContain('[localhost]');
+
+    calledArgs = anErrorLog.transports[1].log.calls.mostRecent().args[0];
+    calledArgs = Object.getOwnPropertySymbols(calledArgs).map(s => calledArgs[s]);
+
+    expect(calledArgs[1]).toContain('a message');
+    expect(calledArgs[1]).toContain('[localhost]');
+  });
+
+  it('should overwritte logs path from env vars', () => {
+    process.env.LOGS_DIR = './some_dir';
+
+    const anErrorLog = createErrorLog();
+
+    expect(anErrorLog.transports[0].dirname).toBe('./some_dir');
+    expect(anErrorLog.transports[0].filename).toBe('error.log');
+
+    anErrorLog.error('a message');
+  });
+
+  it('should overwritte instance name from env vars', () => {
+    process.env.DATABASE_NAME = 'my_instance';
+
+    const anErrorLog = createErrorLog();
+
+    spyOn(anErrorLog.transports[0], 'log');
+    spyOn(anErrorLog.transports[1], 'log');
+
+    anErrorLog.error('a message');
+
+    let calledArgs = anErrorLog.transports[0].log.calls.mostRecent().args[0];
+    calledArgs = Object.getOwnPropertySymbols(calledArgs).map(s => calledArgs[s]);
+
+    expect(calledArgs[1]).toContain('a message');
+    expect(calledArgs[1]).toContain('[my_instance]');
+  });
+
+  it('should add GrayLog transport if defined as env var', () => {
+    process.env.USE_GRAYLOG = 'server_address';
+
+    const anErrorLog = createErrorLog();
+
+    expect(anErrorLog.transports[2].constructor.name).toBe('GrayLogTransport');
+    expect(anErrorLog.transports.length).toBe(3);
+    expect(anErrorLog.transports[2].graylog.config.servers[0].host).toBe('server_address');
+  });
+});
