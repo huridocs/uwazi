a4803f0a0ce30fb3baa3e76b265f006a5a0d1d09
RafaPolit <rafaelpolit@gmail.com>
2018-07-02 10:46:19 -0500

    Fixed tests to comply with new fontawesome implementation.

--- app/react/Layout/specs/Icon.spec.js
+++ app/react/Layout/specs/Icon.spec.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import {Icon} from '../Icon';
+import { shallow } from 'enzyme';
 import Flag from 'react-flags';
 import Immutable from 'immutable';
+import { Icon as UIIcon } from 'UI';
+import { Icon } from '../Icon';
 
 describe('Icon', () => {
   let component;
@@ -11,42 +12,42 @@ describe('Icon', () => {
   describe('Fontawesome', () => {
     beforeEach(() => {
       props = {
-        data: {_id: 'iconid', type: 'Icons', label: 'iconlabel'},
+        data: { _id: 'iconid', type: 'Icons', label: 'iconlabel' },
         className: 'passed-classname',
         size: 'md'
       };
     });
 
-    let render = () => {
+    const render = () => {
       component = shallow(<Icon {...props} />);
     };
 
     it('should render a fontawesome icon', () => {
       render();
       expect(component.find('span').props().className).toBe('passed-classname');
-      expect(component.find('i').props().className).toContain('fa fa-iconid');
+      expect(component.find(UIIcon).props().icon).toBe('iconid');
     });
 
     it('should render different size icons', () => {
       render();
-      expect(component.find('i').props().className).toContain('fa-2x');
+      expect(component.find(UIIcon).props().size).toBe('2x');
 
       props.size = 'sm';
       render();
-      expect(component.find('i').props().className).toContain('fa-lg');
+      expect(component.find(UIIcon).props().size).toBe('lg');
     });
   });
 
   describe('Flags', () => {
     beforeEach(() => {
       props = {
-        data: {_id: 'flagid', type: 'Flags', label: 'flaglabel'},
+        data: { _id: 'flagid', type: 'Flags', label: 'flaglabel' },
         className: 'passed-flag-classname',
         size: 'md'
       };
     });
 
-    let render = () => {
+    const render = () => {
       component = shallow(<Icon {...props} />);
     };
 
@@ -68,7 +69,7 @@ describe('Icon', () => {
 
     describe('when data is immutable', () => {
       it('should render a Flag icon', () => {
-        props.data = Immutable.fromJS({_id: 'flagid', type: 'Flags', label: 'flaglabel'});
+        props.data = Immutable.fromJS({ _id: 'flagid', type: 'Flags', label: 'flaglabel' });
         render();
         expect(component.find('span').props().className).toBe('passed-flag-classname');
         expect(component.find(Flag).props().name).toBe('flagid');
