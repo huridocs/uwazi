79aa93dc4493989ca62ce6fa25c7667a0fd4d7fe
RafaPolit <rafaelpolit@gmail.com>
2018-07-04 17:54:40 -0500

    Implemented dataset-connected basic BarChart.

--- app/react/Markdown/components/index.js
+++ app/react/Markdown/components/index.js
@@ -3,11 +3,13 @@ import ItemList from './ItemList';
 import MarkdownLink from './MarkdownLink';
 import SearchBox from './SearchBox';
 import Counter from './Counter';
+import BarChart from './BarChart';
 
 export default {
   MarkdownMedia,
   ItemList,
   MarkdownLink,
   SearchBox,
-  Counter
+  Counter,
+  BarChart,
 };
