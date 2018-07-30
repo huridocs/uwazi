2cfd5174c83309ec1e0bda3ae43cb90d0e34d222
Daneryl <daneryl@gmail.com>
2018-07-30 13:15:46 +0200

    refactor of FiltersForm and suopport for subFilters or "relationship metadata" filters

--- app/react/Library/components/FiltersForm.js
+++ app/react/Library/components/FiltersForm.js
@@ -1,4 +1,4 @@
-import { Field, Form } from 'react-redux-form';
+import { Form } from 'react-redux-form';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Immutable, { is } from 'immutable';
@@ -6,26 +6,13 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Icon } from 'UI';
 
-import { MultiSelect, DateRange, NestedMultiselect, NumericRange, Switcher } from 'app/ReactReduxForms';
-import { activateFilter } from 'app/Library/actions/filterActions';
 import { searchDocuments } from 'app/Library/actions/libraryActions';
 import { t } from 'app/I18N';
 import { wrapDispatch } from 'app/Multireducer';
-import FormGroup from 'app/DocumentForm/components/FormGroup';
-import ShowIf from 'app/App/ShowIf';
 import debounce from 'app/utils/debounce';
 import libraryHelper from 'app/Library/helpers/libraryFilters';
 
-const translatedOptions = property => property.options.map((option) => {
-  option.label = t(property.content, option.label, null, false);
-  if (option.values) {
-    option.options = option.values.map((val) => {
-      val.label = t(property.content, val.label, null, false);
-      return val;
-    });
-  }
-  return option;
-});
+import Filters from './FiltersFromProperties';
 
 export class FiltersForm extends Component {
   constructor(props) {
@@ -59,145 +46,6 @@ export class FiltersForm extends Component {
     this.props.searchDocuments({ search }, this.props.storeKey);
   }
 
-  selectFilter(property, propertyClass, translationContext) {
-    return (
-      <FormGroup key={property._id}>
-        <ul className={propertyClass}>
-          <li>
-            {t(translationContext, property.label)}
-            {property.required ? <span className="required">*</span> : ''}
-            <ShowIf if={property.type === 'multiselect' || property.type === 'relationship'}>
-              <Switcher
-                model={`.filters.${property.name}.and`}
-                prefix={property.name}
-                onChange={this.activateAutoSearch}
-              />
-            </ShowIf>
-          </li>
-          <li className="wide">
-            <MultiSelect
-              model={`.filters.${property.name}.values`}
-              prefix={property.name}
-              options={translatedOptions(property)}
-              optionsValue="id"
-              onChange={this.activateAutoSearch}
-            />
-          </li>
-        </ul>
-      </FormGroup>
-    );
-  }
-
-  nestedFilter(property, propertyClass, translationContext) {
-    return (
-      <FormGroup key={property._id}>
-        <ul className={propertyClass}>
-          <li>
-            {t(translationContext, property.label)}
-            {property.required ? <span className="required">*</span> : ''}
-            <div className="nested-strict">
-              <Field model={`.filters.${property.name}.strict`}>
-                <input
-                  id={`${property.name}strict`}
-                  type="checkbox"
-                  onChange={this.activateAutoSearch}
-                />
-              </Field>
-              <label htmlFor={`${property.name}strict`}>
-                <span>&nbsp;Strict mode</span>
-              </label>
-            </div>
-          </li>
-          <li className="wide">
-            <NestedMultiselect
-              aggregations={this.props.aggregations}
-              property={property}
-              onChange={this.activateAutoSearch}
-            />
-          </li>
-        </ul>
-      </FormGroup>
-    );
-  }
-
-  dateFilter(property, propertyClass, translationContext) {
-    return (
-      <FormGroup key={property._id}>
-        <ul className={propertyClass}>
-          <li>
-            {t(translationContext, property.label)}
-            {property.required ? <span className="required">*</span> : ''}
-          </li>
-          <li className="wide">
-            <DateRange
-              model={`.filters.${property.name}`}
-              onChange={this.activateAutoSearch}
-              format={this.props.dateFormat}
-            />
-          </li>
-        </ul>
-      </FormGroup>
-    );
-  }
-
-  numericFilter(property, propertyClass, translationContext) {
-    return (
-      <FormGroup key={property._id}>
-        <ul className={propertyClass}>
-          <li>
-            {t(translationContext, property.label)}
-            {property.required ? <span className="required">*</span> : ''}
-          </li>
-          <li className="wide">
-            <NumericRange
-              model={`.filters.${property.name}`}
-              onChange={this.activateAutoSearch}
-            />
-          </li>
-        </ul>
-      </FormGroup>
-    );
-  }
-
-  defaultFilter(property, propertyClass, translationContext) {
-    return (
-      <FormGroup key={property._id}>
-        <Field model={`.filters.${property.name}`} >
-          <ul className={propertyClass}>
-            <li>
-              <label>
-                {t(translationContext, property.label)}
-                {property.required ? <span className="required">*</span> : ''}
-              </label>
-            </li>
-            <li className="wide">
-              <input className="form-control" onChange={this.activateAutoSearch} />
-            </li>
-          </ul>
-        </Field>
-      </FormGroup>
-    );
-  }
-
-  relationshipFilter(property, propertyClass, translationContext) {
-    return (
-      <ul className={propertyClass} key={property._id}>
-        <li>
-          <label>
-            {t(translationContext, property.label)}
-            {property.required ? <span className="required">*</span> : ''}
-          </label>
-        </li>
-        <li className="wide">
-          {property.filters.map((prop) => {
-            prop.name = `${property.name}.${prop.name}`;
-            return this.defaultFilter(prop, propertyClass, property.relationType);
-          })}
-        </li>
-      </ul>
-    );
-  }
-
   render() {
     const { templates, documentTypes } = this.props;
 
@@ -227,25 +75,12 @@ export class FiltersForm extends Component {
         })()}
 
         <Form model={model} id="filtersForm" onSubmit={this.submit} onChange={this.onChange}>
-          {fields.map((property) => {
-            const propertyClass = 'search__filter is-active';
-            if (property.type === 'select' || property.type === 'multiselect' || property.type === 'relationship') {
-              return this.selectFilter(property, propertyClass, translationContext);
-            }
-            if (property.type === 'nested') {
-              return this.nestedFilter(property, propertyClass, translationContext);
-            }
-            if (property.type === 'relationshipfilter') {
-              return this.relationshipFilter(property, propertyClass, translationContext);
-            }
-            if (property.type === 'date' || property.type === 'multidate' || property.type === 'multidaterange' || property.type === 'daterange') {
-              return this.dateFilter(property, propertyClass, translationContext);
-            }
-            if (property.type === 'numeric') {
-              return this.numericFilter(property, propertyClass, translationContext);
-            }
-            return this.defaultFilter(property, propertyClass, translationContext);
-          })}
+          <Filters
+            onChange={this.activateAutoSearch}
+            properties={fields}
+            translationContext={translationContext}
+            storeKey={this.props.storeKey}
+          />
         </Form>
       </div>
     );
@@ -253,15 +88,13 @@ export class FiltersForm extends Component {
 }
 
 FiltersForm.propTypes = {
-  templates: PropTypes.instanceOf(Immutable.List),
-  aggregations: PropTypes.object,
-  fields: PropTypes.object.isRequired,
-  searchDocuments: PropTypes.func,
-  activateFilter: PropTypes.func,
+  templates: PropTypes.instanceOf(Immutable.List).isRequired,
+  aggregations: PropTypes.instanceOf(Immutable.Map).isRequired,
+  fields: PropTypes.instanceOf(Immutable.List).isRequired,
+  searchDocuments: PropTypes.func.isRequired,
   search: PropTypes.object,
-  documentTypes: PropTypes.object,
-  storeKey: PropTypes.string,
-  dateFormat: PropTypes.string
+  documentTypes: PropTypes.instanceOf(Immutable.List).isRequired,
+  storeKey: PropTypes.string.isRequired,
 };
 
 export function mapStateToProps(state, props) {
@@ -270,12 +103,11 @@ export function mapStateToProps(state, props) {
     aggregations: state[props.storeKey].aggregations,
     templates: state.templates,
     documentTypes: state[props.storeKey].filters.get('documentTypes'),
-    dateFormat: state.settings.collection.get('dateFormat')
   };
 }
 
 function mapDispatchToProps(dispatch, props) {
-  return bindActionCreators({ searchDocuments, activateFilter }, wrapDispatch(dispatch, props.storeKey));
+  return bindActionCreators({ searchDocuments }, wrapDispatch(dispatch, props.storeKey));
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FiltersForm);
