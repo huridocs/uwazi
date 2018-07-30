868f7d424618c9d1609c3e7caeb6197c0ce20839
Daneryl <daneryl@gmail.com>
2018-07-30 13:15:46 +0200

    refactor of FiltersForm and suopport for subFilters or "relationship metadata" filters

---
+++ app/react/Library/components/FiltersFromProperties.js
@@ -0,0 +1,89 @@
+import { connect } from 'react-redux';
+import Immutable from 'immutable';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { t } from 'app/I18N';
+import FormGroup from 'app/DocumentForm/components/FormGroup';
+
+import DateFilter from './DateFilter';
+import NestedFilter from './NestedFilter';
+import NumberRangeFilter from './NumberRangeFilter';
+import SelectFilter from './SelectFilter';
+import TextFilter from './TextFilter';
+import RelationshipFilter from './RelationshipFilter';
+
+const translatedOptions = property => property.options.map((option) => {
+  option.label = t(property.content, option.label, null, false);
+  if (option.values) {
+    option.options = option.values.map((val) => {
+      val.label = t(property.content, val.label, null, false);
+      return val;
+    });
+  }
+  return option;
+});
+
+export const FiltersFromProperties = ({ onChange, properties, translationContext, modelPrefix = '', ...props }) => (
+  <React.Fragment>
+    {properties.map((property) => {
+      const commonProps = { model: `${modelPrefix}.filters.${property.name}`, label: t(translationContext, property.label), onChange };
+
+      let filter = <TextFilter {...commonProps} />;
+
+      if (property.type === 'relationshipfilter') {
+         filter = <RelationshipFilter {...commonProps} storeKey={props.storeKey} translationContext={translationContext} property={property} />;
+      }
+
+      if (property.type === 'numeric') {
+         filter = <NumberRangeFilter {...commonProps} />;
+      }
+
+      if (property.type === 'select' || property.type === 'multiselect' || property.type === 'relationship') {
+        filter = (<SelectFilter
+          {...commonProps}
+          options={translatedOptions(property)}
+          prefix={property.name}
+          showBoolSwitch={property.type === 'multiselect' || property.type === 'relationship'}
+        />);
+      }
+
+      if (property.type === 'nested') {
+        filter = <NestedFilter {...commonProps} property={property} aggregations={props.aggregations}/>;
+      }
+
+      if (property.type === 'date' || property.type === 'multidate' || property.type === 'multidaterange' || property.type === 'daterange') {
+        filter = <DateFilter {...commonProps} format={props.dateFormat} />;
+      }
+
+      return <FormGroup key={property.name}>{filter}</FormGroup>;
+    })}
+  </React.Fragment>
+);
+
+FiltersFromProperties.defaultProps = {
+  onChange: () => {},
+  dateFormat: '',
+  modelPrefix: '',
+  translationContext: '',
+};
+
+FiltersFromProperties.propTypes = {
+  onChange: PropTypes.func,
+  dateFormat: PropTypes.string,
+  modelPrefix: PropTypes.string,
+  translationContext: PropTypes.string,
+  storeKey: PropTypes.string.isRequired,
+  aggregations: PropTypes.instanceOf(Immutable.Map).isRequired,
+  properties: PropTypes.array.isRequired,
+};
+
+export function mapStateToProps(state, props) {
+  return {
+    dateFormat: state.settings.collection.get('dateFormat'),
+    aggregations: state[props.storeKey].aggregations,
+    storeKey: props.storeKey,
+  };
+}
+
+export default connect(mapStateToProps)(FiltersFromProperties);
---
+++ app/react/Library/components/specs/TextFilter.spec.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import TextFilter from '../TextFilter';
+
+describe('TextFilter', () => {
+  it('should render a text filter field with a label and passing the model', () => {
+    const props = {
+      label: 'label',
+      model: 'model',
+    };
+
+    const component = shallow(<TextFilter {...props}/>);
+    expect(component).toMatchSnapshot();
+  });
+});
