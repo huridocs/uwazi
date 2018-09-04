17e75a9099677cf17e9f382912f575b48fa89fd0
Clément Habinshuti <hello@habbes.xyz>
2018-09-04 15:41:35 +0300

    Add validations to users routes

--- app/api/users/routes.js
+++ app/api/users/routes.js
@@ -1,31 +1,56 @@
+import Joi from 'joi';
 import needsAuthorization from '../auth/authMiddleware';
+import validateRequest from '../utils/validateRequest';
 import users from './users';
 
-const getDomain = (req) => req.protocol + '://' + req.get('host');
-export default app => {
-  app.post('/api/users', needsAuthorization(['admin', 'editor']), (req, res) => {
-    users.save(req.body, req.user, getDomain(req))
-    .then(response => res.json(response))
-    .catch(res.error);
-  });
+const getDomain = req => req.protocol + '://' + req.get('host');
+export default (app) => {
+  app.post('/api/users',
+    needsAuthorization(['admin', 'editor']), 
+    validateRequest(Joi.object().keys({
+      _id: Joi.string().required(),
+      username: Joi.string(),
+      email: Joi.string(),
+      password: Joi.string(),
+      role: Joi.string()
+    })),
+    (req, res) => {
+      users.save(req.body, req.user, getDomain(req))
+      .then(response => res.json(response))
+      .catch(res.error);
+    });
 
-  app.post('/api/users/new', needsAuthorization(), (req, res) => {
-    users.newUser(req.body, getDomain(req))
-    .then(response => res.json(response))
-    .catch(res.error);
-  });
+  app.post('/api/users/new',
+    needsAuthorization(),
+    validateRequest(Joi.object().keys({
+      username: Joi.string().required(),
+      email: Joi.string().required()
+    })),
+    (req, res) => {
+      users.newUser(req.body, getDomain(req))
+      .then(response => res.json(response))
+      .catch(res.error);
+    });
 
-  app.post('/api/recoverpassword', (req, res) => {
-    users.recoverPassword(req.body.email, getDomain(req))
-    .then(() => res.json('OK'))
-    .catch(res.error);
-  });
+  app.post('/api/recoverpassword',
+    validateRequest(Joi.object().keys({
+      email: Joi.string().required()
+    })),
+    (req, res) => {
+      users.recoverPassword(req.body.email, getDomain(req))
+      .then(() => res.json('OK'))
+      .catch(res.error);
+    });
 
-  app.post('/api/resetpassword', (req, res) => {
-    users.resetPassword(req.body)
-    .then(response => res.json(response))
-    .catch(res.error);
-  });
+  app.post('/api/resetpassword',
+    validateRequest(Joi.object().keys({
+      key: Joi.string().required()
+    })),
+    (req, res) => {
+      users.resetPassword(req.body)
+      .then(response => res.json(response))
+      .catch(res.error);
+    });
 
   app.get('/api/users', needsAuthorization(), (req, res) => {
     users.get()
@@ -33,9 +58,14 @@ export default app => {
     .catch(res.error);
   });
 
-  app.delete('/api/users', needsAuthorization(), (req, res) => {
-    users.delete(req.query._id, req.user)
-    .then(response => res.json(response))
-    .catch(res.error);
-  });
+  app.delete('/api/users',
+    needsAuthorization(),
+    validateRequest(Joi.object().keys({
+      _id: Joi.string().required()
+    }), 'query'),
+    (req, res) => {
+      users.delete(req.query._id, req.user)
+      .then(response => res.json(response))
+      .catch(res.error);
+    });
 };
