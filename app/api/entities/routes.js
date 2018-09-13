d75876fd2b40de6200400f28d4f3fdb6033e215b
Daneryl <daneryl@gmail.com>
2018-09-13 12:02:13 +0200

    fixed lints

--- app/api/entities/routes.js
+++ app/api/entities/routes.js
@@ -4,31 +4,40 @@ import thesauris from '../thesauris/thesauris';
 import needsAuthorization from '../auth/authMiddleware';
 
 export default (app) => {
-  app.post('/api/entities', needsAuthorization(['admin', 'editor']), (req, res, next) => entities.save(req.body, { user: req.user, language: req.language })
-  .then((response) => {
-    res.json(response);
-    return templates.getById(response.template);
-  })
-  .then(template => thesauris.templateToThesauri(template, req.language, req.user))
-  .then((templateTransformed) => {
-    req.io.sockets.emit('thesauriChange', templateTransformed);
-  })
-  .catch(next));
+  app.post(
+    '/api/entities',
+    needsAuthorization(['admin', 'editor']),
+    (req, res, next) => entities.save(req.body, { user: req.user, language: req.language })
+    .then((response) => {
+      res.json(response);
+      return templates.getById(response.template);
+    })
+    .then(template => thesauris.templateToThesauri(template, req.language, req.user))
+    .then((templateTransformed) => {
+      req.io.sockets.emit('thesauriChange', templateTransformed);
+    })
+    .catch(next)
+  );
 
   app.post('/api/entities/multipleupdate', needsAuthorization(['admin', 'editor']),
-  (req, res, next) => entities.multipleUpdate(req.body.ids, req.body.values, { user: req.user, language: req.language })
-  .then((docs) => {
-    res.json(docs.map(doc => doc.sharedId));
-  })
-  .catch(next));
+    (req, res, next) => entities.multipleUpdate(req.body.ids, req.body.values, { user: req.user, language: req.language })
+    .then((docs) => {
+      res.json(docs.map(doc => doc.sharedId));
+    })
+    .catch(next));
 
-  app.get('/api/entities/count_by_template', (req, res, next) => entities.countByTemplate(req.query.templateId)
-  .then(response => res.json(response))
-  .catch(next));
+  app.get(
+    '/api/entities/count_by_template',
+    (req, res, next) => entities.countByTemplate(req.query.templateId)
+    .then(response => res.json(response))
+    .catch(next)
+  );
 
-  app.get('/api/entities/get_raw_page', (req, res, next) => entities.getRawPage(req.query.sharedId, req.language, req.query.pageNumber)
-  .then(data => res.json({ data }))
-  .catch(next));
+  app.get('/api/entities/get_raw_page',
+    (req, res, next) => entities.getRawPage(req.query.sharedId, req.language, req.query.pageNumber)
+    .then(data => res.json({ data }))
+    .catch(next)
+  );
 
   app.get('/api/entities', (req, res, next) => {
     entities.getWithRelationships({ sharedId: req.query._id, language: req.language })
