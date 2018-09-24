0c10bd91c650f51631a40160b51c26d7de3ab920
A Happy Cat <acasadotorres@gmail.com>
2018-09-24 11:33:52 +0200

    fixed typo on date range fields translation

--- app/react/Forms/components/DateRange.js
+++ app/react/Forms/components/DateRange.js
@@ -1,18 +1,17 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import DatePicker from './DatePicker';
-import {t} from 'app/I18N';
+import { t } from 'app/I18N';
 
 export default class DateRange extends Component {
-
   constructor(props) {
     super(props);
     const value = props.value || {};
-    this.state = {from: value.from, to: value.to};
+    this.state = { from: value.from, to: value.to };
   }
 
   onChange(prop, value) {
-    let state = Object.assign({}, this.state);
+    const state = Object.assign({}, this.state);
     state[prop] = value;
     this.setState(state);
     this.props.onChange(state);
@@ -20,29 +19,28 @@ export default class DateRange extends Component {
 
   render() {
     return (
-        <div>
-          <div className="DatePicker__From">
-            <span>{t('System', 'Label date "From"', 'From:')}&nbsp;</span>
-              <DatePicker
-                locale={this.props.locale}
-                format={this.props.format}
-                value={this.state.from}
-                onChange={(val) => this.onChange('from', val)}
-              />
-          </div>
-          <div className="DatePicker__To">
-            <span>&nbsp;{t('System', 'Label date "to"', 'To:')}&nbsp;</span>
-              <DatePicker
-                locale={this.props.locale}
-                format={this.props.format}
-                value={this.state.to}
-                onChange={(val) => this.onChange('to', val)}
-              />
-          </div>
+      <div>
+        <div className="DatePicker__From">
+          <span>{t('System', 'Label date From', 'From: ')}&nbsp;</span>
+          <DatePicker
+            locale={this.props.locale}
+            format={this.props.format}
+            value={this.state.from}
+            onChange={val => this.onChange('from', val)}
+          />
         </div>
+        <div className="DatePicker__To">
+          <span>&nbsp;{t('System', 'Label date to', 'To: ')}&nbsp;</span>
+          <DatePicker
+            locale={this.props.locale}
+            format={this.props.format}
+            value={this.state.to}
+            onChange={val => this.onChange('to', val)}
+          />
+        </div>
+      </div>
     );
   }
-
 }
 
 DateRange.propTypes = {
--- app/react/Forms/components/NumericRange.js
+++ app/react/Forms/components/NumericRange.js
@@ -1,18 +1,17 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Numeric from './Numeric';
-import {t} from 'app/I18N';
+import { t } from 'app/I18N';
 
 export default class NumericRange extends Component {
-
   constructor(props) {
     super(props);
     const value = props.value || {};
-    this.state = {from: value.from, to: value.to};
+    this.state = { from: value.from, to: value.to };
   }
 
   onChange(prop, value) {
-    let state = Object.assign({}, this.state);
+    const state = Object.assign({}, this.state);
     state[prop] = value;
     this.setState(state);
     this.props.onChange(state);
@@ -20,19 +19,18 @@ export default class NumericRange extends Component {
 
   render() {
     return (
-        <div>
-          <div className="Numeric__From">
-            <span>{t('System', 'Label date "From"')}</span>
-            <Numeric value={this.state.from} onChange={(val) => this.onChange('from', val)}/>
-          </div>
-          <div className="Numeric__To">
-            <span>&nbsp;{t('System', 'Label date "to"')}</span>
-            <Numeric value={this.state.to} onChange={(val) => this.onChange('to', val)}/>
-          </div>
+      <div>
+        <div className="Numeric__From">
+          <span>{t('System', 'Label date From')}</span>
+          <Numeric value={this.state.from} onChange={val => this.onChange('from', val)}/>
         </div>
+        <div className="Numeric__To">
+          <span>&nbsp;{t('System', 'Label date to')}</span>
+          <Numeric value={this.state.to} onChange={val => this.onChange('to', val)}/>
+        </div>
+      </div>
     );
   }
-
 }
 
 NumericRange.propTypes = {
