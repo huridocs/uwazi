f6b3bf497355392749a6e490d4264947247e26a6
Daneryl <daneryl@gmail.com>
2018-10-03 10:25:00 +0200

    SearchBar does only change the state on submit

--- app/react/Library/components/SearchBar.js
+++ app/react/Library/components/SearchBar.js
@@ -4,56 +4,13 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-import { I18NLink, t } from 'app/I18N';
 import { Icon } from 'UI';
-import { searchDocuments, getSuggestions, hideSuggestions, setOverSuggestions } from 'app/Library/actions/libraryActions';
+import { searchDocuments } from 'app/Library/actions/libraryActions';
+import { t } from 'app/I18N';
 import { wrapDispatch } from 'app/Multireducer';
-import SafeHTML from 'app/utils/SafeHTML';
-import debounce from 'app/utils/debounce';
 import SearchTips from 'app/Library/components/SearchTips';
 
 export class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.search = this.search.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.resetSearch = this.resetSearch.bind(this);
-    this.submitSearch = this.submitSearch.bind(this);
-    this.mouseEnter = this.mouseEnter.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-    this.closeSuggestions = this.closeSuggestions.bind(this);
-  }
-
-  componentWillMount() {
-    this.getSuggestions = debounce(this.getSuggestions, 400);
-  }
-
-  componentWillUnmount() {
-    this.props.setOverSuggestions(false);
-  }
-
-  onChange(e) {
-    this.props.change(`${this.props.storeKey}.search.searchTerm`, e.target.value);
-    this.getSuggestions(e);
-  }
-
-  getSuggestions(e) {
-    this.props.getSuggestions(e.target.value);
-  }
-
-  closeSuggestions() {
-    this.props.setOverSuggestions(false);
-    this.props.hideSuggestions();
-  }
-
-  mouseEnter() {
-    this.props.setOverSuggestions(true);
-  }
-
-  mouseOut() {
-    this.props.setOverSuggestions(false);
-  }
-
   resetSearch() {
     this.props.change(`${this.props.storeKey}.search.searchTerm`, '');
     const search = Object.assign({}, this.props.search);
@@ -71,53 +28,28 @@ export class SearchBar extends Component {
   }
 
   render() {
-    const { search, showSuggestions, suggestions, overSuggestions } = this.props;
+    const { search } = this.props;
     const model = `${this.props.storeKey}.search`;
     return (
       <div className={`search-box${this.props.open ? ' is-active' : ''}`}>
-        <Form model={model} onSubmit={this.search} autoComplete="off">
+        <Form model={model} onSubmit={this.search.bind(this)} autoComplete="off">
           <div className={`input-group${search.searchTerm ? ' is-active' : ''}`}>
             <Field model=".searchTerm" updateOn="submit">
               <input
                 type="text"
                 placeholder={t('System', 'Search', null, false)}
                 className="form-control"
-                onChange={this.onChange}
-                onBlur={this.props.hideSuggestions}
                 autoComplete="off"
               />
               <Icon
                 icon="times"
-                onClick={this.resetSearch}
+                onClick={this.resetSearch.bind(this)}
               />
             </Field>
             <Icon
               icon="search"
-              onClick={this.submitSearch}
+              onClick={this.submitSearch.bind(this)}
             />
-          </div>
-          <div
-            onMouseOver={this.mouseEnter}
-            onMouseLeave={this.mouseOut}
-            className={`search-suggestions${showSuggestions && search.searchTerm || overSuggestions ? ' is-active' : ''}`}
-          >
-            {suggestions.toJS().map((suggestion, index) => {
-              const documentViewUrl = `/${suggestion.type}/${suggestion.sharedId}`;
-              return (
-                <p className="search-suggestions-item" key={index}>
-                  <I18NLink to={documentViewUrl}>
-                    <span><SafeHTML>{suggestion.title}</SafeHTML></span>
-                    <Icon icon="file" />
-                  </I18NLink>
-                </p>);
-            })}
-            <button
-              className="search-suggestions-all"
-              type="submit"
-              onClick={this.closeSuggestions}
-            >
-              View all results for <b>{search.searchTerm}</b>
-            </button>
           </div>
         </Form>
         <SearchTips />
@@ -130,22 +62,13 @@ SearchBar.propTypes = {
   searchDocuments: PropTypes.func.isRequired,
   open: PropTypes.bool,
   change: PropTypes.func.isRequired,
-  getSuggestions: PropTypes.func.isRequired,
-  hideSuggestions: PropTypes.func.isRequired,
-  setOverSuggestions: PropTypes.func.isRequired,
-  suggestions: PropTypes.object,
-  showSuggestions: PropTypes.bool,
   search: PropTypes.object,
-  overSuggestions: PropTypes.bool,
   storeKey: PropTypes.string
 };
 
 export function mapStateToProps(state, props) {
   return {
     search: state[props.storeKey].search,
-    suggestions: state[props.storeKey].ui.get('suggestions'),
-    showSuggestions: state[props.storeKey].ui.get('showSuggestions'),
-    overSuggestions: state[props.storeKey].ui.get('overSuggestions'),
     open: state[props.storeKey].ui.get('filtersPanel') && !state.library.ui.get('selectedDocument')
   };
 }
@@ -153,9 +76,6 @@ export function mapStateToProps(state, props) {
 function mapDispatchToProps(dispatch, props) {
   return bindActionCreators({
     searchDocuments,
-    getSuggestions,
-    hideSuggestions,
-    setOverSuggestions,
     change: formActions.change
   }, wrapDispatch(dispatch, props.storeKey));
 }
