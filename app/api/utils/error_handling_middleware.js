d00ab8f7e68e30341b48638b9c049c964fd5cba8
Daneryl <daneryl@gmail.com>
2018-10-10 15:12:29 +0200

    removed commented code

--- app/api/utils/error_handling_middleware.js
+++ app/api/utils/error_handling_middleware.js
@@ -1,9 +1,6 @@
 import handleError from './handleError';
 
 export default function (error, req, res, next) {
-  // error.url = req.originalurl;
-  // error.body = req.body;
-  // error.query = req.query;
   const handled = handleError(error, { req });
 
   res.status(handled.code);
