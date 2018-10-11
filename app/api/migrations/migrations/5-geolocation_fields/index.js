b9bd6a0d8cb9bf21fdbde8493faebc2ec49bacfe
A Happy Cat <acasadotorres@gmail.com>
2018-10-11 16:41:34 +0200

    migration to sync geolocation properties

---
+++ app/api/migrations/migrations/5-geolocation_fields/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+
+export default {
+  delta: 5,
+
+  name: 'geolocation_fields',
+
+  description: 'replicate geolocation fields data for all languages',
+
+  async up(db) {
+    process.stdout.write(`${this.name}...\r\n`);
+    let index = 1;
+    const cursor = db.collection('entities').find();
+    const processedIds = [];
+    while (await cursor.hasNext()) {
+      const entity = await cursor.next();
+      const sharedId = entity.sharedId;
+      const entityPropertiesNames = Object.keys(entity.metadata);
+      const geolocationProperty = entityPropertiesNames.find(propertyName => propertyName.match('geolocation'));
+      const propertyValue = entity.metadata[geolocationProperty];
+
+      if (!processedIds.includes(sharedId) && geolocationProperty && (propertyValue.lat !== undefined || propertyValue.lon !== undefined)) {
+        const changes = {};
+        changes[`metadata.${geolocationProperty}`] = propertyValue;
+        processedIds.push(sharedId);
+        await db.collection('entities').updateMany({ sharedId }, { $set: changes });
+      }
+      process.stdout.write(`processed -> ${index}\r`);
+      index += 1;
+    }
+    process.stdout.write('\r\n');
+  }
+};
