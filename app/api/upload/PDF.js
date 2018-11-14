f1af1fdbf067ff24c8fd115d2ee5325af298d97f
Yuval Adam <_@yuv.al>
2018-11-14 14:36:20 +0200

    Naive implementation of opportunistic OCR

--- app/api/upload/PDF.js
+++ app/api/upload/PDF.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { spawn } from 'child-process-promise';
 import errorLog from 'api/log/errorLog';
+import debugLog from 'api/log/debugLog';
 
 export default class PDF extends EventEmitter {
   constructor(filepath) {
@@ -15,9 +16,37 @@ export default class PDF extends EventEmitter {
     return path.join(path.dirname(this.filepath), documentId);
   }
 
+  async attemptOcr() {
+    try {
+      // check that both tesseract and convert exist system-wide
+      const res1 = await spawn('tesseract', ['--version'], { capture: ['stdout', 'stderr'] });
+      debugLog.debug(res1.stdout);
+      const res2 = await spawn('convert', ['--version'], { capture: ['stdout', 'stderr'] });
+      debugLog.debug(res1.stdout);
+      debugLog.debug('Tesseract and imagemagick found, continuing with OCR processing');
+
+      // convert PDF to TIFF first
+      await spawn('convert', ['-density', '300', this.filepath, '-depth', '8',
+        '-strip', '-background', 'white', '-alpha', 'off', '/tmp/tmp.tiff']);
+      // run OCR
+      const res = await spawn('tesseract', ['/tmp/tmp.tiff', '-', '-c', 'debug_file=/dev/null', '-l', 'eng'], { capture: ['stdout', 'stderr'] });
+      return result.stdout.split('\f').slice(0, -1);
+    } catch (e) {
+      if (e.errno === 'ENOENT') {
+        debugLog.debug('Tesseract not found, skipping OCR processing');
+      }
+      return null;
+    };
+
+  }
+
   async extractText() {
-    const result = await spawn('pdftotext', [this.filepath, '-'], { capture: ['stdout', 'stderr'] });
-    const pages = result.stdout.split('\f').slice(0, -1);
+    let pages = await this.attemptOcr();
+    if (pages === null) {
+      const result = await spawn('pdftotext', [this.filepath, '-'], { capture: ['stdout', 'stderr'] });
+      pages = result.stdout.split('\f').slice(0, -1);
+    }
+
     return {
       fullText: pages.reduce((memo, page, index) => ({ ...memo, [index + 1]: page.replace(/(\S+)(\s?)/g, `$1[[${index + 1}]]$2`) }), {}),
       totalPages: pages.length
