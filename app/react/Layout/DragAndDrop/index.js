41479460bf1e12c6f91e7d3912b5cf904bef30d3
Daneryl <daneryl@gmail.com>
2018-11-27 13:24:33 +0100

    dragAndDropItem with handleIcon option

--- app/react/Layout/DragAndDrop/index.js
+++ app/react/Layout/DragAndDrop/index.js
@@ -1,7 +1,8 @@
 import DragAndDropContainer from './DragAndDropContainer';
 import DragAndDropItem from './DragAndDropItem';
+import DNDSource from './DragSource';
 
 export {
   DragAndDropContainer,
-  DragAndDropItem
+  DragAndDropItem,
 };
