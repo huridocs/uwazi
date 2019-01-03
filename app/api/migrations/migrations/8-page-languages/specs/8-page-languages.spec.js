19c1b862356afb29c1dd0dedc8d3e2b81ef24db2
A Happy Cat <acasadotorres@gmail.com>
2019-01-03 12:44:47 +0100

    migration for the bug fix of missing pages for added languages

---
+++ app/api/migrations/migrations/8-page-languages/specs/8-page-languages.spec.js
@@ -0,0 +1,30 @@
+import { catchErrors } from 'api/utils/jasmineHelpers';
+import testingDB from 'api/utils/testing_db';
+import migration from '../index.js';
+import fixtures from './fixtures.js';
+
+describe('migration page-languages', () => {
+  beforeEach((done) => {
+    spyOn(process.stdout, 'write');
+    testingDB.clearAllAndLoad(fixtures).then(done).catch(catchErrors(done));
+  });
+
+  afterAll((done) => {
+    testingDB.disconnect().then(done);
+  });
+
+  it('should have a delta number', () => {
+    expect(migration.delta).toBe(8);
+  });
+
+  it('should remove duplicated relationships, sharedIds and languages', async () => {
+    await migration.up(testingDB.mongodb);
+    const pages = await testingDB.mongodb.collection('pages').find().toArray();
+    console.log(pages);
+    expect(pages.length).toBe(4);
+    const pagesInES = pages.filter(p => p.language === 'es');
+    const pagesInPT = pages.filter(p => p.language === 'pt');
+    expect(pagesInES.length).toBe(2);
+    expect(pagesInPT.length).toBe(2);
+  });
+});
