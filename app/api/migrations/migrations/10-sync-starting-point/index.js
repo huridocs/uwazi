0bb6eb3abc4a6c9cdad01f4cbb1ac1e51c135df0
RafaPolit <rafaelpolit@gmail.com>
2019-01-21 13:18:23 -0500

    Created a sync starting point migration setting all documents of relevant collections in the update log with timestamp 1.

--- app/api/migrations/migrations/10-sync-starting-point/index.js
+++ app/api/migrations/migrations/10-sync-starting-point/index.js
@@ -5,8 +5,24 @@ export default {
 
   description: 'Create a sync starting point so that everything required is already in the logs',
 
-  up() {
-    process.stdout.write(`${this.name}...\r\n`);
-    return Promise.reject();
+  async up(db) {
+    process.stdout.write('Sync - Creating update logs: \r\n');
+
+    process.stdout.write('Emptying current logs... \r\n');
+    await db.collection('updatelogs').deleteMany({});
+
+    const collectionsToInclude = ['settings', 'templates', 'entities', 'connections', 'dictionaries', 'translations', 'relationtypes'];
+
+    const updateLogValues = await collectionsToInclude.reduce(async (prev, collection) => {
+      const values = await prev;
+      const ids = await db.collection(collection).distinct('_id', {});
+      process.stdout.write(`Processing ${ids.length} ${collection} \r\n`);
+      return values.concat(ids.map(mongoId => ({ mongoId, timestamp: 1, namespace: collection, deleted: false })));
+    }, Promise.resolve([]));
+
+    process.stdout.write(`Inserting ${updateLogValues.length} update logs...\r\n`);
+    await db.collection('updatelogs').insertMany(updateLogValues);
+
+    process.stdout.write('Done! \r\n');
   }
 };
--- app/api/migrations/migrations/10-sync-starting-point/specs/10-sync-starting-point.spec.js
+++ app/api/migrations/migrations/10-sync-starting-point/specs/10-sync-starting-point.spec.js
@@ -1,11 +1,25 @@
 import { catchErrors } from 'api/utils/jasmineHelpers';
 import testingDB from 'api/utils/testing_db';
 import migration from '../index.js';
-import fixtures from './fixtures.js';
+import fixtures, {
+  template1,
+  template2,
+  entity1,
+  entity3,
+  translation1,
+  connection1,
+  migration1
+} from './fixtures.js';
 
 describe('migration sync-starting-point', () => {
+  let updatelogs;
+
+  const expectLog = id => ({ toHave: (value) => {
+    const log = updatelogs.find(l => l.mongoId.toString() === id.toString());
+    expect(log).toEqual(expect.objectContaining(Object.assign({ timestamp: 1, deleted: false }, value)));
+  } });
+
   beforeEach((done) => {
-    spyOn(process.stdout, 'write');
     testingDB.clearAllAndLoad(fixtures).then(done).catch(catchErrors(done));
   });
 
@@ -17,7 +31,19 @@ describe('migration sync-starting-point', () => {
     expect(migration.delta).toBe(10);
   });
 
-  it('should fail', (done) => {
-    migration.up().catch(catchErrors(done));
+  it('should generate update logs of all the required collections, clearing previous logs', async () => {
+    await migration.up(testingDB.mongodb);
+
+    updatelogs = await testingDB.mongodb.collection('updatelogs').find().toArray();
+
+    expect(updatelogs.length).toBe(10);
+    expectLog(template1).toHave({ namespace: 'templates' });
+    expectLog(template2).toHave({ namespace: 'templates' });
+    expectLog(entity1).toHave({ namespace: 'entities' });
+    expectLog(entity3).toHave({ namespace: 'entities' });
+    expectLog(translation1).toHave({ namespace: 'translations' });
+    expectLog(connection1).toHave({ namespace: 'connections' });
+
+    expect(updatelogs.find(l => l.mongoId.toString() === migration1.toString())).not.toBeDefined();
   });
 });
