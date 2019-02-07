e36b5e173269056e1d0ee8e51d239794e6234011
Daneryl <daneryl@gmail.com>
2019-02-07 12:07:45 +0100

    warning when changing select thesauri

--- app/react/Layout/Warning.js
+++ app/react/Layout/Warning.js
@@ -2,17 +2,28 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Icon } from 'UI';
 
-const Tip = ({ children }) => (
-  <span className="property-help">
-    <Icon icon="exclamation-triangle" />
-    <div className="property-description">
+const Tip = ({ children, inline }) => (
+  inline ?
+    <div className="alert alert-warning fade in">
+      <Icon icon="exclamation-triangle" />
       {children}
     </div>
-  </span>
+    :
+    <span className="property-help">
+      <Icon icon="exclamation-triangle" />
+      <div className="property-description">
+        {children}
+      </div>
+    </span>
 );
 
+Tip.defaultProps = {
+  inline: false
+};
+
 Tip.propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  inline: PropTypes.bool
 };
 
 export default Tip;
