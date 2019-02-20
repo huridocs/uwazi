75cf7b04eb3e7df300db505ae955a8f2fdb0e277
Clément Habinshuti <hello@habbes.xyz>
2019-02-20 20:43:52 +0300

    Add specs for semantic search list

--- app/react/SemanticSearch/components/specs/SearchItem.spec.js
+++ app/react/SemanticSearch/components/specs/SearchItem.spec.js
@@ -1,3 +1,34 @@
-describe('SearchItem', () => {
+import React from 'react';
 
+import { shallow } from 'enzyme';
+
+import { SearchItem } from '../SearchItem';
+
+describe('SearchItem', () => {
+  let search;
+  beforeEach(() => {
+    search = {
+      _id: 'id',
+      searchTerm: 'query',
+      documents: [],
+      status: 'completed'
+    };
+  });
+
+  const getProps = () => ({ search });
+
+  const render = () => shallow(<SearchItem {...getProps()} />);
+
+  it('should render search details and progress with link to results page ', () => {
+    const component = render();
+    expect(component).toMatchSnapshot();
+  });
+
+  describe('when search status is inProgress', () => {
+    it('should render in-progress icon and stop button', () => {
+      search.status = 'inProgress';
+      const component = render();
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
