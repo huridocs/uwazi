c80cf6ab6b0e757af69a6da8c8702331d491b0ae
RafaPolit <rafaelpolit@gmail.com>
2019-03-07 17:58:55 -0500

    Refactor Root.js

--- app/react/App/Root.js
+++ app/react/App/Root.js
@@ -4,6 +4,54 @@ import serialize from 'serialize-javascript';
 
 import laguagesList from '../Settings/utils/languagesList';
 
+const determineHotAssets = query => ({
+  JS: [
+    'http://localhost:8080/pdfjs-dist.js',
+    'http://localhost:8080/nprogress.js',
+    'http://localhost:8080/main.js',
+    'http://localhost:8080/vendor.js',
+  ],
+  CSS: [
+    `http://localhost:8080/CSS/vendor.css${query}`,
+    `http://localhost:8080/CSS/main.css${query}`,
+    'http://localhost:8080/pdfjs-dist.css',
+  ]
+});
+
+const determineAssets = (assets, languageData) => ({
+  JS: [
+    assets['pdfjs-dist'].js,
+    assets.nprogress.js,
+    assets.vendor.js,
+    assets.main.js,
+  ],
+  CSS: [
+    assets.vendor.css[languageData.rtl ? 1 : 0],
+    assets.main.css[languageData.rtl ? 1 : 0],
+    assets['pdfjs-dist'].css[0],
+  ]
+});
+
+const googelFonts = (
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700|Roboto+Slab:100,300,400,700|Roboto:100,300,400,500,700,900"
+  />
+);
+
+const headTag = (head, CSS, reduxData) => (
+  <head>
+    {head.title.toComponent()}
+    {head.meta.toComponent()}
+    {head.link.toComponent()}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {CSS.map((style, key) => <link key={key} href={style} rel="stylesheet" type="text/css" />)}
+    <style type="text/css" dangerouslySetInnerHTML={{ __html: reduxData.settings.collection.customCSS }} />
+    {googelFonts}
+    <link rel="shortcut icon" href="/public/favicon.ico"/>
+  </head>
+);
+
 class Root extends Component {
   renderInitialData() {
     let innerHtml = '';
@@ -22,57 +70,18 @@ class Root extends Component {
 
   render() {
     const isHotReload = process.env.HOT;
-    const { head } = this.props;
-    let JS = [
-      'http://localhost:8080/pdfjs-dist.js',
-      'http://localhost:8080/nprogress.js',
-      'http://localhost:8080/main.js',
-      'http://localhost:8080/vendor.js',
-    ];
+    const { head, language, assets, reduxData, content } = this.props;
 
-    const languageData = laguagesList.find(l => l.key === this.props.language);
-    let query = '';
-    if (languageData.rtl) {
-      query = '?rtl=true';
-    }
+    const languageData = laguagesList.find(l => l.key === language);
+    const query = (languageData && languageData.rtl) ? '?rtl=true' : '';
 
-    let CSS = [
-      `http://localhost:8080/CSS/vendor.css${query}`,
-      `http://localhost:8080/CSS/main.css${query}`,
-      'http://localhost:8080/pdfjs-dist.css',
-    ];
-
-    if (!isHotReload) {
-      JS = [
-        this.props.assets['pdfjs-dist'].js,
-        this.props.assets.nprogress.js,
-        this.props.assets.vendor.js,
-        this.props.assets.main.js,
-      ];
-      CSS = [
-        this.props.assets.vendor.css[languageData.rtl ? 1 : 0],
-        this.props.assets.main.css[languageData.rtl ? 1 : 0],
-        this.props.assets['pdfjs-dist'].css[0],
-      ];
-    }
+    const { JS, CSS } = isHotReload ? determineHotAssets(query) : determineAssets(assets, languageData);
 
     return (
-      <html lang={this.props.language}>
-        <head>
-          {head.title.toComponent()}
-          {head.meta.toComponent()}
-          {head.link.toComponent()}
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          {CSS.map((style, key) => <link key={key} href={style} rel="stylesheet" type="text/css" />)}
-          <style type="text/css" dangerouslySetInnerHTML={{ __html: this.props.reduxData.settings.collection.customCSS }} />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700|Roboto+Slab:100,300,400,700|Roboto:100,300,400,500,700,900"
-          />
-          <link rel="shortcut icon" href="/public/favicon.ico"/>
-        </head>
+      <html lang={language}>
+        {headTag(head, CSS, reduxData)}
         <body>
-          <div id="root" dangerouslySetInnerHTML={{ __html: this.props.content }} />
+          <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
           {this.renderInitialData()}
           {head.script.toComponent()}
           {JS.map((file, index) => <script key={index} src={file} />)}
