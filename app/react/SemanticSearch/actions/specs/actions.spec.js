b3b7cef340e56bae405a2646593783c68b74485f
Clément Habinshuti <hello@habbes.xyz>
2019-03-08 20:05:06 +0300

    Add tests for semantic search actions

--- app/react/SemanticSearch/actions/specs/actions.spec.js
+++ app/react/SemanticSearch/actions/specs/actions.spec.js
@@ -1,3 +1,4 @@
+import Immutable from 'immutable';
 import api from '../../SemanticSearchAPI';
 
 import * as actions from '../actions';
@@ -11,6 +12,12 @@ describe('Semantic Search actions', () => {
   afterEach(() => {
     api.getAllSearches.mockReset();
   });
+  async function expectFetchSearchesToHaveBeenDispatched(dispatchMock) {
+    const fetchSearchesAction = dispatch.mock.calls[0][0];
+    await fetchSearchesAction(dispatchMock);
+    expect(api.getAllSearches).toHaveBeenCalled();
+  }
+
   describe('fetchSearches', () => {
     it('should fetch searches from api and dispatch response in store', async () => {
       const action = actions.fetchSearches();
@@ -44,9 +51,7 @@ describe('Semantic Search actions', () => {
       const action = actions.submitNewSearch(args);
       await action(dispatch);
       expect(api.search).toHaveBeenCalledWith(args);
-      const fetchSearchesAction = dispatch.mock.calls[0][0];
-      await fetchSearchesAction(dispatch);
-      expect(api.getAllSearches).toHaveBeenCalled();
+      expectFetchSearchesToHaveBeenDispatched(dispatch);
     });
   });
 
@@ -62,6 +67,84 @@ describe('Semantic Search actions', () => {
     it('should hide semantic search panel', () => {
       const action = actions.hideSemanticSearch();
       action(dispatch);
+      expect(dispatch.mock.calls).toMatchSnapshot();
+    });
+  });
+
+  describe('deleteSearch', () => {
+    it('should delete search and re-fetch searches', async () => {
+      jest.spyOn(api, 'deleteSearch').mockResolvedValue({ deleted: true });
+      const searchId = 'search';
+      const action = actions.deleteSearch(searchId);
+      await action(dispatch);
+      expect(api.deleteSearch).toHaveBeenCalledWith(searchId);
+      expectFetchSearchesToHaveBeenDispatched(dispatch);
+    });
+  });
+
+  describe('stopSearch', () => {
+    it('should stop search and re-fetch searches', async () => {
+      jest.spyOn(api, 'stopSearch').mockResolvedValue({ status: 'stopped' });
+      const searchId = 'search';
+      const action = actions.stopSearch(searchId);
+      await action(dispatch);
+      expect(api.stopSearch).toHaveBeenCalledWith(searchId);
+      expectFetchSearchesToHaveBeenDispatched(dispatch);
+    });
+  });
+
+  describe('resumeSearch', () => {
+    it('should resume search and re-fetch searches', async () => {
+      jest.spyOn(api, 'resumeSearch').mockResolvedValue({ status: 'pending' });
+      const searchId = 'search';
+      const action = actions.resumeSearch(searchId);
+      await action(dispatch);
+      expect(api.resumeSearch).toHaveBeenCalledWith(searchId);
+      expectFetchSearchesToHaveBeenDispatched(dispatch);
+    });
+  });
+
+  describe('registerForUpdates', () => {
+    it('should request update notifications from API', async () => {
+      jest.spyOn(api, 'registerForUpdates').mockResolvedValue({ ok: true });
+      const action = actions.registerForUpdates();
+      await action();
+      expect(api.registerForUpdates).toHaveBeenCalled();
+    });
+  });
+
+  describe('updateSearch', () => {
+    it('should update specified search in the searches store', async () => {
+      const updatedSearch = { _id: 'search', status: 'completed' };
+      const action = actions.updateSearch(updatedSearch);
+      action(dispatch);
+      expect(dispatch.mock.calls).toMatchSnapshot();
+    });
+  });
+
+  describe('addSearchResults', () => {
+    let currentResults;
+    beforeEach(() => {
+      currentResults = [{ _id: 'search1' }, { _id: 'search2' }];
+    });
+    const getState = () => ({
+      semanticSearch: {
+        search: Immutable.fromJS({
+          results: currentResults
+        })
+      }
+    });
+    it('should add specified documents to current search results', () => {
+      const newDocs = [{ _id: 'new1' }, { _id: 'new2' }];
+      const action = actions.addSearchResults(newDocs);
+      action(dispatch, getState);
+      expect(dispatch.mock.calls).toMatchSnapshot();
+    });
+    it('should not add duplicate documents in search results', () => {
+      currentResults = [{ _id: 'search1' }, { _id: 'search2' }, { _id: 'new1' }];
+      const newDocs = [{ _id: 'new1' }, { _id: 'new2' }];
+      const action = actions.addSearchResults(newDocs);
+      action(dispatch, getState);
       expect(dispatch.mock.calls).toMatchSnapshot();
     });
   });
