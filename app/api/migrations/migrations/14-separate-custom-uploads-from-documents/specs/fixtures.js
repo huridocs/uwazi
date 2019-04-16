1dd3c99ab7fc2a402886e8be36da03fce5a3f7fb
Clément Habinshuti <hello@habbes.xyz>
2019-04-17 01:57:01 +0300

    Create migration for moving custom uploads to separate folder

---
+++ app/api/migrations/migrations/14-separate-custom-uploads-from-documents/specs/fixtures.js
@@ -0,0 +1,9 @@
+import db from 'api/utils/testing_db';
+
+export default {
+  uploads: [
+    { _id: db.id(), filename: 'file1.txt' },
+    { _id: db.id(), filename: 'file2.txt' },
+    { _id: db.id(), filename: 'file3.txt' }
+  ]
+};
