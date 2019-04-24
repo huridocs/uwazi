7f45d11320d54b9169291ede8ab9e23240fd5196
Yuval Adam <_@yuv.al>
2019-04-24 15:30:47 +0300

    Update OCR to use ocrmypdf

--- app/api/upload/PDF.js
+++ app/api/upload/PDF.js
@@ -18,34 +18,24 @@ export default class PDF extends EventEmitter {
 
   async attemptOcr() {
     try {
-      // check that both tesseract and convert exist system-wide
-      const res1 = await spawn('tesseract', ['--version'], { capture: ['stdout', 'stderr'] });
+      // check that ocrmypdf exists system-wide
+      const res1 = await spawn('ocrmypdf', ['--version'], { capture: ['stdout', 'stderr'] });
       debugLog.debug(res1.stdout);
-      const res2 = await spawn('convert', ['--version'], { capture: ['stdout', 'stderr'] });
-      debugLog.debug(res1.stdout);
-      debugLog.debug('Tesseract and imagemagick found, continuing with OCR processing');
+      debugLog.debug('ocrmypdf found, continuing with OCR processing');
 
-      // convert PDF to TIFF first
-      await spawn('convert', ['-density', '300', this.filepath, '-depth', '8',
-        '-strip', '-background', 'white', '-alpha', 'off', '/tmp/tmp.tiff']);
-      // run OCR
-      const res = await spawn('tesseract', ['/tmp/tmp.tiff', '-', '-c', 'debug_file=/dev/null', '-l', 'eng'], { capture: ['stdout', 'stderr'] });
-      return result.stdout.split('\f').slice(0, -1);
+      // run OCR and add the text layer in place on the existing PDF file path
+      const _res = await spawn('ocrmypdf', ['-l', 'eng', this.filepath, this.filepath], { capture: ['stdout', 'stderr'] });
     } catch (e) {
       if (e.errno === 'ENOENT') {
-        debugLog.debug('Tesseract not found, skipping OCR processing');
+        debugLog.debug('ocrmypdf not found, skipping OCR processing');
       }
-      return null;
     };
-
   }
 
   async extractText() {
-    let pages = await this.attemptOcr();
-    if (pages === null) {
-      const result = await spawn('pdftotext', [this.filepath, '-'], { capture: ['stdout', 'stderr'] });
-      pages = result.stdout.split('\f').slice(0, -1);
-    }
+    await this.attemptOcr();
+    const result = await spawn('pdftotext', [this.filepath, '-'], { capture: ['stdout', 'stderr'] });
+    const pages = result.stdout.split('\f').slice(0, -1);
 
     return {
       fullText: pages.reduce((memo, page, index) => ({ ...memo, [index + 1]: page.replace(/(\S+)(\s?)/g, `$1[[${index + 1}]]$2`) }), {}),
