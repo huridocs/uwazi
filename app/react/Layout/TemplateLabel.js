0e83343c578c71ab63d501a329232934fc9a1ecb
Clément Habinshuti <hello@habbes.xyz>
2019-04-30 16:46:00 +0300

    Display custom color if template has one

--- app/react/Layout/TemplateLabel.js
+++ app/react/Layout/TemplateLabel.js
@@ -4,30 +4,33 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import t from '../I18N/t';
 
-const NUM_COLORS = 18;
+const NUM_COLORS = 19;
 
 const getTemplateInfo = createSelector(
   s => s.templates,
   (s, p) => p.template,
   (templates, currentTemplate) => {
-    let typeIndex;
+    let styleProps;
     const name = templates.reduce((result, template, index) => {
       if (template.get('_id') === currentTemplate) {
-        typeIndex = `btn-color btn-color-${index % NUM_COLORS}`;
+        styleProps = template.get('color') ?
+          { className: 'btn-color', style: { backgroundColor: template.get('color') } } :
+          { className: `btn-color btn-color-${index % NUM_COLORS}` };
         return template.get('name');
       }
       return result;
     }, '');
 
-    return { name, typeIndex };
+    return { name, styleProps };
   }
 );
 
 export class TemplateLabel extends Component {
   render() {
+    const { name, template, className, style } = this.props;
     return (
-      <span className={this.props.typeIndex}>
-        {t(this.props.template, this.props.name)}
+      <span className={className} style={style}>
+        {t(template, name)}
       </span>
     );
   }
@@ -36,15 +39,16 @@ export class TemplateLabel extends Component {
 TemplateLabel.propTypes = {
   template: PropTypes.string,
   name: PropTypes.string,
-  typeIndex: PropTypes.string
+  className: PropTypes.string,
+  style: PropTypes.shape({ backgroundColor: PropTypes.string })
 };
 
 const mapStateToProps = (state, props) => {
   const template = getTemplateInfo(state, props);
   return {
     name: template.name,
-    typeIndex: template.typeIndex,
-    template: props.template
+    template: props.template,
+    ...template.styleProps
   };
 };
 
--- app/react/Layout/specs/TemplateLabel.spec.js
+++ app/react/Layout/specs/TemplateLabel.spec.js
@@ -39,21 +39,29 @@ describe('TemplateLabel', () => {
   it('should add consecutive type classNames for each template', () => {
     props.template = 'templateId';
     render();
-    expect(component.prop('typeIndex')).toBe('btn-color btn-color-0');
+    expect(component.prop('className')).toBe('btn-color btn-color-0');
 
     props.template = 'templateId2';
     render();
-    expect(component.prop('typeIndex')).toBe('btn-color btn-color-1');
+    expect(component.prop('className')).toBe('btn-color btn-color-1');
   });
 
-  it('should cycle back through colors if there more than 18 templates', () => {
+  it('should cycle back through colors if there more than 19 templates', () => {
     const templates = [];
-    for (let i = 0; i < 19; i += 1) {
+    for (let i = 0; i < 20; i += 1) {
       templates.push({ _id: `templateId${i}`, name: `title ${i}` });
     }
     initialState.templates = immutable(templates);
-    props.template = 'templateId18';
+    props.template = 'templateId19';
     render();
-    expect(component.prop('typeIndex')).toBe('btn-color btn-color-0');
+    expect(component.prop('className')).toBe('btn-color btn-color-0');
+  });
+
+  it('should display the template color if template has a custom color', () => {
+    initialState.templates = immutable([{ _id: 'templateId', name: 'title', color: '#112233' }]);
+    props.template = 'templateId';
+    render();
+    expect(component.prop('className')).toBe('btn-color');
+    expect(component.prop('style')).toEqual({ backgroundColor: '#112233' });
   });
 });
