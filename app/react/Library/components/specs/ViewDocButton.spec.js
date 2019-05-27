4c469a1c840190dc77c4614b02191fe4395be99d
Clément Habinshuti <hello@habbes.xyz>
2019-05-27 16:01:51 +0300

    Add tests to ViewDocButton

--- app/react/Library/components/specs/ViewDocButton.spec.js
+++ app/react/Library/components/specs/ViewDocButton.spec.js
@@ -1,23 +1,43 @@
 import React from 'react';
+import { fromJS } from 'immutable';
 
 import { shallow } from 'enzyme';
 
 import { ViewDocButton } from '../ViewDocButton';
 
 describe('ViewDocButton', () => {
-  it('should render a view button poiting to the doc url with the searchTerm if pressent', () => {
-    const props = {
+  let props;
+  let component;
+  beforeEach(() => {
+    props = {
       type: 'entity',
       format: 'format',
       sharedId: '123',
-      searchTerm: ''
+      searchTerm: '',
+      activateReference: jest.fn()
     };
+  });
 
-    let component = shallow(<ViewDocButton {...props}/>);
+  const render = () => {
+    component = shallow(<ViewDocButton {...props}/>);
+    return component;
+  };
+
+  it('should render a view button poiting to the doc url with the searchTerm if pressent', () => {
+    render();
     expect(component).toMatchSnapshot();
 
     props.searchTerm = 'something';
-    component = shallow(<ViewDocButton {...props}/>);
+    render();
     expect(component).toMatchSnapshot();
+  });
+  describe('when targetReference is provided', () => {
+    it('should call activateReference when clicked', () => {
+      const event = { stopPropagation: jest.fn() };
+      props.targetReference = fromJS({ range: { start: 200, end: 300 } });
+      render();
+      component.simulate('click', event);
+      expect(props.activateReference).toHaveBeenCalledWith(props.targetReference.toJS());
+    });
   });
 });
