9010001f8465b1300e354b5f2e4ddb3cd3fc52a0
A Happy Cat <acasadotorres@gmail.com>
2019-05-31 12:41:48 +0200

    Captcha component specs

---
+++ app/react/Forms/components/specs/Captcha.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TwitterPicker } from 'react-color';
+
+import Captcha from '../Captcha';
+
+describe('Captcha', () => {
+  let component;
+  let props;
+
+  beforeEach(() => {
+    props = {
+      onChange: () => {}
+    };
+  });
+
+  const render = () => {
+    component = shallow(<Captcha {...props} />);
+  };
+
+  it('should render a Captcha image with an input', () => {
+    render();
+    expect(component).toMatchSnapshot();
+  });
+
+  describe('refresh()', () => {
+    it('should return the refresh captcha method', (done) => {
+      let refreshCaptcha;
+      props.refresh = (_refreshCaptcha) => {
+        refreshCaptcha = _refreshCaptcha;
+      };
+      render();
+      expect(component.find('img').props().src).toBe('/captcha');
+      refreshCaptcha();
+      component.update();
+      expect(component.find('img').props().src).not.toBe('/captcha');
+      done();
+    });
+  });
+});
