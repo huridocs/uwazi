948bef19c2ffd501f52e05e2bc4e79d045fa2c80
A Happy Cat <acasadotorres@gmail.com>
2019-06-17 14:30:04 +0200

    fixed import case

--- app/react/reducer.js
+++ app/react/reducer.js
@@ -6,7 +6,7 @@ import page from 'app/Pages/reducers/reducer';
 import notifications from 'app/Notifications/reducers/notificationsReducer';
 
 import thesauri from 'app/Thesauris/reducers/reducer';
-import activitylog from 'app/activitylog/reducer';
+import activitylog from 'app/Activitylog/reducer';
 import documentViewer from 'app/Viewer/reducers/reducer';
 import entityView from 'app/Entities/reducers/reducer';
 import contextMenu from 'app/ContextMenu/reducers/contextMenuReducer';
