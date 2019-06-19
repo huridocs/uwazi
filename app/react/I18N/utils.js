cd89d6dc3de81d38322b12517263768326df84ec
RafaPolit <rafaelpolit@gmail.com>
2019-06-19 11:00:24 -0500

    Made internal functions private, only exposed util is getLocale.

--- app/react/I18N/utils.js
+++ app/react/I18N/utils.js
@@ -1,24 +1,22 @@
 import * as Cookie from 'tiny-cookie';
 import { isClient } from 'app/utils';
 
+const getURLLocale = (locale, languages = []) => languages.find(l => l.key === locale) ? locale : null;
+
+const getCookieLocale = () => {
+  if (isClient && Cookie.get('locale')) {
+    return Cookie.get('locale');
+  }
+
+  return null;
+  // return I18NUtils.getLocaleIfExists(cookie.locale, languages);
+};
+
+const getDefaultLocale = languages => (languages.find(language => language.default) || {}).key;
+
 const I18NUtils = {
-  getURLLocale: (locale, languages = []) => languages.find(l => l.key === locale) ? locale : null,
-
-  getCookieLocale: () => {
-    if (isClient && Cookie.get('locale')) {
-      return Cookie.get('locale');
-    }
-
-    return null;
-    // return I18NUtils.getLocaleIfExists(cookie.locale, languages);
-  },
-
-  getDefaultLocale: languages => (languages.find(language => language.default) || {}).key,
-
   getLocale(urlLanguage, languages = [], cookie = {}) {
-    const newLanguage = I18NUtils.getURLLocale(urlLanguage, languages) ||
-                        I18NUtils.getCookieLocale(cookie) ||
-                        I18NUtils.getDefaultLocale(languages);
+    const newLanguage = getURLLocale(urlLanguage, languages) || getCookieLocale(cookie) || getDefaultLocale(languages);
     this.saveLocale(newLanguage);
     return newLanguage;
   },
