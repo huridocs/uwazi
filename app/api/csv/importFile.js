6089ab0843f55ee7badf44dc39353307d5ed928a
Daneryl <daneryl@gmail.com>
2019-07-19 09:19:30 +0200

    handle and respond import errors on endpoint

--- app/api/csv/importFile.js
+++ app/api/csv/importFile.js
@@ -3,27 +3,29 @@ import path from 'path';
 import { Readable } from 'stream';
 
 import { generateFileName, fileFromReadStream } from 'api/utils/files';
+import { createError } from 'api/utils';
 import zipFile from 'api/utils/zipFile';
 
 import configPaths from '../config/paths';
 
-const csvOrFileName = fileName =>
-  (entry) => {
-    if (fileName) {
-      return fileName === entry.fileName;
-    }
+const extractFromZip = async (filePath, fileName) => {
+  const readStream = await zipFile(filePath)
+  .findReadStream(entry => entry.fileName === fileName);
 
-    return entry.fileName === 'import.csv';
-  };
+  if (!readStream) {
+    throw createError(`${fileName} file not found`);
+  }
+
+  return readStream;
+};
 
 const importFile = filePath => ({
-  async readStream(fileName) {
+  async readStream(fileName = 'import.csv') {
     if (filePath instanceof Readable) {
       return filePath;
     }
     if (path.extname(filePath) === '.zip') {
-      return zipFile(filePath)
-      .findReadStream(csvOrFileName(fileName));
+      return extractFromZip(filePath, fileName);
     }
     return fs.createReadStream(filePath);
   },
--- app/api/csv/specs/importFile.spec.js
+++ app/api/csv/specs/importFile.spec.js
@@ -9,10 +9,15 @@ describe('importFile', () => {
       path.join(__dirname, '/zipData/import.csv'),
       path.join(__dirname, '/zipData/file1.txt'),
     ], 'ImportFile.zip');
+
+    await createTestingZip([
+      path.join(__dirname, '/zipData/file1.txt'),
+    ], 'badFile.zip');
   });
 
   afterAll(() => {
     fs.unlinkSync(path.join(__dirname, '/zipData/ImportFile.zip'));
+    fs.unlinkSync(path.join(__dirname, '/zipData/badFile.zip'));
   });
 
   describe('readStream', () => {
@@ -57,6 +62,20 @@ describe('importFile', () => {
           });
         });
       });
+    });
+  });
+
+  describe('when there is no import.csv on the zip', () => {
+    it('should throw an error', async () => {
+      const file = importFile(path.join(__dirname, '/zipData/badFile.zip'));
+      let error;
+      try {
+        await file.readStream();
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error.message.match(/import\.csv/)).not.toBeNull();
     });
   });
 });
