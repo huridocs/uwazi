d6f0b91a91a1dbc293ed12f5712a512f62c52854
Clément Habinshuti <hello@habbes.xyz>
2019-07-22 19:42:11 +0300

    Refactor getFieldLabel

--- app/react/Templates/utils/getFieldLabel.js
+++ app/react/Templates/utils/getFieldLabel.js
@@ -1,17 +1,23 @@
 import { t } from 'app/I18N';
 
+function getTitleLabel(template) {
+  const titleField = template.commonProperties.find(p => p.name === 'title');
+  return t(template._id, titleField.label);
+}
+
+function getMetadataFieldLabel(field, template) {
+  const name = field.split('.')[1];
+  const property = template.properties.find(p => p.name === name);
+  return property && t(template._id, property.label) || field;
+}
+
 export default function getFieldLabel(field, template) {
   const _template = template && template.toJS ? template.toJS() : template;
   if (field === 'title') {
-    const titleField = _template.commonProperties.find(p => p.name === 'title');
-    return t(_template._id, titleField.label);
+    return getTitleLabel(_template);
   }
   if (field.startsWith('metadata.') && _template) {
-    const name = field.split('.')[1];
-    const property = _template.properties.find(p => p.name === name);
-    if (property) {
-      return t(_template._id, property.label);
-    }
+    return getMetadataFieldLabel(field, _template) || field;
   }
   return field;
 }
