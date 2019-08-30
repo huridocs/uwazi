7b6278417a08bd240f677eda6d82e06ea6491725
RafaPolit <rafaelpolit@gmail.com>
2019-08-30 14:40:45 -0500

    Refactored actions.

--- app/react/Activitylog/actions/activitylogActions.js
+++ app/react/Activitylog/actions/activitylogActions.js
@@ -2,19 +2,20 @@ import { actions } from 'app/BasicReducer';
 import { RequestParams } from 'app/utils/RequestParams';
 import api from '../../utils/api';
 
-export function activitylogSearch(query) {
+const activitylogSearchBase = (query, append = false) => {
+  const method = append ? 'concat' : 'set';
+
   return dispatch => api.get('activitylog', new RequestParams(query))
   .then((response) => {
     dispatch(actions.set('activitylog/search', response.json));
-    dispatch(actions.set('activitylog/list', response.json.rows));
+    dispatch(actions[method]('activitylog/list', response.json.rows));
   });
+};
+
+export function activitylogSearch(query) {
+  return activitylogSearchBase(query);
 }
 
-// TEST!!!!
 export function activitylogSearchMore(query) {
-  return dispatch => api.get('activitylog', new RequestParams(query))
-  .then((response) => {
-    dispatch(actions.set('activitylog/search', response.json));
-    dispatch(actions.concat('activitylog/list', response.json.rows));
-  });
+  return activitylogSearchBase(query, true);
 }
