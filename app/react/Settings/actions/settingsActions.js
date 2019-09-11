b38094be0692251a9bc7d6c7228600327f087c46
RafaPolit <rafaelpolit@gmail.com>
2019-09-11 17:04:37 -0500

    Fixed settings api.

--- app/react/Settings/actions/settingsActions.js
+++ app/react/Settings/actions/settingsActions.js
@@ -1,8 +1,9 @@
 import { notificationActions } from 'app/Notifications';
 import { t } from 'app/I18N';
+import { RequestParams } from 'app/utils/RequestParams';
 import SettingsAPI from 'app/Settings/SettingsAPI';
 
-const saveSettings = data => dispatch => SettingsAPI.save(data)
+const saveSettings = data => dispatch => SettingsAPI.save(new RequestParams(data))
 .then(() => {
   dispatch(notificationActions.notify(t('System', 'Settings updated'), 'success'));
 });
