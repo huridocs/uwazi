ce359ff9851742c96299c786dc702d94d86a0b06
Benjamin Dittes <bdittes@google.com>
2019-10-10 17:41:45 +0200

    add ===

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { NODE_ENV } = process.env;
 require.extensions['.scss'] = function scss() {};
 require.extensions['.css'] = function css() {};
 
-if (NODE_ENV == 'production') {
+if (NODE_ENV === 'production') {
   require('./prod/app/server.js');
 } else {
   require('./app/server.js');
