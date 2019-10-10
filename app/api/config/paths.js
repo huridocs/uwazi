04376c0a4c7587509b68fa470a1511c968e28911
Benjamin Dittes <bdittes@google.com>
2019-10-10 17:33:21 +0200

    add ;

--- app/api/config/paths.js
+++ app/api/config/paths.js
@@ -1,6 +1,6 @@
 const { ROOT_PATH, UPLOADS_FOLDER, CUSTOM_UPLOADS_FOLDER } = process.env;
 
-const rootPath = ROOT_PATH || `${__dirname}/../../../`
+const rootPath = ROOT_PATH || `${__dirname}/../../../`;
 const uploadedDocuments = UPLOADS_FOLDER || `${rootPath}/uploaded_documents/`;
 const attachments = UPLOADS_FOLDER || `${rootPath}/uploaded_documents/`;
 const customUploads = CUSTOM_UPLOADS_FOLDER || `${rootPath}/custom_uploads/`;
