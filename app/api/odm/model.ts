21d930699c032f6e6686016cb3f5d78f815b7f02
Benjamin Dittes <bdittes@google.com>
2019-10-11 12:33:09 +0200

    format

--- app/api/odm/model.ts
+++ app/api/odm/model.ts
@@ -1,19 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import { model as updatelogsModel } from 'api/updatelogs';
+import { model as updatelogsModel } from "api/updatelogs";
 
-import models from './models';
-import {OdmModel} from './models';
+import models from "./models";
+import { OdmModel } from "./models";
 
 const generateID = mongoose.Types.ObjectId;
 export { generateID };
 
-export default <T extends mongoose.Document>(collectionName: string, schema: mongoose.Schema) => {
-  const getAffectedIds = async (conditions: any) => mongoose.models[collectionName].distinct('_id', conditions);
+export default <T extends mongoose.Document>(
+  collectionName: string,
+  schema: mongoose.Schema
+) => {
+  const getAffectedIds = async (conditions: any) =>
+    mongoose.models[collectionName].distinct("_id", conditions);
 
-  const upsertLogOne = async (doc: mongoose.Document, next: ()=>any) => {
+  const upsertLogOne = async (
+    doc: mongoose.Document,
+    next: (err?: mongoose.NativeError) => void
+  ) => {
     const logData = { namespace: collectionName, mongoId: doc._id };
-    await updatelogsModel.findOneAndUpdate(logData, { ...logData, timestamp: Date.now(), deleted: false }, { upsert: true});
+    await updatelogsModel.findOneAndUpdate(
+      logData,
+      { ...logData, timestamp: Date.now(), deleted: false },
+      { upsert: true }
+    );
     next();
   };
 
@@ -25,21 +36,23 @@ export default <T extends mongoose.Document>(collectionName: string, schema: mon
     );
   }
 
-  schema.post('save', upsertLogOne);
-  schema.post('findOneAndUpdate', upsertLogOne);
+  schema.post("save", upsertLogOne);
+  schema.post("findOneAndUpdate", upsertLogOne);
 
   const MongooseModel = mongoose.model<T>(collectionName, schema);
 
   const saveOne = async (data: mongoose.Document): Promise<T | null> => {
-    const documentExists = await MongooseModel.findById(data._id, '_id');
+    const documentExists = await MongooseModel.findById(data._id, "_id");
 
     if (documentExists) {
-      return MongooseModel.findOneAndUpdate({ _id: data._id }, data, { new: true }).exec();
+      return MongooseModel.findOneAndUpdate({ _id: data._id }, data, {
+        new: true
+      }).exec();
     }
     return MongooseModel.create(data).then(saved => saved.toObject());
   };
 
-  const odmModel : OdmModel<mongoose.Document> = {
+  const odmModel: OdmModel<mongoose.Document> = {
     db: MongooseModel,
     save: (data: mongoose.Document | mongoose.Document[]) => {
       if (Array.isArray(data)) {
@@ -50,13 +63,19 @@ export default <T extends mongoose.Document>(collectionName: string, schema: mon
       return saveOne(data);
     },
 
-    get: (query: any, select = '', pagination = {}) => MongooseModel.find(query, select, Object.assign({ lean: true }, pagination)).exec(),
+    get: (query: any, select = "", pagination = {}) =>
+      MongooseModel.find(
+        query,
+        select,
+        Object.assign({ lean: true }, pagination)
+      ).exec(),
 
     count: (condition: any) => MongooseModel.count(condition).exec(),
 
-    getById: (id: any | string | number) => MongooseModel.findById(id, {}, { lean: true }).exec(),
+    getById: (id: any | string | number) =>
+      MongooseModel.findById(id, {}, { lean: true }).exec(),
 
-    delete: async (condition) => {
+    delete: async condition => {
       let cond = condition;
       if (mongoose.Types.ObjectId.isValid(condition)) {
         cond = { _id: condition };
