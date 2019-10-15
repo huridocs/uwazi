cc456c970fa7a6434db0b02ea26542dde9bc21b1
Clément Habinshuti <hello@habbes.xyz>
2019-10-15 18:43:59 +0300

    Handle mongoose validation errors

--- app/api/utils/handleError.js
+++ app/api/utils/handleError.js
@@ -14,6 +14,10 @@ const prettifyError = (error, { req = {}, uncaught = false } = {}) => {
     result = { code: 422, message: error.message, validations: error.errors };
   }
 
+  if (error.name === 'ValidationError') {
+    result = { code: 422, message: error.message, validations: error.properties };
+  }
+
   if (error.name === 'MongoError') {
     result.code = 500;
   }
