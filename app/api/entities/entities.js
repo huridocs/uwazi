3a0e8470c522730ed2be134d307d3ce3cf027221
GabrielPiles <gabriel.piles.glez@gmail.com>
2019-10-23 17:15:54 +0200

    Allow to pass id to a new entity when calling the api

--- app/api/entities/entities.js
+++ app/api/entities/entities.js
@@ -66,10 +66,17 @@ function updateEntity(entity, _template) {
 function createEntity(doc, languages, sharedId) {
   const docs = languages.map((lang) => {
     const langDoc = Object.assign({}, doc);
+    if (!lang.default) {
+      delete langDoc._id;
+    }
+
     langDoc.language = lang.key;
     langDoc.sharedId = sharedId;
+
+
     return langDoc;
   });
+
   return model.save(docs);
 }
 
