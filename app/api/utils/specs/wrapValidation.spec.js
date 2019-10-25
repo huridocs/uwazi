d86de4f4d7202f32178bfdcebb9c30142dda35f4
Clément Habinshuti <hello@habbes.xyz>
2019-10-25 11:18:49 +0300

    Add support for async validators in wrapValidation

--- app/api/utils/specs/wrapValidation.spec.js
+++ app/api/utils/specs/wrapValidation.spec.js
@@ -1,34 +1,82 @@
 import { wrapValidation } from '../wrapValidation';
 
 describe('wrapValidation', () => {
+  let model;
+  let validator;
+
+  beforeEach(() => {
+    model = {
+      foo: jest.fn(),
+      bar: jest.fn().mockReturnValue(10),
+      baz: jest.fn().mockReturnValue(20)
+    };
+    validator = {
+      foo() { throw new Error('error'); },
+      bar() {}
+    };
+  });
+
   describe('when called with a validator and model', () => {
     it('should return a model that validates input to each method defined in the validator', () => {
-      const foo = jest.fn();
-      const bar = jest.fn();
-      const baz = jest.fn();
-      const model = {
-        foo(...args) { foo(...args); },
-        bar(...args) { bar(...args); },
-        baz(...args) { baz(...args); }
-      };
-      const validator = {
-        foo() { throw new Error('error'); },
-        bar() {}
-      };
-
       const validated = wrapValidation(validator, model);
+
       try {
         validated.foo('arg');
+        fail('should throw error');
       } catch (e) {
         expect(e.message).toBe('error');
-        expect(foo).not.toHaveBeenCalled();
+        expect(model.foo).not.toHaveBeenCalled();
       }
 
-      validated.bar('arg1', 2);
-      expect(bar).toHaveBeenCalledWith('arg1', 2);
+      let res = validated.bar('arg1', 2);
+      expect(model.bar).toHaveBeenCalledWith('arg1', 2);
+      expect(res).toBe(10);
 
-      validated.baz('arg');
-      expect(baz).toHaveBeenCalledWith('arg');
+      res = validated.baz('arg');
+      expect(model.baz).toHaveBeenCalledWith('arg');
+      expect(res).toBe(20);
+    });
+
+    it('should support async validator functions', async () => {
+      validator = {
+        foo() { return Promise.reject(new Error('error')); },
+        bar() { return Promise.resolve(); }
+      }
+      const validated = wrapValidation(validator, model);
+
+      try {
+        await validated.foo('arg');
+        fail('should throw error');
+      } catch (e) {
+        expect(e.message).toBe('error');
+        expect(model.foo).not.toBeCalled();
+      }
+
+      let res = await validated.bar('arg1', 2);
+      expect(model.bar).toHaveBeenCalledWith('arg1', 2);
+      expect(res).toBe(10);
+
+      res = await validated.baz('arg');
+      expect(model.baz).toHaveBeenCalledWith('arg');
+      expect(res).toBe(20);
+    });
+
+    it('should support models that use `this` keyword or mutate their own data', () => {
+      model = {
+        val: 5,
+        foo: jest.fn().mockReturnValue(10),
+        bar(arg) { this.val += 1; return this.val + this.foo(arg); },
+        baz(arg) { return 2 * this.bar(arg); }
+      };
+      const validated = wrapValidation(validator, model);
+
+      let res = validated.bar('arg1');
+      expect(model.foo).toHaveBeenCalledWith('arg1');
+      expect(res).toBe(16);
+
+      res = validated.baz('arg2');
+      expect(model.foo).toHaveBeenCalledWith('arg2');
+      expect(res).toBe(34);
     });
   });
 });
--- app/api/utils/wrapValidation.ts
+++ app/api/utils/wrapValidation.ts
@@ -1,13 +1,14 @@
-export function wrapValidation<V, M>(validator: V, model: M): M {
+export function wrapValidation<V extends any, M extends any>(validator: V, model: M): M {
   const wrappedModel = { ...model };
-  Object.keys(wrappedModel).forEach((fn) => {
-    if (fn in validator) {
-      // @ts-ignore
-      wrappedModel[fn] = (...args) => {
-        // @ts-ignore
-        validator[fn](...args);
-        // @ts-ignore
-        return model[fn](...args);
+  Object.keys(wrappedModel).forEach((fnName) => {
+    if (fnName in validator) {
+      wrappedModel[fnName] = (...args: any[]) => {
+        const validatorFn = validator[fnName].bind(validator);
+        const res = validatorFn(...args);
+        if (res && res.then) {
+          return res.then(() => model[fnName](...args));
+        }
+        return model[fnName](...args);
       };
     }
   });
