f26a3eb6ea48fff0dfdf10dd335ebc6dd8bf53c9
GabrielPiles <gabriel.piles.glez@gmail.com>
2019-10-25 16:06:05 +0200

    Clarify id duplications in not default languages when create entity with id

--- app/api/entities/entities.js
+++ app/api/entities/entities.js
@@ -66,7 +66,8 @@ function updateEntity(entity, _template) {
 function createEntity(doc, languages, sharedId) {
   const docs = languages.map((lang) => {
     const langDoc = Object.assign({}, doc);
-    if (!lang.default) {
+    const avoidIdDuplication = docs._id && !lang.default;
+    if (avoidIdDuplication) {
       delete langDoc._id;
     }
     langDoc.language = lang.key;
