d4110b063672141cad2a91fce6d79375f0328722
Eric Wamugu <wamugu@gmail.com>
2019-10-28 17:15:38 +0200

    search for all layers that are set as interactive:true in styles.json. These will go to the interactiveLayerIds array

--- app/react/Map/Map.js
+++ app/react/Map/Map.js
@@ -139,7 +139,14 @@ export default class Map extends Component {
     this.zoomIn = () => this.zoom(+1);
     this.zoomOut = () => this.zoom(-1);
 
-    this.interactiveLayerIds = ['clusters'];
+    // this.interactiveLayerIds = ['clusters'];
+    this.interactiveLayerIds = [];
+    if (this.mapStyle.has('layers')) {
+      this.interactiveLayerIds = this.mapStyle.get('layers')
+        .filter(l => l.get('interactive'))
+        .map(l => l.get('id'))
+        .toJS();
+    }
   }
 
   processClusterOnClick(cluster) {
