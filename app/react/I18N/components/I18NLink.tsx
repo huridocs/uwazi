eb2c58d193c4a167ce603f296650c5b38d5911f4
RafaPolit <rafaelpolit@gmail.com>
2019-11-15 12:34:06 -0500

    Migrated I18NLink to typescript.

--- app/react/I18N/components/I18NLink.tsx
+++ app/react/I18N/components/I18NLink.tsx
@@ -1,16 +1,27 @@
-import PropTypes from 'prop-types';
+/** @format */
+
+import PropTypes, { InferProps } from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import objectWithoutKeys from 'app/utils/objectWithoutKeys';
 
-export class I18NLink extends Component {
-  constructor(props) {
+const I18NLinkPropTypes = {
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  children: PropTypes.any,
+};
+
+type I18NLinkProps = InferProps<typeof I18NLinkPropTypes>;
+
+export class I18NLink extends Component<I18NLinkProps> {
+  static defaultProps: I18NLinkProps;
+  constructor(props: I18NLinkProps) {
     super(props);
     this.onClick = this.onClick.bind(this);
   }
 
-  onClick(e) {
+  onClick(e: { preventDefault: () => void }) {
     if (this.props.disabled) {
       e.preventDefault();
       return;
@@ -32,12 +43,7 @@ I18NLink.defaultProps = {
   onClick: undefined,
 };
 
-I18NLink.propTypes = {
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
-};
-
-export function mapStateToProps({ locale }, ownProps) {
+export function mapStateToProps({ locale }: { locale?: string }, ownProps: any) {
   return { to: `/${locale || ''}/${ownProps.to}`.replace(/\/+/g, '/') };
 }
 
