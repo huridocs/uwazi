8cb5abe5a286d8cf9d6d0a90c7fc7998cdcce20d
RafaPolit <rafaelpolit@gmail.com>
2019-11-19 12:46:41 -0500

    Extended passport to use token

--- app/api/auth/passport_conf.js
+++ app/api/auth/passport_conf.js
@@ -1,18 +1,25 @@
+/** @format */
+
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import users from 'api/users/users';
 
 const getDomain = req => `${req.protocol}://${req.get('host')}`;
 
-passport.use('local', new LocalStrategy(
-  {
-    passReqToCallback: true
-  },
-  (req, username, password, done) => {
-    users.login({ username, password }, getDomain(req))
-    .then(user => done(null, user))
-    .catch(e => e.code === 401 ? done(null, false) : done(e));
-  })
+passport.use(
+  'local',
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    (req, username, password, done) => {
+      const token = req.body ? req.body.token : undefined;
+      users
+        .login({ username, password, token }, getDomain(req))
+        .then(user => done(null, user))
+        .catch(e => done(e));
+    }
+  )
 );
 
 passport.serializeUser((user, done) => {
@@ -20,8 +27,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  users.getById(id)
-  .then((user) => {
+  users.getById(id).then(user => {
     delete user.password;
     done(null, user);
   });
