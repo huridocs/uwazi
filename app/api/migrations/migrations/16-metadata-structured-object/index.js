2396f074df210d2a8ca5d417a47002d7f4d1ab49
Benjamin Dittes <bdittes@google.com>
2019-11-29 15:20:42 +0100

    fix merge conflict

--- app/api/migrations/migrations/16-metadata-structured-object/index.js
+++ app/api/migrations/migrations/16-metadata-structured-object/index.js
@@ -69,7 +69,7 @@ export default {
       if (index % 100 === 0) {
         process.stdout.write(`Converted entities.metadata -> ${index} / ${total}\r`);
       }
-    };
+    });
     process.stdout.write(`Converted entities.metadata -> ${index}\r`);
     process.stdout.write('\r\n');
   },
