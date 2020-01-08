4fa782e641a2664afe8f552b3dfc949c611eb283
RafaPolit <rafaelpolit@gmail.com>
2020-01-08 13:32:15 -0500

    Prettyfied file.

--- app/api/csv/specs/importFile.spec.js
+++ app/api/csv/specs/importFile.spec.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import path from 'path';
 import fs from 'fs';
 import importFile from '../importFile';
@@ -5,14 +7,12 @@ import { createTestingZip } from './helpers';
 
 describe('importFile', () => {
   beforeAll(async () => {
-    await createTestingZip([
-      path.join(__dirname, '/zipData/import.csv'),
-      path.join(__dirname, '/zipData/file1.txt'),
-    ], 'ImportFile.zip');
+    await createTestingZip(
+      [path.join(__dirname, '/zipData/import.csv'), path.join(__dirname, '/zipData/file1.txt')],
+      'ImportFile.zip'
+    );
 
-    await createTestingZip([
-      path.join(__dirname, '/zipData/file1.txt'),
-    ], 'badFile.zip');
+    await createTestingZip([path.join(__dirname, '/zipData/file1.txt')], 'badFile.zip');
   });
 
   afterAll(() => {
@@ -21,45 +21,45 @@ describe('importFile', () => {
   });
 
   describe('readStream', () => {
-    it('should return a readable stream for the csv file', async (done) => {
+    it('should return a readable stream for the csv file', async done => {
       const file = importFile(path.join(__dirname, '/test.csv'));
       let fileContents;
       (await file.readStream())
-      .on('data', (chunk) => {
-        fileContents += chunk;
-      })
-      .on('end', () => {
-        expect(fileContents).toMatchSnapshot();
-        done();
-      });
-    });
-
-    describe('when file is a a zip', () => {
-      it('should return a stream for the first csv file it encounters', async (done) => {
-        const file = importFile(path.join(__dirname, '/zipData/ImportFile.zip'));
-        let fileContents;
-        (await file.readStream())
-        .on('data', (chunk) => {
+        .on('data', chunk => {
           fileContents += chunk;
         })
         .on('end', () => {
           expect(fileContents).toMatchSnapshot();
           done();
         });
-      });
+    });
 
-      describe('when passing a filename', () => {
-        it('should return a read stream for that file', async (done) => {
-          const file = importFile(path.join(__dirname, '/zipData/ImportFile.zip'));
-          let fileContents;
-          (await file.readStream('file1.txt'))
-          .on('data', (chunk) => {
+    describe('when file is a a zip', () => {
+      it('should return a stream for the first csv file it encounters', async done => {
+        const file = importFile(path.join(__dirname, '/zipData/ImportFile.zip'));
+        let fileContents;
+        (await file.readStream())
+          .on('data', chunk => {
             fileContents += chunk;
           })
           .on('end', () => {
             expect(fileContents).toMatchSnapshot();
             done();
           });
+      });
+
+      describe('when passing a filename', () => {
+        it('should return a read stream for that file', async done => {
+          const file = importFile(path.join(__dirname, '/zipData/ImportFile.zip'));
+          let fileContents;
+          (await file.readStream('file1.txt'))
+            .on('data', chunk => {
+              fileContents += chunk;
+            })
+            .on('end', () => {
+              expect(fileContents).toMatchSnapshot();
+              done();
+            });
         });
       });
     });
