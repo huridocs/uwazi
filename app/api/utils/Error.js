6a28ebfeba5eb38c15844d9f0d943aa7e4fc0714
Daneryl <daneryl@gmail.com>
2020-01-30 16:01:31 -0500

    validateRequests now accepts JSONschema schemas

--- app/api/utils/Error.js
+++ app/api/utils/Error.js
@@ -1,3 +1,7 @@
 export default function(message, code = 500) {
+  if (message instanceof Error) {
+    return { ...message, code };
+  }
+
   return { message, code };
 }
--- app/api/utils/validateRequest.js
+++ app/api/utils/validateRequest.js
@@ -1,18 +1,31 @@
 /** @format */
 
+import Ajv from 'ajv';
 import Joi from 'joi';
 import objectId from 'joi-objectid';
+import { wrapValidator } from 'shared/tsUtils';
 import createError from './Error';
 
 Joi.objectId = objectId(Joi);
+const ajv = Ajv({ allErrors: true });
 
-export default (schema, propTovalidate = 'body') => (req, _res, next) => {
-  const result = Joi.validate(req[propTovalidate], schema);
-  if (result.error) {
-    next(createError(result.error.toString(), 400));
-  }
+export default (schema, propTovalidate = 'body') => async (req, _res, next) => {
+  if (schema.isJoi) {
+    const result = Joi.validate(req[propTovalidate], schema);
+    if (result.error) {
+      next(createError(result.error.toString(), 400));
+    }
 
-  if (!result.error) {
-    next();
+    if (!result.error) {
+      next();
+    }
+  } else {
+    try {
+      const validator = wrapValidator(ajv.compile({ ...schema, $async: true }));
+      const result = await validator(req);
+      next();
+    } catch (e) {
+      next(createError(e, 400));
+    }
   }
 };
