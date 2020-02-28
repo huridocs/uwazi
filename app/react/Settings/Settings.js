168dec2290e37920dee84514e6d7043eddbb7d54
Samantha Schaevitz <sam@huridocs.org>
2020-02-28 16:54:28 +0100

    Use totalRows count as per doc unique for ThesauriList.

--- app/react/Settings/Settings.js
+++ app/react/Settings/Settings.js
@@ -68,22 +68,13 @@ export class Settings extends RouteHandler {
       tup.forEach(perm => {
         const prop = perm[0];
         const results = perm[1][1];
-        if (results.aggregations.all.hasOwnProperty(`_${prop.name}`)) {
-          const { buckets } = results.aggregations.all[`_${prop.name}`];
-          let soFar = 0;
-          buckets.forEach(bucket => {
-            soFar += bucket.filtered.doc_count;
-          });
-          const thesaurus = modeledThesauri.find(t => t._id === prop.content);
-          // NOTE: These suggestions are totaling per-value suggestions per
-          // document, so certain documents with more than one suggested value
-          // may be counted more than once.
-          // TODO(sam): Make document counts unique.
-          if (!thesaurus.hasOwnProperty('suggestions')) {
-            thesaurus.suggestions = 0;
-          }
-          thesaurus.suggestions += soFar;
+        const uniqueDocs = results.totalRows;
+
+        const thesaurus = modeledThesauri.find(t => t._id === prop.content);
+        if (!thesaurus.hasOwnProperty('suggestions')) {
+          thesaurus.suggestions = 0;
         }
+        thesaurus.suggestions += uniqueDocs;
       });
     });
 
