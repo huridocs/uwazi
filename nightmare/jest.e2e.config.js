40154b1d4bcbad0b4ad240c7aae5be9b7c5a1371
Eric Wamugu <wamugu@gmail.com>
2020-05-05 14:35:17 +0200

    temp edit to run on graphs test suite in Github CI

--- nightmare/jest.e2e.config.js
+++ nightmare/jest.e2e.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'E2E',
   displayName: 'E2E',
-  testMatch: ['**/nightmare/**/paths/*.spec.js?(x)'],
+  testMatch: ['**/nightmare/**/paths/graphs.spec.js?(x)'],
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/setUpJestE2E.js'],
 };
