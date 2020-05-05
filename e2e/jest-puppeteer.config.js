2b90ebc33afaa7e7a23ad3881c5a36568e7f833f
jaumec@gmail.com <jaumec@gmail.com>
2020-05-05 09:49:11 -0600

    Removed cypress. Fixed some dependencies

--- e2e/jest-puppeteer.config.js
+++ e2e/jest-puppeteer.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	launch: {
 		dumpio: true,
 		headless: false,
-		slowMo: 0,
+		slowMo: 1,
 		devtools: false,
 		args: ['--disable-infobars', '--disable-gpu', '--window-size=1300,800'],
 	},
