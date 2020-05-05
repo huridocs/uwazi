fe0fe8cd4643c0e0d2990bcdaa9d0659fe489329
RafaPolit <rafaelpolit@gmail.com>
2020-05-05 10:59:08 -0500

    Increased the ES fields limit to 4000

--- database/elastic_mapping.js
+++ database/elastic_mapping.js
@@ -5,7 +5,7 @@ import languages from '../app/shared/languages';
 
 const config = {
   settings: {
-    'index.mapping.total_fields.limit': 2000,
+    'index.mapping.total_fields.limit': 4000,
     'index.number_of_replicas': 0,
     'index.number_of_shards': 3,
     analysis: {
