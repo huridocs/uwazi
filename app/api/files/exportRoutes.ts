7cf89242c711eb3f681b5f2cfc07a08dcf2ce6f6
Federico Nocetti <fnocetti@users.noreply.github.com>
2020-05-14 14:37:46 -0300

    Improve typing and error handling

--- app/api/files/exportRoutes.ts
+++ app/api/files/exportRoutes.ts
@@ -1,3 +1,4 @@
+import { Application, Request, Response, NextFunction } from 'express';
 import { createWriteStream, unlink } from 'fs';
 import errorLog from 'api/log/errorLog';
 import { search } from 'api/search';
@@ -5,18 +6,9 @@ import { validation } from '../utils';
 import { isArray } from 'util';
 import { temporalFilesPath, generateFileName } from './filesystem';
 import { CSVExporter } from 'api/csv';
-import { User } from 'api/users/usersModel';
 import settings from 'api/settings';
 
-type Middleware = (
-  req: { user: User; language: any; query: any },
-  res: { download: any },
-  next: any
-) => Promise<void> | void;
-type Get = { (arg0: string, arg1: Middleware, arg2: Middleware): void };
-type App = { get: Get };
-
-export default (app: App) => {
+export default (app: Application) => {
   const parseQueryProperty = (query: any, property: string) =>
     query[property] ? JSON.parse(query[property]) : query[property];
 
@@ -50,26 +42,27 @@ export default (app: App) => {
         },
       },
     }),
-    async (req, res, next) => {
-      req.query.filters = parseQueryProperty(req.query, 'filters');
-      req.query.types = parseQueryProperty(req.query, 'types');
-      req.query.unpublished = parseQueryProperty(req.query, 'unpublished');
-      req.query.includeUnpublished = parseQueryProperty(req.query, 'includeUnpublished');
-      req.query.allAggregations = parseQueryProperty(req.query, 'allAggregations');
-
-      req.query.ids = parseQueryProperty(req.query, 'ids');
-      if (!isArray(req.query.ids)) delete req.query.ids;
-
-      const results = await search.search(req.query, req.language, req.user);
-      // eslint-disable-next-line camelcase
-      const { dateFormat, site_name } = await settings.get();
-
-      const exporter = new CSVExporter();
+    async (req: Request, res: Response, next: NextFunction) => {
       const temporalFilePath = temporalFilesPath(generateFileName({ originalname: 'export.csv' }));
-      const fileStream = createWriteStream(temporalFilePath);
-      const exporterOptions = { dateFormat, language: req.language };
-
       try {
+        req.query.filters = parseQueryProperty(req.query, 'filters');
+        req.query.types = parseQueryProperty(req.query, 'types');
+        req.query.unpublished = parseQueryProperty(req.query, 'unpublished');
+        req.query.includeUnpublished = parseQueryProperty(req.query, 'includeUnpublished');
+        req.query.allAggregations = parseQueryProperty(req.query, 'allAggregations');
+
+        req.query.ids = parseQueryProperty(req.query, 'ids');
+        if (!isArray(req.query.ids)) delete req.query.ids;
+
+        const results = await search.search(req.query, req.language, req.user);
+        // eslint-disable-next-line camelcase
+        const { dateFormat, site_name } = await settings.get();
+
+        const exporter = new CSVExporter();
+
+        const fileStream = createWriteStream(temporalFilePath);
+        const exporterOptions = { dateFormat, language: req.language };
+
         await exporter.export(results, req.query.types, fileStream, exporterOptions);
 
         res.download(
--- app/shared/types/index.d.ts
+++ app/shared/types/index.d.ts
@@ -1,11 +1,16 @@
-/** @format */
-
 export {};
 
 declare global {
   namespace jest {
     interface Matchers<R> {
       toNeedAuthorization(): R;
+    }
+  }
+
+  namespace Express {
+    interface Request {
+      user: import('api/users/usersModel').User;
+      language: string;
     }
   }
 }
