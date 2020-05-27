f4389fd7fd9b4b5ac1a298488c5becf99303cb18
jaumec@gmail.com <jaumec@gmail.com>
2020-05-27 12:09:33 -0600

    Added more @types and fixed a typo

--- e2e/helpers/proxyMock.ts
+++ e2e/helpers/proxyMock.ts
@@ -8,7 +8,6 @@ export default async () => {
       'https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700|Roboto+Slab:100,300,400,700|Roboto:100,300,400,500,700,900'
     ) {
       request.respond({
-        content: 'application/json',
         headers: { 'Access-Control-Allow-Origin': '*' },
         body: JSON.stringify(''),
       });
