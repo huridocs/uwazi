8bf347cbcd1b8d2fdcde3e549dced7fcd54490a5
jaumec@gmail.com <jaumec@gmail.com>
2020-06-02 21:35:33 -0600

    Increased slowmo to 3 to avoid some rendering flackyness

--- e2e/jest-puppeteer.config.ts
+++ e2e/jest-puppeteer.config.ts
@@ -3,7 +3,7 @@ module.exports = {
   launch: {
     dumpio: false,
     headless: true,
-    slowMo: 2,
+    slowMo: 3,
     defaultViewport: null,
     devtools: false,
     args: ['--disable-infobars', '--disable-gpu', '--window-size=1300,800'],
