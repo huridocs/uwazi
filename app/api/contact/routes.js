02a0225600be257e4971fd8a38b6a9ef40e8ea52
jaumec@gmail.com <jaumec@gmail.com>
2020-06-11 15:27:11 -0600

    Added AJV validation to coontact route instead of Joi

--- app/api/contact/routes.js
+++ app/api/contact/routes.js
@@ -1,19 +1,21 @@
-import Joi from 'joi';
 import contact from './contact';
 import { validation } from '../utils';
 
 export default app => {
   app.post(
     '/api/contact',
-    validation.validateRequest(
-      Joi.object()
-        .keys({
-          email: Joi.string().required(),
-          name: Joi.string().required(),
-          message: Joi.string().required(),
-        })
-        .required()
-    ),
+    validation.validateRequest({
+      properties: {
+        body: {
+          properties: {
+            email: { type: 'string', format: 'email' },
+            name: { type: 'string', minLength: 3 },
+            message: { type: 'string', minLength: 5 },
+          },
+          required: ['email', 'name', 'message'],
+        },
+      },
+    }),
     (req, res, next) =>
       contact
         .sendMessage(req.body)
