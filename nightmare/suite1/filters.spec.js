ad3399ecf2e59ca6d6122a849fa6e3029b50612a
jaumec@gmail.com <jaumec@gmail.com>
2020-06-18 15:54:42 -0600

    Fixed linting errors

--- nightmare/suite1/filters.spec.js
+++ nightmare/suite1/filters.spec.js
@@ -12,50 +12,50 @@ describe('filters path', () => {
   afterAll(async () => nightmare.end());
 
   describe('login', () => {
-    it('should log in as admin then click the settings nav button', (done) => {
+    it('should log in as admin then click the settings nav button', done => {
       loginAsAdminAndGoToSettings(nightmare, catchErrors, done);
     });
   });
 
   describe('Filters tests', () => {
-    it('should click Filters button and then click on Create Group button', (done) => {
+    it('should click Filters button and then click on Create Group button', done => {
       nightmare
-      .waitToClick(selectors.settingsView.filtersButton)
-      .waitToClick(selectors.settingsView.createFilterGroupButton)
-      .wait(selectors.settingsView.newFilterGroupForm)
-      .isVisible(selectors.settingsView.newFilterGroupForm)
-      .then((result) => {
-        expect(result).toBe(true);
-        done();
-      })
-      .catch(catchErrors(done));
+        .waitToClick(selectors.settingsView.filtersButton)
+        .waitToClick(selectors.settingsView.createFilterGroupButton)
+        .wait(selectors.settingsView.newFilterGroupForm)
+        .isVisible(selectors.settingsView.newFilterGroupForm)
+        .then(result => {
+          expect(result).toBe(true);
+          done();
+        })
+        .catch(catchErrors(done));
     });
 
-    it('should create a group called Test Group', (done) => {
+    it('should create a group called Test Group', done => {
       nightmare
-      .clearInput(selectors.settingsView.newFilterGroupForm)
-      .write(selectors.settingsView.newFilterGroupForm, 'Test Group')
-      .waitToClick(selectors.settingsView.filtrableTypesSaveButton)
-      .wait('.alert.alert-success')
-      .isVisible('.alert.alert-success')
-      .then((result) => {
-        expect(result).toBe(true);
-        done();
-      })
-      .catch(catchErrors(done));
+        .clearInput(selectors.settingsView.newFilterGroupForm)
+        .write(selectors.settingsView.newFilterGroupForm, 'Test Group')
+        .waitToClick(selectors.settingsView.filtrableTypesSaveButton)
+        .wait('.alert.alert-success')
+        .isVisible('.alert.alert-success')
+        .then(result => {
+          expect(result).toBe(true);
+          done();
+        })
+        .catch(catchErrors(done));
     });
 
-    it('should delete the filters group', (done) => {
+    it('should delete the filters group', done => {
       nightmare
-      .deleteItemFromList(selectors.settingsView.listOfFilterGroups, 'Test Group')
-      .waitToClick(selectors.settingsView.filtrableTypesSaveButton)
-      .wait('.alert.alert-success')
-      .isVisible('.alert.alert-success')
-      .then((result) => {
-        expect(result).toBe(true);
-        done();
-      })
-      .catch(catchErrors(done));
+        .deleteItemFromList(selectors.settingsView.listOfFilterGroups, 'Test Group')
+        .waitToClick(selectors.settingsView.filtrableTypesSaveButton)
+        .wait('.alert.alert-success')
+        .isVisible('.alert.alert-success')
+        .then(result => {
+          expect(result).toBe(true);
+          done();
+        })
+        .catch(catchErrors(done));
     });
   });
 });
