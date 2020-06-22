194e23ed484577b9a81bac9af866d6ae98338075
jaumec@gmail.com <jaumec@gmail.com>
2020-06-22 09:37:44 -0600

    Supressed a litner warning

--- app/react/Notifications/components/Notification.js
+++ app/react/Notifications/components/Notification.js
@@ -23,6 +23,7 @@ export class Notification extends Component {
         <Icon icon={icon} />
         <span className="alert-text">
           {message.split('\n').map((item, i) => (
+            //eslint-disable-next-line react/no-array-index-key
             <React.Fragment key={i}>
               {item}
               <br />
