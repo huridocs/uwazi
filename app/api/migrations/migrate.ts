f388902de735e23f406d129ccacb0ca32d4add9d
Daneryl <daneryl@gmail.com>
2020-06-25 10:38:22 +0200

    fixed closegraylog on migrate script

--- app/api/migrations/migrate.ts
+++ app/api/migrations/migrate.ts
@@ -1,7 +1,7 @@
 import { DB } from 'api/odm';
 import { tenants } from 'api/tenants/tenantContext';
 import { config } from 'api/config';
-
+import errorLog from 'api/log/errorLog';
 import { migrator } from './migrator';
 
 const run = async () => {
@@ -11,6 +11,8 @@ const run = async () => {
   await tenants.run(async () => {
     await migrator.migrate(db);
   });
+  //@ts-ignore
+  errorLog.closeGraylog();
   await DB.disconnect();
 };
 
