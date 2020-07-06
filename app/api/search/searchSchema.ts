6f4fa37fd50858ab84af27ba3ff6ac8e1f119859
Mercy <facar_paulina@hotmail.com>
2020-07-06 13:22:07 -0500

    Supporting number type in search term

---
+++ app/api/search/searchSchema.ts
@@ -0,0 +1,25 @@
+export const searchSchema = {
+  properties: {
+    query: {
+      properties: {
+        filters: { type: 'object' },
+        types: { type: 'array', items: [{ type: 'string' }] },
+        _types: { type: 'array', items: [{ type: 'string' }] },
+        fields: { type: 'array', items: [{ type: 'string' }] },
+        allAggregations: { type: 'boolean' },
+        userSelectedSorting: { type: 'boolean' },
+        aggregations: { type: 'string' },
+        order: { type: 'string', enum: ['asc', 'desc'] },
+        sort: { type: 'string' },
+        limit: { type: 'number' },
+        from: { type: 'number' },
+        searchTerm: { anyOf: [{ type: 'string' }, { type: 'number' }] },
+        includeUnpublished: { type: 'boolean' },
+        treatAs: { type: 'string' },
+        unpublished: { type: 'boolean' },
+        select: { type: 'array', items: [{ type: 'string' }] },
+        geolocation: { type: 'boolean' },
+      },
+    },
+  },
+};
