d3bd1cb6d0d293ba5d2bf29744d0630765bdfe81
Federico Nocetti <fnocetti@users.noreply.github.com>
2020-07-08 19:37:13 -0300

    Adapt algorithms and UI to new validation cases

--- app/react/Templates/components/FilterSuggestions.js
+++ app/react/Templates/components/FilterSuggestions.js
@@ -5,6 +5,26 @@ import { Icon } from 'UI';
 
 import Icons from './Icons';
 
+function getMatchMessage({ typeConflict, relationConflict, contentConflict }) {
+  let title =
+    'This property has the same configuration as others with the same label and will be used together.';
+
+  if (contentConflict) {
+    title = 'Properties with the same label but different thesauri as content are not allowed.';
+  }
+
+  if (relationConflict) {
+    title =
+      'Relationship properties with the same label but different relationship types are not allowed.';
+  }
+
+  if (typeConflict) {
+    title = 'Properties with the same label but different types are not allowed.';
+  }
+
+  return title;
+}
+
 export class FilterSuggestions extends Component {
   static getTypeIcon(type) {
     return Icons[type] || 'fa fa-font';
@@ -15,6 +35,12 @@ export class FilterSuggestions extends Component {
       this.props.thesauris.toJS().find(thesauri => thesauri._id === thesauriId) || {};
 
     return _thesauri.name;
+  }
+
+  getRelationTypeName(relationTypeId) {
+    const relation = this.props.relationTypes.toJS().find(r => r._id === relationTypeId) || {};
+
+    return relation.name;
   }
 
   findSameLabelProperties(label, templates) {
@@ -28,30 +54,37 @@ export class FilterSuggestions extends Component {
         if (property) {
           return { template: template.name, property };
         }
+
+        return null;
       })
       .filter(match => match);
   }
 
-  filterSuggestions(label, type, content, hasThesauri) {
+  filterSuggestions(label, type, content, relationType, hasThesauri) {
     return this.findSameLabelProperties(label, this.props.templates.toJS()).map(
       (propertyMatch, index) => {
         const typeConflict = propertyMatch.property.type !== type;
+        const relationConflict =
+          relationType && propertyMatch.property.relationType !== relationType;
         const contentConflict = propertyMatch.property.content !== content;
-        return this.renderMatch(propertyMatch, typeConflict, contentConflict, hasThesauri, index);
+        return this.renderMatch(
+          propertyMatch,
+          { typeConflict, relationConflict, contentConflict },
+          hasThesauri,
+          index
+        );
       }
     );
   }
 
-  renderMatch(propertyMatch, typeConflict, contentConflict, hasThesauri, index) {
+  renderMatch(
+    propertyMatch,
+    { typeConflict, relationConflict, contentConflict },
+    hasThesauri,
+    index
+  ) {
     const activeClass = this.props.filter ? 'property-atributes is-active' : 'property-atributes';
-    let title = 'This property has the same configuration and will be used together.';
-    if (contentConflict) {
-      title = "This property has different Thesauri and wont't be used together.";
-    }
-
-    if (typeConflict) {
-      title = "This property has different Type and wont't be used together.";
-    }
+    const title = getMatchMessage({ typeConflict, relationConflict, contentConflict });
     const icon = FilterSuggestions.getTypeIcon(propertyMatch.property.type);
     const type =
       propertyMatch.property.type[0].toUpperCase() + propertyMatch.property.type.slice(1);
@@ -61,16 +94,26 @@ export class FilterSuggestions extends Component {
         <td>
           <Icon icon="file" /> {propertyMatch.template}
         </td>
-        <td className={typeConflict ? 'conflict' : ''}>
-          <Icon icon="exclamation-triangle" />
-          <Icon icon={icon} /> {type}
+        <td className={typeConflict || relationConflict ? 'conflict' : ''}>
+          {typeConflict || relationConflict ? <Icon icon="exclamation-triangle" /> : null}
+          <Icon icon={icon} />
+          {(() => {
+            if (type === 'Relationship') {
+              const relationTypeName = this.getRelationTypeName(
+                propertyMatch.property.relationType
+              );
+              return `${type} (${relationTypeName})`;
+            }
+
+            return type;
+          })()}
         </td>
         {(() => {
           if (hasThesauri && propertyMatch.property.content) {
             const thesauri = this.getThesauriName(propertyMatch.property.content);
             return (
               <td className={contentConflict ? 'conflict' : ''}>
-                <Icon icon="exclamation-triangle" />
+                {contentConflict ? <Icon icon="exclamation-triangle" /> : null}
                 <Icon icon="book" /> {thesauri}
               </td>
             );
@@ -82,47 +125,56 @@ export class FilterSuggestions extends Component {
   }
 
   render() {
-    const { label, type, content } = this.props;
+    const { label, type, content, relationType } = this.props;
     const hasThesauri = typeof content !== 'undefined';
     const activeClass = this.props.filter ? 'property-atributes is-active' : 'property-atributes';
     const title = 'This is the current property and will be used together with equal properties.';
     const icon = FilterSuggestions.getTypeIcon(type);
 
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Document or entity</th>
-            <th>Type</th>
-            {(() => {
-              if (hasThesauri) {
-                return <th>Thesauri</th>;
-              }
-            })()}
-          </tr>
-        </thead>
-        <tbody>
-          <tr className={activeClass} title={title}>
-            <td>
-              <Icon icon="file" /> {this.props.templateName}
-            </td>
-            <td>
-              <Icon icon={icon} /> {type[0].toUpperCase() + type.slice(1)}
-            </td>
-            {(() => {
-              if (hasThesauri) {
-                const thesauri = this.getThesauriName(content);
-                return (
-                  <td>
-                    <Icon icon="book" /> {thesauri}
-                  </td>
-                );
-              }
-            })()}
-          </tr>
-          {this.filterSuggestions(label, type, content, hasThesauri)}
-        </tbody>
-      </table>
+      <React.Fragment>
+        <label className="suggestions-label">
+          Properties from other templates in the collection using the same label
+        </label>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Document or entity</th>
+              <th>Type</th>
+              {(() => {
+                if (hasThesauri) {
+                  return <th>Thesauri</th>;
+                }
+
+                return null;
+              })()}
+            </tr>
+          </thead>
+          <tbody>
+            <tr className={activeClass} title={title}>
+              <td>
+                <Icon icon="file" /> {this.props.templateName}
+              </td>
+              <td>
+                <Icon icon={icon} /> {type[0].toUpperCase() + type.slice(1)}
+              </td>
+              {(() => {
+                if (hasThesauri) {
+                  const thesauri = this.getThesauriName(content);
+                  return (
+                    <td>
+                      <Icon icon="book" /> {thesauri}
+                    </td>
+                  );
+                }
+
+                return null;
+              })()}
+            </tr>
+            {this.filterSuggestions(label, type, content, relationType, hasThesauri)}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   }
 }
@@ -135,19 +187,23 @@ FilterSuggestions.propTypes = {
   templateId: PropTypes.string,
   templates: PropTypes.object,
   thesauris: PropTypes.object,
+  relationTypes: PropTypes.object,
   content: PropTypes.string,
+  relationType: PropTypes.string,
 };
 
 export function mapStateToProps(state, props) {
   return {
     templates: state.templates,
     thesauris: state.thesauris,
+    relationTypes: state.relationTypes,
     templateName: state.template.data.name,
     templateId: state.template.data._id,
     type: state.template.data.properties[props.index].type,
     filter: state.template.data.properties[props.index].filter,
     label: state.template.data.properties[props.index].label,
     content: state.template.data.properties[props.index].content,
+    relationType: state.template.data.properties[props.index].relationType,
   };
 }
 
