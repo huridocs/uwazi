22dd7672d47df138927e2480f4e6ef3cfdb8c45c
Mercy <facar_paulina@hotmail.com>
2020-07-21 16:29:35 -0500

    Listing documents in table wip

--- app/react/Layout/TableViewer.tsx
+++ app/react/Layout/TableViewer.tsx
@@ -1,77 +1,72 @@
-import React from "react";
-import { useTable } from 'react-table'
+import React from 'react';
+import { connect } from 'react-redux';
+import { TemplateSchema } from 'shared/types/templateType';
+import { PropertySchema } from 'shared/types/commonTypes';
 
 export interface DocumentViewerProps {
-  rowListZoomLevel: number,
-  documents: any,
-  storeKey: 'library' | 'uploads',
-  clickOnDocument: (...args: any[]) => any,
-  onSnippetClick: (...args: any[]) => any,
-  deleteConnection: (...args: any[]) => any,
-  search: any,
+  rowListZoomLevel: number;
+  documents: any;
+  storeKey: 'library' | 'uploads';
+  clickOnDocument: (...args: any[]) => any;
+  onSnippetClick: (...args: any[]) => any;
+  deleteConnection: (...args: any[]) => any;
+  search: any;
+  templates: any;
 }
 
-export function TableViewer(props: DocumentViewerProps) {
-  const columns = [
-    { 
-      Header: 'Titulo',
-      accessor: 'title',
-    }
-  ];
-  const data = props.documents.get('rows').map((doc: any) => ({ title: doc.get('title') }));
+function TableView(props: DocumentViewerProps) {
+  const data = props.documents.get('rows').toJS();
+  const templateIds = props.documents
+    .getIn(['aggregations', 'all', '_types', 'buckets'])
+    .filter((t: any) => t.getIn(['filtered', 'doc_count']) > 0)
+    .map((t: any) => t.get('key'))
+    .toJS();
 
-  const table = useTable({columns, data});
+  const templates = props.templates.filter((t: TemplateSchema) =>
+    templateIds.includes(t.get('_id'))
+  );
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = table;
-  
+  const columns = templates.reduce((properties: PropertySchema[], template: TemplateSchema) => {
+    const prs = properties.concat(template.get('properties')?.toJS() || []);
+    return prs;
+  }, []);
+  //show in cards de los templates
+  //all properties
+
   return (
-    // apply the table props
-    <table {...getTableProps()}>
+    <table>
       <thead>
-        {// Loop over the header rows
-        headerGroups.map((headerGroup: any) => (
-          // Apply the header row props
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {// Loop over the headers in each row
-            headerGroup.headers.map((column: any) => (
-              // Apply the header cell props
-              <th {...column.getHeaderProps()}>
-                {// Render the header
-                column.render('Header')}
-              </th>
+        <tr>
+          <th>Title</th>
+          <th>Date added</th>
+          <th>Type</th>
+          {columns.map((column: any) => (
+            <th>{column.label}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row: any) => (
+          <tr>
+            <td>{row.title}</td>
+            <td>{row.createdAt}</td>
+            <td>{row.type}</td>
+            {columns.map((column: any) => (
+              <td>
+                {row.metadata && row.metadata[column.name] && row.metadata[column.name][0]
+                  ? JSON.stringify(row.metadata[column.name][0].value)
+                  : ''}
+              </td>
             ))}
           </tr>
         ))}
-      </thead>
-      {/* Apply the table body props */}
-      <tbody {...getTableBodyProps()}>
-        {// Loop over the table rows
-        rows.map((row: any) => {
-          // Prepare the row for display
-          prepareRow(row)
-          return (
-            // Apply the row props
-            <tr {...row.getRowProps()}>
-              {// Loop over the rows cells
-              row.cells.map((cell: any) => {
-                // Apply the cell props
-                return (
-                  <td {...cell.getCellProps()}>
-                    {// Render the cell contents
-                    cell.render('Cell')}
-                  </td>
-                )
-              })}
-            </tr>
-          )
-        })}
       </tbody>
     </table>
-  )
+  );
 }
+
+const mapStateToProps = (state: any) => ({
+  templates: state.templates,
+});
+
+export const TableViewer = connect(mapStateToProps)(TableView);
