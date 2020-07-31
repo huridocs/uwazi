8c1fe879e361add4013736b8e0784e0fcb0ca456
Daneryl <daneryl@gmail.com>
2020-07-31 10:01:52 +0200

    serialize session user with the tenant name

--- app/api/auth/passport_conf.js
+++ app/api/auth/passport_conf.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import users from 'api/users/users';
+import { tenants } from 'api/tenants/tenantContext';
 
 const getDomain = req => `${req.protocol}://${req.get('host')}`;
 
@@ -21,10 +22,16 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => {
-  done(null, user._id);
+  done(null, `${user._id}///${tenants.current().name}`);
 });
 
-passport.deserializeUser((id, done) => {
+passport.deserializeUser((serializeUser, done) => {
+  const currentTenant = tenants.current().name;
+  const [id, serializedTenant] = serializeUser.split('///');
+  if (serializedTenant !== currentTenant) {
+    return done(null, false);
+  }
+
   users.getById(id, '-password').then(user => {
     done(null, user);
   });
