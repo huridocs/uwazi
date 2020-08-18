7f6ca8e47bb9a6c77661a9a9d75afb99cd82c7e2
Alberto Casado Torres <acasadotorres@gmail.com>
2020-08-18 20:36:20 +0200

    made e2e more resilient to re-renders

--- e2e/suites/copy-from.test.ts
+++ e2e/suites/copy-from.test.ts
@@ -4,12 +4,6 @@ import { adminLogin, logout } from '../helpers/login';
 import proxyMock from '../helpers/proxyMock';
 import insertFixtures from '../helpers/insertFixtures';
 
-async function delay(time: number) {
-  return new Promise(function(resolve) {
-    setTimeout(resolve, time);
-  });
-}
-
 describe('Copy from', () => {
   beforeAll(async () => {
     await insertFixtures();
@@ -21,20 +15,34 @@ describe('Copy from', () => {
     await expect(page).toClick(
       'div.documents-list > div > div.item-group.item-group-zoom-0 > div:nth-child(1) > div.item-actions > div > a'
     );
-    await expect(page).toClick('.tab-link', { text: 'Connections' });
-    await expect(page).toClick('button', { text: 'Edit' });
-    await expect(page).toClick('button', { text: 'Add entities / documents' });
-    await delay(50); //slide animation delay
-    await expect(page).toClick('button', { text: 'Create Entity' });
+    await expect(page).toClick('.tab-link', {
+      text: 'Connections',
+    });
+    await expect(page).toClick('button', {
+      text: 'Edit',
+    });
+    await expect(page).toClick('button', {
+      text: 'Add entities / documents',
+    });
+
+    await page.waitFor(100); // wait for slide animation to end
+    await expect(page).toClick('button', {
+      text: 'Create Entity',
+    });
+    await page.waitFor('textarea[name="relationships.metadata.title"]');
     await expect(page).toFill('textarea[name="relationships.metadata.title"]', 'Test title');
     await page.select('select', '58ada34c299e826748545061');
-    await expect(page).toClick('button', { text: 'Copy From' });
-    await page.waitForSelector('div.search-box > div > input');
+    await page.waitFor(20); // re-render of the form
+    await expect(page).toClick('button', {
+      text: 'Copy From',
+    });
+    await page.waitFor(50); // wait for animation
     await expect(page).toFill('aside.connections-metadata div.search-box > div > input', 'artavia');
-    await delay(50); // search delay
+    await page.waitFor(50); // search delay
     await expect(page).toClick('div.copy-from .item-info', { text: 'Artavia Murillo et al' });
     await expect(page).toClick('button', { text: 'Copy Highlighted' });
     await expect(page).toClick('.side-panel button', { text: 'Save' });
+    await page.waitFor(50); // save delay
     await expect(page).toClick('button', { text: 'Save' });
     await expect(page).toClick('.item-info', { text: 'Test title' });
     await expect(page).toMatchElement(
