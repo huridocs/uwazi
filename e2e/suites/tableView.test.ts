c698edb0be9c2b57692422955f402963209cc58d
Mercy Falconí <mercy@grupocentrico.com>
2020-08-20 17:55:48 -0500

    testing timeout

--- e2e/suites/tableView.test.ts
+++ e2e/suites/tableView.test.ts
@@ -6,15 +6,12 @@ import insertFixtures from '../helpers/insertFixtures';
 
 describe('Table view', () => {
   beforeAll(async () => {
-    jest.setTimeout(60000);
     await insertFixtures();
     await proxyMock();
     await page.goto(`${host}/library/table`);
-    await page.waitFor(300);
-  });
-
-  afterAll(async () => {
-    jest.setTimeout(40000);
+    await page.waitFor('.tableview-wrapper', {
+      visible: true,
+    });
   });
 
   describe('Column selector', () => {
