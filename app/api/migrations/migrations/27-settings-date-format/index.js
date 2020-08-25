0027d8c9a1a4e758360d2c08532d365df6a668ec
jaumec@gmail.com <jaumec@gmail.com>
2020-08-25 15:49:45 -0600

    Added migration for existing configured dateFormat in settings

---
+++ app/api/migrations/migrations/27-settings-date-format/index.js
@@ -0,0 +1,26 @@
+export default {
+  delta: 27,
+
+  name: 'settings-date-format',
+
+  description:
+    'Changes the configured date format in settings to be compatible with Date.js instad of moment.js',
+
+  async up(db) {
+    process.stdout.write(`${this.name}...\r\n`);
+
+    const [settings] = await db
+      .collection('settings')
+      .find()
+      .toArray();
+
+    let { dateFormat } = settings;
+    if (dateFormat) {
+      dateFormat = dateFormat.replace('YYYY', 'yyyy');
+      dateFormat = dateFormat.replace('DD', 'dd');
+
+      return db.collection('settings').updateOne({ _id: settings._id }, { $set: { dateFormat } });
+    }
+    return false;
+  },
+};
