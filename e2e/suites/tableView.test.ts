b312e8257283ff1abec7dc4ec51b9fd17c3188d1
Mercy Falconí <mercy@grupocentrico.com>
2020-08-27 22:06:24 -0500

    adding done to tests

--- e2e/suites/tableView.test.ts
+++ e2e/suites/tableView.test.ts
@@ -5,66 +5,69 @@ import proxyMock from '../helpers/proxyMock';
 import insertFixtures from '../helpers/insertFixtures';
 
 describe('Table view', () => {
-  beforeAll(async () => {
+  beforeAll(async done => {
     await insertFixtures();
     await proxyMock();
     await page.goto(`${host}/library/table`);
+    await page.waitFor('.documents-counter');
     await page.waitFor('.tableview-wrapper', {
       visible: true,
     });
+    done();
   });
 
-  describe('Column selector', () => {
-    it('Should show only selected properties', async () => {
-      await page.click('.hidden-columns-dropdown');
-      const columnsOptions = await page.$$eval('#rw_1_listbox li', options =>
-        options.map(option => ({
-          checked: (<HTMLInputElement>option.children[0]).checked,
-          option: option.textContent,
-        }))
-      );
-      const selectedColumns = columnsOptions
-        .filter(option => option.checked)
-        .map(option => option.option);
-      selectedColumns[0] = 'Title';
-      await page.waitFor('.tableview-wrapper');
-      const optionSelector = '.tableview-wrapper th';
-      const visibleColumns = await page.$$eval(optionSelector, columns =>
-        columns.map(column => column.textContent)
-      );
-      expect(selectedColumns).toEqual(visibleColumns);
-    });
-
-    it('Should show new selected properties', async () => {
-      const newColumn = await page.$$eval('#rw_1_listbox li:nth-child(9)', option => {
-        (<HTMLInputElement>option[0]).click();
-        return option[0].textContent;
-      });
-      await page.waitFor(300);
-      const lastColumn = await page.$$eval(
-        '.tableview-wrapper th:last-child',
-        columns => columns[0].textContent
-      );
-      expect(lastColumn).toEqual(newColumn);
-    });
+  it('Should show only selected properties', async done => {
+    await page.click('.hidden-columns-dropdown');
+    const columnsOptions = await page.$$eval('#rw_1_listbox li', options =>
+      options.map(option => ({
+        checked: (<HTMLInputElement>option.children[0]).checked,
+        option: option.textContent,
+      }))
+    );
+    const selectedColumns = columnsOptions
+      .filter(option => option.checked)
+      .map(option => option.option);
+    selectedColumns[0] = 'Title';
+    await page.waitFor('.tableview-wrapper');
+    const optionSelector = '.tableview-wrapper th';
+    const visibleColumns = await page.$$eval(optionSelector, columns =>
+      columns.map(column => column.textContent)
+    );
+    expect(selectedColumns).toEqual(visibleColumns);
+    done();
   });
 
-  it('Should show all properties if all of them are selected', async () => {
+  it('Should show new selected properties', async done => {
+    const newColumn = await page.$$eval('#rw_1_listbox li:nth-child(9)', option => {
+      (<HTMLInputElement>option[0]).click();
+      return option[0].textContent;
+    });
+    await page.waitFor(200);
+    const lastColumn = await page.$$eval(
+      '.tableview-wrapper th:last-child',
+      columns => columns[0].textContent
+    );
+    expect(lastColumn).toEqual(newColumn);
+    done();
+  });
+
+  it('Should show all properties if all of them are selected', async done => {
     await page.click('.hidden-columns-dropdown');
     const showAllSelector = "#rw_1_listbox > li:nth-child(1) > input[type='checkbox']";
     await page.$$eval(showAllSelector, item => {
       (<HTMLInputElement>item[0]).checked = false;
       (<HTMLInputElement>item[0]).click();
     });
-    await page.waitFor(300);
+    await page.waitFor(200);
     const optionsSelector = '#rw_1_listbox li';
     const headerColumnSelector = '.tableview-wrapper th';
     const optionsCount = await page.$$eval(optionsSelector, options => options.length);
     const columnsCount = await page.$$eval(headerColumnSelector, columns => columns.length);
     expect(optionsCount).toEqual(columnsCount);
+    done();
   });
 
-  it('Should open the selected entity in the side panel', async () => {
+  it('Should open the selected entity in the side panel', async done => {
     const rowCheckboxSelector = ".tableview-wrapper .sticky-col input[type='checkbox']";
     const sidePanelItemNameSelector = '.sidepanel-body .item-name';
     const entityTitle = await page.$$eval(rowCheckboxSelector, columns => {
@@ -73,13 +76,16 @@ describe('Table view', () => {
     });
     await page.waitFor(sidePanelItemNameSelector);
     await expect(page).toMatchElement(sidePanelItemNameSelector, { text: entityTitle?.toString() });
+    done();
   });
 
-  it('Should load more rows if scrolling reach the end of content', async () => {
+  it('Should load more rows if scrolling reach the end of content', async done => {
     const rowSelector = '.tableview-wrapper > table > tbody > tr';
     const lastRowSelector = '.tableview-wrapper > table > tbody > tr:last-child';
     await page.$$eval(lastRowSelector, el => el[0].scrollIntoView());
+    await page.waitFor(200);
     const rowsNumber = await page.$$eval(rowSelector, rows => rows.length);
     expect(rowsNumber).toBe(60);
+    done();
   });
 });
