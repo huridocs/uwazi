e4c14053a62d480582c905cac9ed872322f6d906
Mercy Falconí <mercy@grupocentrico.com>
2020-08-31 19:02:52 -0500

    waiting for rendered columns

--- e2e/suites/tableView.test.ts
+++ e2e/suites/tableView.test.ts
@@ -23,6 +23,7 @@ describe('Table view', () => {
 
     it('Should show only selected properties', async done => {
       await page.click('.hidden-columns-dropdown');
+      await page.waitFor('#rw_1_listbox > li:nth-child(10)');
       const columnsOptions = await page.$$eval('#rw_1_listbox li', options =>
         options.map(option => ({
           checked: (<HTMLInputElement>option.children[0]).checked,
@@ -40,7 +41,7 @@ describe('Table view', () => {
       );
       expect(selectedColumns).toEqual(visibleColumns);
       done();
-    });
+    }, 50000);
 
     it('Should show new selected properties', async done => {
       const newColumn = await page.$$eval('#rw_1_listbox li:nth-child(9)', option => {
