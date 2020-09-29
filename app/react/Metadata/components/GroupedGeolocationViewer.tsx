ee3c5cb6c32dc03fe4e8dc4e2da50959b51fe734
Federico Nocetti <fnocetti@users.noreply.github.com>
2020-09-29 18:22:22 -0300

    Add pills

--- app/react/Metadata/components/GroupedGeolocationViewer.tsx
+++ app/react/Metadata/components/GroupedGeolocationViewer.tsx
@@ -7,22 +7,56 @@ interface GeolocationMarker {
   label: string;
 }
 
-interface GroupedGeolocationViewerProps {
-  members: {
-    label: string;
-    translationContext: string;
-    value: GeolocationMarker[];
-  }[];
+interface GroupMember {
+  label: string;
+  translationContext: string;
+  value: GeolocationMarker[];
 }
+
+interface GroupedGeolocationViewerProps {
+  members: GroupMember[];
+}
+
+const notInherited = (member: GroupMember) => member.value.length === 1 && !member.value[0].label;
+
+const renderGroupInfo = (member: GroupMember) => {
+  return notInherited(member) ? (
+    <dl>
+      <dt></dt>
+      <dd>
+          <span className="pill">{member.label}</span>
+      </dd>
+    </dl>
+  ) : (
+    <dl>
+      <dt><span>{`${member.label}`}</span></dt>
+      <dd>
+        {member.value.map(value => (
+          <span className="pill">{value.label}</span>
+        ))}
+      </dd>
+    </dl>
+  );
+};
 
 export const GroupedGeolocationViewer = (props: GroupedGeolocationViewerProps) => {
   const markers = props.members.reduce(
-    (flat: GeolocationMarker[], member) => flat.concat(member.value),
+    (flat: GeolocationMarker[], member) => {
+      if (notInherited(member)) {
+        return flat.concat([{
+          ...member.value[0],
+          label: member.label
+        }]);
+      }
+
+      return flat.concat(member.value)
+    },
     []
   );
   return (
     <>
       <GeolocationViewer points={markers} onlyForCards={false} />
+      {props.members.map(renderGroupInfo)}
     </>
   );
 };
