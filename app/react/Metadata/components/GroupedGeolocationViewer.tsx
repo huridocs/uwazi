8f4cc64bdc73d07a9e8b50acbf404b881f009369
Federico Nocetti <fnocetti@users.noreply.github.com>
2020-10-05 15:44:54 -0300

    Add translations to geolocation group

--- app/react/Metadata/components/GroupedGeolocationViewer.tsx
+++ app/react/Metadata/components/GroupedGeolocationViewer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GeolocationViewer from 'app/Metadata/components/GeolocationViewer';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
+import { Translate } from 'app/I18N';
 import { IStore } from '../../istore';
 import { ensure } from '../../../shared/tsUtils';
 import { Pill } from './Pill';
@@ -20,38 +21,55 @@ interface GroupMember {
   value: GeolocationMarker[];
 }
 
+interface TemplatesInfo {
+  [template: string]: {
+    name: string;
+    color: string;
+  };
+}
+
 export interface GroupedGeolocationViewerProps {
   members: GroupMember[];
-  colors: {
-    [template: string]: string;
-  };
+  templatesInfo: TemplatesInfo;
 }
 
 const notLabeledOrMultiple = (member: GroupMember) =>
   member.value.length === 1 && !member.value[0].label;
 
-const renderMemberInfo = (colors: { [template: string]: string }) => (member: GroupMember) =>
+const renderMemberInfo = (templatesInfo: TemplatesInfo) => (member: GroupMember) =>
   notLabeledOrMultiple(member) ? (
     <dl key={`${member.translateContext}_${member.name}`}>
       <dt />
       <dd>
         <Pill
           key={`${member.value[0].lat}_${member.value[0].lon}`}
-          color={colors[member.translateContext]}
+          color={templatesInfo[member.translateContext].color}
         >
-          {member.label}
+          <Translate context={member.translateContext}>{member.label}</Translate>
         </Pill>
       </dd>
     </dl>
   ) : (
     <dl key={`${member.translateContext}_${member.name}`}>
       <dt>
-        <span>{`${member.label}`}</span>
+        <span>
+          <Translate context={member.translateContext}>{member.label}</Translate>
+          <> (</>
+          <Translate>linked</Translate>
+          <> </>
+          <Translate context={member.translateContext}>
+            {templatesInfo[member.translateContext].name}
+          </Translate>
+          <>)</>
+        </span>
       </dt>
       <dd>
         {member.value.map(value => (
-          <Pill key={`${value.lat}_${value.lon}`} color={colors[member.translateContext]}>
-            {value.label}
+          <Pill
+            key={`${value.lat}_${value.lon}`}
+            color={templatesInfo[member.translateContext].color}
+          >
+            <Translate context={member.translateContext}>{value.label || ''}</Translate>
           </Pill>
         ))}
       </dd>
@@ -65,38 +83,41 @@ const GroupedGeolocationViewerComponent = (props: GroupedGeolocationViewerProps)
         {
           ...member.value[0],
           label: member.label,
-          color: props.colors[member.translateContext],
+          color: props.templatesInfo[member.translateContext].color,
         },
       ]);
     }
 
     return flat.concat(
-      member.value.map(v => ({ ...v, color: props.colors[member.translateContext] }))
+      member.value.map(v => ({ ...v, color: props.templatesInfo[member.translateContext].color }))
     );
   }, []);
 
   return (
     <>
       <GeolocationViewer points={markers} onlyForCards={false} />
-      {props.members.map(renderMemberInfo(props.colors))}
+      {props.members.map(renderMemberInfo(props.templatesInfo))}
     </>
   );
 };
 
-const colorsMap = createSelector(
+const templatesMap = createSelector(
   (s: IStore) => s.templates,
   templates =>
     templates.reduce(
       (map, template) => ({
         ...map,
-        [ensure<string>(template?.get('_id'))]: template?.get('color'),
+        [ensure<string>(template?.get('_id'))]: {
+          name: template?.get('name'),
+          color: template?.get('color'),
+        },
       }),
       {}
     )
 );
 
 const mapStateToProps = (state: IStore) => ({
-  colors: colorsMap(state),
+  templatesInfo: templatesMap(state),
 });
 
 export const GroupedGeolocationViewer = connect(mapStateToProps)(GroupedGeolocationViewerComponent);
