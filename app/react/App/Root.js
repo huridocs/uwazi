5f740927e8a82722157131aea8fc65000a331e4c
Eric Wamugu <wamugu@gmail.com>
2020-10-06 15:10:26 +0200

    use the DropDown component from App/Form

--- app/react/App/Root.js
+++ app/react/App/Root.js
@@ -49,8 +49,6 @@ const headTag = (head, CSS, reduxData) => (
     />
     {googelFonts}
     <link rel="shortcut icon" href="/public/favicon.ico" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
 );
 
