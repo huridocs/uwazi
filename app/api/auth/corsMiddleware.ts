04bab7419efa21580720218a82b1eedac9c646ef
Alberto Casado Torres <acasadotorres@gmail.com>
2020-11-02 09:10:12 +0100

    deleted console.log

--- app/api/auth/corsMiddleware.ts
+++ app/api/auth/corsMiddleware.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default () => (_req: Request, res: Response, next: NextFunction) => {
-  console.log('here');
-
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
