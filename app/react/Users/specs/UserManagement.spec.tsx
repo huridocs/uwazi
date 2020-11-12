7f96162bc7c28dceb702662a76821fb10077034e
Mercy Falconí <mercy@grupocentrico.com>
2020-11-12 11:56:22 -0500

    updated broken tests

--- app/react/Users/specs/UserManagement.spec.tsx
+++ app/react/Users/specs/UserManagement.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { UserManagement } from 'app/Users/UserManagement';
+import { Tabs } from 'react-tabs-redux';
 
 describe('UserGroupList', () => {
   function render() {
@@ -8,12 +9,14 @@ describe('UserGroupList', () => {
   }
   it('should render users as default tab', () => {
     const component = render();
-    expect(component.props().selectedTab).toBe('users');
+    const tabs = component.find(Tabs).at(0);
+    expect(tabs.props().selectedTab).toBe('users');
   });
   it('should handle select updating the selected tab state', () => {
     const component = render();
-    component.props().handleSelect('usergroups');
-    component.update();
-    expect(component.props().selectedTab).toBe('usergroups');
+    const tabs = component.find(Tabs).at(0);
+    tabs.props().handleSelect?.('usergroups', '');
+    const updatedTabs = component.find(Tabs).at(0);
+    expect(updatedTabs.props().selectedTab).toBe('usergroups');
   });
 });
