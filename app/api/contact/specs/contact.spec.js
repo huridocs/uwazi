54920b94937878b11982e06be7a5e54513f5159f
Kevin Nderitu <nderitukelvin19@gmail.com>
2020-11-16 13:33:13 +0300

    Converted tests to async/await and updated settings.save

--- app/api/contact/specs/contact.spec.js
+++ app/api/contact/specs/contact.spec.js
@@ -2,7 +2,6 @@
 import { catchErrors } from 'api/utils/jasmineHelpers';
 import mailer from 'api/utils/mailer';
 import db from 'api/utils/testing_db';
-import { settingsModel } from 'api/settings/settingsModel';
 import fixtures from './fixtures.js';
 import contact from '../contact';
 
@@ -19,48 +18,44 @@ describe('contact', () => {
   });
 
   describe('sendMessage', () => {
-    it('should send an email with the mailer to the configured email', async done => {
-      contact
-        .sendMessage({
+    it('should send an email with the mailer to the configured email', async () => {
+      try {
+        await contact.sendMessage({
           email: 'bruce@wayne.com',
           name: 'Bruce Wayne',
           message: 'I want to contact you.',
-        })
-        .then(() => {
-          expect(mailer.send).toHaveBeenCalledWith({
-            from: '"Uwazi" <no-reply@uwazi.io>',
-            subject: 'Contact mesage from Bruce Wayne bruce@wayne.com',
-            text: 'I want to contact you.',
-            to: 'contact@uwazi.com',
-          });
-          done();
-        })
-        .catch(catchErrors(done));
+        });
+        expect(mailer.send).toHaveBeenCalledWith({
+          from: '"Uwazi" <no-reply@uwazi.io>',
+          subject: 'Contact mesage from Bruce Wayne bruce@wayne.com',
+          text: 'I want to contact you.',
+          to: 'contact@uwazi.com',
+        });
+      } catch (e) {
+        catchErrors();
+      }
     });
-    it('should send email with the provided sender email and site name', async done => {
+    it('should send email with the provided sender email and site name', async () => {
       const settings = {
         site_name: 'some site name',
         senderEmail: 'sender@email.com',
       };
-      const set = await settingsModel.get();
-      const updated = Object.assign(set[0], { ...settings });
-      await settingsModel.save(updated);
-      contact
-        .sendMessage({
+      try {
+        await settings.save(settings);
+        await contact.sendMessage({
           email: 'bruce@wayne.com',
           name: 'Bruce Wayne',
           message: 'I want to contact you.',
-        })
-        .then(() => {
-          expect(mailer.send).toHaveBeenCalledWith({
-            from: `"${settings.site_name}" <${settings.senderEmail}>`,
-            subject: 'Contact mesage from Bruce Wayne bruce@wayne.com',
-            text: 'I want to contact you.',
-            to: 'contact@uwazi.com',
-          });
-          done();
-        })
-        .catch(catchErrors(done));
+        });
+        expect(mailer.send).toHaveBeenCalledWith({
+          from: `"${settings.site_name}" <${settings.senderEmail}>`,
+          subject: 'Contact mesage from Bruce Wayne bruce@wayne.com',
+          text: 'I want to contact you.',
+          to: 'contact@uwazi.com',
+        });
+      } catch (e) {
+        catchErrors();
+      }
     });
   });
 });
