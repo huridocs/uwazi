46fac1534a19c550759873cda1941ac42d400ff3
Mercy Falconí <mercy@grupocentrico.com>
2020-11-20 20:36:46 -0500

    Fixed code climate issues

--- app/react/Users/components/usergroups/specs/UserGroupsAPI.spec.ts
+++ app/react/Users/components/usergroups/specs/UserGroupsAPI.spec.ts
@@ -1,42 +1,40 @@
 import api from 'app/utils/api';
 import { RequestParams } from 'app/utils/RequestParams';
-import { UserGroupSchema } from 'shared/types/userGroupType';
-import { ObjectIdSchema } from 'shared/types/commonTypes';
 import userGroupsAPI from '../UserGroupsAPI';
 
 jest.mock('app/utils/api', () => ({
-  get: jest
-    .fn()
-    .mockImplementation((_req: RequestParams<UserGroupSchema>) => ({ json: { code: 200 } })),
-  post: jest
-    .fn()
-    .mockImplementation((_req: RequestParams<UserGroupSchema>) => ({ json: { code: 200 } })),
-  delete: jest.fn().mockImplementation((_req: RequestParams<{ _id: ObjectIdSchema }>) => ({
-    json: { code: 200 },
-  })),
+  get: jest.fn().mockResolvedValue({ json: { code: 200 } }),
+  post: jest.fn().mockResolvedValue({ json: { code: 200 } }),
+  delete: jest.fn().mockResolvedValue({ json: { code: 200 } }),
 }));
 
 describe('UserGroupsAPI', () => {
-  beforeEach(() => {});
-
   describe('getUserGroups', () => {
     it('should call get method of usergroups api', async () => {
-      const request = new RequestParams<UserGroupSchema>({ name: 'Group1', members: [] });
-      const response = await userGroupsAPI.getUserGroups(request);
-      expect(api.get).toHaveBeenCalledWith('usergroups', {
-        data: { name: 'Group1', members: [] },
-        headers: {},
-      });
+      const response = await userGroupsAPI.getUserGroups(new RequestParams());
+      expect(api.get).toHaveBeenCalledWith('usergroups', { data: undefined, headers: {} });
       expect(response.code).toBe(200);
     });
   });
 
   describe('saveUserGroup', () => {
     it('should call post method of usergroups api', async () => {
-      const request = new RequestParams<UserGroupSchema>({ name: 'Group1', members: [] });
-      const response = await userGroupsAPI.saveUserGroup(request);
+      const response = await userGroupsAPI.saveUserGroup(
+        new RequestParams({ name: 'Group 1', members: [] })
+      );
       expect(api.post).toHaveBeenCalledWith('usergroups', {
-        data: { name: 'Group1', members: [] },
+        data: { name: 'Group 1', members: [] },
+        headers: {},
+      });
+      expect(response.code).toBe(200);
+    });
+  });
+
+  describe('deleteUserGroup', () => {
+    it('should call delete method of usergroups api', async () => {
+      const response = await userGroupsAPI.deleteUserGroup(new RequestParams({ _id: 'group1' }));
+      expect(api.delete).toHaveBeenCalledWith('usergroups', {
+        data: { _id: 'group1' },
         headers: {},
       });
       expect(response.code).toBe(200);
