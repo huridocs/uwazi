6a3bf161c332a7c8ea16a52344fdfc7616423979
Daneryl <daneryl@gmail.com>
2021-01-10 15:14:26 -0500

    change db options to use mongoose interface

--- app/api/migrations/migrate.ts
+++ app/api/migrations/migrate.ts
@@ -1,3 +1,4 @@
+import { ConnectionOptions } from 'mongoose';
 import { DB } from 'api/odm';
 import { tenants } from 'api/tenants/tenantContext';
 import { config } from 'api/config';
@@ -8,14 +9,10 @@ process.on('unhandledRejection', error => {
   throw error;
 });
 
-let auth = {
-  user: undefined,
-  pass: undefined
-};
+let auth: ConnectionOptions;
 
 if (process.env.DBUSER) {
   auth = {
-    auth: { authSource: 'admin' },
     user: process.env.DBUSER,
     pass: process.env.DBPASS,
   };
