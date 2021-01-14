dca1353bf517a7b187d50e754ee2616d0589330d
jaumec@gmail.com <jaumec@gmail.com>
2021-01-13 19:18:21 -0600

    Trying to disable transiations for copy-from e2e

---
+++ e2e/helpers/disableTransitions.ts
@@ -0,0 +1,15 @@
+/*global page*/
+
+export default async () => {
+  await page.addStyleTag({ content : `
+      *,
+      *::after,
+      *::before {
+          transition-delay: 0s !important;
+          transition-duration: 0s !important;
+          animation-delay: -0.0001s !important;
+          animation-duration: 0s !important;
+          animation-play-state: paused !important;
+          caret-color: transparent !important;
+      }`});
+};
