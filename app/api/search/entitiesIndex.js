08b0914a25b3d43a342a5863dc2d28d717854ed3
Alberto Casado Torres <acasadotorres@gmail.com>
2021-01-29 17:16:46 +0100

    deleted redundant condition

--- app/api/search/entitiesIndex.js
+++ app/api/search/entitiesIndex.js
@@ -182,16 +182,16 @@ const equalPropMapping = (mapA, mapB) => {
     return true;
   }
 
-  const sameAmountOfProps =
-    Object.keys(mapA.properties).length === Object.keys(mapB.properties).length;
-  const sameProps = Object.keys(mapA.properties).reduce(
-    (result, propKey) =>
-      result &&
-      mapB.properties[propKey] &&
-      mapA.properties[propKey].type === mapB.properties[propKey].type,
-    true
-  );
-  return sameAmountOfProps && sameProps;
+  const sameProps =
+    Object.keys(mapA.properties).length === Object.keys(mapB.properties).length &&
+    Object.keys(mapA.properties).reduce(
+      (result, propKey) =>
+        result &&
+        mapB.properties[propKey] &&
+        mapA.properties[propKey].type === mapB.properties[propKey].type,
+      true
+    );
+  return sameProps;
 };
 
 const checkMapping = async (template, elasticIndex) => {
