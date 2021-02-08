b8927ce6dba51de55fdb3f2da4a2159286511e5f
vkozinec142 <vkozinec3@gmail.com>
2021-02-08 09:28:31 +0100

    Added tabs and style cleanup

--- app/react/Attachments/components/AttachmentsModal.tsx
+++ app/react/Attachments/components/AttachmentsModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import ReactModal from 'react-modal';
 import { Tabs, TabLink, TabContent } from 'react-tabs-redux';
 
@@ -12,31 +12,87 @@ interface AttachmentsModalProps {
 
 // eslint-disable-next-line react/prop-types
 const AttachmentsModal: React.FC<AttachmentsModalProps> = ({ isOpen, onClose }) => {
-  const modalStyle = { overlay: { zIndex: 100, backgroundColor: 'rgba(0, 0, 0, 0.5)' } };
-
   return (
     <NeedAuthorization roles={['admin', 'editor']}>
       <ReactModal isOpen={isOpen} style={modalStyle}>
         <div className="attachments-modal-header">
           <h4>Supporting files</h4>
 
-          <button type="button" onClick={onClose}>
+          <button type="button" onClick={onClose} className="attachments-modal-close">
             <Icon icon="times" />
-            Close Modal
+            <span>Cancel</span>
           </button>
         </div>
-        <div className="modal-content">
-          <Tabs>
-            <TabLink to="tab1">Tab1</TabLink>
-            <TabLink to="tab2">Tab2</TabLink>
+        <div className="attachments-modal-content">
+          <Tabs renderActiveTabContentOnly>
+            <div className="attachments-modal-tabs">
+              <TabLink to="uploadComputer">Upload from computer</TabLink>
+              <TabLink to="uploadWeb">Add from web</TabLink>
+            </div>
 
-            <TabContent for="tab1">Tab 1</TabContent>
-            <TabContent for="tab2">Tab 2</TabContent>
+            <div className="attachments-modal-tabs-content">
+              <TabContent for="uploadComputer" className="tab-content centered">
+                <button
+                  type="button"
+                  onClick={() => {}}
+                  className="btn btn-success attachments-modal-file-triggger"
+                >
+                  <Icon icon="link" />
+                  &nbsp; Upload and select file
+                </button>
+
+                <span>Drag and drop file in this window to upload </span>
+              </TabContent>
+              <TabContent for="uploadWeb" className="tab-content centered">
+                <div className="wrapper-web">
+                  <div className="form-group has-feedback">
+                    <input type="text" className="form-control" placeholder="Paste URL here" />
+                    <Icon icon="info-circle" class="feedback-icon" />
+                  </div>
+
+                  <input
+                    type="text"
+                    onChange={() => {}}
+                    value=""
+                    className="form-control"
+                    placeholder="Title"
+                  />
+
+                  <button
+                    type="button"
+                    onClick={() => {}}
+                    className="btn btn-success attachments-modal-file-triggger"
+                  >
+                    <Icon icon="link" />
+                    &nbsp; Add resource
+                  </button>
+                </div>
+              </TabContent>
+            </div>
           </Tabs>
         </div>
       </ReactModal>
     </NeedAuthorization>
   );
+};
+
+const modalStyle = {
+  overlay: {
+    zIndex: 100,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column' as CSSProperties['flexDirection'],
+    width: '80vw',
+    height: '70vh',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
 };
 
 export default AttachmentsModal;
