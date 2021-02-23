c9b28432890f7930aed2d16d06a84d48b50c3a83
Ivan Jakoplic <ijakoplic3@gmail.com>
2021-02-23 14:55:24 +0100

    add attachment modal snapshots

--- app/react/Attachments/components/AttachmentsModal.tsx
+++ app/react/Attachments/components/AttachmentsModal.tsx
@@ -8,7 +8,6 @@ import Dropzone from 'react-dropzone';
 import { LocalForm, Field } from 'react-redux-form';
 
 import { Translate } from 'app/I18N';
-import { NeedAuthorization } from 'app/Auth';
 import { Icon } from 'app/UI';
 
 import { uploadAttachment, uploadAttachmentFromUrl } from '../actions/actions';
@@ -65,104 +64,110 @@ const AttachmentsModal: React.FC<AttachmentsModalProps> = ({
   };
 
   return (
-    <NeedAuthorization roles={['admin', 'editor']}>
-      <ReactModal
-        isOpen={isOpen}
-        className="attachments-modal"
-        overlayClassName="attachments-modal__overlay"
-        ariaHideApp={false}
-      >
-        <div className="attachments-modal__header">
-          <h4>
-            <Translate>Supporting files</Translate>
-          </h4>
+    <ReactModal
+      isOpen={isOpen}
+      className="attachments-modal"
+      overlayClassName="attachments-modal__overlay"
+      ariaHideApp={false}
+    >
+      <div className="attachments-modal__header">
+        <h4>
+          <Translate>Supporting files</Translate>
+        </h4>
 
-          <button
-            type="button"
-            onClick={onClose}
-            className="attachments-modal__close"
-            disabled={getPercentage !== undefined}
-          >
-            <Icon icon="times" />
-            <span>Close</span>
-          </button>
-        </div>
-        <div className="attachments-modal__content">
-          <Tabs renderActiveTabContentOnly>
-            <div className="attachments-modal__tabs">
-              <TabLink to="uploadComputer">
-                <Translate>Upload from computer</Translate>
-              </TabLink>
-              <TabLink to="uploadWeb">
-                <Translate>Add from web</Translate>
-              </TabLink>
-            </div>
+        <button
+          type="button"
+          onClick={onClose}
+          className="attachments-modal__close"
+          disabled={getPercentage !== undefined}
+        >
+          <Icon icon="times" />
+          <span>Close</span>
+        </button>
+      </div>
+      <div className="attachments-modal__content">
+        <Tabs renderActiveTabContentOnly>
+          <div className="attachments-modal__tabs">
+            <TabLink to="uploadComputer" className="modal-tab-1">
+              <Translate>Upload from computer</Translate>
+            </TabLink>
+            <TabLink to="uploadWeb" className="modal-tab-2">
+              <Translate>Add from web</Translate>
+            </TabLink>
+          </div>
 
-            <div className="attachments-modal__tabs-content">
-              <TabContent for="uploadComputer" className="tab-content centered">
-                <Dropzone
-                  disableClick
-                  onDrop={handleDropFiles}
-                  className="attachments-modal__dropzone"
-                  multiple={false}
-                >
-                  {getPercentage === undefined ? (
-                    <>
-                      <button
-                        type="button"
-                        onClick={handleUploadButtonClicked}
-                        className="btn btn-success"
-                      >
-                        <Icon icon="link" />
-                        &nbsp; <Translate>Upload and select file</Translate>
-                      </button>
-                      <input
-                        type="file"
-                        onChange={handleInputFileChange}
-                        style={{ display: 'none' }}
-                        ref={inputFileRef}
-                      />
-                      <h4 className="attachments-modal__dropzone-title">
-                        <Translate>Drag and drop file in this window to upload </Translate>
-                      </h4>
-                    </>
-                  ) : (
-                    <div className="progress attachments-modal-progress">
-                      <div
-                        className="progress-bar progress-bar-success attachments-modal-progress-bar"
-                        role="progressbar"
-                        style={{ width: `${getPercentage}%` }}
-                      />
-                    </div>
-                  )}
-                </Dropzone>
-              </TabContent>
-              <TabContent for="uploadWeb" className="tab-content centered">
-                <div className="wrapper-web">
-                  <LocalForm onSubmit={handleSubmitUrlForm} model="urlForm" validators={validators}>
-                    <div className="form-group has-feedback">
-                      <Field model=".url">
-                        <input type="text" className="form-control" placeholder="Paste URL here" />
-                      </Field>
-                      <Icon icon="info-circle" className="feedback-icon" />
-                    </div>
-
-                    <Field model=".name">
-                      <input type="text" className="form-control" placeholder="Title" />
-                    </Field>
-
-                    <button type="submit" className="btn btn-success">
+          <div className="attachments-modal__tabs-content">
+            <TabContent for="uploadComputer" className="tab-content centered">
+              <Dropzone
+                disableClick
+                onDrop={handleDropFiles}
+                className="attachments-modal__dropzone"
+                multiple={false}
+              >
+                {getPercentage === undefined ? (
+                  <>
+                    <button
+                      type="button"
+                      onClick={handleUploadButtonClicked}
+                      className="btn btn-success"
+                    >
                       <Icon icon="link" />
-                      &nbsp; <Translate>Add resource</Translate>
+                      &nbsp; <Translate>Upload and select file</Translate>
                     </button>
-                  </LocalForm>
-                </div>
-              </TabContent>
-            </div>
-          </Tabs>
-        </div>
-      </ReactModal>
-    </NeedAuthorization>
+                    <input
+                      type="file"
+                      onChange={handleInputFileChange}
+                      style={{ display: 'none' }}
+                      ref={inputFileRef}
+                    />
+                    <h4 className="attachments-modal__dropzone-title">
+                      <Translate>Drag and drop file in this window to upload </Translate>
+                    </h4>
+                  </>
+                ) : (
+                  <div className="progress attachments-modal-progress">
+                    <div
+                      className="progress-bar progress-bar-success attachments-modal-progress-bar"
+                      role="progressbar"
+                      style={{ width: `${getPercentage}%` }}
+                    />
+                  </div>
+                )}
+              </Dropzone>
+            </TabContent>
+            <TabContent for="uploadWeb" className="tab-content centered">
+              <div className="wrapper-web">
+                <LocalForm onSubmit={handleSubmitUrlForm} model="urlForm" validators={validators}>
+                  <div className="form-group has-feedback">
+                    <Field model=".url">
+                      <input
+                        type="text"
+                        className="form-control web-attachment-url"
+                        placeholder="Paste URL here"
+                      />
+                    </Field>
+                    <Icon icon="info-circle" className="feedback-icon" />
+                  </div>
+
+                  <Field model=".name">
+                    <input
+                      type="text"
+                      className="form-control web-attachment-name"
+                      placeholder="Title"
+                    />
+                  </Field>
+
+                  <button type="submit" className="btn btn-success">
+                    <Icon icon="link" />
+                    &nbsp; <Translate>Add resource</Translate>
+                  </button>
+                </LocalForm>
+              </div>
+            </TabContent>
+          </div>
+        </Tabs>
+      </div>
+    </ReactModal>
   );
 };
 
--- app/react/Attachments/components/specs/AttachmentsModal.spec.js
+++ app/react/Attachments/components/specs/AttachmentsModal.spec.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import thunk from 'redux-thunk';
 import { shallow } from 'enzyme';
-
+import configureMockStore from 'redux-mock-store';
 import ReactModal from 'react-modal';
+
 import AttachmentsModal from '../AttachmentsModal';
+
+const mockStore = configureMockStore([thunk]);
+const store = mockStore({});
 
 describe('Attachments Modal', () => {
   let component;
 
   const render = (props = {}) => {
-    component = shallow(
-      <AttachmentsModal {...props}>
-        <div />
-      </AttachmentsModal>
-    );
+    component = shallow(<AttachmentsModal {...props} store={store} />).dive();
   };
 
   it('Should pass isOpen props to attachments modal.', () => {
@@ -21,4 +22,20 @@ describe('Attachments Modal', () => {
     render({ isOpen: true });
     expect(component.find(ReactModal).props().isOpen).toBe(true);
   });
+
+  it('Should match render of upload form', () => {
+    render();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Should match render of web form', () => {
+    render();
+
+    component.find('.modal-tab-2').simulate('click');
+
+    expect(component).toMatchSnapshot();
+  });
+
+  
 });
