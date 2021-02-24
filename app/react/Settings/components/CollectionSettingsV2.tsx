4e8e4ca327494c184f3d6e78c044d6dff0d4aa9d
RafaPolit <rafaelpolit@gmail.com>
2021-02-24 11:57:52 -0500

    Proof of concept

--- app/react/Settings/components/CollectionSettingsV2.tsx
+++ app/react/Settings/components/CollectionSettingsV2.tsx
@@ -1,3 +1,14 @@
 import React from 'react';
 
-export const CollectionSettings = () => <span>a</span>;
+export const CollectionSettings = () => (
+  <SettingProperty />
+    <Label>
+      <span>Use custom landing page</span>
+      <Tooltip>flasdkjf ldkjads;lfkjdsf;lakdsj f;adlsfkj</Tooltip>
+    </Label>
+
+    <ToggleChildren toggled={true}>
+      <input model={}></input>
+    </ToggleChildren>
+  </SettingProperty>
+);
