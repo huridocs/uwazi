8fc84fcb44dbbfe3c21ca301a8ed2ac9fddbcf4a
Santiago <santiago@huridocs.org>
2021-02-26 11:12:02 -0300

    Additional layout of form elements and tips

--- app/react/Settings/components/CollectionSettingsV2.tsx
+++ app/react/Settings/components/CollectionSettingsV2.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tip } from 'app/Layout';
+import { Translate } from 'app/I18N';
 import { ToggleChildren } from './ToggleChildren';
 import { SettingsLabel } from './SettingsLabel';
 import * as CollectionSettingsTips from './collectionSettingsTips';
@@ -16,23 +17,114 @@ import * as CollectionSettingsTips from './collectionSettingsTips';
 // </SettingProperty>
 
 export const CollectionSettings = () => (
-  <div className="collection-settings">
-    <h1>Collection</h1>
-    <h2>General</h2>
+  <div className="panel panel-default collection-settings">
+    <div className="panel-heading">
+      <Translate>Collection</Translate>
+    </div>
+    <h2>
+      <Translate>General</Translate>
+    </h2>
 
     <div className="form-element">
       <SettingsLabel>
-        <span>Collection name</span>
+        <Translate>Collection name</Translate>
       </SettingsLabel>
       <input type="text" />
     </div>
 
     <div className="form-element">
       <SettingsLabel>
-        <span>Use custom landing page</span>
+        <Translate>Custom favicon</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.customFavIcon}</Tip>
+      </SettingsLabel>
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Use custom landing page</Translate>
         <Tip icon="info-circle">{CollectionSettingsTips.landingPageTip}</Tip>
       </SettingsLabel>
       <ToggleChildren toggled={false}>
+        <input type="text" />
+      </ToggleChildren>
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Default view</Translate>
+      </SettingsLabel>
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <span>Date format</span>
+      </SettingsLabel>
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Allow public sharing</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.publicSharing}</Tip>
+      </SettingsLabel>
+      <ToggleChildren toggled={false} />
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Show cookie policy</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.cookiePolicy}</Tip>
+      </SettingsLabel>
+      <ToggleChildren toggled={false} />
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Non-latin characters support</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.characterSupport}</Tip>
+      </SettingsLabel>
+      <ToggleChildren toggled={false} />
+    </div>
+
+    <h2>
+      <Translate>Website analytics</Translate>
+    </h2>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Google Analytics</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.analytics}</Tip>
+      </SettingsLabel>
+      <ToggleChildren toggled={false}>
+        <input type="text" />
+      </ToggleChildren>
+    </div>
+
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Motomo Analytics</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.analytics}</Tip>
+      </SettingsLabel>
+      <ToggleChildren toggled={false}>
+        <input type="text" />
+      </ToggleChildren>
+    </div>
+
+    <h2>
+      <Translate>Form and email configuration</Translate>
+    </h2>
+
+    <h2>
+      <Translate>Maps</Translate>
+    </h2>
+    <div className="form-element">
+      <SettingsLabel>
+        <Translate>Set starting location</Translate>
+        <Tip icon="info-circle">{CollectionSettingsTips.mapAxis}</Tip>
+      </SettingsLabel>
+      <ToggleChildren toggled={false}>
+        <Translate>Longitude</Translate>
+        <input type="text" />
+        <Translate>Latitude</Translate>
         <input type="text" />
       </ToggleChildren>
     </div>
--- app/react/Settings/components/collectionSettingsTips.tsx
+++ app/react/Settings/components/collectionSettingsTips.tsx
@@ -20,3 +20,47 @@ export const landingPageTip = (
     https://yoursite.com/
   </>
 );
+
+export const customFavIcon = (
+  <>
+    Favicon is an icon that appears in the browser tab and bookmarks.
+    <br />
+    You will need to reload the page after updating your Favicon.
+  </>
+);
+
+export const publicSharing = (
+  <>
+    Allow administrators and editors to share entities publicly. Visitors to your Uwazi will be able
+    to see this entities without logging in.
+  </>
+);
+
+export const cookiePolicy = (
+  <>This option will show a notification about the use of cookies in your instance.</>
+);
+
+export const characterSupport = (
+  <>
+    Toggling this button enhances support for non-latin languages as default languages. This will
+    update all template properties automatically. <br />
+    <b>
+      This process could take several minutes and will likely change URLs to library filters. If you
+      have menus or links using such URLs, they will probably stop working after the update. You
+      will need to update them manually.
+    </b>
+    <br />
+    After selecting this option, you will not be able to revert back to using legacy property
+    naming. If you are not facing issues with your template property names, we recommend leaving
+    this unchecked.
+  </>
+);
+
+export const analytics = (
+  <>
+    If you want to track analytics related to your collection visits, Uwazi supports both Google
+    Analytics and Matomo.
+  </>
+);
+
+export const mapAxis = <>Set the default starting point for your geolocation properties.</>;
