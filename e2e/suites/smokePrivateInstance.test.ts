32e5e25767c80113a070269243220a7511fa0e65
RafaPolit <rafaelpolit@gmail.com>
2021-03-08 11:43:11 -0500

    Combined tests

--- e2e/suites/smokePrivateInstance.test.ts
+++ e2e/suites/smokePrivateInstance.test.ts
@@ -4,6 +4,12 @@ import proxyMock from '../helpers/proxyMock';
 import insertFixtures from '../helpers/insertFixtures';
 import disableTransitions from '../helpers/disableTransitions';
 
+const selectors = {
+  collection: {
+    togglePrivate: 'div.form-element:nth-child(8) > div:nth-child(2) > label:nth-child(1)',
+  },
+};
+
 describe('Private instance', () => {
   beforeAll(async () => {
     await insertFixtures();
@@ -11,24 +17,18 @@ describe('Private instance', () => {
     await disableTransitions();
   });
 
-  it('should log in as admin, go to account settings, then collection settings', async () => {
+  it('should log in as admin, and toggle private instance in collection settings', async () => {
     await adminLogin();
     await expect(page).toMatchElement('span', { text: 'Account' });
     await expect(page).toClick('a', { text: 'Account settings' });
     await expect(page).toClick('a', { text: 'Collection' });
-  });
-
-  it('should toggle the private instance button and save', async () => {
-    await expect(page).toClick(
-      //CSS selector for Private Instance toggle button
-      'div.form-element:nth-child(8) > div:nth-child(2) > label:nth-child(1)'
-    );
+    await expect(page).toClick(selectors.collection.togglePrivate);
     await expect(page).toClick('button', { text: 'Save' });
   });
 
   it('shoud log out and be redirected to login page instead of library page', async () => {
     await logout();
     await page.goto(`${host}/library`);
-    await expect(page).toMatch('Login');
+    await expect(page.url()).toBe(`${host}/login`);
   });
 });
