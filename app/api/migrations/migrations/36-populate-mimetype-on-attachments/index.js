a470aa32566545ccd62efb4facd44c4b48f307bd
Kevin Nderitu <kevin@huridocs.org>
2021-03-17 16:55:43 +0300

    Added tests to the migration

--- app/api/migrations/migrations/36-populate-mimetype-on-attachments/index.js
+++ app/api/migrations/migrations/36-populate-mimetype-on-attachments/index.js
@@ -9,11 +9,12 @@ export default {
 
   async up(db) {
     process.stdout.write(`${this.name}...\r\n`);
-    const cursor = await db.collection('files').find();
+    const cursor = await db.collection('files').find({});
 
     // eslint-disable-next-line no-await-in-loop
     while (await cursor.hasNext()) {
-      const file = cursor.next();
+      // eslint-disable-next-line no-await-in-loop
+      const file = await cursor.next();
       if (file.url && !file.mimetype) {
         // eslint-disable-next-line no-await-in-loop
         const response = await request.head(file.url);
--- app/api/migrations/migrations/36-populate-mimetype-on-attachments/specs/36-populate-mimetype-on-attachments.spec.js
+++ app/api/migrations/migrations/36-populate-mimetype-on-attachments/specs/36-populate-mimetype-on-attachments.spec.js
@@ -21,10 +21,45 @@ describe('migration populate-mimetype-on-attachments', () => {
   });
 
   it('should populate mimetype with Content-Type', async () => {
-    headRequestMock.mockReturnValue(
-      Promise.resolve({ headers: { 'Content-Type': 'application/pdf' } })
+    await testingDB.clearAllAndLoad(fixtures);
+    const headers = {
+      get: jest
+        .fn()
+        .mockReturnValueOnce('application/pdf')
+        .mockReturnValueOnce('mimetype2'),
+    };
+    headRequestMock.and.returnValue(
+      Promise.resolve({
+        headers,
+      })
     );
     await migration.up(testingDB.mongodb);
-    expect(request.head).toHaveBeenCalled();
+    expect(request.head).toHaveBeenCalledWith(fixtures.files[0].url);
+    expect(request.head).toHaveBeenCalledWith(fixtures.files[1].url);
+    expect(headers.get).toHaveBeenCalledWith('content-type');
+
+    const files = await testingDB.mongodb
+      .collection('files')
+      .find({})
+      .toArray();
+
+    expect(files[0].mimetype).toEqual('application/pdf');
+    expect(files[1].mimetype).toEqual('mimetype2');
+  });
+
+  it('should not change the value of mimetype if it already exists', async () => {
+    const fixturesWithMimetype = {
+      files: [
+        {
+          url: 'some/url/item.jpg',
+          mimetype: 'application/pdf',
+        },
+      ],
+    };
+    await testingDB.clearAllAndLoad(fixturesWithMimetype);
+
+    const file = await testingDB.mongodb.collection('files').findOne({});
+
+    expect(file.mimetype).toEqual(fixturesWithMimetype.files[0].mimetype);
   });
 });
