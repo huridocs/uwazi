7b1876c466ecfaddb3c931b5390c99985f3a46cc
Federico Nocetti <fnocetti@users.noreply.github.com>
2021-03-24 19:38:08 -0300

    Dispatch remove documents on permission change

--- app/react/Permissions/actions/actions.ts
+++ app/react/Permissions/actions/actions.ts
@@ -3,10 +3,19 @@ import { IStore } from 'app/istore';
 import { notificationActions } from 'app/Notifications';
 import { savePermissions } from 'app/Permissions/PermissionsAPI';
 import { PermissionsDataSchema } from 'shared/types/permissionType';
+import { wrapDispatch } from 'app/Multireducer';
+import { REMOVE_DOCUMENTS_SHAREDIDS } from 'app/Library/actions/actionTypes';
 
-export function saveEntitiesPermissions(permissionsData: PermissionsDataSchema) {
+export function saveEntitiesPermissions(permissionsData: PermissionsDataSchema, storeKey: string) {
   return async (dispatch: Dispatch<IStore>) => {
     await savePermissions(permissionsData);
+    wrapDispatch(
+      dispatch,
+      storeKey
+    )({
+      type: REMOVE_DOCUMENTS_SHAREDIDS,
+      sharedIds: permissionsData.ids,
+    });
     dispatch(notificationActions.notify('Update success', 'success'));
   };
 }
--- app/react/Permissions/actions/specs/actions.spec.ts
+++ app/react/Permissions/actions/specs/actions.spec.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux';
 import { IStore } from 'app/istore';
 import { notificationActions } from 'app/Notifications';
 import { PermissionsDataSchema } from 'shared/types/permissionType';
+import { REMOVE_DOCUMENTS_SHAREDIDS } from 'app/Library/actions/actionTypes';
 import * as api from '../../PermissionsAPI';
 import * as actions from '../actions';
 
@@ -22,8 +23,22 @@ describe('Permissions actions', () => {
         ids: ['sharedId1'],
         permissions: [],
       };
-      await actions.saveEntitiesPermissions(permissionsData)(dispatch);
+      await actions.saveEntitiesPermissions(permissionsData, 'library')(dispatch);
       expect(notificationActions.notify).toHaveBeenCalledWith('Update success', 'success');
+    });
+
+    it('should remove the entities from the library/uploads when changing permissions', async () => {
+      const permissionsData: PermissionsDataSchema = {
+        ids: ['sharedId1'],
+        permissions: [],
+      };
+      await actions.saveEntitiesPermissions(permissionsData, 'library')(dispatch);
+      expect(dispatch).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: REMOVE_DOCUMENTS_SHAREDIDS,
+          sharedIds: ['sharedId1'],
+        })
+      );
     });
   });
 });
