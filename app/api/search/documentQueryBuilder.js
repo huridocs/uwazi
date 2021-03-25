f5801ea8b4852762c1a78b7f6da485f37e6dcb39
Mercy Falconí <mercy@grupocentrico.com>
2021-03-24 19:36:28 -0500

    Changing default term published filter to should

--- app/api/search/documentQueryBuilder.js
+++ app/api/search/documentQueryBuilder.js
@@ -21,6 +21,20 @@ const nested = (filters, path) => ({
 });
 
 export default function() {
+  const defaultFilter = [
+    {
+      bool: {
+        should: [
+          {
+            term: {
+              published: true,
+            },
+          },
+        ],
+      },
+    },
+  ];
+
   const baseQuery = {
     explain: false,
     _source: {
@@ -50,7 +64,7 @@ export default function() {
       bool: {
         must: [{ bool: { should: [] } }],
         must_not: [],
-        filter: [{ term: { published: true } }],
+        filter: defaultFilter,
       },
     },
     sort: [],
@@ -69,7 +83,7 @@ export default function() {
                 filter: {
                   bool: {
                     must: [{ bool: { should: [] } }],
-                    filter: [{ match: { published: true } }],
+                    filter: defaultFilter,
                   },
                 },
               },
@@ -184,21 +198,15 @@ export default function() {
     },
 
     onlyUnpublished() {
-      baseQuery.query.bool.filter[0].term.published = false;
-      aggregations._types.aggregations.filtered.filter.bool.filter[0].match.published = false;
+      baseQuery.query.bool.filter[0].bool.should[0].term.published = false;
+      aggregations._types.aggregations.filtered.filter.bool.filter[0].bool.should[0].term.published = false;
       return this;
     },
 
     includeUnpublished() {
-      const matchPulished = baseQuery.query.bool.filter.findIndex(i => i.term && i.term.published);
-      if (matchPulished >= 0) {
-        baseQuery.query.bool.filter.splice(matchPulished, 1);
-      }
-      const aggPulished = aggregations._types.aggregations.filtered.filter.bool.filter.findIndex(
-        i => i.match && i.match.published
-      );
-      if (aggPulished >= 0) {
-        aggregations._types.aggregations.filtered.filter.bool.filter.splice(aggPulished, 1);
+      const matchPulished = baseQuery.query.bool.filter[0].bool.should[0].term.published;
+      if (matchPulished) {
+        baseQuery.query.bool.filter[0].bool.should = [];
       }
       return this;
     },
