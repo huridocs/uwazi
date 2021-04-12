3ffedc7e45fc918c7e91319dd72da75d2f3227ab
Santiago <santiago@huridocs.org>
2021-04-12 15:23:39 -0300

    Logic to show modal for captcha

--- app/react/Library/components/ExportButton.tsx
+++ app/react/Library/components/ExportButton.tsx
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import { Icon } from 'app/UI';
-import { t } from 'app/I18N';
+import { t, Translate } from 'app/I18N';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
 import { wrapDispatch } from 'app/Multireducer';
 import { exportDocuments } from 'app/Library/actions/exportActions';
+import { User } from 'api/users/usersModel';
+import Modal from 'app/Layout/Modal';
 import { ExportStore } from '../reducers/ExportStoreType';
 
 export type ExportButtonProps = {
   processing: boolean;
   storeKey: string;
+  user: User;
   exportDocuments: (keyStore: string) => any;
 };
 
-class ExportButton extends Component<ExportButtonProps, {}> {
+class ExportButton extends Component<ExportButtonProps, { modal: boolean }> {
   constructor(props: ExportButtonProps) {
     super(props);
+    this.state = {
+      modal: false,
+    };
     this.export = this.export.bind(this);
+    this.showModal = this.showModal.bind(this);
   }
 
   export() {
@@ -25,11 +32,15 @@ class ExportButton extends Component<ExportButtonProps, {}> {
     }
   }
 
+  showModal() {
+    this.setState({ modal: true });
+  }
+
   render() {
     return (
       <button
         type="button"
-        onClick={this.export}
+        onClick={this.props.user._id ? this.export : this.showModal}
         className={`btn btn-primary btn-export ${this.props.processing ? 'btn-disabled' : ''}`}
       >
         {!this.props.processing ? (
@@ -38,6 +49,11 @@ class ExportButton extends Component<ExportButtonProps, {}> {
           <Icon icon="spinner" spin />
         )}
         <span className="btn-label">{t('System', 'Export CSV')}</span>
+        {this.state.modal && (
+          <Modal isOpen>
+            <Translate>Captcha</Translate>
+          </Modal>
+        )}
       </button>
     );
   }
@@ -50,6 +66,7 @@ function mapDispatchToProps(dispatch: Dispatch<any>, props: Pick<ExportButtonPro
 function mapStateToProps(state: ExportStore) {
   return {
     processing: state.exportSearchResults.exportSearchResultsProcessing,
+    user: state.user.toJS(),
   };
 }
 
