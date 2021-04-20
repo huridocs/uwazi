0a008db1f5fedaae5a70fe5c58f303c9494bc820
Santiago <santiago@huridocs.org>
2021-04-20 16:21:54 -0300

    fleshing out component

--- app/react/Templates/components/ViewTemplateAsPage.tsx
+++ app/react/Templates/components/ViewTemplateAsPage.tsx
@@ -3,16 +3,24 @@ import React from 'react';
 import { Tip } from 'app/Layout';
 
 import { ToggleChildren } from 'app/Settings/components/ToggleChildren';
+import { t } from 'app/I18N';
 
 export const ViewTemplateAsPage = () => (
   <div>
     <label>
       Display as page
-      <Tip icon="info-circle">
-        Entity can be displayed in a custom page. For that, a custom page needs to be created in
-        Pages, and then selected here.
+      <Tip icon="info-circle" position="right">
+        {t(
+          'system',
+          'Entity can be displayed in a custom page. For that, a custom page needs to be created in Pages, and then selected here.'
+        )}
       </Tip>
     </label>
-    <ToggleChildren toggled={false} />
+    <ToggleChildren toggled={false}>
+      <select>
+        <option>Placeholder 1</option>
+        <option>Placeholder 2</option>
+      </select>
+    </ToggleChildren>
   </div>
 );
--- app/react/Templates/components/specs/ViewTemplateAsPage.spec.tsx
+++ app/react/Templates/components/specs/ViewTemplateAsPage.spec.tsx
@@ -6,11 +6,8 @@ import { ViewTemplateAsPage } from '../ViewTemplateAsPage';
 describe('ViewTemplateAsPage', () => {
   let component: ShallowWrapper<typeof ViewTemplateAsPage>;
 
-  beforeEach(() => {
-    component = shallow(<ViewTemplateAsPage />);
-  });
-
   it('should contain a label with an info icon and a ToggleChildren component', () => {
+    component = shallow(<ViewTemplateAsPage />);
     expect(component.find('label')).toHaveLength(1);
     expect(component.find('Tip')).toHaveLength(1);
     expect(component.find('ToggleChildren')).toHaveLength(1);
