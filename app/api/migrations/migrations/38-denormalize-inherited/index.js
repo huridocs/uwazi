38d815ff45e9da5ebd78c7e7444b3d8eb9615f95
Alberto Casado Torres <acasadotorres@gmail.com>
2021-04-20 12:58:32 +0200

    fixed scenario for migration when the entity may not have metadata

--- app/api/migrations/migrations/38-denormalize-inherited/index.js
+++ app/api/migrations/migrations/38-denormalize-inherited/index.js
@@ -17,53 +17,49 @@ export default {
 
     while (await cursor.hasNext()) {
       const entity = await cursor.next();
+      if (entity.template && entity.metadata) {
+        const template = templates.find(t => t._id.toString() === entity.template.toString());
 
-      if (!entity.template) {
-        return;
+        await Promise.all(
+          template.properties.map(async prop => {
+            if (prop.type === 'relationship') {
+              const value = entity.metadata[prop.name] || [];
+              const denormalizedValue = await Promise.all(
+                value.map(async _elem => {
+                  const elem = { ..._elem };
+                  const [partner] = await db
+                    .collection('entities')
+                    .find({
+                      sharedId: elem.value,
+                      language: entity.language,
+                    })
+                    .toArray();
+
+                  if (prop.inherit && partner) {
+                    const partnerTemplate = templates.find(
+                      t => t._id.toString() === partner.template.toString()
+                    );
+
+                    const inheritedProperty = partnerTemplate.properties.find(
+                      p => p._id && p._id.toString() === prop.inheritProperty.toString()
+                    );
+
+                    elem.inheritedValue = partner.metadata[inheritedProperty.name] || [];
+                    elem.inheritedType = inheritedProperty.type;
+                  }
+                  return elem;
+                })
+              );
+
+              entity.metadata[prop.name] = denormalizedValue.filter(v => v);
+            }
+          })
+        );
+
+        await db
+          .collection('entities')
+          .updateOne({ _id: entity._id }, { $set: { metadata: entity.metadata } });
       }
-
-      const template = templates.find(t => t._id.toString() === entity.template.toString());
-
-      await Promise.all(
-        template.properties.map(async prop => {
-          if (prop.type === 'relationship') {
-            const value = entity.metadata[prop.name] || [];
-            const denormalizedValue = await Promise.all(
-              value.map(async _elem => {
-                const elem = { ..._elem };
-                const [partner] = await db
-                  .collection('entities')
-                  .find({
-                    sharedId: elem.value,
-                    language: entity.language,
-                  })
-                  .toArray();
-
-                if (prop.inherit && partner) {
-                  const partnerTemplate = templates.find(
-                    t => t._id.toString() === partner.template.toString()
-                  );
-
-                  const inheritedProperty = partnerTemplate.properties.find(
-                    p => p._id && p._id.toString() === prop.inheritProperty.toString()
-                  );
-
-                  elem.inheritedValue = partner.metadata[inheritedProperty.name] || [];
-                  elem.inheritedType = inheritedProperty.type;
-                }
-                return elem;
-              })
-            );
-
-            entity.metadata[prop.name] = denormalizedValue.filter(v => v);
-          }
-        })
-      );
-
-      await db
-        .collection('entities')
-        .updateOne({ _id: entity._id }, { $set: { metadata: entity.metadata } });
-
       process.stdout.write(`-> processed: ${index} \r`);
       index += 1;
     }
