4a068d32893f015794308e5e3900282b135f0782
Santiago <santiago@huridocs.org>
2021-04-26 17:10:46 -0300

    Adapted viewTemplateAsPage test

--- app/react/Templates/components/specs/ViewTemplateAsPage.spec.tsx
+++ app/react/Templates/components/specs/ViewTemplateAsPage.spec.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
 import Immutable from 'immutable';
+import thunk from 'redux-thunk';
+
+import configureStore, { MockStore, MockStoreCreator } from 'redux-mock-store';
+import { shallow, ShallowWrapper } from 'enzyme';
+import { Provider } from 'react-redux';
 
 import { ViewTemplateAsPage } from '../ViewTemplateAsPage';
-import { IStore } from 'app/istore';
+
+const middlewares = [thunk];
 
 describe('ViewTemplateAsPage', () => {
-  let component: ShallowWrapper<typeof ViewTemplateAsPage>;
-  let state: IStore;
-
-  beforeEach(() => {
-    state = {
-      pages: Immutable.fromJS({}),
-    };
+  const mockStoreCreator: MockStoreCreator<object> = configureStore<object>(middlewares);
+  const store: MockStore<object> = mockStoreCreator({
+    pages: Immutable.fromJS([
+      { title: 'page 1', _id: 'abc123', entityView: true },
+      { title: 'page 2', _id: 'def345', entityView: false },
+      { title: 'page 3', _id: 'df3485' },
+    ]),
   });
+  const component: ShallowWrapper<typeof ViewTemplateAsPage> = shallow(
+    <Provider store={store}>
+      <ViewTemplateAsPage />
+    </Provider>
+  )
+    .dive()
+    .dive();
 
   it('should contain a label with a tip and a toggled pages that can be used', () => {
-    component = shallow(<ViewTemplateAsPage />);
     expect(component.find('label')).toHaveLength(1);
     expect(component.find('Tip')).toHaveLength(1);
     expect(component.find('ToggleChildren')).toHaveLength(1);
+    expect(component.find('option').props().children).toBe('page 1');
   });
 });
