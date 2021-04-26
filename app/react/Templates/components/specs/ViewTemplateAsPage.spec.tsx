aded7d272ecc03cc6d68c5d757409b011c435e54
RafaPolit <rafaelpolit@gmail.com>
2021-04-26 12:50:56 -0500

    Incomplete test

--- app/react/Templates/components/specs/ViewTemplateAsPage.spec.tsx
+++ app/react/Templates/components/specs/ViewTemplateAsPage.spec.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
+import Immutable from 'immutable';
 
 import { ViewTemplateAsPage } from '../ViewTemplateAsPage';
+import { IStore } from 'app/istore';
 
 describe('ViewTemplateAsPage', () => {
   let component: ShallowWrapper<typeof ViewTemplateAsPage>;
+  let state: IStore;
 
-  it('should contain a label with an info icon and a ToggleChildren component', () => {
+  beforeEach(() => {
+    state = {
+      pages: Immutable.fromJS({}),
+    };
+  });
+
+  it('should contain a label with a tip and a toggled pages that can be used', () => {
     component = shallow(<ViewTemplateAsPage />);
     expect(component.find('label')).toHaveLength(1);
     expect(component.find('Tip')).toHaveLength(1);
