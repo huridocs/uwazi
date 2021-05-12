900ce0a20d2cf3fbe13843627590a04557ab7ae0
Mercy Falconí <mercy@grupocentrico.com>
2021-05-12 13:02:01 -0500

    extracts search to function

--- app/api/search.v2/specs/snippetsSearch.spec.ts
+++ app/api/search.v2/specs/snippetsSearch.spec.ts
@@ -16,11 +16,15 @@ describe('searchSnippets', () => {
 
   afterAll(async () => testingDB.disconnect());
 
-  it('should return the metadata and fullText snippets of a document', async () => {
-    const { body } = await request(app)
-      .get('/api/v2/entity/entity1SharedId/snippets')
-      .query({ filter: { searchString: 'searched' } })
+  async function searchEntitySnippets(sharedId: string, searchString: string | undefined) {
+    return request(app)
+      .get(`/api/v2/entity/${sharedId}/snippets`)
+      .query({ filter: { searchString } })
       .expect(200);
+  }
+
+  it('should return the metadata and fullText snippets of a document', async () => {
+    const { body } = await searchEntitySnippets('entity1SharedId', 'searched');
     const snippets = body.data;
     expect(snippets.count).toBe(2);
     expect(snippets.metadata.length).toBe(1);
@@ -32,10 +36,7 @@ describe('searchSnippets', () => {
   });
 
   it('should return empty snippets if there is not matches', async () => {
-    const { body } = await request(app)
-      .get('/api/v2/entity/entity1SharedId/snippets')
-      .query({ filter: { searchString: 'nonexistent' } })
-      .expect(200);
+    const { body } = await searchEntitySnippets('entity1SharedId', 'nonexistent');
     const snippets = body.data;
     expect(snippets.count).toBe(0);
     expect(snippets.metadata.length).toBe(0);
@@ -44,10 +45,7 @@ describe('searchSnippets', () => {
 
   it('should return empty snippets if there is no searchString', async () => {
     try {
-      const { body } = await request(app)
-        .get('/api/v2/entity/entity2SharedId/snippets')
-        .query({ filter: { searchString: undefined } })
-        .expect(200);
+      const { body } = await searchEntitySnippets('entity2SharedId', undefined);
       expect(body.data.count).toBe(0);
     } catch (e) {
       fail('should not throw an exception');
@@ -55,10 +53,7 @@ describe('searchSnippets', () => {
   });
 
   it('should return only the metadata snippets if there is no fullText matches', async () => {
-    const { body } = await request(app)
-      .get('/api/v2/entity/entity2SharedId/snippets')
-      .query({ filter: { searchString: 'fulltext' } })
-      .expect(200);
+    const { body } = await searchEntitySnippets('entity2SharedId', 'fulltext');
     const snippets = body.data;
     expect(snippets.count).toBe(1);
     expect(snippets.metadata.length).toBe(1);
@@ -68,10 +63,7 @@ describe('searchSnippets', () => {
 
   it('should support no valid lucene syntax', async () => {
     try {
-      const { body } = await request(app)
-        .get('/api/v2/entity/entity2SharedId/snippets')
-        .query({ filter: { searchString: 'fulltext OR' } })
-        .expect(200);
+      const { body } = await searchEntitySnippets('entity2SharedId', 'fulltext OR');
       expect(body.data.count).toBe(1);
     } catch (e) {
       fail('should not throw an exception');
@@ -84,18 +76,12 @@ describe('searchSnippets', () => {
     });
 
     it('should not return private documents', async () => {
-      const { body } = await request(app)
-        .get('/api/v2/entity/entity1SharedId/snippets')
-        .query({ filter: { searchString: 'searched' } })
-        .expect(200);
+      const { body } = await searchEntitySnippets('entity1SharedId', 'searched');
       expect(body.data.count).toBe(0);
     });
 
     it('should return the snippets of a public document', async () => {
-      const { body } = await request(app)
-        .get('/api/v2/entity/entity3SharedId/snippets')
-        .query({ filter: { searchString: 'searched' } })
-        .expect(200);
+      const { body } = await searchEntitySnippets('entity3SharedId', 'searched');
       const snippets = body.data;
       expect(snippets.count).toBe(1);
       expect(snippets.metadata.length).toBe(1);
@@ -114,18 +100,12 @@ describe('searchSnippets', () => {
     });
 
     it('should not return not shared private documents', async () => {
-      const { body } = await request(app)
-        .get('/api/v2/entity/entity1SharedId/snippets')
-        .query({ filter: { searchString: 'searched' } })
-        .expect(200);
+      const { body } = await searchEntitySnippets('entity1SharedId', 'searched');
       expect(body.data.count).toBe(0);
     });
 
     it('should return the snippets of a private document shared with her', async () => {
-      const { body } = await request(app)
-        .get('/api/v2/entity/entity4SharedId/snippets')
-        .query({ filter: { searchString: 'searched' } })
-        .expect(200);
+      const { body } = await searchEntitySnippets('entity4SharedId', 'searched');
       const snippets = body.data;
       expect(snippets.count).toBe(1);
       expect(snippets.metadata.length).toBe(1);
