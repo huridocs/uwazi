49ad542a87e198c404ea097fa61633fde4048d32
Alberto Casado Torres <acasadotorres@gmail.com>
2021-05-17 15:39:24 +0200

    removed focused test

--- app/react/Library/components/specs/FiltersFromProperties.spec.js
+++ app/react/Library/components/specs/FiltersFromProperties.spec.js
@@ -157,7 +157,7 @@ describe('FiltersFromProperties', () => {
   });
 
   describe('when inheriting properties', () => {
-    fit('should render the filter apropiate for the inherited property', () => {
+    it('should render the filter apropiate for the inherited property', () => {
       props.properties = [
         {
           content: 'aContent',
