d5d19b1c0bbc483087419a155ec87a8f20b908b2
Daneryl <daneryl@gmail.com>
2021-05-21 17:30:15 +0200

    checkMapping tests for inheritValue

--- app/api/migrations/migrations/41-denormalize-inherited-type/specs/41-denormalize-inherited-type.spec.js
+++ app/api/migrations/migrations/41-denormalize-inherited-type/specs/41-denormalize-inherited-type.spec.js
@@ -19,26 +19,26 @@ describe('migration denormalize-inherited-type', () => {
   it('should denormalize the inherited type', async () => {
     await migration.up(testingDB.mongodb);
 
-    const [template_two] = await testingDB.mongodb
+    const [templateTwo] = await testingDB.mongodb
       .collection('templates')
       .find({ name: 'template_two' })
       .toArray();
 
-    const [template_three] = await testingDB.mongodb
+    const [templateThree] = await testingDB.mongodb
       .collection('templates')
       .find({ name: 'template_three' })
       .toArray();
 
-    expect(template_two.properties[0].inherit.type).toBe('text');
-    expect(template_two.properties[0].inherit.property).toEqual(text);
-    expect(template_two.properties[0].inheritProperty).not.toBeDefined();
+    expect(templateTwo.properties[0].inherit.type).toBe('text');
+    expect(templateTwo.properties[0].inherit.property).toEqual(text);
+    expect(templateTwo.properties[0].inheritProperty).not.toBeDefined();
 
-    expect(template_two.properties[1].inherit.type).toBe('multiselect');
-    expect(template_two.properties[1].inherit.property).toEqual(multiselect);
-    expect(template_two.properties[1].inheritProperty).not.toBeDefined();
+    expect(templateTwo.properties[1].inherit.type).toBe('multiselect');
+    expect(templateTwo.properties[1].inherit.property).toEqual(multiselect);
+    expect(templateTwo.properties[1].inheritProperty).not.toBeDefined();
 
-    expect(template_three.properties[1].inherit.type).toBe('numeric');
-    expect(template_three.properties[1].inherit.property).toEqual(numeric);
-    expect(template_three.properties[1].inheritProperty).not.toBeDefined();
+    expect(templateThree.properties[1].inherit.type).toBe('numeric');
+    expect(templateThree.properties[1].inherit.property).toEqual(numeric);
+    expect(templateThree.properties[1].inheritProperty).not.toBeDefined();
   });
 });
