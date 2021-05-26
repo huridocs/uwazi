66362fd0b590dc6629183e4c070f15f501ddf85b
Alberto Casado Torres <acasadotorres@gmail.com>
2021-05-26 11:51:11 +0200

    Migration to add default metadata object to entities without metadata

---
+++ app/api/migrations/migrations/41-default-metadata-object/specs/fixtures.js
@@ -0,0 +1,10 @@
+import db from 'api/utils/testing_db';
+
+export default {
+  entities: [
+    { title: 'test_doc_1' },
+    { title: 'test_doc_2' },
+    { title: 'test_doc_3', metadata: {} },
+    { title: 'test_doc_4', metadata: { some_prop: [{ value: 42 }] } },
+  ],
+};
