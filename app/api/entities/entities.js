4bdfae6d239523763af63b45f280142bbe332aec
Daneryl <daneryl@gmail.com>
2021-06-02 10:52:05 +0200

    separation of schema and data validations

--- app/api/entities/entities.js
+++ app/api/entities/entities.js
@@ -17,7 +17,7 @@ import translate, { getContext } from 'shared/translate';
 import { unique } from 'api/utils/filters';
 import { AccessLevels } from 'shared/types/permissionSchema';
 import { permissionsContext } from 'api/permissions/permissionsContext';
-import { validateEntity } from 'shared/types/entitySchema';
+import { validateEntity } from './validateEntity';
 import { deleteFiles, deleteUploadedFiles } from '../files/filesystem';
 import model from './entitiesModel';
 import settings from '../settings';
