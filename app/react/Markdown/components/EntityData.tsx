82e51ba4fb8fdc6ba3b0e1a9c62a3ec9188df088
RafaPolit <rafaelpolit@gmail.com>
2021-06-08 13:08:57 -0500

    Added options for labels

--- app/react/Markdown/components/EntityData.tsx
+++ app/react/Markdown/components/EntityData.tsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/no-multi-comp */
 // TEST!!!
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 
-import { IStore } from 'app/istore';
+import { ClientTemplateSchema, IStore } from 'app/istore';
 import formatter from 'app/Metadata/helpers/formater';
 import { safeName } from 'shared/propertyNames';
 import { showByType } from 'app/Metadata/components/Metadata';
+import { Translate } from 'app/I18N';
+import { IImmutable } from 'shared/types/Immutable';
+import { ensure } from 'shared/tsUtils';
 
 export interface EntityDataProps {
   value?: string;
   propertyName?: string;
+}
+
+interface Options {
+  formattedEntity: any;
+  property: string;
+  newNameGeneration: boolean;
+  template?: IImmutable<ClientTemplateSchema>;
 }
 
 const mapStateToProps = ({ entityView, templates, thesauris, settings }: IStore) => ({
@@ -24,17 +35,35 @@ const connector = connect(mapStateToProps);
 type MappedProps = ConnectedProps<typeof connector>;
 type ComponentProps = EntityDataProps & MappedProps;
 
-const extractRootProperty = (formattedEntity: any, property: string) => formattedEntity[property];
+const rootProperties = ['title', 'creationDate', 'editDate'];
 
-const extractMetadataProperty = (
-  formattedEntity: any,
-  property: string = '',
-  newNameGeneration: boolean
-) => {
+const getPropertyData = ({ formattedEntity, property, newNameGeneration }: Options) =>
+  formattedEntity.metadata.find((p: any) => p.name === safeName(property, newNameGeneration));
+
+const extractRootProperty = ({ formattedEntity, property }: Options) => formattedEntity[property];
+
+const extractMetadataProperty = ({ formattedEntity, property, newNameGeneration }: Options) => {
   const propertyData = formattedEntity.metadata.find(
     (p: any) => p.name === safeName(property, newNameGeneration)
   );
   return showByType(propertyData, false);
+};
+
+const extractRootLabel = ({ property, template: _template }: Options) => {
+  const template = ensure<IImmutable<ClientTemplateSchema>>(_template);
+  const term =
+    template
+      .get('commonProperties')
+      ?.find(p => p?.get('name') === property)
+      .get('label') || '';
+
+  const context = property === 'title' ? template.get('_id') : 'System';
+  return <Translate context={context}>{term}</Translate>;
+};
+
+const extractMetadataLabel = ({ formattedEntity, property, newNameGeneration }: Options) => {
+  const propertyData = getPropertyData({ formattedEntity, property, newNameGeneration });
+  return <Translate context={propertyData.translateContext}>{propertyData.label}</Translate>;
 };
 
 const logError = (err: any, value?: string, propertyName?: string) => {
@@ -43,6 +72,42 @@ const logError = (err: any, value?: string, propertyName?: string) => {
   console.log('value: ', value, '; propertyName: ', propertyName);
   console.log(err);
   /* eslint-enable no-console */
+};
+
+const getProperty = (
+  value?: EntityDataProps['value'],
+  propertyName?: EntityDataProps['propertyName']
+) => {
+  if (value && propertyName) {
+    throw new Error('Can\'t provide both "value" and "propertyName".');
+  }
+
+  const property = value || propertyName;
+
+  if (!property) {
+    throw new Error('"value" or "propertyName" must be provided.');
+  }
+
+  return property;
+};
+
+const prepareData = (
+  value?: EntityDataProps['value'],
+  propertyName?: EntityDataProps['propertyName']
+) => {
+  const property = getProperty(value, propertyName);
+
+  const isRootProperty = rootProperties.includes(property);
+
+  let method: Function = () => {};
+
+  if (value) {
+    method = isRootProperty ? extractRootProperty : extractMetadataProperty;
+  } else {
+    method = isRootProperty ? extractRootLabel : extractMetadataLabel;
+  }
+
+  return { method, property };
 };
 
 const EntityData = ({
@@ -58,15 +123,9 @@ const EntityData = ({
   let output = <></>;
 
   try {
-    switch (value) {
-      case 'title':
-      case 'creationDate':
-      case 'editDate':
-        output = <>{extractRootProperty(formattedEntity, value)}</>;
-        break;
-      default:
-        output = <>{extractMetadataProperty(formattedEntity, value, newNameGeneration)}</>;
-    }
+    const { property, method } = prepareData(value, propertyName);
+    const template = templates.find(t => t?.get('_id') === entity.get('template'));
+    output = <>{method({ formattedEntity, property, newNameGeneration, template })}</>;
   } catch (err) {
     logError(err, value, propertyName);
   }
