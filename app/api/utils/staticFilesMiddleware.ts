3228cf0a5a3389fb718956a00a49ad61db479d62
Zasa-san <santiago@huridocs.org>
2021-06-09 17:02:41 -0300

    Implemented 404 on staticFilesMiddleware.ts

--- app/api/utils/staticFilesMiddleware.ts
+++ app/api/utils/staticFilesMiddleware.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { pathFunction, fileExists } from 'api/files';
+import { createError } from '.';
 
 const staticFilesMiddleware = (pathFunctions: pathFunction[]) => async (
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   const pathToUse = await pathFunctions.reduce<Promise<pathFunction>>(async (current, path) => {
     if (await fileExists(path(req.params.fileName))) {
@@ -11,8 +13,14 @@ const staticFilesMiddleware = (pathFunctions: pathFunction[]) => async (
     }
     return current;
   }, Promise.resolve(pathFunctions[0]));
-
-  res.sendFile(pathToUse(req.params.fileName));
+  try {
+    if (!req.params.fileName || !(await fileExists(pathToUse(req.params.fileName)))) {
+      throw createError('file not found', 404);
+    }
+    res.sendFile(pathToUse(req.params.fileName));
+  } catch (e) {
+    next();
+  }
 };
 
 export { staticFilesMiddleware };
