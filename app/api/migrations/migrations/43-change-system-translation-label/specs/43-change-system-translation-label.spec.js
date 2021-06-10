d9c1605cb9a659690941fea420c96b8ca98a3dfa
Kevin <kevin@huridocs.org>
2021-06-10 16:46:21 +0300

    removed reliance on index when getting an item from an array

--- app/api/migrations/migrations/43-change-system-translation-label/specs/43-change-system-translation-label.spec.js
+++ app/api/migrations/migrations/43-change-system-translation-label/specs/43-change-system-translation-label.spec.js
@@ -19,10 +19,16 @@ describe('migration change-system-translation-label', () => {
   it('should update system labels to User Interface', async () => {
     const expectedLabel = 'User Interface';
     await migration.up(testingDB.mongodb);
-    const [en, es, pt] = await testingDB.mongodb
+    const translations = await testingDB.mongodb
       .collection('translations')
       .find({})
       .toArray();
+
+    const localeFilter = locale => trans => trans.locale === locale;
+
+    const en = translations.find(localeFilter('en'));
+    const es = translations.find(localeFilter('es'));
+    const pt = translations.find(localeFilter('pt'));
 
     expect(en.contexts[0].label).toEqual(expectedLabel);
     expect(es.contexts[0].label).toEqual(expectedLabel);
