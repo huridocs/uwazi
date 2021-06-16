e144ea9fbb91e58ec1bffa1d8a56616b141fdc80
Laszlo Kecskes <laszlokecskes@protonmail.com>
2021-06-16 10:17:37 +0200

    corrected pupetter test; setting scriptRendered on entityView

--- app/react/Viewer/EntityView.js
+++ app/react/Viewer/EntityView.js
@@ -49,6 +49,7 @@ export default class Entity extends Component {
           template: entityTemplate,
         }
       );
+      pageView.scriptRendered = false;
 
       const pageActions = [
         actions.set('page/pageView', pageView),
