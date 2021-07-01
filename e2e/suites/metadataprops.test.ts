b284b8247a4ddfb3194efcf196e136fa62a840af
Laszlo Kecskes <laszlokecskes@protonmail.com>
2021-07-01 09:38:26 +0200

    implemented test suite

---
+++ e2e/suites/metadataprops.test.ts
@@ -0,0 +1,180 @@
+/* eslint-disable max-statements */
+// import { ExpectTimingActions } from 'expect-puppeteer';
+
+import { adminLogin, logout } from '../helpers/login';
+import { host } from '../config';
+import proxyMock from '../helpers/proxyMock';
+import insertFixtures from '../helpers/insertFixtures';
+import disableTransitions from '../helpers/disableTransitions';
+
+const scrollTo = async (selector: string): Promise<void> => {
+  await page.evaluate(str => document.querySelector(str)?.scrollIntoView(), selector);
+};
+
+const selectDate = async (selector: string, value: string, options?: any): Promise<void> => {
+  await expect(page).toFill(selector, value, options);
+  await expect(page).toClick('.react-datepicker__day--selected');
+};
+
+const clearInput = async (selector: string): Promise<void> => {
+  await expect(page).toClick(selector, { clickCount: 3 });
+  await page.keyboard.press('Backspace');
+};
+
+describe('Metadata Properties', () => {
+  beforeAll(async () => {
+    await insertFixtures();
+    await proxyMock();
+    await disableTransitions();
+  });
+
+  it('should log in as admin then click the settings nav button.', async () => {
+    await adminLogin();
+    await expect(page).toClick('a', { text: 'Account settings' });
+    expect(page.url()).toBe(`${host}/en/settings/account`);
+  });
+
+  it('should test number of available properties.', async () => {
+    await expect(page).toClick('a', { text: 'Templates' });
+    await expect(page).toClick('a', { text: 'Add template' });
+    const propertyList = await page.$$('.property-options-list li');
+    expect(propertyList.length).toBe(17);
+  });
+
+  it('should create a template with all the properties.', async () => {
+    await expect(page).toClick('a', { text: 'Templates' });
+    await expect(page).toClick('a', { text: 'Add template' });
+    await expect(page).toFill('input[name="template.data.name"]', 'All props');
+
+    const propertyAddButtons = await page.$$('.property-options-list li button');
+    //intentionaly leaving geolocation and the fields after out of the test.
+    for (let propIndex = 0; propIndex < 14; propIndex += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await propertyAddButtons[propIndex].click();
+    }
+
+    const propertiesInMetadata = await page.$$('.metadataTemplate li');
+    await expect(propertiesInMetadata[7]).toClick('button', { text: 'Edit' });
+    await expect(propertiesInMetadata[7]).toSelect('select:first-of-type', 'Relacionado a');
+    await expect(page).toClick('button', { text: 'Save' });
+    await expect(page).toClick('div.alert-success');
+  });
+
+  it('should create an entity filling all the props.', async () => {
+    await expect(page).toClick('a', { text: 'Private documents' });
+    await expect(page).toClick('span', { text: 'New entity' });
+    await expect(page).toFill(
+      'textarea[name="uploads.sidepanel.metadata.title"]',
+      'Entity with all props'
+    );
+    await expect(page).toSelect('select:first-of-type', 'All props');
+
+    await expect(page).toFill('.form-group.text input', 'demo text');
+    await expect(page).toFill('.form-group.numeric input', '42');
+    await expect(page).toSelect('.form-group.select select', 'Activo');
+    await scrollTo('.form-group.multiselect li.multiselectItem');
+    await expect(page).toClick('.form-group.multiselect li.multiselectItem', {
+      text: 'Activo',
+    });
+    await expect(page).toClick('.form-group.relationship li.multiselectItem', {
+      text: '19 Comerciantes',
+    });
+    await scrollTo('.form-group.date input');
+    await selectDate('.form-group.date input', '08/09/1966');
+    await selectDate('.form-group.daterange div.DatePicker__From input', '23/11/1963');
+    await selectDate('.form-group.daterange div.DatePicker__To input', '12/09/1964');
+    await expect(page).toClick('.form-group.multidate button.btn.add');
+    await selectDate('.form-group.multidate .multidate-item:first-of-type input', '23/11/1963');
+    await selectDate('.form-group.multidate .multidate-item:nth-of-type(2) input', '12/09/1964');
+    await expect(page).toClick('.form-group.multidaterange button.btn.add');
+    await selectDate(
+      '.form-group.multidaterange .multidate-item:first-of-type div.DatePicker__From input',
+      '23/11/1963'
+    );
+    await selectDate(
+      '.form-group.multidaterange .multidate-item:first-of-type div.DatePicker__To input',
+      '12/09/1964'
+    );
+    await selectDate(
+      '.form-group.multidaterange .multidate-item:nth-of-type(2) div.DatePicker__From input',
+      '23/11/1963'
+    );
+    await selectDate(
+      '.form-group.multidaterange .multidate-item:nth-of-type(2) div.DatePicker__To input',
+      '12/09/1964'
+    );
+    await expect(page).toFill('.form-group.markdown textarea', '***smile***');
+    await expect(page).toFill('.form-group.link #label', 'Huridocs');
+    await expect(page).toFill('.form-group.link #url', 'https://www.huridocs.org/');
+
+    await expect(page).toClick('button', { text: 'Save' });
+    await expect(page).toClick('div.alert-success');
+  }, 60000);
+
+  it('should have all the values correctly saved.', async () => {
+    await expect(page).toMatchElement('.metadata-type-text', { text: 'demo text' });
+    await expect(page).toMatchElement('.metadata-type-numeric', { text: '42' });
+    await expect(page).toMatchElement('.metadata-type-select', { text: 'Activo' });
+    await expect(page).toMatchElement('.metadata-type-multiselect', { text: 'Activo' });
+    await expect(page).toMatchElement('.metadata-type-relationship', { text: '19 Comerciantes' });
+    await expect(page).toMatchElement('.metadata-type-date', { text: 'Sep 8, 1966' });
+    await expect(page).toMatchElement('.metadata-type-daterange', {
+      text: 'Nov 23, 1963 ~ Sep 12, 1964',
+    });
+    await expect(page).toMatchElement('.metadata-type-multidate', {
+      text: 'Multi DateNov 23, 1963Sep 12, 1964',
+    });
+    await expect(page).toMatchElement('.metadata-type-multidaterange', {
+      text: 'Multi Date RangeNov 23, 1963 ~ Sep 12, 1964Nov 23, 1963 ~ Sep 12, 1964',
+    });
+    await expect(page).toMatchElement('.metadata-type-markdown strong', { text: 'smile' });
+    const linkMetaData = await page.$('.metadata-type-link a');
+    const linkText = await (await linkMetaData?.getProperty('text'))?.jsonValue();
+    const link = await (await linkMetaData?.getProperty('href'))?.jsonValue();
+    expect(linkText).toBe('Huridocs');
+    expect(link).toBe('https://www.huridocs.org/');
+  }, 60000);
+
+  it('should be able to remove all the values from properties.', async () => {
+    await expect(page).toClick('button.edit-metadata.btn svg');
+
+    await clearInput('.form-group.text input');
+    await clearInput('.form-group.numeric input');
+    await expect(page).toSelect('.form-group.select select', 'Select...');
+    await scrollTo('.form-group.multiselect li.multiselectItem');
+    await expect(page).toClick('.form-group.multiselect li.multiselectItem', {
+      text: 'Activo',
+    });
+    await expect(page).toClick('.form-group.relationship li.multiselectItem', {
+      text: '19 Comerciantes',
+    });
+    await scrollTo('.form-group.date input');
+    await expect(page).toClick('.form-group.date button');
+    await expect(page).toClick('.form-group.daterange div.DatePicker__From button');
+    await expect(page).toClick('.form-group.daterange div.DatePicker__To button');
+    await expect(page).toClick('.form-group.multidate .multidate-item:nth-of-type(2) > button');
+    await expect(page).toClick('.form-group.multidate .multidate-item:first-of-type > button');
+    await expect(page).toClick(
+      'div.form-group.multidaterange .multidate-item:nth-child(2) > div > button'
+    );
+    await expect(page).toClick(
+      'div.form-group.multidaterange .multidate-item:nth-child(1) > div > button'
+    );
+    await scrollTo('.form-group.markdown');
+    await clearInput('.form-group.markdown textarea');
+    await clearInput('.form-group.link #label');
+    await clearInput('.form-group.link #url');
+
+    await expect(page).toClick('button', { text: 'Save' });
+    await expect(page).toClick('div.alert-success');
+  }, 60000);
+
+  it('should not have metadata.', async () => {
+    const metadataDivs = await page.$$('div.metadata.tab-content-visible div.view > dl');
+    expect(metadataDivs.length).toBe(0);
+  });
+
+  afterAll(async () => {
+    await logout();
+  });
+});
