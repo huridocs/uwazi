6a54476b0538e4f3d4f7d4e6f376cbd955c2f011
Federico Nocetti <fnocetti@users.noreply.github.com>
2021-07-14 14:19:19 -0300

    Refactoring and change icons

--- app/react/Library/components/LibrarySidePanelButtons.tsx
+++ app/react/Library/components/LibrarySidePanelButtons.tsx
@@ -1,97 +1,28 @@
-/* eslint-disable react/no-multi-comp */
 import { Translate } from 'app/I18N';
 import { wrapDispatch } from 'app/Multireducer';
 import { Icon } from 'app/UI';
-import React, { ChangeEvent, Dispatch } from 'react';
+import React, { Dispatch } from 'react';
 import { bindActionCreators } from 'redux';
 import {
   newEntity as newEntityAction,
   showImportPanel as showImportPanelAction,
-  uploadDocument as uploadDocumentAction,
-  createDocument as createDocumentAction,
 } from 'app/Uploads/actions/uploadsActions';
 import { connect } from 'react-redux';
-import { unselectAllDocuments as unselectAllDocumentsAction } from 'app/Library/actions/libraryActions';
+import { NeedAuthorization } from 'app/Auth';
 import Export from './ExportButton';
-import { EntitySchema } from '../../../shared/types/entityType';
+import { PDFUploadButton } from './PDFUploadButton';
 
-const extractTitle = (file: File) => {
-  const title = file.name
-    .replace(/\.[^/.]+$/, '')
-    .replace(/_/g, ' ')
-    .replace(/-/g, ' ')
-    .replace(/ {2}/g, ' ');
-
-  return title.charAt(0).toUpperCase() + title.slice(1);
-};
-
-const onChangePDFs = ({
-  createDocument,
-  uploadDocument,
-  unselectAllDocuments,
-}: {
-  createDocument: any;
-  uploadDocument: any;
-  unselectAllDocuments: any;
-}) => (e: ChangeEvent<HTMLInputElement>) => {
-  const { files } = e.target as HTMLInputElement;
-  const count = files?.length || 0;
-
-  // eslint-disable-next-line no-plusplus
-  for (let index = 0; index < count; index++) {
-    const file = files![index];
-    const doc = { title: extractTitle(file) };
-    createDocument(doc).then((newDoc: EntitySchema) => {
-      uploadDocument(newDoc.sharedId, file);
-    });
-  }
-
-  unselectAllDocuments();
-};
-
-const PDFUploadButtonComponent = ({
-  uploadDocument,
-  unselectAllDocuments,
-  createDocument,
-}: any) => (
-  <label htmlFor="pdf-upload-button" className="btn btn-success share-btn">
-    <Icon icon="cloud-upload-alt" />
-    <span className="btn-label">
-      <Translate>Upload PDF(s) to create</Translate>
-    </span>
-    <input
-      type="file"
-      id="pdf-upload-button"
-      style={{ display: 'none' }}
-      accept="application/pdf"
-      multiple
-      onChange={onChangePDFs({
-        uploadDocument,
-        unselectAllDocuments,
-        createDocument,
-      })}
-    />
-  </label>
-);
-
-function mapDispatchToProps2(dispatch: Dispatch<any>, props: any) {
-  return bindActionCreators(
-    {
-      uploadDocument: uploadDocumentAction,
-      unselectAllDocuments: unselectAllDocumentsAction,
-      createDocument: createDocumentAction,
-    },
-    wrapDispatch(dispatch, props.storeKey)
-  );
+interface LibrarySidePanelButtonsOwnProps {
+  storeKey: string;
 }
 
-const PDFUploadButton = connect(null, mapDispatchToProps2)(PDFUploadButtonComponent);
-
-interface LibrarySidePanelButtonsProps {
-  storeKey: string;
+interface LibrarySidePanelButtonsActions {
   newEntity: (storeKey: string) => void;
   showImportPanel: () => void;
 }
+
+type LibrarySidePanelButtonsProps = LibrarySidePanelButtonsActions &
+  LibrarySidePanelButtonsOwnProps;
 
 const LibrarySidePanelButtonsComponent = ({
   storeKey,
@@ -99,31 +30,41 @@ const LibrarySidePanelButtonsComponent = ({
   showImportPanel,
 }: LibrarySidePanelButtonsProps) => (
   <div className="sidepanel-footer">
-    <button className="btn btn-success share-btn" type="button" onClick={() => newEntity(storeKey)}>
-      <Icon icon="plus" />
-      <span className="btn-label">
-        <Translate>Create entity</Translate>
-      </span>
-    </button>
-    <PDFUploadButton storeKey={storeKey} />
+    <NeedAuthorization>
+      <button className="btn btn-success" type="button" onClick={() => newEntity(storeKey)}>
+        <Icon icon="plus" />
+        <span className="btn-label">
+          <Translate>Create entity</Translate>
+        </span>
+      </button>
+    </NeedAuthorization>
+    <NeedAuthorization>
+      <PDFUploadButton storeKey={storeKey} />
+    </NeedAuthorization>
     <Export storeKey={storeKey} />
-    <button className="btn btn-success share-btn" type="button" onClick={showImportPanel}>
-      <Icon icon="export-csv" />
-      <span className="btn-label">
-        <Translate>Import CSV</Translate>
-      </span>
-    </button>
+    <NeedAuthorization roles={['admin', 'editor']}>
+      <button className="btn btn-success" type="button" onClick={showImportPanel}>
+        <Icon icon="import-csv" transform="up-0.2" />
+        <span className="btn-label">
+          <Translate>Import CSV</Translate>
+        </span>
+      </button>
+    </NeedAuthorization>
   </div>
 );
 
-function mapDispatchToProps(dispatch: Dispatch<any>, props: LibrarySidePanelButtonsProps) {
+function mapDispatchToProps(dispatch: Dispatch<any>, props: LibrarySidePanelButtonsOwnProps) {
   return bindActionCreators(
     { newEntity: newEntityAction, showImportPanel: showImportPanelAction },
     wrapDispatch(dispatch, props.storeKey)
   );
 }
 
-export const LibrarySidePanelButtons = connect(
+export const LibrarySidePanelButtons = connect<
+  {},
+  LibrarySidePanelButtonsActions,
+  LibrarySidePanelButtonsOwnProps
+>(
   null,
   mapDispatchToProps
 )(LibrarySidePanelButtonsComponent);
