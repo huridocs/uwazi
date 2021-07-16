b77fc98db00cb57d74909538f70719d75e1dd378
Santiago <santiago@huridocs.org>
2021-07-16 15:56:11 -0300

    abstracted published filters and moved to FilterForm

--- app/react/Library/components/LibraryFilters.js
+++ app/react/Library/components/LibraryFilters.js
@@ -1,35 +1,15 @@
-import { NeedAuthorization } from 'app/Auth';
 import { t, Translate } from 'app/I18N';
 import SidePanel from 'app/Layout/SidePanel';
 import { resetFilters } from 'app/Library/actions/filterActions';
-import { searchDocuments } from 'app/Library/actions/libraryActions';
 import FiltersForm from 'app/Library/components/FiltersForm';
 import { wrapDispatch } from 'app/Multireducer';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { actions as formActions, Field } from 'react-redux-form';
 import { bindActionCreators } from 'redux';
 import { Icon } from 'UI';
 import { hideFilters } from 'app/Entities/actions/uiActions';
 import { LibrarySidePanelButtons } from 'app/Library/components/LibrarySidePanelButtons';
-
-function togglePublished(storeKey, key) {
-  return (dispatch, getState) => {
-    const { search } = getState()[storeKey];
-
-    // OPTION 1
-    // if (key === 'published' && search.published && !search.restricted) return;
-
-    // OPTION 2
-    // if (key === 'published' && search.published && !search.restricted) {
-    //   dispatch(formActions.change(`${storeKey}.search.restricted`, true));
-    // }
-
-    dispatch(formActions.change(`${storeKey}.search.${key}`, !search[key]));
-    dispatch(searchDocuments({}, storeKey));
-  };
-}
 
 export class LibraryFilters extends Component {
   reset() {
@@ -67,32 +47,6 @@ export class LibraryFilters extends Component {
               </button>
             </div>
           </div>
-          <NeedAuthorization roles={['admin', 'editor', 'collaborator']}>
-            <div className="nested-selector admin-filter">
-              {[
-                { type: 'published', label: 'Published' },
-                { type: 'restricted', label: 'Restricted' },
-              ].map(field => (
-                <Field
-                  model={`${this.props.storeKey}.search.${field.type}`}
-                  className="multiselectItem"
-                  onClick={() => this.props.togglePublished(this.props.storeKey, field.type)}
-                >
-                  <input type="checkbox" className="multiselectItem-input" />
-                  <label className="multiselectItem-label">
-                    <span className="multiselectItem-icon">
-                      <Icon icon={['far', 'square']} className="checkbox-empty" />
-                      <Icon icon="check" className="checkbox-checked" />
-                    </span>
-                    <span className="multiselectItem-name">
-                      <Icon icon="globe-africa" />
-                      <Translate>{field.label}</Translate>
-                    </span>
-                  </label>
-                </Field>
-              ))}
-            </div>
-          </NeedAuthorization>
 
           <FiltersForm storeKey={this.props.storeKey} />
         </div>
@@ -106,19 +60,14 @@ LibraryFilters.defaultProps = {
   storeKey: 'library',
   sidePanelMode: '',
   hideFilters: () => {},
-  unpublished: false,
-  includeUnpublished: false,
 };
 
 LibraryFilters.propTypes = {
   resetFilters: PropTypes.func.isRequired,
-  togglePublished: PropTypes.func.isRequired,
   open: PropTypes.bool,
   storeKey: PropTypes.string,
   sidePanelMode: PropTypes.string,
   hideFilters: PropTypes.func,
-  unpublished: PropTypes.bool,
-  includeUnpublished: PropTypes.bool,
 };
 
 export function mapStateToProps(state, props) {
@@ -126,16 +75,11 @@ export function mapStateToProps(state, props) {
   const isFilterShown = state[props.storeKey].ui.get('filtersPanel') !== false;
   return {
     open: noDocumentSelected && isFilterShown,
-    unpublished: (state[props.storeKey].search || {}).unpublished,
-    includeUnpublished: (state[props.storeKey].search || {}).includeUnpublished,
   };
 }
 
 function mapDispatchToProps(dispatch, props) {
-  return bindActionCreators(
-    { resetFilters, togglePublished, hideFilters },
-    wrapDispatch(dispatch, props.storeKey)
-  );
+  return bindActionCreators({ resetFilters, hideFilters }, wrapDispatch(dispatch, props.storeKey));
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LibraryFilters);
