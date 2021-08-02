2f783fa8822885cc6b3b83721ca05d630e2ba97c
Mercy Falconí <mercy@huridocs.org>
2021-08-02 14:08:06 -0500

    removes queue

--- app/api/utils/testingEnvironment.ts
+++ app/api/utils/testingEnvironment.ts
@@ -6,88 +6,55 @@ import { UserSchema } from 'shared/types/userType';
 import { UserInContextMockFactory } from 'api/utils/testingUserInContext';
 import { setupTestUploadedPaths } from 'api/files';
 
-class TestingEnvironment {
-  private queue: Promise<void>;
+async function setTenant() {
+  testingTenants.mockCurrentTenant({
+    name: testingDB.dbName || 'defaultDB',
+    dbName: testingDB.dbName || 'defaultDB',
+    indexName: 'index',
+  });
+  await setupTestUploadedPaths();
+}
 
-  private appContextSpy: jest.SpyInstance | null = null;
-
-  constructor() {
-    this.queue = Promise.resolve();
-  }
-
-  async setUp(fixtures?: DBFixture, elasticIndex?: string) {
-    this.queue = Promise.resolve();
-    await this.withTenant().withFixtures(fixtures);
-    await this.withElastic(elasticIndex)
-      .withPermissions()
-      .withRequestId();
-    return this;
-  }
-
-  async then(callback: (queue: Promise<any>) => {}) {
-    callback(this.queue);
-  }
-
-  chain(callback: () => Promise<void>): void {
-    this.queue = this.queue.then(callback);
-  }
-
-  withTenant() {
-    this.chain(async () => {
-      await testingTenants.mockCurrentTenant({
-        name: testingDB.dbName || 'defaultDB',
-        dbName: testingDB.dbName || 'defaultDB',
-        indexName: 'index',
-      });
-      await setupTestUploadedPaths();
-    });
-
-    return this;
-  }
-
-  withFixtures(fixtures?: DBFixture) {
-    if (fixtures) {
-      this.chain(async () => {
-        await testingDB.connect();
-        await testingDB.clearAllAndLoadFixtures(fixtures);
-      });
-    }
-    return this;
-  }
-
-  withElastic(elasticIndex?: string) {
-    if (elasticIndex) {
-      testingTenants.changeCurrentTenant({ indexName: elasticIndex });
-      this.chain(async () => {
-        await elasticTesting.reindex();
-      });
-    }
-    return this;
-  }
-
-  withPermissions(user?: UserSchema) {
-    const userInContextMockFactory = new UserInContextMockFactory();
-    if (!user) {
-      userInContextMockFactory.mockEditorUser();
-    } else {
-      userInContextMockFactory.mock(user);
-    }
-    return this;
-  }
-
-  withRequestId(requestId: string = '1234') {
-    this.appContextSpy = jest
-      .spyOn(appContext, 'get')
-      .mockImplementation(key => (key === 'requestId' ? requestId : null));
-    return this;
-  }
-
-  async tearDown() {
-    this.chain(async () => {
-      await testingDB.disconnect();
-    });
-    return this;
+async function setFixtures(fixtures?: DBFixture) {
+  if (fixtures) {
+    await testingDB.connect();
+    await testingDB.clearAllAndLoadFixtures(fixtures);
   }
 }
 
-export const testingEnvironment = new TestingEnvironment();
+async function setElastic(elasticIndex?: string) {
+  if (elasticIndex) {
+    testingTenants.changeCurrentTenant({ indexName: elasticIndex });
+    await elasticTesting.reindex();
+  }
+}
+
+function setPermissions(user?: UserSchema) {
+  const userInContextMockFactory = new UserInContextMockFactory();
+  if (!user) {
+    userInContextMockFactory.mockEditorUser();
+  } else {
+    userInContextMockFactory.mock(user);
+  }
+}
+
+const testingEnvironment = {
+  async setUp(fixtures?: DBFixture, elasticIndex?: string) {
+    await setTenant();
+    await setFixtures(fixtures);
+    await setElastic(elasticIndex);
+    setPermissions();
+    this.setRequestId();
+  },
+
+  setRequestId(requestId: string = '1234') {
+    jest
+      .spyOn(appContext, 'get')
+      .mockImplementation(key => (key === 'requestId' ? requestId : null));
+  },
+  async tearDown() {
+    await testingDB.disconnect();
+  },
+};
+
+export { testingEnvironment };
