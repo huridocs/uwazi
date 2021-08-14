de3c944202223c717bda4d5351499e7ebcf0e9cb
Santiago <santiago@huridocs.org>
2021-08-14 17:45:02 -0300

    avoid invalid values in numeric fields

--- app/react/Metadata/actions/metadataExtractionActions.ts
+++ app/react/Metadata/actions/metadataExtractionActions.ts
@@ -16,12 +16,17 @@ const formFieldUpdater = (value: string, model: string, fieldType?: string) => {
   if (fieldType === 'date') {
     let getDate = Date.parse(`${value} GMT`);
     if (Number.isNaN(getDate)) {
-      const dateFormats = ['DD-MM-YYYY', 'MM-DD-YYYY', 'YYYY-MM-DD', 'YYYY'];
-      const momentDate = moment.parseZone(value, dateFormats).format('x');
+      const momentDate = moment
+        .parseZone(value, ['DD-MM-YYYY', 'MM-DD-YYYY', 'YYYY-MM-DD', 'YYYY'])
+        .format('x');
       getDate = parseInt(momentDate, 10);
     }
     const dateForPicker = getDate / 1000;
     return formActions.change(model, dateForPicker);
+  }
+
+  if (fieldType === 'numeric' && Number.isNaN(Number.parseInt(value, 10))) {
+    return formActions.change(model, '0');
   }
 
   return formActions.change(model, value);
--- app/react/Metadata/actions/specs/metadataExtractionActions.spec.ts
+++ app/react/Metadata/actions/specs/metadataExtractionActions.spec.ts
@@ -1,22 +1,19 @@
 import { actions } from 'app/BasicReducer';
-import api from 'app/utils/api';
 import { actions as formActions } from 'react-redux-form';
 import { updateSelection, formFieldUpdater } from '../metadataExtractionActions';
 
 describe('metadataExtractionActions', () => {
-  beforeAll(() => {
-    spyOn(formActions, 'change');
-    spyOn(actions, 'updateIn');
-    spyOn(api, 'post').and.returnValue(Response);
-  });
-
   describe('formFieldUpdater', () => {
+    beforeEach(() => {
+      spyOn(formActions, 'change');
+    });
+
     it('should update the form fields with the selected value', () => {
       formFieldUpdater('value to put in form', 'fieldModel');
       expect(formActions.change).toHaveBeenCalledWith('fieldModel', 'value to put in form');
     });
 
-    it.each(['01/30/1999', '30/01/1999', '01-30-1999', '30-01-1999', '01 30 1999', '30 01 1999'])(
+    it.each(['01/30/1999', '30/01/1999', '01-30-1999', '01 30 1999', '30 01 1999'])(
       'should format valid date inputs for Datepicker.js component',
       dateStrings => {
         const dateForDatepickerInUTC = 917654400;
@@ -30,9 +27,24 @@ describe('metadataExtractionActions', () => {
       formFieldUpdater('2021', 'fieldModel', 'date');
       expect(formActions.change).toHaveBeenCalledWith('fieldModel', dateForDatepickerInUTC);
     });
+
+    describe('numeric fields', () => {
+      it('should check that selections for numeric fields are actual numbers', () => {
+        formFieldUpdater('12345', 'fieldModel', 'numeric');
+        expect(formActions.change).toHaveBeenCalledWith('fieldModel', '12345');
+      });
+      it('should set the numeric field to 0 if the value is not a number', () => {
+        formFieldUpdater('une two three', 'fieldModel', 'numeric');
+        expect(formActions.change).toHaveBeenCalledWith('fieldModel', '0');
+      });
+    });
   });
 
   describe('updateSelection', () => {
+    beforeEach(() => {
+      spyOn(actions, 'updateIn');
+    });
+
     it('should call updateIn with the selection parameters and the correct storeKey', () => {
       updateSelection(
         { selectedText: 'text selected by the user on the file' },
