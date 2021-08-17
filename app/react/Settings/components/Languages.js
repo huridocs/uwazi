f7a8a670a254c061930ab532ff610173e028efaf
Kevin <nderitukelvin19@gmail.com>
2021-08-17 13:58:08 +0300

    Changed tooltip position

--- app/react/Settings/components/Languages.js
+++ app/react/Settings/components/Languages.js
@@ -14,7 +14,7 @@ export class Languages extends Component {
     return (
       <span>
         {t('System', 'Default language')}
-        <Tip>
+        <Tip position="right" style={{ zIndex: '1' }}>
           This language will be used as default translation when adding new languages, and the
           default language for the site when no other language has been selected.
         </Tip>
