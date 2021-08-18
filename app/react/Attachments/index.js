ffac876b153fcfe49089b6fe8a7ebde8a0d5931b
Alberto Casado Torres <acasadotorres@gmail.com>
2021-08-18 15:51:47 +0200

    fixed import

--- app/react/Attachments/index.js
+++ app/react/Attachments/index.js
@@ -1,13 +1,9 @@
-import loadable from '@loadable/component';
-import AttachmentsList from './components/AttachmentsList';
-import { AttachmentsModal } from './components/AttachmentsModal';
 import UploadAttachment from './components/UploadAttachment';
 import manageAttachmentsReducer from './reducers/manageAttachmentsReducer';
 import reducer from './reducers/reducer';
-
-const RenderAttachment = loadable(async () =>
-  import(/* webpackChunkName: "LazyLoadMarkdownMedia" */ './components/RenderAttachment')
-);
+import { RenderAttachment } from './components/RenderAttachment';
+import AttachmentsList from './components/AttachmentsList';
+import { AttachmentsModal } from './components/AttachmentsModal';
 
 export {
   AttachmentsList,
