4741fbd9455e9829ed0e707b11c43ce452047885
mfacar <mercy@huridocs.org>
2021-08-19 16:25:14 -0500

    showing notification at complete uploading

--- app/react/Attachments/actions/actions.js
+++ app/react/Attachments/actions/actions.js
@@ -53,6 +53,7 @@ export function uploadAttachment(entity, file, __reducerKey) {
         dispatch({ type: types.ATTACHMENT_PROGRESS, entity, progress: Math.floor(data.percent) });
       })
       .on('response', result => {
+        dispatch(notify('Attachment uploaded', 'success'));
         dispatch({
           type: types.ATTACHMENT_COMPLETE,
           entity,
@@ -70,6 +71,7 @@ export function uploadAttachmentFromUrl(entity, name, url, __reducerKey) {
     api
       .post('files', new RequestParams({ originalname: name, url, entity, type: 'attachment' }))
       .then(newFile => {
+        dispatch({ type: types.ATTACHMENT_PROGRESS, entity, progress: 100 });
         dispatch({ type: types.ATTACHMENT_COMPLETE, entity, file: newFile.json, __reducerKey });
         dispatch(notify('Attachment uploaded', 'success'));
       });
