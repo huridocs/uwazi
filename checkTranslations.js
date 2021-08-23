f92358326aba36cf38445f8cce3130e0b65c23e0
Alberto Casado Torres <acasadotorres@gmail.com>
2021-08-23 20:45:01 +0200

    identifying every text not wrapped in a Translate element

--- checkTranslations.js
+++ checkTranslations.js
@@ -1,4 +1,6 @@
 const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+
 const { resolve } = require('path');
 const { readdir, readFile } = require('fs').promises;
 
@@ -20,22 +22,56 @@ const parserOptions = {
   plugins: [
     // enable jsx and flow syntax
     'jsx',
-    'flow',
+    'typescript',
   ],
 };
 
-async function parseFile(filename) {
-  const fileContents = await readFile(filename, 'utf8');
+const wordRegexp = new RegExp(/\b[\w']+\b/g);
+async function parseFile(file) {
+  const result = [];
+  const fileContents = await readFile(file, 'utf8');
 
-  const parseResult = parser.parse(fileContents, parserOptions);
-  console.log(parseResult);
-  return parseResult;
+  const ast = parser.parse(fileContents, parserOptions);
+  traverse(ast, {
+    enter(path) {
+      if (path.type === 'JSXText') {
+        const text = path.node.value.trim();
+        const element = path.parent.openingElement?.name.name;
+        if (wordRegexp.test(text)) {
+          let key;
+          if (element === 'Translate' && element && path.parent.openingElement.attributes.length) {
+            key = path.parent.openingElement.attributes.find(a => a.name.name === 'translationKey')
+              ?.value.value;
+          }
+          const shortName = file.split('app/react/').pop();
+          result.push({ text, element, file: shortName, key });
+        }
+      }
+    },
+  });
+
+  return result;
 }
 
 async function checkTranslations(dir) {
   const allFiles = await getFiles(dir);
-  const files = allFiles.filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+  const files = allFiles.filter(
+    file => !file.match('.spec') && (file.endsWith('.js') || file.endsWith('.ts'))
+  );
+
   const results = await Promise.all(files.map(file => parseFile(file)));
+  const allTexts = results.reduce((acc, curr) => acc.concat(curr), []);
+
+  const textsWithoutTranslateElement = allTexts.filter(t => t.element !== 'Translate');
+  textsWithoutTranslateElement.forEach(({ text, element, file }) => {
+    console.log('\x1b[36m', file, '\x1b[37m', text, '\x1b[31m', element, '\x1b[0m');
+  });
+
+  console.log(
+    '\x1b[31m',
+    `Found ${textsWithoutTranslateElement.length} texts not wrapped in a <Translate> element`,
+    '\x1b[0m'
+  );
 }
 
 checkTranslations('./app/react');
