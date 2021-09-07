d59e936d3748270707299e28ae2455b1f5e8a344
gabo <gabriel.piles.glez@gmail.com>
2021-09-07 19:02:49 +0200

    Task manager simple version working

--- app/api/tasksmanager/specs/taskManager.spec.ts
+++ app/api/tasksmanager/specs/taskManager.spec.ts
@@ -1,25 +1,33 @@
-import { TaskManagerFactory, TaskManager } from 'api/tasksmanager/TaskManager';
+import fs from 'fs';
 
-import { RedisServer } from '../RedisServer';
-import { DummyService } from './DummyService';
+import { TaskManagerFactory, TaskManager, Service } from 'api/tasksmanager/taskManager';
+
 import Redis from 'redis';
+import { RedisServer } from '../RedisServer';
+import { ExternalDummyService } from './ExternalDummyService';
 
 describe('taskManager', () => {
   let taskManager: TaskManager;
 
-  let queueName: string;
+  let service: Service;
   let redisServer: RedisServer;
   let client: Redis.RedisClient;
-  let externalDummyService: DummyService;
+  let externalDummyService: ExternalDummyService;
 
   beforeAll(async () => {
-    queueName = 'KonzNGaboHellKitchen';
+    service = {
+      serviceName: 'KonzNGaboHellKitchen',
+      dataUrl: 'http://localhost:1234/data',
+      filesUrl: 'http://localhost:1234/files',
+      resultsUrl: 'http://localhost:1234/results',
+    };
+
     redisServer = new RedisServer();
     await redisServer.start();
     client = await Redis.createClient('redis://localhost:6379');
-    taskManager = await TaskManagerFactory.create(client, queueName);
+    taskManager = await TaskManagerFactory.create(client, service);
 
-    externalDummyService = new DummyService(1234);
+    externalDummyService = new ExternalDummyService(1234);
     await externalDummyService.start(client);
   });
 
@@ -71,12 +79,56 @@ describe('taskManager', () => {
 
     describe('sending materials', () => {
       it('should send materials to the service', async () => {
-        // const task = { task: 'doit', tenant: 'test' };
-        // const materials = { data: '{"someData": "someValue"}' };
-        // await taskManager.startTask(task, materials);
-        // await externalDummyService.read();
-        // expect(externalDummyService.materials[0]).toEqual(materials);
+        const task = { task: 'Tofu', tenant: 'Me' };
+        const materials1 = { someData: 3 };
+        const materials2 = { someData: 2 };
+        const materials3 = { someData: 3 };
+        await taskManager.sendJSON(materials1);
+        await taskManager.sendJSON(materials2);
+        await taskManager.sendJSON(materials3);
+
+        await taskManager.startTask(task);
+        await externalDummyService.read();
+
+        expect(externalDummyService.materials.length).toEqual(3);
+        expect(externalDummyService.materials[0]).toEqual(materials1);
+        expect(externalDummyService.materials[1]).toEqual(materials2);
+        expect(externalDummyService.materials[2]).toEqual(materials3);
       });
+
+      it('should send files to the service', async () => {
+        const task = { task: 'make_food', tenant: 'test' };
+
+        const file = fs.readFileSync('app/api/tasksmanager/specs/blank.pdf');
+
+        await taskManager.sendFile(file);
+        await taskManager.sendFile(file);
+        await taskManager.sendFile(file);
+
+        await taskManager.startTask(task);
+        await externalDummyService.read();
+
+        expect(externalDummyService.files.length).toEqual(3);
+        expect(externalDummyService.files[0]).toEqual(file);
+        expect(externalDummyService.files[1]).toEqual(file);
+        expect(externalDummyService.files[2]).toEqual(file);
+      });
+    });
+
+    it('should get the results when the task has been finished', async done => {
+      taskManager.end();
+
+      const expectedResults = { results: 'Paella' };
+      const expectFunction = (results: object) => {
+        console.log('almost there');
+        expect(results).toEqual(expectedResults);
+        done();
+      };
+      await TaskManagerFactory.create(client, service, expectFunction);
+
+      const task = { task: 'make_food', tenant: 'test' };
+      externalDummyService.setResults(expectedResults);
+      await externalDummyService.sendFinishedMessage(task);
     });
   });
 });
