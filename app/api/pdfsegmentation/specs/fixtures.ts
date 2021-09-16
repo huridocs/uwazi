baaf6c2aee6cebd9aa6da8dafa71a6df9132f296
gabo <gabriel.piles.glez@gmail.com>
2021-09-16 18:05:53 +0200

    Segment many files

--- app/api/pdfsegmentation/specs/fixtures.ts
+++ app/api/pdfsegmentation/specs/fixtures.ts
@@ -30,6 +30,40 @@ const fixturesOneFile: DBFixture = {
   files: [factory.file('F1', 'A1', 'en', 'document', fixturesPdfName)],
 };
 
+const fixturesTwelveFiles: DBFixture = {
+  settings,
+  entities: [
+    factory.entity('A1', 'templateToSegmentA'),
+    factory.entity('A2', 'templateToSegmentA'),
+    factory.entity('A3', 'templateToSegmentA'),
+    factory.entity('A4', 'templateToSegmentA'),
+    factory.entity('A5', 'templateToSegmentA'),
+    factory.entity('A6', 'templateToSegmentA'),
+    factory.entity('A7', 'templateToSegmentA'),
+    factory.entity('A8', 'templateToSegmentA'),
+    factory.entity('A9', 'templateToSegmentA'),
+    factory.entity('A10', 'templateToSegmentA'),
+    factory.entity('A11', 'templateToSegmentA'),
+    factory.entity('A12', 'templateToSegmentA'),
+    factory.entity('A13', 'templateToSegmentA'),
+    factory.entity('A14', 'templateToSegmentA'),
+  ],
+  files: [
+    factory.file('F1', 'A1', 'en', 'document', fixturesPdfName),
+    factory.file('F2', 'A2', 'en', 'document', fixturesPdfName),
+    factory.file('F3', 'A3', 'en', 'document', fixturesPdfName),
+    factory.file('F4', 'A4', 'en', 'document', fixturesPdfName),
+    factory.file('F5', 'A5', 'en', 'document', fixturesPdfName),
+    factory.file('F6', 'A6', 'en', 'document', fixturesPdfName),
+    factory.file('F7', 'A7', 'en', 'document', fixturesPdfName),
+    factory.file('F8', 'A8', 'en', 'document', fixturesPdfName),
+    factory.file('F9', 'A9', 'en', 'document', fixturesPdfName),
+    factory.file('F10', 'A10', 'en', 'document', fixturesPdfName),
+    factory.file('F11', 'A11', 'en', 'document', fixturesPdfName),
+    factory.file('F12', 'A12', 'en', 'document', fixturesPdfName),
+  ],
+};
+
 const fixturesUseDefaultPdfPerLanguage: DBFixture = {
   settings,
   entities: [
@@ -44,4 +78,4 @@ const fixturesUseDefaultPdfPerLanguage: DBFixture = {
   ],
 };
 
-export { fixturesPdfName, fixturesOneFile, fixturesUseDefaultPdfPerLanguage };
+export { fixturesPdfName, fixturesOneFile, fixturesTwelveFiles, fixturesUseDefaultPdfPerLanguage };
