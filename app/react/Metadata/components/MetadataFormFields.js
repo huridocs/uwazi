4fbd14c20f6e5381fc33641642f31960cc3a0394
Kevin <nderitukelvin19@gmail.com>
2021-09-17 10:59:37 +0300

    Remove documentView storeKey check

--- app/react/Metadata/components/MetadataFormFields.js
+++ app/react/Metadata/components/MetadataFormFields.js
@@ -356,7 +356,7 @@ export const mapStateToProps = (state, ownProps) => {
     attachments = selectedDocuments.size ? selectedDocuments.get(0).get('attachments') : undefined;
   }
 
-  if (storeKey === 'documentView' || storeKey === 'documentViewer') {
+  if (storeKey === 'documentViewer') {
     const entity = state[storeKey].doc;
     attachments = entity.get('attachments');
   }
