bf9f12f133663a17e359ef53a62af1aa1933a54b
RafaPolit <rafaelpolit@gmail.com>
2021-09-17 15:20:39 -0500

    Refactored to allow external ussage of format fns

--- app/api/log/infoFormat.ts
+++ app/api/log/infoFormat.ts
@@ -3,7 +3,7 @@ import winston from 'winston';
 import { tenants } from 'api/tenants';
 import { config } from 'api/config';
 
-const addTenant = winston.format((info, { instanceName }: { instanceName: string }) => {
+const addTenant = (info: any, { instanceName }: { instanceName: string }) => {
   let tenantName = instanceName;
   let tenantError;
 
@@ -15,7 +15,7 @@ const addTenant = winston.format((info, { instanceName }: { instanceName: string
   }
 
   return { ...info, tenant: tenantName, tenantError };
-});
+};
 
 const formatInfo = (info: any) => {
   const message = info.message && info.message.join ? info.message.join('\n') : info.message;
@@ -27,8 +27,8 @@ const formatInfo = (info: any) => {
 const formatter = (DATABASE_NAME: String) =>
   winston.format.combine(
     winston.format.timestamp(),
-    addTenant({ instanceName: DATABASE_NAME }),
+    winston.format(addTenant)({ instanceName: DATABASE_NAME }),
     winston.format.printf(info => formatInfo(info))
   );
 
-export { formatter };
+export { formatter, addTenant, formatInfo };
