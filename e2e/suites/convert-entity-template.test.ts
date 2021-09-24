d5f82984425f1f62e05a2f360b4723c856ee1203
Kevin <nderitukelvin19@gmail.com>
2021-09-24 15:29:36 +0300

    Added e2e

--- e2e/suites/convert-entity-template.test.ts
+++ e2e/suites/convert-entity-template.test.ts
@@ -1,9 +1,10 @@
 /*global page*/
 
 import { ElementHandle } from 'puppeteer';
+import { createEntity } from 'e2e/helpers/createEntity';
+import { createTemplate } from 'e2e/helpers/createTemplate';
 import { adminLogin, logout } from '../helpers/login';
 import proxyMock from '../helpers/proxyMock';
-import { host } from '../config';
 import insertFixtures from '../helpers/insertFixtures';
 import disableTransitions from '../helpers/disableTransitions';
 
@@ -14,88 +15,41 @@ const setupPreFlights = async (): Promise<void> => {
   await disableTransitions();
 };
 
-const createTemplate = async (name: string) => {
-  await page.goto(`${host}/en/settings/account`);
-  await expect(page).toClick('span', { text: 'Templates' });
-  await expect(page).toClick('span', { text: 'Add template' });
-  await expect(page).toFill('input[name="template.data.name"]', name);
-  if (name === 'With image') {
-    const imageElement = await page.$$('ul.property-options-list > .list-group-item');
-    const button = await imageElement[11].$$('button');
-    await button[0].click();
-  }
-  await expect(page).toClick('button[type="submit"]');
-  await expect(page).toClick('span', { text: 'Saved successfully.' });
-};
-
-const createEntity = async (templateName: string) => {
-  await page.goto(`${host}`);
-  await expect(page).toClick('button', { text: 'Create entity' });
-  await expect(page).toFill('textarea[name="library.sidepanel.metadata.title"]', templateName);
-  let options: ElementHandle<Element>[] = [];
-  await page.$$('select.form-control > option').then(selects => {
-    options = selects;
-  });
-
-  // @ts-ignore
-  options.forEach(async (option: ElementHandle): void => {
-    const value = await option.evaluate(optionEl => ({
-      text: optionEl.textContent,
-      value: optionEl.getAttribute('value') as string,
-    }));
-    if (value.text === templateName) {
-      await page.select('select.form-control', value.value);
-    }
-  });
-  await expect(page).toMatchElement('button[form="metadataForm"]', { text: 'Save' });
-  await expect(page).toClick('button[form="metadataForm"]', { text: 'Save' });
-  await expect(page).toClick('span', { text: 'Entity created' });
-};
-
-const uploadPDFToEntity = async (entityName: string) => {
-  await page.goto(`${host}`);
-  await expect(page).toClick('span', { text: 'Restricted' });
-  await expect(page).toClick('div.item-name > span', { text: entityName });
-  await expect(page).toUploadFile('#upload-button-input', `${__dirname}/test_files/valid.pdf`);
-};
-
-const UploadSupportingFileToEntity = async (entityName: string): Promise<void> => {
-  await page.goto(`${host}`);
-  await expect(page).toClick('span', { text: 'Restricted' });
-  await expect(page).toClick('div.item-name > span', { text: entityName });
-  await expect(page).toClick('button[type="button"].upload-button');
-  const [fileChooser] = await Promise.all([
-    page.waitForFileChooser(),
-    page.click('div.attachments-modal__dropzone > button'),
-  ]);
-  await fileChooser.accept([`${__dirname}/test_files/batman.jpg`]);
-};
-
-const convertEntityTemplate = async (entityName: string, targetTemplate: string): Promise<void> => {
-  await page.goto(`${host}`);
-  await expect(page).toClick('span', { text: 'Restricted' });
-  await expect(page).toClick('div.item-name > span', { text: entityName });
-  await expect(page).toClick('div.sidepanel-footer > button[type="button"].edit-metadata');
-};
-
 const setupTest = async () => {
   await createTemplate('Without image');
   await createTemplate('With image');
-  await createEntity('Without image');
-  await uploadPDFToEntity('Without image');
-  await UploadSupportingFileToEntity('Without image');
+  await createEntity('Without image', { pdf: 'valid.pdf', supportingFile: 'batman.jpg' });
 };
 
 describe('Convert entity template', () => {
   beforeAll(async () => {
     await setupPreFlights();
-
-    await convertEntityTemplate('Without image', 'With image');
   });
 
-  it('Should create new entity', async () => {
+  it('Should select image for image property from supporting files', async () => {
     await setupTest();
-    await expect(page).toClick('a[(type = "button")].btn');
+    await expect(page).toClick('a[type="button"]');
+    await page.reload();
+    await expect(page).toClick('button.edit-metadata');
+    await page.waitFor(1000);
+    const options = await page.$$('select.form-control > option');
+
+    const optionsValues = await options.map(async (optionElement: ElementHandle<Element>) => {
+      console.log('option');
+      const value = await optionElement.evaluate(optionEl => ({
+        text: optionEl.textContent,
+        value: optionEl.getAttribute('value') as string,
+      }));
+      return value;
+    });
+
+    const optionValues: any[] = await Promise.all(optionsValues);
+    const value = optionValues.find(option => option.text === 'With image').value;
+
+    await page.select('select.form-control', value);
+    await expect(page).toClick('span', { text: 'Select supporting file' });
+    await page.waitFor(10000);
+    await expect(page).toMatchElement('div.media-grid-card-header > h5', { text: 'batman.jpg' });
   });
 
   afterAll(async () => {
