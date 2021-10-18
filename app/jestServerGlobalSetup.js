277b4a8d74d9331269482f056e99bb6c6061ed3a
Alberto Casado Torres <acasadotorres@gmail.com>
2021-10-18 15:18:12 +0200

    Moved redis installation to a jest pre hook

--- app/jestServerGlobalSetup.js
+++ app/jestServerGlobalSetup.js
@@ -1,6 +1,8 @@
 import { createMongoInstance as checkMongoVersion } from './api/utils/createMongoInstance.js';
+import { downloadRedis } from './api/utils/downloadRedis.js';
 
 module.exports = async () => {
   const mongod = await checkMongoVersion();
   await mongod.stop();
+  downloadRedis();
 };
