d174ea1cf4cf3d3af80bac2152e177c78c75269d
Kevin <nderitukelvin19@gmail.com>
2021-10-18 12:11:31 +0300

    refactored migration test

--- app/api/migrations/migrations/55-replace_dictionary_with_thesaurus/specs/55-replace_dictionary_with_thesaurus.spec.js
+++ app/api/migrations/migrations/55-replace_dictionary_with_thesaurus/specs/55-replace_dictionary_with_thesaurus.spec.js
@@ -3,9 +3,16 @@ import migration from '../index.js';
 import fixtures from './fixtures.js';
 
 describe('migration replace_dictionary_with_thesaurus', () => {
+  let translations;
+
   beforeEach(async () => {
     //spyOn(process.stdout, 'write');
     await testingDB.clearAllAndLoad(fixtures);
+    await migration.up(testingDB.mongodb);
+    translations = await testingDB.mongodb
+      .collection('translations')
+      .find({})
+      .toArray();
   });
 
   afterAll(async () => {
@@ -17,12 +24,6 @@ describe('migration replace_dictionary_with_thesaurus', () => {
   });
 
   it('should update the correct translations', async () => {
-    await migration.up(testingDB.mongodb);
-    const translations = await testingDB.mongodb
-      .collection('translations')
-      .find({})
-      .toArray();
-
     let contexts = [];
     translations.forEach(translation => {
       contexts = contexts.concat(translation.contexts);
@@ -30,12 +31,6 @@ describe('migration replace_dictionary_with_thesaurus', () => {
     expect(contexts.filter(context => context.type === 'Thesaurus').length).toEqual(2);
   });
   it('should not update the translations that have no Dictionary context', async () => {
-    await migration.up(testingDB.mongodb);
-    const translations = await testingDB.mongodb
-      .collection('translations')
-      .find({})
-      .toArray();
-
     let contexts = [];
     translations.forEach(translation => {
       contexts = contexts.concat(translation.contexts);
