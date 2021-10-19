edc70584b38f943bb0c59b4f7860ada45e9f23e7
mfacar <mercy@huridocs.org>
2021-10-19 10:50:22 -0500

    reverts system translations migration

--- app/api/migrations/migrations/56-add_system_key_translations/index.js
+++ app/api/migrations/migrations/56-add_system_key_translations/index.js
@@ -1,15 +1,53 @@
-import { migrateTranslations } from 'api/migrations/systemTranslationsMigration';
+/*
+This migration is meant to be repeatable.
+After copy pasting:
+  - change the contents of system_keys.csv to the new keyset
+  - change the file location in the readCsvToSystemKeys call
+  - change the tests, if necessary
+*/
+
+// eslint-disable-next-line max-statements
+async function insertSystemKeys(db, newKeys) {
+  const translations = await db
+    .collection('translations')
+    .find()
+    .toArray();
+  const locales = translations.map(tr => tr.locale);
+
+  const locToSystemContext = {};
+  translations.forEach(tr => {
+    locToSystemContext[tr.locale] = tr.contexts.find(c => c.id === 'System');
+  });
+  const locToKeys = {};
+  Object.entries(locToSystemContext).forEach(([loc, context]) => {
+    locToKeys[loc] = new Set(context.values.map(v => v.key));
+  });
+
+  newKeys.forEach(row => {
+    const { key, optionalValue } = row;
+
+    locales.forEach(loc => {
+      if (!locToKeys[loc].has(key)) {
+        const newValue = optionalValue || key;
+        locToSystemContext[loc].values.push({ key, value: newValue });
+        locToKeys[loc].add(key);
+      }
+    });
+  });
+
+  await translations.forEach(tr => db.collection('translations').replaceOne({ _id: tr._id }, tr));
+}
 
 export default {
   delta: 56,
 
   name: 'add_system_key_translations',
 
-  description: 'Inserts system keys of added translations',
+  description: 'Adding missing translations for system keys, through importing from a csv file.',
 
   async up(db) {
     process.stdout.write(`${this.name}...\r\n`);
     const systemKeys = [{ key: 'FEATURED' }, { key: 'ALL' }];
-    await migrateTranslations(db, systemKeys);
+    await insertSystemKeys(db, systemKeys);
   },
 };
