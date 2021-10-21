914ee616e0e0ac90047feb433c5e6794d470e279
Santiago <santiago@huridocs.org>
2021-10-21 14:51:15 -0300

    test implementation for form actions

---
+++ app/react/Metadata/actions/specs/supportingFilesActions.spec.ts
@@ -0,0 +1,5 @@
+describe('upload suporting files', () => {
+  it('', () => {
+    throw new Error('should add the supporting file to the correct model');
+  });
+});
--- app/react/Metadata/actions/supportingFilesActions.ts
+++ app/react/Metadata/actions/supportingFilesActions.ts
@@ -1,21 +1,45 @@
 import { Dispatch } from 'redux';
 import { EntitySchema } from 'shared/types/entityType';
-import { FileType } from 'shared/types/fileType';
 import { actions } from 'react-redux-form';
 
-const uploadLocalAttachment = (_entity: EntitySchema, file, __reducerKey: string) => {
-  console.log('calling uploadLocalAttachment');
-  return (dispatch: Dispatch<{}>) => {
-    console.log('dispatching uploadLocalAttachment');
-    console.log('uploading: ', file);
-    dispatch(actions.push('library.sidepanel.metadata.attachments', { fileName: file.name }));
-  };
+const uploadLocalAttachment = (_entity: EntitySchema, file: File, __reducerKey: string) => (
+  dispatch: Dispatch<{}>
+) => {
+  dispatch(
+    actions.push('library.sidepanel.metadata.attachments', {
+      originalname: file.name,
+      location: URL.createObjectURL(file),
+      type: 'attachment',
+      mimetype: file.type,
+    })
+  );
 };
 
 const uploadLocalAttachmentFromUrl = (
-  _entity: EntitySchema,
-  _file: FileType,
+  entity: EntitySchema,
+  name: string,
+  url: string,
   __reducerKey: string
-) => console.log('calling uploadLocalAttachmentFromUrl');
+) => (dispatch: Dispatch<{}>) => {
+  dispatch(
+    actions.push('library.sidepanel.metadata.attachments', {
+      originalname: name,
+      url,
+      ...(entity.sharedId && { entity: entity.sharedId }),
+    })
+  );
+};
 
 export { uploadLocalAttachment, uploadLocalAttachmentFromUrl };
+
+// URL object
+// {
+//   _id: '617186495c0c2b453378497e',
+//   originalname: 'testURL',
+//   url: 'https://...',
+//   entity: 'q513ogyszi',
+//   type: 'attachment',
+//   mimetype: 'image/jpeg',
+//   creationDate: 1634829897888,
+//   __v: 0
+// }
