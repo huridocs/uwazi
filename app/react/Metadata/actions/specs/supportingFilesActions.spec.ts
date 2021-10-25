f4a9cd2dfc272c722ad5cc331debe30e1e3aafd8
Santiago <santiago@huridocs.org>
2021-10-25 11:35:45 -0300

    test for local file upload

--- app/react/Metadata/actions/specs/supportingFilesActions.spec.ts
+++ app/react/Metadata/actions/specs/supportingFilesActions.spec.ts
@@ -1,12 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
 import { Dispatch } from 'redux';
 import { actions as formActions } from 'react-redux-form';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import { ClientEntitySchema } from 'app/istore';
 
-import { uploadLocalAttachmentFromUrl } from '../supportingFilesActions';
+import { uploadLocalAttachment, uploadLocalAttachmentFromUrl } from '../supportingFilesActions';
 
 const entity: ClientEntitySchema = {
   _id: 'entity._id',
 };
+
+const file = new File(['testFile'], 'testFile.txt', {
+  type: 'text/plain',
+});
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore({});
 
 describe('upload supporting files', () => {
   let dispatch: Dispatch<{}>;
@@ -17,7 +31,7 @@ describe('upload supporting files', () => {
   });
 
   describe('uploadLocalAttachmentFromUrl', () => {
-    it('should dispatch the action to update en form', () => {
+    it('should dispatch the action to update the form', () => {
       const action = uploadLocalAttachmentFromUrl(
         entity,
         { url: 'https://test.com', name: 'myURL' },
@@ -29,6 +43,20 @@ describe('upload supporting files', () => {
         originalname: 'myURL',
         url: 'https://test.com',
       });
+    });
+  });
+
+  describe('uploadLocalAttachment', () => {
+    it('should dispatch the action to update the form', () => {
+      const action = uploadLocalAttachment(entity, file, 'reducerKey', 'model.metadata');
+      action(dispatch);
+      // expect(formActions.push).toHaveBeenCalledWith('metadata.model.attachments', {
+      //   originalname: 'myURL',
+      //   url: 'https://test.com',
+      // });
+
+      console.log('getActions: ', store.getActions());
+      console.log('getState: ', store.getState());
     });
   });
 });
