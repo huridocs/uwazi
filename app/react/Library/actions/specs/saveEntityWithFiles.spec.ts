e9992a5aee7e6abc1d1921e962576e6043bd7a5c
Santiago <santiago@huridocs.org>
2021-10-26 13:03:20 -0300

    tidying up

--- app/react/Library/actions/specs/saveEntityWithFiles.spec.ts
+++ app/react/Library/actions/specs/saveEntityWithFiles.spec.ts
@@ -21,40 +21,45 @@ describe('saveEntityWithFiles', () => {
     return mockUpload;
   };
 
-  it('should save the entity with attached files', async () => {
-    const file = new File([Buffer.from('Hello World').toString('base64')], 'testFile.txt', {
-      type: 'text/plain',
-    });
-    Object.assign(file, {
-      on: () => {},
-      pause: () => {},
-      resume: () => {},
-    });
-    let serializedFile = new ArrayBuffer(0);
-    await readFileAsBase64(file, info => {
-      serializedFile = info;
-    });
+  //longer content
+  it.each`
+    fileContent      | fileName        | fileType             | fileSize
+    ${'Hello world'} | ${'text.txt'}   | ${'text/plain'}      | ${12}
+    ${'Sample PDF'}  | ${'sample.pdf'} | ${'application/pdf'} | ${16}
+  `(
+    'should save the entity with attached files',
+    async ({ fileContent, fileName, fileType, fileSize }) => {
+      const file = new File([Buffer.from(fileContent).toString('base64')], fileName, {
+        type: fileType,
+      });
 
-    const entity = {
-      _id: 'entity1',
-      title: 'entity1',
-      attachments: [
-        {
-          _id: 'file1',
-          serializedFile, //'data:text/plain;base64,SGVsbG8gV29ybGQh',
-          originalname: 'file1',
-        },
-      ],
-    };
-    jest.clearAllMocks();
-    //replace by generated from a file
-    //const expectedFile = constructFile(entity.attachments[0]);
-    const mockUpload = mockSuperAgent();
-    const updatedEntity = await saveEntityWithFiles(entity);
-    expect(mockUpload.attach).toHaveBeenCalledWith(
-      'attachments[0]',
-      expect.objectContaining({ size: 12 })
-    );
-    expect(updatedEntity).toEqual({ title: 'entity1' });
-  });
+      let serializedFile = '';
+      await readFileAsBase64(file, info => {
+        serializedFile = info.toString();
+      });
+
+      const entity = {
+        _id: 'entity1',
+        title: 'entity1',
+        attachments: [
+          {
+            _id: 'file1',
+            serializedFile,
+            originalname: fileName,
+          },
+        ],
+      };
+
+      const mockUpload = mockSuperAgent();
+
+      const updatedEntity = await saveEntityWithFiles(entity);
+
+      expect(mockUpload.attach).toHaveBeenCalledWith(
+        'attachments[0]',
+        expect.objectContaining({ size: fileSize, type: fileType })
+      );
+
+      expect(updatedEntity).toEqual({ title: 'entity1' });
+    }
+  );
 });
