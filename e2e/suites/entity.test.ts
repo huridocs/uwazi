ee14c34be2f79fd84977890ea85752343857926d
Santiago <santiago@huridocs.org>
2021-10-28 16:07:38 -0300

    expanded supporting files e2e

--- e2e/suites/entity.test.ts
+++ e2e/suites/entity.test.ts
@@ -1,7 +1,5 @@
 /*global page*/
 
-import { ElementHandle } from 'puppeteer';
-import { ensure } from 'shared/tsUtils';
 import { adminLogin, logout } from '../helpers/login';
 import proxyMock from '../helpers/proxyMock';
 import insertFixtures from '../helpers/insertFixtures';
@@ -11,20 +9,6 @@ import { goToRestrictedEntities } from '../helpers/publishedFilter';
 import { refreshIndex } from '../helpers/elastichelpers';
 import { getContentBySelector } from '../helpers/selectorUtils';
 
-const { toMatchImageSnapshot } = require('jest-image-snapshot');
-
-expect.extend({ toMatchImageSnapshot });
-
-async function assertScreenshot(selector: string) {
-  const attachmentsList = ensure<ElementHandle>(await page.$(selector));
-  const chartScreenshot = await attachmentsList.screenshot();
-  expect(chartScreenshot).toMatchImageSnapshot({
-    failureThreshold: 0.01,
-    failureThresholdType: 'percent',
-    allowSizeMismatch: true,
-  });
-}
-
 async function addSupportingFile(filePath: string) {
   await expect(page).toClick('button', { text: 'Add supporting file' });
   await uploadSupportingFileToEntity(filePath);
@@ -33,21 +17,19 @@ async function addSupportingFile(filePath: string) {
 
 const createEntityWithSupportingFiles = async (
   title: string,
-  files: string[],
+  file: string,
   webAttachment: { name: string; url: string }
 ) => {
   await expect(page).toClick('button', { text: 'Create entity' });
   await expect(page).toFill('textarea[name="library.sidepanel.metadata.title"]', title);
 
-  await addSupportingFile(files[0]);
+  await addSupportingFile(file);
 
   await expect(page).toClick('button', { text: 'Add supporting file' });
   await expect(page).toClick('.tab-link', { text: 'Add from web' });
   await expect(page).toFill('.web-attachment-url', webAttachment.url);
   await expect(page).toFill('.web-attachment-name', webAttachment.name);
   await expect(page).toClick('button', { text: 'Add resource' });
-
-  await addSupportingFile(files[1]);
 
   await expect(page).toClick('button', { text: 'Save' });
 };
@@ -60,6 +42,12 @@ describe('Entities', () => {
     await disableTransitions();
   });
 
+  const entityTitle = 'Entity with supporting files';
+  const webAttachments = {
+    name: 'Resource from web',
+    url: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+  };
+
   it('Should create new entity', async () => {
     await expect(page).toClick('button', { text: 'Create entity' });
     await expect(page).toFill('textarea[name="library.sidepanel.metadata.title"]', 'Test title');
@@ -68,24 +56,53 @@ describe('Entities', () => {
   });
 
   it('Should create a new entity with attachments', async () => {
-    const entityTitle = 'Entity with supporting files';
-    const filesAttachments = [
-      `${__dirname}/test_files/valid.pdf`,
-      `${__dirname}/test_files/batman.jpg`,
-    ];
-    const webAttachments = {
-      name: 'Resource from web',
-      url: 'https://fonts.googleapis.com/icon?family=Material+Icons',
-    };
     await goToRestrictedEntities();
-    await createEntityWithSupportingFiles(entityTitle, filesAttachments, webAttachments);
+    await createEntityWithSupportingFiles(
+      entityTitle,
+      `${__dirname}/test_files/valid.pdf`,
+      webAttachments
+    );
     await refreshIndex();
     await expect(page).toClick('.item-document', {
       text: entityTitle,
     });
     const fileList = await getContentBySelector('.attachment-name span:not(.attachment-size)');
-    expect(fileList).toEqual(['batman.jpg', 'Resource from web', 'valid.pdf']);
-    await assertScreenshot('.attachments-list-parent');
+    expect(fileList).toEqual(['Resource from web', 'valid.pdf']);
+  });
+
+  it('should rename an attachment', async () => {
+    await expect(page).toClick('.item-document', {
+      text: entityTitle,
+    });
+    await expect(page).toClick('button', { text: 'Edit' });
+    await expect(page).toFill(
+      'input[name="library.sidepanel.metadata.attachments.0.originalname"]',
+      'My PDF.pdf'
+    );
+    await expect(page).toClick('button', { text: 'Save' });
+    await refreshIndex();
+    await expect(page).toClick('.item-document', {
+      text: entityTitle,
+    });
+    const fileList = await getContentBySelector('.attachment-name span:not(.attachment-size)');
+    expect(fileList).toEqual(['My PDF.pdf', 'Resource from web']);
+  });
+
+  it('should delete the first attachment', async () => {
+    await expect(page).toClick('.item-document', {
+      text: entityTitle,
+    });
+    await expect(page).toClick('button', { text: 'Edit' });
+
+    await expect(page).toClick('.delete-supporting-file');
+
+    await expect(page).toClick('button', { text: 'Save' });
+    await refreshIndex();
+    await expect(page).toClick('.item-document', {
+      text: entityTitle,
+    });
+    const fileList = await getContentBySelector('.attachment-name span:not(.attachment-size)');
+    expect(fileList).toEqual(['Resource from web']);
   });
 
   afterAll(async () => {
