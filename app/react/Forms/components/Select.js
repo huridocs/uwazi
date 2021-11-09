55eeaa78e79f6b2482cc601ca2b79b163c487b23
Kevin <nderitukelvin19@gmail.com>
2021-11-09 21:53:06 +0300

    Added empty line after import

--- app/react/Forms/components/Select.js
+++ app/react/Forms/components/Select.js
@@ -4,6 +4,7 @@ import { advancedSort } from 'app/utils/advancedSort';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { t } from 'app/I18N';
+
 export default class Select extends Component {
   render() {
     const { options, optionsValue, optionsLabel, placeholder, sort } = this.props;
