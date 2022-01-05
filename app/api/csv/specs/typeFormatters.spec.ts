1ffb53f40def29a8865b598e75cd8addb667b5f6
Sergio Revilla <sergio@huridocs.org>
2022-01-05 13:26:35 +0100

    Fix linting

--- app/api/csv/specs/typeFormatters.spec.ts
+++ app/api/csv/specs/typeFormatters.spec.ts
@@ -190,15 +190,30 @@ describe('csvExporter typeFormatters', () => {
       expect(singleValue).toBe('Entity 1');
       expect(multipleValue).toBe('Entity 1|Entity 2');
       testEmptyField(typeFormatters.relationship);
-    })
+    });
 
     it('should return the inherited entity value when has inherited value', () => {
       const singleField = [
-        { label: 'Entity 1', value: null, inheritedValue: [{ value: 'E1' }], inheritedType: 'text' },
+        {
+          label: 'Entity 1',
+          value: null,
+          inheritedValue: [{ value: 'E1' }],
+          inheritedType: 'text',
+        },
       ];
       const multipleField = [
-        { label: 'Entity 1', value: null, inheritedValue: [{ value: 'E1' }], inheritedType: 'text' },
-        { label: 'Entity 2', value: null, inheritedValue: [{ value: 'E2' }], inheritedType: 'text' },
+        {
+          label: 'Entity 1',
+          value: null,
+          inheritedValue: [{ value: 'E1' }],
+          inheritedType: 'text',
+        },
+        {
+          label: 'Entity 2',
+          value: null,
+          inheritedValue: [{ value: 'E2' }],
+          inheritedType: 'text',
+        },
       ];
 
       const singleValue = typeFormatters.relationship(singleField, {});
@@ -208,8 +223,7 @@ describe('csvExporter typeFormatters', () => {
       expect(multipleValue).toBe('E1|E2');
       testEmptyField(typeFormatters.relationship);
     });
-  })
-
+  });
 
   describe('FILES', () => {
     it('should return the correct DOCUMENTS value', () => {
