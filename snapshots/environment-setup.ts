c95c90d8e9ee73f973bc2284b17c1abf6554871f
Kevin <nderitukelvin19@gmail.com>
2022-01-31 16:40:18 +0300

    Changed comparison method

--- snapshots/environment-setup.ts
+++ snapshots/environment-setup.ts
@@ -3,6 +3,7 @@ import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   failureThreshold: 0.07,
   failureThresholdType: 'percent',
+  comparisonMethod: 'ssim',
   allowSizeMismatch: true,
 });
 
