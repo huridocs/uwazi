45f25faa9655e7707603d360f2c3bbcbbd470d35
Daneryl <daneryl@gmail.com>
2022-02-09 11:15:46 +0100

    client sentry dsn provided by SSR window variable

--- app/react/index.js
+++ app/react/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import * as Sentry from '@sentry/react';
+
 import { BrowserTracing } from '@sentry/tracing';
 
 import { AppContainer } from 'react-hot-loader';
 
-import { SENTRY_APP_DSN } from 'app/config';
 import App from './App';
 
 import './App/sockets';
 
-if (SENTRY_APP_DSN) {
+if (window.SENTRY_APP_DSN) {
   Sentry.init({
-    dsn: SENTRY_APP_DSN,
+    dsn: window.SENTRY_APP_DSN,
     integrations: [new BrowserTracing()],
     tracesSampleRate: 0.1,
   });
