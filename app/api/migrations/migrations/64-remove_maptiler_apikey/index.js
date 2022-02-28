8c7d6f7cf71c42fa2fc7ca6d4c66bb45d6548ca7
Alberto Casado Torres <acasadotorres@gmail.com>
2022-02-28 14:13:14 +0100

    migration to remove the obsolete mapTilerKey

---
+++ app/api/migrations/migrations/64-remove_maptiler_apikey/index.js
@@ -0,0 +1,14 @@
+export default {
+  delta: 64,
+
+  name: 'remove maptiler apikey',
+
+  description: 'remove maptiler apikey',
+
+  reindex: false,
+
+  async up(db) {
+    process.stdout.write(`${this.name}...\r\n`);
+    await db.collection('settings').updateMany({}, { $unset: { mapTilerKey: 1 } });
+  },
+};
