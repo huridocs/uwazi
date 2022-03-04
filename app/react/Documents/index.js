11401d8a12659cab2bbfea9d0c653049147f9e30
Santiago <santiago@huridocs.org>
2022-03-04 15:32:23 -0300

    revert incorrect change from commit eacb068c24d958799fec79c3696580d93fe6f1d5

--- app/react/Documents/index.js
+++ app/react/Documents/index.js
@@ -1,6 +1,6 @@
 import documentsApi from './DocumentsAPI';
 import TocForm from './components/TocForm';
 import ShowToc from './components/ShowToc';
-import { DocumentSidePanel } from './components/DocumentSidePanel';
+import DocumentSidePanel from './components/DocumentSidePanel';
 
 export { documentsApi, TocForm, ShowToc, DocumentSidePanel };
