f50fff57613a958dcdb5c6ec4213632589d5d8bf
Alberto Casado Torres <acasadotorres@gmail.com>
2022-03-11 12:54:58 +0100

    Moved MapBox to first option as it is the default when no value is selected

--- app/react/Settings/components/CollectionSettings.tsx
+++ app/react/Settings/components/CollectionSettings.tsx
@@ -316,8 +316,8 @@ const CollectionSettings = ({
         <SettingsFormElement label="Map provider">
           <div className="col-xs-12 col-lg-3 col-no-gutters">
             <select name="tilesProvider" className="form-control" ref={register}>
-              <option value="google">{t('System', 'Google Maps', null, false)}</option>
               <option value="mapbox">{t('System', 'MapBox', null, false)}</option>
+              <option value="google">{t('System', 'Google Maps', null, false)}</option>
             </select>
           </div>
         </SettingsFormElement>
