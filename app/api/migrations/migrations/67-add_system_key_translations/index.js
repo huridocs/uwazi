85218e593b7512aa569002f834b9ca77267d673b
Alberto Casado Torres <acasadotorres@gmail.com>
2022-03-13 22:02:41 +0100

    Updated migration delta

--- app/api/migrations/migrations/67-add_system_key_translations/index.js
+++ app/api/migrations/migrations/67-add_system_key_translations/index.js
@@ -46,7 +46,7 @@ async function readCsvToSystemKeys(db, filename) {
 }
 
 export default {
-  delta: 64,
+  delta: 67,
 
   reindex: false,
 
@@ -59,7 +59,7 @@ export default {
 
     await readCsvToSystemKeys(
       db,
-      'app/api/migrations/migrations/64-add_system_key_translations/system_keys.csv'
+      'app/api/migrations/migrations/67-add_system_key_translations/system_keys.csv'
     );
   },
 };
--- app/api/migrations/migrations/67-add_system_key_translations/specs/67-add_system_key_translations.spec.js
+++ app/api/migrations/migrations/67-add_system_key_translations/specs/67-add_system_key_translations.spec.js
@@ -31,7 +31,7 @@ describe('migration add_system_key_translations', () => {
   });
 
   it('should have a delta number', () => {
-    expect(migration.delta).toBe(64);
+    expect(migration.delta).toBe(67);
   });
 
   it('should append new keys, leave existing keys intact.', async () => {
