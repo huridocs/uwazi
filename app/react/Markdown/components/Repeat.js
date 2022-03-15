3578c1186b5ebef4f5a9b40a97a40044f4f549f7
Kevin <nderitukelvin19@gmail.com>
2022-03-15 15:58:42 +0300

    Reverted utils and only extracted required function

--- app/react/Markdown/components/Repeat.js
+++ app/react/Markdown/components/Repeat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PagesContext from './Context';
-import { objectPath } from '../utils.tsx';
+import { objectPath } from '../utils';
 
 class Repeat extends Component {
   render() {
