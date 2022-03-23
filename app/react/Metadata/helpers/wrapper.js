57a2adad9c3839df37ecda94a9ba10025ed81b49
Mercy <mercy@huridocs.org>
2022-03-23 16:55:14 -0500

    fix names for file process

--- app/react/Metadata/helpers/wrapper.js
+++ app/react/Metadata/helpers/wrapper.js
@@ -2,23 +2,23 @@ export function wrapEntityMetadata(entity) {
   if (!entity.metadata) {
     return { ...entity };
   }
-  const newUploads = (entity.attachments || [])
+  const newFileMetadataValues = (entity.attachments || [])
     .filter(attachment => attachment.fileLocalID)
     .reduce(
-      (indices, attachment) => ({
-        ...indices,
-        [attachment.fileLocalID]: { value: '', attachment: entity.attachments.indexOf(attachment) },
+      (previousValue, attachment, index) => ({
+        ...previousValue,
+        [attachment.fileLocalID]: { value: '', attachment: index },
       }),
       {}
     );
 
   const metadata = Object.keys(entity.metadata).reduce((wrappedMo, key) => {
-    const imageIndex = newUploads[entity.metadata[key]];
+    const newFileMetadataValue = newFileMetadataValues[entity.metadata[key]];
     return {
       ...wrappedMo,
       [key]: Array.isArray(entity.metadata[key])
         ? entity.metadata[key].map(v => ({ value: v }))
-        : [imageIndex || { value: entity.metadata[key] }],
+        : [newFileMetadataValue || { value: entity.metadata[key] }],
     };
   }, {});
   // suggestedMetadata is always in metadata-object form.
