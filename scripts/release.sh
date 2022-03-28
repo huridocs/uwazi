a5e03806937da8427fb9a94fd00d6518e8cea0c9
Daneryl <daneryl@gmail.com>
2022-03-28 11:03:31 +0200

    automatic release notes

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-server=${1}
-ssh_user=${2}
+# server=${1}
+# ssh_user=${2}
 gh_token=${3}
 
 release_version="$(yarn version | grep version: | cut -d" " -f4)"
+previous_tag="$(git describe --tags "$(git rev-list --tags --max-count=10)" | grep -v "\-rc" | head -n1)"
+release_notes="$(git log --oneline "$previous_tag".. | grep -v Merge | grep "(.*)" | cut -d" " -f2- |  awk '{print "* " $0}')"
 
 tar -czf uwazi.tgz ./prod
 
 GITHUB_TOKEN="$gh_token" gh release create "$release_version"\
   --title "$release_version"\
-  --notes "Release notes"\
+  --notes "## What's changed\n\n$release_notes\n\n**Full Changelog**: https://github.com/huridocs/uwazi/compare/$previous_tag...$release_version"\
   --target production\
   uwazi.tgz
 
