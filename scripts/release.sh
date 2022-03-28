c31fc7b305f1693f11f517b8a9fc0915fda11bce
Daneryl <daneryl@gmail.com>
2022-03-28 11:34:09 +0200

    fix release notes production

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,14 +5,16 @@
 gh_token=${3}
 
 release_version="$(yarn version | grep version: | cut -d" " -f4)"
-previous_tag="$(git describe --tags "$(git rev-list --tags --max-count=10)" | grep -v "\-rc" | head -n1)"
+previous_tag="$(git tag -l --sort=committerdate | grep -v "\-rc" | tail -n1)"
 release_notes="$(git log --oneline "$previous_tag".. | grep -v Merge | grep "(.*)" | cut -d" " -f2- |  awk '{print "* " $0}')"
+
+echo -e "## What's changed\n\n$release_notes\n\n**Full Changelog**: https://github.com/huridocs/uwazi/compare/$previous_tag...$release_version" > release_notes.txt
 
 tar -czf uwazi.tgz ./prod
 
 GITHUB_TOKEN="$gh_token" gh release create "$release_version"\
   --title "$release_version"\
-  --notes "## What's changed\r\n\r\n$release_notes\r\n\r\n**Full Changelog**: https://github.com/huridocs/uwazi/compare/$previous_tag...$release_version"\
+  --notes-file release_notes.txt\
   --target production\
   uwazi.tgz
 
