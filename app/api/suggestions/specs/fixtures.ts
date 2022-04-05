cbe1b100dfaac705c865b308910f2300eccd3031
Kevin <nderitukelvin19@gmail.com>
2022-04-05 09:37:26 +0300

    Added red tests

--- app/api/suggestions/specs/fixtures.ts
+++ app/api/suggestions/specs/fixtures.ts
@@ -1,4 +1,8 @@
+/* eslint-disable max-lines */
+import { getFixturesFactory } from 'api/utils/fixturesFactory';
 import { testingDB, DBFixture } from 'api/utils/testing_db';
+
+const factory = getFixturesFactory();
 
 const shared2enId = testingDB.id();
 const shared6enId = testingDB.id();
@@ -8,6 +12,7 @@ const heroTemplateId = testingDB.id();
 
 const suggestionSharedId6Title = testingDB.id();
 const suggestionSharedId6Enemy = testingDB.id();
+const suggestionSharedId6EnemyEs = testingDB.id();
 
 const fixtures: DBFixture = {
   settings: [
@@ -23,6 +28,16 @@ const fixtures: DBFixture = {
     {
       entityId: 'shared1',
       propertyName: 'title',
+      suggestedValue: 'Red Robin',
+      segment: 'Red Robin, a variation on the traditional Robin persona.',
+      language: 'en',
+      date: 5,
+      page: 2,
+      status: 'ready',
+    },
+    {
+      entityId: 'shared1',
+      propertyName: 'enemy',
       suggestedValue: 'Red Robin',
       segment: 'Red Robin, a variation on the traditional Robin persona.',
       language: 'en',
@@ -152,6 +167,17 @@ const fixtures: DBFixture = {
       page: 3,
       status: 'ready',
     },
+    {
+      _id: suggestionSharedId6EnemyEs,
+      entityId: 'shared6',
+      propertyName: 'enemy',
+      suggestedValue: '',
+      segment: 'Enemy: Batman',
+      language: 'es',
+      date: 5,
+      page: 3,
+      status: 'ready',
+    },
   ],
   entities: [
     {
@@ -159,6 +185,7 @@ const fixtures: DBFixture = {
       sharedId: 'shared1',
       title: 'Robin',
       language: 'en',
+      metadata: { enemy: [{ value: 'Red Robin' }] },
       template: personTemplateId,
     },
     {
@@ -239,6 +266,26 @@ const fixtures: DBFixture = {
       metadata: { enemy: [{ value: '' }], age: [{ value: 40 }] },
       template: heroTemplateId,
     },
+  ],
+  files: [
+    factory.file('F2', 'shared2', 'document', 'documentB.pdf', 'eng', '', [
+      {
+        name: 'super_powers',
+        selection: {
+          text: 'scientific knowledge',
+          selectionRectangles: [{ top: 0, left: 0, width: 0, height: 0, page: '1' }],
+        },
+      },
+    ]),
+    factory.file('F3', 'shared2', 'document', 'documentC.pdf', 'spa', '', [
+      {
+        name: 'super_powers',
+        selection: {
+          text: 'conocimiento científico',
+          selectionRectangles: [{ top: 0, left: 0, width: 0, height: 0, page: '1' }],
+        },
+      },
+    ]),
   ],
   templates: [
     {
