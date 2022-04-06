b9ed01f0358a5f2f92f34e9b8a865fd04cafbd70
Santiago <santiago@huridocs.org>
2022-04-06 10:14:08 -0300

    metadata spec part 2

--- e2e/suites/metadata.spec.ts
+++ e2e/suites/metadata.spec.ts
@@ -1,7 +1,9 @@
+import { waitForNavigation } from '../helpers/formActions';
 import disableTransitions from '../helpers/disableTransitions';
 import insertFixtures from '../helpers/insertFixtures';
-import { adminLogin } from '../helpers/login';
+import { adminLogin, logout } from '../helpers/login';
 import proxyMock from '../helpers/proxyMock';
+import { host } from '../config';
 
 describe('Metadata', () => {
   beforeAll(async () => {
@@ -13,7 +15,8 @@ describe('Metadata', () => {
 
   describe('Thesauri tests', () => {
     it('should create a new thesaurus with two values', async () => {
-      await expect(page).toClick('a', { text: 'Account settings' });
+      await waitForNavigation(expect(page).toClick('a', { text: 'Account settings' }));
+      expect(page.url()).toBe(`${host}/en/settings/account`);
       await expect(page).toClick('a', { text: 'Thesauri' });
       await expect(page).toClick('a', { text: 'Add thesaurus' });
       await expect(page).toFill('input[name="thesauri.data.name"', 'New thesaurus');
@@ -47,6 +50,7 @@ describe('Metadata', () => {
       await expect(page).toClick(
         '.thesauri-list > table > tbody > tr:nth-child(4) > td:nth-child(3) > div > button'
       );
+      await page.waitForSelector('body > div:nth-child(9) > div > div > div > div.modal-body');
       await expect(page).toMatchElement(
         'body > div:nth-child(9) > div > div > div > div.modal-body > h4',
         {
@@ -58,20 +62,42 @@ describe('Metadata', () => {
   });
 
   describe('Templates tests', () => {
-    it('should click Template button and then click on add new template button', () => {});
+    it('should create a new template with no properties added', async () => {
+      await waitForNavigation(expect(page).toClick('a', { text: 'Account settings' }));
+      expect(page.url()).toBe(`${host}/en/settings/account`);
+      await expect(page).toClick('a', { text: 'Templates' });
+      await expect(page).toClick('a', { text: 'Add template' });
+      await expect(page).toFill('input[name="template.data.name"', 'My template');
+      await expect(page).toClick('button', { text: 'Save' });
+      await expect(page).toClick('.alert.alert-success');
+    });
 
-    it('should create a new template with no properties added', () => {});
+    it('should go back and then edit the created template', async () => {
+      await expect(page).toClick('a', { text: 'Templates' });
+      await expect(page).toClick('a', { text: 'My template' });
+      await expect(page).toFill('input[name="template.data.name"', 'My edited template');
+      await expect(page).toClick('.panel-body > div > aside > div > ul > li:nth-child(1) > button');
+      await expect(page).toClick('button', { text: 'Save' });
+      await expect(page).toClick('.alert.alert-success');
+    });
 
-    it('should go back and then edit the created template', () => {});
-
-    it('should go back to Template then delete the created template', () => {});
+    it('should go back to Template then delete the created template', async () => {
+      await expect(page).toClick('a', { text: 'Templates' });
+      await expect(page).toClick(
+        '.settings-content > div > ul > li:nth-child(6) > div > button.btn.btn-danger.btn-xs.template-remove'
+      );
+      await page.waitForSelector('body > div:nth-child(9) > div > div > div > div.modal-body');
+      await expect(page).toMatchElement(
+        'body > div:nth-child(9) > div > div > div > div.modal-body > h4',
+        {
+          text: 'Confirm delete of template: My edited template',
+        }
+      );
+      await expect(page).toClick('button', { text: 'Accept' });
+    });
   });
 
-  describe('Connections tests', () => {
-    it('should create a new connection', () => {});
-
-    it('should go back to Connections then edit the created connection', () => {});
-
-    it('should go back to connections then delete the created connection', () => {});
+  afterAll(async () => {
+    await logout();
   });
 });
