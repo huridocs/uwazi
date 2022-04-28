b5c3cc0ad6b2d45627512fdbbcccaa8cdd2e9623
Santiago <borrajosantiago@gmail.com>
2022-04-28 11:52:18 -0300

    changed validate parameters

--- app/api/thesauri/validateThesauri.ts
+++ app/api/thesauri/validateThesauri.ts
@@ -1,25 +1,15 @@
-/** @format */
 import Ajv from 'ajv';
 import ajvKeywords from 'ajv-keywords';
 import { wrapValidator } from 'shared/tsUtils';
 import { thesaurusSchema } from 'shared/types/thesaurusSchema';
 import { ThesaurusSchema } from 'shared/types/thesaurusType';
-
 import model from './dictionariesModel';
-
-/* eslint-disable max-params */
 
 const ajv = ajvKeywords(Ajv({ allErrors: true }), ['uniqueItemProperties']);
 
 ajv.addKeyword('uniqueName', {
   async: true,
-  validate: async (
-    _config: any,
-    _value: any,
-    _propertySchema: any,
-    _property: any,
-    thesauri: ThesaurusSchema
-  ) => {
+  validate: async (_config: any, thesauri: ThesaurusSchema) => {
     const [duplicated] = await model.get({
       _id: { $ne: thesauri._id },
       name: new RegExp(`^${thesauri.name}$` || '', 'i'),
