6f3ce289da366796746f2aec496a6ef56915b0da
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-04-28 11:42:41 +0200

    codeclimate complexity refactor

--- app/shared/dataUtils.ts
+++ app/shared/dataUtils.ts
@@ -1,25 +1,32 @@
+const arrayDeepEquals = (a: Array<any>, b: Array<any>): boolean => {
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i += 1) {
+    if (!deepEquals(a[i], b[i])) return false;
+  }
+  return true;
+};
+
+const objectDeepEquals = (a: any, b: any): boolean => {
+  const aKeys = Object.keys(a).sort();
+  const bKeys = Object.keys(b).sort();
+  if (!arrayDeepEquals(aKeys, bKeys)) return false;
+
+  for (let i = 0; i < aKeys.length; i += 1) {
+    const key = aKeys[i];
+    if (!deepEquals(a[key], b[key])) return false;
+  }
+  return true;
+};
+
 const deepEquals = (a: any, b: any): boolean => {
   if (Array.isArray(a) || Array.isArray(b)) {
     if (Array.isArray(a) !== Array.isArray(b)) return false;
-    if (a.length !== b.length) return false;
-    for (let i = 0; i < a.length; i += 1) {
-      if (!deepEquals(a[i], b[i])) return false;
-    }
-    return true;
+    return arrayDeepEquals(a, b);
   }
 
   if (a && b && (typeof a === 'object' || typeof b === 'object')) {
     if ((typeof a === 'object') !== (typeof b === 'object')) return false;
-
-    const aKeys = Object.keys(a).sort();
-    const bKeys = Object.keys(b).sort();
-    if (!deepEquals(aKeys, bKeys)) return false;
-
-    for (let i = 0; i < aKeys.length; i += 1) {
-      const key = aKeys[i];
-      if (!deepEquals(a[key], b[key])) return false;
-    }
-    return true;
+    return objectDeepEquals(a, b);
   }
 
   return a === b;
