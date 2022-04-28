ad511036f50cad299206ffc66ef11dab47549503
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-04-28 09:34:12 +0200

    added smoke test for template structural change bug

--- app/api/templates/specs/reindex.spec.js
+++ app/api/templates/specs/reindex.spec.js
@@ -1,9 +1,14 @@
 import db from 'api/utils/testing_db';
 import { search } from 'api/search';
 import { propertyTypes } from 'shared/propertyTypes';
-import templates from '../templates';
+import fixtures, {
+  pageSharedId,
+  propertyToBeInherited2,
+  templateInheritingFromAnother,
+  templateWithContents,
+} from './fixtures/fixtures';
 import { checkIfReindex } from '../reindex';
-import fixtures, { templateWithContents, pageSharedId } from './fixtures/fixtures';
+import templates from '../templates';
 
 const getAndUpdateTemplate = async props => {
   const [template] = await templates.get({ _id: templateWithContents });
@@ -123,6 +128,14 @@ describe('reindex', () => {
         await templates.save(template, 'en', reindex);
         expect(search.indexEntities).not.toHaveBeenCalled();
       });
+
+      // eslint-disable-next-line jest/no-focused-tests
+      fit('should not find structural changes on unchanged inherit', async () => {
+        const [template] = await templates.get({ _id: templateInheritingFromAnother });
+        const reindex = await checkIfReindex(template);
+
+        expect(reindex).toEqual(false);
+      });
     });
     describe('commonProperties', () => {
       it('should not reindex if priority sorting has changed', async () => {
@@ -168,6 +181,16 @@ describe('reindex', () => {
 
         await templates.save(template, 'en', reindex);
         expect(search.indexEntities).toHaveBeenCalled();
+      });
+
+      // eslint-disable-next-line jest/no-focused-tests
+      fit('should find structural changes on changed inherit', async () => {
+        const [template] = await templates.get({ _id: templateInheritingFromAnother });
+        template.properties[0].inherit = {
+          property: propertyToBeInherited2.toString(),
+          type: 'text',
+        };
+        expect(await checkIfReindex(template)).toEqual(true);
       });
     });
     describe('commonProperty', () => {
