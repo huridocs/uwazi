faf45a4607e497f049161980057e57d2f0faea68
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-05-10 08:55:36 +0200

    test outlines

--- app/api/migrations/migrations/75-remove_duplicate_thesauri_entries/specs/75-remove_duplicate_thesauri_entries.spec.js
+++ app/api/migrations/migrations/75-remove_duplicate_thesauri_entries/specs/75-remove_duplicate_thesauri_entries.spec.js
@@ -3,9 +3,12 @@ import migration from '../index.js';
 import fixtures from './fixtures.js';
 
 describe('migration remove_duplicate_thesauri_entries', () => {
+  let savedThesaurus;
+
   beforeEach(async () => {
-    spyOn(process.stdout, 'write');
+    // spyOn(process.stdout, 'write');
     await testingDB.clearAllAndLoad(fixtures);
+    await migration.up();
   });
 
   afterAll(async () => {
@@ -16,13 +19,25 @@ describe('migration remove_duplicate_thesauri_entries', () => {
     expect(migration.delta).toBe(75);
   });
 
-  it('should not fail', async () => {
-    await migration.up();
+  it('should remove duplicated thesauri entries from root and groups', async () => {
     fail('TODO');
   });
 
-  it('should check if a reindex is needed', async () => {
-    expect(migration.reindex).toBe(undefined);
+  it.each([
+    { case: 'select' },
+    { case: 'multiselect' },
+    { case: 'inherited select' },
+    { case: 'inherited multiselect' },
+  ])('should point $case properties to remaining value', () => {
     fail('TODO');
+  });
+
+  it('should reindex if there were changed entities', async () => {
+    expect(migration.reindex).toBe(true);
+  });
+
+  it('should reindex if there were no changes to entities', async () => {
+    fail('TODO');
+    expect(migration.reindex).toBe(true);
   });
 });
