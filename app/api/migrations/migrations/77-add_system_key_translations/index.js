a58a8374f60ec0b773f939ec0e86ce212367bc67
Mercy <mercy@huridocs.org>
2022-05-11 11:40:27 -0500

    increases delta number

--- app/api/migrations/migrations/77-add_system_key_translations/index.js
+++ app/api/migrations/migrations/77-add_system_key_translations/index.js
@@ -46,7 +46,7 @@ async function readCsvToSystemKeys(db, filename) {
 }
 
 export default {
-  delta: 76,
+  delta: 77,
 
   reindex: false,
 
@@ -59,7 +59,7 @@ export default {
 
     await readCsvToSystemKeys(
       db,
-      'app/api/migrations/migrations/76-add_system_key_translations/system_keys.csv'
+      'app/api/migrations/migrations/77-add_system_key_translations/system_keys.csv'
     );
   },
 };
--- app/api/migrations/migrations/77-add_system_key_translations/specs/77-add_system_key_translations.spec.js
+++ app/api/migrations/migrations/77-add_system_key_translations/specs/77-add_system_key_translations.spec.js
@@ -107,7 +107,7 @@ describe('migration add_system_key_translations', () => {
   });
 
   it('should have a delta number', () => {
-    expect(migration.delta).toBe(76);
+    expect(migration.delta).toBe(77);
   });
 
   it('should append new keys, leave existing keys intact.', async () => {
