a38f57dd4c89e8315bc5ff82d84f0f3ad75c8b4d
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-05-25 11:50:37 +0200

    added flow notes to getState

--- app/api/suggestions/getState.ts
+++ app/api/suggestions/getState.ts
@@ -1,6 +1,12 @@
 import { SuggestionState } from 'shared/types/suggestionSchema';
 import { IXSuggestionType } from 'shared/types/suggestionType';
 
+//update flows:
+// - suggestions (i.e. messages from the service - convention: suggestions are unique for file+property)
+// - entity (match through entityId)
+// - file (match through fileId)
+// - related model (match by propertyName)
+
 export const getState = (
   suggestion: IXSuggestionType,
   modelCreationDate: number,
