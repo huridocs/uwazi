888a392f383da4044edf08a6e6728f62eaa41b05
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-05-26 09:55:52 +0200

    disableTransitions on proper order

--- e2e/suites/metadataExtraction.test.ts
+++ e2e/suites/metadataExtraction.test.ts
@@ -4,18 +4,17 @@ import { adminLogin, logout } from '../helpers/login';
 import proxyMock from '../helpers/proxyMock';
 import insertFixtures from '../helpers/insertFixtures';
 import disableTransitions from '../helpers/disableTransitions';
-import { waitForNavigation } from '../helpers/formActions';
 
 describe('Metadata Extraction', () => {
   beforeAll(async () => {
     await insertFixtures();
     await proxyMock();
+    await adminLogin();
     await disableTransitions();
   });
 
   it('should be hidden by a feature toggle.', async () => {
-    await adminLogin();
-    await waitForNavigation(expect(page).toClick('a', { text: 'Account settings' }));
+    await expect(page).toClick('a', { text: 'Account settings' })
     await expect(page).not.toMatchElement('a', { text: 'Metadata Extraction' });
   });
 
