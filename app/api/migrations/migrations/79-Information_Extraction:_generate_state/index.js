978fddb067a86a7aa8dee6d69819f4944c4afe70
Federico Nocetti <federico@huridocs.org>
2022-06-01 20:27:48 -0300

    Draft migration to generate IX states

---
+++ app/api/migrations/migrations/79-Information_Extraction:_generate_state/index.js
@@ -0,0 +1,16 @@
+import { updateStates } from './updateState';
+
+export default {
+  delta: 79,
+
+  name: 'Information_Extraction:_generate_state',
+
+  description: 'Generates and stores the current state for existing suggestions.',
+
+  reindex: false,
+
+  async up(db) {
+    process.stdout.write(`${this.name}...\r\n`);
+    await updateStates(db, { state: { $exists: false } });
+  },
+};
---
+++ app/api/migrations/migrations/79-Information_Extraction:_generate_state/specs/79-Information_Extraction:_generate_state.spec.js
@@ -0,0 +1,26 @@
+import testingDB from 'api/utils/testing_db';
+import migration from '../index.js';
+import fixtures from './fixtures.js';
+
+describe('migration Information Extraction: generate state', () => {
+  beforeEach(async () => {
+    spyOn(process.stdout, 'write');
+    await testingDB.clearAllAndLoad(fixtures);
+  });
+
+  afterAll(async () => {
+    await testingDB.tearDown();
+  });
+
+  it('should have a delta number', () => {
+    expect(migration.delta).toBe(79);
+  });
+
+  it('should fail', async () => {
+    await migration.up();
+  });
+
+  it('should check if a reindex is needed', async () => {
+      expect(migration.reindex).toBe(undefined);
+    });
+});
