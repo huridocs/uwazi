c8351745cc6f8b3a9e262165b1672504af64cfc0
Mercy <mercy@huridocs.org>
2022-06-02 22:29:33 -0500

    e2e for translate from settings

--- e2e/suites/translate.test.ts
+++ e2e/suites/translate.test.ts
@@ -4,7 +4,6 @@ import { adminLogin, logout } from '../helpers/login';
 import proxyMock from '../helpers/proxyMock';
 import insertFixtures from '../helpers/insertFixtures';
 import disableTransitions from '../helpers/disableTransitions';
-import { host } from '../config';
 
 describe('Translations', () => {
   beforeAll(async () => {
@@ -20,6 +19,35 @@ describe('Translations', () => {
     await expect(page).toMatchElement('.live-on');
   };
 
+  const changeLanguage = async (language: string) => {
+    await expect(page).toClick('.menuNav-I18NMenu .rw-select');
+    await expect(page).toClick('.rw-popup-container li span', { text: language });
+    await page.waitForNavigation();
+    await disableTransitions();
+  };
+
+  describe('Translations from settings', () => {
+    it('should be able to translate a page', async () => {
+      await expect(page).toClick('a', { text: 'Account settings' });
+      await expect(page).toClick('span', { text: 'Translations' });
+      await expect(page).toClick('a', { text: 'Mecanismo' });
+
+      await expect(page).toFill(
+        'input[name="translationsForm,1,contexts,10,values,Mecanismo"]',
+        'Mechanism'
+      );
+      await expect(page).toClick('button', { text: 'Save' });
+      await expect(page).toMatchElement('.alert-success');
+
+      await changeLanguage('Português');
+      await expect(page).toClick('a', { text: 'Libreria' });
+      await expect(page).toClick('.multiselectItem', { text: 'Mechanism' });
+      await expect(page).toMatchElement('.item-document', {
+        text: 'Mechanism',
+      });
+    });
+  });
+
   describe('Live translate', () => {
     it('Should active live translate', async () => {
       await activateTranslation();
@@ -33,10 +61,7 @@ describe('Translations', () => {
     });
 
     it('should check the translated text', async () => {
-      await expect(page).toClick('.menuNav-I18NMenu .rw-select');
-      await expect(page).toClick('.rw-popup-container li span', { text: 'Español' });
-      await page.waitForNavigation();
-      await disableTransitions();
+      await changeLanguage('Español');
       await expect(page).toMatchElement('.translation', { text: 'Filtros' });
     });
 
@@ -47,29 +72,7 @@ describe('Translations', () => {
       await expect(page).toMatchElement('.live-off');
     });
   });
-
   afterAll(async () => {
     await logout();
   });
 });
-
-// await expect(page).toMatchElement('span.translation', {
-//   text: 'Create entity',
-// });
-// await expect(page).toClick('span.translation', {
-//   text: 'Create entity',
-// });
-
-// //edit the translation
-// await expect(page).toMatchElement('input#es');
-// await expect(page).toFill('input#es', 'Crear entidad');
-// await expect(page).toClick('button', {
-//   text: 'Submit',
-// });
-
-// //check the translation
-// await page.goto(`${host}/es/library`);
-// await expect(page).toMatchElement('span.translation', { text: 'Crear entidad' });
-
-// //restore language
-// await page.goto(`${host}/en/library`);
