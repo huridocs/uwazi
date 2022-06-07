55c2aa88b271b99d0a946528a32a821c4f42fde8
Daneryl <daneryl@gmail.com>
2022-06-07 12:12:21 +0200

    fix lint errors

--- app/react/RelationTypes/NewRelationType.js
+++ app/react/RelationTypes/NewRelationType.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import RouteHandler from 'app/App/RouteHandler';
-import { actions } from 'app/BasicReducer';
-import { editRelationType } from 'app/RelationTypes/actions/relationTypesActions';
-import api from 'app/RelationTypes/RelationTypesAPI';
 import TemplateCreator from '../Templates/components/TemplateCreator';
 
 export default class NewRelationType extends RouteHandler {
