8d34fd8cb80eb862210e47472286c7ef3987b1a4
Sergio Revilla <sergio@huridocs.org>
2022-06-08 17:37:15 +0200

    Fix import order

--- app/server.js
+++ app/server.js
@@ -20,6 +20,7 @@ import { TwitterIntegration } from 'api/services/twitterintegration/TwitterInteg
 
 import { appContextMiddleware } from 'api/utils/appContextMiddleware';
 import { requestIdMiddleware } from 'api/utils/requestIdMiddleware';
+import { tocService } from 'api/toc_generation/tocService';
 import uwaziMessage from '../message';
 import apiRoutes from './api/api';
 import privateInstanceMiddleware from './api/auth/privateInstanceMiddleware';
@@ -40,7 +41,6 @@ import { routesErrorHandler } from './api/utils/routesErrorHandler';
 import { closeSockets } from './api/socketio/setupSockets';
 import { preserveSync } from './api/services/preserve/preserveSync';
 import { startLegacyServicesNoMultiTenant } from './startLegacyServicesNoMultiTenant';
-import { tocService } from 'api/toc_generation/tocService';
 
 mongoose.Promise = Promise;
 
