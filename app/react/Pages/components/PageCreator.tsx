ad7101a692262907543f866e289ec34b4a81216e
Mercy <mercy@huridocs.org>
2022-06-24 11:38:59 -0500

    update documentation link

--- app/react/Pages/components/PageCreator.tsx
+++ app/react/Pages/components/PageCreator.tsx
@@ -118,7 +118,7 @@ class PageCreator extends Component<mappedProps> {
                   <a
                     target="_blank"
                     rel="noopener noreferrer"
-                    href="https://github.com/huridocs/uwazi/wiki/Components"
+                    href="https://uwazi.readthedocs.io/en/latest/admin-docs/designing-your-website.html"
                   >
                     <Translate>Click here</Translate>
                   </a>{' '}
