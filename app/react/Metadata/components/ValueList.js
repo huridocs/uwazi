a39d37ee1cdeac65a6e5072b72378535da841c1a
RafaPolit <rafaelpolit@gmail.com>
2022-06-29 14:28:43 -0500

    Wrapped count into a span for easier styling

--- app/react/Metadata/components/ValueList.js
+++ app/react/Metadata/components/ValueList.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 import { Icon } from '../../Layout';
 
+const composeCount = (data, key) => {
+  if (data.valueCount > 1) {
+    return (
+      <React.Fragment key={key}>
+        {data.value} <span className="item-count">{data.valueCount}</span>
+      </React.Fragment>
+    );
+  }
+  return data.value;
+};
+
 const withIcon = v =>
   v.icon ? (
     <>
       <Icon className="item-icon item-icon-center" data={v.icon} />
-      {v.value}
+      {composeCount(v, 'withIcon')}
     </>
   ) : (
-    v.value
+    composeCount(v, 'withoutIcon')
   );
 
 const interpose = (array, separator) => [].concat(...array.map(e => [separator, e])).slice(1);
@@ -26,7 +37,7 @@ const renderList = prop => (
 const renderCompact = prop =>
   prop.type === 'multidate' || prop.type === 'multidaterange'
     ? interpose(
-        prop.value.map(v => v.value),
+        prop.value.map((v, i) => composeCount(v, i)),
         <br />
       )
     : interpose(
@@ -35,17 +46,15 @@ const renderCompact = prop =>
       );
 
 const groupRepeatedValues = property =>
-  property.value
-    .reduce((results, v) => {
-      const previousValue = results.find(r => r.value === v.value);
-      if (previousValue) {
-        previousValue.valueCount += 1;
-      } else {
-        results.push({ ...v, valueCount: 1 });
-      }
-      return results;
-    }, [])
-    .map(v => ({ ...v, value: v.valueCount > 1 ? `${v.value} (${v.valueCount})` : v.value }));
+  property.value.reduce((results, v) => {
+    const previousValue = results.find(r => r.value === v.value);
+    if (previousValue) {
+      previousValue.valueCount += 1;
+    } else {
+      results.push({ ...v, valueCount: 1 });
+    }
+    return results;
+  }, []);
 
 const ValueList = ({ property, compact }) => {
   const propertyWithGroupedValues = { ...property, value: groupRepeatedValues(property) };
