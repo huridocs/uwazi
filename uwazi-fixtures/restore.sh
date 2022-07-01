7a869649feda810a3d41b167c8c1786ba0f98872
Sergio Revilla <sergio@huridocs.org>
2022-07-01 10:11:29 +0200

    Merge fixtures submodule (#4807)
    * Remove submodule
    
    * Add fixtures folder

---
+++ uwazi-fixtures/restore.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[[ -f ".env" ]] && source ".env"
+
+DB=${1:-${DATABASE_NAME:-uwazi_development}}
+HOST=${2:-${DBHOST:-127.0.0.1}}
+TRANSPILED=${3:-${TRANSPILED:-false}}
+
+echo -e "\n\nDeleting $DB database"
+mongo -host $HOST $DB --eval "db.dropDatabase()"
+mongorestore -h $HOST uwazi-fixtures/dump/uwazi_development/ --db=$DB
+
+echo "Restoring pdfs..."
+rm ./uploaded_documents/*
+cp ./uwazi-fixtures/uploaded_documents/* ./uploaded_documents/
+
+echo "Running migrations..."
+if [ $TRANSPILED = true ]; then
+  node --max-http-header-size 20000 ./prod/scripts/migrate.js
+else
+  yarn migrate
+fi
+
+echo "Reindexing..."
+if [ $TRANSPILED = true ]; then
+  node --max-http-header-size 20000 ./prod/database/reindex_elastic.js
+else
+  yarn reindex
+fi
