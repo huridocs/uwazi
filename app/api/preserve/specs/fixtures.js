96ab043bacf6ae5935e8f9dff5911d7650a13af5
Kevin Nderitu <nderitukelvin19@gmail.com>
2022-07-04 15:01:25 +0300

    Added Preserve integration main UI (#4760)
    * Added main UI
    
    * Added main UI
    
    * Update creation of templates
    
    * Added main UI
    
    * Update creation of templates
    
    * Put preserve behind a feature toggle
    
    * Added some tests
    
    * Added more tests
    
    * Reimplemented to consider multiple users
    
    * Updated tests
    
    * Updated preserve UI to match data returned by preserve service
    
    * Added a translations migration
    
    * Added masterToken field in preserveSync fixtures
    
    * Updated tests

---
+++ app/api/preserve/specs/fixtures.js
@@ -0,0 +1,43 @@
+import db from 'api/utils/testing_db';
+import { UserRole } from 'shared/types/userSchema';
+
+const userId1 = db.id();
+const userId2 = db.id();
+const templateId = db.id();
+
+export default {
+  settings: [
+    {
+      _id: db.id(),
+      site_name: 'Uwazi',
+      languages: [
+        { key: 'es', label: 'Spanish', default: true },
+        { key: 'en', label: 'English' },
+      ],
+      features: {
+        preserve: {
+          host: 'http://somehost.com',
+          masterToken: 'somedevinetoken',
+        },
+      },
+    },
+  ],
+  users: [
+    {
+      _id: userId1,
+      password: 'somehash',
+      username: 'editor',
+      role: UserRole.EDITOR,
+      email: 'some@mailer.com',
+    },
+    {
+      _id: userId2,
+      password: 'someotherpassword',
+      username: 'editor',
+      email: 'some@mailer.com',
+      role: UserRole.EDITOR,
+    },
+  ],
+};
+
+export { userId1, userId2, templateId };
---
+++ app/api/preserve/specs/preserve.spec.ts
@@ -0,0 +1,78 @@
+import { testingEnvironment } from 'api/utils/testingEnvironment';
+import settings from 'api/settings';
+import templates from 'api/templates';
+import thesauri from 'api/thesauri';
+import { DBFixture } from 'api/utils/testing_db';
+import request from 'shared/JSONRequest';
+import { PreserveConfig } from 'shared/types/settingsType.js';
+import fixtures, { userId1, userId2 } from './fixtures.js';
+import { Preserve } from '../preserve';
+
+describe('Preserve', () => {
+  const user = { _id: userId1 };
+  beforeAll(async () => {
+    spyOn(request, 'post').and.callFake(() => ({
+      json: { data: { token: 'sometoken' } },
+    }));
+    await testingEnvironment.setUp(fixtures as DBFixture, 'preserve-index');
+  });
+  afterAll(async () => {
+    await testingEnvironment.tearDown();
+  });
+
+  describe('setup()', () => {
+    describe('pass', () => {
+      it('should create a thesauri, template and a config when no config is found.', async () => {
+        await Preserve.setup('en', user);
+        const savedSettings: any = await settings.get({});
+        const configs: PreserveConfig['config'] = savedSettings.features.preserve.config;
+        const config = configs.find(conf => conf.user!.toString() === user._id.toString());
+        expect(config?.template).toBeDefined();
+        const template = await templates.getById(config!.template.toString());
+        expect(template?.name).toBe('Preserve');
+        expect(template?.properties).toMatchObject([
+          {
+            type: 'link',
+            name: 'url',
+            label: 'Url',
+          },
+          {
+            type: 'select',
+            name: 'source',
+            label: 'Source',
+          },
+        ]);
+        const thesauriId = template?.properties?.find(prop => prop.type === 'select')?.content;
+        const thesaurus = await thesauri.getById(thesauriId);
+        expect(thesaurus?.name).toBe('Preserve');
+      });
+
+      it('should not create template if another configs exists in the DB', async () => {
+        const savedTemplates = await templates.get({});
+        await Preserve.setup('en', { _id: userId2 });
+        const templatesAfterSetup = await templates.get({});
+        expect(savedTemplates.length).toEqual(templatesAfterSetup.length);
+        const savedSettings: any = await settings.get({});
+        const savedConfigs = savedSettings.features.preserve.config;
+        expect(savedConfigs[0].template).toEqual(savedConfigs[1].template);
+      });
+    });
+
+    describe('fail', () => {
+      it('should throw an error if preserve feature is not configured', async () => {
+        const newSettings = [
+          {
+            ...fixtures.settings,
+            features: {},
+          },
+        ];
+        await testingEnvironment.setUp({ ...fixtures, settings: newSettings }, 'preserve-index');
+
+        await expect(Preserve.setup('en', { _id: 'someid' })).rejects.toEqual({
+          message: 'Preserve configuration not found',
+          code: 402,
+        });
+      });
+    });
+  });
+});
