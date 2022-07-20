4191704ef57cb023f0b9dbc6df789885b67ec40b
Santiago <borrajosantiago@gmail.com>
2022-07-20 10:37:56 -0300

    progress display fix

--- app/react/Metadata/components/MetadataFormButtons.js
+++ app/react/Metadata/components/MetadataFormButtons.js
@@ -83,7 +83,7 @@ class MetadataFormButtons extends Component {
               {uploadFileprogress ? <Icon icon="cog" spin /> : <Icon icon="save" />}
               <span className="btn-label">
                 {uploadFileprogress ? (
-                  <span>`${uploadFileprogress} %`</span>
+                  <span>{`${uploadFileprogress} %`}</span>
                 ) : (
                   <Translate>Save</Translate>
                 )}
