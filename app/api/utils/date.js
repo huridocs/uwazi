1869251073939de48a1c35e326c1db8172ab5053
Kevin <nderitukelvin19@gmail.com>
2022-07-26 12:29:31 +0300

    Renamed date.ts back to date.js

--- app/api/utils/date.js
+++ app/api/utils/date.js
@@ -7,7 +7,7 @@ export default {
     return moment.utc().toDate().getTime();
   },
 
-  descriptionToTimestamp(date: string) {
+  descriptionToTimestamp(date) {
     if (date === 'last-day-last-month') {
       return moment.utc().subtract(1, 'months').endOf('month').unix();
     }
@@ -19,14 +19,14 @@ export default {
     return date;
   },
 
-  addYearsToCurrentDate(yearsToAdd: number) {
+  addYearsToCurrentDate(yearsToAdd) {
     const newDate = new Date();
     newDate.setHours(0, 0, 0, 0);
     newDate.setFullYear(newDate.getFullYear() + yearsToAdd);
     return newDate;
   },
 
-  dateToSeconds(value: string, locale: string | undefined) {
+  dateToSeconds(value, locale) {
     // console.log('Date value: ', value, ' locale: ', locale);
     const parsedValue = value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     let getDate = parser.fromString(parsedValue, locale);
