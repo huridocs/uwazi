6f6309cdc643827d7050e19943d2e964f766b7df
Daneryl <daneryl@gmail.com>
2022-07-29 15:27:43 +0200

    fix function cognitive complexity

--- app/api/files/storage.ts
+++ app/api/files/storage.ts
@@ -42,32 +42,35 @@ const streamToBuffer = async (stream: Readable): Promise<Buffer> =>
 const s3KeyWithPath = (filename: string, type: FileTypes) =>
   paths[type](filename).split('/').slice(-2).join('/');
 
-export const readableFile = async (filename: string, type: FileTypes): Promise<Readable> => {
-  if (tenants.current().featureFlags?.s3Storage) {
-    const s3 = s3instance();
-    try {
-      const response = await s3.send(
-        new GetObjectCommand({
+const readFromS3 = async (filename: string, type: FileTypes): Promise<Readable> => {
+  const s3 = s3instance();
+  try {
+    const response = await s3.send(
+      new GetObjectCommand({
+        Bucket: tenants.current().name.replace('_', '-'),
+        Key: s3KeyWithPath(filename, type),
+      })
+    );
+    return response.Body as Readable;
+  } catch (e: unknown) {
+    if (e instanceof NoSuchKey) {
+      await s3.send(
+        new PutObjectCommand({
           Bucket: tenants.current().name.replace('_', '-'),
           Key: s3KeyWithPath(filename, type),
+          Body: await readFile(paths[type](filename)),
         })
       );
-      return response.Body as Readable;
-    } catch (e: unknown) {
-      if (e instanceof NoSuchKey) {
-        await s3.send(
-          new PutObjectCommand({
-            Bucket: tenants.current().name.replace('_', '-'),
-            Key: s3KeyWithPath(filename, type),
-            Body: await readFile(paths[type](filename)),
-          })
-        );
-        return readableFile(filename, type);
-      }
-      throw e;
+      return readFromS3(filename, type);
     }
+    throw e;
   }
+};
 
+export const readableFile = async (filename: string, type: FileTypes): Promise<Readable> => {
+  if (tenants.current().featureFlags?.s3Storage) {
+    return readFromS3(filename, type);
+  }
   return createReadStream(paths[type](filename));
 };
 
