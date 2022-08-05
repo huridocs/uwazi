27643e551f14642ca2dea05c9bca5f9135abc2d5
Mercy <mercy@huridocs.org>
2022-08-04 19:39:43 -0500

    auto fixed eslint errors

--- app/react/SemanticSearch/components/ResultsSidePanel.js
+++ app/react/SemanticSearch/components/ResultsSidePanel.js
@@ -3,13 +3,11 @@ import { connect } from 'react-redux';
 import ResultsFiltersPanel from './ResultsFiltersPanel';
 import DocumentResultsPanel from './DocumentResultsPanel';
 
-export function ResultsSidePanel() {
-  return (
-    <>
-      <ResultsFiltersPanel storeKey="library" />
-      <DocumentResultsPanel />
-    </>
-  );
-}
+export const ResultsSidePanel = () => (
+  <>
+    <ResultsFiltersPanel storeKey="library" />
+    <DocumentResultsPanel />
+  </>
+);
 
 export default connect()(ResultsSidePanel);
