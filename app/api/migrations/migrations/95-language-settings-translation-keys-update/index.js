8003719c95bae50f6a245c6e28822c76d2e09a66
mfacar <mercy@huridocs.org>
2022-08-26 20:04:33 -0500

    update of translations

---
+++ app/api/migrations/migrations/95-language-settings-translation-keys-update/index.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-await-in-loop */
+const obsoleteTranslationKeys = [
+  'Default language',
+  'Default language description',
+  'Some adavanced search features may not be available for this language.',
+];
+
+async function insertSystemKeys(db, newKeys) {
+  const translations = await db.collection('translations').find().toArray();
+  const locales = translations.map(tr => tr.locale);
+
+  const locToSystemContext = {};
+  translations.forEach(tr => {
+    locToSystemContext[tr.locale] = tr.contexts.find(c => c.id === 'System');
+  });
+  const locToKeys = {};
+  Object.entries(locToSystemContext).forEach(([loc, context]) => {
+    locToKeys[loc] = new Set(context.values.map(v => v.key));
+  });
+
+  newKeys.forEach(row => {
+    const { key, value: optionalValue } = row;
+
+    locales.forEach(loc => {
+      if (!locToKeys[loc].has(key)) {
+        const newValue = optionalValue || key;
+        locToSystemContext[loc].values.push({ key, value: newValue });
+        locToKeys[loc].add(key);
+      }
+    });
+  });
+
+  await Promise.all(
+    translations.map(tr => db.collection('translations').replaceOne({ _id: tr._id }, tr))
+  );
+}
+export default {
+  delta: 95,
+
+  name: 'language-settings-translation-keys-update',
+
+  description:
+    'The migration update the removed and added keys from language settings into the System context of translations.',
+
+  reindex: false,
+
+  async up(db) {
+    process.stdout.write(`${this.name}...\r\n`);
+
+    const translations = await db.collection('translations').find({});
+
+    while (await translations.hasNext()) {
+      const language = await translations.next();
+      const updatedContexts = language.contexts.map(context => {
+        if (context.id !== 'System') {
+          return context;
+        }
+
+        const updatedValues = context.values.filter(
+          value => !obsoleteTranslationKeys.includes(value.key)
+        );
+
+        return { ...context, values: updatedValues };
+      });
+
+      await db
+        .collection('translations')
+        .updateOne({ _id: language._id }, { $set: { contexts: updatedContexts } });
+    }
+
+    const systemKeys = [
+      {
+        key: 'Available default translation',
+      },
+      { key: 'Reset default translation' },
+    ];
+    await insertSystemKeys(db, systemKeys);
+  },
+};
---
+++ app/api/migrations/migrations/95-language-settings-translation-keys-update/specs/95-language-settings-translation-keys-update.spec.js
@@ -0,0 +1,84 @@
+import testingDB from 'api/utils/testing_db';
+import migration from '../index.js';
+import { fixtures, templateId, defaultTemplateName, defaultTemplateTitle } from './fixtures.js';
+
+describe('migration remove-obsolete-translation-keys', () => {
+  const locales = ['en', 'es', 'hu'];
+
+  const newKeyValues = [
+    {
+      key: 'Available default translation',
+      value: 'Available default translation',
+    },
+  ];
+
+  const obsoleteTranslationKeys = [
+    'Default language',
+    'Default language description',
+    'Some adavanced search features may not be available for this language.',
+  ];
+
+  const alreadyInAllContexts = {
+    key: 'Duplicated label',
+    en: 'Duplicated label',
+    es: 'Nombre duplicado',
+    hu: 'Ismétlődő címke',
+  };
+  beforeEach(async () => {
+    spyOn(process.stdout, 'write');
+    migration.reindex = false;
+  });
+
+  afterAll(async () => {
+    await testingDB.disconnect();
+  });
+
+  it('should have a delta number', () => {
+    expect(migration.delta).toBe(95);
+  });
+
+  it('should append new keys, leave existing keys intact.', async () => {
+    await testingDB.setupFixturesAndContext(fixtures);
+    await migration.up(testingDB.mongodb);
+
+    const allTranslations = await testingDB.mongodb.collection('translations').find().toArray();
+    function testKeyValue(key, value, locale, contextId) {
+      expect(
+        allTranslations
+          .find(tr => tr.locale === locale)
+          .contexts.find(c => c.id === contextId)
+          .values.find(v => v.key === key).value
+      ).toBe(value);
+    }
+
+    newKeyValues.forEach(({ key, value }) => {
+      locales.forEach(loc => {
+        testKeyValue(key, value, loc, 'System');
+      });
+    });
+
+    obsoleteTranslationKeys.forEach(key => {
+      locales.forEach(loc => {
+        expect(
+          allTranslations
+            .find(tr => tr.locale === loc)
+            .contexts.find(c => c.id === 'System')
+            .values.find(v => v.key === key)
+        ).toBeUndefined();
+      });
+    });
+
+    locales.forEach(loc => {
+      testKeyValue(alreadyInAllContexts.key, alreadyInAllContexts[loc], loc, 'System');
+    });
+    locales.forEach(loc => {
+      expect(
+        allTranslations
+          .find(tr => tr.locale === loc)
+          .contexts.find(c => c.id === templateId.toString()).values
+      ).toHaveLength(2);
+      testKeyValue(defaultTemplateName, defaultTemplateName, loc, templateId.toString());
+      testKeyValue(defaultTemplateTitle, defaultTemplateTitle, loc, templateId.toString());
+    });
+  });
+});
