4b1f553110b07832d225a3b48fe14bde2ebeb959
Santiago <borrajosantiago@gmail.com>
2022-09-01 14:14:39 -0300

    fixed migracion expected delta

--- app/api/migrations/migrations/102-add_system_key_translations/specs/102-add_system_key_translations.spec.js
+++ app/api/migrations/migrations/102-add_system_key_translations/specs/102-add_system_key_translations.spec.js
@@ -27,7 +27,7 @@ describe('migration add_system_key_translations', () => {
   });
 
   it('should have a delta number', () => {
-    expect(migration.delta).toBe(100);
+    expect(migration.delta).toBe(102);
   });
 
   it('should append new keys, leave existing keys intact.', async () => {
