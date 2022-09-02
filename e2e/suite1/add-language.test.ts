6425f26abf47204f516a1237ec907d367606899d
Santiago <borrajosantiago@gmail.com>
2022-09-02 15:04:18 -0300

    updated e2e

--- e2e/suite1/add-language.test.ts
+++ e2e/suite1/add-language.test.ts
@@ -14,20 +14,32 @@ describe('Add language', () => {
   });
 
   // eslint-disable-next-line max-statements
-  it('Should add a language successfully', async () => {
+  it('Should add the first language successfully', async () => {
     await expect(page).toClick('a', { text: 'Settings' });
     await expect(page).toClick('span', { text: 'Languages' });
     await expect(page).toClick('button', { text: 'Add language' });
     await page.waitForSelector('div.modal-content');
     await expect(page).toFill('div.modal-content div.modal-body input', 'CONFIRM');
     await expect(page).toClick('span', { text: 'Accept' });
-    await expect(page).toMatch('Saved successfully.');
+    await expect(page).toMatch('New language added');
   });
 
   it('should show the added language in the translation context', async () => {
     await expect(page).toClick('span', { text: 'Translations' });
     await expect(page).toClick('a', { text: 'Estado' });
     await expect(page).toMatch('ab');
+  });
+
+  it('should delete the added language', async () => {
+    await expect(page).toClick('a', { text: 'Settings' });
+    await expect(page).toClick('span', { text: 'Languages' });
+    await expect(page).toClick('.installed-languages > div:nth-child(4) > div:nth-child(4)', {
+      text: 'Delete language',
+    });
+    await page.waitForSelector('div.modal-content');
+    await expect(page).toFill('div.modal-content div.modal-body input', 'CONFIRM');
+    await expect(page).toClick('span', { text: 'Accept' });
+    await expect(page).toMatch('Language deleted');
   });
 
   afterAll(async () => {
