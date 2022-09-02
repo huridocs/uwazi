ff98d9674ab8eba4b0ead3096b27ef08f8c2e3a7
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-09-02 11:18:07 +0200

    add docker option to admin_user and blank_state

--- database/admin_user.sh
+++ database/admin_user.sh
@@ -6,6 +6,13 @@ cd "$parent_path"
 DB=${1:-uwazi_development}
 HOST=${2:-${DBHOST:-127.0.0.1}}
 
+if [ -n "${MONGO_CONTAINER+set}" ]
+then
+    docker_prefix="docker exec $MONGO_CONTAINER"
+else
+    docker_prefix=""
+fi
+
 echo -e "\n\nDeleting $DB database on $HOST"
-mongo -host $HOST $DB --eval "db.users.drop()"
+$docker_prefix mongo -host $HOST $DB --eval "db.users.drop()"
 mongorestore -h $HOST blank_state/admin_user/ --db=$DB
--- database/blank_state.sh
+++ database/blank_state.sh
@@ -8,8 +8,15 @@ cd "$parent_path"
 DB=${1:-${DATABASE_NAME:-uwazi_development}}
 HOST=${2:-${DBHOST:-127.0.0.1}}
 
+if [ -n "${MONGO_CONTAINER+set}" ]
+then
+    docker_prefix="docker exec $MONGO_CONTAINER"
+else
+    docker_prefix=""
+fi
+
 echo -e "\n\nDeleting $DB database on $HOST"
-mongo -host $HOST $DB --eval "db.dropDatabase()"
+$docker_prefix mongo -host $HOST $DB --eval "db.dropDatabase()"
 mongorestore -h $HOST blank_state/uwazi_development/ --db=$DB
 
 INDEX_NAME=$DB DATABASE_NAME=$DB yarn migrate
