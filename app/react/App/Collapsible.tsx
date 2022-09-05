6762b308a049bc3998328eeb108b3d60ce9e6df5
Kevin <nderitukelvin19@gmail.com>
2022-09-05 11:38:07 +0300

    Added collapsible component

---
+++ app/react/App/Collapsible.tsx
@@ -0,0 +1,31 @@
+import { Icon } from 'UI';
+import React, { ReactElement, useEffect, useState } from 'react';
+
+interface CollapsibleProps {
+  className?: string;
+  header: string | HTMLElement;
+  headerInfo?: string;
+  children: ReactElement<any, any>;
+  collapse?: boolean;
+}
+
+const Collapsible = ({ header, children, className, headerInfo, collapse }: CollapsibleProps) => {
+  const [collapsed, setCollapsed] = useState(collapse);
+  useEffect(() => {
+    setCollapsed(collapse);
+  }, [collapse]);
+  return (
+    <div className={className} onClick={() => setCollapsed(!collapsed)}>
+      <div className="header">
+        <span className="header-icon">
+          <Icon icon={collapsed ? 'caret-right' : 'caret-down'} />
+        </span>
+        <span>{header}</span>
+        {headerInfo && <span className="header-info">{headerInfo}</span>}
+      </div>
+      {!collapsed && <div className="content">{children}</div>}
+    </div>
+  );
+};
+
+export { Collapsible };
---
+++ app/react/App/specs/Collapsible.spec.tsx
@@ -0,0 +1,57 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { mount, CommonWrapper } from 'enzyme';
+import React from 'react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureStore, { MockStoreCreator } from 'redux-mock-store';
+import { Collapsible } from '../Collapsible';
+
+describe('Collapsible', () => {
+  let component: CommonWrapper;
+  const middlewares = [thunk];
+  const mockStoreCreator: MockStoreCreator<object> = configureStore<object>(middlewares);
+
+  const render = (props = {}) =>
+    mount(
+      <Provider store={mockStoreCreator({})}>
+        <Collapsible header="Some header" collapse={props.collapse}>
+          <div id="test" />
+        </Collapsible>
+      </Provider>
+    );
+
+  it('should render children', () => {
+    component = render();
+    expect(component.contains(<div id="test" />)).toBe(true);
+  });
+
+  it('should hide children when clicked', () => {
+    const mountComp = render();
+    mountComp.simulate('click');
+    expect(mountComp.contains(<div id="test" />)).toBe(false);
+  });
+
+  it('should collapse if collapse prop is set', () => {
+    const mountComp = render({ collapse: true });
+    expect(mountComp.contains(<div id="test" />)).toBe(false);
+  });
+
+  it('should collapse if collapse prop is not set', () => {
+    const mountComp = render();
+    expect(mountComp.contains(<div id="test" />)).toBe(true);
+  });
+
+  it('should react to collapse prop', () => {
+    const props = {
+      collapse: false,
+    };
+    component = render(props);
+    expect(component.contains(<div id="test" />)).toBe(true);
+    props.collapse = true;
+    component.update();
+    // expect(component.contains(<div id="test" />)).toBe(false);
+  });
+});
