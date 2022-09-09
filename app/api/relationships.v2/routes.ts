c1d5f150409832b91530f8cab6995754b88aa99b
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-09-09 12:24:14 +0200

    comment out routes file

--- app/api/relationships.v2/routes.ts
+++ app/api/relationships.v2/routes.ts
@@ -1,136 +1,136 @@
-import Joi from 'joi';
-import Ajv from 'ajv';
-import { validation } from '../utils';
-import needsAuthorization from '../auth/authMiddleware';
+// import Joi from 'joi';
+// import Ajv from 'ajv';
+// import { validation } from '../utils';
+// import needsAuthorization from '../auth/authMiddleware';
 
-export default app => {
-  app.post(
-    '/api/relationships.v2/bulk',
-    needsAuthorization(['admin', 'editor', 'collaborator']),
-    async (req, res, next) => {
-      // the option to do saves and deletes in one endpoint
-      res.status(418);
-      res.json({ error: 'not implemented yet' });
-    }
-  );
+// export default app => {
+//   app.post(
+//     '/api/relationships.v2/bulk',
+//     needsAuthorization(['admin', 'editor', 'collaborator']),
+//     async (req, res, next) => {
+//       // the option to do saves and deletes in one endpoint
+//       res.status(418);
+//       res.json({ error: 'not implemented yet' });
+//     }
+//   );
 
-  app.post(
-    '/api/relationships.v2',
-    needsAuthorization(['admin', 'editor']),
-    // validation.validateRequest(
-    //   Joi.object()
-    //     .keys({
-    //       _id: Joi.objectId(),
-    //       __v: Joi.number(),
-    //       entity: Joi.string(),
-    //       hub: Joi.string().allow(''),
-    //       template: Joi.string(),
-    //       metadata: Joi.any(),
-    //       language: Joi.string(),
-    //       range: Joi.object().keys({
-    //         start: Joi.number(),
-    //         end: Joi.number(),
-    //         text: Joi.string(),
-    //       }),
-    //     })
-    //     .required()
-    // ),
-    (req, res, next) => {
-      // save relationships (based on post api/references)
-      res.status(418);
-      res.json({ error: 'not implemented yet' });
-    }
-  );
+//   app.post(
+//     '/api/relationships.v2',
+//     needsAuthorization(['admin', 'editor']),
+//     // validation.validateRequest(
+//     //   Joi.object()
+//     //     .keys({
+//     //       _id: Joi.objectId(),
+//     //       __v: Joi.number(),
+//     //       entity: Joi.string(),
+//     //       hub: Joi.string().allow(''),
+//     //       template: Joi.string(),
+//     //       metadata: Joi.any(),
+//     //       language: Joi.string(),
+//     //       range: Joi.object().keys({
+//     //         start: Joi.number(),
+//     //         end: Joi.number(),
+//     //         text: Joi.string(),
+//     //       }),
+//     //     })
+//     //     .required()
+//     // ),
+//     (req, res, next) => {
+//       // save relationships (based on post api/references)
+//       res.status(418);
+//       res.json({ error: 'not implemented yet' });
+//     }
+//   );
 
-  app.delete(
-    '/api/relationships.v2',
-    needsAuthorization(['admin', 'editor']),
-    // validation.validateRequest(
-    //   Joi.object()
-    //     .keys({
-    //       _id: Joi.objectId().required(),
-    //     })
-    //     .required(),
-    //   'query'
-    // ),
-    (req, res, next) => {
-      // delete relationships
-      res.status(418);
-      res.json({ error: 'not implemented yet' });
-    }
-  );
+//   app.delete(
+//     '/api/relationships.v2',
+//     needsAuthorization(['admin', 'editor']),
+//     // validation.validateRequest(
+//     //   Joi.object()
+//     //     .keys({
+//     //       _id: Joi.objectId().required(),
+//     //     })
+//     //     .required(),
+//     //   'query'
+//     // ),
+//     (req, res, next) => {
+//       // delete relationships
+//       res.status(418);
+//       res.json({ error: 'not implemented yet' });
+//     }
+//   );
 
-  app.get(
-    '/api/relationships.v2/by_entity/',
-    // validation.validateRequest(
-    //   Joi.object()
-    //     .keys({
-    //       sharedId: Joi.string().required(),
-    //       file: Joi.string(),
-    //       onlyTextReferences: Joi.string(),
-    //     })
-    //     .required(),
-    //   'query'
-    // ),
-    (req, res, next) => {
-      // get references for an entity, based on get /api/references/by_document/
-      res.status(418);
-      res.json({ error: 'not implemented yet' });
-    }
-  );
+//   app.get(
+//     '/api/relationships.v2/by_entity/',
+//     // validation.validateRequest(
+//     //   Joi.object()
+//     //     .keys({
+//     //       sharedId: Joi.string().required(),
+//     //       file: Joi.string(),
+//     //       onlyTextReferences: Joi.string(),
+//     //     })
+//     //     .required(),
+//     //   'query'
+//     // ),
+//     (req, res, next) => {
+//       // get references for an entity, based on get /api/references/by_document/
+//       res.status(418);
+//       res.json({ error: 'not implemented yet' });
+//     }
+//   );
 
-  // how does this group?
-  // app.get('/api/references/group_by_connection/', (req, res, next) => {
-  //   relationships
-  //     .getGroupsByConnection(req.query.sharedId, req.language, {
-  //       excludeRefs: true,
-  //       user: req.user,
-  //     })
-  //     .then(response => {
-  //       res.json(response);
-  //     })
-  //     .catch(next);
-  // });
+//   // how does this group?
+//   // app.get('/api/references/group_by_connection/', (req, res, next) => {
+//   //   relationships
+//   //     .getGroupsByConnection(req.query.sharedId, req.language, {
+//   //       excludeRefs: true,
+//   //       user: req.user,
+//   //     })
+//   //     .then(response => {
+//   //       res.json(response);
+//   //     })
+//   //     .catch(next);
+//   // });
 
-  // what does this do?
-  // app.get(
-  //   '/api/references/search/',
-  //   validation.validateRequest(
-  //     Joi.object().keys({
-  //       sharedId: Joi.string().allow(''),
-  //       filter: Joi.string().allow(''),
-  //       limit: Joi.string().allow(''),
-  //       sort: Joi.string().allow(''),
-  //       order: Joi.string(),
-  //       treatAs: Joi.string(),
-  //       searchTerm: Joi.string().allow(''),
-  //     }),
-  //     'query'
-  //   ),
-  //   (req, res, next) => {
-  //     req.query.filter = JSON.parse(req.query.filter || '{}');
-  //     const { sharedId, ...query } = req.query;
-  //     relationships
-  //       .search(req.query.sharedId, query, req.language, req.user)
-  //       .then(results => res.json(results))
-  //       .catch(next);
-  //   }
-  // );
+//   // what does this do?
+//   // app.get(
+//   //   '/api/references/search/',
+//   //   validation.validateRequest(
+//   //     Joi.object().keys({
+//   //       sharedId: Joi.string().allow(''),
+//   //       filter: Joi.string().allow(''),
+//   //       limit: Joi.string().allow(''),
+//   //       sort: Joi.string().allow(''),
+//   //       order: Joi.string(),
+//   //       treatAs: Joi.string(),
+//   //       searchTerm: Joi.string().allow(''),
+//   //     }),
+//   //     'query'
+//   //   ),
+//   //   (req, res, next) => {
+//   //     req.query.filter = JSON.parse(req.query.filter || '{}');
+//   //     const { sharedId, ...query } = req.query;
+//   //     relationships
+//   //       .search(req.query.sharedId, query, req.language, req.user)
+//   //       .then(results => res.json(results))
+//   //       .catch(next);
+//   //   }
+//   // );
 
-  app.get(
-    '/api/relationships.v2/count_by_relationtype',
-    // validation.validateRequest(
-    //   Joi.object()
-    //     .keys({
-    //       relationtypeId: Joi.objectId().required(),
-    //     })
-    //     .required(),
-    //   'query'
-    // ),
-    (req, res, next) => {
-      // a count aggregation on relationtype
-      res.status(418);
-      res.json({ error: 'not implemented yet' });
-    }
-  );
-};
+//   app.get(
+//     '/api/relationships.v2/count_by_relationtype',
+//     // validation.validateRequest(
+//     //   Joi.object()
+//     //     .keys({
+//     //       relationtypeId: Joi.objectId().required(),
+//     //     })
+//     //     .required(),
+//     //   'query'
+//     // ),
+//     (req, res, next) => {
+//       // a count aggregation on relationtype
+//       res.status(418);
+//       res.json({ error: 'not implemented yet' });
+//     }
+//   );
+// };
