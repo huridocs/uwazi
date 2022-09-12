4547037ce87a8a6268cecfb682fa113f8d4a6724
Federico Nocetti <federico@huridocs.org>
2022-09-11 22:33:04 -0300

    Remove unused code

--- app/api/relationships.v2/database/EntitiesDataSource.ts
+++ app/api/relationships.v2/database/EntitiesDataSource.ts
@@ -1,5 +1,4 @@
 import { MongoDataSource } from './MongoDataSource';
-import { MongoResultSet } from './MongoResultSet';
 
 export class EntitiesDataSource extends MongoDataSource {
   async entitiesExist(sharedIds: string[]) {
@@ -7,10 +6,5 @@ export class EntitiesDataSource extends MongoDataSource {
       .collection('entities')
       .countDocuments({ sharedId: { $in: sharedIds } }, { session: this.session });
     return countInExistence === sharedIds.length;
-  }
-
-  getByIds(sharedIds: string[]) {
-    const cursor = this.db.collection('entities').find({ sharedId: { $in: sharedIds } });
-    return new MongoResultSet(cursor, MongoResultSet.NoOpMapper);
   }
 }
