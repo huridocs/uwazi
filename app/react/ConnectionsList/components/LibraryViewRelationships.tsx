a73a55efe40b88088b6104f8b6180172c905af10
Kevin <nderitukelvin19@gmail.com>
2022-09-16 12:05:34 +0300

    Fixed react keys on map functions

--- app/react/ConnectionsList/components/LibraryViewRelationships.tsx
+++ app/react/ConnectionsList/components/LibraryViewRelationships.tsx
@@ -1,32 +1,31 @@
 import React, { useEffect } from 'react';
 import { connect, ConnectedProps } from 'react-redux';
-import PropTypes from 'prop-types';
-import { Map, List } from 'immutable';
-import { bindActionCreators } from 'redux';
+import { Map } from 'immutable';
+import { bindActionCreators, Dispatch } from 'redux';
 import { Icon } from 'app/UI';
 import { Item } from 'app/Layout';
 import { Collapsible } from 'app/App/Collapsible';
 import { StickyHeader } from 'app/App/StickyHeader';
 import RelationshipMetadata from 'app/Relationships/components/RelationshipMetadata';
 import LoadMoreRelationshipsButton from 'app/Relationships/components/LoadMoreRelationshipsButton';
+import { IStore } from 'app/istore';
 import * as actions from '../../Relationships/actions/actions';
 
 interface LibraryViewRelationshipsProps {
   expanded: boolean;
 }
 
-function mapStateToProps(state: any) {
+function mapStateToProps(state: IStore) {
   const { relationships, library } = state;
   return {
-    parentEntity: library.ui.getIn(['selectedDocuments', 0]),
+    parentEntity: library.ui.get('selectedDocuments').get(0),
     searchResults: relationships.list.searchResults,
-    search: relationships.list.sort,
     hubs: relationships.hubs,
     relationTypes: actions.selectRelationTypes(state),
   };
 }
 
-function mapDispatchToProps(dispatch: any) {
+function mapDispatchToProps(dispatch: Dispatch<{}>) {
   return bindActionCreators(
     {
       selectConnection: actions.selectConnection,
@@ -42,58 +41,60 @@ type ComponentProps = ConnectedProps<typeof connector> & LibraryViewRelationship
 
 const createRightRelationshipGroups = (
   rightRelationships: any,
-  relationTypes: any[],
-  selectConnection: Function,
+  props: ComponentProps,
   expanded: boolean
-) => (
-  <div className="sidepanel-relationship-right">
-    {rightRelationships.map((relationship: Map<any, any>, index: number) => {
-      let header;
-      const relationshipTemplateId = relationship.get('template');
-      const relationType = relationTypes.find(r => r._id === relationshipTemplateId);
-      if (relationType) {
-        header = relationshipTemplateId ? (
-          relationTypes.find(r => r._id === relationshipTemplateId).name
-        ) : (
-          <Icon icon="link" />
-        );
-      }
+) => {
+  const { relationTypes, selectConnection, parentEntity } = props;
+  return (
+    <div className="sidepanel-relationship-right">
+      {rightRelationships.map((relationship: Map<any, any>, index: number) => {
+        let header;
+        const relationshipTemplateId = relationship.get('template');
+        const relationType = relationTypes.find(r => r._id === relationshipTemplateId);
+        if (relationType) {
+          header = relationshipTemplateId ? (
+            relationTypes.find(r => r._id === relationshipTemplateId)?.name
+          ) : (
+            <Icon icon="link" />
+          );
+        }
 
-      const entityRelationships = relationship.get('relationships');
-      return (
-        <Collapsible
-          header={header}
-          className="sidepanel-relationship-collapsible"
-          headerInfo={`(${entityRelationships.size})`}
-          collapse={!expanded}
-          key={Math.random().toString() + index.toString()}
-        >
-          <>
-            {entityRelationships.map((rel: any, entityRelationshipsIndex: number) => (
-              <div
-                className="sidepanel-relationship-right-entity"
-                key={Math.random().toString() + entityRelationshipsIndex.toString()}
-                onClick={() => selectConnection(rel.get('entityData'))}
-              >
-                <Item
-                  active={false}
-                  doc={rel.get('entityData')}
-                  className="item-collapsed"
-                  noMetadata
-                />
-              </div>
-            ))}
-          </>
-        </Collapsible>
-      );
-    })}
-  </div>
-);
+        const entityRelationships = relationship.get('relationships');
+        return (
+          <Collapsible
+            header={header}
+            className="sidepanel-relationship-collapsible"
+            headerInfo={`(${entityRelationships.size})`}
+            collapse={!expanded}
+            key={(parentEntity.get('_id') as string).toString() + index.toString()}
+          >
+            <>
+              {entityRelationships.map((rel: any, entityRelationshipsIndex: number) => (
+                <div
+                  className="sidepanel-relationship-right-entity"
+                  key={index.toString() + entityRelationshipsIndex.toString()}
+                  onClick={() => selectConnection(rel.get('entityData'))}
+                >
+                  <Item
+                    active={false}
+                    doc={rel.get('entityData')}
+                    className="item-collapsed"
+                    noMetadata
+                  />
+                </div>
+              ))}
+            </>
+          </Collapsible>
+        );
+      })}
+    </div>
+  );
+};
 
 const renderLabel = (template: any, relationTypes: any) =>
   template ? (
     <span className="sidepanel-relationship-left-label">
-      {`${relationTypes.find(r => r._id === template).name}`}
+      {`${relationTypes.find((r: any) => r._id === template).name}`}
     </span>
   ) : (
     <span className="sidepanel-relationship-left-label">
@@ -101,43 +102,25 @@ const renderLabel = (template: any, relationTypes: any) =>
     </span>
   );
 
-const createLabelGroups = (
-  hub: any,
-  relationTypes: any[],
-  selectConnection: Function,
-  expanded: boolean,
-  index: number
-) => {
+const createLabelGroups = (props: ComponentProps, hub: any, index: number) => {
+  const { relationTypes, expanded, parentEntity } = props;
   const template = hub.getIn(['leftRelationship', 'template']);
   return (
     <StickyHeader
       scrollElementSelector=".scrollable"
       stickyElementSelector=".sidepanel-relationship-left-label"
-      key={Math.random().toString() + index.toString()}
+      key={(parentEntity.get('_id') as string).toString() + index.toString()}
     >
       <div className="sidepanel-relationship">
         {renderLabel(template, relationTypes)}
-        {createRightRelationshipGroups(
-          hub.get('rightRelationships'),
-          relationTypes,
-          selectConnection,
-          expanded
-        )}
+        {createRightRelationshipGroups(hub.get('rightRelationships'), props, expanded)}
       </div>
     </StickyHeader>
   );
 };
 
 const LibraryViewRelationshipsComp = (props: ComponentProps) => {
-  const {
-    hubs,
-    searchResults,
-    parentEntity,
-    parseResults,
-    relationTypes,
-    expanded,
-    selectConnection,
-  } = props;
+  const { hubs, searchResults, parentEntity, parseResults } = props;
 
   useEffect(() => {
     if (parentEntity) {
@@ -147,9 +130,8 @@ const LibraryViewRelationshipsComp = (props: ComponentProps) => {
   return (
     <>
       <div className="sidepanel-relationships">
-        {hubs.map((hub: any, index: number) =>
-          createLabelGroups(hub, relationTypes, selectConnection, expanded, index)
-        )}
+        {parentEntity &&
+          hubs.map((hub: any, index: number) => createLabelGroups(props, hub, index))}
       </div>
       <LoadMoreRelationshipsButton />
       <RelationshipMetadata />
@@ -157,14 +139,8 @@ const LibraryViewRelationshipsComp = (props: ComponentProps) => {
   );
 };
 
-LibraryViewRelationshipsComp.propTypes = {
-  selectConnection: PropTypes.func.isRequired,
-  parentEntity: PropTypes.instanceOf(Map),
-  hubs: PropTypes.instanceOf(List).isRequired,
-  searchResults: PropTypes.instanceOf(Map).isRequired,
-  parseResults: PropTypes.func.isRequired,
-  relationTypes: PropTypes.instanceOf(Array).isRequired,
-  expanded: PropTypes.bool,
+LibraryViewRelationshipsComp.defaultProps = {
+  expanded: true,
 };
 
 const LibraryViewRelationships = connector(LibraryViewRelationshipsComp);
