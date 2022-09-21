9c6d9429c88199144ae83833145e585c651abcc4
Santiago <santiago@huridocs.org>
2022-09-21 16:36:52 -0300

    continue updating component

--- app/react/I18N/components/EditTranslationFrom.tsx
+++ app/react/I18N/components/EditTranslationFrom.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect, ConnectedProps } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import { IImmutable } from 'shared/types/Immutable';
-import { TranslationType } from 'shared/translationType';
+import { TranslationContext, TranslationType } from 'shared/translationType';
 import { IStore } from 'app/istore';
 import { BackButton } from 'app/Layout';
 import { Icon } from 'app/UI';
@@ -49,39 +50,96 @@ type mappedProps = ConnectedProps<typeof connector> & { context: string };
 const EditTranslationFromComponent = ({
   context,
   translations,
+  settings,
   saveTranslations,
   importTranslations,
 }: mappedProps) => {
   const preparedTranslations = prepateTranslations(translations, context);
   const contextLabel = preparedTranslations[0].contexts[0].label;
-  console.log(preparedTranslations);
+  const contextTerms = Object.keys(preparedTranslations[0].contexts[0].values);
+
+  const defaultLanguage = settings.collection
+    .get('languages')
+    ?.find(language => language?.get('default') === true)
+    .get('key');
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: [...preparedTranslations],
+    mode: 'onSubmit',
+  });
+
+  const submit = (values: { [s: string]: { locale: string; contexts: TranslationContext[] } }) => {
+    const translationsToSave = preparedTranslations.reduce(
+      (valueToSave: any[], originalValue: TranslationType) => {
+        const formValues = (
+          Object.values(values) as { locale: string; contexts: TranslationContext[] }[]
+        ).find(value => value.locale === originalValue.locale);
+
+        const [updatedContext] = originalValue.contexts || [];
+
+        const newValues = formValues?.contexts[0].values;
+
+        return [
+          ...valueToSave,
+          {
+            ...originalValue,
+            contexts: [{ ...updatedContext, values: newValues }],
+          },
+        ];
+      },
+      []
+    );
+
+    return saveTranslations(translationsToSave);
+  };
+
   return (
     <div className="EditTranslationForm">
-      {/* <Form model="translationsForm" onSubmit={this.save}> */}
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <Translate>Translations</Translate> <Icon icon="angle-right" /> {contextLabel}
+      <form onSubmit={handleSubmit(submit)}>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <Translate>Translations</Translate> <Icon icon="angle-right" /> {contextLabel}
+          </div>
         </div>
-      </div>
-      <div className="settings-footer">
-        <div className="btn-cluster">
-          <BackButton to="/settings/translations" className="btn-plain" />
-          {context === 'System' && importButton(importTranslations)}
+        <ul className="list-group">
+          {contextTerms.sort().map(term => (
+            <li key={term} className="list-group-item">
+              <h5>{term}</h5>
+              {preparedTranslations.map((translation: TranslationType, index: number) => (
+                <div className="input-group">
+                  <span className="input-group-addon">{translation.locale}</span>
+                  <input name={`[${index}].locale`} type="hidden" ref={register()} />
+                  <input
+                    className="form-control"
+                    type="text"
+                    name={`[${index}].contexts[0].values[${term}]`}
+                    ref={register()}
+                  />
+                </div>
+              ))}
+            </li>
+          ))}
+        </ul>
+
+        <div className="settings-footer">
+          <div className="btn-cluster">
+            <BackButton to="/settings/translations" className="btn-plain" />
+            {context === 'System' && importButton(importTranslations)}
+          </div>
+          <div className="btn-cluster content-right">
+            <I18NLink to="/settings/translations" className="btn btn-extra-padding btn-default">
+              <span className="btn-label">
+                <Translate>Cancel</Translate>
+              </span>
+            </I18NLink>
+            <button type="submit" className="btn btn-extra-padding btn-success save-template">
+              <span className="btn-label">
+                <Translate>Save</Translate>
+              </span>
+            </button>
+          </div>
         </div>
-        <div className="btn-cluster content-right">
-          <I18NLink to="/settings/translations" className="btn btn-extra-padding btn-default">
-            <span className="btn-label">
-              <Translate>Cancel</Translate>
-            </span>
-          </I18NLink>
-          <button type="submit" className="btn btn-extra-padding btn-success save-template">
-            <span className="btn-label">
-              <Translate>Save</Translate>
-            </span>
-          </button>
-        </div>
-      </div>
-      {/* </Form> */}
+      </form>
     </div>
   );
 };
