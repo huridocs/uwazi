e5153167df3343f64297381d6cb05d19804684fc
Mercy <mercy@huridocs.org>
2022-09-22 09:06:04 -0500

    4893 - untranslatable labels in settings translations (#5119)
    * wrapping into translate tab missing labels
    * migration added to change connection for relationship type

---
+++ app/api/migrations/migrations/110-replace_connection_with_relationship_type/specs/110-replace_connection_with_relationship.spec.js
@@ -0,0 +1,47 @@
+import testingDB from 'api/utils/testing_db';
+import migration from '../index.js';
+import fixtures from './fixtures.js';
+
+describe('migration replace_connection_with_relationship_type', () => {
+  beforeEach(async () => {
+    spyOn(process.stdout, 'write');
+    await testingDB.setupFixturesAndContext(fixtures);
+  });
+
+  afterAll(async () => {
+    await testingDB.disconnect();
+  });
+
+  it('should have a delta number', () => {
+    expect(migration.delta).toBe(110);
+  });
+
+  it('should replace  `connection` for `relationship type` while keeping other contexts', async () => {
+    await migration.up(testingDB.mongodb);
+
+    const allTranslations = await testingDB.mongodb.collection('translations').find().toArray();
+
+    const relationShipContexts = allTranslations.map(t => ({
+      locale: t.locale,
+      contexts: t.contexts
+        .filter(context => context.type === 'Relationship Type')
+        .map(context => context.label),
+    }));
+
+    const otherContexts = allTranslations.map(t => ({
+      locale: t.locale,
+      contexts: t.contexts
+        .filter(context => context.type !== 'Relationship Type')
+        .map(context => context.label),
+    }));
+
+    expect(relationShipContexts).toEqual([
+      { locale: 'en', contexts: ['Related', 'Inherited'] },
+      { locale: 'es', contexts: ['Related', 'Inherited'] },
+    ]);
+    expect(otherContexts).toEqual([
+      { locale: 'en', contexts: ['User Interface'] },
+      { locale: 'es', contexts: ['User Interface'] },
+    ]);
+  });
+});
