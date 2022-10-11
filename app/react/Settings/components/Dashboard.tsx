da84d6ca81f460038a95ba7c1f01c3c2d7b9e5f7
Santiago <santiago@huridocs.org>
2022-10-11 16:58:09 -0300

    loading users

--- app/react/Settings/components/Dashboard.tsx
+++ app/react/Settings/components/Dashboard.tsx
@@ -1,56 +1,89 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Dictionary, groupBy } from 'lodash';
 import { Translate } from 'app/I18N';
+import { UserSchema } from 'shared/types/userType';
+import UsersAPI from 'app/Users/UsersAPI';
+import { RequestParams } from 'app/utils/RequestParams';
 
-const Dashboard = () => (
-  <main className="settings-content">
-    <div className="settings-dashboard">
-      <div className="panel">
-        <div className="panel-heading">
-          <Translate>Dashboard</Translate>
-        </div>
+const Dashboard = () => {
+  const [users, setUsers] = useState<UserSchema[]>([]);
+  const [userBrakdown, setUserBrakdown] = useState<Dictionary<UserSchema[]>>({
+    admin: [],
+    editor: [],
+    collaborator: [],
+  });
 
-        <div className="panel-body">
-          <div className="cards">
-            <article className="card">
-              <div className="heading">
-                <h2>Users</h2>
-              </div>
-              <div className="body">
-                <p>User info</p>
-              </div>
-            </article>
+  useEffect(() => {
+    UsersAPI.get(new RequestParams())
+      .then((response: UserSchema[]) => {
+        setUsers(response);
+        setUserBrakdown(groupBy(response, 'role'));
+      })
+      .catch(() => {});
+  }, []);
 
-            <article className="card">
-              <div className="heading">
-                <h2>Entities</h2>
-              </div>
-              <div className="body">
-                <p>Entities info</p>
-              </div>
-            </article>
+  return (
+    <main className="settings-content">
+      <div className="settings-dashboard">
+        <div className="panel">
+          <div className="panel-heading">
+            <Translate>Dashboard</Translate>
+          </div>
 
-            <article className="card">
-              <div className="heading">
-                <h2>Files</h2>
-              </div>
-              <div className="body">
-                <p>Files info</p>
-              </div>
-            </article>
+          <div className="panel-body">
+            <div className="cards">
+              <article className="card">
+                <div className="heading">
+                  <h2>Users</h2>
+                </div>
+                <div className="body">
+                  {users.length} <Translate>Total users</Translate>
+                </div>
+                <div className="footer">
+                  <p>
+                    {userBrakdown.admin.length} <Translate>Admins</Translate>
+                  </p>
+                  <p>
+                    {userBrakdown.editor?.length || 0} <Translate>Editors</Translate>
+                  </p>
+                  <p>
+                    {userBrakdown.collaborator?.length || 0} <Translate>Collaborators</Translate>
+                  </p>
+                </div>
+              </article>
 
-            <article className="card">
-              <div className="heading">
-                <h2>Storage</h2>
-              </div>
-              <div className="body">
-                <p>Storage info</p>
-              </div>
-            </article>
+              <article className="card">
+                <div className="heading">
+                  <h2>Entities</h2>
+                </div>
+                <div className="body">
+                  <p>Entities info</p>
+                </div>
+              </article>
+
+              <article className="card">
+                <div className="heading">
+                  <h2>Files</h2>
+                </div>
+                <div className="body">
+                  <p>Files info</p>
+                </div>
+              </article>
+
+              <article className="card">
+                <div className="heading">
+                  <h2>Storage</h2>
+                </div>
+                <div className="body">
+                  <p>Storage info</p>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export { Dashboard };
