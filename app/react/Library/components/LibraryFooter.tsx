16e655bddf19e6bbdabbd69522151234f4fd13f5
mfacar <mercy@huridocs.org>
2022-10-12 21:13:51 -0500

    toogle footer buttons

--- app/react/Library/components/LibraryFooter.tsx
+++ app/react/Library/components/LibraryFooter.tsx
@@ -1,7 +1,7 @@
 import { Translate } from 'app/I18N';
 import { wrapDispatch } from 'app/Multireducer';
 import { Icon } from 'app/UI';
-import React, { Dispatch } from 'react';
+import React, { Dispatch, useState } from 'react';
 import { bindActionCreators } from 'redux';
 import {
   newEntity as newEntityAction,
@@ -23,36 +23,52 @@ interface LibraryFooterActions {
 
 type LibraryFooterProps = LibraryFooterActions & LibraryFooterOwnProps;
 
-const LibraryFooterComponent = ({ storeKey, newEntity, showImportPanel }: LibraryFooterProps) => (
-  <div className="library-footer with-sidepanel remove-nesting">
-    <div className="btn-cluster">
-      <NeedAuthorization roles={['admin', 'editor', 'collaborator']}>
-        <button
-          className="btn btn-default btn-footer-hover-success"
-          type="button"
-          onClick={() => newEntity(storeKey)}
-        >
-          <Icon icon="plus" />
-          <span className="btn-label">
-            <Translate>Create entity</Translate>
-          </span>
+const LibraryFooterComponent = ({ storeKey, newEntity, showImportPanel }: LibraryFooterProps) => {
+  const [footerVisible, setFooterVisible] = useState(false);
+  return (
+    <>
+      <div className="library-footer with-sidepanel remove-nesting">
+        <div className="close-actions-button">
+          <button type="button">
+            <Translate>Close actions</Translate>
+          </button>
+          <hr />
+        </div>
+        <div className="btn-cluster">
+          <NeedAuthorization roles={['admin', 'editor', 'collaborator']}>
+            <button
+              className="btn btn-default btn-footer-hover-success"
+              type="button"
+              onClick={() => newEntity(storeKey)}
+            >
+              <Icon icon="plus" />
+              <span className="btn-label">
+                <Translate>Create entity</Translate>
+              </span>
+            </button>
+          </NeedAuthorization>
+          <NeedAuthorization roles={['admin', 'editor', 'collaborator']}>
+            <PDFUploadButton storeKey={storeKey} />
+          </NeedAuthorization>
+          <NeedAuthorization roles={['admin', 'editor']}>
+            <button className="btn btn-default sm-order-1" type="button" onClick={showImportPanel}>
+              <Icon icon="import-csv" transform="up-0.2" />
+              <span className="btn-label">
+                <Translate>Import CSV</Translate>
+              </span>
+            </button>
+          </NeedAuthorization>
+          <Export className="sm-order-1" storeKey={storeKey} />
+        </div>
+      </div>
+      <div className="open-actions-button">
+        <button type="button">
+          <Translate>Open actions</Translate>
         </button>
-      </NeedAuthorization>
-      <NeedAuthorization roles={['admin', 'editor', 'collaborator']}>
-        <PDFUploadButton storeKey={storeKey} />
-      </NeedAuthorization>
-      <NeedAuthorization roles={['admin', 'editor']}>
-        <button className="btn btn-default sm-order-1" type="button" onClick={showImportPanel}>
-          <Icon icon="import-csv" transform="up-0.2" />
-          <span className="btn-label">
-            <Translate>Import CSV</Translate>
-          </span>
-        </button>
-      </NeedAuthorization>
-      <Export className="sm-order-1" storeKey={storeKey} />
-    </div>
-  </div>
-);
+      </div>
+    </>
+  );
+};
 
 function mapDispatchToProps(dispatch: Dispatch<any>, props: LibraryFooterOwnProps) {
   return bindActionCreators(
