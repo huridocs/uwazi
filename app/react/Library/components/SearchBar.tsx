c6240fdb5c8fdefdd0afc10b40141349c386f147
mfacar <mercy@huridocs.org>
2022-10-16 21:43:02 -0500

    update component

--- app/react/Library/components/SearchBar.tsx
+++ app/react/Library/components/SearchBar.tsx
@@ -1,113 +1,110 @@
 import { Field, Form, actions as formActions } from 'react-redux-form';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect, ConnectedProps } from 'react-redux';
+import React from 'react';
 
 import { Icon } from 'UI';
-import { searchDocuments, processFilters } from 'app/Library/actions/libraryActions';
+import {
+  searchDocuments as searchDocumentsAction,
+  processFilters,
+} from 'app/Library/actions/libraryActions';
 import { t, Translate } from 'app/I18N';
 import { wrapDispatch } from 'app/Multireducer';
 import ModalTips from 'app/App/ModalTips';
 import { SearchTipsContent } from 'app/App/SearchTipsContent';
 import { submitNewSearch } from 'app/SemanticSearch/actions/actions';
 import { FeatureToggleSemanticSearch } from 'app/SemanticSearch/components/FeatureToggleSemanticSearch';
+import { IStore } from 'app/istore';
 
-// eslint-disable-next-line import/exports-last
-export class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.search = this.search.bind(this);
-    this.resetSearch = this.resetSearch.bind(this);
-    this.submitSearch = this.submitSearch.bind(this);
-    this.submitSemanticSearch = this.submitSemanticSearch.bind(this);
-  }
-
-  resetSearch() {
-    this.props.change(`${this.props.storeKey}.search.searchTerm`, '');
-    const search = { ...this.props.search };
-    search.searchTerm = '';
-    this.props.searchDocuments({ search }, this.props.storeKey);
-  }
-
-  submitSemanticSearch() {
-    this.props.semanticSearch(this.props.search);
-  }
-
-  submitSearch() {
-    const search = { ...this.props.search };
-    this.props.searchDocuments({ search }, this.props.storeKey);
-  }
-
-  search(search) {
-    this.props.searchDocuments({ search }, this.props.storeKey);
-  }
-
-  render() {
-    const { search, storeKey } = this.props;
-    const model = `${storeKey}.search`;
-    return (
-      <div className="search-box">
-        <Form model={model} onSubmit={this.search} autoComplete="off">
-          <div className={`input-group${search.searchTerm ? ' is-active' : ''}`}>
-            <Field model=".searchTerm">
-              <input
-                type="text"
-                placeholder={t('System', 'Search', null, false)}
-                aria-label={t('System', 'Search text description', null, false)}
-                className="form-control"
-                autoComplete="off"
-              />
-              <Icon icon="times" onClick={this.resetSearch} aria-label="Reset Search input" />
-            </Field>
-            <Icon icon="search" onClick={this.submitSearch} aria-label="Search button" />
-          </div>
-          <FeatureToggleSemanticSearch>
-            <button
-              disabled={search.searchTerm ? '' : 'disabled'}
-              type="button"
-              onClick={this.submitSemanticSearch}
-              className="btn btn-success semantic-search-button"
-            >
-              <Icon icon="flask" /> <Translate>Semantic Search</Translate>
-            </button>
-          </FeatureToggleSemanticSearch>
-        </Form>
-        <ModalTips
-          label={t('System', 'Search Tips', null, false)}
-          title={t('System', 'Narrow down your searches', null, false)}
-        >
-          <SearchTipsContent />
-        </ModalTips>
-      </div>
-    );
-  }
+interface SearchBarOwnProps {
+  storeKey: 'library' | 'uploads';
+  counter: React.ReactElement;
 }
-
-SearchBar.propTypes = {
-  searchDocuments: PropTypes.func.isRequired,
-  semanticSearch: PropTypes.func.isRequired,
-  change: PropTypes.func.isRequired,
-  search: PropTypes.object,
-  storeKey: PropTypes.string.isRequired,
-};
-
-export function mapStateToProps(state, props) {
+const mapStateToProps = (state: IStore, props: SearchBarOwnProps) => {
   const search = processFilters(state[props.storeKey].search, state[props.storeKey].filters.toJS());
   return {
     search,
   };
-}
+};
 
-function mapDispatchToProps(dispatch, props) {
-  return bindActionCreators(
+const mapDispatchToProps = (dispatch: Dispatch<{}>, props: SearchBarOwnProps) =>
+  bindActionCreators(
     {
-      searchDocuments,
+      searchDocuments: searchDocumentsAction,
       change: formActions.change,
       semanticSearch: submitNewSearch,
     },
     wrapDispatch(dispatch, props.storeKey)
   );
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type mappedProps = ConnectedProps<typeof connector> & SearchBarOwnProps;
+// eslint-disable-next-line import/exports-last
+const SearchBarComponent = ({
+  search,
+  storeKey,
+  searchDocuments,
+  change,
+  semanticSearch,
+}: mappedProps) => {
+  const resetSearch = () => {
+    change(`${storeKey}.search.searchTerm`, '');
+    const newSearch = { ...search };
+    newSearch.searchTerm = '';
+    searchDocuments({ search: newSearch }, storeKey);
+  };
+
+  const submitSemanticSearch = () => {
+    semanticSearch(search);
+  };
+
+  const submitSearch = () => {
+    searchDocuments(search, storeKey);
+  };
+
+  const runSearch = (newSearch: any) => {
+    searchDocuments({ search: newSearch }, storeKey);
+  };
+
+  const model = `${storeKey}.search`;
+
+  return (
+    <div className="search-box">
+      <Form model={model} onSubmit={runSearch}>
+        <div className={`input-group${search.searchTerm ? ' is-active' : ''}`}>
+          <Field model=".searchTerm">
+            <input
+              type="text"
+              placeholder={t('System', 'Search', null, false)}
+              aria-label={t('System', 'Search text description', null, false)}
+              className="form-control"
+              autoComplete="off"
+            />
+            <Icon icon="times" onClick={resetSearch} aria-label="Reset Search input" />
+          </Field>
+          <Icon icon="search" onClick={submitSearch} aria-label="Search button" />
+        </div>
+        <FeatureToggleSemanticSearch>
+          <button
+            disabled={!search.searchTerm}
+            type="button"
+            onClick={submitSemanticSearch}
+            className="btn btn-success semantic-search-button"
+          >
+            <Icon icon="flask" /> <Translate>Semantic Search</Translate>
+          </button>
+        </FeatureToggleSemanticSearch>
+      </Form>
+      <ModalTips
+        label={t('System', 'Search Tips', null, false)}
+        title={t('System', 'Narrow down your searches', null, false)}
+      >
+        <SearchTipsContent />
+      </ModalTips>
+    </div>
+  );
+};
+
+const container = connector(SearchBarComponent);
+export { container as SearchBar };
