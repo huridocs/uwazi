625f2661a395ab384426585b7737f4fe47ea57f4
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-10-17 11:09:22 +0200

    add  feature flag to eventlistener

--- app/api/entities/eventListeners.ts
+++ app/api/entities/eventListeners.ts
@@ -1,10 +1,15 @@
 import { EventsBus } from 'api/eventsbus';
 import { search } from 'api/search';
+import { getConnection } from 'api/common.v2/database/getConnectionForCurrentTenant';
 import { RelationshipsCreatedEvent } from 'api/relationships.v2/events/RelationshipsCreatedEvent';
+import { MongoSettingsDataSource } from 'api/settings.v2/database/MongoSettingsDataSource';
 
 const registerEventListeners = (eventsBus: EventsBus) => {
   eventsBus.on(RelationshipsCreatedEvent, async ({ markedEntities }) => {
-    if (markedEntities.length) {
+    if (
+      (await new MongoSettingsDataSource(getConnection()).readNewRelationshipsAllowed()) &&
+      markedEntities.length
+    ) {
       await search.indexEntities({ sharedId: { $in: markedEntities } });
     }
   });
