26302beafbff4d7ef047cead666ed1b6c3255ff1
Laszlo Kecskes <laszlokecskes@protonmail.com>
2022-10-24 09:00:36 +0200

    made transactionManager input optional for defaults

--- app/api/authorization.v2/database/data_source_defaults.ts
+++ app/api/authorization.v2/database/data_source_defaults.ts
@@ -1,9 +1,11 @@
+import { DefaultTransactionManager } from 'api/common.v2/database/data_source_defaults';
 import { getConnection } from 'api/common.v2/database/getConnectionForCurrentTenant';
 import { MongoTransactionManager } from 'api/common.v2/database/MongoTransactionManager';
 import { MongoPermissionsDataSource } from './MongoPermissionsDataSource';
 
-const DefaultPermissionsDataSource = (transactionManager: MongoTransactionManager) => {
+const DefaultPermissionsDataSource = (_transactionManager?: MongoTransactionManager) => {
   const connection = getConnection();
+  const transactionManager = _transactionManager || DefaultTransactionManager();
   return new MongoPermissionsDataSource(connection, transactionManager);
 };
 
