99e7ee570407ecb5352322ea1e44b3f12bafbbcb
Sergio Revilla <sergio@huridocs.org>
2022-10-25 11:33:47 +0200

    Extract elastic test

--- app/api/stats/specs/RetrieveStatsService.spec.ts
+++ app/api/stats/specs/RetrieveStatsService.spec.ts
@@ -14,8 +14,7 @@ describe('RetrieveStats', () => {
       .spyOn(elastic.cat, 'indices')
       // @ts-ignore
       .mockResolvedValue({ body: [{ 'store.size': 5000 }] });
-    const connection = await testingDB.connect();
-    db = connection.db;
+    db = (await testingDB.connect()).db;
     jest.spyOn(db, 'stats').mockResolvedValue({
       // @ts-ignore
       storageSize: 15000,
@@ -50,17 +49,20 @@ describe('RetrieveStats', () => {
 
     const stats = await new RetrieveStatsService(db).execute();
 
-    expect(elasticMock).toHaveBeenCalledWith({
-      pretty: true,
-      format: 'application/json',
-      bytes: 'b',
-      h: 'store.size',
-    });
     expect(stats).toEqual({
       users: { total: 3, admin: 1, editor: 1, collaborator: 1 },
       entities: { total: 10 },
       files: { total: 0 },
       storage: { total: 20000 },
+    });
+  });
+
+  it('retrieves elastic stats with proper format', () => {
+    expect(elasticMock).toHaveBeenCalledWith({
+      pretty: true,
+      format: 'application/json',
+      bytes: 'b',
+      h: 'store.size',
     });
   });
 });
