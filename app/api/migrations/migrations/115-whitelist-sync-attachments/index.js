cc5ccaba6bfc8fa4a79df6ab4c0c41b21e5cee17
Sergio Revilla <sergio@huridocs.org>
2022-11-09 12:14:51 +0100

    Migrate sync configs

---
+++ app/api/migrations/migrations/115-whitelist-sync-attachments/index.js
@@ -0,0 +1,24 @@
+import testingDB from 'api/utils/testing_db';
+
+export default {
+  delta: 115,
+
+  name: 'whitelist-sync-attachments',
+
+  description: 'Adds the attachments whitelisting flag to existing sync configurations',
+
+  reindex: false,
+
+  async up(db) {
+    const [{ sync }] = await db.collection('settings').find().toArray();
+    if (!sync) return;
+
+    sync.forEach(aSync => {
+      Object.values(aSync.config.templates).forEach(config => {
+        config.attachments = true;
+      });
+    });
+
+    await db.collection('settings').updateOne({}, { $set: { sync } });
+  },
+};
