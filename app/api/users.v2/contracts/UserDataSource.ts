faff006f2f48e4787ee359851398e802278b1bf9
Federico Nocetti <federico@huridocs.org>
2022-11-09 00:52:34 -0300

    Remove unused code

--- app/api/users.v2/contracts/UserDataSource.ts
+++
@@ -1,6 +0,0 @@
-import { ResultSet } from 'api/common.v2/contracts/ResultSet';
-import { User } from '../model/User';
-
-export interface UserDataSource {
-  get(_id: string): ResultSet<User>;
-}
--- app/api/users.v2/services/service_factories.ts
+++
@@ -1,9 +0,0 @@
-import { getClient } from 'api/common.v2/database/getConnectionForCurrentTenant';
-import { MongoTransactionManager } from 'api/common.v2/database/MongoTransactionManager';
-import { DefaultUserDataSource } from '../database/data_source_defaults';
-import { GetUserService as GenericGetUserService } from './GetUserService';
-
-const GetUserService = () =>
-  new GenericGetUserService(DefaultUserDataSource(new MongoTransactionManager(getClient())));
-
-export { GetUserService };
