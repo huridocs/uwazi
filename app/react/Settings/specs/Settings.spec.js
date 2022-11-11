a4febda523845caa4b633679ca739f00d0f023f7
Santiago <santiago@huridocs.org>
2022-11-11 15:25:15 -0300

    tested render logic in Settings

--- app/react/Settings/specs/Settings.spec.js
+++ app/react/Settings/specs/Settings.spec.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import UsersAPI from 'app/Users/UsersAPI';
 import ThesauriAPI from 'app/Thesauri/ThesauriAPI';
 import { I18NApi } from 'app/I18N';
@@ -8,27 +10,27 @@ import SettingsAPI from '../SettingsAPI';
 import { Settings } from '../Settings';
 
 describe('Settings', () => {
+  let user;
+  const dictionaries = [{ _id: 1, name: 'Countries' }];
+  const models = [];
+  const translations = [{ _id: 1, locale: 'es', values: {} }];
+  const templates = [];
+  const settings = { siteName: 'BatCave' };
+  const suggestions = {};
+
+  beforeEach(() => {
+    user = { name: 'doe', role: 'admin' };
+    spyOn(UsersAPI, 'currentUser').and.callFake(async () => Promise.resolve(user));
+    spyOn(ThesauriAPI, 'getThesauri').and.callFake(async () => Promise.resolve(dictionaries));
+    spyOn(ThesauriAPI, 'getModelStatus').and.callFake(async () => Promise.resolve(models));
+    spyOn(I18NApi, 'get').and.callFake(async () => Promise.resolve(translations));
+    spyOn(SettingsAPI, 'get').and.callFake(async () => Promise.resolve(settings));
+    spyOn(SettingsAPI, 'stats').and.callFake(async () => Promise.resolve({ files: 3, users: 2 }));
+    spyOn(TemplatesAPI, 'get').and.callFake(async () => Promise.resolve(templates));
+    spyOn(api, 'search').and.callFake(async () => Promise.resolve(suggestions));
+  });
+
   describe('requestState', () => {
-    let user;
-    const dictionaries = [{ _id: 1, name: 'Countries' }];
-    const models = [];
-    const translations = [{ _id: 1, locale: 'es', values: {} }];
-    const templates = [];
-    const settings = { siteName: 'BatCave' };
-    const suggestions = {};
-
-    beforeEach(() => {
-      user = { name: 'doe', role: 'admin' };
-      spyOn(UsersAPI, 'currentUser').and.callFake(async () => Promise.resolve(user));
-      spyOn(ThesauriAPI, 'getThesauri').and.callFake(async () => Promise.resolve(dictionaries));
-      spyOn(ThesauriAPI, 'getModelStatus').and.callFake(async () => Promise.resolve(models));
-      spyOn(I18NApi, 'get').and.callFake(async () => Promise.resolve(translations));
-      spyOn(SettingsAPI, 'get').and.callFake(async () => Promise.resolve(settings));
-      spyOn(SettingsAPI, 'stats').and.callFake(async () => Promise.resolve({ files: 3, users: 2 }));
-      spyOn(TemplatesAPI, 'get').and.callFake(async () => Promise.resolve(templates));
-      spyOn(api, 'search').and.callFake(async () => Promise.resolve(suggestions));
-    });
-
     it('should get the current user, and metadata', async () => {
       const actions = await Settings.requestState(new RequestParams());
       expect(actions).toMatchSnapshot();
@@ -38,6 +40,22 @@ describe('Settings', () => {
       user = { name: 'jane', role: 'editor' };
       const actions = await Settings.requestState(new RequestParams());
       expect(actions).toMatchSnapshot();
+    });
+  });
+
+  describe('render', () => {
+    let component;
+    const render = children => {
+      const context = { store: { getState: () => ({}), dispatch: jasmine.createSpy('dispatch') } };
+      component = shallow(<Settings>{children}</Settings>, { context });
+    };
+
+    it('should add the only-desktop class when recieving children only', () => {
+      render('Account settings');
+      expect(component).toMatchSnapshot();
+
+      render();
+      expect(component).toMatchSnapshot();
     });
   });
 });
