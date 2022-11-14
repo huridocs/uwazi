e70792f10a946059145d78c5d63aaf1420c218a7
Alberto Casado Torres <konzz@users.noreply.github.com>
2022-11-14 12:01:19 +0100

    5268 global js (#5304)
    * Global JS
    
    * Version bump
    
    * UI changes to reflect when custom JS is enabled
    
    * Snapshot updated
    
    * Translations update
    
    * fixes
    
    * fixed migration deltas
    
    Co-authored-by: Joan Gallego Girona <daneryl@gmail.com>

--- app/react/Settings/components/Customisation.js
+++ app/react/Settings/components/Customisation.js
@@ -1,42 +1,82 @@
 import { Field, Form, actions as formActions } from 'react-redux-form';
+import { Tabs, TabLink, TabContent } from 'react-tabs-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Immutable from 'immutable';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-import { Translate, t } from 'app/I18N';
+import { Translate } from 'app/I18N';
 
 import saveSettings from '../actions/settingsActions';
 
 class Customisation extends Component {
   componentDidMount() {
-    this.props.loadForm('settings.settings', { customCSS: this.props.settings.get('customCSS') });
+    this.props.loadForm('settings.settings', {
+      customCSS: this.props.settings.get('customCSS'),
+      customJS: this.props.settings.get('customJS'),
+    });
   }
 
   render() {
+    const allowcustomJS = this.props.settings.get('allowcustomJS');
     return (
-      <div className="settings-content">
-        <div className="panel panel-default settings-custom-css">
-          <div className="panel-heading">{t('System', 'Custom styles')}</div>
-          <div className="panel-body">
-            <Form model="settings.settings" onSubmit={this.props.saveSettings}>
-              <Field model=".customCSS" className="test">
-                <label className="form-group-label" htmlFor="custom_css">
-                  {t('System', 'Custom CSS')}
-                  <textarea className="form-control" id="custom_css" />
-                </label>
-              </Field>
-              <div className="settings-footer remove-extra-nesting">
-                <div className="btn-cluster content-right">
-                  <button type="submit" className="btn btn-success btn-extra-padding">
-                    <Translate>Save</Translate>
-                  </button>
-                </div>
-              </div>
-            </Form>
-          </div>
+      <div className="settings-content global-css-js">
+        <div className="panel-heading">
+          {allowcustomJS ? (
+            <Translate>Global CSS & JS</Translate>
+          ) : (
+            <Translate>Global CSS</Translate>
+          )}
         </div>
+        <Tabs>
+          <div style={{ position: 'relative' }}>
+            <TabLink to="css" default role="button" tabIndex="0" aria-label="CSS">
+              <Translate>Custom CSS</Translate>
+            </TabLink>
+            {allowcustomJS && (
+              <TabLink to="js" default role="button" tabIndex="0" aria-label="JS">
+                <Translate>Custom JS</Translate>
+              </TabLink>
+            )}
+          </div>
+          <TabContent for="css" className="css">
+            <div className="panel panel-default settings-custom">
+              <div className="panel-body">
+                <Form model="settings.settings" onSubmit={this.props.saveSettings}>
+                  <Field model=".customCSS" className="field">
+                    <textarea className="form-control" id="custom_css" />
+                  </Field>
+                  <div className="settings-footer">
+                    <div className="btn-cluster content-right">
+                      <button type="submit" className="btn btn-success btn-extra-padding">
+                        <Translate>Save</Translate>
+                      </button>
+                    </div>
+                  </div>
+                </Form>
+              </div>
+            </div>
+          </TabContent>
+          <TabContent for="js" className="js">
+            <div className="panel panel-default settings-custom-js">
+              <div className="panel-body">
+                <Form model="settings.settings" onSubmit={this.props.saveSettings}>
+                  <Field model=".customJS">
+                    <textarea className="form-control" id="custom_js" />
+                  </Field>
+                  <div className="settings-footer">
+                    <div className="btn-cluster content-right">
+                      <button type="submit" className="btn btn-success btn-extra-padding">
+                        <Translate>Save</Translate>
+                      </button>
+                    </div>
+                  </div>
+                </Form>
+              </div>
+            </div>
+          </TabContent>
+        </Tabs>
       </div>
     );
   }
