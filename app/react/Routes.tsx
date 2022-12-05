70969592fd9af7ffe11fe73bdf08ed82c7fcc522
Santiago <santiago@huridocs.org>
2022-12-05 15:44:27 -0300

    restored routes layout + cleanup

--- app/react/Routes.tsx
+++ app/react/Routes.tsx
@@ -3,22 +3,16 @@ import React, { ReactElement } from 'react';
 import { createRoutesFromElements, Route } from 'react-router-dom';
 import { App } from 'app/App/App';
 import Activitylog from 'app/Activitylog/Activitylog';
-import { trackPage } from 'app/App/GoogleAnalytics';
 import Configure2fa from 'app/Auth2fa/Configure2fa';
 import EditTranslations from 'app/I18N/EditTranslations';
-import blankState from 'app/Library/helpers/blankState';
-import Library, { LibraryCards } from 'app/Library/Library';
+import { LibraryCards } from 'app/Library/Library';
 import { LibraryMap } from 'app/Library/LibraryMap';
 import { MetadataExtractionDashboard } from 'app/MetadataExtraction/MetadataExtractionDashboard';
-import { IXSuggestions } from 'app/MetadataExtraction/SuggestionsContainer';
 import EditPage from 'app/Pages/EditPage';
 import NewPage from 'app/Pages/NewPage';
 import Pages from 'app/Pages/Pages';
-import PageView from 'app/Pages/PageView';
 import EditRelationType from 'app/RelationTypes/EditRelationType';
 import NewRelationType from 'app/RelationTypes/NewRelationType';
-import OneUpReview from 'app/Review/OneUpReview';
-import SemanticSearchResultsView from 'app/SemanticSearch/SemanticSearchResultsView';
 import {
   PreserveSettings,
   AccountSettings,
@@ -40,140 +34,106 @@ import NewTemplate from 'app/Templates/NewTemplate';
 import EditThesauri from 'app/Thesauri/EditThesauri';
 import NewThesauri from 'app/Thesauri/NewThesauri';
 import ThesaurusCockpit from 'app/Thesauri/ThesaurusCockpit';
-import Uploads from 'app/Uploads/UploadsRoute';
 import { Login } from 'app/Users/Login';
-import ResetPassword from 'app/Users/ResetPassword';
-import UnlockAccount from 'app/Users/UnlockAccount';
-import ViewerRoute from 'app/Viewer/ViewerRoute';
 import GeneralError from 'app/App/ErrorHandling/GeneralError';
 import { UserManagement } from 'app/Users/UserManagement';
-import { store } from 'app/store';
 import { LibraryTable } from 'app/Library/LibraryTable';
-import { validateHomePageRoute } from 'app/utils/routeHelpers';
-import { fromJS } from 'immutable';
-import createStore from './store';
 import { ProtectedRoute } from './ProtectedRoute';
-import { RequestParams } from './utils/RequestParams';
-import { IStore } from './istore';
 
-const onEnter = () => {
-  trackPage();
-};
+// const onEnter = () => {
+//   trackPage();
+// };
 
-const goToLogin = () => {
-  window.location.assign('/login');
-};
+// const goToLogin = () => {
+//   window.location.assign('/login');
+// };
 
-const enterLogin = ({ location }) => {
-  if (location.action === 'REPLACE') {
-    goToLogin();
-  }
-};
+// const enterLogin = ({ location }) => {
+//   if (location.action === 'REPLACE') {
+//     goToLogin();
+//   }
+// };
 
-const needsAuth = (_nxtState, _replace) => {
-  if (!store.getState().user.get('_id')) {
-    goToLogin();
-  }
-};
+// const needsAuth = (_nxtState, _replace) => {
+//   if (!store.getState().user.get('_id')) {
+//     goToLogin();
+//   }
+// };
 
-const enterOnLibrary = (_nxtState, _replace) => {
-  const state = store.getState();
-  if (blankState() && !state.user.get('_id')) {
-    goToLogin();
-    return () => {};
-  }
+// const enterOnLibrary = (_nxtState, _replace) => {
+//   const state = store.getState();
+//   if (blankState() && !state.user.get('_id')) {
+//     goToLogin();
+//     return () => {};
+//   }
 
-  trackPage();
-  return () => {};
-};
+//   trackPage();
+//   return () => {};
+// };
 
-const getDefaultLibraryComponent = defaultLibraryView => {
-  switch (defaultLibraryView) {
-    case 'table':
-      return {
-        component: LibraryTable,
-        onEnter: enterOnLibrary,
-      };
-    case 'map':
-      return {
-        component: LibraryMap,
-        onEnter,
-      };
-    case 'cards':
-    default:
-      return {
-        component: Library,
-        onEnter: enterOnLibrary,
-      };
-  }
-};
+// const getDefaultLibraryComponent = defaultLibraryView => {
+//   switch (defaultLibraryView) {
+//     case 'table':
+//       return {
+//         component: LibraryTable,
+//         onEnter: enterOnLibrary,
+//       };
+//     case 'map':
+//       return {
+//         component: LibraryMap,
+//         onEnter,
+//       };
+//     case 'cards':
+//     default:
+//       return {
+//         component: Library,
+//         onEnter: enterOnLibrary,
+//       };
+//   }
+// };
 
-const getPageIndexRoute = customHomePage => {
-  onEnter();
-  const pageId = customHomePage[customHomePage.indexOf('page') + 1];
-  const component = props => <PageView {...props} params={{ sharedId: pageId }} />;
-  component.requestState = async requestParams =>
-    PageView.requestState(requestParams.set({ sharedId: pageId }));
+// const getPageIndexRoute = customHomePage => {
+//   onEnter();
+//   const pageId = customHomePage[customHomePage.indexOf('page') + 1];
+//   const component = props => <PageView {...props} params={{ sharedId: pageId }} />;
+//   component.requestState = async requestParams =>
+//     PageView.requestState(requestParams.set({ sharedId: pageId }));
 
-  return {
-    component,
-    customHomePageId: pageId,
-  };
-};
+//   return {
+//     component,
+//     customHomePageId: pageId,
+//   };
+// };
 
-const libraryDefaults = (callBack, state, defaultView) => {
-  if (state.user.get('_id')) {
-    return callBack(null, {
-      onEnter: (_nextState, replace) => {
-        replace('/library/?q=(includeUnpublished:!t)');
-      },
-    });
-  }
+// const libraryDefaults = (callBack, state, defaultView) => {
+//   if (state.user.get('_id')) {
+//     return callBack(null, {
+//       onEnter: (_nextState, replace) => {
+//         replace('/library/?q=(includeUnpublished:!t)');
+//       },
+//     });
+//   }
 
-  return callBack(null, getDefaultLibraryComponent(defaultView));
-};
+//   return callBack(null, getDefaultLibraryComponent(defaultView));
+// };
 
-const getIndexRoute = (_nextState, callBack) => {
-  const state = store.getState();
-  const homePageSetting = state.settings.collection.get('home_page') || '';
-  const customHomePage = homePageSetting ? homePageSetting.split('/').filter(v => v) : [];
-  const defaultView = state.settings.collection.get('defaultLibraryView');
+// const getIndexRoute = (_nextState, callBack) => {
+//   const state = store.getState();
+//   const homePageSetting = state.settings.collection.get('home_page') || '';
+//   const customHomePage = homePageSetting ? homePageSetting.split('/').filter(v => v) : [];
+//   const defaultView = state.settings.collection.get('defaultLibraryView');
 
-  if (!validateHomePageRoute(homePageSetting) || customHomePage.length === 0) {
-    return libraryDefaults(callBack, state, defaultView);
-  }
-  return customHomePage.includes('page')
-    ? callBack(null, getPageIndexRoute(customHomePage))
-    : callBack(null, {
-        onEnter: (_nxtState, replace) => {
-          replace(customHomePage.join('/'));
-        },
-      });
-};
-
-// const routes = (
-//   <Route getIndexRoute={getIndexRoute}>
-//     <Route path="library" element={<Library />} onEnter={enterOnLibrary} />
-//     <Route path="library/map" element={<LibraryMap />} onEnter={onEnter} />
-//     <Route path="library/table" element={<LibraryTable />} onEnter={enterOnLibrary} />
-//     <Route path="review" element={<OneUpReview />} onEnter={needsAuth} />
-//     <Route path="uploads" element={<Uploads />} />
-//     <Route path="login" element={<Login />} onEnter={enterLogin} />
-//     <Route path="setpassword/:key" element={<ResetPassword />} />
-//     <Route path="unlockaccount/:username/:code" element={<UnlockAccount />} />
-//     <Route path="document/:sharedId*" element={<ViewerRoute />} onEnter={onEnter} />
-//     <Route path="entity/:sharedId(/:tabView)" element={<ViewerRoute />} onEnter={onEnter} />
-//     <Route path="page/:sharedId" element={<PageView />} onEnter={onEnter} />
-//     <Route path="page/:sharedId/:slug" element={<PageView />} onEnter={onEnter} />
-//     <Route
-//       path="semanticsearch/:searchId"
-//       element={<SemanticSearchResultsView />}
-//       onEnter={onEnter}
-//     />
-//     <Route path="error/:errorCode" element={<GeneralError />} />
-//     <Route path="404" element={<GeneralError />} />
-//   </Route>
-// );
+//   if (!validateHomePageRoute(homePageSetting) || customHomePage.length === 0) {
+//     return libraryDefaults(callBack, state, defaultView);
+//   }
+//   return customHomePage.includes('page')
+//     ? callBack(null, getPageIndexRoute(customHomePage))
+//     : callBack(null, {
+//         onEnter: (_nxtState, replace) => {
+//           replace(customHomePage.join('/'));
+//         },
+//       });
+// };
 
 const adminRoute = (element: ReactElement) => <ProtectedRoute onlyAdmin>{element}</ProtectedRoute>;
 
@@ -233,15 +193,18 @@ const routesLayout = (
     <Route path="library/table" element={<LibraryTable />} />
     <Route path="error/:errorCode" element={<GeneralError />} />
     <Route path="404" element={<GeneralError />} />
-    <Route path="*" element={<GeneralError />} />
   </Route>
 );
 
 const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     {routesLayout}
-    <Route path=":lang">{routesLayout}</Route>
+    <Route path=":lang">
+      {routesLayout}
+      <Route path="*" element={<GeneralError />} />
+    </Route>
+    <Route path="*" element={<GeneralError />} />
   </Route>
 );
 
-export { getIndexRoute, routes };
+export { routes };
