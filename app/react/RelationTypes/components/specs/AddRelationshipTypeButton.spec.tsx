1592a744a9d5b5002e2b1a787cd35832e96819e0
Santiago <santiago@huridocs.org>
2023-01-11 14:44:42 -0300

    add relationship and thesaurus buttons spec

--- app/react/RelationTypes/components/specs/AddRelationshipTypeButton.spec.tsx
+++ app/react/RelationTypes/components/specs/AddRelationshipTypeButton.spec.tsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import Immutable from 'immutable';
-import { screen, fireEvent, waitFor } from '@testing-library/react';
+import { screen, fireEvent, act } from '@testing-library/react';
 import { defaultState, renderConnectedContainer } from 'app/utils/test/renderConnected';
 import { AddRelationshipTypeButton } from '../AddRelationshipTypeButton';
 import * as relationTypeActions from '../../actions/relationTypeActions';
@@ -39,7 +39,7 @@ describe('Add relationship type button', () => {
     });
 
     it('should close on cancel', async () => {
-      await waitFor(() => {
+      await act(() => {
         fireEvent.click(screen.getByText('Cancel').parentElement!);
       });
 
@@ -47,7 +47,7 @@ describe('Add relationship type button', () => {
     });
 
     it('should display an error if input is left empty', async () => {
-      await waitFor(() => {
+      await act(() => {
         fireEvent.click(screen.getByText('Save').parentElement!);
       });
 
@@ -55,7 +55,7 @@ describe('Add relationship type button', () => {
     });
 
     it('should display an error if the relationship type name already exists', async () => {
-      await waitFor(async () => {
+      await act(async () => {
         fireEvent.change(await screen.findByRole('textbox'), {
           target: { value: 'Existing relationship type' },
         });
@@ -67,7 +67,7 @@ describe('Add relationship type button', () => {
     });
 
     it('should save with the correct format and close the modal', async () => {
-      await waitFor(async () => {
+      await act(async () => {
         fireEvent.change(await screen.findByRole('textbox'), {
           target: { value: 'My new relationship type' },
         });
