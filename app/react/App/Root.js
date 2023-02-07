575cab849f0478c13a94c8e94ad77d1cd96bdc03
mfacar <mercy@huridocs.org>
2023-02-07 10:53:48 -0500

    reverts import of cdn for flowbite

--- app/react/App/Root.js
+++ app/react/App/Root.js
@@ -28,9 +28,6 @@ const googelFonts = (
     href="https://fonts.googleapis.com/css?family=Roboto+Mono:100,300,400,500,700|Roboto+Slab:100,300,400,700|Roboto:100,300,400,500,700,900"
   />
 );
-// const flowbite = (
-//   <link rel="stylesheet" href="https://unpkg.com/flowbite@latest/dist/flowbite.min.css" />
-// );
 
 const getFaviconURL = reduxData => {
   const favicon = reduxData.settings.collection.get('favicon');
@@ -42,7 +39,7 @@ const getFaviconURL = reduxData => {
   return favicon;
 };
 
-export const headTag = (head, CSS, reduxData) => (
+const headTag = (head, CSS, reduxData) => (
   <head>
     {head.title.toComponent()}
     {head.meta.toComponent()}
@@ -60,7 +57,6 @@ export const headTag = (head, CSS, reduxData) => (
     )}
     {googelFonts}
     <link rel="shortcut icon" href={getFaviconURL(reduxData)} />
-    {/* {flowbite} */}
   </head>
 );
 
@@ -131,4 +127,5 @@ Root.propTypes = {
   assets: PropTypes.object,
 };
 
+export { headTag };
 export default Root;
