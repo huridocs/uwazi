5edea3d9c6023bccaa83d212e947e6a5d3f1399d
mfacar <mercy@huridocs.org>
2023-02-16 18:11:38 -0500

    update of test

--- app/react/stories/specs/LocalTable.cy.tsx
+++ app/react/stories/specs/LocalTable.cy.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { mount } from '@cypress/react18';
 import { composeStories } from '@storybook/testing-react';
-
 import _ from 'lodash';
 import * as stories from '../LocalTable.stories';
 
 const { Basic } = composeStories(stories);
 
 describe('LocalTable.cy.tsx', () => {
-  it('Should empty the field when clicking the cross', () => {
+  it('Should return a table with the columns and row specified', () => {
     mount(<Basic />);
     const toStrings = (cells: JQuery<HTMLElement>) => _.map(cells, 'textContent');
-
     cy.get('tr th').then(toStrings).should('eql', ['', 'Icon', 'Title', 'Date added']);
+    const firstRow = cy.get('tbody>tr').get('td');
+    firstRow.eq(0).get('[type="checkbox"]');
   });
 });
