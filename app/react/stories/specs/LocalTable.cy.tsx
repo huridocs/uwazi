574a4c43cd07e38f3156e1c0a75023596c5d9c46
mfacar <mercy@huridocs.org>
2023-02-22 14:36:43 -0500

    basic tests for table

--- app/react/stories/specs/LocalTable.cy.tsx
+++ app/react/stories/specs/LocalTable.cy.tsx
@@ -7,13 +7,32 @@ import * as stories from '../LocalTable.stories';
 const { Basic } = composeStories(stories);
 
 describe('LocalTable.cy.tsx', () => {
-  it('Should return a table with the columns and row specified', () => {
+  beforeEach(() => {
     mount(<Basic />);
+  });
+
+  const checkRowContent = (rowNumber: number, cellsContent: string[]) => {
+    cellsContent.forEach((content, index) =>
+      cy.get(`tbody>tr:nth-child(${rowNumber}) td:nth-child(${index + 2})`).contains(content)
+    );
+  };
+
+  it('Should return a table with the columns and row specified', () => {
     const toStrings = (cells: JQuery<HTMLElement>) => _.map(cells, 'textContent');
     cy.get('tr th').then(toStrings).should('eql', ['', 'Icon', 'Title', 'Date added']);
     cy.get('tbody>tr td').get('[type="checkbox"]');
-    cy.get('tbody>tr:first-child td:nth-child(3)').debug();
-    cy.get('tbody>tr:first-child td:nth-child(3)').contains('Entity 1');
-    cy.get('tbody>tr:first-child td:nth-child(4)').contains('1676306456');
+    checkRowContent(1, ['check', 'Entity 1', '1676306456']);
+    checkRowContent(2, ['plus', 'Entity 2', '1676425085']);
+  });
+
+  it('Should be sortable by title', () => {
+    cy.get('tr th').contains('Title').click();
+    checkRowContent(1, ['plus', 'Entity 2', '1676425085']);
+    checkRowContent(2, ['check', 'Entity 1', '1676306456']);
+  });
+
+  it('Should set the specified class of a cell', () => {
+    cy.get('tbody>tr:nth-child(1) td:nth-child(3)').should('not.have.class', 'italic');
+    cy.get('tbody>tr:nth-child(1) td:nth-child(4)').should('have.class', 'italic');
   });
 });
