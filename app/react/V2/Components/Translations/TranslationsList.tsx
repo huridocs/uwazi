6a69b646a464151e631a4aca5859c1a294395509
Santiago <borrajosantiago@gmail.com>
2023-02-23 13:09:46 -0300

    added a placeholder localtable

--- app/react/V2/Components/Translations/TranslationsList.tsx
+++ app/react/V2/Components/Translations/TranslationsList.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
+import { Checkbox } from 'flowbite-react';
+import { Translate } from 'app/I18N';
 import { translationsAtom } from './atoms';
+import { LocalTable } from '../Table/PlaceholderLocalTable';
+
+const renderCheckbox = data => <Checkbox />;
 
 const TranslationsList = () => {
   const translations = useRecoilValue(translationsAtom);
-  return <div>{JSON.stringify(translations, null, 2)}</div>;
+  console.log(translations);
+  return (
+    <div className="tw-content" style={{ width: '100%' }}>
+      <div className="p-5">
+        <h1 className="text-2xl">Translations</h1>
+        <LocalTable
+          columns={[
+            { id: 'select', Header: '', Cell: renderCheckbox },
+            { Header: 'Icon', accessor: 'icon', disableSortBy: true },
+            { Header: 'Title', accessor: 'title', id: 'title' },
+            { Header: 'Date added', accessor: 'created', disableSortBy: true, className: 'italic' },
+          ]}
+          data={[
+            { title: 'Entity 1', created: 1676306456, icon: 'check' },
+            { title: 'Entity 2', created: 1676425085, icon: 'plus' },
+          ]}
+          title={<Translate>System</Translate>}
+        />
+      </div>
+    </div>
+  );
 };
 
 export { TranslationsList };
