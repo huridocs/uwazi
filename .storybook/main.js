2d6589fb3c50feee8b14e6a7479ae1c15116821e
Santiago <borrajosantiago@gmail.com>
2023-02-23 16:43:57 -0300

    defined global styles for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,29 +2,31 @@ const custom = require('../webpack.config.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  "stories": [
-    "../app/react/stories/**/*.stories.mdx",
-    "../app/react/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../app/react/stories/**/*.stories.mdx',
+    '../app/react/stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
   },
-  "previewHead": (head) => (`
+  previewHead: head => `
     ${head}
     <style>
-      html, body {
-        background: #fcfcfc;
+      html {
+        /* This shold match the application globals */
+        font-family: sans;
+        font-size: 16px;
       }
     </style>
-  `),
-  webpackFinal: async (config) => {
+  `,
+  webpackFinal: async config => {
     config.plugins.push(new MiniCssExtractPlugin({}));
-    return { ...config,  module: { ...config.module, rules: custom.module.rules } };
+    return { ...config, module: { ...config.module, rules: custom.module.rules } };
   },
-}
+};
