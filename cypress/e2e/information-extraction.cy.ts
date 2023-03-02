bfcb3b8c0bb4fea7cfbd67e64429fbff4c7d69a6
Kevin <kevin@huridocs.org>
2023-03-02 19:32:11 +0300

    Updated cypress tests

--- cypress/e2e/information-extraction.cy.ts
+++ cypress/e2e/information-extraction.cy.ts
@@ -6,6 +6,7 @@ const labelEntityTitle = (
   selector: string = 'span[role="presentation"]'
 ) => {
   cy.get('.view-doc').eq(entityPos).click();
+  cy.wait(2000);
   //@ts-ignore
   cy.contains(selector, selectValue).setSelection(selectValue);
   cy.get('button.edit-metadata').click();
@@ -20,6 +21,12 @@ const labelEntityTitle = (
 const changeLanguage = () => {
   cy.get('.menuNav-language > .dropdown').click();
   cy.get('div[aria-label="Languages"]  li.menuNav-item:nth-child(2) a').click();
+};
+
+const englishLoggedInUwazi = () => {
+  cy.visit('http://localhost:3000');
+  changeLanguage();
+  login('admin', 'admin');
 };
 
 describe('Information Extraction', () => {
@@ -37,26 +44,50 @@ describe('Information Extraction', () => {
     labelEntityTitle(1, 'Uwazi Heroes Investigation');
   });
 
-  it('Should configure properties', () => {
+  it('Should create an extractor', () => {
     cy.get('.only-desktop a[aria-label="Settings"]').click();
     cy.contains('span', 'Metadata Extraction').click();
-    cy.contains('span', 'Configure properties').click();
+    cy.contains('span', 'Create Extractor').click();
+    cy.get('input.extractor-name-input').type('Extractor 1');
     cy.contains('span.multiselectItem-name', 'Ordenes del presidente').click();
-    cy.contains('span.multiselectItem-name', 'Ordenes de la corte').click();
-    cy.get('button.btn-success').click();
-    cy.get('.table tbody tr').should('have.length', 2);
-    const text = 'Ordenes del presidente,Ordenes de la corte';
+    cy.get('.multiselectChild.is-active label[for="58ada34c299e82674854505b-title"]').click();
+    cy.contains('span.multiselectItem-name', 'Causa').click();
+    cy.get('.multiselectChild.is-active label[for="58ada34c299e826748545061-title"]').click();
+    cy.contains('.modal-footer .extractor-footer span', 'Add').click();
+    cy.get('.table tbody tr').should('have.length', 1);
+    const text = 'Ordenes del presidenteCausa';
     cy.get('td.templateNameViewer').eq(0).should('contain.text', text);
-    cy.get('td.templateNameViewer').eq(1).should('contain.text', text);
+  });
+
+  it('should edit an extractor', () => {
+    englishLoggedInUwazi();
+    cy.get('.only-desktop a[aria-label="Settings"]').click();
+    cy.contains('span', 'Metadata Extraction').click();
+    cy.get('.extractor-checkbox > input').click();
+    cy.contains('span', 'Edit Extractor').click();
+    cy.get('input.extractor-name-input').type(' edited');
+    cy.get('.multiselectChild.is-active label[for="58ada34c299e826748545061-title"]').click();
+    cy.contains('span.multiselectItem-name', 'Ordenes de la corte').click();
+    cy.get('.multiselectChild.is-active label[for="58ada34c299e82674854504e-title"]').click();
+    cy.contains('.modal-footer .extractor-footer span', 'Save').click();
+    cy.get('.table tbody tr').should('have.length', 1);
+    const text = 'Ordenes del presidente';
+    cy.get('td.templateNameViewer').eq(0).should('contain.text', text);
   });
 
   it('should show title initial suggestion states as Empty / Label', () => {
-    cy.visit('http://localhost:3000');
-    changeLanguage();
-    login('admin', 'admin');
+    englishLoggedInUwazi();
     cy.get('.only-desktop a[aria-label="Settings"]').click();
     cy.contains('span', 'Metadata Extraction').click();
 
-    cy.get('a.btn-success.btn-xs').eq(1).click();
+    cy.get('a.btn-success.btn-xs').click();
+    cy.get('table > tbody > tr:nth-child(1) > td:nth-child(4)').should(
+      'contain.text',
+      'Lorem Ipsum'
+    );
+    cy.get('table > tbody > tr:nth-child(2) > td:nth-child(4)').should(
+      'contain.text',
+      'Artavia Murillo y otros. Resolución de la Corte IDH de 31 de marzo de 2014'
+    );
   });
 });
