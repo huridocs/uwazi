054c7d304473e8abf5c11e7249a2e3ba78b5ad31
Laszlo Kecskes <laszlokecskes@protonmail.com>
2023-03-02 12:58:48 +0100

    sfixing selects

--- app/api/migrations/migrations/125-thesaurus_group_child_id_fix/index.js
+++ app/api/migrations/migrations/125-thesaurus_group_child_id_fix/index.js
@@ -7,28 +7,74 @@ const generateNewIds = async db => {
     dict => dict.values
   );
   dictionaries.forEach(dict => {
+    const idMapForThesaurus = {};
     const groups = dict.values.filter(v => v.values);
     groups.forEach(group => {
       const groupId = group.id;
       const childIds = new Set(group.values.map(v => v.id));
       if (childIds.has(groupId)) {
         const newGroupId = generateID();
-        idMap[groupId] = newGroupId;
+        idMapForThesaurus[groupId] = newGroupId;
         reindex = true;
       }
+      idMap[dict._id.toString()] = idMapForThesaurus;
     });
   });
   return { idMap, reindex };
 };
 
 const updateGroupIds = async (db, idMap) => {
-  const pairs = Object.entries(idMap);
+  const flatIdMap = Object.fromEntries(
+    Object.values(idMap)
+      .map(v => Object.entries(v))
+      .flat()
+  );
+  const pairs = Object.entries(flatIdMap);
   for (let i = 0; i < pairs.length; i += 1) {
     const [oldId, newId] = pairs[i];
     // eslint-disable-next-line no-await-in-loop
     await db
       .collection('dictionaries')
       .updateMany({ 'values.id': oldId }, { $set: { 'values.$.id': newId } });
+  }
+};
+
+const selectTypes = new Set(['select', 'multiselect']);
+
+const getSelectNamesPerContent = async db => {
+  const templates = await db.collection('templates').find({}).toArray();
+  const properties = templates.map(t => t.properties || []).flat();
+  const selects = properties.filter(p => selectTypes.has(p.type));
+
+  const contents = Array.from(new Set(selects.map(s => s.content)));
+  const namesPerContent = Object.fromEntries(contents.map(c => [c, new Set()]));
+
+  selects.forEach(s => {
+    namesPerContent[s.content].add(s.name);
+  });
+
+  return Object.fromEntries(Object.entries(namesPerContent).map(([k, v]) => [k, Array.from(v)]));
+};
+
+// eslint-disable-next-line max-statements
+const fixSelectsAndMultiSelects = async (db, idMap) => {
+  const namesPerContent = Object.entries(await getSelectNamesPerContent(db));
+  for (let i = 0; i < namesPerContent.length; i += 1) {
+    const [thesaurusId, propertyNames] = namesPerContent[i];
+    const idPairs = Object.entries(idMap[thesaurusId] || {});
+    for (let j = 0; j < propertyNames.length; j += 1) {
+      const propertyName = propertyNames[j];
+      for (let k = 0; k < idPairs.length; k += 1) {
+        const [oldId, newId] = idPairs[k];
+        // eslint-disable-next-line no-await-in-loop
+        await db
+          .collection('entities')
+          .updateMany(
+            { [`metadata.${propertyName}.parent.id`]: oldId },
+            { $set: { [`metadata.${propertyName}.$.parent.id`]: newId } }
+          );
+      }
+    }
   }
 };
 
@@ -50,5 +96,6 @@ export default {
     this.reindex = reindex;
 
     await updateGroupIds(db, idMap);
+    await fixSelectsAndMultiSelects(db, idMap);
   },
 };
