6753ce46e4b7bfb71ffc101e9fa9265f3ccf41e7
Kevin <kevin@huridocs.org>
2023-03-06 19:31:52 +0300

    Updated migration

--- app/api/migrations/migrations/126-update_translations/specs/126-update_translations.spec.js
+++ app/api/migrations/migrations/126-update_translations/specs/126-update_translations.spec.js
@@ -13,7 +13,7 @@ describe('migration update translations of settings tooltips', () => {
   });
 
   it('should have a delta number', () => {
-    expect(migration.delta).toBe(125);
+    expect(migration.delta).toBe(126);
   });
 
   it('should update the keys that have changed', async () => {
