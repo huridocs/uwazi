99e27e81ccc7c7cf33b98de428895fe520816c3c
Kevin <kevin@huridocs.org>
2023-03-06 16:47:24 +0300

    Updated timeouts

--- cypress/e2e/information-extraction.cy.ts
+++ cypress/e2e/information-extraction.cy.ts
@@ -94,7 +94,7 @@ describe('Information Extraction', () => {
 
   it(
     'should show title initial suggestion states as Empty / Label',
-    { defaultCommandTimeout: 10000 },
+    { defaultCommandTimeout: 12000 },
     () => {
       englishLoggedInUwazi();
       navigateToMetadataExtractionPage();
