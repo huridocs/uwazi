d5b0f816ff34254e3bfb0b7631893071b0e4210d
Santiago <borrajosantiago@gmail.com>
2023-03-13 10:13:05 -0300

    updated test

--- app/react/stories/specs/Table.cy.tsx
+++ app/react/stories/specs/Table.cy.tsx
@@ -6,6 +6,8 @@ import * as stories from '../Table.stories';
 
 const { Basic } = composeStories(stories);
 
+const data = Basic.args?.data || [];
+
 describe('Table', () => {
   beforeEach(() => {
     mount(<Basic />);
@@ -13,24 +15,26 @@ describe('Table', () => {
 
   const checkRowContent = (rowNumber: number, cellsContent: string[]) => {
     cellsContent.forEach((content, index) =>
-      cy.get(`tbody>tr:nth-child(${rowNumber}) td:nth-child(${index + 2})`).contains(content)
+      cy
+        .get(`tbody.text-gray-900 > :nth-child(${rowNumber}) > :nth-child(${index + 2})`)
+        .contains(content)
     );
   };
 
   it('Should return a table with the columns and row specified', () => {
     const toStrings = (cells: JQuery<HTMLElement>) => map(cells, 'textContent');
-    cy.get('tr th').then(toStrings).should('eql', ['', 'Icon', 'Title', 'Date added']);
+    cy.get('tr th').then(toStrings).should('eql', ['', 'Title', 'Description', 'Date added']);
     cy.get('tbody>tr td').get('[type="checkbox"]');
 
-    checkRowContent(1, ['plus', 'Entity 2', '2']);
-    checkRowContent(2, ['check', 'Entity 1', '1']);
-    checkRowContent(3, ['flag', 'Entity 3', '3']);
+    checkRowContent(1, ['Entity 2', data[0].description, '2']);
+    checkRowContent(2, ['Entity 1', data[1].description, '1']);
+    checkRowContent(3, ['Entity 3', data[2].description, '3']);
   });
 
   it('Should be sortable by title', () => {
     cy.get('tr th').contains('Title').click();
-    checkRowContent(1, ['check', 'Entity 1', '1']);
-    checkRowContent(2, ['plus', 'Entity 2', '2']);
-    checkRowContent(3, ['flag', 'Entity 3', '3']);
+    checkRowContent(1, ['Entity 1', data[1].description, '1']);
+    checkRowContent(2, ['Entity 2', data[0].description, '2']);
+    checkRowContent(3, ['Entity 3', data[2].description, '3']);
   });
 });
