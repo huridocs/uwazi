196caaa1da4c1345c914f2c86887504dfc86930b
Santiago <borrajosantiago@gmail.com>
2023-03-20 16:28:41 -0300

    updated selectors and asserts

--- app/react/stories/specs/Notification.cy.tsx
+++ app/react/stories/specs/Notification.cy.tsx
@@ -8,23 +8,23 @@ const { Basic, WithHeading, WithDetails } = composeStories(stories);
 describe('Notification', () => {
   it('should render the heading when passed', () => {
     mount(<WithHeading />);
-    cy.get('.text-lg').should('exist');
+    cy.contains('.text-lg', 'This is the title of the notification').should('exist');
 
     mount(<Basic />);
-    cy.get('.text-lg').should('not.exist');
+    cy.contains('.text-lg', 'This is the title of the notification').should('not.exist');
   });
 
   it('should not show the button for details when details are not available', () => {
     mount(<Basic />);
-    cy.get('#details').should('not.exist');
+    cy.contains('View more').should('not.exist');
   });
 
   it('should show and hide more information', () => {
     mount(<WithDetails />);
     cy.contains('View more').click();
-    cy.get('.text-sm.mb-4').should('be.visible');
+    cy.contains('.text-sm.mb-4', 'This is some extra information').should('be.visible');
 
     cy.contains('View less').click();
-    cy.get('.text-sm.mb-4').should('not.exist');
+    cy.contains('.text-sm.mb-4', 'This is some extra information').should('not.exist');
   });
 });
