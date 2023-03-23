4f7acb59064b12c061161e5b26eba0fdce0f4a67
Santiago <borrajosantiago@gmail.com>
2023-03-23 12:14:34 -0300

    small refactor

--- app/react/V2/Components/UI/NotificationsContainer.tsx
+++ app/react/V2/Components/UI/NotificationsContainer.tsx
@@ -4,64 +4,66 @@ import { notificationAtom } from 'app/V2/atoms';
 import { Notification } from 'V2/Components/UI/Notification';
 
 const NotificationsContainer = () => {
+  const timeout = 6000;
   const [timerId, setTimerId] = useState<NodeJS.Timeout>();
   const notification = useRecoilValue(notificationAtom);
+  const resetRecoilAtom = useResetRecoilState(notificationAtom);
   const notificationIsSet = notification.text && notification.type;
-  const resetState = useResetRecoilState(notificationAtom);
 
   useEffect(() => {
     if (notificationIsSet && !timerId) {
       const timer = setTimeout(() => {
-        resetState();
-      }, 6000);
+        resetRecoilAtom();
+      }, timeout);
 
       setTimerId(timer);
     }
 
-    return () => clearTimeout(timerId);
-  }, [resetState, notificationIsSet, timerId]);
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+        setTimerId(undefined);
+      }
+    };
+  }, [resetRecoilAtom, notificationIsSet, timerId]);
 
   const onClickHandler = () => {
-    resetState();
+    resetRecoilAtom();
   };
 
-  const handleMouseEnter = () => {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-  };
+  const handleMouseEnter = () => timerId && clearTimeout(timerId);
 
   const handleMouseLeave = () => {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
+    if (timerId) clearTimeout(timerId);
 
     const timer = setTimeout(() => {
-      resetState();
-    }, 6000);
+      resetRecoilAtom();
+    }, timeout);
 
     setTimerId(timer);
   };
 
+  if (!notificationIsSet) {
+    return <div />;
+  }
+
   return (
     <div className="tw-content">
-      {notificationIsSet && (
-        <div
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className="fixed bottom-1 left-2 md:w-2/5 w-4/5 z-10"
-        >
-          <div className="shadow-lg">
-            <Notification
-              type={notification.type}
-              text={notification.text}
-              details={notification.details}
-              heading={notification.heading}
-              dismissAction={onClickHandler}
-            />
-          </div>
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className="fixed bottom-1 left-2 md:w-2/5 w-4/5 z-10"
+      >
+        <div className="shadow-lg">
+          <Notification
+            type={notification.type}
+            text={notification.text}
+            details={notification.details}
+            heading={notification.heading}
+            dismissAction={onClickHandler}
+          />
         </div>
-      )}
+      </div>
     </div>
   );
 };
