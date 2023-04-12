1a0f9b2faa1b0723a309281844635512c8db4c01
A happy cat <acasadotorres@gmail.com>
2023-04-12 11:12:09 +0200

    IX e2e refactor

--- cypress/e2e/information-extraction.cy.ts
+++ cypress/e2e/information-extraction.cy.ts
@@ -44,16 +44,19 @@ describe('Information Extraction', () => {
   it('Should create an extractor', () => {
     navigateToMetadataExtractionPage();
     cy.contains('span', 'Create Extractor').click();
-    cy.get('[data-testid=modal]').as('newExtractorModal');
-    cy.get('@newExtractorModal').get('input').type('Extractor 1');
-    cy.get('@newExtractorModal').contains('li', 'Ordenes del presidente').as('firstTemplate');
-    cy.get('@firstTemplate').find('label').click();
+    cy.get('[data-testid=modal]').get('input').type('Extractor 1');
+
+    cy.get('[data-testid=modal]')
+      .contains('li', 'Ordenes del presidente')
+      .as('firstTemplate')
+      .click();
     cy.get('@firstTemplate').contains('label', 'Title').click();
-    cy.get('@newExtractorModal').contains('li', 'Causa').as('secondTemplate');
-    cy.get('@secondTemplate').find('label').click();
+
+    cy.get('[data-testid=modal]').contains('li', 'Causa').as('secondTemplate').click();
     cy.get('@secondTemplate').contains('label', 'Title').click();
+
     cy.contains('button', 'Add').click();
-    cy.get('.table tbody tr').should('have.length', 1); // Forced to wait for the table to populate
+    cy.contains('.table td', 'Extractor 1');
     cy.get('.table').toMatchImageSnapshot();
   });
 
@@ -71,13 +74,15 @@ describe('Information Extraction', () => {
     cy.get('.extractor-checkbox > input').click();
     cy.contains('button', 'Edit Extractor').click();
     cy.get('input.extractor-name-input').type(' edited');
+
     cy.contains('.multiselectChild label', 'Title').click();
-    cy.contains('li', 'Ordenes de la corte').as('template');
-    cy.get('@template').find('label').click();
+
+    cy.contains('li', 'Ordenes de la corte').as('template').click();
     cy.get('@template').contains('label', 'Title').click();
+
     cy.contains('button', 'Save').click();
-    cy.contains('.table thead tr th span', 'Property').should('be.visible');
-    cy.get('.table').toMatchImageSnapshot();
+    cy.get('[data-testid=modal]').should('not.be.visible');
+    cy.contains('.table td', 'Extractor 1 edited');
   });
 
   it('should show title initial suggestion states as Empty / Label', () => {
@@ -114,7 +119,6 @@ describe('Information Extraction', () => {
     navigateToMetadataExtractionPage();
     cy.get('.extractor-checkbox input').click();
     cy.contains('button', 'Delete').click();
-    cy.contains('button', 'Create Extractor').should('be.visible');
     cy.get('table').toMatchImageSnapshot();
   });
 });
