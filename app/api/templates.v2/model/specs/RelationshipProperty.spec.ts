f6ef56701377fa92e1e29289e117c2d3e5ac679b
Laszlo Kecskes <laszlokecskes@protonmail.com>
2023-04-18 10:40:54 +0200

    correct failing tests

--- app/api/templates.v2/model/specs/RelationshipProperty.spec.ts
+++ app/api/templates.v2/model/specs/RelationshipProperty.spec.ts
@@ -1,10 +1,14 @@
 import { MatchQueryNode } from 'api/relationships.v2/model/MatchQueryNode';
 import { TraversalQueryNode } from 'api/relationships.v2/model/TraversalQueryNode';
+import { getFixturesFactory } from 'api/utils/fixturesFactory';
 import { RelationshipProperty } from '../RelationshipProperty';
+
+const fixtureFactory = getFixturesFactory();
 
 describe('buildQueryRootedInTemplate()', () => {
   it('should return query traversing from the entities of the template of the property to the configured pathes', () => {
     const property = new RelationshipProperty(
+      fixtureFactory.id('prop1').toString(),
       'prop1',
       'prop1',
       [new TraversalQueryNode('out', {}, [new MatchQueryNode()])],
