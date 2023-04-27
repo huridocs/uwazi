3b7efcc9e59c9941c55d1062ec138d03892ab87d
Laszlo Kecskes <laszlokecskes@protonmail.com>
2023-04-27 16:58:52 +0200

    deletion flow works

--- app/react/Entities/components/V2NewRelationshipsBoard.js
+++ app/react/Entities/components/V2NewRelationshipsBoard.js
@@ -1,4 +1,5 @@
 /* eslint-disable react/button-has-type */
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
@@ -8,11 +9,14 @@ import { setTargetDocument } from 'app/Connections/actions/actions';
 import SearchForm from 'app/Connections/components/SearchForm';
 import SearchResults from 'app/Connections/components/SearchResults';
 import { Icon } from 'app/UI';
+import { objectIndex as _objectIndex } from 'shared/data_utils/objectIndex';
 import {
   deleteRelationships,
   getRelationshipsByEntity,
   saveRelationship,
 } from '../actions/specs/V2NewRelationshipsActions';
+
+const objectIndex = _.memoize(_objectIndex);
 
 class V2NewRelationshipsBoard extends Component {
   constructor(props, context) {
@@ -32,16 +36,40 @@ class V2NewRelationshipsBoard extends Component {
 
   async saveRelationship() {
     const { sharedId, targetDocument } = this.props;
-    await saveRelationship(this.newEntryType, sharedId, targetDocument);
+    const [saved] = await saveRelationship(this.newEntryType, sharedId, targetDocument);
+    this.relationships.push(saved);
+    this.forceUpdate();
+  }
+
+  deleteRelationship(id) {
+    return async () => {
+      await deleteRelationships([id]);
+      this.relationships = this.relationships.filter(r => r._id !== id);
+      this.forceUpdate();
+    };
   }
 
   render() {
     const { sharedId, searchResults, uiState, relationTypes, targetDocument } = this.props;
+    const relTypesById = objectIndex(
+      relationTypes,
+      rt => rt._id,
+      rt => rt
+    );
     return (
       <>
         <div>{sharedId}</div>
         <div>Existing:</div>
-        <div>{JSON.stringify(this.relationships)}</div>
+        <div>
+          {this.relationships.map(r => (
+            <div>
+              {relTypesById[r.type].name}&emsp;
+              <Icon icon="arrow-right" />
+              &emsp;{r.to.entity}&emsp;
+              <button onClick={this.deleteRelationship(r._id).bind(this)}>X</button>
+            </div>
+          ))}
+        </div>
         <br />
         <div>Add new:</div>
         <div>
@@ -54,7 +82,9 @@ class V2NewRelationshipsBoard extends Component {
               <option value={rt._id}>{rt.name}</option>
             ))}
           </select>
+          &emsp;
           <Icon icon="arrow-right" />
+          &emsp;
           <input
             type="text"
             id="newEntryTargetSelector"
@@ -62,6 +92,7 @@ class V2NewRelationshipsBoard extends Component {
             value={targetDocument || 'select target entity'}
             disabled
           />
+          &emsp;
           <button disabled={!targetDocument} onClick={this.saveRelationship.bind(this)}>
             Save
           </button>
