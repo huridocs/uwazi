25436dcd7b49ad3ccf92e19ef8b6731e7a609c21
mfacar <mercy@huridocs.org>
2023-04-28 19:36:23 -0500

    adds system translations

---
+++ app/api/migrations/migrations/133-update_translations/index.js
@@ -0,0 +1,65 @@
+const newKeys = [
+  { key: 'Active languages' },
+  { key: 'Install' },
+  { key: 'Install language' },
+  { key: 'Install Language' },
+  { key: 'Please type in' },
+  { key: 'Uninstall' },
+];
+
+const updateTranslation = (currentTranslation, keysToUpdate, loc) => {
+  const translation = { ...currentTranslation };
+  const newTranslation = keysToUpdate.find(row => row.key === currentTranslation.key);
+  if (newTranslation) {
+    translation.key = newTranslation.newKey;
+    if (loc === 'en' || currentTranslation.value === newTranslation.oldValue) {
+      translation.value = newTranslation.newValue;
+    }
+  }
+  return translation;
+};
+
+export default {
+  delta: 133,
+
+  reindex: false,
+
+  name: 'update_translations',
+
+  description: 'Updates translations for new Languages UI',
+
+  async up(db) {
+    const keysToInsert = newKeys;
+    const translations = await db.collection('translations').find().toArray();
+    const locToSystemContext = {};
+    translations.forEach(tr => {
+      locToSystemContext[tr.locale] = tr.contexts.find(c => c.id === 'System');
+    });
+
+    const alreadyInDB = [];
+    Object.entries(locToSystemContext).forEach(([loc, context]) => {
+      const contextValues = context.values.reduce((newValues, currentTranslation) => {
+        const translation = updateTranslation(currentTranslation, [], loc);
+        newValues.push(translation);
+        keysToInsert.forEach(newEntry => {
+          if (newEntry.key === currentTranslation.key) {
+            alreadyInDB.push(currentTranslation.key);
+          }
+        });
+        return newValues;
+      }, []);
+      keysToInsert
+        .filter(k => !alreadyInDB.includes(k.key))
+        .forEach(newEntry => {
+          contextValues.push({ key: newEntry.key, value: newEntry.key });
+        });
+      context.values = contextValues;
+    });
+
+    await Promise.all(
+      translations.map(tr => db.collection('translations').replaceOne({ _id: tr._id }, tr))
+    );
+
+    process.stdout.write(`${this.name}...\r\n`);
+  },
+};
