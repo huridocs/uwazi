f98e35291ec17e84d36d942432ecba57bd0ec8e5
Santiago <borrajosantiago@gmail.com>
2023-05-02 14:54:20 -0300

    responsive and overlay option

--- app/react/V2/Components/UI/Sidepanel.tsx
+++ app/react/V2/Components/UI/Sidepanel.tsx
@@ -6,15 +6,28 @@ const Sidepanel = () => {
   const isOpen = useRecoilValue(sidepanelShow);
   const { content, withOverlay } = useRecoilValue(sidepanelConfig);
 
-  const overlay = withOverlay ? 'bg-gray-900 bg-opacity-50 w-full' : '';
+  const sidepanelStyles = 'bg-white w-4/5 md:w-[400px] border-l-2';
+  const contentStyles = 'p-2';
 
-  return isOpen ? (
-    <aside className="bg-white absolute top-0 right-0 z-50 h-full overflow-y-auto overflow-x-hidden w-[400px] border-l-2">
-      <div className="p-2">{content}</div>
-    </aside>
-  ) : (
-    <div />
-  );
+  if (isOpen && withOverlay) {
+    return (
+      <div className="z-2 bg-gray-900 bg-opacity-50 fixed h-full w-full top-0 left-0">
+        <aside className={`z-3 absolute h-full top-0 right-0 ${sidepanelStyles}`}>
+          <div className={contentStyles}>{content}</div>
+        </aside>
+      </div>
+    );
+  }
+
+  if (isOpen) {
+    return (
+      <aside className={`z-2 fixed h-full top-0 right-0 ${sidepanelStyles}`}>
+        <div className={contentStyles}>{content}</div>
+      </aside>
+    );
+  }
+
+  return <div />;
 };
 
 export { Sidepanel };
--- app/react/stories/Sidepanel.stories.tsx
+++ app/react/stories/Sidepanel.stories.tsx
@@ -36,6 +36,18 @@ const PageContent = ({ withOverlay }: { withOverlay: boolean }) => {
         ac est consectetur, malesuada nisl nec, blandit lorem.
       </p>
 
+      <p className="mb-1">
+        Fusce id mi eu mauris bibendum dignissim nec in sem. Sed ultrices varius mauris quis
+        placerat. Donec imperdiet sodales diam sed imperdiet. Aenean a nisl venenatis lectus mattis
+        pellentesque. Duis fermentum ante a ultricies feugiat. Proin dapibus luctus purus id
+        viverra. Aenean a aliquet nibh. Aenean facilisis justo quis sem auctor, nec mollis tortor
+        placerat. Cras eget enim mollis, mollis risus gravida, pharetra risus. Mauris dapibus
+        malesuada mi, quis ornare felis imperdiet eget. Donec sed quam non dolor sodales hendrerit.
+        Aenean suscipit, velit sed laoreet cursus, ante odio tristique lectus, a porta eros felis eu
+        sem. Curabitur eu gravida dolor. Ut iaculis lacus vitae libero viverra interdum. Phasellus
+        ac est consectetur, malesuada nisl nec, blandit lorem.
+      </p>
+
       <hr className="mb-2" />
 
       <button
