3c66d0872b7b4dde92eee421927d2eeef8319434
Santiago <borrajosantiago@gmail.com>
2023-05-04 12:07:23 -0300

    animated sidepanel

--- app/react/V2/Components/UI/Sidepanel.tsx
+++ app/react/V2/Components/UI/Sidepanel.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRecoilValue } from 'recoil';
+import { useTransition, animated } from '@react-spring/web';
 import { XMarkIcon } from '@heroicons/react/20/solid';
 import { sidepanelConfig, sidepanelShow } from 'app/V2/atoms';
 import { Translate } from 'app/I18N';
@@ -32,30 +33,45 @@ const renderSidepanelContent = (
 const Sidepanel = () => {
   const isOpen = useRecoilValue(sidepanelShow);
   const { content, closeSidepanelFunction, title, withOverlay } = useRecoilValue(sidepanelConfig);
+  const transition = useTransition(isOpen, {
+    from: { x: 1000 },
+    enter: { x: 0 },
+    leave: { x: 1000 },
+  });
 
-  if (isOpen && withOverlay) {
-    return (
-      <div className="fixed h-full w-full top-0 left-0 flex">
-        <div
-          className="bg-gray-900 bg-opacity-50 w-0 md:flex-grow"
-          onClick={closeSidepanelFunction}
-        />
-        <aside className="bg-white w-full border-l-2 px-2 py-4 md:w-[400px]">
-          {renderSidepanelContent(content, closeSidepanelFunction, title)}
-        </aside>
-      </div>
+  if (withOverlay) {
+    return transition((animation, condition) =>
+      condition ? (
+        <div className="fixed h-full w-full top-0 left-0 flex">
+          <div
+            className="bg-gray-900 bg-opacity-50 w-0 md:flex-grow"
+            onClick={closeSidepanelFunction}
+          />
+          <animated.aside
+            style={animation}
+            className="bg-white w-full border-l-2 px-2 py-4 md:w-[400px]"
+          >
+            {renderSidepanelContent(content, closeSidepanelFunction, title)}
+          </animated.aside>
+        </div>
+      ) : (
+        <div />
+      )
     );
   }
 
-  if (isOpen) {
-    return (
-      <aside className="fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]">
+  return transition((animation, condition) =>
+    condition ? (
+      <animated.aside
+        style={animation}
+        className="fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
+      >
         {renderSidepanelContent(content, closeSidepanelFunction, title)}
-      </aside>
-    );
-  }
-
-  return <div />;
+      </animated.aside>
+    ) : (
+      <div />
+    )
+  );
 };
 
 export { Sidepanel };
