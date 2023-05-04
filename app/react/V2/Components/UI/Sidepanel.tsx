8f13e40ec76617baab66937da5d5bdbeb7d04e28
Santiago <borrajosantiago@gmail.com>
2023-05-04 19:55:34 -0300

    transitions with/out overlay

--- app/react/V2/Components/UI/Sidepanel.tsx
+++ app/react/V2/Components/UI/Sidepanel.tsx
@@ -5,28 +5,21 @@ import { XMarkIcon } from '@heroicons/react/20/solid';
 import { sidepanelConfig, sidepanelShow } from 'app/V2/atoms';
 import { Translate } from 'app/I18N';
 
-const sidepanelContent = (
-  content: React.ReactNode,
-  closeSidepanelFunction: () => any,
-  title?: React.ReactNode
-) => (
-  <>
-    <div className="flex mb-2 text-gray-500">
-      <h1 className="grow uppercase font-bold">{title}</h1>
-      <button
-        type="button"
-        className="justify-end"
-        data-testid="Close sidepanel"
-        onClick={closeSidepanelFunction}
-      >
-        <span className="sr-only">
-          <Translate>Close</Translate>
-        </span>
-        <XMarkIcon className="w-5" />
-      </button>
-    </div>
-    <div>{content}</div>
-  </>
+const sidepanelHeader = (closeSidepanelFunction: () => any, title?: React.ReactNode) => (
+  <div className="flex mb-2 text-gray-500">
+    <h1 className="grow uppercase font-bold">{title}</h1>
+    <button
+      type="button"
+      className="justify-end"
+      data-testid="Close sidepanel"
+      onClick={closeSidepanelFunction}
+    >
+      <span className="sr-only">
+        <Translate>Close</Translate>
+      </span>
+      <XMarkIcon className="w-5" />
+    </button>
+  </div>
 );
 
 const Sidepanel = () => {
@@ -35,23 +28,24 @@ const Sidepanel = () => {
 
   if (withOverlay) {
     return (
-      <Transition show={isOpen}>
-        <div className="fixed h-full w-full top-0 left-0 flex">
-          <Transition.Child
-            enter="transition-opacity ease-in duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-50 bg-gray-900 w-0 md:flex-grow"
-            onClick={closeSidepanelFunction}
-          />
-          <Transition.Child
-            as="aside"
-            enter="transition transform ease-in duration-300"
-            enterFrom="-translate-x-1000"
-            enterTo="translate-x-0 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
-          >
-            {sidepanelContent(content, closeSidepanelFunction, title)}
-          </Transition.Child>
-        </div>
+      <Transition show={isOpen} className="fixed h-full w-full top-0 left-0 flex">
+        <Transition.Child
+          className="transition-opacity duration-300 ease-in bg-gray-900 w-0 md:flex-grow"
+          enterFrom="opacity-0"
+          enterTo="opacity-50"
+          leaveTo="opacity-0"
+          onClick={closeSidepanelFunction}
+        />
+        <Transition.Child
+          as="aside"
+          className="transition transform duration-300 ease-in bg-white border-l-2 px-2 py-4 w-full md:w-[400px]"
+          enterFrom="-translate-x-[-500px]"
+          enterTo="translate-x-0"
+          leaveTo="-translate-x-[-500px]"
+        >
+          {sidepanelHeader(closeSidepanelFunction, title)}
+          <div>{content}</div>
+        </Transition.Child>
       </Transition>
     );
   }
@@ -60,14 +54,13 @@ const Sidepanel = () => {
     <Transition
       show={isOpen}
       as="aside"
-      enter="transition transform ease-in duration-300"
-      enterFrom="-translate-x-1000"
-      enterTo="translate-x-0 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
-      // leave="transition ease duration-300 transform"
-      // leaveFrom="translate-x-0 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
-      // leaveTo="-translate-x-1000"
+      className="transition transform ease-in duration-300 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
+      enterFrom="-translate-x-[-500px]"
+      enterTo="translate-x-0"
+      leaveTo="-translate-x-[-500px]"
     >
-      {sidepanelContent(content, closeSidepanelFunction, title)}
+      {sidepanelHeader(closeSidepanelFunction, title)}
+      <div>{content}</div>
     </Transition>
   );
 };
