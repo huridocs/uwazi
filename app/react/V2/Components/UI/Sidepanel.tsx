70b56f113f6c89e85c30b1827cc55ead6e5797cf
Santiago <borrajosantiago@gmail.com>
2023-05-04 19:04:38 -0300

    different animation handler

--- app/react/V2/Components/UI/Sidepanel.tsx
+++ app/react/V2/Components/UI/Sidepanel.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Transition } from '@headlessui/react';
 import { useRecoilValue } from 'recoil';
-import { useTransition, animated } from '@react-spring/web';
 import { XMarkIcon } from '@heroicons/react/20/solid';
 import { sidepanelConfig, sidepanelShow } from 'app/V2/atoms';
 import { Translate } from 'app/I18N';
 
-const renderSidepanelContent = (
+const sidepanelContent = (
   content: React.ReactNode,
   closeSidepanelFunction: () => any,
   title?: React.ReactNode
@@ -29,48 +29,46 @@ const renderSidepanelContent = (
   </>
 );
 
-// eslint-disable-next-line max-statements
 const Sidepanel = () => {
   const isOpen = useRecoilValue(sidepanelShow);
   const { content, closeSidepanelFunction, title, withOverlay } = useRecoilValue(sidepanelConfig);
-  const transition = useTransition(isOpen, {
-    from: { x: 1000 },
-    enter: { x: 0 },
-    leave: { x: 1000 },
-  });
 
   if (withOverlay) {
-    return transition((animation, condition) =>
-      condition ? (
+    return (
+      <Transition show={isOpen}>
         <div className="fixed h-full w-full top-0 left-0 flex">
-          <div
-            className="bg-gray-900 bg-opacity-50 w-0 md:flex-grow"
+          <Transition.Child
+            enter="transition-opacity ease-in duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-50 bg-gray-900 w-0 md:flex-grow"
             onClick={closeSidepanelFunction}
           />
-          <animated.aside
-            style={animation}
-            className="bg-white w-full border-l-2 px-2 py-4 md:w-[400px]"
+          <Transition.Child
+            as="aside"
+            enter="transition transform ease-in duration-300"
+            enterFrom="-translate-x-1000"
+            enterTo="translate-x-0 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
           >
-            {renderSidepanelContent(content, closeSidepanelFunction, title)}
-          </animated.aside>
+            {sidepanelContent(content, closeSidepanelFunction, title)}
+          </Transition.Child>
         </div>
-      ) : (
-        <div />
-      )
+      </Transition>
     );
   }
 
-  return transition((animation, condition) =>
-    condition ? (
-      <animated.aside
-        style={animation}
-        className="fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
-      >
-        {renderSidepanelContent(content, closeSidepanelFunction, title)}
-      </animated.aside>
-    ) : (
-      <div />
-    )
+  return (
+    <Transition
+      show={isOpen}
+      as="aside"
+      enter="transition transform ease-in duration-300"
+      enterFrom="-translate-x-1000"
+      enterTo="translate-x-0 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
+      // leave="transition ease duration-300 transform"
+      // leaveFrom="translate-x-0 fixed h-full w-full top-0 right-0 bg-white border-l-2 px-2 py-4 md:w-[400px]"
+      // leaveTo="-translate-x-1000"
+    >
+      {sidepanelContent(content, closeSidepanelFunction, title)}
+    </Transition>
   );
 };
 
