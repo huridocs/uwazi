8df85505fd81616a24c22d2f0bfa038fd089e1fe
mfacar <mercy@huridocs.org>
2023-05-08 23:01:03 -0500

    adds unit tests

--- app/react/V2/CustomHooks/specs/useApiCaller.spec.tsx
+++ app/react/V2/CustomHooks/specs/useApiCaller.spec.tsx
@@ -5,40 +5,77 @@ import { act, renderHook } from '@testing-library/react';
 import * as recoil from 'recoil';
 import { RecoilRoot, RecoilState } from 'recoil';
 import { RequestParams } from 'app/utils/RequestParams';
-import { notificationAtomType } from 'app/V2/atoms';
 import { useApiCaller } from '../useApiCaller';
 
 describe('describe useApiCaller', () => {
-  // eslint-disable-next-line max-statements
-  it('should handle a success response', async () => {
-    const setNotificationMock = jest.fn();
+  let apiCallerHook: {
+    current: {
+      requestAction: (
+        arg0: jest.Mock<any, any>,
+        arg1: RequestParams<{ data: string }>,
+        arg2: string
+      ) => any;
+    };
+  };
+  const setNotificationMock = jest.fn();
+
+  beforeEach(() => {
     jest
       .spyOn(recoil, 'useSetRecoilState')
-      .mockImplementation((_state: RecoilState<notificationAtomType>) => setNotificationMock);
+      .mockImplementation((_state: RecoilState<any>) => setNotificationMock);
 
-    const { result } = renderHook(() => useApiCaller(), { wrapper: RecoilRoot });
-    const apiMock = jest
-      .fn()
-      .mockResolvedValue(new Response(JSON.stringify({ data: 'result' }), { status: 200 }));
+    ({ result: apiCallerHook } = renderHook(() => useApiCaller(), { wrapper: RecoilRoot }));
+  });
 
-    let apiResult;
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  const checkHookInvokation = async (apiMock: jest.Mock<any, any>, success: boolean = true) => {
+    // eslint-disable-next-line max-statements
     await act(async () => {
-      apiResult = await result.current.requestAction(
+      const apiResult = await apiCallerHook.current.requestAction(
         apiMock,
         new RequestParams({ data: 'paramid' }),
         'successful action'
       );
-
-      const data = await apiResult.data;
-      const error = await apiResult.error;
-      expect(data).toEqual({ data: 'result' });
-      expect(error).toBeUndefined();
       expect(setNotificationMock).toHaveBeenCalled();
-      expect(setNotificationMock.mock.calls[0][0].type).toEqual('success');
-      expect(setNotificationMock.mock.calls[0][0].text.props.children).toEqual('successful action');
+
+      if (success) {
+        expect(await apiResult.data).toEqual({ data: 'result' });
+        expect(await apiResult.error).toBeUndefined();
+        expect(setNotificationMock.mock.calls[0][0].type).toEqual('success');
+        expect(setNotificationMock.mock.calls[0][0].text.props.children).toEqual(
+          'successful action'
+        );
+      } else {
+        expect(await apiResult.data).toBeUndefined();
+        expect(await apiResult.error).toEqual('An error occurred');
+        expect(setNotificationMock.mock.calls[0][0].type).toEqual('error');
+        expect(setNotificationMock.mock.calls[0][0].text.props.children).toEqual(
+          'An error occurred'
+        );
+      }
     });
+  };
+
+  it('should handle a success response', async () => {
+    const apiMock = jest
+      .fn()
+      .mockResolvedValue(new Response(JSON.stringify({ data: 'result' }), { status: 200 }));
+
+    await checkHookInvokation(apiMock, true);
   });
 
-  it.todo('should handle a response with error');
-  it.todo('should handle an exception');
+  it('should handle a response with error', async () => {
+    const apiMock = jest.fn().mockResolvedValue(new Response(JSON.stringify({}), { status: 500 }));
+
+    await checkHookInvokation(apiMock, false);
+  });
+
+  it('should handle an exception', async () => {
+    const apiMock = jest.fn().mockRejectedValue(new Error('An error occurred'));
+
+    await checkHookInvokation(apiMock, false);
+  });
 });
