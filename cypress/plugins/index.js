a56d0a5f3110307fbe384019454edfd46235c67f
mfacar <mercy@huridocs.org>
2023-05-18 14:54:51 -0500

    google headless window size

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,4 +4,10 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--window-size=1280,720');
+    }
+    return launchOptions;
+  });
 };
