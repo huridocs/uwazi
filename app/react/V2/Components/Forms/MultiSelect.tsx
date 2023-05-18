106c841d20996fa30d444c5b979f0f26e9909c99
Santiago <borrajosantiago@gmail.com>
2023-05-18 10:04:15 -0300

    changed multiselect strategy + test

--- app/react/V2/Components/Forms/MultiSelect.tsx
+++ app/react/V2/Components/Forms/MultiSelect.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Checkbox } from 'flowbite-react';
 import { sortBy } from 'lodash';
+import { usePopper } from 'react-popper';
+import { Popover, Transition } from '@headlessui/react';
 import { XMarkIcon, PlusCircleIcon } from '@heroicons/react/20/solid';
 import { Translate } from 'app/I18N';
-import { useOnClickOutside } from 'V2/shared/useOnClickOutside';
 import { Pill } from '../UI';
 
 type Option = { label: string; value: string; selected?: boolean };
@@ -14,56 +15,24 @@ interface MultiSelectProps {
   onChange?: (options: Option[]) => any;
 }
 
-const CONTEXT_MENU_MAX_HEIGHT = 224;
-
 const MultiSelect = ({ label, options, onChange = () => {} }: MultiSelectProps) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const contextMenuRef = useRef<HTMLUListElement>(null);
-
-  const [showMenu, setShowMenu] = useState(false);
-  const [verticalPosition, setVerticalPosition] = useState('0px');
   const [optionsState, setOptionsState] = useState<Option[]>(sortBy(options, 'label'));
-
-  useOnClickOutside<HTMLDivElement>(containerRef, () => setShowMenu(false));
+  const [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'bottom-end',
+  });
 
   const selectedOptions = optionsState.filter(option => option.selected);
 
   return (
-    <div
-      className="border rounded-lg border-gray-50 relative"
-      data-testid="multiselect-comp"
-      ref={containerRef}
-    >
+    <Popover className="border rounded-lg border-gray-50 relative" data-testid="multiselect-comp">
       <div className="border-b border-gray-50 bg-gray-50 p-2 flex justify-between">
         <div className="text-indigo-700 text-base">{label}</div>
         <div className="left-0">
-          <button
-            type="button"
-            className="text-indigo-700"
-            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-              setVerticalPosition('0px');
-              setShowMenu(!showMenu);
-              setTimeout(() => {
-                // @ts-ignore
-                const box = e.target.getBoundingClientRect();
-                const windowHeight = window.innerHeight;
-                const contextMenu = contextMenuRef.current?.getBoundingClientRect();
-
-                const contextMenuHeight = contextMenu
-                  ? contextMenu.height
-                  : CONTEXT_MENU_MAX_HEIGHT;
-                const contextMenuBottom = box.bottom + contextMenuHeight;
-                if (contextMenuBottom >= windowHeight) {
-                  const yPosition = -contextMenuHeight;
-                  setVerticalPosition(`${yPosition + box.height}px`);
-                } else {
-                  setVerticalPosition(`${box.height}px`);
-                }
-              }, 0);
-            }}
-          >
+          <Popover.Button ref={setReferenceElement} className="text-indigo-700">
             <PlusCircleIcon className="text-lg w-6" />
-          </button>
+          </Popover.Button>
         </div>
       </div>
 
@@ -95,11 +64,21 @@ const MultiSelect = ({ label, options, onChange = () => {} }: MultiSelectProps) 
         )}
       </div>
 
-      {showMenu ? (
-        <ul
-          ref={contextMenuRef}
-          style={{ top: verticalPosition || '0px' }}
+      <Transition
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-95 opacity-0"
+        enterTo="transform scale-100 opacity-100"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100"
+        leaveTo="transform scale-95 opacity-0"
+      >
+        <Popover.Panel
+          ref={setPopperElement}
+          style={styles.popper}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...attributes.popper}
           className="p-2 w-fit max-w-md rounded max-h-56 overflow-y-auto absolute right-8 shadow-md bg-white"
+          as="ul"
         >
           {optionsState.map((option: Option) => (
             <li key={option.label} className="py-1 flex gap-1 align-top">
@@ -120,9 +99,9 @@ const MultiSelect = ({ label, options, onChange = () => {} }: MultiSelectProps) 
               <span className="ml-2">{option.label}</span>
             </li>
           ))}
-        </ul>
-      ) : null}
-    </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 };
 
--- app/react/V2/Components/Forms/specs/MultiSelect.cy.tsx
+++ app/react/V2/Components/Forms/specs/MultiSelect.cy.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import { mount } from '@cypress/react18';
 import { composeStories } from '@storybook/testing-react';
+import { sortBy } from 'lodash';
 import * as stories from 'app/stories/Forms/MultiSelect.stories';
 
 const { Basic: MultiSelect } = composeStories(stories);
+
+const expectedArguments = sortBy(
+  [
+    { label: 'Someone', value: 'someone' },
+    { label: 'Another', value: 'another', selected: true },
+    { label: 'Another name', value: 'another name' },
+    { label: 'And another', value: 'and another' },
+    { label: 'Item A', value: 'item1' },
+    { label: 'Item B', value: 'item2' },
+    { label: 'Item C', value: 'item3' },
+    { label: 'Item F', value: 'item4' },
+    { label: 'Item G', value: 'item5' },
+    { label: 'Item E', value: 'item6' },
+    { label: 'Item I', value: 'item7' },
+    { label: 'Item J', value: 'item8' },
+    { label: 'Item H', value: 'item9' },
+    { label: 'Item with extra extra extra long name 1', value: 'lItem1' },
+    { label: 'Item with extra extra extra long name 2', value: 'lItem2' },
+    { label: 'Item with extra extra extra long name 3', value: 'lItem3' },
+    { label: 'Item with extra extra extra long name 4', value: 'lItem4' },
+    { label: 'Item with extra extra extra long name 5', value: 'lItem5' },
+    { label: 'Item with extra extra extra extra extraextraextra long name', value: 'xlItem' },
+  ],
+  'label'
+);
 
 describe('MultiSelect', () => {
   describe('Rendering', () => {
@@ -28,29 +54,12 @@ describe('MultiSelect', () => {
       cy.get('ul li').should('not.exist');
     });
 
-    it('should not close when clicking elements within the component', () => {
-      cy.get('[data-testid="multiselect-comp"] button').click();
-      cy.get('ul li').get('input').eq(1).click();
-      cy.get('ul li').get('input').eq(2).click();
-
-      cy.contains('div', 'Groups').click();
-      cy.get('[data-testid="pill-comp"]')
-        .eq(0)
-        .within(() => {
-          cy.get('button').click();
-        });
-
-      cy.get('ul li').should('have.length', 19);
-    });
-
     it('should sort the option by label', () => {
       cy.get('[data-testid="multiselect-comp"] button').click();
       cy.get('ul li').get('span').eq(1).should('contain', 'And another');
       cy.get('ul li').get('span').eq(2).should('contain', 'Another');
       cy.get('ul li').get('span').eq(19).should('contain', 'Someone');
     });
-
-    it('should position the menu inside the viewport', () => {});
   });
 
   describe('Main area', () => {
@@ -76,28 +85,19 @@ describe('MultiSelect', () => {
   });
 
   describe('onChange', () => {
-    // it('should execute the onchange action when adding or removing items and return the updated options', () => {
-    //   const onChangeSpy = cy.spy();
-    //   mount(
-    //     <MultiSelect
-    //       onChange={options => {
-    //         onChangeSpy(options);
-    //       }}
-    //     />
-    //   );
-    //   cy.get('[data-testid="multiselect-comp"] button').click();
-    //   cy.get('ul li')
-    //     .get('input')
-    //     .eq(1)
-    //     .click()
-    //     .then(() => {
-    //       // eslint-disable-next-line no-unused-expressions
-    //       expect(onChangeSpy).to.have.been.calledOnce;
-    //       const args = onChangeSpy.args[0];
-    //       console.log(some(args, { label: 'Another', value: 'another', selected: true }));
-    //       // eslint-disable-next-line no-unused-expressions
-    //       // expect().to.be.true;
-    //     });
-    // });
+    it('should execute the onchange action when adding or removing items and return the updated options', () => {
+      const onChangeSpy = cy.stub().as('onChange');
+      mount(
+        <MultiSelect
+          onChange={options => {
+            onChangeSpy(options);
+          }}
+        />
+      );
+      cy.get('[data-testid="multiselect-comp"] button').click();
+      cy.get('ul li').get('input').eq(1).click();
+
+      cy.get('@onChange').should('have.been.calledOnceWith', expectedArguments);
+    });
   });
 });
