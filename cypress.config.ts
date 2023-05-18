3e84fe09adb5c1b98544ed91297af7c4e7c76563
mfacar <mercy@huridocs.org>
2023-05-18 15:52:10 -0500

    atttemp

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'cypress';
 const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 export default defineConfig({
-  viewportWidth: 900,
-  viewportHeight: 960,
+  viewportWidth: 1366,
+  viewportHeight: 768,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -5,7 +5,7 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
   on('before:browser:launch', (browser, launchOptions) => {
-    if (browser.name.startsWith('chrom') && browser.isHeadless) {
+    if (browser.name.startsWith('chrom')) {
       launchOptions.args.push('--window-size=900,960');
     }
     return launchOptions;
