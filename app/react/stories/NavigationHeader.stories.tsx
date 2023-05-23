0be6688c909389bce204a7a5f6316808c851197e
Mercy <mercy@huridocs.org>
2023-05-22 23:00:12 -0500

    5764 - Storybook upgrade (#5844)
    * upgrade of storybook
    * upgrades storybook & updates stories
    * uses primary for main stories, merge fixes
    * fixes sidepanel story
    * new minor version

--- app/react/stories/NavigationHeader.stories.tsx
+++ app/react/stories/NavigationHeader.stories.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { NavigationHeader } from 'V2/Components/UI/NavigationHeader';
 import { LEGACY_createStore as createStore } from 'V2/shared/testingHelpers';
 
-const NavigationHeaderStory = {
+const meta: Meta<typeof NavigationHeader> = {
   title: 'Components/NavigationHeader',
   component: NavigationHeader,
   parameters: {
@@ -17,28 +17,34 @@ const NavigationHeaderStory = {
   },
 };
 
-const Template: ComponentStory<typeof NavigationHeader> = args => (
-  <MemoryRouter>
-    <Provider store={createStore()}>
-      <div className="tw-content">
-        <NavigationHeader backUrl={args.backUrl}>{args.children}</NavigationHeader>
-      </div>
-    </Provider>
-  </MemoryRouter>
-);
+type Story = StoryObj<typeof NavigationHeader>;
 
-const Basic = Template.bind({});
-const WithLink = Template.bind({});
-
-Basic.args = {
-  children: 'Page title',
+const Primary: Story = {
+  render: args => (
+    <MemoryRouter>
+      <Provider store={createStore()}>
+        <div className="tw-content">
+          <NavigationHeader backUrl={args.backUrl}>{args.children}</NavigationHeader>
+        </div>
+      </Provider>
+    </MemoryRouter>
+  ),
+};
+const Basic: Story = {
+  ...Primary,
+  args: {
+    children: 'Page title',
+  },
 };
 
-WithLink.args = {
-  children: 'Page title',
-  backUrl: '/backUrl',
+const WithLink = {
+  ...Primary,
+  args: {
+    children: 'Page title',
+    backUrl: '/backUrl',
+  },
 };
 
 export { Basic, WithLink };
 
-export default NavigationHeaderStory as ComponentMeta<typeof NavigationHeader>;
+export default meta;
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,8 +1,4 @@
-import { setGlobalConfig } from '@storybook/testing-react';
-import * as sbPreview from '../../.storybook/preview';
 import '../../app/react/App/styles/globals.css';
 
 import './commands';
 import './component';
-
-setGlobalConfig(sbPreview);
