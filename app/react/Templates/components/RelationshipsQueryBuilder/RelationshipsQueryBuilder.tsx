6377b3845d419e91d438e5cf2e7c72cb0ef5e4ce
Federico Nocetti <federico@huridocs.org>
2023-06-05 09:39:35 -0300

    Implement filters builder in visual query builder

--- app/react/Templates/components/RelationshipsQueryBuilder/RelationshipsQueryBuilder.tsx
+++ app/react/Templates/components/RelationshipsQueryBuilder/RelationshipsQueryBuilder.tsx
@@ -3,9 +3,15 @@
 /* eslint-disable react/no-multi-comp */
 import { MultiSelect } from 'app/Forms';
 import { ClientTemplateSchema, IStore, RelationshipTypesType } from 'app/istore';
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { MatchQuery, TraverseQuery } from 'shared/types/api.v2/templates.createTemplateRequest';
+import {
+  AndFilter,
+  Filter,
+  MatchQuery,
+  TemplateFilter,
+  TraverseQuery,
+} from 'shared/types/api.v2/templates.createTemplateRequest';
 
 const edgeStylesBase = {
   styles: {
@@ -152,15 +158,18 @@ const Node = ({
             float: 'left',
             borderBottom: 'solid 1px lightgrey',
             marginBottom: '5px',
-            paddingBottom: '5px',
+            paddingBottom: '9px',
+            whiteSpace: 'nowrap',
+            position: 'relative',
           }}
         >
-          <span style={{ marginRight: '5px' }}>{caption}</span>
+          <span style={{ marginRight: '27px', fontWeight: 'bold' }}>{caption}</span>
           <input
             type="button"
             value="x"
             onClick={onDeleteElementHandler}
             disabled={deleteDisabled}
+            style={{ right: 0, position: 'absolute' }}
           />
         </div>
         {children}
@@ -174,16 +183,322 @@ const Node = ({
   </div>
 );
 
+interface FilterNodeProps {
+  caption?: string;
+  isFirst?: boolean;
+  isLast?: boolean;
+  isRoot?: boolean;
+  children?: null | JSX.Element | JSX.Element[];
+  nested: JSX.Element[];
+  canAdd: boolean;
+  canDelete: boolean;
+  onAddElementHandler: (newFilter: Filter) => void;
+  onDeleteElementHandler: () => void;
+}
+
+const FilterNode = ({
+  caption,
+  isFirst,
+  isLast,
+  isRoot,
+  nested,
+  children,
+  canAdd,
+  canDelete,
+  onAddElementHandler,
+  onDeleteElementHandler,
+}: FilterNodeProps) => (
+  <div style={boxStyles}>
+    {!isRoot ? <Edges isFirst={isFirst} isLast={isLast} /> : null}
+    <div style={nodeContainerStyles}>
+      <div style={nodeStyles}>
+        {caption ? (
+          <div
+            style={{
+              float: 'left',
+              borderBottom: children ? 'solid 1px lightgrey' : undefined,
+              marginBottom: '5px',
+              paddingBottom: '9px',
+              whiteSpace: 'nowrap',
+              position: 'relative',
+            }}
+          >
+            <span style={{ marginRight: '27px' }}>{caption}</span>
+            {canDelete ? (
+              <input
+                type="button"
+                value="x"
+                onClick={onDeleteElementHandler}
+                style={{ right: 0, position: 'absolute' }}
+              />
+            ) : null}
+          </div>
+        ) : null}
+        {children}
+      </div>
+    </div>
+    {nested.length ? <Edges isRight /> : null}
+    <div style={childrenStyles}>
+      {nested}
+      {canAdd ? (
+        <div style={boxStyles}>
+          <Edges isFirst={false} isLast />
+          <div style={nodeContainerStyles}>
+            <div style={{ ...nodeStyles, border: 'none' }} />
+            <AddFilterButton onChange={onAddElementHandler} isRoot={isRoot} />
+          </div>
+        </div>
+      ) : null}
+    </div>
+  </div>
+);
+
+interface TemplateFilterNodeComponentProps {
+  value: TemplateFilter;
+  isFirst?: boolean;
+  isLast?: boolean;
+  isRoot?: boolean;
+  onChange: (value: TemplateFilter) => void;
+  onDelete: () => void;
+  templates: ClientTemplateSchema[];
+  path: string;
+}
+
+const TemplateFilterNodeComponent = ({
+  value,
+  isFirst,
+  isLast,
+  isRoot,
+  onChange,
+  onDelete,
+  templates,
+  path,
+}: TemplateFilterNodeComponentProps) => {
+  const onTemplatesChangeHandler = (newTemplates: string[]) =>
+    onChange({ ...value, value: newTemplates });
+
+  return (
+    <FilterNode
+      caption="Templates"
+      isFirst={isFirst}
+      isLast={isLast}
+      nested={[]}
+      onAddElementHandler={() => {}}
+      onDeleteElementHandler={onDelete}
+      canAdd={false}
+      canDelete
+      isRoot={isRoot}
+    >
+      <MultiSelect
+        prefix={path}
+        onChange={onTemplatesChangeHandler}
+        options={templates}
+        value={value.value}
+        optionsValue="_id"
+        optionsLabel="name"
+      />
+    </FilterNode>
+  );
+};
+
+const TemplateFilterNode = connect((state: IStore) => ({
+  templates: state.templates.toJS(),
+}))(TemplateFilterNodeComponent);
+
+interface AndFilterNodeProps {
+  value: AndFilter;
+  isFirst?: boolean;
+  isLast?: boolean;
+  isRoot?: boolean;
+  onChange: (value: AndFilter) => void;
+  onDelete: () => void;
+  path: string;
+}
+
+const AndFilterNode = ({
+  value,
+  isFirst,
+  isLast,
+  isRoot,
+  onChange,
+  onDelete,
+  path,
+}: AndFilterNodeProps) => {
+  const createOnChildChangeHandler = (index: number) => (newFilterValue: Filter) => {
+    const filters = [...(value.value ?? [])];
+    filters[index] = newFilterValue;
+    onChange({ ...value, value: filters });
+  };
+
+  const createOnDeleteChildHandler = (index: number, total: number) => () => {
+    if (total > 1) {
+      onChange({ ...value, value: value.value?.filter((_m, i) => i !== index) });
+    } else {
+      const filters = [...(value.value ?? [])];
+      filters[index] = { type: 'void' };
+      onChange({ ...value, value: filters });
+    }
+  };
+
+  const onAddElementHandler = (newFilter: Filter) => {
+    onChange({ ...value, value: [...value.value, newFilter] });
+  };
+
+  return (
+    <FilterNode
+      caption="AND"
+      isFirst={isFirst}
+      isLast={isLast}
+      nested={value.value.map((filter, index) => (
+        <AbstractFilter
+          value={filter}
+          onChange={createOnChildChangeHandler(index)}
+          onDelete={createOnDeleteChildHandler(index, value.value.length)}
+          path={`${path}.${index}`}
+          isFirst={index === 0}
+          isLast={value.value.length === 1 && filter.type === 'void'}
+        />
+      ))}
+      onAddElementHandler={onAddElementHandler}
+      onDeleteElementHandler={onDelete}
+      canAdd={value.value.length > 0 && value.value[0].type !== 'void'}
+      canDelete
+      isRoot={isRoot}
+    />
+  );
+};
+interface AddFilterButtonProps {
+  onChange: (value: Filter) => void;
+  isRoot?: boolean;
+}
+
+const AddFilterButton = ({ onChange, isRoot }: AddFilterButtonProps) => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      {!show ? (
+        <button
+          type="button"
+          onClick={() => {
+            setShow(true);
+          }}
+        >
+          {isRoot ? 'Add filter' : 'Add'}
+        </button>
+      ) : null}
+      {show ? (
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              onChange({ type: 'and', value: [{ type: 'void' }] });
+              setShow(false);
+            }}
+          >
+            And operator
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              onChange({ type: 'template', value: [] });
+              setShow(false);
+            }}
+          >
+            Templates filter
+          </button>
+          <button type="button" onClick={() => setShow(false)}>
+            Cancel
+          </button>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+interface VoidFilterNodeProps {
+  onChange: (value: Filter) => void;
+  isFirst?: boolean;
+  isLast?: boolean;
+  isRoot?: boolean;
+}
+
+const VoidFilterNode = ({ onChange, isFirst, isLast, isRoot }: VoidFilterNodeProps) => (
+  <FilterNode
+    isFirst={isFirst}
+    isLast={isLast}
+    nested={[]}
+    onAddElementHandler={() => {}}
+    onDeleteElementHandler={() => {}}
+    canAdd={false}
+    canDelete={false}
+    isRoot={isRoot}
+  >
+    <AddFilterButton onChange={newFilter => onChange(newFilter)} isRoot={isRoot} />
+  </FilterNode>
+);
+
 const createDefaultTraversal = () =>
   ({
     direction: 'out',
     types: [],
     match: [
       {
-        filter: { type: 'template', value: [] },
+        filter: { type: 'void' },
       },
     ],
   } as TraverseQuery);
+interface FilterProps {
+  value: Filter;
+  isFirst?: boolean;
+  isLast?: boolean;
+  isRoot?: boolean;
+  onChange: (value: Filter) => void;
+  onDelete: () => void;
+  path: string;
+}
+
+const AbstractFilter = ({
+  value,
+  isFirst,
+  isLast,
+  onChange,
+  onDelete,
+  path,
+  isRoot,
+}: FilterProps) => {
+  switch (value.type) {
+    case 'template':
+      return (
+        <TemplateFilterNode
+          value={value}
+          isFirst={isFirst}
+          isLast={isLast}
+          onChange={onChange}
+          onDelete={onDelete}
+          path={path}
+          isRoot={isRoot}
+        />
+      );
+    case 'and':
+      return (
+        <AndFilterNode
+          value={value}
+          isFirst={isFirst}
+          isLast={isLast}
+          onChange={onChange}
+          onDelete={onDelete}
+          path={path}
+          isRoot={isRoot}
+        />
+      );
+    case 'void':
+      return (
+        <VoidFilterNode onChange={onChange} isFirst={isFirst} isLast={isLast} isRoot={isRoot} />
+      );
+    default:
+      return <>Invalid filter type</>;
+  }
+};
 
 interface MatchNodeProps {
   value: MatchQuery;
@@ -191,18 +506,16 @@ interface MatchNodeProps {
   isLast?: boolean;
   onChange: (value: MatchQuery) => void;
   onDelete: () => void;
-  templates: ClientTemplateSchema[];
   path: string;
   canDelete?: boolean;
 }
 
-const MatchNodeComponent = ({
+const MatchNode = ({
   value,
   isFirst,
   isLast,
   onChange,
   onDelete,
-  templates,
   path,
   canDelete,
 }: MatchNodeProps) => {
@@ -216,8 +529,11 @@ const MatchNodeComponent = ({
     onChange({ ...value, traverse: value.traverse?.filter((_m, i) => i !== index) });
   };
 
-  const onTemplatesChangeHandler = (newTemplates: string[]) =>
-    onChange({ ...value, filter: { type: 'template', value: newTemplates } });
+  const onFilterChangeHandler = (filter: Filter) => onChange({ ...value, filter });
+
+  const onDeleteFilterHandler = () => {
+    onChange({ ...value, filter: { type: 'void' } });
+  };
 
   const onAddElementHandler = () =>
     onChange({
@@ -246,21 +562,19 @@ const MatchNodeComponent = ({
       onDeleteElementHandler={onDelete}
       deleteDisabled={!canDelete}
     >
-      <MultiSelect
-        prefix={path}
-        onChange={onTemplatesChangeHandler}
-        options={templates}
-        value={value.filter.type === 'template' ? value.filter.value : []}
-        optionsValue="_id"
-        optionsLabel="name"
+      <span>Filter</span>
+      <AbstractFilter
+        isFirst
+        isLast={false}
+        value={value.filter}
+        onChange={onFilterChangeHandler}
+        onDelete={onDeleteFilterHandler}
+        path={`${path}.filter.`}
+        isRoot
       />
     </Node>
   );
 };
-
-const MatchNode = connect((state: IStore) => ({
-  templates: state.templates.toJS(),
-}))(MatchNodeComponent);
 
 interface TravesalNodeProps {
   value: TraverseQuery;
@@ -294,13 +608,7 @@ const TravesalNodeComponent = ({
   const onAddElementHandler = () =>
     onChange({
       ...value,
-      match: [
-        ...(value.match ?? []),
-        {
-          filter: { type: 'template', value: [] },
-          traverse: [],
-        },
-      ],
+      match: [...(value.match ?? []), createDefaultTraversal().match[0]],
     });
 
   const onDirectionChangeHandler = (event: { target: { value: string } }) =>
