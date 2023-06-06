380d7c1c4e3764cf9d22694859f7d6f4e433451b
Federico Nocetti <federico@huridocs.org>
2023-06-05 22:46:25 -0300

    Abstract EntityRelatioshipUpdateService creation to factory

--- app/api/relationships.v2/infrastructure/registerUpdateRelationshipPropertiesJob.ts
+++ app/api/relationships.v2/infrastructure/registerUpdateRelationshipPropertiesJob.ts
@@ -1,11 +1,8 @@
 import { DefaultTransactionManager } from 'api/common.v2/database/data_source_defaults';
-import { DefaultEntitiesDataSource } from 'api/entities.v2/database/data_source_defaults';
-import { EntityRelationshipsUpdateService } from 'api/entities.v2/services/EntityRelationshipsUpdateService';
 import { Queue } from 'api/queue.v2/application/Queue';
-import { DefaultTemplatesDataSource } from 'api/templates.v2/database/data_source_defaults';
 import { tenants } from 'api/tenants';
 import { search } from 'api/search';
-import { DefaultRelationshipDataSource } from '../database/data_source_defaults';
+import { EntityRelationshipsUpdateService } from 'api/entities.v2/services/service_factories';
 import { UpdateRelationshipPropertiesJob } from '../services/propertyUpdateStrategies/UpdateRelationshipPropertiesJob';
 
 export function registerUpdateRelationshipPropertiesJob(queue: Queue) {
@@ -16,18 +13,13 @@ export function registerUpdateRelationshipPropertiesJob(queue: Queue) {
         tenants
           .run(async () => {
             const transactionManager = DefaultTransactionManager();
-            const relationshipsDS = DefaultRelationshipDataSource(transactionManager);
-            const entitiesDS = DefaultEntitiesDataSource(transactionManager);
-            const templatesDS = DefaultTemplatesDataSource(transactionManager);
+            const updater = EntityRelationshipsUpdateService(transactionManager);
+            const indexEntity = async (sharedId: string) =>
+              tenants.run(async () => search.indexEntities({ sharedId }), namespace);
 
             resolve({
-              updater: new EntityRelationshipsUpdateService(
-                entitiesDS,
-                templatesDS,
-                relationshipsDS
-              ),
-              indexEntity: async (sharedId: string) =>
-                tenants.run(async () => search.indexEntities({ sharedId }), namespace),
+              updater,
+              indexEntity,
               transactionManager,
             });
           }, namespace)
