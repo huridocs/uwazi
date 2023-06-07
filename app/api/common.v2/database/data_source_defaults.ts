09d8bb5a3ad7587363e00bef53fc61528c50286a
Laszlo Kecskes <laszlokecskes@protonmail.com>
2023-06-07 09:38:12 +0100

    reverting import order change

--- app/api/common.v2/database/data_source_defaults.ts
+++ app/api/common.v2/database/data_source_defaults.ts
@@ -1,6 +1,5 @@
-import { getClient } from 'api/common.v2/database/getConnectionForCurrentTenant';
-
 import { IdGenerator } from '../contracts/IdGenerator';
+import { getClient } from './getConnectionForCurrentTenant';
 import { MongoIdHandler } from './MongoIdGenerator';
 import { MongoTransactionManager } from './MongoTransactionManager';
 
