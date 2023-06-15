3ffafd0b10a9ae6a3aa27a42de69c50ba1e6cdfd
A happy cat <acasadotorres@gmail.com>
2023-06-15 15:21:21 +0200

    updated scrolling styles for modal with large content

--- app/react/V2/Components/UI/Modal.tsx
+++ app/react/V2/Components/UI/Modal.tsx
@@ -21,12 +21,12 @@ const Modal = ({ children, size }: ModalProps) => {
   return (
     <div
       aria-hidden="false"
-      className="flex overflow-x-hidden fixed inset-0 top-0 left-0 z-50 justify-center items-center bg-gray-900 bg-opacity-50"
+      className="fixed inset-0 top-0 left-0 z-50 flex items-center justify-center overflow-x-hidden bg-gray-900 bg-opacity-50"
       data-testid="modal"
       role="dialog"
       aria-label="Modal"
     >
-      <div className={`overflow-y-auto py-4 max-h-screen ${sizes[size]}`}>
+      <div className={`max-h-screen ${sizes[size]}`}>
         <div className="bg-white rounded-lg shadow">{children}</div>
       </div>
     </div>
@@ -49,13 +49,13 @@ Modal.Header = ({ children, className }: ModalChildrenProps) => (
 );
 
 Modal.Body = ({ children, className }: ModalChildrenProps) => (
-  <div className={`p-6 ${className}`} data-testid="modal-body">
+  <div className={`p-6 overflow-y-auto h-full md:h-[70vh] ${className}`} data-testid="modal-body">
     {children}
   </div>
 );
 
 Modal.Footer = ({ children }: ModalChildrenProps) => (
-  <div className="flex justify-center p-6 space-x-2 rounded-b border-t border-gray-200">
+  <div className="flex justify-center p-6 space-x-2 border-t border-gray-200 rounded-b">
     {children}
   </div>
 );
