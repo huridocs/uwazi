3f6dfcdfc8fbaaaa9e00aefde9e8413118de6756
A happy cat <acasadotorres@gmail.com>
2023-06-15 13:48:06 +0200

    InputField error message property and css tweaksgit status

--- app/react/V2/Components/Forms/InputField.tsx
+++ app/react/V2/Components/Forms/InputField.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEventHandler, Ref } from 'react';
 import { XMarkIcon } from '@heroicons/react/20/solid';
 import { Translate } from 'app/I18N';
+import { isString } from 'lodash';
 
 interface InputFieldProps {
   id: string;
@@ -9,6 +10,7 @@ interface InputFieldProps {
   hideLabel?: boolean;
   placeholder?: string;
   hasErrors?: boolean;
+  errorMessage?: string | React.ReactNode;
   value?: string;
   className?: string;
   type?: 'text' | 'email' | 'password';
@@ -19,17 +21,21 @@ interface InputFieldProps {
   onBlur?: ChangeEventHandler<HTMLInputElement>;
 }
 
+const renderChild = (child: string | React.ReactNode) =>
+  isString(child) ? <Translate>{child}</Translate> : child;
+
 const InputField = React.forwardRef(
   (
     {
       id,
       label,
-      disabled,
-      hideLabel,
-      placeholder,
-      hasErrors,
-      value,
-      className,
+      disabled = false,
+      hideLabel = false,
+      placeholder = '',
+      hasErrors = false,
+      errorMessage = '',
+      value = '',
+      className = '',
       type = 'text',
       autoComplete = 'on',
       name = '',
@@ -39,24 +45,25 @@ const InputField = React.forwardRef(
     }: InputFieldProps,
     ref: Ref<any>
   ) => {
-    const hasClearFieldButton = Boolean(clearFieldAction);
-    let fieldStyles = 'border-gray-300 border text-gray-900';
+    let fieldStyles = 'border-gray-300 border text-gray-900 focus:ring-primary-500';
     let clearFieldStyles = 'enabled:hover:text-primary-700 text-gray-900';
+    let labelStyles = hideLabel ? 'sr-only' : 'block mb-2 text-sm font-medium text-gray-700';
 
-    if (hasErrors) {
+    if (hasErrors || errorMessage) {
       fieldStyles =
-        'border-error-300 focus:border-error-500 focus:ring-error-500 border-2 text-red-900';
+        'border-error-300 focus:border-error-500 focus:ring-error-500 border-2 text-error-900 bg-error-50 placeholder-error-700';
       clearFieldStyles = 'enabled:hover:text-error-700 text-error-900';
+      labelStyles = `${labelStyles} text-error-700`;
     }
 
-    if (hasClearFieldButton) {
+    if (clearFieldAction) {
       fieldStyles = `${fieldStyles} pr-10`;
     }
 
     return (
       <div className={className}>
-        <label htmlFor={id} className={hideLabel ? 'sr-only' : ''}>
-          {label}
+        <label htmlFor={id} className={labelStyles}>
+          {renderChild(label)}
         </label>
         <div className="relative w-full">
           <input
@@ -72,7 +79,7 @@ const InputField = React.forwardRef(
             className={`${fieldStyles} disabled:text-gray-500 rounded-lg bg-gray-50 block flex-1 w-full text-sm p-2.5`}
             placeholder={placeholder}
           />
-          {hasClearFieldButton && (
+          {Boolean(clearFieldAction) && (
             <button
               type="button"
               onClick={clearFieldAction}
@@ -84,6 +91,9 @@ const InputField = React.forwardRef(
               <XMarkIcon className="w-5" />
               <Translate className="sr-only">Clear</Translate>
             </button>
+          )}
+          {errorMessage && (
+            <p className="mt-2 text-sm text-error-600">{renderChild(errorMessage)}</p>
           )}
         </div>
       </div>
--- app/react/V2/Components/Forms/index.ts
+++ app/react/V2/Components/Forms/index.ts
@@ -2,7 +2,7 @@ import { InputField } from './InputField';
 import { Select } from './Select';
 import { MultiSelect } from '../Forms/MultiSelect';
 import { RadioSelect } from '../Forms/RadioSelect';
-import { FormInput } from '../Forms/FormInput';
+import { FormInput } from '../../Routes/Settings/Translations/components/FormInput';
 import { ConfirmNavigationModal } from '../Forms/ConfirmNavigationModal';
 
 export { InputField, FormInput, Select, MultiSelect, RadioSelect, ConfirmNavigationModal };
