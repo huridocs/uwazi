e76ca427d0e9b6b98c062b2aa3f1b67f30ded821
Kevin <kevin@huridocs.org>
2023-06-21 12:42:04 +0300

    Removed unexisting import

--- app/react/V2/Components/Forms/index.ts
+++ app/react/V2/Components/Forms/index.ts
@@ -2,6 +2,5 @@ export { InputField } from './InputField';
 export { Select } from './Select';
 export { MultiSelect } from '../Forms/MultiSelect';
 export { RadioSelect } from '../Forms/RadioSelect';
-export { FormInput } from '../Forms/FormInput';
 export { ConfirmNavigationModal } from '../Forms/ConfirmNavigationModal';
 export type { MultiSelectProps } from '../Forms/MultiSelect';
