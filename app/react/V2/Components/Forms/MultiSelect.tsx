d268c80e9d36d09560f51c1a1774042ed95153bf
Santiago <borrajosantiago@gmail.com>
2023-06-21 13:25:15 -0300

    fixed vissual flickering when component updated

--- app/react/V2/Components/Forms/MultiSelect.tsx
+++ app/react/V2/Components/Forms/MultiSelect.tsx
@@ -31,27 +31,72 @@ const MultiSelect = ({
   const [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>(null);
   const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement: 'bottom-end',
+    placement: 'bottom-start',
+    strategy: 'absolute',
   });
 
   const selectedOptions = optionsState.filter(option => option.selected);
 
   return (
-    <Popover className="relative rounded-lg border border-gray-50" data-testid="multiselect-comp">
-      <div className="flex justify-between p-2 bg-gray-50 border-b border-gray-50">
-        <div className="text-base text-indigo-700">{renderChild(label)}</div>
-        <div className="left-0">
-          <Popover.Button
-            ref={setReferenceElement}
-            className="text-primary-700 disabled:text-primary-300"
-            disabled={disabled}
-          >
-            <span className="sr-only">{t('System', 'Select', null, false)}</span>
-            <PlusCircleIcon className="w-6 text-lg" />
-          </Popover.Button>
+    <>
+      <Popover className="relative rounded-lg border border-gray-50" data-testid="multiselect-comp">
+        <div className="flex justify-between p-2 bg-gray-50 border-b border-gray-50">
+          <div className="text-base text-indigo-700">{renderChild(label)}</div>
+          <div className="left-0">
+            <Popover.Button
+              ref={setReferenceElement}
+              className="text-primary-700 disabled:text-primary-300"
+              disabled={disabled}
+            >
+              <span className="sr-only">{t('System', 'Select', null, false)}</span>
+              <PlusCircleIcon className="w-6 text-lg" />
+            </Popover.Button>
+          </div>
         </div>
-      </div>
 
+        <Transition
+          enter="transition duration-100 ease-out"
+          enterFrom="transform scale-95 opacity-0"
+          enterTo="transform scale-100 opacity-100"
+          leave="transition duration-75 ease-out"
+          leaveFrom="transform scale-100 opacity-100"
+          leaveTo="transform scale-95 opacity-0"
+        >
+          <Popover.Panel
+            ref={setPopperElement}
+            style={styles.popper}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...attributes.popper}
+            as="div"
+          >
+            <ul className="overflow-y-auto p-2 max-w-md max-h-56 bg-white rounded shadow-md w-fit">
+              {optionsState.map((option: Option) => (
+                <li key={option.label} className="flex gap-2 py-1 align-top">
+                  <Checkbox
+                    className="cursor-pointer"
+                    id={option.value}
+                    checked={Boolean(option.selected)}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      const isChecked = e.target.checked;
+                      const selected = optionsState.map(opt => {
+                        if (opt.value === option.value) {
+                          return { ...opt, selected: isChecked };
+                        }
+                        return opt;
+                      });
+                      setOptionsState(selected);
+                      onChange(selected);
+                    }}
+                  />
+                  <label className="cursor-pointer" htmlFor={option.value}>
+                    {option.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
       <div className="flex flex-wrap gap-2 px-5 py-4 min-h-fit">
         {selectedOptions.length
           ? selectedOptions.map((option: Option) => (
@@ -78,49 +123,7 @@ const MultiSelect = ({
             ))
           : renderChild(placeholder, 'text-gray-500')}
       </div>
-
-      <Transition
-        enter="transition duration-100 ease-out"
-        enterFrom="transform scale-95 opacity-0"
-        enterTo="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leaveFrom="transform scale-100 opacity-100"
-        leaveTo="transform scale-95 opacity-0"
-      >
-        <Popover.Panel
-          ref={setPopperElement}
-          style={styles.popper}
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...attributes.popper}
-          className="overflow-y-auto p-2 max-w-md max-h-56 bg-white rounded shadow-md w-fit"
-          as="ul"
-        >
-          {optionsState.map((option: Option) => (
-            <li key={option.label} className="flex gap-2 py-1 align-top">
-              <Checkbox
-                className="cursor-pointer"
-                id={option.value}
-                checked={Boolean(option.selected)}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  const isChecked = e.target.checked;
-                  const selected = optionsState.map(opt => {
-                    if (opt.value === option.value) {
-                      return { ...opt, selected: isChecked };
-                    }
-                    return opt;
-                  });
-                  setOptionsState(selected);
-                  onChange(selected);
-                }}
-              />
-              <label className="cursor-pointer" htmlFor={option.value}>
-                {option.label}
-              </label>
-            </li>
-          ))}
-        </Popover.Panel>
-      </Transition>
-    </Popover>
+    </>
   );
 };
 
