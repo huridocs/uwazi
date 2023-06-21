1e1a30357c03b5cfe246481b9b20909dea911d65
Santiago <borrajosantiago@gmail.com>
2023-06-21 14:51:29 -0300

    moved tests into settings folder

--- cypress/e2e/settings/groups.cy.ts
+++ cypress/e2e/settings/groups.cy.ts
@@ -1,6 +1,13 @@
 import 'cypress-axe';
 import { clearCookiesAndLogin } from '../helpers/login';
-import { namesShouldMatch } from './helpers';
+
+const namesShouldMatch = (names: string[]) => {
+  cy.get('table tbody tr').each((row, index) => {
+    cy.wrap(row).within(() => {
+      cy.get('td').eq(1).should('contain.text', names[index]);
+    });
+  });
+};
 
 describe('Groups', () => {
   before(() => {
--- cypress/e2e/users&groups/helpers.ts
+++
@@ -1,9 +0,0 @@
-const namesShouldMatch = (names: string[]) => {
-  cy.get('table tbody tr').each((row, index) => {
-    cy.wrap(row).within(() => {
-      cy.get('td').eq(1).should('contain.text', names[index]);
-    });
-  });
-};
-
-export { namesShouldMatch };
