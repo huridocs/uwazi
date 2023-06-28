145e2bf87518fd251318879d1207aea7af294c56
Santiago <borrajosantiago@gmail.com>
2023-06-28 17:08:07 -0300

    test + behavior changes

--- app/react/V2/Components/UI/Paginator.tsx
+++ app/react/V2/Components/UI/Paginator.tsx
@@ -10,16 +10,16 @@ interface PaginatorProps {
   preventScrollReset?: boolean;
 }
 
-const calculateMorePages = (currentPage: string, totalPages: string) => {
-  let page = Number(currentPage);
+const calculateMorePages = (currentPage: number, totalPages: number) => {
+  let page = currentPage + 1;
   const pages = [];
 
   while (pages.length < 5) {
-    if (page + 1 === Number(totalPages)) {
+    if (page + 1 === totalPages) {
       break;
     }
 
-    pages.push(Number(page + 1).toString());
+    pages.push((page + 1).toString());
     page += 1;
   }
 
@@ -27,8 +27,9 @@ const calculateMorePages = (currentPage: string, totalPages: string) => {
 };
 
 const Paginator = ({ currentPage, totalPages, buildUrl, preventScrollReset }: PaginatorProps) => {
-  const [showMore, setShowMore] = useState(false);
   const page = Number(currentPage);
+  const lastPage = Number(totalPages);
+  const [showMore, setShowMore] = useState(lastPage - page < 6);
   const isFirstPage = currentPage === '1';
   const isLastPage = currentPage === totalPages;
   const prevPage = isFirstPage ? '1' : (page - 1).toString();
@@ -38,16 +39,27 @@ const Paginator = ({ currentPage, totalPages, buildUrl, preventScrollReset }: Pa
     <nav aria-label="Pagination">
       <ul className="inline-flex items-center -space-x-px">
         <li key="previous">
-          <Link
-            to={buildUrl(prevPage)}
-            preventScrollReset={preventScrollReset}
-            className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          >
-            <Translate className="sr-only">Previous</Translate>
-            <ChevronLeftIcon className="w-[17px]" />
-          </Link>
+          {isFirstPage ? (
+            <button
+              type="button"
+              disabled
+              className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300"
+            >
+              <ChevronLeftIcon className="w-[17px]" />
+            </button>
+          ) : (
+            <Link
+              to={buildUrl(prevPage)}
+              preventScrollReset={preventScrollReset}
+              className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            >
+              <Translate className="sr-only">Previous</Translate>
+              <ChevronLeftIcon className="w-[17px]" />
+            </Link>
+          )}
         </li>
-        {isFirstPage ? undefined : (
+
+        {!isFirstPage && (
           <li key="first">
             <Link
               to={buildUrl('1')}
@@ -59,6 +71,20 @@ const Paginator = ({ currentPage, totalPages, buildUrl, preventScrollReset }: Pa
             </Link>
           </li>
         )}
+
+        {!isFirstPage && page - 1 !== 1 && (
+          <li key={page - 1}>
+            <Link
+              to={buildUrl((page - 1).toString())}
+              preventScrollReset={preventScrollReset}
+              aria-current="page"
+              className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            >
+              {page - 1}
+            </Link>
+          </li>
+        )}
+
         <li key="current">
           <Link
             to={buildUrl(currentPage)}
@@ -70,49 +96,77 @@ const Paginator = ({ currentPage, totalPages, buildUrl, preventScrollReset }: Pa
           </Link>
         </li>
 
-        {showMore ? (
-          calculateMorePages(currentPage, totalPages).map(pageNumber => (
-            <li key={`more-${pageNumber}`}>
-              <Link
-                to={buildUrl(pageNumber)}
-                preventScrollReset={preventScrollReset}
-                className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-              >
-                {pageNumber}
-              </Link>
-            </li>
-          ))
-        ) : (
-          <li key="more">
-            <button
-              onClick={() => setShowMore(true)}
-              type="button"
-              className="h-[35px] px-3 py-2 m-0 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        {!isLastPage && page + 1 !== lastPage && (
+          <li key={page + 1}>
+            <Link
+              to={buildUrl((page + 1).toString())}
+              preventScrollReset={preventScrollReset}
+              aria-current="page"
+              className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
             >
-              <Translate className="sr-only">Show more</Translate>
-              ...
-            </button>
+              {page + 1}
+            </Link>
           </li>
         )}
 
-        <li key="last">
-          <Link
-            to={buildUrl(totalPages)}
-            preventScrollReset={preventScrollReset}
-            className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          >
-            {totalPages}
-          </Link>
-        </li>
+        {page + 1 !== lastPage &&
+          page !== lastPage &&
+          (showMore ? (
+            calculateMorePages(page, lastPage).map(pageNumber => (
+              <li key={`more-${pageNumber}`}>
+                <Link
+                  to={buildUrl(pageNumber)}
+                  preventScrollReset={preventScrollReset}
+                  className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                >
+                  {pageNumber}
+                </Link>
+              </li>
+            ))
+          ) : (
+            <li key="more">
+              <button
+                onClick={() => setShowMore(true)}
+                type="button"
+                className="h-[35px] px-3 py-2 m-0 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              >
+                <Translate className="sr-only">Show more</Translate>
+                ...
+              </button>
+            </li>
+          ))}
+
+        {!isLastPage && (
+          <li key="last">
+            <Link
+              to={buildUrl(totalPages)}
+              preventScrollReset={preventScrollReset}
+              className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            >
+              {totalPages}
+            </Link>
+          </li>
+        )}
+
         <li key="next">
-          <Link
-            to={buildUrl(nextPage)}
-            preventScrollReset={preventScrollReset}
-            className="block px-3 py-2 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          >
-            <Translate className="sr-only">Next</Translate>
-            <ChevronRightIcon className="w-[17px]" />
-          </Link>
+          {isLastPage ? (
+            <button
+              type="button"
+              disabled
+              className="block px-3 py-2 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300"
+            >
+              <ChevronRightIcon className="w-[17px]" />
+            </button>
+          ) : (
+            <Link
+              to={buildUrl(nextPage)}
+              preventScrollReset={preventScrollReset}
+              className="block px-3 py-2 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            >
+              <Translate className="sr-only">Next</Translate>
+              <ChevronRightIcon className="w-[17px]" />
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
---
+++ app/react/V2/Components/UI/specs/Paginator.cy.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import { mount } from '@cypress/react18';
+import { composeStories } from '@storybook/react';
+import * as stories from 'app/stories/Paginator.stories';
+
+const { Basic } = composeStories(stories);
+
+describe('Paginator', () => {
+  it('should render with the current page and the correct basic links', () => {
+    mount(<Basic currentPage="5" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+    cy.get('a').eq(0).should('have.attr', 'href', '/path?page=4');
+    cy.get('a').eq(1).should('have.attr', 'href', '/path?page=1');
+    cy.get('a').eq(2).should('have.attr', 'href', '/path?page=4');
+    cy.get('a').eq(3).should('have.attr', 'href', '/path?page=5');
+    cy.get('a').eq(4).should('have.attr', 'href', '/path?page=6');
+    cy.get('a').eq(5).should('have.attr', 'href', '/path?page=25');
+    cy.get('a').eq(6).should('have.attr', 'href', '/path?page=6');
+    cy.get('button').should('exist');
+  });
+
+  it('should disable the previous page button when on first page', () => {
+    mount(<Basic currentPage="1" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+    cy.get('a').eq(0).should('have.attr', 'href', '/path?page=1');
+    cy.get('button').eq(0).should('have.attr', 'disabled');
+  });
+
+  it('should disable the next page button when on last page', () => {
+    mount(<Basic currentPage="25" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+    cy.get('a').eq(3).should('have.attr', 'href', '/path?page=25');
+    cy.get('button').eq(0).should('have.attr', 'disabled');
+  });
+
+  describe('go to first and last page', () => {
+    it('should not show the link to first page if on fist page', () => {
+      mount(<Basic currentPage="1" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.get('a').eq(0).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(1).should('have.attr', 'href', '/path?page=2');
+    });
+
+    it('should not show the link to last page if on last page', () => {
+      mount(<Basic currentPage="25" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.get('a').eq(0).should('have.attr', 'href', '/path?page=24');
+      cy.get('a').eq(1).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(2).should('have.attr', 'href', '/path?page=24');
+      cy.get('a').eq(3).should('have.attr', 'href', '/path?page=25');
+    });
+  });
+
+  describe('current page siblings', () => {
+    it('should show not show them if they are the same as the first or last page', () => {
+      mount(<Basic currentPage="2" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.get('a').eq(0).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(1).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(2).should('have.attr', 'href', '/path?page=2');
+
+      mount(<Basic currentPage="24" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.get('a').eq(3).should('have.attr', 'href', '/path?page=24');
+      cy.get('a').eq(4).should('have.attr', 'href', '/path?page=25');
+      cy.get('a').eq(5).should('have.attr', 'href', '/path?page=25');
+    });
+
+    it('should not show the previous sibling on first page', () => {
+      mount(<Basic currentPage="1" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.get('a').eq(0).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(1).should('have.attr', 'href', '/path?page=2');
+    });
+
+    it('should not show the next sibling on last page', () => {
+      mount(<Basic currentPage="25" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.get('a').eq(3).should('have.attr', 'href', '/path?page=25');
+    });
+  });
+
+  describe('show more', () => {
+    it('should show more pages', () => {
+      mount(<Basic currentPage="5" totalPages="25" buildUrl={page => `path?page=${page}`} />);
+      cy.contains('button', 'Show more').click();
+      cy.get('a').eq(3).should('have.attr', 'href', '/path?page=5');
+      cy.get('a').eq(4).should('have.attr', 'href', '/path?page=6');
+      cy.get('a').eq(5).should('have.attr', 'href', '/path?page=7');
+      cy.get('a').eq(6).should('have.attr', 'href', '/path?page=8');
+      cy.get('a').eq(7).should('have.attr', 'href', '/path?page=9');
+      cy.get('a').eq(8).should('have.attr', 'href', '/path?page=10');
+      cy.get('a').eq(9).should('have.attr', 'href', '/path?page=11');
+      cy.get('a').eq(10).should('have.attr', 'href', '/path?page=25');
+    });
+
+    it('should not show more pages button if there are none', () => {
+      mount(<Basic currentPage="2" totalPages="3" buildUrl={page => `path?page=${page}`} />);
+      cy.contains('button', 'Show more').should('not.exist');
+      cy.get('a').eq(0).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(1).should('have.attr', 'href', '/path?page=1');
+      cy.get('a').eq(2).should('have.attr', 'href', '/path?page=2');
+      cy.get('a').eq(3).should('have.attr', 'href', '/path?page=3');
+      cy.get('a').eq(4).should('have.attr', 'href', '/path?page=3');
+    });
+
+    it('should not show more pages button if it is unnecessary', () => {
+      mount(<Basic currentPage="2" totalPages="7" buildUrl={page => `path?page=${page}`} />);
+      cy.contains('button', 'Show more').should('not.exist');
+
+      mount(<Basic currentPage="1" totalPages="3" buildUrl={page => `path?page=${page}`} />);
+      cy.contains('button', 'Show more').should('not.exist');
+
+      mount(<Basic currentPage="5" totalPages="5" buildUrl={page => `path?page=${page}`} />);
+      cy.contains('button', 'Show more').should('not.exist');
+    });
+  });
+});
