06d1a79795b6564f836cd379d0d43ee4384d6962
Santiago <borrajosantiago@gmail.com>
2023-06-30 16:33:38 -0300

    added initial table

--- app/react/V2/Routes/Settings/IX/IXDashboard.tsx
+++ app/react/V2/Routes/Settings/IX/IXDashboard.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { IncomingHttpHeaders } from 'http';
 import { LoaderFunction, useLoaderData } from 'react-router-dom';
 import * as ixAPI from 'V2/api/ix';
+import { SettingsContent } from 'V2/Components/Layouts/SettingsContent';
+import { Table } from 'V2/Components/UI';
+import { Translate } from 'app/I18N';
 import { IXExtractorInfo } from './types';
+import { tableColumns } from './components/TableElements';
 
 const IXDashboard = () => {
-  const extractors = useLoaderData() as IXExtractorInfo;
+  const extractors = useLoaderData() as IXExtractorInfo[];
 
   return (
     <div className="tw-content" style={{ width: '100%', overflowY: 'auto' }}>
       <SettingsContent>
         <SettingsContent.Header title="Metadata extraction dashboard" />
 
-        <SettingsContent.Body>content</SettingsContent.Body>
+        <SettingsContent.Body>
+          <Table<IXExtractorInfo>
+            data={extractors}
+            columns={tableColumns}
+            title={<Translate>Extractors</Translate>}
+          />
+        </SettingsContent.Body>
 
         <SettingsContent.Footer>footer</SettingsContent.Footer>
       </SettingsContent>
