a1046a8b6366292bad020fc4b5ab5903a576a333
Kevin <kevin@huridocs.org>
2023-07-20 14:02:37 +0300

    Fixed the popover

--- app/react/V2/Routes/Settings/IX/components/SuggestedValue.tsx
+++ app/react/V2/Routes/Settings/IX/components/SuggestedValue.tsx
@@ -24,51 +24,61 @@ const SuggestedValue = ({ value, suggestion }: { value: string; suggestion: stri
   const currentValue = value === '' ? '-' : value;
   const suggestedValue = suggestion === '' ? '-' : suggestion;
   return (
-    // <Popover>
-    //   <Popover.Button
-    //     ref={setReferenceElement}
-    //     className="w-full h-full text-xs font-normal focus-visible:border-none"
-    //   >
-    //     <div>
-    //       <div className="text-gray-500">{currentValue}</div>
-    //       <div className={`text-left ${colorClass}`}>{suggestedValue}</div>
-    //     </div>
-    //   </Popover.Button>
-    //   <Popover.Overlay
-    //     ref={setPopperElement}
-    //     style={styles.popper}
-    //     // eslint-disable-next-line react/jsx-props-no-spreading
-    //     {...attributes.popper}
-    //     as="div"
-    //   >
-    //     <div className="max-w-md px-[10px] py-3 rounded mb-3 overflow-y-auto text-xs font-normal text-gray-500 bg-white shadow-md max-h-56 w-fit">
-    //       <div>
-    //         <span className="font-bold">Current value:</span> {currentValue}
-    //       </div>
-    //       <div>
-    //         <span className="font-bold">Suggestion:</span> {suggestedValue}
-    //       </div>
-    //       <div ref={setArrowElement} style={styles.arrow} />
-    //     </div>
-    //   </Popover.Overlay>
-    // </Popover>
-    <>
-      <div
-        ref={setPopperElement}
-        {...attributes.popper}
-        style={styles.popper}
-        className="max-w-md px-[10px] py-3 rounded mb-3 overflow-y-auto text-xs font-normal text-gray-500 bg-white shadow-md max-h-56 w-fit"
+    <Popover>
+      <Popover.Button
+        ref={setReferenceElement}
+        className="w-full h-full text-xs font-normal text-left focus-visible:outline-none"
       >
-        This is a popup
-        <div ref={setArrowElement} style={styles.arrow} />
-      </div>
-      <button type="button" ref={setReferenceElement}>
         <div>
-          <div className="text-xs font-normal text-gray-500">{currentValue}</div>
-          <div className={`text-xs font-normal ${colorClass}`}>{suggestedValue}</div>
+          <div className="text-gray-500">{currentValue}</div>
+          <div className={`text-left ${colorClass}`}>{suggestedValue}</div>
         </div>
-      </button>
-    </>
+      </Popover.Button>
+      <Popover.Overlay
+        ref={setPopperElement}
+        style={styles.popper}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...attributes.popper}
+        as="div"
+      >
+        <div className="max-w-md px-[10px] py-3 rounded mb-3 overflow-y-auto text-xs font-normal text-gray-500 bg-white shadow-md max-h-56 w-fit">
+          <div>
+            <span className="font-bold">Current value:</span> {currentValue}
+          </div>
+          <div>
+            <span className="font-bold">Suggestion:</span> {suggestedValue}
+          </div>
+        </div>
+        <span
+          ref={setArrowElement}
+          className="absolute bottom-[4px] left-0 w-4 h-4 rotate-45 bg-white"
+          // style={styles.arrow}
+          style={{
+            position: styles.arrow.position,
+            left: styles.arrow.left,
+            transform: styles.arrow.transform + ' rotate(45deg)',
+            boxShadow: '2px 2px 3px rgba(40, 40, 40, 0.1)',
+          }}
+        />
+      </Popover.Overlay>
+    </Popover>
+    // <>
+    //   <div
+    //     ref={setPopperElement}
+    //     {...attributes.popper}
+    //     style={styles.popper}
+    //     className="max-w-md px-[10px] py-3 rounded mb-3 overflow-y-auto text-xs font-normal text-gray-500 bg-white shadow-md max-h-56 w-fit hidden"
+    //   >
+    //     This is a popup
+    //     <div ref={setArrowElement} style={styles.arrow} className="mt-2" />
+    //   </div>
+    //   <button type="button" ref={setReferenceElement}>
+    //     <div>
+    //       <div className="text-xs font-normal text-gray-500">{currentValue}</div>
+    //       <div className={`text-xs font-normal ${colorClass}`}>{suggestedValue}</div>
+    //     </div>
+    //   </button>
+    // </>
   );
 };
 
