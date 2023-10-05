20ca72904adf9eb238e373b6edb3e3ff63148d01
Federico Nocetti <federico@huridocs.org>
2023-10-04 21:56:17 -0300

    Add js wrapper for bash scripts

---
+++ scripts/bash.js
@@ -0,0 +1,34 @@
+/* eslint-disable global-require */
+const { spawn } = require('child_process');
+const url = require('url');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('@babel/register')({ extensions: ['.js', '.jsx', '.ts', '.tsx'] });
+}
+process.env.ROOT_PATH = process.env.ROOT_PATH || __dirname;
+
+const { config } = require('../app/api/config');
+
+const file = process.argv[2];
+
+if (file) {
+  const mongoUri = url.parse(config.DBHOST);
+  const bashProcess = spawn(file, [], {
+    env: {
+      ...process.env,
+      DBHOST: mongoUri.hostname,
+    },
+  });
+
+  bashProcess.stdout.pipe(process.stdout);
+  bashProcess.stderr.pipe(process.stderr);
+
+  bashProcess.on('close', code => {
+    process.exit(code);
+  });
+
+  bashProcess.on('error', error => {
+    process.stderr.write(`[spawn error] ${error} \n`);
+    process.exit(1);
+  });
+}
