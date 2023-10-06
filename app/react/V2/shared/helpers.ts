5a77c8ff50848d3d648ebaa3bc3e3210e886d9ea
Santiago <borrajosantiago@gmail.com>
2023-10-06 17:48:05 -0300

    refactor date helpers

--- app/react/V2/shared/helpers.ts
+++
@@ -1,7 +0,0 @@
-import moment from 'moment-timezone';
-
-const propertyValueFormatter = {
-  date: (timestamp: string) => moment.utc(timestamp, 'X').format('ll'),
-};
-
-export { propertyValueFormatter };
