ae2faf1315d9c19eb53b9ed5f9e8b2f76d07a024
mfacar <mercy@huridocs.org>
2023-10-12 12:23:35 -0500

    refactor to reduce complexity

---
+++ app/react/V2/CustomHooks/dragAndDrop/definitions.ts
@@ -0,0 +1,208 @@
+import { Dispatch } from 'react';
+import update from 'immutability-helper';
+import { omit } from 'lodash';
+import { IDraggable, ItemTypes } from 'app/V2/shared/types';
+import ID from 'shared/uniqueID';
+
+interface IDnDContext {
+  type: ItemTypes;
+  addItem: (item: IDraggable) => void;
+  removeItem: (item: IDraggable) => void;
+  updateItems: (index: number, values: IDraggable) => void;
+  updateActiveItems: (items: IDraggable[]) => void;
+  sort: Function;
+  activeItems: IDraggable[];
+  availableItems: IDraggable[];
+}
+const setIdAndParent = (item: IDraggable, parent?: IDraggable) => {
+  const id = item.id || ID();
+  return { ...item, id, ...(parent ? { parent } : {}) };
+};
+const mapWithParent: (items: IDraggable[], parent?: IDraggable) => IDraggable[] = (items, parent) =>
+  items.map(item => {
+    const itemWithId = setIdAndParent(item, parent);
+    if (item.items && item.items.length > 0) {
+      return { ...itemWithId, items: mapWithParent(item.items, itemWithId) };
+    }
+    return itemWithId;
+  });
+
+const mapWithID = (items: IDraggable[]) => items.map(item => setIdAndParent(item));
+
+const removeChildFromParent = (
+  activeItems: IDraggable[],
+  setActiveItems: Dispatch<React.SetStateAction<IDraggable[]>>,
+  newItem: IDraggable
+) => {
+  if (newItem.parent) {
+    const indexOfCurrentParent = activeItems.findIndex(ai => ai.id === newItem.parent!.id);
+
+    setActiveItems((prevActiveItems: IDraggable[]) => {
+      const index = prevActiveItems[indexOfCurrentParent].items!.findIndex(
+        ai => ai.id === newItem.id
+      );
+      return update(prevActiveItems, {
+        [indexOfCurrentParent]: {
+          items: { $splice: [[index, 1]] },
+        },
+      });
+    });
+  }
+};
+
+const findItemIndex = (items: IDraggable[], searchedItem: IDraggable) =>
+  items.findIndex(item => item.id === searchedItem.id);
+
+const removeItemFromList = (
+  setActiveItems: Dispatch<React.SetStateAction<IDraggable[]>>,
+  indexOfNewItem: number
+) => {
+  if (indexOfNewItem > -1) {
+    setActiveItems((prevActiveItems: IDraggable[]) =>
+      update(prevActiveItems, {
+        $splice: [[indexOfNewItem, 1]],
+      })
+    );
+  }
+};
+
+const addItemToParent = (
+  setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
+  indexOfNewItem: number,
+  newItem: IDraggable,
+  parent: IDraggable
+) => {
+  removeItemFromList(setActiveItems, indexOfNewItem);
+
+  setActiveItems((prevActiveItems: IDraggable[]) => {
+    const indexOfParent = findItemIndex(prevActiveItems, parent);
+    if (indexOfParent > -1) {
+      return prevActiveItems[indexOfParent].items
+        ? update(prevActiveItems, {
+            [indexOfParent]: {
+              items: {
+                $push: [{ ..._.omit(newItem, ['parent', 'container', 'items']), parent }],
+              },
+            },
+          })
+        : update(prevActiveItems, {
+            [indexOfParent]: {
+              items: {
+                $set: [{ ...newItem, parent }],
+              },
+            },
+          });
+    }
+    return prevActiveItems;
+  });
+};
+
+const addActiveItem =
+  (
+    activeItems: IDraggable[],
+    setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
+    availableItems: IDraggable[],
+    setAvailableItems: React.Dispatch<React.SetStateAction<IDraggable[]>>
+  ) =>
+  (newItem: IDraggable, parent?: IDraggable) => {
+    removeChildFromParent(activeItems, setActiveItems, newItem);
+
+    const indexOfNewItem = findItemIndex(activeItems, newItem);
+
+    if (parent) {
+      addItemToParent(setActiveItems, indexOfNewItem, newItem, parent);
+    } else if (indexOfNewItem === -1) {
+      setActiveItems((prevActiveItems: IDraggable[]) =>
+        update(prevActiveItems, {
+          $push: [omit(newItem, ['parent', 'container', 'items'])],
+        })
+      );
+    }
+    const indexOfSource = findItemIndex(availableItems, newItem);
+    removeItemFromList(setAvailableItems, indexOfSource);
+  };
+
+const removeActiveItem =
+  (
+    activeItems: IDraggable[],
+    setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
+    setAvailableItems: React.Dispatch<React.SetStateAction<IDraggable[]>>
+  ) =>
+  (item: IDraggable) => {
+    if (item.parent !== undefined) {
+      removeChildFromParent(activeItems, setActiveItems, item);
+    } else {
+      const index = findItemIndex(activeItems, item);
+      removeItemFromList(setActiveItems, index);
+    }
+
+    const availableSubItems = (item.items || []).map(ai => omit(ai, ['parent', 'container']));
+    setAvailableItems(prevAvailableItems =>
+      update(prevAvailableItems, {
+        $push: [omit(item, ['parent', 'container', 'items']), ...availableSubItems],
+      })
+    );
+  };
+
+const sortChildren = (
+  activeItems: IDraggable[],
+  setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
+  {
+    currentItem,
+    target,
+    dragIndex,
+    hoverIndex,
+  }: { currentItem: IDraggable; target: IDraggable; dragIndex: number; hoverIndex: number }
+) => {
+  const indexOfParent = findItemIndex(activeItems, currentItem.parent!);
+  const targetIndex = findItemIndex(activeItems[indexOfParent].items || [], target);
+
+  if (targetIndex === hoverIndex) {
+    setActiveItems((prevActiveItems: IDraggable[]) =>
+      update(prevActiveItems, {
+        [indexOfParent]: {
+          items: {
+            $splice: [
+              [dragIndex, 1],
+              [hoverIndex, 0, prevActiveItems[indexOfParent].items![dragIndex]],
+            ],
+          },
+        },
+      })
+    );
+  }
+};
+
+const sortParents = (
+  activeItems: IDraggable[],
+  setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
+  {
+    target,
+    dragIndex,
+    hoverIndex,
+  }: { currentItem: IDraggable; target: IDraggable; dragIndex: number; hoverIndex: number }
+) => {
+  const targetIndex = findItemIndex(activeItems, target);
+  if (targetIndex === hoverIndex) {
+    setActiveItems((prevActiveItems: IDraggable[]) =>
+      update(prevActiveItems, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, prevActiveItems[dragIndex]],
+        ],
+      })
+    );
+  }
+};
+const sortActiveitems =
+  (activeItems: IDraggable[], setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>) =>
+  (currentItem: IDraggable, target: IDraggable, dragIndex: number, hoverIndex: number) => {
+    if (currentItem.parent !== undefined) {
+      sortChildren(activeItems, setActiveItems, { currentItem, target, dragIndex, hoverIndex });
+    } else {
+      sortParents(activeItems, setActiveItems, { currentItem, target, dragIndex, hoverIndex });
+    }
+  };
+
+export type { IDnDContext };
+export { addActiveItem, removeActiveItem, sortActiveitems, mapWithParent, mapWithID };
--- app/react/V2/CustomHooks/useDnDContext.tsx
+++ app/react/V2/CustomHooks/useDnDContext.tsx
@@ -1,182 +1,23 @@
 /* eslint-disable max-statements */
 import { useState } from 'react';
 import update from 'immutability-helper';
-import _ from 'lodash';
-import ID from 'shared/uniqueID';
 import { IDraggable, ItemTypes } from '../shared/types';
-
-interface RemoveOptions {
-  omitSource: boolean;
-}
-interface IDnDContext {
-  type: ItemTypes;
-  addItem: (item: IDraggable) => void;
-  removeItem: (item: IDraggable, options?: RemoveOptions) => void;
-  updateItems: (index: number, values: IDraggable) => void;
-  updateActiveItems: (items: IDraggable[]) => void;
-  sort: Function;
-  activeItems: IDraggable[];
-  availableItems: IDraggable[];
-  get?: (item: IDraggable) => IDnDContext;
-}
-
-const addActiveItem =
-  (
-    activeItems: IDraggable[],
-    setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
-    availableItems: IDraggable[],
-    setAvailableItems: React.Dispatch<React.SetStateAction<IDraggable[]>>
-  ) =>
-  (newItem: IDraggable, parent?: IDraggable) => {
-    if (newItem.parent) {
-      const indexOfCurrentParent = activeItems.findIndex(ai => ai.id === newItem.parent!.id);
-
-      setActiveItems((prevActiveItems: IDraggable[]) => {
-        const index = prevActiveItems[indexOfCurrentParent].items!.findIndex(
-          ai => ai.id === newItem.id
-        );
-        return update(prevActiveItems, {
-          [indexOfCurrentParent]: {
-            items: { $splice: [[index, 1]] },
-          },
-        });
-      });
-    }
-    const indexOfNewItem = activeItems.findIndex(ai => ai.id === newItem.id);
-
-    if (parent) {
-      if (indexOfNewItem > -1) {
-        setActiveItems((prevActiveItems: IDraggable[]) =>
-          update(prevActiveItems, {
-            $splice: [[indexOfNewItem, 1]],
-          })
-        );
-      }
-
-      setActiveItems((prevActiveItems: IDraggable[]) => {
-        const indexOfParent = prevActiveItems.findIndex(ai => ai.id === parent.id);
-        if (indexOfParent > -1) {
-          return prevActiveItems[indexOfParent].items
-            ? update(prevActiveItems, {
-                [indexOfParent]: {
-                  items: {
-                    $push: [{ ..._.omit(newItem, ['parent', 'container', 'items']), parent }],
-                  },
-                },
-              })
-            : update(prevActiveItems, {
-                [indexOfParent]: {
-                  items: {
-                    $set: [{ ...newItem, parent }],
-                  },
-                },
-              });
-        }
-        return prevActiveItems;
-      });
-    } else if (indexOfNewItem === -1) {
-      setActiveItems((prevActiveItems: IDraggable[]) =>
-        update(prevActiveItems, {
-          $push: [_.omit(newItem, ['parent', 'container', 'items'])],
-        })
-      );
-    }
-    const indexOfSource = availableItems.findIndex(ai => ai.id === newItem.id);
-
-    if (indexOfSource > -1) {
-      setAvailableItems(prevAvailableItems =>
-        update(prevAvailableItems, { $splice: [[indexOfSource, 1]] })
-      );
-    }
-  };
-
-const removeActiveItem =
-  (
-    activeItems: IDraggable[],
-    setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>,
-    setAvailableItems: React.Dispatch<React.SetStateAction<IDraggable[]>>
-  ) =>
-  (item: IDraggable, options: RemoveOptions = { omitSource: false }) => {
-    if (item.parent !== undefined) {
-      const indexOfParent = activeItems.findIndex(ai => ai.id === item.parent!.id);
-      setActiveItems((prevActiveItems: IDraggable[]) => {
-        const index = prevActiveItems[indexOfParent].items!.findIndex(ai => ai.id === item.id);
-        return update(prevActiveItems, {
-          [indexOfParent]: {
-            items: { $splice: [[index, 1]] },
-          },
-        });
-      });
-    } else {
-      const index = activeItems.findIndex(ai => ai.id === item.id);
-      setActiveItems((prevActiveItems: IDraggable[]) =>
-        update(prevActiveItems, {
-          $splice: [[index, 1]],
-        })
-      );
-    }
-
-    if (!options.omitSource) {
-      const availableSubItems = (item.items || []).map(ai => _.omit(ai, ['parent', 'container']));
-      setAvailableItems(prevAvailableItems =>
-        update(prevAvailableItems, {
-          $push: [_.omit(item, ['parent', 'container', 'items']), ...availableSubItems],
-        })
-      );
-    }
-  };
-
-const sortActiveitems =
-  (activeItems: IDraggable[], setActiveItems: React.Dispatch<React.SetStateAction<IDraggable[]>>) =>
-  (currentItem: IDraggable, target: IDraggable, dragIndex: number, hoverIndex: number) => {
-    const { parent } = currentItem;
-
-    if (parent !== undefined) {
-      const indexOfParent = activeItems.findIndex(item => item.id === parent.id);
-
-      setActiveItems((prevActiveItems: IDraggable[]) =>
-        update(prevActiveItems, {
-          [indexOfParent]: {
-            items: {
-              $splice: [
-                [dragIndex, 1],
-                [hoverIndex, 0, prevActiveItems[indexOfParent].items![dragIndex]],
-              ],
-            },
-          },
-        })
-      );
-    } else {
-      setActiveItems((prevActiveItems: IDraggable[]) =>
-        update(prevActiveItems, {
-          $splice: [
-            [dragIndex, 1],
-            [hoverIndex, 0, prevActiveItems[dragIndex]],
-          ],
-        })
-      );
-    }
-  };
+import {
+  IDnDContext,
+  addActiveItem,
+  removeActiveItem,
+  mapWithID,
+  mapWithParent,
+  sortActiveitems,
+} from './dragAndDrop/definitions';
 
 const useDnDContext = (
   type: ItemTypes,
   initialItems: IDraggable[] = [],
   sourceItems: IDraggable[] = []
 ) => {
-  const setParent: (items: IDraggable[], parent?: IDraggable) => IDraggable[] = (items, parent) =>
-    items.map(item => {
-      const id = item.id || ID();
-      const itemWithId = { ...item, id, ...(parent ? { parent } : {}) };
-      if (item.items && item.items.length > 0) {
-        return { ...itemWithId, items: setParent(item.items, itemWithId) };
-      }
-      return itemWithId;
-    });
-
-  const setID = (items: IDraggable[]) => items.map(item => ({ ...item, id: item.id || ID() }));
-
-  const [activeItems, setActiveItems] = useState<IDraggable[]>(setParent(initialItems));
-  const [availableItems, setAvailableItems] = useState<IDraggable[]>(setID(sourceItems || []));
+  const [activeItems, setActiveItems] = useState<IDraggable[]>(mapWithParent(initialItems));
+  const [availableItems, setAvailableItems] = useState<IDraggable[]>(mapWithID(sourceItems || []));
 
   const updateItems = (index: number, values: IDraggable) => {
     setActiveItems((prevActiveItems: IDraggable[]) =>
@@ -203,5 +44,4 @@ const useDnDContext = (
   return dndContext;
 };
 
-export type { IDnDContext };
 export { useDnDContext };
