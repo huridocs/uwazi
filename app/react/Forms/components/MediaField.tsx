4dc29858d21e6818bc76414bf25ec870c6fa5b0b
Santiago <borrajosantiago@gmail.com>
2023-11-29 15:34:32 -0300

    timelinks for newly added files

--- app/react/Forms/components/MediaField.tsx
+++ app/react/Forms/components/MediaField.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 import React, { useEffect, useState } from 'react';
 import { isObject } from 'lodash';
 import { Translate } from 'app/I18N';
@@ -15,14 +16,32 @@ type MediaFieldProps = MediaModalProps & {
   multipleEdition: boolean;
 };
 
-const getValue = (value: MediaFieldProps['value']) =>
-  isObject(value) && value.data ? value.data : (value as string);
+const getValue = (value: MediaFieldProps['value']) => {
+  let valueString = value as string;
+
+  if (isObject(value)) {
+    const hasTimeLinks = value.data.startsWith('(');
+
+    if (hasTimeLinks) {
+      valueString = value.data.substring(1, value.data.indexOf(','));
+    } else {
+      valueString = value.data;
+    }
+  }
+
+  return valueString;
+};
 
 const prepareValue = (
   value: MediaFieldProps['value'],
   localAttachments: MediaFieldProps['localAttachments']
 ) => {
+  if (!value) {
+    return undefined;
+  }
+
   const valueString = getValue(value);
+
   const values = {
     data: valueString,
     fileURL: valueString,
@@ -76,15 +95,18 @@ const MediaField = (props: MediaFieldProps) => {
   };
 
   const file = prepareValue(value, localAttachments);
+
   const constructTimelinksString = (timelinks: TimeLink[]) => {
     if (!file || !file.data) {
       return null;
     }
+
     const timelinksObj = timelinks.reduce((current: any, timelink) => {
       current[`${timelink.timeHours}:${timelink.timeMinutes}:${timelink.timeSeconds}`] =
         timelink.label;
       return current;
     }, {});
+
     const [, fileLocalID] = file.data.match(/\(?(.*?)(, {|$)/) || ['', file.data];
 
     return {
@@ -103,7 +125,7 @@ const MediaField = (props: MediaFieldProps) => {
         URL.revokeObjectURL(file.fileURL);
       }
     },
-    []
+    [file]
   );
 
   return (
