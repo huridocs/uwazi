641db936fa7e94c19f5141dfe0962ac390edb7b8
Kevin <kevin@huridocs.org>
2023-12-01 18:29:20 +0300

    Removed more timeouts

--- e2e/setupJestPuppeteer.js
+++ e2e/setupJestPuppeteer.js
@@ -1,5 +1,5 @@
 //eslint-disable-next-line
 const { setDefaultOptions } = require('expect-puppeteer');
 
-setDefaultOptions({ timeout: 3000 });
+setDefaultOptions({ timeout: 2000 });
 jest.setTimeout(40000);
