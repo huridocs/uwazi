40c0309558a8b174392be51647f774736bce78e9
Kevin <kevin@huridocs.org>
2023-12-08 12:26:06 +0300

    Waited for network

--- e2e/suite1/convert-entity-template.test.ts
+++ e2e/suite1/convert-entity-template.test.ts
@@ -36,6 +36,7 @@ describe('Image is rendered when editing an entity in document view', () => {
     await expect(page).toClick('.metadata-sidepanel button.edit-metadata', {
       text: 'Edit',
     });
+    await page.waitForNetworkIdle();
     await expect(page).toClick('.form-group.image span', { text: 'Add file' });
     await expect(page).toMatchElement('div.media-grid-card-header > h5', { text: 'batman.jpg' });
   });
