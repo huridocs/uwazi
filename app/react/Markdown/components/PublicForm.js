67c238b9a9021aa74a069d84a40b2033a33f16b3
Kevin <kevin@huridocs.org>
2023-12-13 15:21:52 +0300

    Fixed lint issues

--- app/react/Markdown/components/PublicForm.js
+++ app/react/Markdown/components/PublicForm.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-pascal-case */
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions, Control } from 'react-redux-form';
 import { bindActionCreators } from 'redux';
