5043fa8306c1184709758a2a2094c62b71ef9319
Santiago <borrajosantiago@gmail.com>
2023-12-14 15:58:35 -0300

    await pdfjslib

--- app/react/PDF/PDFJS.js
+++ app/react/PDF/PDFJS.js
@@ -1,23 +1,28 @@
 /* eslint-disable import/no-mutable-exports, global-require, prefer-destructuring */
+import * as pdfjs from 'pdfjs-dist';
+import { EventBus } from 'pdfjs-dist/web/pdf_viewer.mjs';
 import { isClient } from 'app/utils';
-import { EventBus } from 'pdfjs-dist/web/pdf_viewer';
 
 let PDFJS = {};
 let pdfjsLib = {};
 
-if (isClient) {
-  require('../../../node_modules/pdfjs-dist/web/pdf_viewer.css');
-  PDFJS = require('../../../node_modules/pdfjs-dist/web/pdf_viewer.js');
+const pdfjsLoader = async () => {
+  if (isClient) {
+    import('pdfjs-dist/web/pdf_viewer.css');
+    PDFJS = await import('pdfjs-dist/web/pdf_viewer.mjs');
 
-  if (process.env.HOT || process.env.NODE_ENV === 'test') {
-    pdfjsLib = require('pdfjs-dist');
-  } else {
-    pdfjsLib = require('pdfjs-dist/webpack');
+    if (process.env.HOT || process.env.NODE_ENV === 'test') {
+      pdfjsLib = pdfjs;
+    } else {
+      pdfjsLib = await import('pdfjs-dist/webpack.mjs');
+    }
+
+    const { WorkerMessageHandler } = await import('pdfjs-dist/build/pdf.worker.min.mjs');
+    pdfjsLib.GlobalWorkerOptions.workerSrc = WorkerMessageHandler;
   }
+};
 
-  pdfjsLib.GlobalWorkerOptions.workerSrc = require('pdfjs-dist/build/pdf.worker.entry');
-}
+await pdfjsLoader();
 
 export default { ...PDFJS, ...pdfjsLib };
-
 export { EventBus };
