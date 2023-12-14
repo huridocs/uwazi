d8cf133a6bb5684e466af394e20eee64ea89fe0f
Santiago <borrajosantiago@gmail.com>
2023-12-14 17:06:49 -0300

    ignore unnecesary types

--- app/react/V2/Components/PDFViewer/pdfjs.ts
+++ app/react/V2/Components/PDFViewer/pdfjs.ts
@@ -9,11 +9,14 @@ const CMAP_URL = 'legacy_character_maps';
 
 const pdfjsLoader = async () => {
   if (process.env.HOT || process.env.NODE_ENV === 'test') {
+    //@ts-ignore
     //this is to trigger pdfjs-dist fake worker instantiation in non production environments
     const fakeWorker = await import('pdfjs-dist/build/pdf.worker.min.mjs');
     pdfjs = pdfJsDist;
     pdfjs.GlobalWorkerOptions.workerSrc = fakeWorker;
   } else {
+    //@ts-ignore
+    //webpack bundled version for production, types are not needed.
     pdfjs = await import('pdfjs-dist/webpack.mjs');
   }
 };
