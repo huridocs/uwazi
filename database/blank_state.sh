a10f40d9c6898bf974c4d0e89e76134d04bfd880
Álvaro Hernández <alvaral475@gmail.com>
2024-01-16 13:28:19 +0100

    Refactor filter

--- database/blank_state.sh
+++ database/blank_state.sh
@@ -7,10 +7,39 @@ cd "$parent_path"
 
 DB=${1:-${DATABASE_NAME:-uwazi_development}}
 HOST=${2:-${DBHOST:-127.0.0.1}}
+FORCE_FLAG=${3:-false}
 
-echo -e "\n\nDeleting $DB database on $HOST"
-mongosh --quiet -host $HOST $DB --eval "db.dropDatabase()"
-mongorestore -h $HOST blank_state/uwazi_development/ --db=$DB
+recreate_database() {
+    mongosh --quiet -host $HOST $DB --eval "db.dropDatabase()"
+    mongorestore -h $HOST blank_state/uwazi_development/ --db=$DB
 
-INDEX_NAME=$DB DATABASE_NAME=$DB yarn migrate
-INDEX_NAME=$DB DATABASE_NAME=$DB yarn reindex
+    INDEX_NAME=$DB DATABASE_NAME=$DB yarn migrate
+    INDEX_NAME=$DB DATABASE_NAME=$DB yarn reindex
+
+    exit 0
+}
+
+echo -e "\nDB IS $DB"
+echo -e "\nHOST IS $HOST"
+echo -e "\nFORCE FLAG IS $FORCE_FLAG"
+
+mongo_indexof_db=$(mongosh --quiet -host $HOST --eval 'db.getMongo().getDBNames().indexOf("'$DB'")')
+# mongo_indexof_db="-1"
+
+echo -e "\nResult db: $mongo_indexof_db"
+
+if [ $mongo_indexof_db -ne "-1" ]; then
+    echo -e "\n$DB database already exists."
+    
+    if [ $FORCE_FLAG == "true" ]; then
+        echo -e "\nForce flag is true"
+        recreate_database
+    else
+        echo -e "\nForce flag is false"
+        echo -e "\n$DB already database exists. It will not be deleted."
+        exit 2
+    fi
+else
+    echo -e "\n$DB database does not exist"
+    recreate_database
+fi
