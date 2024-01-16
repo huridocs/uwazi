a91c1f0b89944c6317ffbf846d89ff586accf07d
Santiago <borrajosantiago@gmail.com>
2024-01-16 10:58:14 -0300

    use LocalForm

--- app/react/Markdown/components/ContactForm.js
+++ app/react/Markdown/components/ContactForm.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { actions, Field } from 'react-redux-form';
 import { Translate, t } from 'app/I18N';
 import api from 'app/utils/api';
 import { Icon } from 'UI';
@@ -7,66 +8,90 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { notify } from 'app/Notifications/actions/notificationsActions';
 import { RequestParams } from 'app/utils/RequestParams';
+import { LocalForm } from 'app/Forms/Form';
+import { FormCaptcha } from './FormCaptcha';
 
 class ContactForm extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = { name: '', email: '', message: '' };
     this.submit = this.submit.bind(this);
+    this.validators = {
+      name: { required: val => val },
+      email: { required: val => val },
+      message: { required: val => val },
+      captcha: { required: val => val && val.text.length },
+    };
+    this.refreshFn = refresh => {
+      this.refreshCaptcha = refresh;
+    };
   }
 
-  onChange(key, e) {
-    const changedData = {};
-    changedData[key] = e.target.value;
-    this.setState(changedData);
+  attachDispatch(dispatch) {
+    this.formDispatch = dispatch;
   }
 
-  async submit(e) {
-    e.preventDefault();
-    await api.post('contact', new RequestParams(this.state));
-    this.props.notify(t('System', 'Message sent', null, false), 'success');
-    this.setState({ name: '', email: '', message: '' });
+  resetForm() {
+    this.formDispatch(actions.reset('contactform'));
+  }
+
+  async submit(values) {
+    console.log(values);
+    try {
+      await api.post('contact', new RequestParams(values));
+      this.props.notify(t('System', 'Message sent', null, false), 'success');
+      this.resetForm();
+    } catch (e) {
+      this.props.notify(t('System', 'An error occurred', null, false), 'danger');
+      this.resetForm();
+    }
+
+    this.refreshCaptcha();
   }
 
   render() {
     return (
-      <form onSubmit={this.submit} className="contact-form">
+      <LocalForm
+        validators={this.validators}
+        model="contactform"
+        className="contact-form"
+        getDispatch={dispatch => this.attachDispatch(dispatch)}
+        onSubmit={this.submit}
+      >
         <div className="form-group">
           <label className="form-group-label" htmlFor="name">
             <Translate>Name</Translate>
+            <span className="required">*</span>
           </label>
-          <input
-            required
-            name="name"
-            className="form-control"
-            onChange={this.onChange.bind(this, 'name')}
-            value={this.state.name}
-          />
+
+          <Field model=".name">
+            <input type="text" required name="name" className="form-control" />
+          </Field>
         </div>
+
         <div className="form-group">
           <label className="form-group-label" htmlFor="email">
             <Translate>Email</Translate>
+            <span className="required">*</span>
           </label>
-          <input
-            required
-            name="email"
-            className="form-control"
-            onChange={this.onChange.bind(this, 'email')}
-            value={this.state.email}
-          />
+
+          <Field model=".email">
+            <input type="email" required name="email" className="form-control" />
+          </Field>
         </div>
+
         <div className="form-group">
           <label className="form-group-label" htmlFor="message">
             <Translate>Message</Translate>
+            <span className="required">*</span>
           </label>
-          <textarea
-            required
-            name="message"
-            className="form-control"
-            onChange={this.onChange.bind(this, 'message')}
-            value={this.state.message}
-          />
+
+          <Field model=".message">
+            <textarea required name="message" className="form-control" />
+          </Field>
         </div>
+
+        <FormCaptcha refresh={this.refreshFn} />
+
         <button type="submit" className="btn btn-success">
           <Icon icon="paper-plane" />
           &nbsp;
@@ -74,7 +99,7 @@ class ContactForm extends Component {
             <Translate>Send</Translate>
           </span>
         </button>
-      </form>
+      </LocalForm>
     );
   }
 }
