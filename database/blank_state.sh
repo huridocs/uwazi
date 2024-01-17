0f5a8102cbda49d20b479570a6dfca65d77a4597
Álvaro Hernández <alvaral475@gmail.com>
2024-01-17 14:20:43 +0100

    Refactor code

--- database/blank_state.sh
+++ database/blank_state.sh
@@ -7,8 +7,7 @@ cd "$parent_path"
 
 DB=${1:-${DATABASE_NAME:-uwazi_development}}
 HOST=${2:-${DBHOST:-127.0.0.1}}
-FORCE_FLAG=${3:-false}
-FORCE_DB_STATE=${4}
+FORCE_FLAG=${3}
 
 recreate_database() {
     mongosh --quiet -host $HOST $DB --eval "db.dropDatabase()"
@@ -23,28 +22,22 @@ recreate_database() {
 echo -e "\nDB IS $DB"
 echo -e "\nHOST IS $HOST"
 echo -e "\nFORCE FLAG IS $FORCE_FLAG"
-echo -e "\nFORCE DB STATE $FORCE_DB_STATE"
 
-if [ -z "$FORCE_DB_STATE" ]; then
-        echo -e "\nFORCE_DB_STATE is empty"
-        mongo_indexof_db=$(mongosh --quiet -host $HOST --eval 'db.getMongo().getDBNames().indexOf("'$DB'")')
-    else
-        echo -e "\nFORCE_DB_STATE is not empty"
-        mongo_indexof_db=$FORCE_DB_STATE
-fi
+mongo_indexof_db=$(mongosh --quiet -host $HOST --eval 'db.getMongo().getDBNames().indexOf("'$DB'")')
+# mongo_indexof_db="-1"
 
 echo -e "\nResult db: $mongo_indexof_db"
 
 if [ $mongo_indexof_db -ne "-1" ]; then
     echo -e "\n$DB database already exists."
     
-    if [ $FORCE_FLAG == "true" ]; then
-        echo -e "\nForce flag is true"
-        recreate_database
-    else
-        echo -e "\nForce flag is false"
+    if [ -z "$FORCE_FLAG" ]; then
+        echo -e "\nForce flag not set"
         echo -e "\n$DB already database exists. It will not be deleted."
-        exit 2
+        exit 2    
+    else
+        echo -e "\nForce flag set"
+        recreate_database
     fi
 else
     echo -e "\n$DB database does not exist"
