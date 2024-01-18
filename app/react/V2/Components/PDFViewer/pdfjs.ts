aac4fbac3b1708391b7a328d1b0115d7f8c66deb
Santiago <borrajosantiago@gmail.com>
2024-01-18 10:37:33 -0300

    fix type error

--- app/react/V2/Components/PDFViewer/pdfjs.ts
+++ app/react/V2/Components/PDFViewer/pdfjs.ts
@@ -2,7 +2,7 @@ import * as pdfJsDist from 'pdfjs-dist';
 import * as viewer from 'pdfjs-dist/web/pdf_viewer.mjs';
 import 'pdfjs-dist/web/pdf_viewer.css';
 
-let pdfjs;
+let pdfjs = pdfJsDist;
 const PDFJSViewer = viewer;
 const { EventBus } = viewer;
 const CMAP_URL = 'legacy_character_maps';
@@ -12,7 +12,6 @@ const pdfjsLoader = async () => {
     //@ts-ignore
     //this is to trigger pdfjs-dist fake worker instantiation in non production environments
     const fakeWorker = await import('pdfjs-dist/build/pdf.worker.min.mjs');
-    pdfjs = pdfJsDist;
     pdfjs.GlobalWorkerOptions.workerSrc = fakeWorker;
   } else {
     //@ts-ignore
@@ -23,6 +22,6 @@ const pdfjsLoader = async () => {
 
 await pdfjsLoader();
 
-const PDFJS: typeof pdfjs = pdfjs;
+const PDFJS = pdfjs;
 
 export { PDFJS, PDFJSViewer, EventBus, CMAP_URL };
