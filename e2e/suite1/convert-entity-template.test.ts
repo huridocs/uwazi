249c695d9f983cfbe1c63e5dbc2ac16d53d838ba
mfacar <mercy@huridocs.org>
2024-01-26 14:27:49 -0500

    wait for selector instead of time

--- e2e/suite1/convert-entity-template.test.ts
+++ e2e/suite1/convert-entity-template.test.ts
@@ -37,7 +37,7 @@ describe('Image is rendered when editing an entity in document view', () => {
     await expect(page).toClick('.metadata-sidepanel button.edit-metadata', {
       text: 'Edit',
     });
-    await page.waitForTimeout(200);
+    await page.waitForSelector('.form-group.image span');
     await expect(page).toClick('.form-group.image span', { text: 'Add file' });
     await expect(page).toMatchElement('div.media-grid-card-header > h5', { text: 'batman.jpg' });
   });
