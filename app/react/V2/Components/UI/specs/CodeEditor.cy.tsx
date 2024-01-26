90e86c6d3a6184cf2c9cfa722a50a2a831bf7c99
Santiago <borrajosantiago@gmail.com>
2024-01-26 09:45:12 -0300

    initial tests for code editor

---
+++ app/react/V2/Components/UI/specs/CodeEditor.cy.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import 'cypress-axe';
+import { mount } from '@cypress/react18';
+import { composeStories } from '@storybook/react';
+import * as stories from 'app/stories/CodeEditor.stories';
+
+const { HTMLEditor, JSEditor } = composeStories(stories);
+
+describe('Code editor', () => {
+  it('should render the editor with existing HTML', () => {
+    mount(<HTMLEditor />);
+    cy.contains('<h1>Main Heading</h1>').should('exist');
+    cy.get('.view-lines').children().should('have.length', 21);
+  });
+
+  it('should be able to edit', () => {
+    mount(<HTMLEditor />);
+    cy.contains('<p>Subtitle or tagline goes here</p>').then(element => {
+      cy.wrap(element).click();
+      cy.wrap(element).focused().type('{ctrl}a');
+      cy.wrap(element).focused().type('{del}');
+    });
+
+    cy.contains('<h1>Main Heading</h1>').should('not.exist');
+  });
+
+  it('should mount an empty editor if there is no code', () => {
+    JSEditor.args.code = undefined;
+    mount(<JSEditor />);
+    cy.get('.view-lines').children().should('have.length', 1);
+  });
+});
