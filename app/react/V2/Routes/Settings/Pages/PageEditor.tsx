3b5224a3807a0def40713456f29e0112fa1f65cd
Santiago <borrajosantiago@gmail.com>
2024-01-27 12:33:29 -0300

    basic layout

--- app/react/V2/Routes/Settings/Pages/PageEditor.tsx
+++ app/react/V2/Routes/Settings/Pages/PageEditor.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IncomingHttpHeaders } from 'http';
 import { LoaderFunction, useLoaderData } from 'react-router-dom';
+import { Translate, t } from 'app/I18N';
 import * as pagesAPI from 'V2/api/pages';
 import { Page } from 'V2/shared/types';
+import { SettingsContent } from 'V2/Components/Layouts/SettingsContent';
+import { Button, Tabs } from 'V2/Components/UI';
 
 const pageEditorLoader =
   (headers?: IncomingHttpHeaders): LoaderFunction =>
@@ -13,17 +16,51 @@ const pageEditorLoader =
       return page;
     }
 
-    return null;
+    return {};
   };
 
 const PageEditor = () => {
   const page = useLoaderData() as Page;
+  const [pageName, setPageName] = useState<string>(
+    page?.title || t('System', 'New page', null, false)
+  );
 
-  if (page) {
-    return <h1>Edit page {page.title}</h1>;
-  }
+  return (
+    <div className="tw-content" style={{ width: '100%', overflowY: 'auto' }}>
+      <SettingsContent>
+        <SettingsContent.Header
+          path={new Map([['Translations', '/settings/translations']])}
+          title={pageName}
+        />
 
-  return <h1>New page</h1>;
+        <SettingsContent.Body>
+          <Tabs onTabSelected={tab => {}}>
+            <Tabs.Tab id="Basic" label={<Translate>Basic</Translate>}></Tabs.Tab>
+
+            <Tabs.Tab id="Code" label={<Translate>Code</Translate>}></Tabs.Tab>
+
+            <Tabs.Tab id="Advanced" label={<Translate>Advanced</Translate>}></Tabs.Tab>
+          </Tabs>
+        </SettingsContent.Body>
+
+        <SettingsContent.Footer>
+          <div className="flex gap-2 justify-end">
+            <Button styling="light" onClick={() => {}}>
+              <Translate>Cancel</Translate>
+            </Button>
+
+            <Button styling="solid" color="primary" onClick={() => {}}>
+              <Translate>Save & Preview</Translate>
+            </Button>
+
+            <Button styling="solid" color="success" onClick={() => {}}>
+              <Translate>Save</Translate>
+            </Button>
+          </div>
+        </SettingsContent.Footer>
+      </SettingsContent>
+    </div>
+  );
 };
 
 export { PageEditor, pageEditorLoader };
