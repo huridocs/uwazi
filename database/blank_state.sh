098c72ffd99325a367e49096725e21c5f616794c
Daneryl <daneryl@gmail.com>
2024-02-06 15:25:22 +0100

    fix blank_state tests and refactor

--- database/blank_state.sh
+++ database/blank_state.sh
@@ -1,126 +1,27 @@
 #!/bin/bash
+set -e
 
-[[ -f ".env" ]] && source ".env"
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
+cd "$parent_path" || exit
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
+FORCE_FLAG=false
 
-# Comprobamos si hay parámetros
-if [ $# -gt 0 ]; then
-    echo -e "\nHay params"
+filtered=()
+args=("$@")
+for item in "${args[@]}"; do
+    if [ "$item" == '--force' ]; then
+        FORCE_FLAG=true
+    else
+        filtered+=("$item")
+    fi
+done
 
-    case $# in
-        1)
-            echo -e "\n1 param y es: $1"
-            if [ $1 == "--force" ]; then
-                # Si es force seteamos la FORCE_FLAG
-                FORCE_FLAG=$1
-                echo -e "\nFORCE_FLAG ahora vale: $FORCE_FLAG"
-            
-                # Seteamos DB y HOST por defecto
-                DB=${DATABASE_NAME:-uwazi_development}
-            else
-                # Asignamos el valor a DB
-                DB=$1
-            fi
-
-            # Seteamos HOST por defecto
-            HOST=${DBHOST:-127.0.0.1}
-
-            echo -e "\nDB ahora vale: $DB"
-            echo -e "\nHOST ahora vale: $HOST"
-
-            # exit 0
-            ;;
-        2)
-            echo -e "\n2 params"
-            if [ $1 == "--force" ]; then
-                # Seteamos la FORCE_FLAG
-                FORCE_FLAG=$1
-                
-                # Asignamos el valor a DB
-                DB=$2
-            else
-                # Seteamos la FORCE_FLAG
-                FORCE_FLAG=$2
-
-                # Asignamos el valor a DB
-                DB=$1
-            fi
-            echo -e "\nFORCE_FLAG ahora vale: $FORCE_FLAG"
-            
-            # Seteamos HOST por defecto
-            HOST=${DBHOST:-127.0.0.1}
-
-            echo -e "\nDB ahora vale: $DB"
-            echo -e "\nHOST ahora vale: $HOST"
-
-            # exit 0
-            ;;
-        3)
-            echo -e "\n3 params"
-
-            if [ $1 == "--force" ]; then
-                echo -e "Force es el primero"
-                # Seteamos la FORCE_FLAG
-                FORCE_FLAG=$1
-
-                # Asignamos el valor a DB
-                DB=$2
-
-                # Asignamos el valor a HOST
-                HOST=$3
-
-            elif [ $2 == "--force" ]; then
-                echo -e "Force es el segundo"
-                # Seteamos la FORCE_FLAG
-                FORCE_FLAG=$2
-
-                # Asignamos el valor a DB
-                DB=$1
-
-                # Asignamos el valor a HOST
-                HOST=$3
-            else
-                # Seteamos la FORCE_FLAG
-                echo -e "Force es el tercero"
-                FORCE_FLAG=$3
-
-                # Asignamos el valor a DB
-                DB=$1
-
-                # Asignamos el valor a HOST
-                HOST=$2
-            fi
-
-            echo -e "\nFORCE_FLAG ahora vale: $FORCE_FLAG"
-            echo -e "\nDB ahora vale: $DB"
-            echo -e "\nHOST ahora vale: $HOST"
-
-            # exit 0
-            ;;
-    esac 
-else
-    echo -e "\nNo hay params"
-    # Se setean db y host por defecto
-    
-    DB=${DATABASE_NAME:-uwazi_development}
-    HOST=${DBHOST:-127.0.0.1}
-
-    echo -e "\nDB ahora vale: $DB"
-    echo -e "\nHOST ahora vale: $HOST"
-
-    # exit 0
-fi
-
-echo -e "\nANTES DE HACER LAS OPERACIONES: "            
-echo -e "\nFORCE_FLAG es: $FORCE_FLAG"
-echo -e "\nDB es: $DB"
-echo -e "\nHOST es: $HOST"
+DB=${filtered[0]:-${DATABASE_NAME:-uwazi_development}}
+HOST=${filtered[1]:-${DBHOST:-127.0.0.1}}
 
 recreate_database() {
-    mongosh --quiet -host $HOST $DB --eval "db.dropDatabase()"
-    mongorestore -h $HOST blank_state/uwazi_development/ --db=$DB
+    mongosh --quiet -host "$HOST" "$DB" --eval "db.dropDatabase()"
+    mongorestore -h "$HOST" blank_state/uwazi_development/ --db="$DB"
 
     INDEX_NAME=$DB DATABASE_NAME=$DB yarn migrate
     INDEX_NAME=$DB DATABASE_NAME=$DB yarn reindex
@@ -128,16 +29,13 @@ recreate_database() {
     exit 0
 }
 
-mongo_indexof_db=$(mongosh --quiet -host $HOST --eval 'db.getMongo().getDBNames().indexOf("'$DB'")')
+mongo_indexof_db=$(mongosh --quiet -host "$HOST" --eval "db.getMongo().getDBNames().indexOf('$DB')")
 
-if [ $mongo_indexof_db -ne "-1" ]; then    
-    if [ -z "$FORCE_FLAG" ]; then
+if [ "$mongo_indexof_db" -ne "-1" ]; then    
+    if [ "$FORCE_FLAG" = false ]; then
         echo -e "\n$DB already database exists. It will not be deleted."
         exit 2    
-    else
-        recreate_database
     fi
-else
-    recreate_database
 fi
+recreate_database
 
--- scripts/test_blank_state.sh
+++ scripts/test_blank_state.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )
+cd "$parent_path" || exit
 
-assert() {                         
+assert_equals() {
   E_PARAM_ERR=98
   E_ASSERT_FAILED=99
 
-  if [ -z "$2" ] 
+  if [ -z "$3" ]
   then                   
     return $E_PARAM_ERR   
   fi
 
   message=$1
-  assertion=$2
 
-  if [ ! $assertion ] 
+  if [ ! "$2" == "$3" ]
   then
     echo "❌ $message"
     exit $E_ASSERT_FAILED
@@ -29,26 +28,34 @@ mongosh --quiet new-db --eval "db.dropDatabase()"
 
 echo -e "\nTest blank state -> new_db"
 yarn blank-state new-db > /dev/null 2>&1; result=$?
-assert "Creating new-db should be successful" "$result == 0"
+assert_equals "Creating new-db should be successful" $result 0
 
 echo -e "\nTest blank state -> Database existing should exit with error"
 yarn blank-state new-db > /dev/null 2>&1; result=$?
-assert "Creating new-db again should throw error" "$result == 2"
+assert_equals "Creating new-db again should throw error" $result 2
 
-echo -e "\nTest blank state -> Database existing with --force flag"
-yarn blank-state --force > /dev/null 2>&1; result=$?
-assert "Creating default db with force should be successful" "$result == 0"
+echo -e "\nTest blank state -> Database existing with force flag"
+yarn blank-state --force new-db > /dev/null 2>&1; result=$?
+assert_equals "Creating new-db again should be successful" $result 0
 
-mongosh --quiet uwazi_development --eval "db.dropDatabase()"
+echo -e "\nTest blank state -> Database existing with force flag in any order"
+yarn blank-state new-db --force > /dev/null 2>&1; result=$?
+assert_equals "Creating new-db again should be successful" $result 0
+
+export DATABASE_NAME="uwazi_development_test";
+mongosh --quiet uwazi_development_test --eval "db.dropDatabase()"
 
 echo -e "\nTest blank state -> Default params"
 yarn blank-state > /dev/null 2>&1; result=$?
-assert "Creating default db should be successful" "$result == 0"
+assert_equals "Creating default db should be successful" $result 0
+
+echo -e "\nTest blank state -> Database existing with --force flag"
+yarn blank-state --force > /dev/null 2>&1; result=$?
+assert_equals "Creating default db with force should be successful" $result 0
 
 echo -e "\nTest blank state -> Database deafult existing should exit with error"
 yarn blank-state > /dev/null 2>&1; result=$?
-assert "Creating default db again should throw error" "$result == 2"
+assert_equals "Creating default db again should throw error" $result 2
 
-echo -e "\nTest blank state -> Database default existing with --force flag"
-yarn blank-state --force > /dev/null 2>&1; result=$?
-assert "Creating default db with force should be successful" "$result == 0"
+mongosh --quiet uwazi_development_test --eval "db.dropDatabase()"
+mongosh --quiet new-db --eval "db.dropDatabase()"
