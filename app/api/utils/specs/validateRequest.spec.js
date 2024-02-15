a41630dc6f7c4a0962adb777647c3417aaff57c5
Laszlo Kecskes <laszlokecskes@protonmail.com>
2024-02-15 10:39:42 +0100

    remove joi from validation utilities

--- app/api/utils/specs/validateRequest.spec.js
+++ app/api/utils/specs/validateRequest.spec.js
@@ -1,5 +1,3 @@
-import Joi from 'joi';
-
 import middleware from '../validateRequest.js';
 
 describe('validateRequest', () => {
@@ -16,92 +14,61 @@ describe('validateRequest', () => {
     };
   });
 
-  describe('using JOI schema', () => {
-    it('should call next when body or query is valid', () => {
-      const schema = Joi.object().keys({
-        prop1: Joi.string(),
-        prop2: Joi.number(),
-      });
+  it('should call next when body or query is valid', async () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        body: {
+          type: 'object',
+          properties: {
+            prop1: { type: 'string' },
+            prop2: { type: 'number' },
+          },
+        },
+        query: {
+          type: 'object',
+          properties: {
+            prop1: { type: 'string' },
+            prop2: { type: 'number' },
+          },
+        },
+      },
+    };
 
-      req.body = { prop1: 'string', prop2: 25 };
-      middleware(schema)(req, res, next);
-      expect(next).toHaveBeenCalledWith();
+    req.body = { prop1: 'string', prop2: 25 };
+    await middleware(schema)(req, res, next);
+    expect(next).toHaveBeenCalledWith();
 
-      req.query = { prop1: 'string', prop2: 25 };
-      middleware(schema, 'query')(req, res, next);
-      expect(next).toHaveBeenCalledWith();
-    });
-
-    it('should call next with the error', () => {
-      const schema = Joi.object().keys({
-        prop1: Joi.string(),
-        prop2: Joi.number(),
-      });
-
-      req.body = { prop1: 55, prop2: 'must be number' };
-      middleware(schema)(req, res, next);
-
-      expect(next.mock.calls[0]).toMatchSnapshot();
-    });
+    next.mockReset();
+    req.query = { prop1: 'string', prop2: 25 };
+    await middleware(schema)(req, res, next);
+    expect(next).toHaveBeenCalledWith();
   });
 
-  describe('using AJV schema', () => {
-    it('should call next when body or query is valid', async () => {
-      const schema = {
-        type: 'object',
-        properties: {
-          body: {
-            type: 'object',
-            properties: {
-              prop1: { type: 'string' },
-              prop2: { type: 'number' },
-            },
-          },
-          query: {
-            type: 'object',
-            properties: {
-              prop1: { type: 'string' },
-              prop2: { type: 'number' },
-            },
+  it('should call next with the error', async () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        body: {
+          type: 'object',
+          properties: {
+            prop1: { type: 'string' },
+            prop2: { type: 'number' },
           },
         },
-      };
+      },
+    };
 
-      req.body = { prop1: 'string', prop2: 25 };
-      await middleware(schema)(req, res, next);
-      expect(next).toHaveBeenCalledWith();
-
-      next.mockReset();
-      req.query = { prop1: 'string', prop2: 25 };
-      await middleware(schema)(req, res, next);
-      expect(next).toHaveBeenCalledWith();
+    req.body = { prop1: 55, prop2: 'must be number' };
+    await middleware(schema)(req, res, next);
+    const expected = expect.objectContaining({
+      message: 'validation failed',
+      errors: [
+        expect.objectContaining({ message: 'must be string' }),
+        expect.objectContaining({ message: 'must be number' }),
+      ],
+      code: 400,
     });
-
-    it('should call next with the error', async () => {
-      const schema = {
-        type: 'object',
-        properties: {
-          body: {
-            type: 'object',
-            properties: {
-              prop1: { type: 'string' },
-              prop2: { type: 'number' },
-            },
-          },
-        },
-      };
-
-      req.body = { prop1: 55, prop2: 'must be number' };
-      await middleware(schema)(req, res, next);
-      const expected = expect.objectContaining({
-        message: 'validation failed',
-        errors: [
-          expect.objectContaining({ message: 'must be string' }),
-          expect.objectContaining({ message: 'must be number' }),
-        ],
-        code: 400,
-      });
-      expect(next.mock.calls[0]).toEqual([expected]);
-    });
+    expect(next.mock.calls[0]).toEqual([expected]);
   });
 });
--- app/api/utils/validateRequest.js
+++ app/api/utils/validateRequest.js
@@ -1,11 +1,7 @@
 import Ajv from 'ajv';
-import Joi from 'joi';
-import objectId from 'joi-objectid';
 import { wrapValidator } from 'shared/tsUtils';
 import addFormats from 'ajv-formats';
 import createError from './Error';
-
-Joi.objectId = objectId(Joi);
 
 const ajv = new Ajv({ allErrors: true });
 ajv.addVocabulary(['tsType']);
@@ -13,17 +9,6 @@ const coercedAjv = new Ajv({ allErrors: true, coerceTypes: 'array' });
 coercedAjv.addVocabulary(['tsType']);
 
 addFormats(ajv);
-
-const JoiDeprecatedValidation = (schema, propTovalidate, req, next) => {
-  const result = Joi.validate(req[propTovalidate], schema);
-  if (result.error) {
-    next(createError(result.error.toString(), 400));
-  }
-
-  if (!result.error) {
-    next();
-  }
-};
 
 const ajvValidation = async (ajvInstance, schema, req, next) => {
   try {
@@ -40,17 +25,9 @@ const ajvValidation = async (ajvInstance, schema, req, next) => {
   }
 };
 
-const createRequestValidator =
-  ajvInstance =>
-  (schema, propTovalidate = 'body') =>
-  async (req, _res, next) => {
-    if (schema.isJoi) {
-      JoiDeprecatedValidation(schema, propTovalidate, req, next);
-      return;
-    }
-
-    await ajvValidation(ajvInstance, schema, req, next);
-  };
+const createRequestValidator = ajvInstance => schema => async (req, _res, next) => {
+  await ajvValidation(ajvInstance, schema, req, next);
+};
 
 export const validateAndCoerceRequest = createRequestValidator(coercedAjv);
 
