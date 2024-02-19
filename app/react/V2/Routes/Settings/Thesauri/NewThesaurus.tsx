0e6d45468b971e40ef2b0182622d38c7fe0340ef
Kevin Nderitu <kevin@huridocs.org>
2024-02-19 15:11:43 +0300

    New thesauri item form

--- app/react/V2/Routes/Settings/Thesauri/NewThesaurus.tsx
+++ app/react/V2/Routes/Settings/Thesauri/NewThesaurus.tsx
@@ -1,28 +1,79 @@
-import { ColumnDef, Row, createColumnHelper } from '@tanstack/react-table';
-import { Translate, t } from 'app/I18N';
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { Translate } from 'app/I18N';
 import { SettingsContent } from 'app/V2/Components/Layouts/SettingsContent';
-import { Button, Table } from 'app/V2/Components/UI';
+import { Button, Sidepanel, Table } from 'app/V2/Components/UI';
 import React, { useState } from 'react';
-import { ThesaurusSchema } from 'shared/types/thesaurusType';
-import { ThesaurusLabel } from './components/TableComponents';
+import { ThesaurusSchema, ThesaurusValueSchema } from 'shared/types/thesaurusType';
+import { EditButton, ThesaurusValueLabel } from './components/TableComponents';
 import { InputField } from 'app/V2/Components/Forms';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AddItemForm } from './components/AddItemForm';
+import { useForm } from 'react-hook-form';
+import uniqueID from 'shared/uniqueID';
+import ThesauriAPI from 'app/Thesauri/ThesauriAPI';
+import { RequestParams } from 'app/utils/RequestParams';
+import { useSetRecoilState } from 'recoil';
+import { notificationAtom } from 'app/V2/atoms/notificationAtom';
 
 const NewThesauri = () => {
   const columnHelper = createColumnHelper<any>();
+  const navigate = useNavigate();
+  const [isSidepanelOpen, setIsSidepanelOpen] = useState(false);
+
+  const setNotifications = useSetRecoilState(notificationAtom);
+
+  const {
+    watch,
+    setValue,
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ThesaurusSchema>({
+    mode: 'onSubmit',
+  });
+
+  const addItemSubmit = (value: ThesaurusValueSchema) => {
+    const id = uniqueID();
+    const curatedValue = { label: value.label, id };
+    setValue(
+      'values',
+      getValues().values
+        ? [...(getValues().values as ThesaurusValueSchema[]), curatedValue]
+        : [curatedValue]
+    );
+    setIsSidepanelOpen(false);
+  };
+
+  const submitThesauri = async (data: ThesaurusSchema) => {
+    try {
+      await ThesauriAPI.save(new RequestParams(data));
+      setNotifications({
+        type: 'success',
+        text: <Translate>Thesauri added.</Translate>,
+      });
+      navigate('/settings/thesauri');
+    } catch (e) {
+      setNotifications({
+        type: 'error',
+        text: <Translate>Error adding thesauri.</Translate>,
+      });
+    }
+  };
+
   const columns = [
-    columnHelper.accessor('name', {
-      id: 'name',
+    columnHelper.accessor('label', {
+      id: 'label',
       header: 'Label',
-      cell: ThesaurusLabel,
+      cell: ThesaurusValueLabel,
       meta: { headerClassName: 'w-9/12' },
-    }) as ColumnDef<ThesaurusSchema, 'name'>,
-    columnHelper.accessor('_id', {
+    }) as ColumnDef<ThesaurusValueSchema, 'label'>,
+    columnHelper.accessor('id', {
       header: 'Action',
-      cell: '',
+      cell: EditButton,
       enableSorting: false,
       meta: { action: () => {}, headerClassName: 'text-center w-1/12' },
-    }) as ColumnDef<ThesaurusSchema, '_id'>,
+    }) as ColumnDef<ThesaurusValueSchema, 'id'>,
   ];
 
   return (
@@ -34,7 +85,7 @@ const NewThesauri = () => {
       <SettingsContent>
         <SettingsContent.Header
           path={new Map([['Thesauri', '/settings/thesauri']])}
-          title="untitled"
+          title={watch('name')}
         />
         <SettingsContent.Body>
           <div data-testid="thesauri" className="mb-4 border rounded-md shadow-sm border-gray-50">
@@ -44,20 +95,22 @@ const NewThesauri = () => {
                 id="thesauri-name"
                 placeholder="untitled"
                 className="mb-2"
+                {...register('name', { required: true })}
+                hasErrors={!!errors.name}
               />
             </div>
-            <Table<ThesaurusSchema>
+            <Table<ThesaurusValueSchema>
               enableSelection
               columns={columns}
-              data={[]}
-              initialState={{ sorting: [{ id: 'name', desc: false }] }}
+              data={getValues().values || []}
+              initialState={{ sorting: [{ id: 'label', desc: false }] }}
             />
           </div>
         </SettingsContent.Body>
         <SettingsContent.Footer className="bg-indigo-50">
           <div className="flex justify-between w-full">
             <div className="flex gap-2">
-              <Button>
+              <Button onClick={() => setIsSidepanelOpen(true)}>
                 <Translate>Add item</Translate>
               </Button>
               <Button styling="outline">
@@ -76,19 +129,22 @@ const NewThesauri = () => {
                   <Translate>Cancel</Translate>
                 </Button>
               </Link>
-              <Button
-                styling="solid"
-                color="success"
-                onClick={() => {
-                  console.log('Saving thesauri');
-                }}
-              >
+              <Button styling="solid" color="success" onClick={handleSubmit(submitThesauri)}>
                 <Translate>Save</Translate>
               </Button>
             </div>
           </div>
         </SettingsContent.Footer>
       </SettingsContent>
+      <Sidepanel
+        title={<Translate className="uppercase">Add item</Translate>}
+        isOpen={isSidepanelOpen}
+        closeSidepanelFunction={() => setIsSidepanelOpen(false)}
+        size="medium"
+        withOverlay
+      >
+        <AddItemForm submit={addItemSubmit} closePanel={() => setIsSidepanelOpen(false)} />
+      </Sidepanel>
     </div>
   );
 };
