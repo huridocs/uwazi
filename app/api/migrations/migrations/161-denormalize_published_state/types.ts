0eee116bedb11036bffad06b0fac2f39c79ee2e6
Laszlo Kecskes <laszlokecskes@protonmail.com>
2024-02-22 13:53:07 +0100

    add migration types

--- app/api/migrations/migrations/161-denormalize_published_state/types.ts
+++ app/api/migrations/migrations/161-denormalize_published_state/types.ts
@@ -1,13 +1,65 @@
 import { ObjectId } from 'mongodb';
 
+type PropertyType =
+  | 'date'
+  | 'daterange'
+  | 'geolocation'
+  | 'image'
+  | 'link'
+  | 'markdown'
+  | 'media'
+  | 'multidate'
+  | 'multidaterange'
+  | 'multiselect'
+  | 'nested'
+  | 'numeric'
+  | 'preview'
+  | 'relationship'
+  | 'select'
+  | 'text'
+  | 'generatedid'
+  | 'newRelationship';
+
+interface Property {
+  _id?: ObjectId;
+  label: string;
+  name: string;
+  type: PropertyType;
+  content?: string;
+  inherit?: {
+    property?: string;
+    type?: PropertyType;
+  };
+}
+
+interface Template {
+  _id?: ObjectId;
+  name: string;
+  properties?: Property[];
+}
+
+interface MetadataObject {
+  value: string;
+  label?: string;
+}
+
+interface Metadata {
+  [k: string]: MetadataObject[] | undefined;
+}
+
 interface Entity {
-  _id: ObjectId;
-  title: string;
-  [k: string]: unknown | undefined;
+  _id?: ObjectId;
+  sharedId?: string;
+  language?: string;
+  title?: string;
+  template?: ObjectId;
+  published?: boolean;
+  metadata?: Metadata;
 }
 
 interface Fixture {
+  templates: Template[];
   entities: Entity[];
 }
 
-export type { Entity, Fixture };
+export type { Entity, Fixture, Template };
