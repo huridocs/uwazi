2f98e4a3b41d32e1b3089d5df949d0b889c0949b
Kevin Nderitu <kevin@huridocs.org>
2024-02-23 13:22:35 +0300

    A bit of refactoring

--- app/react/V2/Routes/Settings/Thesauri/helpers.ts
+++ app/react/V2/Routes/Settings/Thesauri/helpers.ts
@@ -1,25 +1,49 @@
-import { ThesaurusValueSchema } from 'shared/types/thesaurusType';
+import { ThesaurusSchema, ThesaurusValueSchema } from 'shared/types/thesaurusType';
 
 const mergeValues = (
-  oldValues: ThesaurusValueSchema[],
-  newValues: ThesaurusValueSchema & { groupId?: string }[]
-) => {
-  let currentValues = [...oldValues];
-  const itemsWithGroups = newValues.filter(item => item.groupId && item.groupId !== '');
-  const itemsWithoutGroups = newValues.filter(item => !item.groupId || item.groupId === '');
-  currentValues = currentValues.map(value => {
+  oldItems: ThesaurusValueSchema[],
+  newItems: ThesaurusValueSchema & { groupId?: string }[]
+): ThesaurusValueSchema[] => {
+  const itemsWithGroups = newItems.filter(item => item.groupId && item.groupId !== '');
+  const itemsWithoutGroups = newItems
+    .filter(item => !item.groupId || item.groupId === '')
+    .map(item => {
+      delete item.groupId;
+      return item;
+    });
+  oldItems = oldItems.map(value => {
     const groupItem = itemsWithGroups.find(item => value.id === item.groupId);
     if (groupItem) {
       delete groupItem.groupId;
-      // @ts-ignore
-      delete groupItem.id;
       value.values?.push(groupItem as ThesaurusValueSchema);
       return value;
     }
     return value;
   });
 
-  return [...currentValues, ...itemsWithoutGroups] as ThesaurusValueSchema[];
+  return [...oldItems, ...itemsWithoutGroups] as ThesaurusValueSchema[];
 };
 
-export { mergeValues };
+const sanitizeThesaurusValues = (
+  thesaurus: ThesaurusSchema,
+  values: ThesaurusValueSchema[]
+): ThesaurusSchema => {
+  const sanitizedThesaurus = { ...thesaurus, values };
+  sanitizedThesaurus.values = values.map(sValue => {
+    delete sValue.id;
+    // @ts-ignore
+    delete sValue.groupId;
+    if (sValue.values) {
+      sValue.values = sValue.values.map(ssValue => {
+        delete ssValue.id;
+        // @ts-ignore
+        delete ssValue.groupId;
+        return ssValue;
+      });
+    }
+    return sValue;
+  }) as ThesaurusValueSchema[];
+  return sanitizedThesaurus;
+};
+
+export { mergeValues, sanitizeThesaurusValues };
