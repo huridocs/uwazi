772e62e3c716a5226950620d7c85eabedd03acfa
Santiago <borrajosantiago@gmail.com>
2024-02-26 18:08:51 -0300

    refactor

--- app/api/migrations/migrations/161-update-translations/index.ts
+++ app/api/migrations/migrations/161-update-translations/index.ts
@@ -1,10 +1,10 @@
 import { Db } from 'mongodb';
 
 const newKeys = [
-  { key: 'Do you want to delete the following items?' },
   { key: 'Reset passwords' },
-  { key: 'Do you want reset the password for the following users?' },
+  { key: 'Do you want to delete the following items?' },
   { key: 'Do you want disable 2FA for the following users?' },
+  { key: 'Do you want reset the password for the following users?' },
 ];
 
 export default {
@@ -17,23 +17,23 @@ export default {
   description: 'Updates some translations in settings',
 
   async up(db: Db) {
+    const documents: any[] = [];
     const settings = await db.collection('settings').findOne();
-    const languages = settings?.languages
+    settings?.languages
       .map((l: any) => l.key)
-      .filter((value: string, index: number, array: any[]) => array.indexOf(value) === index);
+      .filter((value: string, index: number, array: any[]) => array.indexOf(value) === index)
+      .forEach((l: any) =>
+        newKeys.map(k =>
+          documents.push({
+            key: k.key,
+            value: k.key,
+            language: l,
+            context: { id: 'System', type: 'Uwazi UI', label: 'User Interface' },
+          })
+        )
+      );
 
-    const insertMany = languages.map(async (l: any) =>
-      db.collection('translationsV2').insertMany(
-        newKeys.map(k => ({
-          key: k.key,
-          value: k.key,
-          language: l,
-          context: { id: 'System', type: 'Uwazi UI', label: 'User Interface' },
-        }))
-      )
-    );
-
-    await Promise.all(insertMany);
+    await db.collection('translationsV2').insertMany(documents);
 
     process.stdout.write(`${this.name}...\r\n`);
   },
