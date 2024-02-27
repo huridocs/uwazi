96c4c1e4dbc3a774c5ad209cdcf548d0d0c07e49
Santiago <borrajosantiago@gmail.com>
2024-02-27 09:26:29 -0300

    wip migration

---
+++ app/api/migrations/migrations/161-update-translations/index.ts
@@ -0,0 +1,48 @@
+import { Db } from 'mongodb';
+
+const newKeys = [
+  { key: 'Reset passwords' },
+  { key: 'Do you want to delete the following items?' },
+  { key: 'Do you want disable 2FA for the following users?' },
+  { key: 'Do you want reset the password for the following users?' },
+];
+
+const deletedKeys: { key: string }[] = [];
+
+export default {
+  delta: 161,
+
+  reindex: false,
+
+  name: 'update_translations',
+
+  description: 'Updates some translations in settings',
+
+  async up(db: Db) {
+    const settings = await db.collection('settings').findOne();
+    const languages = settings?.languages
+      .map((l: any) => l.key)
+      .filter((value: string, index: number, array: any[]) => array.indexOf(value) === index);
+
+    await db.collection('translationsV2').deleteMany({
+      key: { $in: deletedKeys.concat(newKeys).map(k => k.key) },
+      'context.id': 'System',
+    });
+
+    const insertMany = languages.map(async (l: any) =>
+      db.collection('translationsV2').insertMany(
+        newKeys.map(k => ({
+          key: k.key,
+          value: k.key,
+          language: l,
+          context: { id: 'System', type: 'Uwazi UI', label: 'User Interface' },
+        }))
+      )
+    );
+    await Promise.all(insertMany);
+
+    process.stdout.write(`${this.name}...\r\n`);
+  },
+};
+
+export { newKeys, deletedKeys };
