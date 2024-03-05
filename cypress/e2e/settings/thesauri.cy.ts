24a2931a8d9886e95cc5474ac8eaf96e1c783d2b
Kevin Nderitu <kevin@huridocs.org>
2024-03-05 11:16:55 +0300

    Fixed cypress tests

--- cypress/e2e/settings/thesauri.cy.ts
+++ cypress/e2e/settings/thesauri.cy.ts
@@ -31,6 +31,10 @@ describe('Menu configuration', () => {
     cy.get('input#item-name').type('Second Item');
     cy.getByTestId('menu-form-submit').click();
 
+    cy.get('tbody tr').should('have.length', 2);
+    cy.get('tbody tr:nth-of-type(1) td:nth-of-type(2)').should('have.text', 'First Item');
+    cy.get('tbody tr:nth-of-type(2) td:nth-of-type(2)').should('have.text', 'Second Item');
+
     cy.contains('button', 'Save').click();
     cy.wait('@fetchThesauri');
   });
@@ -46,24 +50,31 @@ describe('Menu configuration', () => {
     cy.get('input#group-name').type('Second group');
     cy.getByTestId('menu-form-submit').click();
 
+    cy.get('tbody tr').should('have.length', 2);
+    cy.get('tbody tr:nth-of-type(1) td:nth-of-type(2)').should('have.text', 'First group');
+    cy.get('tbody tr:nth-of-type(2) td:nth-of-type(2)').should('have.text', 'Second group');
+
     cy.contains('button', 'Save').click();
     cy.wait('@fetchThesauri');
   });
 
-  it('tests Edit', () => {
+  it('should Edit', () => {
     cy.contains('span', 'Thesauri').click();
     cy.wait('@fetchThesauri');
     cy.intercept('GET', '/api/dictionaries').as('fetchThesauriEditItems');
+    cy.intercept('GET', '/api/stats').as('fetchStats');
     cy.contains('button', 'Edit').click();
+    cy.wait('@fetchStats');
     cy.get('#thesauri-name').type(' edited');
     cy.contains('button', 'Edit').eq(0).click();
     cy.get('input#item-name').type(' edited');
     cy.getByTestId('menu-form-submit').click();
+
     cy.contains('button', 'Save').click();
     cy.wait('@fetchThesauriEditItems');
   });
 
-  it('tests edit groups', () => {
+  it('should edit groups', () => {
     cy.contains('span', 'Thesauri').click();
     cy.wait('@fetchThesauri');
     cy.intercept('GET', '/api/dictionaries').as('fetchThesauriEditGroups');
@@ -72,11 +83,14 @@ describe('Menu configuration', () => {
     cy.contains('button', 'Edit').eq(0).click();
     cy.get('input#group-name').type(' edited');
     cy.getByTestId('menu-form-submit').click();
+
+    cy.get('tbody tr:nth-of-type(1) td:nth-of-type(2)').should('have.text', 'First group edited');
+
     cy.contains('button', 'Save').click();
     cy.wait('@fetchThesauriEditGroups');
   });
 
-  it('tests delete', () => {
+  it('should delete', () => {
     cy.contains('span', 'Thesauri').click();
     cy.wait('@fetchThesauri');
     cy.intercept('GET', '/api/dictionaries').as('fetchThesauriDelete');
@@ -84,5 +98,22 @@ describe('Menu configuration', () => {
     cy.contains('button', 'Delete').click();
     cy.contains('button', 'Accept').click();
     cy.wait('@fetchThesauriDelete');
+
+    cy.get('tbody tr').should('have.length', 1);
+  });
+
+  it('should not delete when in use', () => {
+    cy.intercept('GET', 'api/templates').as('fetchtemplates');
+    cy.contains('span', 'Templates').click();
+    cy.contains('Edit').click();
+    cy.get('aside .list-group-item:nth-of-type(3) button').click();
+    cy.get('.metadataTemplate li:nth-of-type(3) .property-edit').click();
+    cy.get('select').eq(1).select('New Thesauri with groups edited');
+
+    cy.contains('Save').click();
+    cy.wait('@fetchtemplates');
+    cy.contains('span', 'Thesauri').click();
+
+    cy.get('tbody tr:nth-of-type(1) input').should('be.disabled');
   });
 });
