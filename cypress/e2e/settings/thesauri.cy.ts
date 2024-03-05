4228b4e741724fd324d167c0068ac2a97fa054b6
Kevin Nderitu <kevin@huridocs.org>
2024-03-05 14:15:55 +0300

    Removed wait

--- cypress/e2e/settings/thesauri.cy.ts
+++ cypress/e2e/settings/thesauri.cy.ts
@@ -67,7 +67,6 @@ describe('Menu configuration', () => {
     cy.wait('@fetchStats');
     cy.get('#thesauri-name').type(' edited');
     cy.contains('button', 'Edit').eq(0).click();
-    cy.wait(50);
     cy.get('input#item-name').type(' edited');
     cy.getByTestId('menu-form-submit').click();
 
