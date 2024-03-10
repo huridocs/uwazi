b71dd0af997d376625c539a338ba2dd7b3522146
A happy cat <acasadotorres@gmail.com>
2024-03-10 14:05:26 +0100

    Removed unnecesary files and fixed type

--- app/react/Map/index.js
+++ app/react/Map/index.js
@@ -1,10 +1,10 @@
 import loadable from '@loadable/component';
 import React from 'react';
 import { Map } from 'app/Map/MapContainer';
-import * as helper from './helper';
+import * as helper from './helper.js';
 
 const LMap = loadable(async () => {
-  const { LMap: LMapComponent } = await import(/* webpackChunkName: "LazyLoadMap" */ './LMap.tsx');
+  const { LMap: LMapComponent } = await import(/* webpackChunkName: "LazyLoadMap" */ './LMap.js');
   return props => <LMapComponent {...props} />;
 });
 
