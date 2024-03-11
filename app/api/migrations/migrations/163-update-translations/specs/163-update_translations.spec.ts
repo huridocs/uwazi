1591a9353a2d9e321b68a666dca7b6ff705abba0
Santiago <borrajosantiago@gmail.com>
2024-03-11 11:18:40 -0300

    fix migration spec

--- app/api/migrations/migrations/163-update-translations/specs/163-update_translations.spec.ts
+++ app/api/migrations/migrations/163-update-translations/specs/163-update_translations.spec.ts
@@ -23,7 +23,7 @@ describe('migration update translations of settings', () => {
   });
 
   it('should have a delta number', () => {
-    expect(migration.delta).toBe(162);
+    expect(migration.delta).toBe(163);
   });
 
   it('should delete old translations', async () => {
