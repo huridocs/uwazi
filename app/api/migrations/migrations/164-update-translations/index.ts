250d62674f10588b90b958dfcfcec959841de3d8
Santiago <borrajosantiago@gmail.com>
2024-03-12 12:51:58 -0300

    upped migration number

--- app/api/migrations/migrations/164-update-translations/index.ts
+++ app/api/migrations/migrations/164-update-translations/index.ts
@@ -10,7 +10,7 @@ const newKeys = [
 const deletedKeys: { key: string }[] = [{ key: 'You are about to delete a page' }];
 
 export default {
-  delta: 163,
+  delta: 164,
 
   reindex: false,
 
--- app/api/migrations/migrations/164-update-translations/specs/164-update_translations.spec.ts
+++ app/api/migrations/migrations/164-update-translations/specs/164-update_translations.spec.ts
@@ -23,7 +23,7 @@ describe('migration update translations of settings', () => {
   });
 
   it('should have a delta number', () => {
-    expect(migration.delta).toBe(163);
+    expect(migration.delta).toBe(164);
   });
 
   it('should delete old translations', async () => {
