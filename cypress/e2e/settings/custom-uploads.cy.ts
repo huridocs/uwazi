eb4c7346402255d9da49e7f6d82876c73a8696fe
Santiago <borrajosantiago@gmail.com>
2024-03-12 18:00:56 -0300

    more e2e

--- cypress/e2e/settings/custom-uploads.cy.ts
+++ cypress/e2e/settings/custom-uploads.cy.ts
@@ -42,19 +42,43 @@ describe('customization', () => {
     cy.contains('button', 'Dismiss').click();
   });
 
-  describe('uploading progress and blocking', () => {
-    it('should show progress and number of files left', () => {
-      cy.contains('Import asset').click();
-      cy.contains('Browse files to upload');
-      cy.get('input[type=file]').selectFile(
-        ['./cypress/test_files/valid.pdf', './cypress/test_files/short-video-thumbnail.jpg'],
-        {
-          force: true,
-        }
-      );
-      cy.contains('button', 'Add').click();
-      cy.contains(/Uploading\.\.\. valid.pdf \d+% \d remaining files/gm);
-      cy.contains('button', 'Dismiss').click();
+  it('should show progress and number of files left', () => {
+    cy.contains('Import asset').click();
+    cy.contains('Browse files to upload');
+    cy.get('input[type=file]').selectFile(
+      ['./cypress/test_files/valid.pdf', './cypress/test_files/short-video-thumbnail.jpg'],
+      {
+        force: true,
+      }
+    );
+    cy.contains('button', 'Add').click();
+    cy.contains(/Uploading\.\.\. valid.pdf \d+% \d remaining files/gm);
+    cy.contains('button', 'Dismiss').click();
+  });
+
+  it('should delete a file', () => {
+    cy.contains('td', 'short-video.mp4').parent().contains('button', 'Delete').click();
+    cy.contains('li', 'short-video.mp4');
+    cy.contains('button', 'Accept').click();
+    cy.contains('button', 'Dismiss').click();
+    cy.contains('short-video.mp4').should('not.exist');
+  });
+
+  it('should delete multiple files', () => {
+    cy.contains('span', 'Select all')
+      .parent()
+      .within(() => {
+        cy.get('input').click();
+      });
+    cy.get('[data-testid="settings-content-footer"]').within(() => {
+      cy.contains('button', 'Delete').click();
     });
+    cy.contains('li', 'batman.jpg');
+    cy.contains('li', 'sample.pdf');
+    cy.contains('li', 'valid.pdf');
+    cy.contains('li', 'short-video-thumbnail.jpg');
+    cy.contains('button', 'Accept').click();
+    cy.contains('button', 'Dismiss').click();
+    cy.get('tbody').children().should('have.length', 0);
   });
 });
