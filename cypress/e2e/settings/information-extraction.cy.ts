ec5fd3e42755eb17ce75a22825c123cb4f1276a1
Kevin <kevin@huridocs.org>
2024-03-22 17:58:46 +0300

    Fixed cypress

--- cypress/e2e/settings/information-extraction.cy.ts
+++ cypress/e2e/settings/information-extraction.cy.ts
@@ -280,7 +280,7 @@ describe('Information Extraction', () => {
     });
 
     it('should clear the existing selection', () => {
-      cy.contains('button', 'Clear PDF selection').click();
+      cy.contains('button', 'Clear').click();
       cy.get('div.highlight-rectangle').should('have.length', 0);
     });
 
