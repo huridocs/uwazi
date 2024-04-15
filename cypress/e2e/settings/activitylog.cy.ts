2fdd21459cb92846763bc0c9b0b66c0a68c882b4
mfacar <mercy@huridocs.org>
2024-04-15 13:55:38 -0500

    updates field names

--- cypress/e2e/settings/activitylog.cy.ts
+++ cypress/e2e/settings/activitylog.cy.ts
@@ -30,7 +30,7 @@ describe('Activity log', () => {
   };
 
   it('should list the last activity log entries', () => {
-    cy.get('tr').should('have.length.at.least', 20);
+    cy.get('tr').should('have.length.at.least', 10);
     checkCells(1, 1, 'span');
     checkCells(1, 2, 'span');
     checkCells(1, 3, ' div > div:nth-child(1)');
@@ -42,7 +42,7 @@ describe('Activity log', () => {
   it('should filter by user', () => {
     cy.clearAndType('input[name=username]', 'editor');
     cy.contains('Updated user');
-    cy.get('tr').should('have.length.at.most', 15);
+    cy.get('tr').should('have.length.at.most', 5);
   });
 
   it('should show a tooltip with the detail of an activity entry', () => {
@@ -61,7 +61,7 @@ describe('Activity log', () => {
   });
 
   it('should update the list when filters are cleaned', () => {
-    cy.get('tr').should('have.length.at.least', 20);
+    cy.get('tr').should('have.length.at.least', 10);
   });
 
   it('should open the detail of an entry', () => {
@@ -74,14 +74,14 @@ describe('Activity log', () => {
   it('should filter by method', () => {
     cy.clearAndType('input[name=search]', 'DELETE');
     cy.contains('editor');
-    cy.get('tr').should('have.length.at.most', 5);
+    cy.get('tr').should('have.length.at.most', 3);
   });
 
   it('should filter by dates', () => {
     cy.get('input[name=search]').clear();
-    cy.get('input[name=start]').type('2023-06-22', { delay: 0 });
-    cy.get('input[name=end]').clear();
-    cy.get('input[name=end]').type('2023-06-23', { delay: 0 });
+    cy.get('input[name=from]').type('2023-06-22', { delay: 0 });
+    cy.get('input[name=to]').clear();
+    cy.get('input[name=to]').type('2023-06-23', { delay: 0 });
     cy.contains('admin');
     cy.get('tr').should('have.length.at.most', 2);
   });
