b133eacb395e4f28226efb140d24b3ae1a587b8e
Santiago <borrajosantiago@gmail.com>
2024-04-15 16:24:37 -0300

    restore dnd old package

--- app/react/Settings/index.js
+++ app/react/Settings/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-named-as-default */
 import EntityTypesList from './components/EntityTypesList';
-import FiltersForm from './components/FiltersForm';
 import { Settings } from './Settings';
 import SettingsAPI from './SettingsAPI';
 import { PreserveSettings } from './components/PreserveSettings';
 
-export { Settings, SettingsAPI, PreserveSettings, EntityTypesList, FiltersForm };
+export { Settings, SettingsAPI, PreserveSettings, EntityTypesList };
