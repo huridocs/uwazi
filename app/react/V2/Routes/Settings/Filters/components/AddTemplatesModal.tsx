49d3419f3a8edf645824668c387dd4376b37c0ee
Santiago <71732018+Zasa-san@users.noreply.github.com>
2024-04-22 12:21:56 -0300

    6585 - V2 settings filters (#6673)
    * basic layout
    
    * remove old dnd and filters form route
    
    * remove from routes
    
    * restore dnd old package
    
    * update english csv
    
    * added table
    
    * add new template modal
    
    * save, cancel, remove edit from single items
    
    * logic to add new filters
    
    * refactor + confirm navigation + delete rows
    
    * delete subitems
    
    * save filters and update atom
    
    * clear blocker state after save
    
    * sidepanel with wip validation
    
    * correct validation
    
    * translations
    
    * rehuse translations
    
    * refactor
    
    * e2e tests
    
    * fixed label

---
+++ app/react/V2/Routes/Settings/Filters/components/AddTemplatesModal.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Translate } from 'app/I18N';
+import { Button, Modal } from 'app/V2/Components/UI';
+import { MultiselectList } from 'app/V2/Components/Forms';
+import { ClientTemplateSchema } from 'app/istore';
+
+type AddTemplateModalProps = {
+  onCancel: React.Dispatch<React.SetStateAction<boolean>>;
+  onAdd: (templateIds: string[]) => void;
+  templates?: ClientTemplateSchema[];
+};
+
+const AddTemplatesModal = ({ onCancel, onAdd, templates }: AddTemplateModalProps) => {
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const items = templates?.map(template => ({
+    label: template.name,
+    value: template._id,
+    searchLabel: template.name,
+  }));
+
+  return (
+    <Modal size="lg">
+      <Modal.Header>
+        <h1 className="text-xl font-medium text-gray-900">
+          <Translate>Add entity type</Translate>
+        </h1>
+        <Modal.CloseButton onClick={() => onCancel(false)} />
+      </Modal.Header>
+      <Modal.Body className="pt-4">
+        <MultiselectList
+          className="pt-4 max-h-96"
+          items={items || []}
+          onChange={s => setSelected(s)}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <div className="flex flex-col w-full">
+          <div className="flex gap-2">
+            <Button styling="light" onClick={() => onCancel(false)} className="grow">
+              <Translate>Cancel</Translate>
+            </Button>
+            <Button
+              onClick={async () => {
+                onCancel(false);
+                onAdd(selected);
+              }}
+              className="grow"
+              disabled={!selected.length}
+            >
+              <Translate>Add</Translate> {selected.length ? `(${selected.length})` : ''}
+            </Button>
+          </div>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export { AddTemplatesModal };
---
+++ app/react/V2/Routes/Settings/Filters/components/helpers.ts
@@ -0,0 +1,70 @@
+import { ClientSettingsFilterSchema } from 'app/apiResponseTypes';
+import { ClientTemplateSchema } from 'app/istore';
+
+type LoaderData = {
+  filters: ClientSettingsFilterSchema[] | undefined;
+  templates: ClientTemplateSchema[];
+};
+
+const filterAvailableTemplates = (
+  templates: ClientTemplateSchema[],
+  filters?: ClientSettingsFilterSchema[]
+) => {
+  const usedTemplatesIds: string[] = [];
+
+  filters?.forEach(filter => {
+    if (filter.items) {
+      filter.items.forEach(item => {
+        usedTemplatesIds.push(item.id!);
+      });
+    }
+    if (filter.id) {
+      usedTemplatesIds.push(filter.id);
+    }
+  });
+
+  return templates.filter(template => !usedTemplatesIds.includes(template._id));
+};
+
+const updateFilters = (selectedTemplatesIds: string[], templates?: ClientTemplateSchema[]) => {
+  const newFilters = selectedTemplatesIds.map(templateId => {
+    const template = templates?.find(templ => templ._id === templateId);
+    return { id: templateId, name: template?.name };
+  });
+  return newFilters;
+};
+
+const deleteFilters = (
+  originalFilters?: ClientSettingsFilterSchema[],
+  filtersToRemove?: (string | undefined)[]
+) => {
+  if (!filtersToRemove) {
+    return originalFilters;
+  }
+
+  return originalFilters
+    ?.map(filter => {
+      if (filtersToRemove.includes(filter.id!)) {
+        return {};
+      }
+
+      if (filter.items) {
+        const nestedFilters = filter.items.filter(item => !filtersToRemove.includes(item.id!));
+        return { ...filter, items: nestedFilters };
+      }
+
+      return { ...filter };
+    })
+    .filter(filter => {
+      if (!filter.id) {
+        return false;
+      }
+      if (filter.items && filter.items.length === 0) {
+        return false;
+      }
+      return true;
+    });
+};
+
+export type { LoaderData };
+export { filterAvailableTemplates, updateFilters, deleteFilters };
