438749e6003069eaeb400650845c364d00994af6
Santiago <borrajosantiago@gmail.com>
2024-04-24 16:33:28 -0300

    wip account for all cases

--- app/react/V2/Components/Analitycs/Matomo.tsx
+++ app/react/V2/Components/Analitycs/Matomo.tsx
@@ -3,30 +3,73 @@ import { useAtomValue } from 'jotai';
 import { settingsAtom } from 'V2/atoms';
 
 const Matomo = () => {
-  const { matomoConfig } = useAtomValue(settingsAtom);
-  const { id, url } = JSON.parse(matomoConfig || '');
+  // WIP: currently users Matomo is stored as a string like:
+  // '{"url":"https://matomo.huridata.org","id":"123"}'.
+  // The global one could be just a json with url and id
+
+  const { matomoConfig, globalMatomo } = useAtomValue(settingsAtom);
+  const { id, url } = JSON.parse(matomoConfig || '{}') as { id?: string; url?: string };
+  const { id: secondaryWebsiteId, url: secondaryTracker } = globalMatomo || {
+    id: '1',
+    secondaryUrl: 'wip.huridata.org',
+  };
 
   useEffect(() => {
-    if (id && url) {
-      const matomoUrl = url.replace(/\/?$/, '/');
+    const script = document.createElement('script');
+    const hasUserMatomo = Boolean(id && url);
+    const hasGlobalMatomo = Boolean(secondaryWebsiteId && secondaryTracker);
+    const matomoUrl = url?.replace(/\/?$/, '/');
 
-      const script = document.createElement('script');
-
-      script.innerHTML = `
+    switch (true) {
+      case hasGlobalMatomo && !hasUserMatomo:
+        script.innerHTML = `
         var _paq = _paq || [];
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
         (function() {
-          var u="${matomoUrl}";
-          _paq.push(['setTrackerUrl', u+'piwik.php']);
-          _paq.push(['setSiteId', '${id}']);
+          _paq.push(['addTracker', ${secondaryTracker}, ${secondaryWebsiteId}]);
           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
           g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
         })();`;
+        break;
 
+      case !hasGlobalMatomo && hasUserMatomo:
+        script.innerHTML = `
+          var _paq = _paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="${matomoUrl}";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', '${id}']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+          })();`;
+        break;
+
+      case hasGlobalMatomo && hasUserMatomo:
+        script.innerHTML = `
+            var _paq = _paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="${matomoUrl}";
+              _paq.push(['setTrackerUrl', u+'piwik.php']);
+              _paq.push(['setSiteId', '${id}']);
+              _paq.push(['addTracker', ${secondaryTracker}, ${secondaryWebsiteId}]);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+            })();`;
+        break;
+
+      default:
+        break;
+    }
+
+    if (hasUserMatomo || hasGlobalMatomo) {
       document.body.appendChild(script);
     }
-  }, [id, url]);
+  }, [id, secondaryTracker, secondaryWebsiteId, url]);
 
   return undefined;
 };
