f8173b8867fd085a44a54c27bfcb04936032c3f9
Alberto Casado Torres <konzz@users.noreply.github.com>
2024-05-07 16:00:40 +0200

    ix multiselect support (#6727)
    * Multiselect WIP
    
    * zero length values is a valid suggestions
    
    * Select and multiselect props
    
    * rendering multiple options
    
    * accepting suggestions
    
    * Types refactor
    
    * fixed types
    
    * backend fixes
    
    * fixed property undefined on first render
    
    * fixed issues with children
    
    * fixed specs
    
    * updated spec
    
    * removed unused vars
    
    * deleted unused vars
    
    * fixed issue with title property
    
    * fixed issue when bulk accepting parent suggestions
    
    * fixed some e2e
    
    ---------
    
    Co-authored-by: Laszlo Kecskes <laszlokecskes@protonmail.com>

--- app/react/V2/Routes/Settings/IX/components/helpers.ts
+++ app/react/V2/Routes/Settings/IX/components/helpers.ts
@@ -1,16 +1,91 @@
+/* eslint-disable max-statements */
 import { ClientEntitySchema } from 'app/istore';
-import { EntitySuggestionType } from 'shared/types/suggestionType';
+import { PropertySchema } from 'shared/types/commonTypes';
+import {
+  SuggestionValue,
+  TableSuggestion,
+  SingleValueSuggestion,
+  MultiValueSuggestion,
+} from '../types';
+
+const generateChildrenRows = (_suggestion: MultiValueSuggestion) => {
+  const suggestion: MultiValueSuggestion = { ..._suggestion, isChild: false };
+
+  const currentValues = [
+    ...(Array.isArray(suggestion.currentValue) ? suggestion.currentValue : []),
+  ];
+
+  const suggestedValues = [
+    ...(Array.isArray(suggestion.suggestedValue) ? suggestion.suggestedValue : []),
+  ];
+
+  suggestion.children = [];
+
+  const { children, ...suggestionWithoutChildren } = suggestion;
+  suggestedValues.forEach(suggestedValue => {
+    const valuePresent = currentValues.find(v => v === suggestedValue);
+    if (valuePresent) {
+      currentValues.splice(currentValues.indexOf(valuePresent), 1);
+    }
+
+    suggestion.children?.push({
+      ...suggestionWithoutChildren,
+      suggestedValue,
+      currentValue: valuePresent || '',
+      propertyName: suggestion.propertyName,
+      disableRowSelection: true,
+      isChild: true,
+      entityTitle: '',
+    });
+  });
+
+  currentValues.forEach(currentValue => {
+    suggestion.children?.push({
+      ...suggestionWithoutChildren,
+      suggestedValue: '',
+      currentValue,
+      disableRowSelection: true,
+      isChild: true,
+      entityTitle: '',
+    });
+  });
+
+  return suggestion;
+};
+
+const _replaceSuggestion = (suggestion: TableSuggestion, currentSuggestions: TableSuggestion[]) =>
+  currentSuggestions.map(currentSuggestion => {
+    if (currentSuggestion._id === suggestion._id) {
+      return suggestion;
+    }
+    return currentSuggestion;
+  });
+
+const _updateTitleInSuggestion = (
+  currentSuggestions: TableSuggestion[],
+  _suggestionToUpdate: TableSuggestion,
+  updatedEntity: ClientEntitySchema
+) => {
+  const suggestionToUpdate = { ..._suggestionToUpdate };
+  const newTitle = updatedEntity.title;
+  suggestionToUpdate.currentValue = newTitle;
+  suggestionToUpdate.entityTitle = newTitle!;
+  suggestionToUpdate.state.match = suggestionToUpdate.suggestedValue === newTitle;
+
+  return _replaceSuggestion(suggestionToUpdate, currentSuggestions);
+};
 
 // eslint-disable-next-line max-statements
 const updateSuggestionsByEntity = (
-  currentSuggestions: EntitySuggestionType[],
-  updatedEntity?: ClientEntitySchema
-): EntitySuggestionType[] => {
+  currentSuggestions: TableSuggestion[],
+  updatedEntity?: ClientEntitySchema,
+  property?: PropertySchema
+): TableSuggestion[] => {
   if (!updatedEntity) {
     return currentSuggestions;
   }
 
-  const suggestionToUpdate = currentSuggestions.find(
+  let suggestionToUpdate = currentSuggestions.find(
     currentSuggestion => currentSuggestion.entityId === updatedEntity._id
   );
 
@@ -21,61 +96,97 @@ const updateSuggestionsByEntity = (
   }
 
   if (propertyToUpdate === 'title' && updatedEntity.title) {
-    const newTitle = updatedEntity.title;
-    suggestionToUpdate.currentValue = newTitle;
-    suggestionToUpdate.entityTitle = newTitle;
-    suggestionToUpdate.state.match = suggestionToUpdate.suggestedValue === newTitle;
+    return _updateTitleInSuggestion(currentSuggestions, suggestionToUpdate, updatedEntity);
   }
 
-  if (
-    propertyToUpdate !== 'title' &&
-    updatedEntity.metadata &&
-    updatedEntity.metadata[propertyToUpdate]?.length
-  ) {
-    const newValue = updatedEntity.metadata[propertyToUpdate]![0].value;
+  if (!updatedEntity.metadata) {
+    return currentSuggestions;
+  }
+
+  if (updatedEntity.metadata[propertyToUpdate]?.length) {
+    const newValue = (
+      property?.type === 'multiselect'
+        ? updatedEntity.metadata[propertyToUpdate]?.map(v => v.value)
+        : updatedEntity.metadata[propertyToUpdate]![0].value
+    ) as SuggestionValue;
+
     suggestionToUpdate.currentValue = newValue;
     suggestionToUpdate.state.match = suggestionToUpdate.suggestedValue === newValue;
   }
 
-  if (
-    propertyToUpdate !== 'title' &&
-    (!updatedEntity.metadata || !updatedEntity.metadata[propertyToUpdate]?.length)
-  ) {
+  if (!updatedEntity.metadata[propertyToUpdate]?.length) {
     suggestionToUpdate.currentValue = '';
     suggestionToUpdate.state.match = suggestionToUpdate.suggestedValue === '';
   }
 
-  const suggestions = currentSuggestions.map(currentSuggestion => {
-    if (currentSuggestion._id === suggestionToUpdate._id) {
-      return suggestionToUpdate;
-    }
-    return currentSuggestion;
-  });
+  if (property?.type === 'multiselect') {
+    suggestionToUpdate = generateChildrenRows(suggestionToUpdate as MultiValueSuggestion);
+  }
 
-  return suggestions;
+  return _replaceSuggestion(suggestionToUpdate, currentSuggestions);
+};
+
+const updateMultiValueSuggestions = (
+  _parentSuggestion: MultiValueSuggestion,
+  acceptedSuggestion: SingleValueSuggestion
+): MultiValueSuggestion => {
+  let parentSuggestion = { ..._parentSuggestion };
+
+  const shouldAddValue = acceptedSuggestion.suggestedValue !== '';
+  const value = shouldAddValue
+    ? acceptedSuggestion.suggestedValue || ''
+    : acceptedSuggestion.currentValue || '';
+
+  parentSuggestion.currentValue = (_parentSuggestion.currentValue as SuggestionValue[]) || [];
+
+  parentSuggestion.currentValue = shouldAddValue
+    ? parentSuggestion.currentValue.concat(value)
+    : parentSuggestion.currentValue.filter(v => v !== value);
+
+  parentSuggestion.state.match = parentSuggestion.currentValue.every(v =>
+    parentSuggestion.suggestedValue.includes(v)
+  );
+
+  parentSuggestion = generateChildrenRows(parentSuggestion);
+  return parentSuggestion;
 };
 
 const updateSuggestions = (
-  currentSuggestions: EntitySuggestionType[],
-  suggestionsToAccept: EntitySuggestionType[] | []
-): EntitySuggestionType[] => {
+  currentSuggestions: TableSuggestion[],
+  suggestionsToAccept: TableSuggestion[]
+): TableSuggestion[] => {
   if (!suggestionsToAccept.length) {
     return currentSuggestions;
   }
 
-  const acceptedSuggestions = suggestionsToAccept.map(suggestionToAccept => {
-    const updated = { ...suggestionToAccept };
-    updated.state.match = true;
-    updated.currentValue = suggestionToAccept.suggestedValue;
+  const acceptedSuggestions = suggestionsToAccept.map(acceptedSuggestion => {
+    let suggestion = (
+      acceptedSuggestion.isChild
+        ? { ...currentSuggestions.find(s => s._id === acceptedSuggestion._id) }
+        : { ...acceptedSuggestion }
+    ) as TableSuggestion;
 
-    if (
-      suggestionToAccept.propertyName === 'title' &&
-      typeof suggestionToAccept.suggestedValue === 'string'
-    ) {
-      updated.entityTitle = suggestionToAccept.suggestedValue;
+    if (acceptedSuggestion.isChild) {
+      suggestion = updateMultiValueSuggestions(
+        suggestion as MultiValueSuggestion,
+        acceptedSuggestion as SingleValueSuggestion
+      );
+    } else {
+      suggestion.state.match = true;
+      suggestion.currentValue = acceptedSuggestion.suggestedValue;
     }
 
-    return updated;
+    if ('children' in suggestion && suggestion.children?.length) {
+      suggestion = generateChildrenRows(suggestion as MultiValueSuggestion);
+    }
+
+    if (
+      acceptedSuggestion.propertyName === 'title' &&
+      typeof acceptedSuggestion.suggestedValue === 'string'
+    ) {
+      suggestion.entityTitle = acceptedSuggestion.suggestedValue;
+    }
+    return suggestion;
   });
 
   const merged = [
@@ -92,4 +203,4 @@ const updateSuggestions = (
   return merged;
 };
 
-export { updateSuggestions, updateSuggestionsByEntity };
+export { updateSuggestions, updateSuggestionsByEntity, generateChildrenRows };
