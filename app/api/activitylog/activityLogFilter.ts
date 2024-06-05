a2036e3dc06c82a6f7c3261b96d6a7d9dc307770
mfacar <mercy@huridocs.org>
2024-06-05 18:07:17 -0500

    fixes to review & increase coverage

--- app/api/activitylog/activityLogFilter.ts
+++ app/api/activitylog/activityLogFilter.ts
@@ -1,56 +1,39 @@
 import { isEmpty } from 'lodash';
 import moment from 'moment';
+import { FilterQuery } from 'mongoose';
 import { ActivityLogGetRequest } from 'shared/types/activityLogApiTypes';
+import { ActivityLogEntryType } from 'shared/types/activityLogEntryType';
+import { escapeEspecialChars } from 'shared/data_utils/stringUtils';
 import { ParsedActions } from './activitylogParser';
 import { EntryValue } from './activityLogBuilder';
 
-type ActivityLogQuery = ActivityLogGetRequest['query'];
-
-const prepareToFromRanges = (sanitizedTime: any) => {
-  const fromDate = sanitizedTime.from && moment.unix(parseInt(sanitizedTime.from, 10));
-  const toDate = sanitizedTime.to && moment.unix(parseInt(sanitizedTime.to, 10)).add(1, 'days');
+type ActivityLogQuery = Required<ActivityLogGetRequest>['query'];
+type ActivityLogQueryTime = Required<ActivityLogQuery>['time'];
+const prepareToFromRanges = (sanitizedTime: ActivityLogQueryTime) => {
+  const fromDate = sanitizedTime.from && moment.unix(sanitizedTime.from);
+  const toDate = sanitizedTime.to && moment.unix(sanitizedTime.to).add(1, 'days');
   return {
-    ...(fromDate && { $gte: fromDate }),
-    ...(toDate && fromDate !== toDate && { $lte: toDate }),
-    ...(toDate && fromDate === toDate && { $lt: toDate }),
+    ...(fromDate && { $gte: fromDate.valueOf() }),
+    ...(toDate && { $lt: toDate.valueOf() }),
   };
 };
-
 const parsedActionsEntries = Object.entries(ParsedActions);
-
 const queryURL = (matchedEntries: [string, EntryValue][]) =>
   matchedEntries.map(([key]) => {
     const entries = key.split(/\/(.*)/s);
-    const condition: { $and: {}[] } = {
+    const condition = {
       $and: [
-        {
-          url: {
-            $regex: `^\\/${entries[1].replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&')}$`,
-          },
-        },
-        {
-          method: entries[0],
-        },
+        { url: { $regex: `^\\/${escapeEspecialChars(entries[1])}$` } },
+        { method: entries[0] },
       ],
     };
     return condition;
   });
-
 const andCondition = (method: string, regex: string, property: string = 'body') => ({
-  $and: [
-    {
-      method,
-    },
-    {
-      [property]: {
-        $regex: regex,
-      },
-    },
-  ],
+  $and: [{ method }, { [property]: { $regex: regex } }],
 });
-
 const bodyCondition = (methods: string[]) => {
-  const orContent: {}[] = [];
+  const orContent: FilterQuery<ActivityLogEntryType>[] = [];
   methods.forEach(method => {
     switch (method) {
       case 'CREATE':
@@ -64,20 +47,16 @@ const bodyCondition = (methods: string[]) => {
         orContent.push(andCondition('DELETE', '^({"_id").*'));
         break;
       default:
-        orContent.push({
-          method,
-        });
+        orContent.push({ method });
         break;
     }
   });
   return orContent;
 };
-const sanitizeTime = (time: { [k: string]: unknown }) => (memo: {}, k: string) =>
+const sanitizeTime = (time: ActivityLogQueryTime) => (memo: {}, k: string) =>
   time[k] !== null ? Object.assign(memo, { [k]: time[k] }) : memo;
-
 const equivalentHttpMethod = (method: string): string =>
   ['CREATE', 'UPDATE'].includes(method.toUpperCase()) ? 'POST' : method.toUpperCase();
-
 const matchWithParsedEntry = (
   key: string,
   queryMethods: string[],
@@ -87,11 +66,22 @@ const matchWithParsedEntry = (
   key.toUpperCase().match(`(${queryMethods.join('|')}).*`) &&
   ((value.method || '').toUpperCase().match(`(${methods.join('|')}).*`) ||
     value.desc.toUpperCase().match(`(${methods.join('|')}).*`));
-
+const reduceUniqueCondition: (
+  condition: FilterQuery<ActivityLogEntryType>
+) => FilterQuery<ActivityLogEntryType> = (condition: FilterQuery<ActivityLogEntryType>) => {
+  const keys = Object.keys(condition);
+  return keys.reduce((memo, key) => {
+    if (['$and', '$or'].includes(key) && condition[key]?.length === 1) {
+      const reducedCondition = reduceUniqueCondition(condition[key][0]);
+      return { ...memo, ...reducedCondition };
+    }
+    return { ...memo, [key]: condition[key] };
+  }, {});
+};
 class ActivityLogFilter {
-  andQuery: {}[] = [];
+  andQuery: FilterQuery<ActivityLogEntryType>[] = [];
 
-  searchQuery: {}[] = [];
+  searchQuery: FilterQuery<ActivityLogEntryType>[] = [];
 
   query: ActivityLogQuery;
 
@@ -102,31 +92,22 @@ class ActivityLogFilter {
 
   timeQuery() {
     const { time = {}, before = -1 } = this.query || {};
-
-    const sanitizedTime = Object.keys(time).reduce(sanitizeTime(time), {});
-
+    const sanitizedTime: ActivityLogQueryTime = Object.keys(time).reduce(sanitizeTime(time), {});
     if (before === -1 && isEmpty(sanitizedTime)) {
       return;
     }
-
     const timeFilter = {
       ...(!isEmpty(sanitizedTime) ? { time: prepareToFromRanges(sanitizedTime) } : {}),
       ...(before !== -1 ? { time: { $lt: before } } : {}),
     };
-
     this.andQuery.push(timeFilter);
   }
 
   setRequestFilter(property: 'url' | 'query' | 'body' | 'params', exact = false) {
     const filterValue = (this.query || {})[property];
     if (filterValue !== undefined) {
-      const exp = filterValue.replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&');
-
-      this.andQuery.push({
-        [property]: {
-          $regex: exact ? `^${exp}$` : exp,
-        },
-      });
+      const exp = escapeEspecialChars(filterValue);
+      this.andQuery.push({ [property]: { $regex: exact ? `^${exp}$` : exp } });
     }
   }
 
@@ -140,21 +121,15 @@ class ActivityLogFilter {
   searchFilter() {
     const { search } = this.query || {};
     if (search !== undefined) {
-      const regex = {
-        $regex: `.*${search.replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&')}.*`,
-        $options: 'si',
-      };
+      const regex = { $regex: `.*${escapeEspecialChars(search)}.*`, $options: 'si' };
       this.searchQuery.push({
         $or: [
           {
             method: {
-              $regex: `${search
-                .toUpperCase()
-                .replace('CREATE', 'POST')
-                .replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&')}`,
+              $regex: `${escapeEspecialChars(search.toUpperCase().replace('CREATE', 'POST'))}`,
             },
           },
-          { url: { $regex: `^${search.replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&')}$` } },
+          { url: { $regex: `^${escapeEspecialChars(search)}$` } },
           { query: regex },
           { body: regex },
           { params: regex },
@@ -190,7 +165,6 @@ class ActivityLogFilter {
       value.desc.toLowerCase().includes(search.toLocaleLowerCase() || '')
     );
     const orUrlItems = queryURL(matchedURLs);
-
     if (matchedURLs.length > 0) {
       this.searchQuery.push({ $or: orUrlItems });
     } else {
@@ -204,10 +178,7 @@ class ActivityLogFilter {
   findFilter() {
     const { find } = this.query || {};
     if (find) {
-      const regex = {
-        $regex: `.*${find.replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&')}.*`,
-        $options: 'si',
-      };
+      const regex = { $regex: `.*${escapeEspecialChars(find)}.*`, $options: 'si' };
       this.andQuery.push({
         $or: [
           { method: regex },
@@ -223,13 +194,8 @@ class ActivityLogFilter {
   userFilter() {
     const { username } = this.query || {};
     if (username) {
-      const orUser: {}[] = [
-        {
-          username: {
-            $regex: `.*${username.replace(/[.*\/+?^${}()|[\]\\]/g, '\\$&')}.*`,
-            $options: 'si',
-          },
-        },
+      const orUser: FilterQuery<ActivityLogEntryType>[] = [
+        { username: { $regex: `.*${escapeEspecialChars(username)}.*`, $options: 'si' } },
       ];
       if (username === 'anonymous') {
         orUser.push({ username: null });
@@ -245,8 +211,9 @@ class ActivityLogFilter {
     this.findFilter();
     this.userFilter();
     this.timeQuery();
-    return !isEmpty(this.andQuery) ? { $and: this.andQuery } : {};
+    const rootQuery = !isEmpty(this.andQuery) ? reduceUniqueCondition({ $and: this.andQuery }) : {};
+    return rootQuery;
   }
 }
-
-export { ActivityLogFilter };
+export type { ActivityLogQueryTime };
+export { ActivityLogFilter, prepareToFromRanges, bodyCondition };
