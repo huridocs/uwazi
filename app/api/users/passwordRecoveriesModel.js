a72296cc69cf78f5d1a27037e80faae2c3199ad6
Federico Nocetti <fnocetti@users.noreply.github.com>
2024-06-06 06:09:16 -0300

    HOTFIX Adjust password recovery ttl. Validate empty login credentials. (#6861)
    * Prevent login with empty credentials
    
    * Fix password recovery ttl
    
    * upgrade version
    
    ---------
    
    Co-authored-by: Laszlo Kecskes <laszlokecskes@protonmail.com>

--- app/api/users/passwordRecoveriesModel.js
+++ app/api/users/passwordRecoveriesModel.js
@@ -7,7 +7,7 @@ const ONE_DAY = 60 * 60 * 24;
 const schema = new mongoose.Schema({
   key: String,
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
-  expiresAt: { type: Date, expires: ONE_DAY, default: Date.now() },
+  expiresAt: { type: Date, expires: ONE_DAY, default: Date.now },
 });
 
 export default instanceModel('passwordrecoveries', schema);
