ab35199e5d397fb1ddd1b3b3d7fb018dae614222
mfacar <mercy@huridocs.org>
2024-06-11 21:56:52 -0500

    adds test cases

--- cypress/e2e/settings/activitylog.cy.ts
+++ cypress/e2e/settings/activitylog.cy.ts
@@ -2,26 +2,46 @@ import 'cypress-axe';
 import { clearCookiesAndLogin } from '../helpers';
 
 describe('Activity log', () => {
-  // eslint-disable-next-line max-statements
   before(() => {
     const env = { DATABASE_NAME: 'uwazi_e2e', INDEX_NAME: 'uwazi_e2e' };
     cy.exec('yarn e2e-fixtures', { env });
     clearCookiesAndLogin('editor', 'editor');
+  });
+
+  it('should register entity creation in activity log', () => {
+    cy.contains('a', 'Library').click();
+    cy.contains('Create entity').click();
+    cy.get('#metadataForm select').select('Reporte');
+    cy.contains('Title').scrollIntoView();
+    cy.get('textarea[name="library.sidepanel.metadata.title"]:not([disabled])').type('AL Report');
+    cy.contains('aside.side-panel.metadata-sidepanel.is-active .btn-success', 'Save').click();
+    cy.contains('Entity created');
+  });
+
+  it('should register entity edition in activity log', () => {
+    cy.contains('div.item-document', 'AL Report').click();
+    cy.contains('.metadata-sidepanel.is-active .edit-metadata', 'Edit').click();
+    cy.get('#metadataForm select').eq(0).select('Reporte');
+    cy.contains('Title').scrollIntoView();
+    cy.get('textarea[name="library.sidepanel.metadata.title"]:not([disabled])').type('Report AL');
+    cy.contains('.metadata-sidepanel.is-active .btn-success', 'Save').click();
+  });
+
+  it('should register account edition in activity log', () => {
     cy.get('.only-desktop a[aria-label="Settings"]').click();
     cy.contains('span', 'Account').click();
     cy.contains('button', 'Enable').click();
     cy.contains('button', 'Cancel').click();
     cy.clearAndType('input[name=email]', 'editor@uwazi.com', { delay: 0 });
     cy.contains('button', 'Update').click();
+  });
 
+  it('should pass accessibility check', () => {
     clearCookiesAndLogin();
     cy.get('.only-desktop a[aria-label="Settings"]').click();
     cy.contains('span', 'Activity log').click();
     cy.contains('editor');
     cy.injectAxe();
-  });
-
-  it('should pass accessibility check', () => {
     cy.checkA11y();
   });
 
@@ -115,24 +135,41 @@ describe('Activity log', () => {
     cy.contains('button', 'Clear all').click();
     applyFilters();
     cy.contains('button', 'Filters').click();
-    cy.get('input[name=from]').type('2023-06-21', { delay: 0 });
-    cy.get('input[name=to]').clear();
-    cy.get('input[name=to]').type('2023-06-24', { delay: 0 });
-    cy.get('input[name=to]').realPress('Tab');
+    cy.get('#from').type('2023-06-21', { delay: 0 });
+    cy.get('#to').clear();
+    cy.get('#to').type('2023-06-24', { delay: 0 });
+    cy.get('#to').realPress('Tab');
     applyFilters();
     cy.get('tr').should('have.length.at.most', 2);
   });
 
-  it('should do a composed filter', () => {
+  // eslint-disable-next-line max-statements
+  it('should do a composed filter with search', () => {
     cy.contains('button', 'Filters').click();
     cy.contains('button', 'Clear all').click();
     cy.clearAndType('input[name=username]', 'admin', { delay: 0 });
     cy.clearAndType('input[name=search]', 'new user', { delay: 0 });
     selectMethod(0);
-    cy.get('input[name=from]').type('2023-06-22', { delay: 0 });
-    cy.clearAndType('input[name=to]', '2023-06-22', { delay: 0 });
+    cy.get('#from').type('2023-06-22', { delay: 0 });
+    cy.get('#to').clear();
+    cy.get('#to').type('2023-06-22', { delay: 0 });
     applyFilters();
     cy.get('tr').should('have.length', 2);
     cy.contains('Cynthia');
+  });
+
+  // eslint-disable-next-line max-statements
+  it('should do a composed filter with today', () => {
+    cy.contains('button', 'Filters').click();
+    cy.contains('button', 'Clear all').click();
+    cy.clearAndType('input[name=username]', 'editor', { delay: 0 });
+    selectMethod(0);
+    cy.get('#from').type('2023-06-22', { delay: 0 });
+    cy.get('#to').click();
+    cy.get('.datepicker-controls .today-btn').click();
+    applyFilters();
+    cy.get('tr').should('have.length', 4);
+    cy.get('table > tbody > tr:nth-child(2) > td:nth-child(3)').contains('Created entity:');
+    cy.get('table > tbody > tr:nth-child(2) > td:nth-child(3)').contains('AL Report');
   });
 });
