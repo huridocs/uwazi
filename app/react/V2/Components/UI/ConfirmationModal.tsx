7fb9af1d96eb56cf472036db343dc8ff8e015200
Santiago <borrajosantiago@gmail.com>
2024-06-13 11:41:31 -0300

    disable autocomplete

--- app/react/V2/Components/UI/ConfirmationModal.tsx
+++ app/react/V2/Components/UI/ConfirmationModal.tsx
@@ -86,6 +86,7 @@ const ConfirmationModal = ({
               id="confirm-password"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               type="password"
+              autoComplete="off"
               onChange={e => {
                 setInputValue(e.currentTarget.value);
                 setConfirmed(e.currentTarget.value.length > 0);
