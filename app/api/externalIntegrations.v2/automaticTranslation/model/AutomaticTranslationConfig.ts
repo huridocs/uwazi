e7c32e8226c34fe8de8f9a6bfdbcdf3eecf2ba8b
RafaPolit <rafaelpolit@gmail.com>
2024-08-08 09:20:53 -0500

    Fixed Typo

--- app/api/externalIntegrations.v2/automaticTranslation/model/AutomaticTranslationConfig.ts
+++ app/api/externalIntegrations.v2/automaticTranslation/model/AutomaticTranslationConfig.ts
@@ -1,21 +1,21 @@
 import { LanguageISO6391 } from 'shared/types/commonTypes';
-import { AutomatciTranslationTemplateConfig } from './AutomaticTranslationTemplateConfig';
+import { AutomaticTranslationTemplateConfig } from './AutomaticTranslationTemplateConfig';
 
 class AutomaticTranslationConfig {
   readonly active: boolean;
 
   readonly languages: LanguageISO6391[];
 
-  readonly templates: AutomatciTranslationTemplateConfig[];
+  readonly templates: AutomaticTranslationTemplateConfig[];
 
   constructor(
     active: boolean,
     languages: LanguageISO6391[],
-    templates: AutomatciTranslationTemplateConfig[]
+    templates: AutomaticTranslationTemplateConfig[]
   ) {
     this.active = active;
     this.languages = languages;
-    this.templates = templates;
+    this.templates = templates.filter(t => t.commonProperties.length || t.properties.length);
   }
 }
 
