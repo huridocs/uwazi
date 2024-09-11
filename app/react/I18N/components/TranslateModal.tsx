ed947a95c84cdbf27e48b9b6dd0609056d8552e4
Santiago <borrajosantiago@gmail.com>
2024-09-11 12:40:13 -0300

    test new endpoint

--- app/react/I18N/components/TranslateModal.tsx
+++ app/react/I18N/components/TranslateModal.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from 'react';
 import { useAtom, useAtomValue } from 'jotai';
 import { useForm } from 'react-hook-form';
 import { Modal } from 'V2/Components/UI';
 import { settingsAtom, translationsAtom, inlineEditAtom } from 'V2/atoms';
 import { InputField } from 'app/V2/Components/Forms';
+import { postV2, getV2 } from 'V2/api/translations';
 
 const TranslateModal = () => {
   const [inlineEditState, setInlineEditState] = useAtom(inlineEditAtom);
@@ -11,10 +13,7 @@ const TranslateModal = () => {
   const { languages } = useAtomValue(settingsAtom);
 
   const defaultValues = {
-    formValues: [
-      { language: 'en', key: 'library', value: 'Library' },
-      { language: 'es', key: 'library', value: 'Biblioteca' },
-    ],
+    formValues: [],
   };
 
   const { register, handleSubmit, setValue } = useForm({
@@ -25,25 +24,38 @@ const TranslateModal = () => {
   useEffect(() => {
     if (inlineEditState.context) {
       const formValues = translations.map(translation => {
-        const language = languages.find(lang => lang.key === translation.locale);
+        const language = languages?.find(lang => lang.key === translation.locale);
         const value = translation.contexts.find(ctx => ctx.id === inlineEditState.context)?.values[
           inlineEditState.translationKey
         ];
-        return { language: language.key, value, key: inlineEditState.translationKey };
+        return { language: language?.key, value, key: inlineEditState.translationKey };
       });
       setValue('formValues', formValues);
     }
-  }, [inlineEditState]);
+  }, [inlineEditState, languages, setValue, translations]);
 
-  const submit = values => {
-    console.log(values);
+  const submit = async values => {
+    const updatedTranslations = await postV2(
+      [
+        {
+          language: 'en',
+          value: 'TEST',
+          key: 'Filters (FILTERS CONFIGURATION)',
+        },
+      ],
+      {
+        type: 'Uwazi UI',
+        label: 'User Interface',
+        id: 'System',
+      }
+    );
   };
 
   return (
     inlineEditState.context && (
       <div className="tw-content">
         <Modal size="xxxl">
-          <Modal.Header>Translate!</Modal.Header>
+          <Modal.Header>Translate</Modal.Header>
           <Modal.CloseButton
             onClick={() =>
               setInlineEditState({ inlineEdit: true, translationKey: '', context: '' })
@@ -55,15 +67,16 @@ const TranslateModal = () => {
             <p>{inlineEditState.translationKey}</p>
 
             <form onSubmit={handleSubmit(submit)}>
-              {defaultValues.formValues.map((language, index) => {
-                return (
-                  <InputField
-                    label={<span>{language.language.toUpperCase()}</span>}
-                    id={`formValues.${index}.value`}
-                    {...register(`formValues.${index}.value`)}
-                  />
-                );
-              })}
+              {defaultValues.formValues.map((language, index) => (
+                <InputField
+                  label={<span>{language.language.toUpperCase()}</span>}
+                  id={`formValues.${index}.value`}
+                  // no unique identifiers for these fields
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={`formValues.${index}.value`}
+                  {...register(`formValues.${index}.value`)}
+                />
+              ))}
               <button type="submit">Save</button>
             </form>
           </Modal.Body>
