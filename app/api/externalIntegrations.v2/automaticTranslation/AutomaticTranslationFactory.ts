2c104b2d394ea5fb823bbfc5512154e601812f84
RafaPolit <rafaelpolit@gmail.com>
2024-09-26 12:41:48 -0500

    Prepared base concept of Listener

--- app/api/externalIntegrations.v2/automaticTranslation/AutomaticTranslationFactory.ts
+++ app/api/externalIntegrations.v2/automaticTranslation/AutomaticTranslationFactory.ts
@@ -1,9 +1,13 @@
 import { DefaultTransactionManager } from 'api/common.v2/database/data_source_defaults';
 import { getConnection } from 'api/common.v2/database/getConnectionForCurrentTenant';
 import { MongoTemplatesDataSource } from 'api/templates.v2/database/MongoTemplatesDataSource';
+import { DefaultTemplatesDataSource } from 'api/templates.v2/database/data_source_defaults';
+import { DefaultEntitiesDataSource } from 'api/entities.v2/database/data_source_defaults';
 import { GenerateAutomaticTranslationsCofig } from './GenerateAutomaticTranslationConfig';
 import { MongoATConfigDataSource } from './infrastructure/MongoATConfigDataSource';
 import { AJVATConfigValidator } from './infrastructure/AJVATConfigValidator';
+import { SaveEntityTranslations } from './SaveEntityTranslations';
+import { AJVTranslationResultValidator } from './infrastructure/AJVTranslationResultValidator';
 
 const AutomaticTranslationFactory = {
   defaultGenerateATConfig() {
@@ -11,6 +15,15 @@ const AutomaticTranslationFactory = {
       new MongoATConfigDataSource(getConnection(), DefaultTransactionManager()),
       new MongoTemplatesDataSource(getConnection(), DefaultTransactionManager()),
       new AJVATConfigValidator()
+    );
+  },
+
+  defaultSaveEntityTranslations() {
+    const transactionManager = DefaultTransactionManager();
+    return new SaveEntityTranslations(
+      DefaultTemplatesDataSource(transactionManager),
+      DefaultEntitiesDataSource(transactionManager),
+      new AJVTranslationResultValidator()
     );
   },
 };
---
+++ app/api/externalIntegrations.v2/automaticTranslation/adapters/driving/ATServiceListener.ts
@@ -0,0 +1,38 @@
+import { tenants } from 'api/tenants';
+import { emitToTenant } from 'api/socketio/setupSockets';
+import { TaskManager } from 'api/services/tasksmanager/TaskManager';
+import { ATTranslationResultValidator } from '../../contracts/ATTranslationResultValidator';
+import { AJVTranslationResultValidator } from '../../infrastructure/AJVTranslationResultValidator';
+import { InvalidATServerResponse } from '../../errors/generateATErrors';
+import { AutomaticTranslationFactory as ATFactory } from '../../AutomaticTranslationFactory';
+
+export class ATServiceListener {
+  static SERVICE_NAME = 'AutomaticTranslation';
+
+  private taskManager: TaskManager;
+
+  constructor() {
+    const validator: ATTranslationResultValidator = new AJVTranslationResultValidator();
+    this.taskManager = new TaskManager({
+      serviceName: ATServiceListener.SERVICE_NAME,
+      processResults: async result => {
+        if (!validator.validate(result)) {
+          throw new InvalidATServerResponse(validator.getErrors()[0]);
+        }
+
+        await tenants.run(async () => {
+          await ATFactory.defaultSaveEntityTranslations().execute(result);
+          emitToTenant(result.key[0], 'ATResultReceived', result);
+        }, result.key[0]);
+      },
+    });
+  }
+
+  start(interval = 500) {
+    this.taskManager.subscribeToResults(interval);
+  }
+
+  async stop() {
+    await this.taskManager.stop();
+  }
+}
