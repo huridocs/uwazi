4989b78113d00505cbba1a30c72331e4888c4271
Daneryl <daneryl@gmail.com>
2024-10-04 13:01:22 +0200

    AT input validation tweaks
    - Abstract a WIP generic Validator class,
    - Bubble up all error info

---
+++ app/api/externalIntegrations.v2/automaticTranslation/infrastructure/Validator.ts
@@ -0,0 +1,37 @@
+import Ajv, { JSONSchemaType, ValidateFunction } from 'ajv';
+import { JTDSchemaType, Schema } from 'ajv/dist/core';
+
+export class ValidationError extends Error {}
+
+export class Validator<T> {
+  private errors: ValidationError[] = [];
+
+  private validateFunction: ValidateFunction;
+
+  constructor(schema: Schema) {
+    const ajv = new Ajv({ strict: false });
+    this.validateFunction = ajv.compile<T>(schema);
+  }
+
+  getErrors() {
+    return this.errors;
+  }
+
+  validate(data: unknown): data is T {
+    const result = this.validateFunction(data);
+    this.errors = this.validateFunction.errors
+      ? this.validateFunction.errors.map(e => new ValidationError(e.message, { cause: e }))
+      : [];
+    return result;
+  }
+
+  ensure(data: unknown): asserts data is T {
+    this.validateFunction(data);
+    this.errors = this.validateFunction.errors
+      ? this.validateFunction.errors.map(e => new ValidationError(e.message, { cause: e }))
+      : [];
+    if (this.errors.length) {
+      throw this.errors[0];
+    }
+  }
+}
