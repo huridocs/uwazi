ca45185842e228ac22ee5be17a75c89ff62cc501
Joao <joaovictor.go.ro@gmail.com>
2025-02-13 10:24:31 -0400

    feat: implement PXExtractParagraphsFromEntities use case for paragraph extraction

---
+++ app/api/paragraphExtraction/application/PXExtractParagraphFromEntities.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+import { UseCase } from 'api/common.v2/contracts/UseCase';
+
+type Input = z.infer<typeof Schema>;
+
+type Output = any;
+
+type Dependencies = {};
+
+const Schema = z.object({
+  extractorId: z.string({ message: 'You should provide an Extractor' }),
+  entitySharedIds: z.array(z.string({ message: 'You should provide an Entity' })).min(1),
+});
+
+export class PXExtractParagraphsFromEntities implements UseCase<Input, Output> {
+  constructor(private dependencies: Dependencies) {}
+
+  async execute(input: Input): Promise<Output> {
+    /**
+     * The idea here is to:
+     * 1. Append each one of the input into a Queue
+     * 2. This Queue would execute the PXExtractParagraphsFromEntity use case.
+     *
+     * This Queue must have the following requirements:
+     * 1. Persist Tasks/Jobs (This will prevent from lost the Task if the process gets down)
+     * 2. Retry mechanism
+     * 3. Skip Tasks that exceeded the Retry limit and save those so us, developers see what when wrong and manually fix them
+     * 4. Some how handle metadata (e.g., pending, in-progress, completed, failed) for a Task, this metadata should be associated with the Entity
+     * 5. When making queries to populate our user interfaces, we should retrieve the Entity and associated status
+     */
+  }
+}
