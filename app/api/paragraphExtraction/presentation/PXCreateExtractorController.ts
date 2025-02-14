fda5158e9d84270134be96c855fe4ad5a5e8fdd5
Joao <joaovictor.go.ro@gmail.com>
2025-02-14 09:01:20 -0400

    feat: add PXCreateExtractorController for handling extractor creation requests

---
+++ app/api/paragraphExtraction/presentation/PXCreateExtractorController.ts
@@ -0,0 +1,45 @@
+import { Request } from 'express';
+
+import {
+  AbstractController,
+  Dependencies as AbstractControllerDependencies,
+} from 'api/common.v2/AbstractController';
+
+import { Input, InputSchema, PXCreateExtractor } from '../application/PXCreateExtractor';
+
+type Response = {
+  extractorId: string;
+};
+
+type PXExtractorsControllersProps = {
+  createExtractor: PXCreateExtractor;
+} & AbstractControllerDependencies;
+
+class PXCreateExtractorController extends AbstractController {
+  createExtractor: PXCreateExtractor;
+
+  constructor(dependencies: PXExtractorsControllersProps) {
+    super(dependencies);
+    this.createExtractor = dependencies.createExtractor;
+
+    this.app.post('paragraphExtraction/extractors', this.handle.bind(this));
+  }
+
+  async handle(request: Request): Promise<void> {
+    const dto: Input = {
+      sourceTemplateId: request.body.sourceTemplateId,
+      targetTemplateId: request.body.targetTemplateId,
+    };
+    InputSchema.parse(dto);
+
+    const output = await this.createExtractor.execute(dto);
+
+    const response: Response = {
+      extractorId: output.id,
+    };
+
+    this.jsonResponse(response);
+  }
+}
+
+export { PXCreateExtractorController };
