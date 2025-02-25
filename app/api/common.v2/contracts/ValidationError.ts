d26fb1e07576edbc445daa52b3868a07b4761714
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-02-25 07:56:32 -0500

    remove unused code

--- app/api/common.v2/contracts/ValidationError.ts
+++
@@ -1,9 +0,0 @@
-export class ValidationError extends Error {
-  code: string;
-
-  constructor(message: string, code: string, name?: string, options?: ErrorOptions) {
-    super(message, options);
-    this.name = name || 'ValidationError';
-    this.code = code;
-  }
-}
