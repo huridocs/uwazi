aa6e37f849881809ccdf31196a28c7e06a681978
Daneryl <daneryl@gmail.com>
2025-03-07 08:57:40 +0100

    Sentry error handling tweaks
    Sentry now will have enough time to capture error when unhandled error
    occurs, before forcing process exit.
    
    Sentry is now capturing errors on the handleError function instead of
    the express middleware, some async processes use handleError directly.

--- app/api/utils/error_handling_middleware.js
+++ app/api/utils/error_handling_middleware.js
@@ -1,13 +1,7 @@
-import * as Sentry from '@sentry/node';
-import { config } from 'api/config';
 import { handleError } from './handleError';
 
 export default (error, req, res, next) => {
   const { message, code, ...rest } = handleError(error, { req });
-
-  if (config.sentry.dsn && code >= 500) {
-    Sentry.captureException(error);
-  }
 
   res.status(code);
   res.json({ error: message, ...rest });
