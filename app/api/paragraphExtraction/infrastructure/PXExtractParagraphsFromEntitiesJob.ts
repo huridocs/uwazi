db6bb9ad10d5bb320ce8c324444cf84a040bf444
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-03-13 16:24:46 -0400

    handle errors

--- app/api/paragraphExtraction/infrastructure/PXExtractParagraphsFromEntitiesJob.ts
+++ app/api/paragraphExtraction/infrastructure/PXExtractParagraphsFromEntitiesJob.ts
@@ -5,14 +5,29 @@ import {
 
 import { PXExtractParagraphsFromEntityInput } from '../application/PXExtractParagraphsFromEntity';
 import { PXExtractParagraphsFromEntityFactory } from './PXExtractParagraphsFromEntityFactory';
+import { PXValidationError } from '../domain/PXValidationError';
 
 type Params = TenantAwareDispatchableParams & PXExtractParagraphsFromEntityInput;
 
 class PXExtractParagraphsFromEntityJob extends TenantAwareDispatchable<Params> {
+  private whiteListError = [
+    PXValidationError.codes.SEGMENTATION_FILES_NOT_FOUND,
+    PXValidationError.codes.SEGMENTATIONS_UNAVAILABLE,
+  ];
+
   async handle() {
     const useCase = PXExtractParagraphsFromEntityFactory.createDefault(this.params.tenantName);
 
-    await useCase.execute(this.params);
+    try {
+      await useCase.execute(this.params);
+    } catch (e) {
+      if (!(e instanceof PXValidationError) || this.whiteListError.includes(e.code)) {
+        throw e; // unexpect error or whitelisted errors we should retry
+      }
+
+      // If we reach here, it means that a Validation error were trowed
+      // It does not make any sense to keep retrying
+    }
   }
 }
 
