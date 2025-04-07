c8554f150b461f94f799a73f8c1abd08544582c5
mfacar <mercy@huridocs.org>
2025-04-07 11:24:39 -0500

    paragraphs empty condition

--- app/react/V2/Routes/Settings/ParagraphExtraction/components/paragraphs/Table.tsx
+++ app/react/V2/Routes/Settings/ParagraphExtraction/components/paragraphs/Table.tsx
@@ -30,16 +30,21 @@ const ParagraphsTable = ({
   const templates = useAtomValue(templatesAtom);
   const { extractor, sourceEntity } = useLoaderData() as PXParagraphLoaderResponse;
 
-  const languages = [
-    {
-      _id: pxParagraphData[0]._id,
-      name: availableLanguages.find(lang => lang.key === pxParagraphData[0].language)?.label || '',
-    },
-    ...(pxParagraphData[0].subRows || []).map(subRow => ({
-      _id: subRow._id,
-      name: availableLanguages.find(lang => lang.key === subRow.language)?.label || '',
-    })),
-  ];
+  const languages =
+    pxParagraphData.length > 0
+      ? [
+          {
+            _id: pxParagraphData[0]._id,
+            name:
+              availableLanguages.find(lang => lang.key === pxParagraphData[0].language)?.label ||
+              '',
+          },
+          ...(pxParagraphData[0].subRows || []).map(subRow => ({
+            _id: subRow._id,
+            name: availableLanguages.find(lang => lang.key === subRow.language)?.label || '',
+          })),
+        ]
+      : [];
 
   const template = templates.find(temp => temp._id === extractor?.sourceTemplateId)!;
 
