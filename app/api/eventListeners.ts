4bcedb6a34d86ce41fe656c3c68ae77b0311a346
Joan Gallego Girona <daneryl@gmail.com>
2025-04-07 10:41:20 +0200

    Queue v2 improvements (#7867)
    * Report properly when job is marked as failed
    
    the job reported on onError event is not the one updated, it will
    properly report failed: true
    
    * Incremental retry lock window
    
    every time a job fails the lock window is doubled
    
    * Remove testing entity denormalization job
    
    * handle error now handles, PXValidationErrors
    
    PXValidation errors are now marked as debug and 422 http status

--- app/api/eventListeners.ts
+++ app/api/eventListeners.ts
@@ -1,15 +1,11 @@
-import { DenormalizeEntityInMemoryTestJob } from '../queueRegistry';
-import { EntityCreatedEvent } from './entities/events/EntityCreatedEvent';
 import { EventsBus } from './eventsbus';
 import { AutomaticTranslationFactory } from './externalIntegrations.v2/automaticTranslation/AutomaticTranslationFactory';
 import { PXEntityDeletedListener } from './paragraphExtraction/infrastructure/PXEntityDeletedListener';
 import { PXEntityUpdatedListener } from './paragraphExtraction/infrastructure/PXEntityUpdatedListener';
 import { PXFilesDeletedListener } from './paragraphExtraction/infrastructure/PXFilesDeletedListener';
 import { PXFileUpdatedListener } from './paragraphExtraction/infrastructure/PXFileUpdatedListener';
-import { DefaultDispatcher } from './queue.v2/configuration/factories';
 import { registerEventListeners as registerSegmentationListeners } from './services/pdfsegmentation/eventListeners';
 import { Suggestions } from './suggestions/suggestions';
-import { tenants } from './tenants';
 
 const registerEventListeners = (eventsBus: EventsBus) => {
   Suggestions.registerEventListeners(eventsBus);
@@ -19,16 +15,6 @@ const registerEventListeners = (eventsBus: EventsBus) => {
   new PXFilesDeletedListener(eventsBus).start();
   new PXEntityDeletedListener(eventsBus).start();
   new PXEntityUpdatedListener(eventsBus).start();
-
-  eventsBus.on(EntityCreatedEvent, async event => {
-    if (!tenants.current().featureFlags?.deactivateTestJob && event.entities[0]) {
-      const dispatcher = await DefaultDispatcher(tenants.current().name);
-      await dispatcher.dispatch(DenormalizeEntityInMemoryTestJob, {
-        sharedId: event.entities[0].sharedId!,
-        tenantName: tenants.current().name,
-      });
-    }
-  });
 };
 
 export { registerEventListeners };
