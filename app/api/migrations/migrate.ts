b5c28b2f2541ed2e72bb4f2ff769475f77bab016
Daneryl <daneryl@gmail.com>
2025-04-14 08:39:29 +0200

    Mongo auth support
    this includes all node processes and the relevant scripts (blank_state)

--- app/api/migrations/migrate.ts
+++ app/api/migrations/migrate.ts
@@ -1,20 +1,10 @@
-import { ConnectOptions } from 'mongoose';
 import { DB } from 'api/odm';
 import { tenants } from 'api/tenants/tenantContext';
 import { config } from 'api/config';
 import { migrator } from './migrator';
 
-let auth: ConnectOptions;
-
-if (process.env.DBUSER) {
-  auth = {
-    user: process.env.DBUSER,
-    pass: process.env.DBPASS,
-  };
-}
-
 export const runMigration = async () => {
-  await DB.connect(config.DBHOST, auth);
+  await DB.connect(config.DBHOST, config.DBAUTH);
   const { db } = DB.connectionForDB(config.defaultTenant.dbName);
   let migrations: any[] = [];
   await tenants.run(async () => {
