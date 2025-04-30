8b1f7dfdfafe1ee9bcf8bb4b6465109c52978578
A happy cat <acasadotorres@gmail.com>
2025-04-30 15:37:43 +0200

    minor fixed to the ui

--- app/react/V2/Routes/Settings/ParagraphExtraction/components/FilterSidePanel/FilterSidepanelButton.tsx
+++ app/react/V2/Routes/Settings/ParagraphExtraction/components/FilterSidePanel/FilterSidepanelButton.tsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { useSetAtom } from 'jotai';
 import { Translate } from 'app/I18N';
 import { Button } from 'app/V2/Components/UI';
-import { Icon } from 'app/UI';
+import { FunnelIcon } from '@heroicons/react/24/solid';
 import { filterSidepanelAtom } from './filterSidepanelAtom';
 
-const FilterSidepanelButton = () => {
+const FilterSidepanelButton = ({ activeFilters = 0 }: { activeFilters?: number }) => {
   const setOpen = useSetAtom(filterSidepanelAtom);
 
   return (
     <Button
-      className="leading-4 flex gap-2 items-center text-gray-800"
+      className="flex items-center gap-2 leading-4 text-gray-800"
       styling="light"
       onClick={() => setOpen(true)}
     >
-      <Icon icon="filter" />
-      <Translate>Filters</Translate>
+      <FunnelIcon
+        className={`inline w-4 mr-2 ${activeFilters > 0 ? 'text-primary-900' : 'text-gray-800'} `}
+      />
+      <Translate>Stats & Filters</Translate>
+      {activeFilters > 0 && (
+        <span className="px-3 py-[2px] ml-2 text-xs text-white rounded-md bg-primary-900">
+          {activeFilters}
+        </span>
+      )}
     </Button>
   );
 };
