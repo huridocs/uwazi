6fd198f50d8b812096c2100612f996973c9405a8
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-06-06 14:37:18 -0400

    refactor messages from websocket

--- app/api/services/informationextraction/WebSocketEvents.ts
+++ app/api/services/informationextraction/WebSocketEvents.ts
@@ -1,4 +1,4 @@
 export enum IXWebSocketEvents {
-  NoFilesForTraining = 'ix:no_files_for_training',
-  NoEntitiesForTraining = 'ix:no_entities_for_training',
+  ErrorWhenSendingFilesForTraining = 'ix:error_when_sending_files_for_training',
+  ErrorWhenSendingEntitiesForTraining = 'ix:error_when_sending_entities_for_training',
 }
