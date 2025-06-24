1da4ee373402394bba490e9ce08c0b3df58dceb1
A happy cat <acasadotorres@gmail.com>
2025-06-24 15:06:25 +0200

    updated selector

--- cypress/e2e/settings/translations.cy.ts
+++ cypress/e2e/settings/translations.cy.ts
@@ -32,7 +32,7 @@ describe('Translations', () => {
     it('should sort the tables', () => {
       const checkOrder = (items: string[]) => {
         items.forEach(item => {
-          cy.contains('[data-testid=table-header]', item).scrollIntoView();
+          cy.contains('caption', item).scrollIntoView();
         });
       };
 
