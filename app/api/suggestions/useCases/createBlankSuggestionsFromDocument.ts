91541982a83b9f1ac4c74d68eb45ad4669258f4f
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-06-30 16:20:40 -0400

    feat: add CreateBlankSuggestionsFromDocument

---
+++ app/api/suggestions/useCases/createBlankSuggestionsFromDocument.ts
@@ -0,0 +1,65 @@
+import { UseCase } from 'api/common.v2/contracts/UseCase';
+import entities from 'api/entities';
+import { Extractors } from 'api/services/informationextraction/ixextractors';
+import settings from 'api/settings';
+import { LanguageUtils } from 'shared/language';
+import { FileType } from 'shared/types/fileType';
+import { IXSuggestionType } from 'shared/types/suggestionType';
+import { IXServices } from 'api/services/informationextraction/IXServices';
+import { SuggestionFactory } from '../suggestionFactory';
+import { Suggestions } from '../suggestions';
+import {
+  ExtractorsNotAvailableError,
+  FileTypeNotSupportedError,
+  LanguageNotSupportedError,
+} from '../ixValidationError';
+
+type Input = {
+  file: FileType;
+};
+
+export class CreateBlankSuggestionsFromDocument implements UseCase<Input, void> {
+  // eslint-disable-next-line class-methods-use-this
+  async execute({ file }: Input): Promise<void> {
+    const isDocument = file.type === 'document';
+    if (!isDocument) {
+      throw new FileTypeNotSupportedError(file.type!);
+    }
+
+    const { languages } = await settings.get();
+    const isLanguageSupported = languages?.some(
+      l => l.key === LanguageUtils.fromISO639_3(file.language!).key
+    );
+
+    if (!isLanguageSupported) {
+      throw new LanguageNotSupportedError(file.language!);
+    }
+    const [entity] = await entities.get({ sharedId: file.entity! }, { template: 1 });
+
+    const extractors = await Extractors.get({
+      templates: { $in: [entity.template] },
+      'source.pdf': { $exists: true },
+    });
+
+    if (!extractors.length) {
+      throw new ExtractorsNotAvailableError(entity.template);
+    }
+
+    const targetProperty = await IXServices.getTargetProperty({ extractor: extractors[0] });
+
+    const suggestions: IXSuggestionType[] = [];
+
+    extractors.forEach(e =>
+      suggestions.push(
+        SuggestionFactory.createForPdf({
+          file,
+          entity,
+          extractor: e,
+          targetProperty,
+        })
+      )
+    );
+
+    await Suggestions.createMultiple(suggestions);
+  }
+}
