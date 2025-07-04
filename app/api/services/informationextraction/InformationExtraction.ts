7dad5cf04cb563556d604ee32bfa4f78c16eda80
RafaPolit <rafaelpolit@gmail.com>
2025-07-04 10:58:16 -0500

    Individualized the two batch sizes

--- app/api/services/informationextraction/InformationExtraction.ts
+++ app/api/services/informationextraction/InformationExtraction.ts
@@ -28,7 +28,8 @@ import { ModelStatus } from 'shared/types/IXModelSchema';
 import { IXSuggestionType } from 'shared/types/suggestionType';
 import { FileType } from 'shared/types/fileType';
 import {
-  BATCH_SIZE as SUGGESTIONS_BATCH_SIZE,
+  BATCH_SIZE_FOR_PDF,
+  BATCH_SIZE_FOR_PROPERTY,
   FileWithAggregation,
   getEntitiesForSuggestions,
   getFilesForSuggestions,
@@ -635,7 +636,7 @@ class InformationExtraction {
     if (extractor.source.pdf) {
       const suggestionsStatus = await this.getSuggestionsStatus(extractorId, model);
       const remaining = (model.totalSuggestionsToFind || 0) - suggestionsStatus.processed;
-      const batchSize = model.testRun ? Math.min(remaining, SUGGESTIONS_BATCH_SIZE) : undefined;
+      const batchSize = model.testRun ? Math.min(remaining, BATCH_SIZE_FOR_PDF) : undefined;
       const files = await getFilesForSuggestions(extractorId, batchSize);
 
       if (files.length === 0) {
@@ -650,7 +651,7 @@ class InformationExtraction {
     if (extractor.source.property) {
       const suggestionsStatus = await this.getSuggestionsStatus(extractorId, model);
       const remaining = (model.totalSuggestionsToFind || 0) - suggestionsStatus.processed;
-      const batchSize = model.testRun ? Math.min(remaining, SUGGESTIONS_BATCH_SIZE) : undefined;
+      const batchSize = model.testRun ? Math.min(remaining, BATCH_SIZE_FOR_PROPERTY) : undefined;
       const entitiesForSuggestions = await getEntitiesForSuggestions(extractorId, batchSize);
 
       if (entitiesForSuggestions.length === 0) {
