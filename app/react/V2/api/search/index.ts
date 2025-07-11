6fd57945678c11ad593c754352be1c11024f8486
Santiago <borrajosantiago@gmail.com>
2025-07-11 12:06:30 -0300

    update lookup endpoint

--- app/react/V2/api/search/index.ts
+++ app/react/V2/api/search/index.ts
@@ -1,29 +1,41 @@
 import { IncomingHttpHeaders } from 'http';
+import qs from 'qs';
 import api from 'app/utils/api';
 import { RequestParams } from 'app/utils/RequestParams';
-
-interface LookupOption {
-  value: string;
-  label: string;
-  template: string;
-}
+import { SearchQuery } from 'shared/types/SearchQueryType';
+import { EntitySchema } from 'shared/types/entityType';
 
 interface LookupResponse {
-  count: number;
-  options: LookupOption[];
+  data: (Pick<EntitySchema, 'title' | 'sharedId' | 'template'> & { _id: string })[];
+  links?: {
+    self: string;
+    first?: string | null;
+    last?: string | null;
+    next?: string | null;
+    prev?: string | null;
+  };
 }
 
 const lookup = async (
-  searchTerm: string,
-  templates?: string[],
+  entityTitle: string,
+  template?: string,
   headers?: IncomingHttpHeaders
 ): Promise<LookupResponse> => {
   try {
-    const requestParams = new RequestParams({ searchTerm, templates }, headers);
+    const search: SearchQuery = {
+      fields: ['title', 'sharedId', 'template'],
+      filter: {
+        searchString: entityTitle,
+        ...(template && { template }),
+      },
+    };
+
+    const requestParams = new RequestParams(qs.stringify(search), headers);
     if (headers && headers['Content-Language']) {
       api.locale(headers['Content-Language']);
     }
-    const response = await api.get('search/lookup', requestParams);
+
+    const response: { json: LookupResponse } = await api.get('v2/search', requestParams);
     return response.json;
   } catch (e) {
     return e;
