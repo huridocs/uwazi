0969fc9b6dca578fc606fe6e4ee3816321899a82
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-07-16 16:07:24 -0400

    refactor: change test so it don't use production code

--- app/api/services/informationextraction/specs/InformationExtraction.spec.ts
+++ app/api/services/informationextraction/specs/InformationExtraction.spec.ts
@@ -15,6 +15,14 @@ import { PropertyTypeSchema } from 'shared/types/commonTypes';
 import testingDB from 'api/utils/testing_db';
 import { SegmentationModel } from 'api/services/pdfsegmentation/segmentationModel';
 import { filesModel } from 'api/files/filesModel';
+import entities from 'api/entities';
+import { EnforcedWithId } from 'api/odm';
+import settings from 'api/settings';
+import { Suggestions } from 'api/suggestions/suggestions';
+import { LanguageUtils } from 'shared/language';
+import { IXExtractorType } from 'shared/types/extractorType';
+import { FileType } from 'shared/types/fileType';
+import { IXSuggestionType } from 'shared/types/suggestionType';
 import { factory, fixtures } from './fixtures';
 import {
   CommonSuggestion,
@@ -28,7 +36,7 @@ import { ExternalDummyService } from '../../tasksmanager/specs/ExternalDummyServ
 import { IXModelsModel } from '../IXModelsModel';
 import { Extractors } from '../ixextractors';
 import { IXWebSocketEvents } from '../WebSocketEvents';
-import { NoLabeledEntities, NoSegmentedFiles } from '../ixMaterials';
+import { FileWithAggregation, NoLabeledEntities, NoSegmentedFiles } from '../ixMaterials';
 import { TEST_RUN_SUGGESTIONS_SIZE } from '../ixmodels';
 
 let informationExtractionForJob: InformationExtraction;
@@ -66,6 +74,44 @@ jest.mock('api/queue.v2/configuration/factories', () => ({
     });
   },
 }));
+
+const _getEntityFromFile = async (file: EnforcedWithId<FileType> | FileWithAggregation) => {
+  let [entity] = await entities.getUnrestricted({
+    sharedId: file.entity,
+    language: LanguageUtils.fromISO639_3(file.language!)?.ISO639_1,
+  });
+
+  if (!entity) {
+    const defaultLanguage = await settings.getDefaultLanguage();
+    [entity] = await entities.getUnrestricted({
+      sharedId: file.entity,
+      language: defaultLanguage?.key,
+    });
+  }
+  return entity;
+};
+
+const _saveSuggestionProcess = async (file: FileWithAggregation, extractor: IXExtractorType) => {
+  const entity = await _getEntityFromFile(file);
+  const [existingSuggestions] = await IXSuggestionsModel.get({
+    entityId: entity.sharedId,
+    extractorId: extractor._id,
+    fileId: file._id,
+  });
+
+  const suggestion: IXSuggestionType = {
+    ...existingSuggestions,
+    entityId: entity.sharedId!,
+    fileId: file._id,
+    language: LanguageUtils.fromISO639_3(file.language)?.ISO639_1 || 'other',
+    extractorId: extractor._id,
+    propertyName: extractor.property,
+    status: 'processing',
+    date: new Date().getTime(),
+  };
+
+  return Suggestions.save(suggestion);
+};
 
 const readDocument = async (letter: string, xmlName?: string) => {
   const _xmlName = xmlName ?? `document${letter}.xml`;
@@ -153,7 +199,7 @@ describe('InformationExtraction', () => {
   ) => {
     const extractorId = factory.id(extractorName);
     const [extractor] = await Extractors.get({ _id: extractorId });
-    await informationExtraction.saveSuggestionProcess(
+    await _saveSuggestionProcess(
       {
         _id: factory.id(id),
         entity,
