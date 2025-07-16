2e128a167765b685aa79e2eeb30e2d742d5b8b4b
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-07-16 15:51:15 -0400

    refactor: use SuggestionFactory on CreateBlankSuggestionsForProperty

--- app/api/suggestions/useCases/createBlankSuggestionsForProperty.ts
+++ app/api/suggestions/useCases/createBlankSuggestionsForProperty.ts
@@ -1,49 +1,16 @@
 import { UseCase } from 'api/common.v2/contracts/UseCase';
-import { getSuggestionState } from 'shared/getIXSuggestionState';
-import { IXServices } from 'api/services/informationextraction/IXServices';
 import { Suggestions } from '../suggestions';
 import { CreateBlankSuggestionsInput } from './createBlankSuggestionStrategy';
+import { SuggestionFactory } from '../suggestionFactory';
 
 type Input = CreateBlankSuggestionsInput;
 
 export class CreateBlankSuggestionsForProperty implements UseCase<Input, void> {
   // eslint-disable-next-line class-methods-use-this
-  async execute({ extractor, templateId, entities, isMultiValued, targetProperty }: Input) {
-    const suggestions = entities.map(entity => {
-      const blank = {
-        language: entity.language,
-        entityId: entity.sharedId,
-        entityTemplate: templateId,
-        extractorId: extractor._id,
-        propertyName: extractor.property,
-        status: 'ready' as 'ready',
-        error: '',
-        segment: '',
-        suggestedValue: isMultiValued ? [] : '',
-        date: new Date().getTime(),
-      };
-
-      const currentValue = IXServices.extractCurrentValue({ entity, targetProperty });
-      const labeledValue = IXServices.extractLabeledValueFromEntity({ entity, targetProperty });
-
-      const state = getSuggestionState(
-        {
-          date: blank.date,
-          error: blank.error,
-          status: blank.status,
-          suggestedValue: blank.suggestedValue,
-          segment: blank.segment,
-          currentValue,
-          labeledValue,
-          modelCreationDate: undefined as any,
-          state: undefined as any,
-        },
-        targetProperty.type
-      );
-
-      return { ...blank, state };
-    });
-
+  async execute({ extractor, entities, targetProperty }: Input) {
+    const suggestions = entities.map(entity =>
+      SuggestionFactory.createForProperty({ entity, extractor, targetProperty })
+    );
     await Suggestions.createMultiple(suggestions);
   }
 }
