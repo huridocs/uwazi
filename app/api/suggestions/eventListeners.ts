081875adebe368095d0e73cbed0786259fc36fac
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-07-21 07:31:07 -0400

    refactor: remove unused code

--- app/api/suggestions/eventListeners.ts
+++ app/api/suggestions/eventListeners.ts
@@ -1,26 +1,17 @@
 import { EntityDeletedEvent } from 'api/entities/events/EntityDeletedEvent';
 import { EventsBus } from 'api/eventsbus';
-import { files } from 'api/files';
 import { FilesDeletedEvent } from 'api/files/events/FilesDeletedEvent';
 import { Extractors } from 'api/services/informationextraction/ixextractors';
 import settings from 'api/settings';
 import templates from 'api/templates';
 import { TemplateDeletedEvent } from 'api/templates/events/TemplateDeletedEvent';
 import { TemplateUpdatedEvent } from 'api/templates/events/TemplateUpdatedEvent';
-import { objectIndex } from 'shared/data_utils/objectIndex';
-import { shallowObjectDiff } from 'shared/data_utils/shallowObjectDiff';
-import { ObjectIdSchema } from 'shared/types/commonTypes';
-import { EntitySchema } from 'shared/types/entityType';
-import { IXExtractorType } from 'shared/types/extractorType';
-import { EnforcedWithId } from 'api/odm';
-import { FileType } from 'shared/types/fileType';
 import { IXSuggestionType } from 'shared/types/suggestionType';
 import { EntityCreatedEvent } from 'api/entities/events/EntityCreatedEvent';
 import { DefaultSettingsDataSource } from 'api/settings.v2/database/data_source_defaults';
 import { DefaultTransactionManager } from 'api/common.v2/database/data_source_defaults';
 import { DefaultLogger } from 'api/log.v2/infrastructure/StandardLogger';
 import { Suggestions } from './suggestions';
-import { getBlankSuggestionForPdf } from './blankSuggestions';
 import { AfterFileUpdatedListener } from './listeners/afterFileCreatedListener';
 import { CreateBlankSuggestionsFromDocument } from './useCases/createBlankSuggestionsFromDocument';
 import { SuggestionFactory } from './suggestionFactory';
@@ -31,99 +22,6 @@ import { ProcessSuggestionsAfterTemplateChanged } from './useCases/processSugges
 const featureIsEnabled = async () => {
   const configuration = await settings.get();
   return !!configuration.features?.metadataExtraction;
-};
-
-const extractedMetadataChanged = async (
-  existingEntity: EntitySchema,
-  newEntity: EntitySchema,
-  extractors: IXExtractorType[]
-) => {
-  if (!extractors.length || !newEntity.metadata) return false;
-  const templatesByProperty = objectIndex(
-    extractors,
-    e => e.property,
-    e => new Set(e.templates.map(t => t.toString()))
-  );
-  const changedMetadata = shallowObjectDiff(newEntity.metadata, existingEntity.metadata || {}).all;
-  if (newEntity.title !== existingEntity.title) changedMetadata.push('title');
-  if (!existingEntity.template) return false;
-  for (let i = 0; i < changedMetadata.length; i += 1) {
-    const property = changedMetadata[i];
-    if (
-      property in templatesByProperty &&
-      templatesByProperty[property].has(existingEntity.template.toString())
-    ) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const createDefaultSuggestionsForFiles = async (
-  fileList: EnforcedWithId<FileType>[],
-  entityTemplateId: ObjectIdSchema,
-  extractorsInvolved: IXExtractorType[],
-  defaultLanguage: string
-) => {
-  const blankSuggestions: IXSuggestionType[] = [];
-
-  const template = await templates.getById(entityTemplateId);
-  const extractorPropertySet = new Set(extractorsInvolved.map(e => e.property));
-  const involvedProperties =
-    template!.properties?.filter(p => extractorPropertySet.has(p.name)) || [];
-  const involvedPropertiesByName = objectIndex(
-    involvedProperties,
-    p => p.name,
-    p => p
-  );
-
-  fileList.forEach(file => {
-    extractorsInvolved.forEach(extractor => {
-      const propertyType = involvedPropertiesByName[extractor.property]?.type;
-      if (file.entity) {
-        blankSuggestions.push(
-          getBlankSuggestionForPdf({
-            file,
-            extractorId: extractor._id,
-            propertyName: extractor.property,
-            template: entityTemplateId,
-            propertyType,
-            defaultLanguage,
-          })
-        );
-      }
-    });
-  });
-
-  await Suggestions.saveMultiple(blankSuggestions);
-};
-
-const handleTemplateChange = async (
-  originalDoc: EntitySchema,
-  modifiedDoc: EntitySchema,
-  extractors: IXExtractorType[]
-) => {
-  const originalTemplateId = originalDoc.template!.toString();
-  const modifiedTemplateId = modifiedDoc.template!.toString();
-
-  if (originalTemplateId === modifiedTemplateId) return;
-
-  await Suggestions.delete({ entityId: modifiedDoc.sharedId });
-
-  const extractorsForEntity = extractors.filter(extractor =>
-    extractor.templates.map(templateId => templateId.toString()).includes(modifiedTemplateId)
-  );
-
-  if (extractorsForEntity.length) {
-    const docFiles = await files.get({ entity: modifiedDoc.sharedId, type: 'document' });
-    const defaultLanguage = (await settings.getDefaultLanguage()).key;
-    await createDefaultSuggestionsForFiles(
-      docFiles,
-      modifiedTemplateId,
-      extractorsForEntity,
-      defaultLanguage
-    );
-  }
 };
 
 const registerEventListeners = (eventsBus: EventsBus) => {
