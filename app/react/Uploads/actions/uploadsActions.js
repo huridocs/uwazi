39d88b9ce6194a1036f0f1ae54895b4804af98a3
Santiago <borrajosantiago@gmail.com>
2025-08-19 15:38:55 -0300

    eslint fix

--- app/react/Uploads/actions/uploadsActions.js
+++ app/react/Uploads/actions/uploadsActions.js
@@ -8,9 +8,7 @@ import * as types from 'app/Uploads/actions/actionTypes';
 import * as libraryTypes from 'app/Library/actions/actionTypes';
 import { RequestParams } from 'app/utils/RequestParams';
 import { t } from 'app/I18N';
-
 import { APIURL } from '../../config.js';
-import api from '../../utils/api';
 import EntitiesApi from '../../Entities/EntitiesAPI';
 
 export function enterUploads() {
