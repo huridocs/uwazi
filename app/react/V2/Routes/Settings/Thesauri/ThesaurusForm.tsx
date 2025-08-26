0e63a278a34b936842efa79102cdf1aca6f1de0f
Santiago <borrajosantiago@gmail.com>
2025-08-26 20:38:22 -0300

    lint fix

--- app/react/V2/Routes/Settings/Thesauri/ThesaurusForm.tsx
+++ app/react/V2/Routes/Settings/Thesauri/ThesaurusForm.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { useNavigate, useRevalidator } from 'react-router';
 import { SubmitHandler, UseFormReturn } from 'react-hook-form';
 import { useSetAtom } from 'jotai';
-import { isEqual } from 'lodash';
 import { Row } from '@tanstack/react-table';
 import { Translate } from 'app/I18N';
 import { ClientThesaurus } from 'app/apiResponseTypes';
