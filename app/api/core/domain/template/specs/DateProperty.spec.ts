9dcb8c6b89bd8fdb755fd02c4d79dff1c75abfaf
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-03 14:00:21 -0400

    feat: add DateProperty class with validation for type handling and corresponding tests

---
+++ app/api/core/domain/template/specs/DateProperty.spec.ts
@@ -0,0 +1,21 @@
+import { PropertyTypeInvalidTypeError } from '../errors';
+import { DateProperty } from '../DateProperty';
+
+describe('DateProperty', () => {
+  it('should set defaults values if not provided', () => {
+    const property = new DateProperty({
+      id: 'any_id',
+      label: 'A Title',
+    });
+
+    expect(property).toMatchObject({
+      type: 'date',
+    });
+  });
+
+  it('should throw if providing a type different from date', () => {
+    expect(() => new DateProperty({ id: 'any', label: 'A label', type: 'text' })).toThrow(
+      new PropertyTypeInvalidTypeError('text', 'DateProperty')
+    );
+  });
+});
