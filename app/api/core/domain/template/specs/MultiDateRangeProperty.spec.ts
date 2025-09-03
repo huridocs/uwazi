c76e65db9083eff9ec0fc97d7f5fbea68ab999ba
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-03 14:03:01 -0400

    feat: add MultiDateRangeProperty

---
+++ app/api/core/domain/template/specs/MultiDateRangeProperty.spec.ts
@@ -0,0 +1,21 @@
+import { PropertyTypeInvalidTypeError } from '../errors';
+import { MultiDateRangeProperty } from '../MultiDateRangeProperty';
+
+describe('MultiDateRangeProperty', () => {
+  it('should set defaults values if not provided', () => {
+    const property = new MultiDateRangeProperty({
+      id: 'any_id',
+      label: 'A Title',
+    });
+
+    expect(property).toMatchObject({
+      type: 'multidaterange',
+    });
+  });
+
+  it('should throw if providing a type different from multidaterange', () => {
+    expect(() => new MultiDateRangeProperty({ id: 'any', label: 'A label', type: 'text' })).toThrow(
+      new PropertyTypeInvalidTypeError('text', 'MultiDateRangeProperty')
+    );
+  });
+});
