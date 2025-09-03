6b9c7fa32f19f9469c2f04c78f90bc0cf233edd2
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-03 14:00:49 -0400

    feat: enhance GeolocationProperty with type handling and validation

--- app/api/core/domain/template/GeoLocationProperty.ts
+++ app/api/core/domain/template/GeoLocationProperty.ts
@@ -1,17 +1,31 @@
-import { Property, PropertyProps } from 'api/templates.v2/model/Property';
+import { Property, PropertyProps, PropertyTypes } from 'api/templates.v2/model/Property';
 import { propertyTypes } from 'shared/propertyTypes';
 import { PropertyName } from './PropertyName';
+import { PropertyTypeInvalidTypeError } from './errors';
+
+type Props = {
+  type?: PropertyTypes;
+} & Omit<PropertyProps, 'type'>;
 
 class GeolocationProperty extends Property {
-  constructor(props: PropertyProps) {
-    super(props);
+  constructor(props: Props) {
+    super({ ...props, type: props.type || 'geolocation' });
 
     const name = props.name
       ? new PropertyName(props.name)
       : PropertyName.fromLabel(`${props.label}_${propertyTypes.geolocation}`);
 
     this.name = name;
+
+    this.validate();
+  }
+
+  protected validate() {
+    if (this.type !== 'geolocation') {
+      throw new PropertyTypeInvalidTypeError(this.type, 'GeolocationProperty');
+    }
   }
 }
 
 export { GeolocationProperty };
+export type { Props as GeolocationPropertyProps };
