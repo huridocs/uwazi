e9f31037abd24472adc00299750db40d4ba0dcc8
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-03 14:01:33 -0400

    feat: add NumericProperty

---
+++ app/api/core/domain/template/specs/NumericProperty.spec.ts
@@ -0,0 +1,21 @@
+import { PropertyTypeInvalidTypeError } from '../errors';
+import { NumericProperty } from '../NumericProperty';
+
+describe('NumericProperty', () => {
+  it('should set defaults values if not provided', () => {
+    const property = new NumericProperty({
+      id: 'any_id',
+      label: 'A Title',
+    });
+
+    expect(property).toMatchObject({
+      type: 'numeric',
+    });
+  });
+
+  it('should throw if providing a type different from numeric', () => {
+    expect(() => new NumericProperty({ id: 'any', label: 'A label', type: 'text' })).toThrow(
+      new PropertyTypeInvalidTypeError('text', 'NumericProperty')
+    );
+  });
+});
