a959931c7090e51a210b16aa6f95bb3a788ae202
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-03 09:20:04 -0400

    feat: implement CommonProperty class with validation logic

---
+++ app/api/core/domain/template/specs/CommonProperty.spec.ts
@@ -0,0 +1,23 @@
+import { CommonProperty } from 'api/templates.v2/model/CommonProperty';
+import { CommonPropertyInvalidError } from '../errors';
+
+describe('CommonProperty', () => {
+  it('should set defaults values if not provided', () => {
+    const commonProperty = new CommonProperty({
+      id: 'any_id',
+      label: 'A Title',
+      type: 'text',
+    });
+
+    expect(commonProperty).toMatchObject({
+      isCommonProperty: true,
+    });
+  });
+
+  it('should throw if providing isCommonProperty equals to false', () => {
+    expect(
+      () =>
+        new CommonProperty({ id: 'any', isCommonProperty: false, label: 'A label', type: 'date' })
+    ).toThrow(new CommonPropertyInvalidError());
+  });
+});
