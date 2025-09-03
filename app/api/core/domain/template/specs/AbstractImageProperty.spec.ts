1bee34ed6904dfa644ac6ad681dfb63ca5b342f3
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-03 13:59:00 -0400

    feat: add AbstractImageProperty class with validation and default properties

---
+++ app/api/core/domain/template/specs/AbstractImageProperty.spec.ts
@@ -0,0 +1,25 @@
+import { AbstractImageProperty, ImageStyle } from '../AbstractImageProperty';
+import { InvalidStyleTypeError } from '../errors';
+
+class Testing extends AbstractImageProperty {}
+
+describe('AbstractImageProperty', () => {
+  it('should set defaults values if not provided', () => {
+    const property = new Testing({
+      id: 'any_id',
+      label: 'A Title',
+      type: 'image',
+    });
+
+    expect(property).toMatchObject({
+      style: ImageStyle.Cover,
+      fullWidth: false,
+    });
+  });
+
+  it('should throw if providing a style type different from ImageStyle', () => {
+    expect(
+      () => new Testing({ id: 'any', label: 'A label', type: 'image', style: 'any' as any })
+    ).toThrow(new InvalidStyleTypeError('any'));
+  });
+});
