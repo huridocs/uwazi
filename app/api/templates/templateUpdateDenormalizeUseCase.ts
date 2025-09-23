3805de301dc21ac2bdb0aa1c73c7c5f203cb29d6
Daneryl <daneryl@gmail.com>
2025-09-23 08:39:52 +0200

    V2 update template full reindex is now jobified

--- app/api/templates/templateUpdateDenormalizeUseCase.ts
+++ app/api/templates/templateUpdateDenormalizeUseCase.ts
@@ -67,6 +67,7 @@ export const denormalizeTemplateEntities = async (
       newGeneratedIdProps: [],
       deletedProperties,
       renamedProperties,
+      fullReindex: false,
       tenantName: tenants.current().name,
       userId,
     });
