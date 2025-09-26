895c4ca582d3861c3f83bbf19d701f3ab53724fd
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2025-09-26 10:49:39 -0400

    Revert "fix: set reindex true"
    This reverts commit e83fb3e130aadbf671b6bddd64046503da625904.

--- app/api/migrations/migrations/177-unsure_common_properties/index.ts
+++ app/api/migrations/migrations/177-unsure_common_properties/index.ts
@@ -28,7 +28,7 @@ export default {
   description:
     'Ensure Common Properties are present in every Template and fix Property name inconsistency',
 
-  reindex: true,
+  reindex: false,
 
   async up(db: Db) {
     process.stdout.write(`${this.name}...\r\n`);
