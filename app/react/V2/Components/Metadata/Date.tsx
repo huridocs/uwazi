99741df474713cbad23f5f007d5a397587d51ccb
Santiago <borrajosantiago@gmail.com>
2025-10-14 16:07:26 -0300

    remove parsing from date component

--- app/react/V2/Components/Metadata/Date.tsx
+++ app/react/V2/Components/Metadata/Date.tsx
@@ -1,47 +1,39 @@
 import React from 'react';
-import { useParams } from 'react-router';
 import { Translate } from 'app/I18N';
-import { secondsToDate } from 'V2/shared/dateHelpers';
 import { MetadataFieldProps } from './types';
 import { MetadataLabel } from './MetadataLabel';
 import { MetadataCard } from './MetadataCard';
 
-type DateValue = { value: number } | { value: { from: number; to: number } };
+type DateValue = { label: string } | { label: { from: string; to: string } };
 
 type DateProps = MetadataFieldProps & {
   timestamps: DateValue[];
 };
 
-const Date = ({ timestamps, label, translationContext, hideLabel }: DateProps) => {
-  const locale = useParams()?.lang || 'en';
-
-  return (
-    <MetadataCard>
-      <MetadataLabel label={label} translationContext={translationContext} hideLabel={hideLabel} />
-      <div className="flex flex-col gap-1">
-        {timestamps.map(stamp => {
-          if (typeof stamp.value === 'number') {
-            return (
-              <dd className="font-medium text-gray-900">{secondsToDate(stamp.value, locale)}</dd>
-            );
-          }
-          return (
-            <dd className="font-medium text-gray-900">
-              <span className="sr-only">
-                <Translate>From</Translate>
-              </span>
-              <span>{secondsToDate(stamp.value.from, locale)}</span>
-              <span aria-hidden="true"> - </span>
-              <span className="sr-only">
-                <Translate>To</Translate>
-              </span>
-              <span>{secondsToDate(stamp.value.to, locale)}</span>
-            </dd>
-          );
-        })}
-      </div>
-    </MetadataCard>
-  );
-};
+const Date = ({ timestamps, label, translationContext, hideLabel }: DateProps) => (
+  <MetadataCard>
+    <MetadataLabel label={label} translationContext={translationContext} hideLabel={hideLabel} />
+    <div className="flex flex-col gap-1">
+      {timestamps.map(stamp => {
+        if (typeof stamp.label === 'string') {
+          return <dd className="font-medium text-gray-900">{stamp.label}</dd>;
+        }
+        return (
+          <dd className="font-medium text-gray-900">
+            <span className="sr-only">
+              <Translate>From</Translate>
+            </span>
+            <span>{stamp.label.from}</span>
+            <span aria-hidden="true"> - </span>
+            <span className="sr-only">
+              <Translate>To</Translate>
+            </span>
+            <span>{stamp.label.to}</span>
+          </dd>
+        );
+      })}
+    </div>
+  </MetadataCard>
+);
 
 export { Date };
