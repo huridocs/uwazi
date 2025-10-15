f23a704804adac121bfed7498ea4bf8c85221904
Santiago <borrajosantiago@gmail.com>
2025-10-15 10:13:39 -0300

    use displayLabel in date

--- app/react/V2/Components/Metadata/Date.tsx
+++ app/react/V2/Components/Metadata/Date.tsx
@@ -15,20 +15,20 @@ const Date = ({ timestamps, label, translationContext, hideLabel }: DateProps) =
     <MetadataLabel label={label} translationContext={translationContext} hideLabel={hideLabel} />
     <div className="flex flex-col gap-1" role="group">
       {timestamps.map(stamp => {
-        if (typeof stamp.label === 'string') {
-          return <dd className="font-medium text-gray-900">{stamp.label}</dd>;
+        if (typeof stamp.displayValue === 'string') {
+          return <dd className="font-medium text-gray-900">{stamp.displayValue}</dd>;
         }
         return (
           <dd className="font-medium text-gray-900">
             <span className="sr-only">
               <Translate>From</Translate>
             </span>
-            <span>{stamp.label.from}</span>
+            <span>{stamp.displayValue.from}</span>
             <span aria-hidden="true"> - </span>
             <span className="sr-only">
               <Translate>To</Translate>
             </span>
-            <span>{stamp.label.to}</span>
+            <span>{stamp.displayValue.to}</span>
           </dd>
         );
       })}
