1305183b6245bdfe362cd92a52842f4e6adc6304
mfacar <mercy@huridocs.org>
2025-10-16 23:41:18 -0500

    fixs type conflict

--- app/react/V2/Components/Metadata/Relationship.tsx
+++ app/react/V2/Components/Metadata/Relationship.tsx
@@ -1,51 +1,41 @@
 import React from 'react';
 import { I18NLinkV2 } from 'app/I18N';
+import { RelationshipMetadataProperty } from 'app/V2/domain/entities/types';
 import { MetadataFieldProps } from './types';
 import { CountryFlag } from '../CustomIcons';
 import { PropertyLabel } from './PropertyLabel';
 import { MetadataCard } from './MetadataCard';
 
-type RelationshipValue = {
-  value: string;
-  label: string;
-  icon?: { _id: string } | string;
-  url: string;
-};
-
 type RelationshipProps = MetadataFieldProps & {
-  values: RelationshipValue[];
+  values: Extract<RelationshipMetadataProperty['values'], Array<any>>;
 };
 
-const formatRelationshipLabel = (value: RelationshipProps['values'][0]) => {
-  return value.label;
+const Relationship = ({ label, translationContext, hideLabel, values }: RelationshipProps) => {
+  return (
+    <MetadataCard>
+      <dt>
+        <PropertyLabel label={label} translationContext={translationContext} hideLabel={hideLabel} />
+      </dt>
+      <div className="flex flex-col gap-1">
+        {values.map((value, index) => (
+          <span key={index} className="flex flex-row flex-nowrap gap-2 align-middle">
+            <dd className="font-medium text-gray-900">
+              {value.icon && <CountryFlag id={value.icon} />}
+              <I18NLinkV2
+                className="underline"
+                to={value.url || `/entityv2/${value.value}`}
+                target="_blank"
+                rel="noreferrer"
+                localized={false}
+              >
+                {value.label}
+              </I18NLinkV2>
+            </dd>
+          </span>
+        ))}
+      </div>
+    </MetadataCard>
+  );
 };
-
-const Relationship = ({ label, translationContext, hideLabel, values }: RelationshipProps) => (
-  <MetadataCard>
-    <dt>
-      <PropertyLabel label={label} translationContext={translationContext} hideLabel={hideLabel} />
-    </dt>
-    <dd className="flex flex-col gap-1">
-      {values.map((value, index) => (
-        <span
-          // eslint-disable-next-line react/no-array-index-key
-          key={index}
-          className="flex font-medium text-gray-900 flex-row flex-nowrap gap-2 align-middle"
-        >
-          {value.icon && <CountryFlag id={value.icon._id || ''} />}
-          <I18NLinkV2
-            className="underline"
-            to={value.url}
-            target="_blank"
-            rel="noreferrer"
-            localized={false}
-          >
-            {formatRelationshipLabel(value)}
-          </I18NLinkV2>
-        </span>
-      ))}
-    </dd>
-  </MetadataCard>
-);
 
 export { Relationship };
