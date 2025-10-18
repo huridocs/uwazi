22f29473fa1d0e1135ff81995f8881a8cafbbfb2
Santiago <borrajosantiago@gmail.com>
2025-10-18 09:38:55 -0300

    cleanup + more story options

--- app/react/stories/Metadata.stories.tsx
+++ app/react/stories/Metadata.stories.tsx
@@ -3,10 +3,17 @@ import { Meta, StoryObj } from '@storybook/react';
 import { BrowserRouter } from 'react-router';
 import { createStore, Provider } from 'jotai';
 import { EntitySchema } from 'shared/types/entityType';
+import { ClientSettings, ClientThesaurus, Template } from 'app/apiResponseTypes';
 import { MetadataDisplay } from 'V2/Components/Metadata';
 import { settingsAtom } from 'V2/atoms';
 import { FluentCompositionBuilder, ProcessingContext } from 'V2/application';
-import { rawEntity, processingContext } from './fixtures/MetadataDisplayFixtures';
+import {
+  rawEntity,
+  processingContextBase,
+  thesauri,
+  templates,
+  settings,
+} from './fixtures/MetadataDisplayFixtures';
 
 const store = createStore();
 store.set(settingsAtom, { mapLayers: ['Streets', 'Hybrid', 'Satellite'] });
@@ -14,12 +21,23 @@ store.set(settingsAtom, { mapLayers: ['Streets', 'Hybrid', 'Satellite'] });
 const MetadataDisplayComponent = ({
   dbEntity,
   context,
+  contextThesauri,
+  contextTemplates,
+  contextSettings,
 }: {
   dbEntity: EntitySchema;
   context: ProcessingContext;
+  contextThesauri: ClientThesaurus[];
+  contextTemplates: Template[];
+  contextSettings: ClientSettings;
 }) => {
   //This methods are meant to be used in loaders
-  const fluentBuilder = FluentCompositionBuilder.create(context);
+  const fluentBuilder = FluentCompositionBuilder.create({
+    ...context,
+    thesauri: contextThesauri,
+    templates: contextTemplates,
+    settings: contextSettings,
+  });
   const { entity } = fluentBuilder.forDetailView().processEntity(dbEntity);
 
   return (
@@ -41,12 +59,26 @@ const meta: Meta<typeof MetadataDisplayComponent> = {
 type Story = StoryObj<typeof MetadataDisplayComponent>;
 
 const Primary: Story = {
-  render: args => <MetadataDisplayComponent dbEntity={args.dbEntity} context={args.context} />,
+  render: args => (
+    <MetadataDisplayComponent
+      dbEntity={args.dbEntity}
+      context={args.context}
+      contextThesauri={args.contextThesauri}
+      contextTemplates={args.contextTemplates}
+      contextSettings={args.contextSettings}
+    />
+  ),
 };
 
 const Basic = {
   ...Primary,
-  args: { dbEntity: rawEntity, context: processingContext },
+  args: {
+    dbEntity: rawEntity,
+    context: processingContextBase,
+    contextThesauri: thesauri,
+    contextTemplates: templates,
+    contextSettings: settings,
+  },
 };
 
 export { Basic };
