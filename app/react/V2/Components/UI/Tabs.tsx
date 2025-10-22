402d54c3d17dc247780545d9c127510c6128fefe
Santiago <borrajosantiago@gmail.com>
2025-10-22 15:33:30 -0300

    improvements to Tabs

--- app/react/V2/Components/UI/Tabs.tsx
+++ app/react/V2/Components/UI/Tabs.tsx
@@ -1,28 +1,35 @@
 /* eslint-disable react/no-multi-comp */
 import React, { useState } from 'react';
 
-const Tab = ({
-  id,
-  label,
-  children,
-}: {
+type TabProps = {
   id: string;
   label: React.ReactNode;
   children: React.ReactNode;
-}) => (
+};
+
+const Tab = ({ id, label, children }: TabProps) => (
   <div className="hidden" id={id} data-label={label}>
     {children}
   </div>
 );
 
 interface TabsProps {
-  children: React.ReactComponentElement<typeof Tab>[];
+  children: React.ReactElement<TabProps> | React.ReactElement<TabProps>[];
   onTabSelected?: (activeTab: string) => void;
+  initialTabId?: string;
   unmountTabs?: boolean;
+  className?: string;
 }
 
-const Tabs = ({ children, onTabSelected, unmountTabs = true }: TabsProps) => {
-  const [activeTab, setActiveTab] = useState(children[0].props.id);
+const Tabs = ({
+  children,
+  onTabSelected,
+  initialTabId,
+  className,
+  unmountTabs = true,
+}: TabsProps) => {
+  const tabChildren = Array.isArray(children) ? children : [children];
+  const [activeTab, setActiveTab] = useState(initialTabId || tabChildren[0].props.id);
 
   const handleClick = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -36,23 +43,29 @@ const Tabs = ({ children, onTabSelected, unmountTabs = true }: TabsProps) => {
     }
   };
 
-  const buttonClass = 'text-sm flex-1 font-medium py-2 w-80 h-14';
   const activeClass = 'text-gray-900 bg-gray-50';
   const inactiveClass = 'text-gray-500';
 
   return (
     <div className="flex flex-col h-full">
       <div
-        className="flex rounded-md border border-gray-50 shadow-sm md:w-1/2"
+        role="tablist"
+        aria-orientation="horizontal"
+        className={`inline-grid grid-flow-col auto-cols-auto rounded-md shadow divide-x-2 divide-gray-100 w-max ${className || ''}`}
         data-testid="tabs-comp"
       >
-        {children.map((child, index: number) => (
+        {tabChildren.map(child => (
           <button
             key={child.props.id}
+            id={`tab-${child.props.id}`}
+            role="tab"
+            aria-controls={`panel-${child.props.id}`}
+            aria-selected={activeTab === child.props.id}
+            tabIndex={activeTab === child.props.id ? 0 : -1}
             type="button"
-            className={`${buttonClass} ${
+            className={`p-2 text-left flex items-center justify-start h-full ${
               activeTab === child.props.id ? activeClass : inactiveClass
-            } ${index !== children.length - 1 ? 'border-r-2 border-gray-50' : ''}`}
+            }`}
             onClick={e => handleClick(e, child.props.id)}
           >
             {child.props.label}
@@ -60,29 +73,38 @@ const Tabs = ({ children, onTabSelected, unmountTabs = true }: TabsProps) => {
         ))}
       </div>
       <div className="py-4 grow">
-        {children.map(child => {
+        {tabChildren.map(child => {
+          const isActive = child.props.id === activeTab;
+
           if (unmountTabs) {
-            if (child.props.id === activeTab) {
+            if (isActive) {
               return (
-                <div className="w-full h-full" key={child.props.id}>
+                <div
+                  className="w-full h-full"
+                  key={child.props.id}
+                  id={`panel-${child.props.id}`}
+                  role="tabpanel"
+                  aria-labelledby={`tab-${child.props.id}`}
+                >
                   {child.props.children}
                 </div>
               );
             }
+
+            return null;
           }
 
-          if (!unmountTabs) {
-            return (
-              <div
-                key={child.props.id}
-                className={`w-full h-full ${child.props.id !== activeTab ? 'hidden' : ''}`}
-              >
-                {child.props.children}
-              </div>
-            );
-          }
-
-          return null;
+          return (
+            <div
+              key={child.props.id}
+              id={`panel-${child.props.id}`}
+              role="tabpanel"
+              aria-labelledby={`tab-${child.props.id}`}
+              className={`w-full h-full ${!isActive ? 'hidden' : ''}`}
+            >
+              {child.props.children}
+            </div>
+          );
         })}
       </div>
     </div>
