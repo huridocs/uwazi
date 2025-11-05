4defb7ba85253d7c035fa959ed1c1977b8f8bccc
RafaPolit <rafaelpolit@gmail.com>
2025-11-05 16:34:28 -0500

    Changed service factory to use FileStorageStrategy instead

--- app/api/csv.v2/services/service_factories.ts
+++ app/api/csv.v2/services/service_factories.ts
@@ -1,9 +1,9 @@
+import { FileStorageStrategyFactory } from 'api/files.v2/infrastructure/FileStorageStrategyFactory';
 import { DefaultCsvImportsDataSource } from '../database/data_source_defaults';
-import { FileStorageAdapter } from '../database/FileStorageAdapter';
 import { RegisterCsvImportUseCase } from './RegisterCsvImportUseCase';
 
 export const RegisterCsvImportUseCaseFactory = () => {
   const csvImportsDS = DefaultCsvImportsDataSource();
-  const fileStorage = new FileStorageAdapter();
+  const fileStorage = FileStorageStrategyFactory.createDefault();
   return new RegisterCsvImportUseCase({ csvImportsDS, fileStorage });
 };
