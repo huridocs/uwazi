2b60bf4ad0475db61821d09c9b999b2de8cb2f1c
RafaPolit <rafaelpolit@gmail.com>
2025-11-06 12:51:08 -0500

    Changed to FileSystemStorage instead of Strategy

--- app/api/csv.v2/services/service_factories.ts
+++ app/api/csv.v2/services/service_factories.ts
@@ -1,11 +1,14 @@
-import { FileStorageStrategyFactory } from 'api/files.v2/infrastructure/FileStorageStrategyFactory';
+import { FileSystemStorage } from 'api/files.v2/infrastructure/FileSystemStorage';
+import { PathManager } from 'api/files.v2/infrastructure/PathManager';
 import { TransactionManagerFactory } from 'api/core/infrastructure/factories/TransactionManagerFactory';
+import { tenants } from 'api/tenants/tenantContext';
 import { DefaultCsvImportsDataSource } from '../database/data_source_defaults';
 import { RegisterCsvImportUseCase } from './RegisterCsvImportUseCase';
 
 export const RegisterCsvImportUseCaseFactory = () => {
   const transactionManager = TransactionManagerFactory.default();
   const csvImportsDS = DefaultCsvImportsDataSource(transactionManager);
-  const fileStorage = FileStorageStrategyFactory.createDefault();
+  const tenant = tenants.current();
+  const fileStorage = new FileSystemStorage(new PathManager({ tenant }));
   return new RegisterCsvImportUseCase({ csvImportsDS, fileStorage, transactionManager });
 };
