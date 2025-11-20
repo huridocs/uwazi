042ce6ef0acbbb71ae33ee417e26b1078c9c77e1
Santiago <borrajosantiago@gmail.com>
2025-11-20 15:02:25 -0300

    small type updates

--- app/react/V2/domain/entities/Entity.ts
+++ app/react/V2/domain/entities/Entity.ts
@@ -1,13 +1,14 @@
 import { FileType } from 'shared/types/fileType';
 import { EntityWithFilesSchema } from 'shared/types/entityType';
-import { EntityPermissions, MetadataProperty, DateMetadataProperty, EntityTemplate } from './types';
+import { AdapterEntityTemplate } from 'V2/application/services/processors/types';
+import { EntityPermissions, MetadataProperty, DateMetadataProperty } from './types';
 
 export interface Entity {
   readonly _id: string;
   readonly sharedId: string;
   readonly title: string;
   readonly language: string;
-  readonly template?: EntityTemplate;
+  readonly template?: AdapterEntityTemplate;
   creationDate: DateMetadataProperty;
   editDate: DateMetadataProperty;
   readonly icon?: { _id: string } | any;
