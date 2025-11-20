dadf6610ae1bcea35e3978e50de291ec17853851
mfacar <mercy@huridocs.org>
2025-11-19 22:01:53 -0500

    eslint

--- cypress/e2e/helpers/a11y.ts
+++ cypress/e2e/helpers/a11y.ts
@@ -1,29 +1,28 @@
 import type { Result } from 'axe-core';
 
 const formatViolationData = (violations: Result[]) =>
-    violations.map(violation => ({
-        id: violation.id,
-        impact: violation.impact,
-        description: violation.description,
-        nodes: violation.nodes.length,
-        target: violation.nodes
-            .map(node => node.target && node.target.join(', '))
-            .filter(Boolean)
-            .join(' | '),
-    }));
+  violations.map(violation => ({
+    id: violation.id,
+    impact: violation.impact,
+    description: violation.description,
+    nodes: violation.nodes.length,
+    target: violation.nodes
+      .map(node => node.target && node.target.join(', '))
+      .filter(Boolean)
+      .join(' | '),
+  }));
 
 const logA11yViolations = (violations: Result[]) => {
-    if (!violations?.length) {
-        return;
-    }
+  if (!violations?.length) {
+    return;
+  }
 
-    cy.task(
-        'log',
-        `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'} detected`
-    );
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${violations.length === 1 ? '' : 's'} detected`
+  );
 
-    cy.task('table', formatViolationData(violations));
+  cy.task('table', formatViolationData(violations));
 };
 
 export { logA11yViolations };
-
