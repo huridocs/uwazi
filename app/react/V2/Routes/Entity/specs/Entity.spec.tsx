328a72c8da3eeac730c19a73e5d0c1020dae0207
mfacar <mercy@huridocs.org>
2026-01-06 10:34:35 -0500

    update test

--- app/react/V2/Routes/Entity/specs/Entity.spec.tsx
+++ app/react/V2/Routes/Entity/specs/Entity.spec.tsx
@@ -77,7 +77,7 @@ describe('Entity view', () => {
       render(
         <TestRouterContext
           loaderData={{
-            entity: { ...sampleEntity, mainDocument: mainDocumentFile },
+            entity: { ...sampleEntity, mainDocument: [mainDocumentFile] },
             pagePlaintext: '',
           }}
         >
@@ -101,7 +101,7 @@ describe('Entity view', () => {
       render(
         <TestRouterContext
           loaderData={{
-            entity: { ...sampleEntity, mainDocument: mainDocumentFile },
+            entity: { ...sampleEntity, mainDocument: [mainDocumentFile] },
             pagePlaintext: '',
           }}
         >
@@ -279,7 +279,7 @@ describe('Entity view', () => {
       render(
         <TestRouterContext
           loaderData={{
-            entity: { ...sampleEntity, mainDocument: mainDocumentFile },
+            entity: { ...sampleEntity, mainDocument: [mainDocumentFile] },
             pagePlaintext: pageText,
           }}
         >
@@ -309,7 +309,7 @@ describe('Entity view', () => {
       render(
         <TestRouterContext
           loaderData={{
-            entity: { ...sampleEntity, mainDocument: mainDocumentFile },
+            entity: { ...sampleEntity, mainDocument: [mainDocumentFile] },
             pagePlaintext: pageText,
           }}
         >
