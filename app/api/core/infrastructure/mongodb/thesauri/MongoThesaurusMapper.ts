35a294953a8c598cc44a5167c79109f93ecc013d
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2026-01-27 10:50:16 -0400

    provide validate flag to correct parameter

--- app/api/core/infrastructure/mongodb/thesauri/MongoThesaurusMapper.ts
+++ app/api/core/infrastructure/mongodb/thesauri/MongoThesaurusMapper.ts
@@ -19,20 +19,20 @@ export class MongoThesaurusMapper {
   }
 
   static toDomain(schema: ThesaurusDBO, validate = true): Thesaurus {
-    return new Thesaurus({
-      id: schema._id.toHexString(),
-      name: schema.name,
-      values: schema.values?.map(
-        value => ({
+    return new Thesaurus(
+      {
+        id: schema._id.toHexString(),
+        name: schema.name,
+        values: schema.values?.map(value => ({
           id: value.id,
           label: value.label,
           values: value.values?.map(subValue => ({
             id: subValue.id,
             label: subValue.label,
           })),
-        }),
-        validate
-      ),
-    });
+        })),
+      },
+      validate
+    );
   }
 }
