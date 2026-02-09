cee60e9bc755c2f122d743fd52a6b30541a0b345
Santiago <borrajosantiago@gmail.com>
2026-02-09 13:24:02 -0300

    better fallback

--- app/shared/atomStore/server.store.ts
+++ app/shared/atomStore/server.store.ts
@@ -2,13 +2,13 @@ import { createStore } from 'jotai';
 import { AsyncLocalStorage } from 'async_hooks';
 
 const asyncLocalStorage = new AsyncLocalStorage<ReturnType<typeof createStore>>();
-const fallbackStore = createStore();
 
 const runWithStore = <T>(fn: () => T) => asyncLocalStorage.run(createStore(), fn);
 
 const getStore = (): ReturnType<typeof createStore> => {
   const store = asyncLocalStorage.getStore();
-  return store || fallbackStore;
+  const fallback = createStore();
+  return store || fallback;
 };
 
 export { runWithStore, getStore };
