1eea37973e5337387976617de9b9ec3a5322bb14
Santiago <borrajosantiago@gmail.com>
2026-02-10 12:00:00 -0300

    handle ssr route correctly

--- app/react/server.ts
+++ app/react/server.ts
@@ -3,7 +3,7 @@ import { runWithStore } from 'shared/atomStore/server.store';
 import { EntryServer } from './entry-server';
 
 export const serverSideRender = async (app: Application) => {
-  app.get(/^\/(?!api(\/|$)).*$/, (req, res, next) => {
-    runWithStore(async () => EntryServer(req, res)).catch(next);
+  app.get(/^\/(?!api(\/|$)).*$/, async (req, res) => {
+    await runWithStore(async () => EntryServer(req, res));
   });
 };
