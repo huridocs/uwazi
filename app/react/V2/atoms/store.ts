f16a18ab78be441e5491023c3f5bc52ad5e7f187
A happy cat <acasadotorres@gmail.com>
2026-02-20 16:58:26 +0100

    merge solved

--- app/react/V2/atoms/store.ts
+++ app/react/V2/atoms/store.ts
@@ -14,7 +14,6 @@ import { translationsAtom, localeAtom } from './translationsAtoms';
 import { userAtom } from './userAtom';
 import { thesauriAtom } from './thesauriAtom';
 import { serverIsMobileAtom } from './isMobileAtom';
-import { pdfScaleAtom } from './pdfScaleAtom';
 import { acceptedSuggestions as ixAcceptedSuggestions } from '../Routes/Settings/IX/components/atoms';
 
 type AtomStoreData = {
@@ -66,10 +65,6 @@ if (isClient && window.__atomStoreData__) {
   atomStore.sub(thesauriAtom, () => {
     const value = atomStore.get(thesauriAtom);
     reduxStore?.dispatch({ type: 'dictionaries/SET', value });
-  });
-  atomStore.sub(pdfScaleAtom, () => {
-    const value = atomStore.get(pdfScaleAtom);
-    reduxStore?.dispatch({ type: 'viewer/documentScale/SET', value });
   });
   atomStore.sub(translationsAtom, () => {
     const value = atomStore.get(translationsAtom);
