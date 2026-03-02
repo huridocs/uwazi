8b965de6184d2258f674892654246199c60cf7f4
A happy cat <acasadotorres@gmail.com>
2026-03-02 15:17:14 +0100

    onsave spec

--- app/react/V2/Routes/Entity/Components/ReferencesPanel/specs/CreateReference.cy.tsx
+++ app/react/V2/Routes/Entity/Components/ReferencesPanel/specs/CreateReference.cy.tsx
@@ -3,6 +3,7 @@ import 'cypress-axe';
 import { mount } from '@cypress/react18';
 import { composeStories } from '@storybook/react';
 import * as stories from 'app/stories/CreateReference.stories';
+import { CreateReference } from 'V2/Routes/Entity/Components/ReferencesPanel/CreateReference';
 import { logA11yViolations } from '../../../../../../../../cypress/support/helpers/a11y.js';
 
 const { Default, EmptyRelationshipTypes, LongSelection, TextMode } = composeStories(stories);
@@ -36,6 +37,59 @@ describe('CreateReference', () => {
     mount(<LongSelection />);
     cy.contains('button', 'Cancel').should('be.visible');
     cy.contains('button', 'Save').should('be.visible');
+  });
+
+  it('should call onSave when in entity mode', () => {
+    const onSave = cy.stub().as('onSave');
+    const mockEntities = [
+      {
+        _id: 'entity-1',
+        sharedId: 'shared-entity-1',
+        title: 'Simple entity',
+        template: { _id: 'template-1', name: 'Template', label: 'Template', color: '#A4CAFE' },
+        metadata: [],
+      },
+    ] as any[];
+
+    const searchFunction = cy
+      .stub()
+      .as('searchFunction')
+      .callsFake(() => Promise.resolve(mockEntities));
+
+    const selection = {
+      text: 'Selected text',
+      selectionRectangles: [{ top: 0, left: 0, width: 10, height: 10, regionId: '1' }],
+    } as any;
+
+    const relationshipTypes = [{ _id: 'rel-1', name: 'Related to' }] as any[];
+
+    mount(
+      <div className="tw-content" style={{ width: '400px', height: '900px' }}>
+        <CreateReference
+          selection={selection}
+          relationshipTypes={relationshipTypes}
+          searchFunction={searchFunction}
+          mode="entity"
+          onSave={onSave}
+        />
+      </div>
+    );
+
+    cy.get('#entity-search').type('Simple');
+
+    cy.get('@searchFunction').should('have.been.called');
+    cy.contains('Simple entity').click();
+    cy.contains('Related to').click();
+
+    cy.contains('button', 'Save').click();
+
+    cy.get('@onSave').should('have.been.calledOnce');
+    cy.get('@onSave')
+      .its('firstCall.args.0')
+      .should('include', {
+        targetEntityId: 'shared-entity-1',
+        relationshipType: 'rel-1',
+      });
   });
 
   it('should render in text mode', () => {
