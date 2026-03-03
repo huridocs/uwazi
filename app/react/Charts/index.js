0d057c578fd6c410ec35e5ff88d2f02fa13127d0
mfacar <mercy@huridocs.org>
2026-03-03 08:36:58 -0500

    using resolveComponent for charts

--- app/react/Charts/index.js
+++ app/react/Charts/index.js
@@ -4,23 +4,25 @@ import { colorScheme } from './utils/colorScheme.js';
 import arrayUtils from './utils/arrayUtils.js';
 import { ExtendedTooltip } from './components/ExtendedTooltip.js';
 
-const Bar = loadable(
-  async () => import(/* webpackChunkName: "LazyLoadBar" */ './components/Bar.js')
-);
+const Bar = loadable(() => import(/* webpackChunkName: "LazyLoadBar" */ './components/Bar.js'), {
+  resolveComponent: m => m.Bar,
+});
 
 const ColoredBar = loadable(
-  async () => import(/* webpackChunkName: "LazyLoadColoredBar" */ './components/ColoredBar.js')
+  () => import(/* webpackChunkName: "LazyLoadColoredBar" */ './components/ColoredBar.js'),
+  { resolveComponent: m => m.ColoredBar }
 );
 
-const Pie = loadable(
-  async () => import(/* webpackChunkName: "LazyLoadPie" */ './components/Pie.js')
-);
+const Pie = loadable(() => import(/* webpackChunkName: "LazyLoadPie" */ './components/Pie.js'), {
+  resolveComponent: m => m.RechartsPie,
+});
 
 const StackedDualBarChart = loadable(
-  async () =>
+  () =>
     import(
       /* webpackChunkName: "LazyLoadStackedDualBarChart" */ './components/StackedDualBarChart.js'
-    )
+    ),
+  { resolveComponent: m => m.StackedDualBarChart }
 );
 
 export { Bar, ColoredBar, ExtendedTooltip, Pie, StackedDualBarChart, colorScheme, arrayUtils };
