400963b3025c1a33ea9cf2430ef00a39c13985f8
Santiago <borrajosantiago@gmail.com>
2026-03-16 16:54:36 -0300

    implement type safety for event bus

--- app/react/V2/Components/PDFViewer/pdfjs.ts
+++ app/react/V2/Components/PDFViewer/pdfjs.ts
@@ -5,7 +5,7 @@ import 'pdfjs-dist/web/pdf_viewer.css';
 
 let pdfjs = pdfJsDist;
 const PDFJSViewer = viewer;
-const { EventBus } = viewer;
+const { EventBus: PDFJSEventBus } = viewer;
 const CMAP_URL = 'legacy_character_maps/';
 
 const pdfjsLoader = async () => {
@@ -25,5 +25,26 @@ await pdfjsLoader();
 
 const PDFJS = pdfjs;
 
-export type { PDFDocumentProxy };
+type PDFEventMap = {
+  pageready: { pageNumber: number };
+  pagerendered: { pageNumber: number };
+  renderpage: { pageNumber: number };
+  unmountpage: { pageNumber: number };
+};
+
+class EventBus extends PDFJSEventBus {
+  on<K extends keyof PDFEventMap>(eventName: K, listener: (data: PDFEventMap[K]) => void): void {
+    super.on(eventName as string, listener as (data: object) => void);
+  }
+
+  off<K extends keyof PDFEventMap>(eventName: K, listener: (data: PDFEventMap[K]) => void): void {
+    super.off(eventName as string, listener as (data: object) => void);
+  }
+
+  dispatch<K extends keyof PDFEventMap>(eventName: K, data: PDFEventMap[K]): void {
+    super.dispatch(eventName as string, data);
+  }
+}
+
+export type { PDFDocumentProxy, PDFEventMap };
 export { PDFJS, PDFJSViewer, EventBus, CMAP_URL };
