254624c60aaf005f74631324d3dd62404021d75d
Santiago <borrajosantiago@gmail.com>
2026-03-23 19:24:07 -0300

    test fixes

--- cypress/e2e/base/main-document-and-references.cy.ts
+++ cypress/e2e/base/main-document-and-references.cy.ts
@@ -213,14 +213,13 @@ describe('Entity with main documents', () => {
     cy.contains('.item-document', 'Entity with main documents').contains('View').click();
     cy.contains('span', 'La Sentencia de fondo').should('be.visible');
     cy.contains('span', 'La Sentencia de fondo').realClick({ clickCount: 3 });
-    cy.get('.fa-file', { timeout: 5000 }).then(() => {
-      cy.get('.fa-file').realClick();
-    });
+    cy.get('.fa-file', { timeout: 5000 }).should('be.visible');
+    cy.get('.fa-file').realClick();
     cy.contains('.create-reference', 'Relacionado a').should('be.visible');
     cy.contains('li.multiselectItem', 'Relacionado a').realClick();
-    cy.get('aside.create-reference input').type('Patrick Robinson', { timeout: 5000 });
-    cy.contains('Tracy Robinson', { timeout: 5000 });
-    cy.contains('.item-name', 'Patrick Robinson', { timeout: 5000 }).realClick();
+    cy.get('aside.create-reference input').type('Patrick Robinson');
+    cy.contains('.item-name', 'Tracy Robinson');
+    cy.contains('.item-name', 'Patrick Robinson').realClick();
     cy.contains('aside.create-reference .btn-success', 'Save', { timeout: 5000 }).click({
       timeout: 5000,
     });
