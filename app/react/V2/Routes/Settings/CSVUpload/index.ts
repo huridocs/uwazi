b5e7787ffe5980f8025fc0ea19a79758c557b571
Santiago <borrajosantiago@gmail.com>
2026-04-13 15:40:59 -0300

    fix import

--- app/react/V2/Routes/Settings/CSVUpload/index.ts
+++ app/react/V2/Routes/Settings/CSVUpload/index.ts
@@ -1,4 +1,4 @@
-export { CSVList } from './CSVList.jsx';
+export { CSVList } from './CSVList.js';
 export { csvListLoader } from './Loaders/csvListLoader.js';
 export { UploadStatus } from './UploadStatus.js';
 export { uploadStatusLoader } from './Loaders/uploadStatusLoader.js';
