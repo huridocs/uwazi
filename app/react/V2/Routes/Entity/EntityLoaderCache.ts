40dbc3a637d076c8de4442ca90ea855d7350cda9
Santiago <borrajosantiago@gmail.com>
2026-04-30 13:37:30 -0300

    update v2 entity loader

--- app/react/V2/Routes/Entity/EntityLoaderCache.ts
+++ app/react/V2/Routes/Entity/EntityLoaderCache.ts
@@ -1,5 +1,5 @@
-import { Entity } from '#V2/domain/index.js';
 import { SnippetsSearchResponse } from '#V2/api/types.js';
+import { Entity } from '#V2/api/entities/types.js';
 import { isClient } from '#app/utils/index.js';
 
 interface CachedItem<T> {
--- app/react/V2/Routes/Entity/types.ts
+++ app/react/V2/Routes/Entity/types.ts
@@ -1,8 +1,8 @@
+import { Entity } from '#V2/api/entities/types.js';
 import { SnippetsSearchResponse } from '#V2/api/types.js';
-import { Entity } from '#V2/domain/entities/Entity.js';
 
 type LoaderResponse =
-  | { entity: Entity; pagePlaintext?: string; searchResults?: SnippetsSearchResponse }
+  | { entity?: Entity; pagePlaintext?: string; searchResults?: SnippetsSearchResponse }
   | undefined;
 
 export type { LoaderResponse };
