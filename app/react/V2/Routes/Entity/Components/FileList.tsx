4767760e776bea0b21175e24104e835b929bc3c5
Alberto Casado Torres <konzz@users.noreply.github.com>
2026-05-12 13:08:52 +0200

    entities in library no longer calculating preview (#9081)
    * entities in library no longer calculating preview
    
    * updated specs
    
    * using entity from props
    
    * updated spec

--- app/react/V2/Routes/Entity/Components/FileList.tsx
+++ app/react/V2/Routes/Entity/Components/FileList.tsx
@@ -3,6 +3,7 @@ import { useAtomValue } from 'jotai';
 import { Translate } from '#app/I18N/index.js';
 import { EntityFile, FileCard } from '#V2/Components/UI/Files/FileCard.js';
 import { Entity } from '#V2/api/entities/types.js';
+import { settingsAtom } from '#V2/atoms/settingsAtom.js';
 import { templatesAtom } from '#V2/atoms/templatesAtom.js';
 import { localeAtom } from '#V2/atoms/translationsAtoms.js';
 import { formatEntityFiles } from '#V2/formatters/index.js';
@@ -14,14 +15,16 @@ type FileListProps = {
 const FileList = ({ entity }: FileListProps) => {
   const templates = useAtomValue(templatesAtom);
   const locale = useAtomValue(localeAtom);
+  const settings = useAtomValue(settingsAtom);
+  const defaultLanguage = settings?.languages?.find(l => l.default)?.key;
 
   const files: EntityFile[] = useMemo(
     () =>
-      formatEntityFiles(entity, templates, locale).map(({ file, fileType }) => ({
+      formatEntityFiles(entity, templates, locale, defaultLanguage).map(({ file, fileType }) => ({
         ...file,
         fileType,
       })),
-    [entity, locale, templates]
+    [defaultLanguage, entity, locale, templates]
   );
 
   if (files.length === 0) {
