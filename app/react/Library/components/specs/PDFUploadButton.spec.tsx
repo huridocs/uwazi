12726a043b3de6340bd32bfd275a63d9aa779af0
Santiago <71732018+Zasa-san@users.noreply.github.com>
2026-06-04 09:06:54 -0300

    8951 - Create from PDF client integration (#9175)
    * basic behavior with current UI
    
    * overhaul action and styles
    
    * small fixes
    
    * test update
    
    * use uploadService + better reporting

--- app/react/Library/components/specs/PDFUploadButton.spec.tsx
+++ app/react/Library/components/specs/PDFUploadButton.spec.tsx
@@ -3,14 +3,12 @@
  */
 import React from 'react';
 import { act, fireEvent, screen } from '@testing-library/react';
-import * as IDGenerator from '#shared/IDGenerator.js';
 import { renderConnectedContainer, defaultState } from '#app/utils/test/renderConnected.js';
 import * as uploadActions from '#app/Uploads/actions/uploadsActions.js';
-import * as libraryActions from '#app/Library/actions/libraryActions.js';
 import { TestAtomStoreProvider } from '#V2/testing/index.js';
 import { templatesAtom } from '#V2/atoms/index.js';
 import { PDFUploadButton } from '../PDFUploadButton.js';
-import { template1, template2, template3 } from './fixtures/templates.js';
+import { template1, template2 } from './fixtures/templates.js';
 
 describe('PDFUploadButton', () => {
   const files = [new File([], 'file1 with some name.pdf'), new File([], 'file2-pdf.pdf')];
@@ -24,15 +22,15 @@ describe('PDFUploadButton', () => {
     );
 
   beforeEach(() => {
-    spyOn(uploadActions, 'createDocument').and.returnValue(async () =>
-      Promise.resolve({ sharedId: 'abc1' })
-    );
-    spyOn(uploadActions, 'uploadDocument').and.returnValue(async () => Promise.resolve());
-    spyOn(libraryActions, 'unselectAllDocuments').and.returnValue(async () => Promise.resolve());
-    spyOn(IDGenerator, 'generateID').and.returnValue('generatedID');
+    spyOn(uploadActions, 'uploadAndCreate').and.callFake((batch: File[], onProgress: any) => {
+      if (onProgress && batch[0]) {
+        onProgress(10, batch[0].name);
+      }
+      return async () => Promise.resolve({ sharedId: 'abc1' });
+    });
   });
 
-  it('should upload all documents passed and unselect everything to close the sidebar', async () => {
+  it('should trigger the upload process', async () => {
     render();
     const fileInput = screen.getByLabelText('Upload PDF(s) to create');
 
@@ -42,29 +40,20 @@ describe('PDFUploadButton', () => {
       });
     });
 
-    expect(uploadActions.createDocument).toHaveBeenNthCalledWith(1, {
-      title: 'File1 with some name',
-    });
-    expect(uploadActions.createDocument).toHaveBeenNthCalledWith(2, { title: 'File2 pdf' });
-    expect(uploadActions.uploadDocument).toHaveBeenNthCalledWith(1, 'abc1', files[0]);
-    expect(uploadActions.uploadDocument).toHaveBeenNthCalledWith(2, 'abc1', files[1]);
-    expect(libraryActions.unselectAllDocuments).toHaveBeenCalledTimes(1);
-  });
+    expect(uploadActions.uploadAndCreate).toHaveBeenCalledTimes(1);
 
-  it('should use generated id when applicable', async () => {
-    (uploadActions.createDocument as jasmine.Spy).calls.reset();
-    render([template2, template3]);
-    const fileInput = screen.getByLabelText('Upload PDF(s) to create');
+    expect(uploadActions.uploadAndCreate).toHaveBeenCalledWith(
+      files,
+      expect.any(Function),
+      expect.any(Function)
+    );
 
-    await act(async () => {
-      fireEvent.change(fileInput, {
-        target: { files },
-      });
-    });
+    const { calls } = (uploadActions.uploadAndCreate as jest.Mock).mock;
+    expect(calls[0][0][0].name).toBe(files[0].name);
+    expect(calls[0][0][1].name).toBe(files[1].name);
 
-    expect(uploadActions.createDocument).toHaveBeenNthCalledWith(1, {
-      title: 'generatedID',
-    });
-    expect(uploadActions.createDocument).toHaveBeenNthCalledWith(2, { title: 'generatedID' });
+    const inputEl = fileInput as HTMLInputElement;
+    expect(inputEl.value).toBe('');
+    expect(inputEl.files).toBeNull();
   });
 });
