cc5c5f9b657d6497b9f3575573881e05fd2092c2
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2026-06-16 09:45:48 -0400

    remove unnecessary type casting on files dao factory

--- app/api/core/infrastructure/factories/FilesDAOFactory.ts
+++ app/api/core/infrastructure/factories/FilesDAOFactory.ts
@@ -1,13 +1,12 @@
 import { getConnection } from '../mongodb/common/getConnectionForCurrentTenant.js';
 import { MongoFilesDAO } from '../mongodb/files/MongoFilesDAO.js';
-import { MongoTransactionManager } from '../mongodb/common/MongoTransactionManager.js';
 import { TransactionManagerFactory } from './TransactionManagerFactory.js';
 
 class FilesDAOFactory {
   static default(): MongoFilesDAO {
     return new MongoFilesDAO({
       db: getConnection(),
-      transactionManager: TransactionManagerFactory.default() as MongoTransactionManager,
+      transactionManager: TransactionManagerFactory.default(),
     });
   }
 }
