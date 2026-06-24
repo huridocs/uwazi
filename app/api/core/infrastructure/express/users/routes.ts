7fe2ab87635895e7771136cfa430c89ec3498af1
Santiago <borrajosantiago@gmail.com>
2026-06-24 12:15:49 -0300

    fix feature flag interference with tests

--- app/api/core/infrastructure/express/users/routes.ts
+++ app/api/core/infrastructure/express/users/routes.ts
@@ -1,22 +1,29 @@
-import type { Application } from 'express';
+import type { Application, NextFunction, Request, Response } from 'express';
 import { validatePasswordMiddleWare, needsAuthorization } from '#api/auth/index.js';
 import { CreateUserController } from './CreateUserController.js';
 import { validation } from '#api/utils/index.js';
 import { userSchema } from '#shared/types/userSchema.js';
+import { tenants } from '#api/tenants/index.js';
 
 export const userRoutes = (app: Application) => {
   app.post(
     '/api/users/new',
     needsAuthorization(),
     validatePasswordMiddleWare,
-    // for legacy reasons, should be removed
-    validation.validateRequest({
-      type: 'object',
-      properties: {
-        body: userSchema,
-      },
-      required: ['body'],
-    }),
+    async (req: Request, res: Response, next: NextFunction) => {
+      // for legacy reasons, should be removed one the flag is gone
+      if (tenants.current().featureFlags?.v2CreateUser) {
+        next();
+      } else {
+        await validation.validateRequest({
+          type: 'object',
+          properties: {
+            body: userSchema,
+          },
+          required: ['body'],
+        })(req, res, next);
+      }
+    },
     CreateUserController.createHandler()
   );
 };
