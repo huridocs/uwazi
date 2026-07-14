5e2ee1935e4e312c9a8b99d12fab07d2e7878e81
Joan Gallego Girona <daneryl@gmail.com>
2026-07-14 12:02:08 +0200

    Add per-tenant maintenance mode toggled during migration reindex (#9353)

--- app/api/tenants/tenantsModel.ts
+++ app/api/tenants/tenantsModel.ts
@@ -52,6 +52,7 @@ const mongoSchema = new mongoose.Schema({
   },
   globalMatomo: { id: String, url: String },
   ciMatomoActive: Boolean,
+  maintenance: Boolean,
 });
 
 type DBTenant = Partial<Tenant> & { name: string };
@@ -143,6 +144,15 @@ class TenantsModel extends EventEmitter {
       );
     }
     return this.model.find({}, Object.keys(mongoSchema.paths)).lean();
+  }
+
+  async setMaintenance(tenantName: string, maintenance: boolean) {
+    if (!this.model) {
+      throw new Error(
+        'tenants model has not been initialized, make sure you called initialize() method'
+      );
+    }
+    await this.model.updateOne({ name: tenantName }, { $set: { maintenance } });
   }
 }
 
