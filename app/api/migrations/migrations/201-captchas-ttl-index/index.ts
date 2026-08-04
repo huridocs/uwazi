fbeda3fca741886efd97973930348d300e17c997
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2026-08-04 13:20:59 -0400

    create ttl index for captcha collection

---
+++ app/api/migrations/migrations/201-captchas-ttl-index/index.ts
@@ -0,0 +1,21 @@
+import { Db } from 'mongodb';
+
+export default {
+  delta: 201,
+
+  reindex: false,
+
+  name: '201-captchas-ttl-index',
+
+  description: 'Creates a TTL index on captchas.createdAt so captchas auto-expire after 10 hours.',
+
+  async up(db: Db) {
+    process.stdout.write(`${this.name}...\r\n`);
+
+    await db
+      .collection('captchas')
+      .createIndex({ createdAt: 1 }, { expireAfterSeconds: 36000 });
+
+    process.stdout.write(`${this.name}: index created.\r\n`);
+  },
+};
