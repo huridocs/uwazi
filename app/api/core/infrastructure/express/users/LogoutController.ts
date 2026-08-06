caccd5bfb43ea36cee94d18993bff4ea4251a348
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2026-08-06 10:38:17 -0400

    add logging to feature flags

--- app/api/core/infrastructure/express/users/LogoutController.ts
+++ app/api/core/infrastructure/express/users/LogoutController.ts
@@ -3,15 +3,30 @@ import { ExecutionContext } from '#api/core/libs/ExecutionContext.js';
 
 class LogoutController extends AbstractController {
   protected async handle(): Promise<void> {
-    const userId = this.user.isAnonymous() ? undefined : this.user._id;
+    try {
+      const userId = this.user.isAnonymous() ? undefined : this.user._id;
 
-    ExecutionContext.logger.info('User logged out', {
-      namespace: 'Auth',
-      userId,
-    });
+      ExecutionContext.logger.info('User logged out', {
+        namespace: 'Auth_Logout',
+        success: true,
+        userId,
+      });
 
-    this.request.session.destroy(() => {});
-    this.response.redirect('/');
+      this.request.session.destroy(() => {});
+      this.response.redirect('/');
+    } catch (error: unknown) {
+      ExecutionContext.logger.info(
+        `Logout failed: ${error instanceof Error ? error.message : 'Unknown error'}`,
+        {
+          namespace: 'Auth_Logout',
+          success: false,
+          error: JSON.stringify(error),
+          notify: true,
+        }
+      );
+
+      throw error;
+    }
   }
 }
 
