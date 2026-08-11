fc1a03191a4fd9dbdf1bb66c3a28e75a3e94f5d5
Joao Rodrigues <joaovictor.go.ro@gmail.com>
2026-08-11 13:11:39 -0400

    add postgres users query service to factory

--- app/api/core/infrastructure/factories/UsersQueryServiceFactory.ts
+++ app/api/core/infrastructure/factories/UsersQueryServiceFactory.ts
@@ -1,10 +1,24 @@
+import { ExecutionContext } from '#api/core/libs/ExecutionContext.js';
 import { getConnection } from '../mongodb/common/getConnectionForCurrentTenant.js';
 import { MongoUsersQueryService } from '../mongodb/user/MongoUsersQueryService.js';
+import { PostgresUserGroupsDAO } from '../postgresql/user/PostgresUserGroupsDAO.js';
+import { PostgresUsersDAO } from '../postgresql/user/PostgresUsersDAO.js';
+import { PostgresUsersQueryService } from '../postgresql/user/PostgresUsersQueryService.js';
 import { TransactionManagerFactory } from './TransactionManagerFactory.js';
+import { UserGroupsDAOFactory } from './UserGroupsDAOFactory.js';
 import { UsersDAOFactory } from './UsersDAOFactory.js';
 
 class UsersQueryServiceFactory {
   static default(): MongoUsersQueryService {
+    const tenant = ExecutionContext.currentTenant;
+
+    if (tenant.featureFlags?.postgresUsers && tenant.featureFlags?.postgresUsergroups) {
+      return new PostgresUsersQueryService({
+        usersDAO: UsersDAOFactory.default() as any as PostgresUsersDAO,
+        userGroupsDAO: UserGroupsDAOFactory.default() as PostgresUserGroupsDAO,
+      }) as any as MongoUsersQueryService;
+    }
+
     return new MongoUsersQueryService({
       db: getConnection(),
       transactionManager: TransactionManagerFactory.default(),
