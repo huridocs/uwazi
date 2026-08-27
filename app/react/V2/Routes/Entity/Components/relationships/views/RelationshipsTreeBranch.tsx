d21358efddea13a142393063164aea4c8c5e221b
mfacar <mercy@huridocs.org>
2026-08-27 11:52:58 -0500

    fixes from dependabot

--- app/react/V2/Routes/Entity/Components/relationships/views/RelationshipsTreeBranch.tsx
+++ app/react/V2/Routes/Entity/Components/relationships/views/RelationshipsTreeBranch.tsx
@@ -89,7 +89,7 @@ const RelationshipsTreeBranch = ({
         count={count}
       />
       {expanded && items.length > 0 && (
-        <div className="ml-[14px]">
+        <div className="ml-3.5">
           {items.map((child, index) => {
             const line = getTreeLine(index, items.length);
             if (
